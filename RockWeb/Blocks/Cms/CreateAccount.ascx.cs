6cc3392311691dd66015fb0001487f9c5e8e9617
edmistj <jon@voracitysolutions.com>
2011-05-06 18:30:50 -0700

    Updates for the RockWeb project. - Removed the Facebook API for later inclusion in a dev branch - Add custom dlls in the /bin folder - Copied uncommitted changes from the Rock.Web project

--- RockWeb/Blocks/Cms/CreateAccount.ascx.cs
+++ RockWeb/Blocks/Cms/CreateAccount.ascx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Facebook;
 using Rock.Models.Cms;
 using Rock.Repository.Cms;
 
@@ -12,7 +11,6 @@ namespace RockWeb.Blocks.Cms
 
         protected void Page_Init( object sender, EventArgs e )
         {
-            fbSubmit.Click += fbSubmit_Click;
             string clientId = cuWizard.CreateUserStep.ContentTemplateContainer.FindControl( "UserName" ).ClientID;
             string jqScript = @"
     $(document).ready(function () {
@@ -48,56 +46,10 @@ namespace RockWeb.Blocks.Cms
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.QueryString["code"] != null)
-            {
-                ShowView(Request.QueryString["code"]);
-            }
-
             _returnUrl = Request.QueryString["returnurl"];
         }
 
-        protected void fbRegister_Click(object sender, EventArgs e)
-        {
-            var oAuthClient = new FacebookOAuthClient(FacebookApplication.Current) { RedirectUri = new Uri(GetCreateAccountUrl()) };
-
-            // Get user permission. Basic permissions are all that's needed.
-            var loginUri = oAuthClient.GetLoginUrl(new Dictionary<string, object> { { "state", _returnUrl } });
-            Response.Redirect(loginUri.AbsoluteUri);
-        }
-
-        protected void fbSubmit_Click(object sender, EventArgs e)
-        {
-            var userRepository = new EntityUserRepository();
-            var user = new User
-                           {
-                               Username = fbUserName.Text,
-                               FacebookId = fbFacebookId.Value
-                           };
-
-            //userRepository.Add(user);
-            userRepository.Save(user, null);
-        }
-
-        private void ShowView(string code)
-        {
-            FacebookOAuthResult oAuthResult;
-
-            if (FacebookOAuthResult.TryParse(Request.Url, out oAuthResult))
-            {
-                if (oAuthResult.IsSuccess)
-                {
-                    var oAuthClient = new FacebookOAuthClient(FacebookApplication.Current) { RedirectUri = new Uri(GetCreateAccountUrl()) };
-                    dynamic tokenResult = oAuthClient.ExchangeCodeForAccessToken(code);
-                    string accessToken = tokenResult.access_token;
-
-                    FacebookClient fbClient = new FacebookClient(accessToken);
-                    dynamic me = fbClient.Get("me");
-                    fbFacebookId.Value = me.id;
-                }
-            }
-
-            // TODO: Show user feedback indicating facebook access was not successful
-        }
+        
 
         private string GetCreateAccountUrl()
         {
