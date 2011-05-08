1ef21ee09f365959ceff507b19a03d11300a9504
edmistj <jon@voracitysolutions.com>
2011-05-07 22:08:03 -0700

    Blog Updates CSS Template Updates

--- Rock.Framework/Helpers/HtmlHelper.cs
+++ Rock.Framework/Helpers/HtmlHelper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 
 using System.Web;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace Rock.Helpers
 {
@@ -22,5 +24,27 @@ namespace Rock.Helpers
 
             return value;
         }
+
+        /// <summary>
+        /// Hashes a string using MD5
+        /// </summary>
+        /// <param name="value">String to format</param>
+        /// <returns></returns>
+        public static string CalculateMD5Hash( string input )
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes( input );
+            byte[] hash = md5.ComputeHash( inputBytes );
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for ( int i = 0; i < hash.Length; i++ )
+            {
+                sb.Append( hash[i].ToString( "X2" ) );
+            }
+            return sb.ToString();
+        }
+
     }
 }
