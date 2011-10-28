0e1c155e271fb8e3e4779ab89c694d1903fdf624
edmistj <jonathan.edmiston@gmail.com>
2011-10-28 16:52:39 -0700

    Added Job's model, repository and services.

--- Rock.Framework/EntityFramework/RockContext.cs
+++ Rock.Framework/EntityFramework/RockContext.cs
@@ -50,6 +50,7 @@ namespace Rock.EntityFramework
         public DbSet<Rock.Models.Groups.GroupRole> GroupRoles { get; set; }
         public DbSet<Rock.Models.Groups.GroupType> GroupTypes { get; set; }
         public DbSet<Rock.Models.Groups.Member> Members { get; set; }
+        public DbSet<Rock.Models.Util.Jobs> Jobss { get; set; }
 
         protected override void OnModelCreating( DbModelBuilder modelBuilder )
         {
@@ -81,6 +82,7 @@ namespace Rock.EntityFramework
             modelBuilder.Configurations.Add( new Rock.Models.Groups.GroupRoleConfiguration() );
             modelBuilder.Configurations.Add( new Rock.Models.Groups.GroupTypeConfiguration() );
             modelBuilder.Configurations.Add( new Rock.Models.Groups.MemberConfiguration() );
+            modelBuilder.Configurations.Add( new Rock.Models.Util.JobsConfiguration() );
 		}
     }
 }
