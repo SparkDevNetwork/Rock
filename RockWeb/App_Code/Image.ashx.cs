393348ecc1a4dba37f573f8c3288a99e47953822
Nick Airdo <nairdo@gmail.com>
2011-11-07 00:30:34 -0700

    now caches

--- RockWeb/App_Code/Image.ashx.cs
+++ RockWeb/App_Code/Image.ashx.cs
@@ -30,7 +30,7 @@ namespace RockWeb
 
         public void ProcessRequest( HttpContext context )
         {
-			if ( context.Request.QueryString == null )
+			if ( context.Request.QueryString == null || context.Request.QueryString.Count == 0)
 			{
 				return;
 			}
@@ -44,25 +44,63 @@ namespace RockWeb
 			// Fetch the file...
 			Rock.Models.Cms.File file = ( int.TryParse( anID, out id ) ) ? fileService.GetFile( id ) : fileService.GetByGuid( anID );
 
+			// is it cached?
+			string cacheName = Uri.EscapeDataString( context.Request.Url.Query );
+			string physFilePath = context.Request.MapPath( string.Format( "~/cache/{0}", cacheName ) );
+			bool cached = FetchFromCache( file, physFilePath );
+
 			// Image resizing requested?
-			if ( WantsImageResizing( context ) )
+			if ( ! cached && WantsImageResizing( context ) )
 			{
-				ResizeSettings settings = new ResizeSettings(context.Request.QueryString);
-				MemoryStream x = new MemoryStream( file.Data );
-				Bitmap bi = new Bitmap( x );
-				MemoryStream resizedStream = new MemoryStream();
-				ImageBuilder.Current.Build( new MemoryStream( file.Data ), resizedStream, settings);
-				file.Data = resizedStream.GetBuffer();
+				ResizeAndCache( context, file, physFilePath );
 			}
 
 			// Post process
 			SendFile( context, file );
         }
 
+		private static void ResizeAndCache( HttpContext context, Rock.Models.Cms.File file, string physFilePath )
+		{
+			ResizeSettings settings = new ResizeSettings( context.Request.QueryString );
+			MemoryStream resizedStream = new MemoryStream();
+			ImageBuilder.Current.Build( new MemoryStream( file.Data ), resizedStream, settings );
+			file.Data = resizedStream.GetBuffer();
+
+			// now cache the file for later use
+			try
+			{
+				using ( BinaryWriter binWriter = new BinaryWriter( System.IO.File.Open( physFilePath, FileMode.Create ) ) )
+				{
+					binWriter.Write( file.Data );
+				}
+			}
+			catch { /* do nothing, not critical if this fails, although TODO: log */ }
+		}
+
+		private static bool FetchFromCache( Rock.Models.Cms.File file, string physFilePath )
+		{
+			bool cached = false;
+			if ( System.IO.File.Exists( physFilePath ) && file.CreatedDateTime < System.IO.File.GetCreationTime( physFilePath ) )
+			{
+				try
+				{
+					using ( BinaryReader binReader = new BinaryReader( System.IO.File.Open( physFilePath, FileMode.Open, FileAccess.Read, System.IO.FileShare.Read ) ) )
+					{
+						file.Data = System.IO.File.ReadAllBytes( physFilePath );
+					}
+					cached = true;
+				}
+				catch { /* ok, so we'll just skip using the cache, but TODO: log this */}
+
+			}
+			return cached;
+		}
+
 		private bool WantsImageResizing( HttpContext context )
 		{
 			NameValueCollection nvc = context.Request.QueryString;
-			return ( nvc["maxwidth"] != null || nvc["maxheight"] != null || nvc["width"] != null || nvc["height"] != null );
+			return ( nvc["maxwidth"] != null || nvc["maxheight"] != null || nvc["width"] != null ||
+				nvc["height"] != null || nvc["rotate"] != null || nvc["scale"] != null );
 		}
 
 		private static void SendFile( HttpContext context, Rock.Models.Cms.File file )
@@ -83,7 +121,7 @@ namespace RockWeb
         }
 
 		/// <summary>
-		/// Renders an Error image
+		/// Utility method to renders an error image.  Not sure if I like this idea.
 		/// </summary>
 		/// <param name="context">HttpContext of current request</param>
 		/// <param name="errorMessage">error message text to render</param>
