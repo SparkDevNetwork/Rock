4cd1108951a516dc374fba8c53158c0c29008472
David Turner <david@azturners.com>
2011-12-13 05:12:25 -0700

    Update Error/Help rendering/css

--- Rock/Web/UI/Controls/LabeledTextBox.cs
+++ Rock/Web/UI/Controls/LabeledTextBox.cs
@@ -17,6 +17,56 @@ namespace Rock.Web.UI.Controls
     public class LabeledTextBox : TextBox
     {
         /// <summary>
+        /// Gets or sets the help tip.
+        /// </summary>
+        /// <value>
+        /// The help tip.
+        /// </value>
+        [
+        Bindable( true ),
+        Category( "Appearance" ),
+        DefaultValue( "" ),
+        Description( "The help tip." )
+        ]
+        public string Tip
+        {
+            get
+            {
+                string s = ViewState["Tip"] as string;
+                return s == null ? string.Empty : s;
+            }
+            set
+            {
+                ViewState["Tip"] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the help block.
+        /// </summary>
+        /// <value>
+        /// The help block.
+        /// </value>
+        [
+        Bindable( true ),
+        Category( "Appearance" ),
+        DefaultValue( "" ),
+        Description( "The help block." )
+        ]
+        public string Help
+        {
+            get
+            {
+                string s = ViewState["Help"] as string;
+                return s == null ? string.Empty : s;
+            }
+            set
+            {
+                ViewState["Help"] = value;
+            }
+        }
+
+        /// <summary>
         /// Gets or sets the label text.
         /// </summary>
         /// <value>
@@ -47,9 +97,16 @@ namespace Rock.Web.UI.Controls
         /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter"/> that receives the rendered output.</param>
         protected override void Render( HtmlTextWriter writer )
         {
-            writer.Write( string.Format( @"<dt><label for=""{0}"">{1}</label></dt><dd>",this.ClientID, LabelText ) );
+            writer.Write( string.Format( @"<dl><dt><label for=""{0}"">{1}</label></dt><dd>", this.ClientID, LabelText ) );
             base.Render( writer );
-            writer.Write( @"</dd>" );
+
+            if ( Tip.Trim() != string.Empty )
+                writer.Write( string.Format( @"<a class=""help-tip"" href=""#"">help<span>{0}</span></a>", Tip.Trim() ) );
+
+            if ( Help.Trim() != string.Empty )
+                writer.Write( string.Format( @"<span class=""help-block"">{0}</span>", Help.Trim() ) );
+
+            writer.Write( @"</dd></dl>" );
         }
 
     }
