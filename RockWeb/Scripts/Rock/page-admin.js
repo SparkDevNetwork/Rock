ad9a36622b5989b363bf2b22f2549017073c330f
edmistj <jonathan.edmiston@gmail.com>
2012-01-20 13:30:10 -0700

    Zone admin tweaks

--- RockWeb/Scripts/Rock/page-admin.js
+++ RockWeb/Scripts/Rock/page-admin.js
@@ -162,6 +162,7 @@
     // of each block's container and config options
     $('#cms-admin-footer .block-config').click(function () {
         $('.zone-configuration').hide();
+        $('.zone-instance').removeClass('outline');
         $('.block-configuration').toggle();
         return false;
     });
@@ -170,13 +171,14 @@
     // of each zone's container and config options
     $('#cms-admin-footer .page-zones').click(function () {
         $('.block-configuration').hide();
+        $('.zone-instance').toggleClass('outline');
         $('.zone-configuration').toggle();
         return false;
     });
 
     // Bind the block configure icon so that edit icons are displayed on hover
     $(".block-configuration").hover(function () {
-        var barWidth = $('.block-configuration-bar').outerWidth() + 45 + 'px';
+        var barWidth = $('.block-configuration-bar', this).outerWidth() + 45 + 'px';
         $(this).stop().animate({ width: barWidth }, 200).css({ 'z-index': '10' });
     }, function () {
         $(this).stop().animate({ width: '24px' }, 200).css({ 'z-index': '1' });
@@ -184,7 +186,7 @@
 
     // Bind the zone configure icon so that edit icons are displayed on hover
     $(".zone-configuration").hover(function () {
-        var barWidth = $('.zone-configuration-bar').outerWidth() + 45 + 'px';
+        var barWidth = $('.zone-configuration-bar', this).width() + 45 + 'px';
         $(this).stop().animate({ width: barWidth }, 200).css({ 'z-index': '10' });
     }, function () {
         $(this).stop().animate({ width: '24px' }, 200).css({ 'z-index': '1' });
