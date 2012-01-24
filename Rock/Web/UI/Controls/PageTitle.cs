0cdcf1b2c3f494385e6df3297ceac5ae2ecbace3
David Turner <davidturner@ccvonline.com>
2012-01-24 15:05:09 -0700

    Added Zone and PageTitle controls to make it easier for web devs to create layouts.  Code behind is no longer needed for a layout

---
+++ Rock/Web/UI/Controls/PageTitle.cs
@@ -0,0 +1,15 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+namespace Rock.Web.UI.Controls
+{
+    /// <summary>
+    /// Renders the title of a page
+    /// </summary>
+    public class PageTitle : System.Web.UI.WebControls.Literal
+    {
+    }
+}
