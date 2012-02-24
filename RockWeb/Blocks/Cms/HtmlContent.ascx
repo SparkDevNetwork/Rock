3fd98094a124b7513c5a700c165df5bc46d45d0c
David Turner <davidturner@ccvonline.com>
2012-02-24 16:22:55 -0700

    Add versioning, approval, and active date support to HTML Content module

--- RockWeb/Blocks/Cms/HtmlContent.ascx
+++ RockWeb/Blocks/Cms/HtmlContent.ascx
@@ -1,105 +1,60 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="HtmlContent.ascx.cs" Inherits="RockWeb.Blocks.Cms.HtmlContent" %>
-<script type="text/javascript"> 
 
-    $(document).ready(function () {
+<asp:UpdatePanel runat="server" class="html-content-block">
+<ContentTemplate>
 
-        var ckoptionsBasic = {
-            langCode: 'en',
-            skin: 'kama',
-            width: 820,
-            resize_enabled: false,
-            startupFocus: true,
-            toolbar: [
-                        ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'],
-                        ['Bold', 'Italic', 'Underline'],
-                        ['Image', 'Link', 'Unlink', 'Anchor', 'Table'],
+    <asp:Literal ID="lPreText" runat="server"></asp:Literal><asp:Literal ID="lHtmlContent" runat="server"></asp:Literal><asp:Literal ID="lPostText" runat="server"></asp:Literal>
 
-                        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-                        ['NumberedList', 'BulletedList']
-            ]
-        };
+    <asp:PlaceHolder ID="phEditContent" runat="server" Visible="false">
 
-        var ckoptionsAdv = {
-            langCode: 'en',
-            skin: 'kama',
-            width: '100%',
-            resize_enabled: false,
-            startupFocus: true,
-            toolbar: [
-                        ['Source'], ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Find', 'Replace'],
-                        ['Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript'],
-                        ['Image', 'Link', 'Unlink', 'Anchor', 'Table', 'HorizontalRule'],
-
-                        ['Undo', 'Redo', '-', 'SelectAll', 'RemoveFormat'],
-                        ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
-                        ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv'],
-                        ['Format'],
-                        ['TextColor', 'BGColor'],
-                        ['Font', 'FontSize'], ['Maximize', 'ShowBlocks']
-            ]
-        };
-
-        $('#html-content-editor-<%=BlockInstance.Id %>').modal({
-            backdrop: true,
-            keyboard: true
-        });
-        
-        $('#html-content-editor-<%=BlockInstance.Id %>').bind('shown', function() {
-            $(this).appendTo($('form'));
-            $('#html-content-editor-<%=BlockInstance.Id %> textarea.html-content-editor').ckeditor(ckoptionsAdv).end();
-        });
-
-        $('#html-content-editor-<%=BlockInstance.Id %>').bind('hide', function () {
-            $('#html-content-editor-<%=BlockInstance.Id %>').find('textarea.html-content-editor').ckeditorGet().destroy();
-        });
-
-        $('#bid_<%=BlockInstance.Id %> .block-configuration a.edit').click(function (ev) {
-            $('#html-content-editor-<%=BlockInstance.Id %>').modal('show');
-            return false;
-        });
-    });
-
-    Sys.Application.add_load(function () {
-
-        $('#html-content-editor-<%=BlockInstance.Id %> .btn').click(function () {
-            $('#html-content-editor-<%=BlockInstance.Id %>').modal('hide');
-        });
-
-    });
-
-</script>
-
-<div id="html-content-view-<%=BlockInstance.Id %>" class="html-content-block">
-    
-    <asp:UpdatePanel ID="pnlContent" runat="server" UpdateMode="Conditional">
-        <Triggers>
-            <asp:AsyncPostBackTrigger ControlID="btnSaveContent" EventName="Click" />
-        </Triggers>
-        <ContentTemplate>
-            <asp:Literal ID="lPreText" runat="server"></asp:Literal><asp:Literal ID="lHtmlContent" runat="server"></asp:Literal><asp:Literal ID="lPostText" runat="server"></asp:Literal>
-        </ContentTemplate>
-    </asp:UpdatePanel>
-
-    <asp:UpdatePanel ID="upContentDialog" UpdateMode="Conditional" ChildrenAsTriggers="true" runat="server">
-    <ContentTemplate>
-
-        <div id="html-content-editor-<%=BlockInstance.Id %>"" class="modal hide fade">
+        <div id="html-content-editor-<%=BlockInstance.Id %>" class="modal hide fade">
             <div class="modal-header">
                 <a href="#" class="close">&times;</a>
                 <h3>HTML Content</h3>
             </div>
-            <div class="modal-body">
-                <asp:TextBox ID="txtHtmlContentEditor" CssClass="html-content-editor" TextMode="MultiLine" runat="server"></asp:TextBox>
+            <div id="html-content-versions-<%=BlockInstance.Id %>" style="display:none">
+                <Rock:Grid ID="rGrid" runat="server" AllowPaging="false" >
+                    <Columns>
+                        <asp:TemplateField SortExpression="Version" HeaderText="Version">
+                            <ItemTemplate>
+                                <a html-id='<%# Eval("Id") %>' class="html-content-show-version-<%=BlockInstance.Id %>" href="#"><%# Eval("Version") %></a>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:BoundField DataField="ModifiedDateTime" HeaderText="Modified" SortExpression="ModifiedDateTime" />
+                        <asp:BoundField DataField="ModifiedByPerson" HeaderText="By" SortExpression="ModifiedByPerson" />
+                        <Rock:BoolField DataField="Approved" HeaderText="Approved" SortExpression="Approved" />
+                        <asp:BoundField DataField="ApprovedByPerson" HeaderText="By" SortExpression="ApprovedByPerson" />
+                        <asp:BoundField DataField="StartDateTime" DataFormatString="MM/dd/yy" HeaderText="Start" SortExpression="StartDateTime" />
+                        <asp:BoundField DataField="ExpireDateTime" DataFormatString="MM/dd/yy" HeaderText="Expire" SortExpression="ExpireDateTime" />
+                    </Columns>
+                </Rock:Grid>
+                <div class="modal-footer">
+                    <button id="html-content-versions-cancel-<%=BlockInstance.Id %>" class="btn secondary">Cancel</button>
+                </div>
             </div>
-            <div class="modal-footer">
-                <input id="btnCancel" runat="server" type="button" class="btn secondary" value="Cancel" />
-                <asp:Button ID="btnSaveContent" runat="server" Text="Save" CssClass="btn primary" OnClick="btnSaveContent_Click" />
+            <div id="html-content-edit-<%=BlockInstance.Id %>">
+                <asp:Panel ID="pnlVersioningHeader" runat="server" class="html-content-edit-header">
+                    <asp:HiddenField ID="hfVersion" runat="server" />
+                    <a id="html-content-version-<%=BlockInstance.Id %>">Version <asp:Literal ID="lVersion" runat="server"></asp:Literal></a>
+                    Start: <asp:TextBox ID="tbStartDate" runat="server"></asp:TextBox>
+                    Expire: <asp:TextBox ID="tbExpireDate" runat="server"></asp:TextBox>
+                    <asp:CheckBox ID="cbApprove" runat="server" TextAlign="Left" Text="Approve" />
+                </asp:Panel>
+                <div class="modal-body">
+                    <asp:TextBox ID="txtHtmlContentEditor" CssClass="html-content-editor" TextMode="MultiLine" runat="server"></asp:TextBox>
+                </div>
+                <div class="modal-footer">
+                    <asp:CheckBox ID="cbOverwriteVersion" runat="server" TextAlign="Right" Text="don't save a new version" />
+                    <input id="btnCancel" runat="server" type="button" class="btn secondary" value="Cancel" />
+                    <asp:Button ID="btnSaveContent" runat="server" Text="Save" CssClass="btn primary" OnClick="btnSaveContent_Click" />
+                </div>
             </div>
         </div>
 
-    </ContentTemplate>
-    </asp:UpdatePanel>
+    </asp:PlaceHolder>
 
-</div>
+</ContentTemplate>
+</asp:UpdatePanel>
+
 
 
--- RockWeb/Blocks/Cms/HtmlContent.ascx.cs
+++ RockWeb/Blocks/Cms/HtmlContent.ascx.cs
@@ -6,113 +6,252 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
+using Rock;
 using Rock.CMS;
 
 namespace RockWeb.Blocks.Cms
 {
+    #region Properties
+
     [Rock.Security.AdditionalActions( new string[] { "Approve" } )]
     [Rock.Attribute.Property( 0, "Pre-Text", "PreText", "", "HTML text to render before the blocks main content.", false, "" )]
     [Rock.Attribute.Property( 1, "Post-Text", "PostText", "", "HTML text to render after the blocks main content.", false, "" )]
     [Rock.Attribute.Property( 2, "Cache Duration", "CacheDuration", "", "Number of seconds to cache the content.", false, "0", "Rock", "Rock.FieldTypes.Integer")]
-    [Rock.Attribute.Property( 3, "Entity Aware Key", "EntityAwareKey", "", "Query string key to use to 'personalize' the content to a particular entity.", false, "" )]
+    [Rock.Attribute.Property( 3, "Context Parameter", "ContextParameter", "", "Query string parameter to use for 'personalizing' content based on unique values.", false, "" )]
+    [Rock.Attribute.Property( 4, "Context Name", "ContextName", "", "Name to use to further 'personalize' content.  Blocks with the same name, and referenced with the same context parameter will share html values.", false, "" )]
+    [Rock.Attribute.Property( 5, "Support Versions", "Advanced", "Support content versioning?", false, "False", "Rock", "Rock.FieldTypes.Boolean" )]
+    [Rock.Attribute.Property( 6, "Require Approval", "Advanced", "Require that content be approved?", false, "False", "Rock", "Rock.FieldTypes.Boolean" )]
+
+    #endregion
+
     public partial class HtmlContent : Rock.Web.UI.Block
     {
-        public override List<Control> GetConfigurationControls( bool canConfig, bool canEdit)
+        #region Private Global Variables
+ 
+        bool _supportVersioning = false;
+        bool _requireApproval = false;
+
+        #endregion
+
+        #region Overriddend Block Methods
+
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            _supportVersioning = bool.Parse( AttributeValue( "SupportVersions" ) ?? "false" );
+            _requireApproval = bool.Parse( AttributeValue( "RequireApproval" ) ?? "false");
+
+            this.AttributesUpdated += HtmlContent_AttributesUpdated;
+
+            ShowView();
+
+            rGrid.DataKeyNames = new string[] { "id" };
+            rGrid.GridRebind += new Rock.Web.UI.Controls.GridRebindEventHandler( rGrid_GridRebind );
+            rGrid.ShowActionRow = false;
+        }
+
+        public override List<Control> GetConfigurationControls( bool canConfig, bool canEdit )
         {
             List<Control> configControls = new List<Control>();
 
             // add edit icon to config controls if user has edit permission
             if ( canConfig || canEdit )
             {
-                System.Web.UI.HtmlControls.HtmlGenericControl aAttributes = new System.Web.UI.HtmlControls.HtmlGenericControl( "a" );
-                aAttributes.Attributes.Add( "class", "edit icon-button" );
-                aAttributes.Attributes.Add( "href", "#" );
-                aAttributes.Attributes.Add( "title", "Edit HTML" );
-                configControls.Add( aAttributes );
+                LinkButton lbEdit = new LinkButton();
+                lbEdit.CssClass = "edit icon-button";
+                lbEdit.ToolTip = "Edit HTML";
+                lbEdit.Click += new EventHandler( lbEdit_Click );
+                configControls.Add( lbEdit );
             }
 
-            configControls.AddRange( base.GetConfigurationControls(canConfig, canEdit) );
+            configControls.AddRange( base.GetConfigurationControls( canConfig, canEdit ) );
 
             return configControls;
         }
 
-        protected void Page_Init( object sender, EventArgs e )
+        #endregion
+
+        #region Events
+
+        protected void lbEdit_Click( object sender, EventArgs e )
         {
-            if ( !IsPostBack )
+            phEditContent.Visible = true;
+
+            PageInstance.AddScriptLink( this.Page, "~/scripts/ckeditor/ckeditor.js" );
+            PageInstance.AddScriptLink( this.Page, "~/scripts/ckeditor/adapters/jquery.js" );
+            PageInstance.AddScriptLink( this.Page, "~/Scripts/Rock/htmlContentOptions.js" );
+
+            HtmlContentService service = new HtmlContentService();
+            Rock.CMS.HtmlContent content = service.GetActiveContent( BlockInstance.Id, EntityValue() );
+
+            string script = string.Format( @"
+
+    $(document).ready(function () {{
+
+        $('#html-content-editor-{0}').modal({{
+            show: true,
+            backdrop: true,
+            keyboard: true
+        }});
+
+        $('#html-content-editor-{0}').bind('shown', function () {{
+            $(this).appendTo($('form'));
+            $('#html-content-editor-{0} textarea.html-content-editor').ckeditor(ckoptionsAdv).end();
+        }});
+
+        $('#html-content-editor-{0}').bind('hide', function () {{
+            $('#html-content-editor-{0}').find('textarea.html-content-editor').ckeditorGet().destroy();
+        }});
+
+    }});
+
+    Sys.Application.add_load(function () {{
+
+        $('#html-content-edit-{0} .btn').click(function () {{
+            $('#html-content-editor-{0}').modal('hide');
+        }});
+
+    }});
+",
+                BlockInstance.Id);
+            this.Page.ClientScript.RegisterStartupScript( this.GetType(), string.Format( "edit-html-content-{0}", BlockInstance.Id ), script, true );
+
+            if ( _supportVersioning )
             {
-                // register scripts
-                PageInstance.AddScriptLink( this.Page, "~/scripts/ckeditor/ckeditor.js" );
-                PageInstance.AddScriptLink( this.Page, "~/scripts/ckeditor/adapters/jquery.js" );
+                PageInstance.AddScriptLink( this.Page, "~/scripts/Kendo/kendo.core.min.js" );
+                PageInstance.AddScriptLink( this.Page, "~/scripts/Kendo/kendo.fx.min.js" );
+                PageInstance.AddScriptLink( this.Page, "~/scripts/Kendo/kendo.popup.min.js" );
 
-                string blockContent = "";
+                PageInstance.AddScriptLink( this.Page, "~/scripts/Kendo/kendo.calendar.min.js" );
+                PageInstance.AddScriptLink( this.Page, "~/scripts/Kendo/kendo.datepicker.min.js" );
 
-                // get settings
-                string entityKey = AttributeValue( "EntityAwareKey" );
-                string entityValue = PageParameter( entityKey );
-                int cacheDuration = Int32.Parse ( AttributeValue( "CacheDuration" ) );
+                PageInstance.AddCSSLink( this.Page, "~/CSS/Kendo/kendo.common.min.css" );
+                PageInstance.AddCSSLink( this.Page, "~/CSS/Kendo/kendo.rock.min.css" );
 
-                // get blocks cached content
-                string cachedContent = null;
 
-                cachedContent = GetCacheItem( entityValue ) as string;
+                script = string.Format( @"
 
-                // if content not cached load it from DB
-                if ( cachedContent == null )
+    $(document).ready(function () {{
+
+        $('#{2}').kendoDatePicker({{ open:function(e){{
+            window.setTimeout(function(){{ $('.k-calendar-container').parent('.k-animation-container').css('zIndex', '11000'); }}, 1);
+        }} }});
+
+        $('#{3}').kendoDatePicker({{ open:function(e){{
+            window.setTimeout(function(){{ $('.k-calendar-container').parent('.k-animation-container').css('zIndex', '11000'); }}, 1);
+        }} }});
+
+        $('#html-content-version-{0}').click(function () {{
+            $('#html-content-versions-{0}').show();
+            $('#html-content-edit-{0}').hide();
+        }});
+
+        $('#html-content-versions-cancel-{0}').click(function () {{
+            $('#html-content-edit-{0}').show();
+            $('#html-content-versions-{0}').hide();
+            return false;
+        }});
+
+        $('a.html-content-show-version-{0}').click(function () {{
+
+            if (CKEDITOR.instances['{5}'].checkDirty() == false ||
+                confirm('Loading a previous version will cause any changes you\'ve made to the existing text to be lost.  Are you sure you want to continue?'))
+            {{
+                $.ajax({{
+                    type: 'GET',
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    url: rock.baseUrl + 'REST/CMS/HtmlContent/' + $(this).attr('html-id'),
+                    success: function (getData, status, xhr) {{
+
+                        htmlContent = getData;
+                        
+                        $('#html-content-version-{0}').text('Version ' + htmlContent.Version);
+                        $('#{1}').val(htmlContent.Version);
+                        $('#{2}').val(htmlContent.StartDateTime);
+                        $('#{3}').val(htmlContent.ExpireDateTime);
+                        $('#{4}').attr('checked', htmlContent.Approved);
+
+                        CKEDITOR.instances['{5}'].setData(htmlContent.Content, function() {{
+                            CKEDITOR.instances['{5}'].resetDirty();
+                            $('#html-content-edit-{0}').show();
+                            $('#html-content-versions-{0}').hide();
+                        }});
+
+                    }},
+                    error: function (xhr, status, error) {{
+                        alert(status + ' [' + error + ']: ' + xhr.responseText);
+                    }}
+                }});
+            }}
+        }});
+        
+
+    }});
+",
+                    BlockInstance.Id,
+                    hfVersion.ClientID,
+                    tbStartDate.ClientID,
+                    tbExpireDate.ClientID,
+                    cbApprove.ClientID,
+                    txtHtmlContentEditor.ClientID );
+                this.Page.ClientScript.RegisterStartupScript( this.GetType(), string.Format( "edit-html-content-version-{0}", BlockInstance.Id ), script, true );
+
+                pnlVersioningHeader.Visible = true;
+                cbOverwriteVersion.Visible = true;
+
+                hfVersion.Value = content.Version.ToString();
+                lVersion.Text = content.Version.ToString();
+                tbStartDate.Text = content.StartDateTime.HasValue ? content.StartDateTime.Value.ToShortDateString() : string.Empty;
+                tbExpireDate.Text = content.ExpireDateTime.HasValue ? content.ExpireDateTime.Value.ToShortDateString() : string.Empty;
+
+                if ( _requireApproval )
                 {
-                    Rock.CMS.HtmlContent content = new HtmlContentService().GetActiveContentByBlockKey( BlockInstance.Id, entityValue );
-
-                    if ( content != null )
-                    {
-                        blockContent = content.Content;
-
-                        // cache content
-                        if ( cacheDuration > 0 )
-                            AddCacheItem( entityValue, content.Content, cacheDuration );
-                    }
+                    cbApprove.Checked = content.Approved;
+                    cbApprove.Enabled = UserAuthorized( "Approve" );
+                    cbApprove.Visible = true;
                 }
                 else
-                    blockContent = cachedContent;
-               
-                // add content to the editor
-                txtHtmlContentEditor.Text = blockContent;
-
-                // add content to the content window
-                lPreText.Text = AttributeValue( "PreText" );
-                lHtmlContent.Text = blockContent;
-                lPostText.Text = AttributeValue( "PostText" );
-
+                    cbApprove.Visible = false;
             }
-        }
+            else
+            {
+                pnlVersioningHeader.Visible = false;
+                cbOverwriteVersion.Visible = false;
+            }
 
-        protected override void  OnInit(EventArgs e)
-        {
- 	        base.OnInit(e);
+            txtHtmlContentEditor.Text = content.Content;
 
-            this.AttributesUpdated += HtmlContent_AttributesUpdated;
-            this.AddAttributeUpdateTrigger( pnlContent );
-        }
-
-        void HtmlContent_AttributesUpdated( object sender, EventArgs e )
-        {
-            lPreText.Text = AttributeValue( "PreText" );
-            lPostText.Text = AttributeValue( "PostText" );
-            pnlContent.Update();
+            BindGrid();
         }
 
         protected void btnSaveContent_Click( object sender, EventArgs e )
         {
             if ( UserAuthorized( "Edit" ) || UserAuthorized( "Configure" ) )
             {
+                Rock.CMS.HtmlContent content = null;
+                HtmlContentService service = new HtmlContentService();
+
                 // get settings
-                string entityKey = AttributeValue( "EntityAwareKey" );
-                string entityValue = PageParameter( entityKey );
-                int cacheDuration = Int32.Parse( AttributeValue( "CacheDuration" ) );
+                string entityValue = EntityValue();
 
                 // get current  content
-                HtmlContentService service = new HtmlContentService();
-                Rock.CMS.HtmlContent content = service.GetActiveContentByBlockKey( BlockInstance.Id, entityValue );
+                int version = 0;
+                if ( !Int32.TryParse( hfVersion.Value, out version ) )
+                    version = 0;
+                content = service.GetByBlockIdAndEntityValueAndVersion( BlockInstance.Id, entityValue, version );
+
+                // if the existing content changed, and the overwrite option was not checked, create a new version
+                if ( content != null && 
+                    _supportVersioning && 
+                    content.Content != txtHtmlContentEditor.Text && 
+                    !cbOverwriteVersion.Checked )
+                    content = null;
 
                 // if a record doesn't exist then  create one
                 if ( content == null )
@@ -120,22 +259,147 @@ namespace RockWeb.Blocks.Cms
                     content = new Rock.CMS.HtmlContent();
                     content.BlockId = BlockInstance.Id;
                     content.EntityValue = entityValue;
-                    content.Approved = true;
-                    content.ApprovedByPersonId = CurrentPersonId;
+
+                    if ( _supportVersioning )
+                    {
+                        int? maxVersion = service.Queryable().
+                            Where( c => c.BlockId == BlockInstance.Id &&
+                                c.EntityValue == entityValue ).
+                            Select( c => ( int? )c.Version ).Max();
+
+                        content.Version = maxVersion.HasValue ? maxVersion.Value + 1 : 1;
+                    }
+                    else
+                        content.Version = 0;
+
                     service.Add( content, CurrentPersonId );
+                }
+
+                if ( _supportVersioning )
+                {
+                    DateTime startDate;
+                    if ( DateTime.TryParse( tbStartDate.Text, out startDate ) )
+                        content.StartDateTime = startDate;
+                    else
+                        content.StartDateTime = null;
+
+                    DateTime expireDate;
+                    if ( DateTime.TryParse( tbExpireDate.Text, out expireDate ) )
+                        content.ExpireDateTime = expireDate;
+                    else
+                        content.ExpireDateTime = null;
+                }
+                else
+                {
+                    content.StartDateTime = null;
+                    content.ExpireDateTime = null;
+                }
+
+                if ( !_requireApproval || UserAuthorized( "Approve" ) )
+                {
+                    content.Approved = !_requireApproval || cbApprove.Checked;
+                    if ( content.Approved )
+                    {
+                        content.ApprovedByPersonId = CurrentPersonId;
+                        content.ApprovedDateTime = DateTime.Now;
+                    }
                 }
 
                 content.Content = txtHtmlContentEditor.Text;
 
                 service.Save( content, CurrentPersonId );
 
-                // refresh cache content if desired
-                this.AddCacheItem( entityValue, txtHtmlContentEditor.Text, cacheDuration );
+                // flush cache content 
+                this.FlushCacheItem( entityValue );
 
-                // set page content to new content
-                lHtmlContent.Text = txtHtmlContentEditor.Text;
             }
+
+            ShowView();
         }
+
+        void rGrid_GridRebind( object sender, EventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+        void HtmlContent_AttributesUpdated( object sender, EventArgs e )
+        {
+            lPreText.Text = AttributeValue( "PreText" );
+            lPostText.Text = AttributeValue( "PostText" );
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ShowView()
+        {
+            string entityValue = EntityValue();
+            string html = "";
+
+            int cacheDuration = Int32.Parse( AttributeValue( "CacheDuration" ) );
+            string cachedContent = GetCacheItem( entityValue ) as string;
+
+            // if content not cached load it from DB
+            if ( cachedContent == null )
+            {
+                Rock.CMS.HtmlContent content = new HtmlContentService().GetActiveContent( BlockInstance.Id, entityValue );
+
+                if ( content != null )
+                {
+                    html = content.Content;
+
+                    // cache content
+                    if ( cacheDuration > 0 )
+                        AddCacheItem( entityValue, html, cacheDuration );
+                }
+            }
+            else
+                html = cachedContent;
+
+            // add content to the content window
+            lPreText.Text = AttributeValue( "PreText" );
+            lHtmlContent.Text = html;
+            lPostText.Text = AttributeValue( "PostText" );
+        }
+
+        private void BindGrid()
+        {
+            HtmlContentService service = new HtmlContentService();
+
+            var versions = service.GetContent(BlockInstance.Id, EntityValue()).
+                Select( v => new {
+                    v.Id,
+                    v.Version,
+                    v.Content,
+                    ModifiedDateTime = v.ModifiedDateTime.ToElapsedString(),
+                    ModifiedByPerson = v.ModifiedByPerson != null ? v.ModifiedByPerson.FullName : "",
+                    v.Approved,
+                    ApprovedByPerson = v.ApprovedByPerson != null ? v.ApprovedByPerson.FullName : "",
+                    v.StartDateTime,
+                    v.ExpireDateTime
+                }).ToList();
+
+            rGrid.DataSource = versions;
+            rGrid.DataBind();
+        }
+
+        private string EntityValue()
+        {
+            string entityValue = string.Empty;
+
+            string contextParameter = AttributeValue( "ContextParameter" );
+            if ( !string.IsNullOrEmpty( contextParameter ) )
+                entityValue = string.Format( "{0}={1}", contextParameter, PageParameter( contextParameter ) ?? string.Empty );
+
+            string contextParameterValue = PageParameter( contextParameter );
+            if ( !string.IsNullOrEmpty( contextParameterValue ) )
+                entityValue += "&ContextName=" + contextParameterValue;
+
+            return entityValue;
+        }
+
+        #endregion
     }
 }
 
