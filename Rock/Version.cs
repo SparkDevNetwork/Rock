a9c55d7445cba59d8b0b543eb5e019ec7594be92
David Turner <davidturner@ccvonline.com>
2012-03-01 16:36:50 -0700

    Add method to get current Rock version:
    Rock.Version.Current.ToString()
    
    Fixes #48

---
+++ Rock/Version.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Reflection;
+
+namespace Rock
+{
+    public static class Version
+    {
+        public static System.Version Current
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+    }
+}
