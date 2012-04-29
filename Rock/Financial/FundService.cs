012b3a0f245fe96e7a58aa5aa4ff0a78d2447cdb
JasonOffutt <jason.offutt@gmail.com>
2012-04-29 01:40:56 -0700

    First pass through at adding service classes for new Financial objects.

---
+++ Rock/Financial/FundService.cs
@@ -0,0 +1,17 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using Rock.Data;
+
+namespace Rock.Financial
+{
+    /// <summary>
+    /// Service class for Fund objects.
+    /// </summary>
+    public partial class FundService : Service<Fund>
+    {
+    }
+}
---
+++ Rock/Financial/PersonAccountLookupService.cs
@@ -0,0 +1,17 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using Rock.Data;
+
+namespace Rock.Financial
+{
+    /// <summary>
+    /// Service class for PersonAccountLookup objects.
+    /// </summary>
+    public partial class PersonAccountLookupService : Service<PersonAccountLookup>
+    {
+    }
+}
