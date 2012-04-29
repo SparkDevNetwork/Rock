e389a401d630eaf54d13b018df2ad906def71830
JasonOffutt <jason.offutt@gmail.com>
2012-04-29 01:40:56 -0700

    First pass through at adding service classes for new Financial objects.

---
+++ Rock/Financial/BatchService.cs
@@ -0,0 +1,16 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using Rock.Data;
+
+namespace Rock.Financial
+{
+    /// <summary>
+    /// Service class for Batch objects.
+    /// </summary>
+    public partial class BatchService : Service<Batch>
+    {
+    }
+}
