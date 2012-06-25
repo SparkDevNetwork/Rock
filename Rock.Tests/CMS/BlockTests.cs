edb812dcd8625a7ea58cfe70c886875386b9a40e
JasonOffutt <jason.offutt@gmail.com>
2012-06-25 09:11:57 -0700

    First Pass at implementing back end for exporting pages and blocks

---
+++ Rock.Tests/CMS/BlockTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Rock.Tests.CMS
+{
+    public class BlockTests
+    {
+        public class TheToXmlMethod
+        {
+
+        }
+    }
+}
---
+++ Rock/Data/IExportable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rock.Data
+{
+    public interface IExportable
+    {
+        string Export();
+        void Import(string data);
+    }
+}
