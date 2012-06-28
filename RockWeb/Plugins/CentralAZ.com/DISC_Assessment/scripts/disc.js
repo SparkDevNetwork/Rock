cd483cef577280c3272ae8a9d075a09bb5c01118
swedd <derek.mangrum@gmail.com>
2012-06-28 12:39:32 -0700

    DISC Assessmetn work

--- RockWeb/Plugins/CentralAZ.com/DISC_Assessment/scripts/disc.js
+++ RockWeb/Plugins/CentralAZ.com/DISC_Assessment/scripts/disc.js
@@ -1,6 +1,16 @@
 ﻿$(document).ready(function () {
-	$('#tabs li a:not(:first)').addClass('inactive');
-	$('.tabContent:not(:first)').hide();
+	// Checks to see if the test has been scored
+	// the isScored variable is passed in from the code-behind file
+	if (!isScored) {
+		$('#tabs li a:not(:first)').addClass('inactive');
+		$('.tabContent:not(:first)').hide();
+		$('.tabContent:first').show();
+	}
+	else {
+		$('#tabs li a:not(:last)').addClass('inactive');
+		$('.tabContent:not(:last)').hide();
+		$('.tabContent:last').show();
+	}
 
 	$('#tabs li a').click(function () {
 		var t = $(this).attr('href');
