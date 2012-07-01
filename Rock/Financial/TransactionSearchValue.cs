a764c19ee5e29b924a4249461d10aba934771b65
Jacob Fetner <J94f@aol.com>
2012-07-01 17:52:56 -0400

    JRF financial display page for transactions

---
+++ Rock/Financial/TransactionSearchValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Rock.Core;
+
+namespace Rock.Financial
+{
+    public class TransactionSearchValue
+    {
+        public RangeValue<DateTime?> DateRange { get; set; }
+        public RangeValue<decimal?> AmountRange { get; set; }
+        public Fund Fund { get; set; }
+        public DefinedValue CurrencyType { get; set; }
+        public DefinedValue CreditCardType { get; set; }
+        public DefinedValue SourceType { get; set; }
+        public string TransactionCode { get; set; }
+    }
+}
