710ec8b44b6c6ee6d0257cb5ed6459d7032c678e
David Turner <davidturner@ccvonline.com>
2012-07-03 16:11:04 -0700

    More changes

---
+++ Rock.Custom.CCVOnline/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Rock.Custom.CCVOnline.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Rock.Custom.CCVOnline.Data.CCVContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsNamespace = "Rock.Custom.CCVOnline";
+        }
+
+        protected override void Seed(Rock.Custom.CCVOnline.Data.CCVContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
