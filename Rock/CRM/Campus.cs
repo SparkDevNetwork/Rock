d522c21e4963c73f2e3d89b32f45e4491b9ae74b
David Turner <david@azturners.com>
2012-07-11 13:24:58 -0700

    Add Campus model/table

---
+++ Rock/CRM/Campus.cs
@@ -0,0 +1,119 @@
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Runtime.Serialization;
+using System.Web;
+
+using Rock.Data;
+
+namespace Rock.CRM
+{
+    /// <summary>
+    /// Campus POCO Entity.
+    /// </summary>
+    [Table( "crmCampus" )]
+    public partial class Campus : ModelWithAttributes<Campus>, IAuditable
+    {
+		/// <summary>
+		/// Gets or sets the System.
+		/// </summary>
+		/// <value>
+		/// System.
+		/// </value>
+		[Required]
+		[DataMember]
+		public bool System { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name.
+        /// </summary>
+        /// <value>
+        /// Given Name.
+        /// </value>
+        [MaxLength( 100 )]
+        [DataMember]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Created Date Time.
+        /// </summary>
+        /// <value>
+        /// Created Date Time.
+        /// </value>
+        [DataMember]
+        public DateTime? CreatedDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Modified Date Time.
+        /// </summary>
+        /// <value>
+        /// Modified Date Time.
+        /// </value>
+        [DataMember]
+        public DateTime? ModifiedDateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Created By Person Id.
+        /// </summary>
+        /// <value>
+        /// Created By Person Id.
+        /// </value>
+        [DataMember]
+        public int? CreatedByPersonId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Modified By Person Id.
+        /// </summary>
+        /// <value>
+        /// Modified By Person Id.
+        /// </value>
+        [DataMember]
+        public int? ModifiedByPersonId { get; set; }
+
+        /// <summary>
+        /// Gets the auth entity.
+        /// </summary>
+        [NotMapped]
+        public override string AuthEntity { get { return "CRM.Campus"; } }
+
+        /// <summary>
+        /// Gets a Data Transfer Object (lightweight) version of this object.
+        /// </summary>
+        /// <value>
+        /// A <see cref="Rock.CRM.DTO.Campus"/> object.
+        /// </value>
+        [NotMapped]
+        public Rock.CRM.DTO.Campus DataTransferObject
+        {
+            get
+            {
+                Rock.CRM.DTO.Campus dto = new Rock.CRM.DTO.Campus();
+                dto.Id = this.Id;
+                dto.Guid = this.Guid;
+                dto.System = this.System;
+                dto.Name = this.Name;
+                return dto;
+            }
+        }
+
+    }
+    /// <summary>
+    /// Campus Configuration class.
+    /// </summary>
+    public partial class CampusConfiguration : EntityTypeConfiguration<Campus>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CampusConfiguration"/> class.
+        /// </summary>
+        public CampusConfiguration()
+        {
+        }
+    }
+
+}
