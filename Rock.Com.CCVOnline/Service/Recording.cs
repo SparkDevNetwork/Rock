30e336a2d25d38e866e10f3f079a3a77ffd667ed
David Turner <davidturner@ccvonline.com>
2012-07-13 04:29:41 -0700

    More changes for command center

--- Rock.Com.CCVOnline/Service/Recording.cs
+++ Rock.Com.CCVOnline/Service/Recording.cs
@@ -19,6 +19,8 @@ namespace Rock.Com.CCVOnline.Service
         [DataMember]
         public int? CampusId { get; set; }
 
+        public virtual Rock.CRM.Campus Campus { get; set; }
+
         [MaxLength( 100 )]
         [DataMember]
         public string Label { get; set; }
@@ -38,12 +40,16 @@ namespace Rock.Com.CCVOnline.Service
         [DataMember]
         public DateTime? StartTime { get; set; }
 
+        [MaxLength( 400 )]
+        [DataMember]
+        public string StartResponse { get; set; }
+
         [DataMember]
         public DateTime? StopTime { get; set; }
 
-        [MaxLength( 100 )]
+        [MaxLength( 400 )]
         [DataMember]
-        public string RecordingPath { get; set; }
+        public string StopResponse { get; set; }
 
         /// <summary>
 		/// Gets or sets the Created Date Time.
@@ -99,32 +105,6 @@ namespace Rock.Com.CCVOnline.Service
             }
         }
 
-
-        public Recording()
-            : base()
-        {
-        }
-
-        public Recording( Rock.Com.CCVOnline.Service.RecordingDTO dto )
-            : base()
-        {
-            this.Id = dto.Id;
-            this.Guid = dto.Guid;
-            this.Date = dto.Date;
-            this.CampusId = dto.CampusId;
-            this.Label = dto.Label;
-            this.App = dto.App;
-            this.StreamName = dto.StreamName;
-            this.RecordingName = dto.RecordingName;
-            this.StartTime = dto.StartTime;
-            this.StopTime = dto.StopTime;
-            this.RecordingPath = dto.RecordingPath;
-            this.CreatedDateTime = dto.CreatedDateTime;
-            this.ModifiedDateTime = dto.ModifiedDateTime;
-            this.CreatedByPersonId = dto.CreatedByPersonId;
-            this.ModifiedByPersonId = dto.ModifiedByPersonId;
-        }
-
         /// <summary>
         /// Gets the data transfer object.
         /// </summary>
@@ -142,8 +122,9 @@ namespace Rock.Com.CCVOnline.Service
                 dto.StreamName = this.StreamName;
                 dto.RecordingName = this.RecordingName;
                 dto.StartTime = this.StartTime;
+                dto.StartResponse = this.StartResponse;
                 dto.StopTime = this.StopTime;
-                dto.RecordingPath = this.RecordingPath;
+                dto.StopResponse = this.StopResponse;
                 dto.CreatedDateTime = this.CreatedDateTime;
                 dto.ModifiedDateTime = this.ModifiedDateTime;
                 dto.CreatedByPersonId = this.CreatedByPersonId;
@@ -153,11 +134,72 @@ namespace Rock.Com.CCVOnline.Service
         }
 
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Recording"/> class.
+        /// </summary>
+        public Recording()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Recording"/> class.
+        /// </summary>
+        /// <param name="dto">The dto.</param>
+        public Recording( Rock.Com.CCVOnline.Service.RecordingDTO dto )
+            : base()
+        {
+            this.Id = dto.Id;
+            this.Guid = dto.Guid;
+            this.Date = dto.Date;
+            this.CampusId = dto.CampusId;
+            this.Label = dto.Label;
+            this.App = dto.App;
+            this.StreamName = dto.StreamName;
+            this.RecordingName = dto.RecordingName;
+            this.StartTime = dto.StartTime;
+            this.StartResponse = dto.StartResponse;
+            this.StopTime = dto.StopTime;
+            this.StopResponse = dto.StopResponse;
+            this.CreatedDateTime = dto.CreatedDateTime;
+            this.ModifiedDateTime = dto.ModifiedDateTime;
+            this.CreatedByPersonId = dto.CreatedByPersonId;
+            this.ModifiedByPersonId = dto.ModifiedByPersonId;
+        }
+
+        public bool SendRequest(string action)
+        {
+            Rock.Net.WebResponse response = RecordingService.SendRecordingRequest( App, StreamName, RecordingName, action.ToLower() );
+
+            if ( response != null && response.HttpStatusCode == System.Net.HttpStatusCode.OK )
+            {
+
+                if ( action.ToLower() == "start" )
+                {
+                    StartTime = DateTime.Now;
+                    StartResponse = RecordingService.ParseResponse( response.Message );
+                }
+                else
+                {
+                    StopTime = DateTime.Now;
+                    StopResponse = RecordingService.ParseResponse( response.Message );
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
     }
     /// <summary>
     /// Blog Configuration class.
     /// </summary>
     public partial class RecordingConfiguration : EntityTypeConfiguration<Recording>
     {
+        public RecordingConfiguration()
+        {
+			this.HasRequired( c => c.Campus ).WithMany().HasForeignKey( c => c.CampusId ).WillCascadeOnDelete(false);
+		}
     }
 }
