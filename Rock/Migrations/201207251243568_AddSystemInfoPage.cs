5918b2b66b7247c87d980068fcc1de05d267b9b6
David Turner <david@azturners.com>
2012-07-27 07:12:26 -0700

    Move SystemInfo page to admin footer

--- Rock/Migrations/201207251243568_AddSystemInfoPage.cs
+++ Rock/Migrations/201207251243568_AddSystemInfoPage.cs
@@ -16,9 +16,12 @@ SET @BlockId = SCOPE_IDENTITY()
 -- cmsPage --
 DECLARE @PageId int
 DECLARE @Order int
-SELECT @Order = ISNULL(MAX([order])+1,0) FROM [cmsPage] WHERE [ParentPageId] = 77;
-INSERT INTO [cmsPage] ([Name],[Title],[IsSystem],[ParentPageId],[SiteId],[Layout],[RequiresEncryption],[EnableViewState],[MenuDisplayDescription],[MenuDisplayIcon],[MenuDisplayChildPages],[DisplayInNavWhen],[Order],[OutputCacheDuration],[Description],[IncludeAdminFooter],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[IconUrl],[Guid])VALUES('System Information','System Information',0,77,1,'Default',0,1,1,0,0,0,ISNULL(@Order,0),0,'Displays status and performance information for the currently running instance of Rock ChMS',1,'Jul 25 2012 06:00:00:000AM','Jul 25 2012 06:00:00:000AM',1,1,NULL,'8A97CC93-3E93-4286-8440-E5217B65A904')
+SELECT @Order = ISNULL(MAX([order])+1,0) FROM [cmsPage] WHERE [ParentPageId] = 74;
+INSERT INTO [cmsPage] ([Name],[Title],[IsSystem],[ParentPageId],[SiteId],[Layout],[RequiresEncryption],[EnableViewState],[MenuDisplayDescription],[MenuDisplayIcon],[MenuDisplayChildPages],[DisplayInNavWhen],[Order],[OutputCacheDuration],[Description],[IncludeAdminFooter],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[IconUrl],[Guid])VALUES('System Information','System Information',1,74,1,'Dialog',0,1,0,0,0,2,ISNULL(@Order,0),0,'Displays status and performance information for the currently running instance of Rock ChMS',0,'Jul 25 2012 06:00:00:000AM','Jul 25 2012 06:00:00:000AM',1,1,NULL,'8A97CC93-3E93-4286-8440-E5217B65A904')
 SET @PageId = SCOPE_IDENTITY()
+
+-- cmsPageRoute
+INSERT INTO [cmsPageRoute] ([IsSystem],[PageId],[Route],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])VALUES(1,@PageId,'SystemInfo','Jul 25 2012 06:00:00:000AM','Jul 25 2012 06:00:00:000AM',1,1,'617CF50F-C199-470A-8B32-F9115BDD02C0')
 
 -- cmsBlockInstance
 INSERT INTO [cmsBlockInstance] ([IsSystem],[PageId],[Layout],[BlockId],[Zone],[Order],[Name],[OutputCacheDuration],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])VALUES(0,@PageId,NULL,@BlockId,'Content',0,'SystemInfo',0,'Jul 25 2012 06:00:00:000AM','Jul 25 2012 06:00:00:000AM',1,1,'D1FD9A6B-C213-4074-8D84-EE5353635443')
@@ -34,6 +37,9 @@ DELETE [cmsBlockInstance] WHERE [Guid] = 'D1FD9A6B-C213-4074-8D84-EE5353635443'
 
 -- cmsPage --
 DELETE [cmsPage] WHERE [Guid] = '8A97CC93-3E93-4286-8440-E5217B65A904'
+
+-- cmsPageRoute
+DELETE [cmsPageRoute] WHERE [Guid] = '617CF50F-C199-470A-8B32-F9115BDD02C0'
 
 -- cmsBlock --
 DELETE [cmsBlock] WHERE [Guid] = 'DE08EFD7-4CF9-4BD5-9F72-C0151FD08523'
