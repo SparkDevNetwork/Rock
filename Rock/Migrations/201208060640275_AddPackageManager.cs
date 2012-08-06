d0e8ddc4b59e64049c98c4f0b3a47a471efb06f7
Nick Airdo <nairdo@gmail.com>
2012-08-06 00:04:09 -0700

    Initial version of the Rock Plugin Manager

---
+++ Rock/Migrations/201208060640275_AddPackageManager.cs
@@ -0,0 +1,82 @@
+namespace Rock.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPackageManager : DbMigration
+    {
+        public override void Up()
+        {
+			// add the Plugin Manager page to the admin section of the website.
+			Sql( string.Format( @"
+DECLARE @PluginSettingsPageGuid uniqueidentifier
+SET @PluginSettingsPageGuid = '{0}'
+DECLARE @PageGuid uniqueidentifier
+SET @PageGuid = '{1}'
+DECLARE @BlockGuid uniqueidentifier
+SET @BlockGuid = '{2}'
+DECLARE @BlockInstanceGuid uniqueidentifier
+SET @BlockInstanceGuid = '8B083CEA-0548-4AF2-86F7-46A88FDE07D5'
+
+-- cmsBlock --
+DECLARE @BlockId int
+INSERT INTO [cmsBlock] ([IsSystem],[Path],[Name],[Description],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])VALUES(1,'~/Blocks/Administration/PluginManager.ascx','Plugin Manager','Allows installed plugins to be viewed or removed and new ones to be added from the RockQuary.','Jul 13 2012 12:50:50:000PM','Jul 13 2012 12:50:50:000PM',1,1,@BlockGuid)
+SET @BlockId = SCOPE_IDENTITY()
+
+-- find parent page --
+DECLARE @ParentPageId int
+SELECT @ParentPageId = [Id] FROM cmsPage WHERE [Guid] = @PluginSettingsPageGuid
+
+-- cmsPage --
+DECLARE @PageId int
+DECLARE @Order int
+SELECT @Order = ISNULL(MAX([order])+1,0) FROM [cmsPage] WHERE [ParentPageId] = @ParentPageId;
+INSERT INTO [cmsPage] ([Name],[Title],[IsSystem],[ParentPageId],[SiteId],[Layout],[RequiresEncryption],[EnableViewState],[MenuDisplayDescription],[MenuDisplayIcon],[MenuDisplayChildPages],[DisplayInNavWhen],[Order],[OutputCacheDuration],[Description],[IncludeAdminFooter],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[IconUrl],[Guid])
+	VALUES('Plugin Manager','Plugin Manager',1,@ParentPageId,1,'Default',0,1,0,0,0,0,ISNULL(@Order,0),0,'Screen to administrate plugins.',1,'Jul 13 2012 12:50:50:000PM','Jul 13 2012 12:50:50:000PM',1,1,NULL,@PageGuid)
+SET @PageId = SCOPE_IDENTITY()
+
+-- cmsBlockInstance
+INSERT INTO [cmsBlockInstance] ([IsSystem],[PageId],[Layout],[BlockId],[Zone],[Order],[Name],[OutputCacheDuration],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])
+VALUES(1,@PageId,NULL,@BlockId,'Content',0,'Plugin Manager',0,'Jul 13 2012 12:50:50:000PM','Jul 13 2012 12:50:50:000PM',1,1,@BlockInstanceGuid)
+
+-- coreAttribute --
+DECLARE @AttributeId int
+INSERT INTO [coreAttribute] ([IsSystem],[FieldTypeId],[Entity],[EntityQualifierColumn],[EntityQualifierValue],[Key],[Name],[Category],[Description],[Order],[IsGridColumn],[DefaultValue],[IsMultiValue],[IsRequired],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])
+	VALUES(1,1,'','','','PackageSourceUrl','Package Source URL','Config','URL to the Rock Quarry plugin source repository API (v2)',0,0,'http://quarry.rockchms.com/api/v2/',0,0,'Jul 13 2012 12:50:50:000PM','Jul 13 2012 12:50:50:000PM',1,1,'306E7E7C-9416-4098-9C25-488380B940A5')
+SET @AttributeId = SCOPE_IDENTITY()
+
+-- coreAttributeValue --
+INSERT INTO [coreAttributeValue] ([IsSystem],[AttributeId],[EntityId],[Order],[Value],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])
+	VALUES(0,@AttributeId,NULL,NULL,'http://quarry.rockchms.com/api/v2/','Jul 13 2012 12:50:00:000PM','Jul 13 2012 12:50:00:000PM',1,1,'5CB48974-6BB6-435B-A04A-2BF9B7CD778E')
+
+-- cmsAuth --
+INSERT INTO [cmsAuth] ([EntityType],[EntityId],[Order],[Action],[AllowOrDeny],[SpecialRole],[PersonId],[GroupId],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])
+	VALUES('CMS.Page',@PageId,0,'Edit','A',0,NULL,2,'Jul 13 2012 12:50:00:000PM','Jul 13 2012 12:50:00:000PM',1,1,'632E474F-29D0-4D7D-A51A-5FD2D8DE74FE')
+
+", Rock.SystemGuid.Page.PLUGIN_SETTINGS, Rock.SystemGuid.Page.PLUGIN_MANAGER, Rock.SystemGuid.Block.PLUGIN_MANAGER ) );
+        }
+        
+        public override void Down()
+        {
+
+			// Remove the block instance, the page and the block
+			Sql( string.Format( @"
+-- cmsBlockInstance --
+DELETE [cmsBlockInstance] WHERE [Guid] = '8B083CEA-0548-4AF2-86F7-46A88FDE07D5'
+
+-- cmsPage --
+DELETE [cmsPage] WHERE [Guid] = '{0}'
+
+-- cmsBlock --
+DELETE [cmsBlock] WHERE [Guid] = '{1}'
+
+-- coreAttribute --
+DELETE [cmsAttribute] WHERE [Guid] = '306E7E7C-9416-4098-9C25-488380B940A5'
+
+-- coreAttributeValue --
+DELETE [cmsAttributeValue] WHERE [Guid] = '5CB48974-6BB6-435B-A04A-2BF9B7CD778E'
+
+
+", Rock.SystemGuid.Page.PLUGIN_MANAGER, Rock.SystemGuid.Block.PLUGIN_MANAGER ));
+        }
+    }
+}
---
+++ Rock/Services/NuGet/WebProjectManager.cs
@@ -0,0 +1,273 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using NuGet;
+
+namespace Rock.Services.NuGet
+{
+	/// <summary>
+	/// This is the service layer that handles installing, updating, removing Packages (aka Plugins)
+	/// from the website / local filesystem and the Rock Quarry (our NuGet server).
+	/// </summary>
+	public class WebProjectManager
+	{
+		private readonly IProjectManager _projectManager;
+
+		/// <summary>
+		/// Creates a WebProjectManager service.
+		/// </summary>
+		/// <param name="remoteSource">URL of the NuGet server API (ex, http://nuget.org/api/v2 ).</param>
+		/// <param name="siteRoot">The physical path to the web root.</param>
+		public WebProjectManager( string remoteSource, string siteRoot )
+		{
+			string webRepositoryDirectory = GetWebRepositoryDirectory( siteRoot );
+			var fileSystem = new PhysicalFileSystem( webRepositoryDirectory );
+			var packagePathResolver = new RockPackagePathResolver( fileSystem );
+			_projectManager = new ProjectManager( sourceRepository: PackageRepositoryFactory.Default.CreateRepository( remoteSource ),
+									   pathResolver: packagePathResolver,
+									   localRepository: new LocalPackageRepository( packagePathResolver, fileSystem ),
+									   project: new WebProjectSystem( siteRoot ) );
+		}
+
+		/// <summary>
+		/// Represents the local NuGet package repository.
+		/// </summary>
+		public IPackageRepository LocalRepository
+		{
+			get
+			{
+				return _projectManager.LocalRepository;
+			}
+		}
+
+		/// <summary>
+		/// Represents the remote/source NuGet package repository.
+		/// </summary>
+		public IPackageRepository SourceRepository
+		{
+			get
+			{
+				return _projectManager.SourceRepository;
+			}
+		}
+
+		/// <summary>
+		/// Gets the newest/latest package or all of them matching the given keyword search.
+		/// </summary>
+		/// <param name="searchTerms">a string of space delimited search terms</param>
+		/// <param name="includeAllVersions">if true, returns all matching packages; otherwise only the latest</param>
+		/// <param name="includeAllVersions">Indicates whether to include all versions of an update as opposed to only including the latest version.</param>
+		/// <returns>a list of packages</returns>
+		public IQueryable<IPackage> GetLatestRemotePackages( string searchTerms, bool includeAllVersions )
+		{
+			var packages = GetPackages( SourceRepository, searchTerms );
+			return packages.Where( x => x.IsLatestVersion || includeAllVersions );
+		}
+
+		/// <summary>
+		/// Gets all matching packages for the given keyword search.
+		/// </summary>
+		/// <param name="searchTerms">a string of space delimited search terms</param>
+		/// <returns>a list of packages</returns>
+		public IQueryable<IPackage> GetRemotePackages( string searchTerms )
+		{
+			return GetPackages( SourceRepository, searchTerms );
+		}
+
+		/// <summary>
+		/// Gets all matching installed packages for the given keyword search.
+		/// </summary>
+		/// <param name="searchTerms">a string of space delimited search terms</param>
+		/// <returns>a list of packages</returns>
+		public IQueryable<IPackage> GetInstalledPackages( string searchTerms )
+		{
+			return GetPackages( LocalRepository, searchTerms );
+		}
+
+		/// <summary>
+		/// Gets all matching installed packages for the given keyword search for which updates are available.
+		/// </summary>
+		/// <param name="searchTerms">a string of space delimited search terms</param>
+		/// <returns>a list of packages</returns>
+		//public IQueryable<IPackage> GetPackagesWithUpdates( string searchTerms )
+		//{
+		//    IQueryable<IPackage> packages = LocalRepository.GetUpdates( SourceRepository.GetPackages(), includePrerelease: false, includeAllVersions: true, targetFramework: null ).AsQueryable();
+		//    return GetPackages( packages, searchTerms );
+		//}
+
+		/// <summary>
+		/// Gets the locally installed package for the given id.
+		/// </summary>
+		/// <param name="packageId">the Id of a package</param>
+		/// <returns>a package</returns>
+		public IPackage GetInstalledPackage( string packageId )
+		{
+			return LocalRepository.FindPackage( packageId );
+		}
+
+		/// <summary>
+		/// Installs and adds a package reference to the project
+		/// </summary>
+		/// <returns>Warnings encountered when installing the package.</returns>
+		public IEnumerable<string> InstallPackage( IPackage package )
+		{
+			return PerformLoggedAction( () =>
+			{
+				_projectManager.AddPackageReference( package.Id, package.Version, ignoreDependencies: false, allowPrereleaseVersions: false );
+			} );
+		}
+
+		/// <summary>
+		/// Updates a package reference. Installs the package to the App_Data repository if it does not already exist.
+		/// </summary>
+		/// <returns>Warnings encountered when updating the package.</returns>
+		public IEnumerable<string> UpdatePackage( IPackage package )
+		{
+			return PerformLoggedAction( () =>
+			{
+				_projectManager.UpdatePackageReference( package.Id, package.Version, updateDependencies: true, allowPrereleaseVersions: false);
+			} );
+		}
+		/// <summary>
+		/// Removes a package reference and uninstalls the package
+		/// </summary>
+		/// <returns>Warnings encountered when uninstalling the package.</returns>
+		public IEnumerable<string> UninstallPackage( IPackage package, bool removeDependencies )
+		{
+			return PerformLoggedAction( () =>
+			{
+				_projectManager.RemovePackageReference( package.Id, forceRemove: false, removeDependencies: removeDependencies );
+			} );
+		}
+
+		/// <summary>
+		/// Will let you know if this exact package is installed locally
+		/// </summary>
+		/// <param name="package"></param>
+		/// <returns></returns>
+		public bool IsPackageInstalled( IPackage package )
+		{
+			return LocalRepository.Exists( package );
+		}
+
+		/// <summary>
+		/// Will let you know if this package is installed locally. If anyVersion is true
+		/// it does not matter which version of the package; otherwise only returns true if
+		/// an exact match.
+		/// </summary>
+		/// <param name="package"></param>
+		/// <param name="anyVersion"></param>
+		/// <returns></returns>
+		public bool IsPackageInstalled( IPackage package, bool anyVersion )
+		{
+			return (anyVersion) ? LocalRepository.Exists( package.Id ) : LocalRepository.Exists( package );
+		}
+
+		/// <summary>
+		/// Gets the latest version of the given package.
+		/// </summary>
+		/// <param name="package">a package</param>
+		/// <returns>a package; otherwise null if no package was found</returns>
+		public IPackage GetUpdate( IPackage package )
+		{
+			return SourceRepository.GetUpdates( LocalRepository.GetPackages(), includePrerelease: false, includeAllVersions: true, targetFramework: null ).FirstOrDefault( p => package.Id == p.Id );
+		}
+
+		/// <summary>
+		/// Gets all versions of the given package.
+		/// </summary>
+		/// <param name="package">a package</param>
+		/// <returns>a list of packages</returns>
+		public IEnumerable<IPackage> GetUpdates()
+		{
+			return SourceRepository.GetUpdates( LocalRepository.GetPackages(), includePrerelease: false, includeAllVersions: true, targetFramework: null );
+		}
+
+		private IEnumerable<string> PerformLoggedAction( Action action )
+		{
+			ErrorLogger logger = new ErrorLogger();
+			_projectManager.Logger = logger;
+			try
+			{
+				action();
+			}
+			finally
+			{
+				_projectManager.Logger = null;
+			}
+			return logger.Errors;
+		}
+
+		internal IEnumerable<IPackage> GetPackagesRequiringLicenseAcceptance( IPackage package )
+		{
+			return GetPackagesRequiringLicenseAcceptance( package, localRepository: LocalRepository, sourceRepository: SourceRepository );
+		}
+
+		internal static IEnumerable<IPackage> GetPackagesRequiringLicenseAcceptance( IPackage package, IPackageRepository localRepository, IPackageRepository sourceRepository )
+		{
+			var dependencies = GetPackageDependencies( package, localRepository, sourceRepository );
+
+			return from p in dependencies
+				   where p.RequireLicenseAcceptance
+				   select p;
+		}
+
+		private static IEnumerable<IPackage> GetPackageDependencies( IPackage package, IPackageRepository localRepository, IPackageRepository sourceRepository )
+		{
+			InstallWalker walker = new InstallWalker( localRepository: localRepository, sourceRepository: sourceRepository, logger: NullLogger.Instance, ignoreDependencies: false, allowPrereleaseVersions: false, targetFramework: null );
+			IEnumerable<PackageOperation> operations = walker.ResolveOperations( package );
+
+			return from operation in operations
+				   where operation.Action == PackageAction.Install
+				   select operation.Package;
+		}
+
+		internal static IQueryable<IPackage> GetPackages( IPackageRepository repository, string searchTerm )
+		{
+			return GetPackages( repository.GetPackages(), searchTerm );
+		}
+
+		internal static IQueryable<IPackage> GetPackages( IQueryable<IPackage> packages, string searchTerm )
+		{
+			if ( !String.IsNullOrEmpty( searchTerm ) )
+			{
+				searchTerm = searchTerm.Trim();
+				packages = packages.Find( searchTerm );
+			}
+			return packages;
+		}
+
+		internal static string GetWebRepositoryDirectory( string siteRoot )
+		{
+			return Path.Combine( siteRoot, "App_Data", "packages" );
+		}
+
+		private class ErrorLogger : ILogger
+		{
+			private readonly IList<string> _errors = new List<string>();
+
+			public IEnumerable<string> Errors
+			{
+				get
+				{
+					return _errors;
+				}
+			}
+
+			public void Log( MessageLevel level, string message, params object[] args )
+			{
+				if ( level == MessageLevel.Warning || level == MessageLevel.Error )
+				{
+					_errors.Add( String.Format( CultureInfo.CurrentCulture, message, args ) );
+				}
+			}
+		}
+	}
+}
