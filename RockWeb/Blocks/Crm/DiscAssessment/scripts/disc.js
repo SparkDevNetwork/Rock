941c4bbebaf192c534534c67b5ee20b1b8651d41
Derek Mangrum <derek.mangrum@gmail.com>
2012-08-30 11:23:28 -0700

    Updates bootstrap and jquery. Modified DISC block stuff, moving .js file out of CentralAZ custom and into core.

--- RockWeb/Blocks/Crm/DiscAssessment/scripts/disc.js
+++ RockWeb/Blocks/Crm/DiscAssessment/scripts/disc.js
@@ -1,66 +1,59 @@
-﻿$(document).ready(function () {
-	// Checks to see if the test has been scored
-	// the isScored variable is passed in from the code-behind file
-	if (!isScored) {
-		$('#tabs li a:not(:first)').addClass('inactive');
-		$('.tabContent:not(:first)').hide();
-		$('.tabContent:first').show();
-	}
-	else {
-		$('#tabs li a:not(:last)').addClass('inactive');
-		$('.tabContent:not(:last)').hide();
-		$('.tabContent:last').show();
-	}
+﻿var numberOfQuestionsAnswered = 0,
+	isScored = false;
 
-	$('#tabs li a').click(function () {
-		var t = $(this).attr('href');
-		if ($(this).hasClass('inactive')) {
-			$('#tabs li a').addClass('inactive');
-			$(this).removeClass('inactive');
-			$('.tabContent').hide();
-			$(t).fadeIn('slow');
-		}
-		return false;
-	})
+$(document).ready(function () {
+    "use strict";
+    $('[id$="btnScoreTest"]').attr('disabled', 'disabled');
+    // Checks to see if the test has been scored
+    // the isScored variable is passed in from the code-behind file
+    if (isScored) {
+        $('[id$="navTabs"] a:last').tab('show'); // Select last tab
+    }
 });
 
 function moveOn(curQ) {
-	var qmTxt = curQ + "m";
-	var qlTxt = curQ + "l";
-	var mResponse = "";
-	var lResponse = "";
-	var qm = $('input[name$="' + qmTxt + '"]:radio');
-	var ql = $('input[name$="' + qlTxt + '"]:radio');
+    "use strict";
+    var qmTxt = curQ + "m",
+		qlTxt = curQ + "l",
+		mResponse = "",
+		lResponse = "",
+		qm = $('input[name$="' + qmTxt + '"]:radio'),
+		ql = $('input[name$="' + qlTxt + '"]:radio'),
+		mDone = 0,
+		lDone = 0,
+		x = 0,
+		hdr = $('#page-header');
 
-	var mDone = 0;
-	var lDone = 0;
+    for (x = 0; x < qm.length; x++) {
+        if (qm[x].checked) {
+            mDone = 1;
+            mResponse = qm[x].id.substr(-4, 3);
+            break;
+        }
+    }
 
-	for (var x = 0; x < qm.length; x++) {
-		if (qm[x].checked) {
-			mDone = 1;
-			mResponse = qm[x].id.substr(-4, 3);
-			break;
-		}
-	}
+    for (x = 0; x < ql.length; x++) {
+        if (ql[x].checked) {
+            lDone = 1;
+            lResponse = ql[x].id.substr(-4, 3);
+            break;
+        }
+    }
 
-	for (var x = 0; x < ql.length; x++) {
-		if (ql[x].checked) {
-			lDone = 1;
-			lResponse = ql[x].id.substr(-4, 3);
-			break;
-		}
-	}
-
-	if (mDone && lDone && (mResponse !== lResponse)) {
-		if (curQ != 30) {
-			curQ++;
-			var hdr = $('#page-header');
-			$(window).scrollTop($('td[id$="q' + pad2(curQ) + '"]').offset().top - hdr.height() - 20);
-		}
-	}
-	return true;
-};
+    if (mDone && lDone && (mResponse !== lResponse)) {
+        if (curQ < 30) {
+            curQ++;
+            $(window).scrollTop($('td[id$="q' + pad2(curQ) + '"]').offset().top - hdr.height() - 20);
+        }
+        numberOfQuestionsAnswered++;
+        if (numberOfQuestionsAnswered >= 30) {
+            $('[id$="btnScoreTest"]').removeAttr('disabled');
+        }
+    }
+    return true;
+}
 
 function pad2(number) {
-	return (number < 10 ? '0' : '') + number;
+    "use strict";
+    return (number < 10 ? '0' : '') + number;
 }
