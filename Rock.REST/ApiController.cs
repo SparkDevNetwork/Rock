60cfa13fcff4a1537ffc624ca6c7322f1a765b6e
David Turner <david@azturners.com>
2012-08-31 10:03:24 -0700

    Add Address Standardization and Geocoding to web.api

--- Rock.REST/ApiController.cs
+++ Rock.REST/ApiController.cs
@@ -8,11 +8,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
-using System.Security.Principal;
 using System.ServiceModel.Channels;
+using System.Web.Http;
 
-using Rock;
 using Rock.Data;
 
 namespace Rock.Rest
