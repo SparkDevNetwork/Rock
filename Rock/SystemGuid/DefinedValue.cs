18485812c283647dc933256a6b21ca00e3ad8abd
David Turner <david@azturners.com>
2012-09-14 08:57:20 -0700

    Update Contact Information block on person detail page to pull data.

---
+++ Rock/SystemGuid/DefinedValue.cs
@@ -0,0 +1,22 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+
+namespace Rock.SystemGuid
+{
+    /// <summary>
+    /// Static Guids used by the Rock ChMS application
+    /// </summary>
+    public static class DefinedValue
+    {
+		/// <summary>
+		/// Guid for the types of Person phone numbers (such as Primary, Secondary, etc.)
+		/// </summary>
+		public static Guid PERSON_PHONE_PRIMARY { get { return new Guid( "407E7E45-7B2E-4FCD-9605-ECB1339F2453" ); } }
+	
+    }
+}
