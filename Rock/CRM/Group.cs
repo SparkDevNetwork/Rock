89c3b431926d10f11308c1fd32b4e10d1fdf6e05
mikepetersonccv <mike@mikeapril.com>
2012-10-26 09:05:48 -0700

    minor namespace fixes

--- Rock/CRM/Group.cs
+++ Rock/CRM/Group.cs
@@ -112,7 +112,7 @@ namespace Rock.Crm
         /// Gets the auth entity.
         /// </summary>
         [NotMapped]
-        public override string EntityTypeName { get { return "Groups.Group"; } }
+        public override string EntityTypeName { get { return "Crm.Group"; } }
         
         /// <summary>
         /// Gets or sets the Groups.
