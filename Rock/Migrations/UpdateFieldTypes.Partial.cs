73ae1d31fed7265b63679d781ae52831a46ff09f
David Turner <david@azturners.com>
2012-10-26 10:50:53 -0700

    Fix early migration downgrade that inadvertenly had an insert that it shouldn't of

--- Rock/Migrations/UpdateFieldTypes.Partial.cs
+++ Rock/Migrations/UpdateFieldTypes.Partial.cs
@@ -50,7 +50,7 @@ INSERT INTO [coreFieldType] ([System],[Name],[Description],[Assembly],[Class],[C
         {
             Sql( @"
 DECLARE @AdminID int
-SELECT @AdminID = [Id] FROM [crmPerson] WHERE [Guid] = 'AD28DA19-4AF1-408F-9090-2672F8376F27'INSERT INTO [coreFieldType] ([System],[Name],[Description],[Assembly],[Class],[CreatedDateTime],[ModifiedDateTime],[CreatedByPersonId],[ModifiedByPersonId],[Guid])VALUES(1,'Currency','A Currency Field','Rock','Rock.Field.Types.Currency','Jul 19 2012  6:00:00:000AM','Jul 19 2012  6:00:00:000AM',1,1,'50EABC9A-A29D-4A65-984A-87891B230533')
+SELECT @AdminID = [Id] FROM [crmPerson] WHERE [Guid] = 'AD28DA19-4AF1-408F-9090-2672F8376F27'
 
 PRINT 'Updating Field Types'
 UPDATE [coreFieldType] SET [Class]='Rock.FieldTypes.Boolean',[ModifiedDateTime]='2012-07-13 13:00:19.380',[ModifiedByPersonId]=@AdminID WHERE [Guid]='1EDAFDED-DFE6-4334-B019-6EECBA89E05A'
