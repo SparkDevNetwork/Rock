41a83f1f4b7b979b3358d94dbe08afb2a828c128
David Turner <david@azturners.com>
2012-11-13 14:23:12 -0700

    Update post save and delete workflow triggers to use an async transaction.

--- Rock/Transactions/AuditTransaction.cs
+++ Rock/Transactions/AuditTransaction.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.IO;
 
 using Rock.Core;
 
