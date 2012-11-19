56740a605379b9470753ae0bcde6c7fc55e7821b
mikepetersonccv <mike@mikeapril.com>
2012-11-19 12:24:28 -0700

    sweep and fix copyright headers on cs files

--- Rock/Jobs/SendGridProcess.cs
+++ Rock/Jobs/SendGridProcess.cs
@@ -1,22 +1,8 @@
-#region License
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
- * under the License.
- * 
- */
-#endregion
-
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
 using System;
 using System.Web;
 using System.IO;
