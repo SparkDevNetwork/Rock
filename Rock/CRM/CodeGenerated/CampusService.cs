041580b5e267b1d627d700990b385d2c3293fb8b
mikepetersonccv <mike@mikeapril.com>
2012-11-19 16:58:28 -0700

    Updated CodeGenerator with the CanDelete and code gen'd

--- Rock/CRM/CodeGenerated/CampusService.cs
+++ Rock/CRM/CodeGenerated/CampusService.cs
@@ -68,5 +68,37 @@ namespace Rock.Crm
                     Guid = m.Guid,
                 });
         }
+
+        /// <summary>
+        /// Determines whether this instance can delete the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns>
+        ///   <c>true</c> if this instance can delete the specified item; otherwise, <c>false</c>.
+        /// </returns>
+        public bool CanDelete( Campus item, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            RockContext context = new RockContext();
+            context.Database.Connection.Open();
+
+            using ( var cmdCheckRef = context.Database.Connection.CreateCommand() )
+            {
+                cmdCheckRef.CommandText = string.Format( "select count(*) from crmGroup where CampusId = {0} ", item.Id );
+                var result = cmdCheckRef.ExecuteScalar();
+                int? refCount = result as int?;
+                if ( refCount > 0 )
+                {
+                    Type entityType = RockContext.GetEntityFromTableName( "crmGroup" );
+                    string friendlyName = entityType != null ? entityType.GetFriendlyTypeName() : "crmGroup";
+
+                    errorMessage = string.Format("This {0} is assigned to a {1}.", Campus.FriendlyTypeName, friendlyName);
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
--- Rock/Util/CodeGenerated/WorkflowLogService.cs
+++ Rock/Util/CodeGenerated/WorkflowLogService.cs
@@ -69,5 +69,19 @@ namespace Rock.Util
                     Guid = m.Guid,
                 });
         }
+
+        /// <summary>
+        /// Determines whether this instance can delete the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns>
+        ///   <c>true</c> if this instance can delete the specified item; otherwise, <c>false</c>.
+        /// </returns>
+        public bool CanDelete( WorkflowLog item, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
     }
 }
