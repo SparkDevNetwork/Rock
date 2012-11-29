db81fc08430b68277ec83a2ddceed84dcaa2534a
mikepetersonccv <mike@mikeapril.com>
2012-11-29 13:47:50 -0700

    fix up filename for grid related javascript

--- Rock/Web/UI/RockPage.cs
+++ Rock/Web/UI/RockPage.cs
@@ -725,8 +725,8 @@ namespace Rock.Web.UI
             // Add the page admin script
             AddScriptLink( Page, "~/Scripts/Rock/page-admin.js" );
             
-            // add the confirmDelete script for Grids
-            AddScriptLink( Page, "~/Scripts/Rock/confirmDelete.js" );
+            // add the scripts for RockGrid
+            AddScriptLink( Page, "~/Scripts/Rock/grid.js" );
 
             // add Kendo js and css here since we don't know if a partial postback will have a kendo control until runtime
             AddScriptLink( Page, "~/scripts/Kendo/kendo.core.min.js" );
