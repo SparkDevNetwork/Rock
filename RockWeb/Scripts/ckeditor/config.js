7e3c9cd9d8e14c8fcbd626e7543b9c93f84fadb9
mikepetersonccv <mike@mikeapril.com>
2012-11-29 13:21:59 -0700

    + Updated ckeditor toolbar to have RockCustomConfigLight and RockCustomConfigFull + Updated HtmlContent block to use same ckeditor control as MarketingCampaignAds and FieldType.HtmlField + HtmlContent now uses the RockCustomConfigFull toolbar

--- RockWeb/Scripts/ckeditor/config.js
+++ RockWeb/Scripts/ckeditor/config.js
@@ -6,20 +6,27 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.editorConfig = function( config )
 {
 	// Custom config that removes some of the buggy plugins
-	config.toolbar = 'RockCustomConfig';
+	config.toolbar = 'RockCustomConfigLight';
  
-	config.toolbar_RockCustomConfig =
+	config.toolbar_RockCustomConfigLight =
 	[
-		{ name: 'document', items : [ 'Source', 'Save', 'Preview' ] },
-		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
-		{ name: 'tools', items : [ 'Maximize','-','About' ] },
-                '/',
-		{ name: 'styles', items : [ 'Styles','Format' ] },
-		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
-		{ name: 'colors', items : [ 'TextColor','BGColor' ] },
-		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
-		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+        { name: 'document', items: ['Source'] },
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'NumberedList', 'BulletedList', 'Link', 'Image', 'PasteFromWord', '-', 'RemoveFormat'] },
+        { name: 'editing', items: ['Format'] },
 	];
 
+	config.toolbar_RockCustomConfigFull =
+	[
+        { name: 'document', items: ['Source'] },
+        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+        { name: 'editing', items: ['Find', 'Replace', '-', 'Scayt'] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'styles', items: ['Styles', 'Format'] },
+                '/',
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] },
+        {
+            name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
+              'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'Image', 'Table']
+        },
+	];
 };
