32cd13f73c5e3fcfcb1c29cc455505323f617c4d
David Turner <david@azturners.com>
2012-12-18 14:28:21 -0700

    Add base abstract class for cached models, and update pagecache object not to use dto object

---
+++ Rock/Web/Cache/CachedModel.cs
@@ -0,0 +1,192 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Rock.Model;
+using Rock.Security;
+
+namespace Rock.Web.Cache
+{
+    /// <summary>
+    /// A Secured data transfer object
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Serializable]
+    public abstract class CachedModel<T> : ISecured, Rock.Attribute.IHasAttributes
+        where T : ISecured, Rock.Attribute.IHasAttributes, new()
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>
+        /// The id.
+        /// </value>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the GUID.
+        /// </summary>
+        /// <value>
+        /// The GUID.
+        /// </value>
+        public virtual Guid Guid { get; set; }
+
+        /// <summary>
+        /// Copies from model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        public virtual void CopyFromModel( Rock.Data.IEntity model )
+        {
+            this.Id = model.Id;
+            this.Guid = model.Guid;
+
+            var secureModel = model as ISecured;
+            if ( secureModel != null )
+            {
+                this.TypeId = secureModel.TypeId;
+                this.TypeName = secureModel.TypeName;
+                this.SupportedActions = secureModel.SupportedActions;
+            }
+        }
+
+        #region ISecured Implementation
+
+        /// <summary>
+        /// Gets the Entity Type ID for this entity.
+        /// </summary>
+        /// <value>
+        /// The type id.
+        /// </value>
+        [DataMember]
+        public virtual int TypeId { get; private set; }
+
+        /// <summary>
+        /// The auth entity. Classes that implement the <see cref="ISecured" /> interface should return
+        /// a value that is unique across all <see cref="ISecured" /> classes.  Typically this is the
+        /// qualified name of the class.
+        /// </summary>
+        [DataMember]
+        public virtual string TypeName { get; private set; }
+
+        /// <summary>
+        /// A parent authority.  If a user is not specifically allowed or denied access to
+        /// this object, Rock will check access to the parent authority specified by this property.
+        /// </summary>
+        public virtual ISecured ParentAuthority
+        {
+            get
+            {
+                if ( this.Id == 0 )
+                {
+                    return new GlobalDefault();
+                }
+                else
+                {
+                    return new T();
+                }
+            }
+        }
+
+        /// <summary>
+        /// A list of actions that this class supports.
+        /// </summary>
+        public virtual List<string> SupportedActions { get; private set; }
+
+        /// <summary>
+        /// Return <c>true</c> if the user is authorized to perform the selected action on this object.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <param name="person">The person.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified action is authorized; otherwise, <c>false</c>.
+        /// </returns>
+        public virtual bool IsAuthorized( string action, Person person )
+        {
+            return Security.Authorization.Authorized( this, action, person );
+        }
+
+        /// <summary>
+        /// If a user or role is not specifically allowed or denied to perform the selected action,
+        /// return <c>true</c> if they should be allowed anyway or <c>false</c> if not.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns></returns>
+        public virtual bool IsAllowedByDefault( string action )
+        {
+            return action == "View";
+        }
+
+        #endregion
+
+        #region IHasAttribute Implementation
+
+        /// <summary>
+        /// Dictionary of categorized attributes.  Key is the category name, and Value is list of attributes in the category
+        /// </summary>
+        /// <value>
+        /// The attribute categories.
+        /// </value>
+        public SortedDictionary<string, List<string>> AttributeCategories
+        {
+            get
+            {
+                var attributeCategories = new SortedDictionary<string, List<string>>();
+
+                foreach ( int id in AttributeIds )
+                {
+                    var attribute = AttributeCache.Read( id );
+                    if ( !attributeCategories.ContainsKey( attribute.Key ) )
+                        attributeCategories.Add( attribute.Category, new List<string>() );
+                    attributeCategories[attribute.Category].Add( attribute.Key );
+                }
+
+                return attributeCategories;
+            }
+
+            set { }
+        }
+
+        /// <summary>
+        /// List of attributes associated with the page.  This object will not include values.
+        /// To get values associated with the current page instance, use the AttributeValues
+        /// </summary>
+        public Dictionary<string, Rock.Web.Cache.AttributeCache> Attributes
+        {
+            get
+            {
+                var attributes = new Dictionary<string, Rock.Web.Cache.AttributeCache>();
+
+                foreach ( int id in AttributeIds )
+                {
+                    Rock.Web.Cache.AttributeCache attribute = AttributeCache.Read( id );
+                    attributes.Add( attribute.Key, attribute );
+                }
+
+                return attributes;
+            }
+
+            set
+            {
+                this.AttributeIds = new List<int>();
+                foreach ( var attribute in value )
+                    this.AttributeIds.Add( attribute.Value.Id );
+            }
+        }
+        /// <summary>
+        /// The attribute ids
+        /// </summary>
+        protected List<int> AttributeIds = new List<int>();
+
+        /// <summary>
+        /// Dictionary of all attributes and their value.
+        /// </summary>
+        public Dictionary<string, List<Rock.Model.AttributeValueDto>> AttributeValues { get; set; }
+
+        #endregion
+    }
+}
