02e659848abf5580f88dd1094cefa8f9cb08d30b
mikepetersonccv <mike@mikeapril.com>
2012-12-19 14:09:40 -0700

    + fixed admin controls to work consistently across browsers + fixed bug in block/move

--- RockWeb/Scripts/Rock/popup.js
+++ RockWeb/Scripts/Rock/popup.js
@@ -3,59 +3,49 @@
     $find('modal-popup').hide();
 }
 
-$(document).ready(function () {
+function showModalPopup(sender, popupUrl) {
 
-    // Bind the click event for all of the links that use the iframe to show the 
-    // modal div/iframe
-    $('a.show-modal-iframe').click(function () {
+    var $primaryBtn = $('#modal-popup_panel div.modal-footer a.btn.primary');
+    if (sender.attr('primary-button') !== undefined) {
+        $primaryBtn.text(sender.attr('primary-button'));
+    } else {
+        $primaryBtn.text('Save');
+    }
+    if ($primaryBtn.text() !== '') {
+        $primaryBtn.show();
+    } else {
+        $primaryBtn.hide();
+    }
 
-        var $primaryBtn = $('#modal-popup_panel div.modal-footer a.btn.primary');
-        if ($(this).attr('primary-button') !== undefined) {
-            $primaryBtn.text($(this).attr('primary-button'));
-        } else {
-            $primaryBtn.text('Save');
-        }
-        if ($primaryBtn.text() !== '') {
-            $primaryBtn.show();
-        } else {
-            $primaryBtn.hide();
-        }
+    var $secondaryBtn = $('#modal-popup_panel div.modal-footer a.btn.secondary');
+    if (sender.attr('secondary-button') !== undefined) {
+        $secondaryBtn.text(sender.attr('secondary-button'));
+    } else {
+        $secondaryBtn.text('Cancel');
+    }
+    if ($secondaryBtn.text() !== '') {
+        $secondaryBtn.show();
+    } else {
+        $secondaryBtn.hide();
+    }
 
-        var $secondaryBtn = $('#modal-popup_panel div.modal-footer a.btn.secondary');
-        if ($(this).attr('secondary-button') !== undefined) {
-            $secondaryBtn.text($(this).attr('secondary-button'));
-        } else {
-            $secondaryBtn.text('Cancel');
-        }
-        if ($secondaryBtn.text() !== '') {
-            $secondaryBtn.show();
-        } else {
-            $secondaryBtn.hide();
-        }
+    // Use the anchor tag's href attribute as the source for the iframe
+    $('#modal-popup_iframe').attr('src', popupUrl);
 
-        // Use the anchor tag's href attribute as the source for the iframe
-        $('#modal-popup_iframe').attr('src', $(this).attr('href'));
+    // If the anchor tag specifies a modal height, set the dialog's height
+    if (sender.attr('height') != undefined) {
+        $('#modal-popup_panel div.modal-body').css('height', sender.attr('height'));
+        $('#modal-popup_contentPanel.iframe').css('height', sender.attr('height'));
+    }
+    else {
+        $('#modal-popup_panel div.modal-body').css('height', '');
+        $('#modal-popup_contentPanel.iframe').css('height', '500px');
+    }
 
-        // If the anchor tag specifies a modal height, set the dialog's height
-        if ($(this).attr('height') != undefined) {
-            $('#modal-popup_panel div.modal-body').css('height', $(this).attr('height'));
-            $('#modal-popup_contentPanel.iframe').css('height', $(this).attr('height'));
-        }
-        else {
-            $('#modal-popup_panel div.modal-body').css('height', '');
-            $('#modal-popup_contentPanel.iframe').css('height', $(this).attr('height'));
-        }
+    // Use the anchor tag's title attribute as the title of the dialog box
+    if (sender.attr('title') != undefined)
+        $('#modal-popup_panel h3').html(sender.attr('title') + ' <small></small>');
 
-        // Use the anchor tag's title attribute as the title of the dialog box
-        if ($(this).attr('title') != undefined)
-            $('#modal-popup_panel h3').html($(this).attr('title') + ' <small></small>');
-
-        // popup the dialog box
-        $find('modal-popup').show();
-
-        // Cancel the default behavior of the anchor tag
-        return false;
-
-    });
-
-});
+    // popup the dialog box
+    $find('modal-popup').show();
+}
