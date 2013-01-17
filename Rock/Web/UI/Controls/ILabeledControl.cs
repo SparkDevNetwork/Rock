b85cdafa89efaf72a1982e4469d8783e597caf77
unknown <mpeterson@ccv12-d009.ccvonline.com>
2013-01-17 12:16:04 -0700

    made an ILabelControl interface for use with all of our various LabledXXX controls so that our DataAnnotationValidator can take advantage of it

---
+++ Rock/Web/UI/Controls/ILabeledControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Web.UI.Controls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    interface ILabeledControl
+    {
+        /// <summary>
+        /// Gets or sets the label text.
+        /// </summary>
+        /// <value>
+        /// The label text.
+        /// </value>
+        string LabelText { get; set; }
+    }
+}
