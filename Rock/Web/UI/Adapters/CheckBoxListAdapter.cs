4dd4236095f7e28bd8bdaa9992ecdd28be58f833
David Turner <david@azturners.com>
2013-02-04 09:57:22 -0700

    Add control adapters for CheckBox, CheckBoxList, and RadioButton

---
+++ Rock/Web/UI/Adapters/CheckBoxListAdapter.cs
@@ -0,0 +1,78 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.Adapters;
+
+namespace Rock.Web.UI.Adapters
+{
+    /// <summary>
+    /// Control adapter for checkbox list
+    /// </summary>
+    public class CheckBoxListAdapter : WebControlAdapter
+    {
+        /// <summary>
+        /// Creates the beginning tag for the Web control in the markup that is transmitted to the target browser.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> containing methods to render the target-specific output.</param>
+        protected override void RenderBeginTag( System.Web.UI.HtmlTextWriter writer )
+        {
+        }
+
+        /// <summary>
+        /// Creates the ending tag for the Web control in the markup that is transmitted to the target browser.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> containing methods to render the target-specific output.</param>
+        protected override void RenderEndTag( System.Web.UI.HtmlTextWriter writer )
+        {
+        }
+
+        /// <summary>
+        /// Generates the target-specific inner markup for the Web control to which the control adapter is attached.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> containing methods to render the target-specific output.</param>
+        protected override void RenderContents( System.Web.UI.HtmlTextWriter writer )
+        {
+            CheckBoxList cbl = Control as CheckBoxList;
+            if ( cbl != null )
+            {
+                int i = 0;
+                foreach ( ListItem li in cbl.Items )
+                {
+                    writer.WriteLine();
+                    writer.AddAttribute( "class", "checkbox inline" );
+                    writer.RenderBeginTag( HtmlTextWriterTag.Label );
+
+                    string itemId = string.Format( "{0}_{1}", cbl.ClientID, i++ );
+                    writer.AddAttribute( "id", itemId );
+                    writer.AddAttribute( "type", "checkbox" );
+                    writer.AddAttribute( "name", cbl.UniqueID );
+                    writer.AddAttribute( "value", li.Value );
+                    if ( li.Selected )
+                    {
+                        writer.AddAttribute( "checked", "checked" );
+                    }
+                    writer.RenderBeginTag( HtmlTextWriterTag.Input );
+                    writer.RenderEndTag();
+
+                    writer.Write( li.Text );
+
+                    writer.RenderEndTag();
+
+                    if ( Page != null && Page.ClientScript != null )
+                    {
+                        Page.ClientScript.RegisterForEventValidation( cbl.UniqueID, li.Value );
+                    }
+                }
+
+                if ( Page != null && Page.ClientScript != null )
+                {
+                    Page.ClientScript.RegisterForEventValidation( cbl.UniqueID );
+                }
+            }
+        }
+    }
+}
---
+++ Rock/Web/UI/Controls/HelpBlock.cs
@@ -0,0 +1,57 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Rock.Web.UI.Controls
+{
+    /// <summary>
+    /// Displays a help icon that will display the Text property when clicked
+    /// </summary>
+    [ToolboxData( "<{0}:HelpBlock runat=server></{0}:HelpBlock>" )]
+    public class HelpBlock : Literal
+    {
+        protected override void OnInit( System.EventArgs e )
+        {
+            string script = @"
+Sys.Application.add_load(function () {
+
+    $('a.help').click(function () {
+        $(this).siblings('div.alert-info').slideToggle(function(){
+            $('.scroll-container').each(function() {
+                $(this).tinyscrollbar_update('relative');
+            });
+        });
+    });
+});
+";
+            this.Page.ClientScript.RegisterStartupScript( this.Page.GetType(), "help-block", script, true );
+        }
+        public override void RenderControl( HtmlTextWriter writer )
+        {
+            if ( this.Text.Trim() != string.Empty )
+            {
+                writer.AddAttribute( "class", "help" );
+                writer.AddAttribute( "href", "#" );
+                writer.RenderBeginTag( HtmlTextWriterTag.A );
+                writer.AddAttribute( "class", "icon-question-sign" );
+                writer.RenderBeginTag( HtmlTextWriterTag.I );
+                writer.RenderEndTag();
+                writer.RenderEndTag();
+
+                writer.AddAttribute( "class", "alert alert-info" );
+                writer.AddAttribute( "style", "display:none" );
+                writer.RenderBeginTag( HtmlTextWriterTag.Div );
+                writer.RenderBeginTag( HtmlTextWriterTag.Small );
+                writer.Write( this.Text.Trim() );
+                writer.RenderEndTag();
+                writer.RenderEndTag();
+            }
+        }
+    }
+}
