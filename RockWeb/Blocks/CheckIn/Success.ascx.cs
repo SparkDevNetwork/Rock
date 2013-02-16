4c357f8cbbca5cd2364d7c12ec4026f0bfcd9160
edmistj <jonedmiston@ccvonline.com>
2013-02-15 17:36:10 -0700

    Checkin UI changes

--- RockWeb/Blocks/CheckIn/Success.ascx.cs
+++ RockWeb/Blocks/CheckIn/Success.ascx.cs
@@ -41,7 +41,7 @@ namespace RockWeb.Blocks.CheckIn
                                     foreach ( var schedule in group.Schedules.Where( s => s.Selected ) )
                                     {
                                         var li = new HtmlGenericControl("li");
-                                        li.InnerText = string.Format("{0} was checked into {1} at {2} at {3}",
+                                        li.InnerText = string.Format("{0} was checked into {1} for the {2} at {3}",
                                             person.ToString(), group.ToString(), location.ToString(), schedule.ToString(), schedule.SecurityCode);
                                         phResults.Controls.Add(li);
                                     }
