14c6bb5bec15e1d65a787c9e9ddaeacd52fd1283
Bobbi Perreault <bcp@faxt.com>
2013-02-26 14:10:03 -0500

    Migration

---
+++ Rock/Migrations/201302261729333_financialBatchNewField.cs
@@ -0,0 +1,32 @@
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+namespace Rock.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class financialBatchNewField : RockMigration
+    {
+        /// <summary>
+        /// Operations to be performed during the upgrade process.
+        /// </summary>
+        public override void Up()
+        {
+            AddColumn("dbo.FinancialBatch", "BatchTypeValueId", c => c.Int(nullable: false));
+        }
+        
+        /// <summary>
+        /// Operations to be performed during the downgrade process.
+        /// </summary>
+        public override void Down()
+        {
+            DropColumn("dbo.FinancialBatch", "BatchTypeValueId");
+        }
+    }
+}
