a5b8f8956278fe82b0b55dec5db1d297ec8cdd18
David Turner <david@azturners.com>
2013-02-28 06:26:38 -0700

    Move common properties of Search MEF components to base class

--- Rock/Search/Person/Phone.cs
+++ Rock/Search/Person/Phone.cs
@@ -3,16 +3,12 @@
 // SHAREALIKE 3.0 UNPORTED LICENSE:
 // http://creativecommons.org/licenses/by-nc-sa/3.0/
 //
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
-using System.Web;
-using Rock;
-using Rock.Attribute;
+
 using Rock.Model;
-using Rock.Web.UI;
 
 namespace Rock.Search.Person
 {
@@ -22,35 +18,22 @@ namespace Rock.Search.Person
     [Description( "Person Phone Search" )]
     [Export(typeof(SearchComponent))]
     [ExportMetadata("ComponentName", "Person Phone")]
-    [TextField( "Search Label", "The text to display in the search type dropdown", false, "Phone" )]
-    [TextField( "Result URL", "The url to redirect user to after they have entered search text.  (use '{0}' for the search text)" )]
     public class Phone : SearchComponent
     {
-        /// <summary>
-        /// The text to display as the search type
-        /// </summary>
-        public override string SearchLabel
-        {
-            get
-            {
-                if ( !String.IsNullOrWhiteSpace( GetAttributeValue( "SearchLabel" ) ) )
-                    return GetAttributeValue( "SearchLabel" );
-                else
-                    return "Phone";
-            }
-        }
 
         /// <summary>
-        /// The URL to redirect user to for search
+        /// Gets the attribute value defaults.
         /// </summary>
-        public override string ResultUrl
+        /// <value>
+        /// The attribute defaults.
+        /// </value>
+        public override Dictionary<string, string> AttributeValueDefaults
         {
             get
             {
-                if ( !String.IsNullOrWhiteSpace( GetAttributeValue( "ResultURL" ) ) )
-                    return GetAttributeValue( "ResultURL" );
-                else
-                    return string.Empty;
+                var defaults = new Dictionary<string, string>();
+                defaults.Add( "SearchLabel", "Phone" );
+                return defaults;
             }
         }
 
