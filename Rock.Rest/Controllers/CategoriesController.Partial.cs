f0b775a65d288c3b127b32b8a843229bed984c7f
David Turner <david@azturners.com>
2013-03-05 06:30:40 -0700

    Add GetEntityType() method to the EntityTypeCache object

--- Rock.Rest/Controllers/CategoriesController.Partial.cs
+++ Rock.Rest/Controllers/CategoriesController.Partial.cs
@@ -66,7 +66,7 @@ namespace Rock.Rest.Controllers
                     // Get the GetByCategory method
                     if ( cachedEntityType.AssemblyName != null )
                     {
-                        Type entityType = Type.GetType( cachedEntityType.AssemblyName );
+                        Type entityType = cachedEntityType.GetEntityType();
                         if ( entityType != null )
                         {
                             Type[] modelType = { entityType };
