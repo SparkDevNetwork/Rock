eda4fa797175fceb73e49676e1b950092a81a882
mikepetersonccv <MikePeterson@ccvonline.com>
2013-03-08 15:24:51 -0700

    + new GroupTypeFieldAttribute/FieldType, new CampusFieldAttribute/FieldType + updated GroupFieldType to use GroupPicker instead of DropDownList + fixed up <none> html in ItemPickers

---
+++ Rock/Attribute/GroupTypeFieldAttribute.cs
@@ -0,0 +1,33 @@
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rock.Attribute
+{
+    /// <summary>
+    /// Field Attribute to select 0 or 1 GroupType
+    /// </summary>
+    public class GroupTypeFieldAttribute : FieldAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GroupTypeFieldAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="required">if set to <c>true</c> [required].</param>
+        /// <param name="defaultGroupTypeId">The default group type id.</param>
+        /// <param name="category">The category.</param>
+        /// <param name="order">The order.</param>
+        /// <param name="key">The key.</param>
+        public GroupTypeFieldAttribute( string name, string description = "", bool required = true, string defaultGroupTypeId = "", string category = "", int order = 0, string key = null )
+            : base( name, description, required, defaultGroupTypeId, category, order, key, typeof( Rock.Field.Types.GroupTypeFieldType ).FullName )
+        {
+        }
+    }
+}
