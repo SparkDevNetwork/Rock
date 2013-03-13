d21411ad6dd47bebe5dbf9a60ef51171aa8e3796
JasonOffutt <jason.offutt@gmail.com>
2013-03-13 14:04:25 -0700

    Stubbing out pledge list modules.

---
+++ RockWeb/Blocks/Administration/PledgeList.ascx
@@ -0,0 +1,14 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PledgeList.ascx.cs" Inherits="RockWeb.Blocks.Administration.PledgeList" %>
+
+<asp:UpdatePanel ID="upPanel" runat="server">
+    <ContentTemplate>
+        <Rock:ModalAlert ID="mdGridWarning" runat="server"/>
+        <Rock:GridFilter ID="rFilter" runat="server">
+            <Rock:PersonPicker ID="ppPerson" runat="server"/>
+        </Rock:GridFilter>
+        <Rock:Grid ID="gPledges" runat="server" AllowSorting="True" OnRowDataBound="gPledges_RowDataBound" OnRowSelected="gPledges_Edit">
+            <Columns>
+            </Columns>
+        </Rock:Grid>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Blocks/Administration/PledgeList.ascx.cs
@@ -0,0 +1,79 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Attribute;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Administration
+{
+    [DetailPage]
+    public partial class PledgeList : RockBlock
+    {
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            gPledges.DataKeyNames = new[] { "id" };
+            gPledges.Actions.IsAddEnabled = true;
+            gPledges.Actions.AddClick += gPledges_Add;
+            gPledges.GridRebind += gPledges_GridRebind;
+
+            bool canAddEditDelete = IsUserAuthorized( "Edit" );
+            gPledges.Actions.IsAddEnabled = canAddEditDelete;
+            gPledges.IsDeleteEnabled = canAddEditDelete;
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            if ( !Page.IsPostBack )
+            {
+                BindGrid();
+            }
+            
+            base.OnLoad( e );
+        }
+
+        private void BindGrid()
+        {
+            var pledgeService = new PledgeService();
+            var sortProperty = gPledges.SortProperty;
+            var pledges = pledgeService.Queryable();
+
+            // TODO: Add filter criteria
+
+            gPledges.DataSource = sortProperty != null ? pledges.Sort( sortProperty ).ToList() : pledges.OrderBy( p => p.FundId ).ToList();
+            gPledges.DataBind();
+        }
+
+        private void gPledges_GridRebind( object sender, EventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+        private void gPledges_Add( object sender, EventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void gPledges_RowDataBound( object sender, GridViewRowEventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void gPledges_Edit( object sender, RowEventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
