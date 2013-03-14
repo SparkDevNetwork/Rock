05f570e59870a3b5fffc8429e9e40551c6eb0b91
JasonOffutt <jason.offutt@gmail.com>
2013-03-14 11:18:41 -0700

    First working version of Populate Financials seed script.

--- Dev Tools/Sql/Populate_Financials.sql
+++ Dev Tools/Sql/Populate_Financials.sql
@@ -4,7 +4,8 @@ declare @ValueId int
 declare @FundId int
 declare @MissionsFundId int
 declare @BuildingFundId int
-declare @PersonId int
+declare @PledgyPersonId int
+declare @DoraPersonId int
 declare @PledgeId int
 
 -- Fund Attributes
@@ -16,8 +17,22 @@ end
 set @FundEntityTypeId = (select Id from EntityType where Name = 'Rock.Model.Fund')
 
 -- Defined Types
+-- TODO: This DefinedType will probably need to move out of this script and be included in a Migration
+insert into DefinedType ( IsSystem, FieldTypeId [Order], Category, Name, [Description], [Guid] )
+values ( 1, 1, 0, 'Financial', 'Frequency Type', 'Types of payment frequencies', newid() )
+set @TypeId = scope_identity()
 
 -- Defined Values
+-- TODO: These DefinedValues will probably need to move out of this script and be included in a Migration
+insert into DefinedValue ( IsSystem, DefinedTypeid, [Order], Name, [Description], [Guid] )
+values ( 1, @TypeId, 0, 'Weekly', 'Weekly', newid() )
+
+insert into DefinedValue ( IsSystem, DefinedTypeid, [Order], Name, [Description], [Guid] )
+values ( 1, @TypeId, 0, 'Bi-Weekly', 'Bi-Weekly', newid() )
+
+insert into DefinedValue ( IsSystem, DefinedTypeid, [Order], Name, [Description], [Guid] )
+values ( 1, @TypeId, 0, 'Monthly', 'Monthly', newid() )
+set @ValueId = scope_identity()
 
 -- Funds
 -- Create "General Fund" that doesn't take pledges
@@ -32,7 +47,7 @@ set @FundId = scope_identity()
 -- Campus-specific "Building Campaign" that allows pledges, child of "Building Campaign"
 insert into Fund ( Name, PublicName, [Description], ParentFundId, IsTaxDeductible, [Order], IsActive, StartDate, EndDate, IsPledgable, GlCode,	Entity,	EntityId, [Guid], FundTypeValueId )
 values ( 'gilbertbuildings', 'Gilbert Building Campaign Fund', 'This is the Gilbert Building Campaign fund', @FundId, 1, 1, 1, dateadd(day, -90, getdate()), dateadd(day, 90, getdate()), 1, '9101112', 'Rock.Model.Fund', @FundEntityTypeId, newid(), null )
-set @BuldingFundId = scope_identity()
+set @BuildingFundId = scope_identity()
 
 -- Missions Fund that takes pledges
 insert into Fund ( Name, PublicName, [Description], ParentFundId, IsTaxDeductible, [Order], IsActive, StartDate, EndDate, IsPledgable, GlCode,	Entity,	EntityId, [Guid], FundTypeValueId )
@@ -42,11 +57,21 @@ set @MissionsFundId = scope_identity()
 -- Person
 insert into Person ( IsSystem, GivenName, NickName, LastName, PhotoId, BirthDay, BirthMonth, BirthYear, Gender, AnniversaryDate, GraduationDate, Email, IsEmailActive, EmailNote, ViewedCount, [Guid], RecordTypeValueId, RecordStatusValueId, RecordStatusReasonId, PersonStatusValueId, SuffixValueId, TitleValueId, MaritalStatusValueId, IsDeceased )
 values (  0, 'Pledgy', 'The Pledgester', 'McPledgerson', null, 1, 1, 1980, 0, null, null, 'pledgy@thepledgester.com', 1, 'Personal Email', null, newid(), 1, 3, null, null, null, null, null, 0 )
-set @PersonId = scope_identity()
+set @PledgyPersonId = scope_identity()
+
+insert into Person ( IsSystem, GivenName, NickName, LastName, PhotoId, BirthDay, BirthMonth, BirthYear, Gender, AnniversaryDate, GraduationDate, Email, IsEmailActive, EmailNote, ViewedCount, [Guid], RecordTypeValueId, RecordStatusValueId, RecordStatusReasonId, PersonStatusValueId, SuffixValueId, TitleValueId, MaritalStatusValueId, IsDeceased )
+values (  0, 'Dora', null, 'Donatesalot', null, 1, 1, 1980, 0, null, null, 'dora@thepledgester.com', 1, 'Personal Email', null, newid(), 1, 3, null, null, null, null, null, 0 )
+set @DoraPersonId = scope_identity()
 
 -- Pledges
 insert into Pledge ( PersonId, FundId, Amount, StartDate, EndDate, FrequencyAmount, [Guid], FrequencyTypeValueId )
-values ( @PersonId, @BuildingFundId, 2000.00, dateadd(day, -30, getdate()), dateadd(day, 30, getdate()), 100.00, newid(), @ValueId )
+values ( @PledgyPersonId, @BuildingFundId, 2000.00, dateadd(day, -30, getdate()), dateadd(day, 30, getdate()), 100.00, newid(), @ValueId )
 
 insert into Pledge ( PersonId, FundId, Amount, StartDate, EndDate, FrequencyAmount, [Guid], FrequencyTypeValueId )
-values ( @PersonId, @MissionsFundId, 5000.00, dateadd(day, -30, getdate()), dateadd(day, 30, getdate()), 50.00, newid(), @ValueId )
+values ( @PledgyPersonId, @MissionsFundId, 5000.00, dateadd(day, -30, getdate()), dateadd(day, 30, getdate()), 50.00, newid(), @ValueId )
+
+insert into Pledge ( PersonId, FundId, Amount, StartDate, EndDate, FrequencyAmount, [Guid], FrequencyTypeValueId )
+values ( @DoraPersonId, @BuildingFundId, 2000.00, dateadd(day, -30, getdate()), dateadd(day, 30, getdate()), 100.00, newid(), @ValueId )
+
+insert into Pledge ( PersonId, FundId, Amount, StartDate, EndDate, FrequencyAmount, [Guid], FrequencyTypeValueId )
+values ( @DoraPersonId, @MissionsFundId, 5000.00, dateadd(day, -30, getdate()), dateadd(day, 30, getdate()), 50.00, newid(), @ValueId )
