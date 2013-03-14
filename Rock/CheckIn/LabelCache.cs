cce930b65133b71c139c2b68e1e174d5373a7d4f
David Turner <david@azturners.com>
2013-03-14 06:05:37 -0700

    Update checkin to print labels created from binary file types and group type attributes rather than from hard-coded values.

---
+++ Rock/CheckIn/LabelCache.cs
@@ -0,0 +1,40 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System;
+using System.Collections.Generic;
+
+namespace Rock.CheckIn
+{
+    /// <summary>
+    /// Cached Check in Label
+    /// </summary>
+    public class LabelCache
+    {
+        /// <summary>
+        /// Gets or sets the GUID.
+        /// </summary>
+        /// <value>
+        /// The GUID.
+        /// </value>
+        public Guid Guid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>
+        /// The URL.
+        /// </value>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the merge fields.
+        /// </summary>
+        /// <value>
+        /// The merge fields.
+        /// </value>
+        public Dictionary<string, string> MergeFields { get; set; }
+    }
+}
