11e596e94ed19eeb6a18003d150395238829de7c
mikepetersonccv <MikePeterson@ccvonline.com>
2013-03-21 16:48:06 -0700

    fix up ul/li html bugs in sitemap

--- RockWeb/Blocks/Administration/SiteMap.ascx.cs
+++ RockWeb/Blocks/Administration/SiteMap.ascx.cs
@@ -26,7 +26,7 @@ public partial class SiteMap : RockBlock
         foreach ( var page in pageService.Queryable().Where( a => a.ParentPageId == null ).OrderBy( a => a.Order ).ThenBy( a => a.Name) )
         {
             treeHtml += string.Format( "<li><i class=\"icon-file-alt\"></i><a href='{0}' >" + page.Name + "</a>" + Environment.NewLine, RockPage.BuildUrl( page.Id, new Dictionary<string, string>() ) );
-            AddChildNodes( ref treeHtml, page, pageList );
+            AddChildNodes( ref treeHtml, page, pageList, false );
             treeHtml += "</li>" + Environment.NewLine;
         }
 
@@ -40,16 +40,21 @@ public partial class SiteMap : RockBlock
     /// <param name="nodeHtml">The node HTML.</param>
     /// <param name="parentPage">The parent page.</param>
     /// <param name="pageList">The page list.</param>
-    protected void AddChildNodes( ref string nodeHtml, Rock.Model.Page parentPage, List<Rock.Model.Page> pageList )
+    protected void AddChildNodes( ref string nodeHtml, Rock.Model.Page parentPage, List<Rock.Model.Page> pageList, bool writeLiTag )
     {
         var childPages = pageList.Where( a => a.ParentPageId.Equals( parentPage.Id ) );
         if ( childPages.Count() > 0 )
         {
+            if ( writeLiTag )
+            {
+                nodeHtml += "<li>";
+            }
+
             nodeHtml += "<ul>" + Environment.NewLine;
 
             foreach ( var childPage in childPages.OrderBy( a => a.Order ).ThenBy( a => a.Name ) )
             {
-                string childNodeHtml = string.Format( "<li><i class=\"icon-file-alt\"></i><a href='{0}' >" + childPage.Name + "</a>" + Environment.NewLine, RockPage.BuildUrl( childPage.Id, new Dictionary<string, string>() ) );
+                string childNodeHtml = string.Format( "<li><i class=\"icon-file-alt\"></i><a href='{0}' class='btn'>" + childPage.Name + "</a>" + Environment.NewLine, RockPage.BuildUrl( childPage.Id, new Dictionary<string, string>() ) );
                 if ( childPage.Blocks.Count > 0 )
                 {
                     childNodeHtml += "<ul>";
@@ -57,15 +62,24 @@ public partial class SiteMap : RockBlock
                     {
                         childNodeHtml += string.Format( "<li><i class=\"icon-th-large\"></i>{1}:{0}</li>", block.Name, block.BlockType.Name );
                     }
+                    AddChildNodes( ref childNodeHtml, childPage, pageList, true );
                     childNodeHtml += "</ul>";
                 }
-
-                AddChildNodes( ref childNodeHtml, childPage, pageList );
+                else
+                {
+                    AddChildNodes( ref childNodeHtml, childPage, pageList, false );
+                }
+                
                 childNodeHtml += "</li>" + Environment.NewLine;
                 nodeHtml += childNodeHtml;
             }
 
             nodeHtml += "</ul>" + Environment.NewLine;
+            if ( writeLiTag )
+            {
+                nodeHtml += "</li>";
+            }
+
         }
     }
 }
