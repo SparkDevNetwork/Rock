493f574ee16b49e986261fd7c5850ec8e70591af
Bobbi Perreault <bcp@faxt.com>
2013-03-26 17:47:57 -0400

    OK!  Migration for Financial Batch. Whew

--- Rock/Migrations/201303032120222_FinancialBatchNewFields.cs
+++ Rock/Migrations/201303032120222_FinancialBatchNewFields.cs
@@ -24,14 +24,10 @@ namespace Rock.Migrations
             CreateIndex("dbo.FinancialBatch", "CampusId");
             CreateIndex("dbo.FinancialBatch", "BatchType_Id");
 
-            
             Sql( @"
                 SET IDENTITY_INSERT [DefinedType] ON
                 INSERT INTO [DefinedType] ([Id], [IsSystem], [FieldTypeId], [Order], [Category], [Name], [Description], [Guid]) VALUES (18, 1, 1, 0, N'FinancialBatch',N'FinancialBatch',N'Type of Batch','9E358FBE-2321-4C54-895F-C888E29298AE')
                 SET IDENTITY_INSERT [DefinedType] OFF
-
-
-
 
                 SET IDENTITY_INSERT [DefinedValue] ON
                 INSERT INTO [DefinedValue] ([Id], [IsSystem], [DefinedTypeId], [Order], [Name], [Description], [Guid]) VALUES (31,1,18,0,'ACH','ACH','E6F877F3-D2CC-443E-976A-4402502F544F')
@@ -41,6 +37,32 @@ namespace Rock.Migrations
                 INSERT INTO [DefinedValue] ([Id], [IsSystem], [DefinedTypeId], [Order], [Name], [Description], [Guid]) VALUES (35,1,18,4,'Amex','Amex','378D8EAD-7FA6-4D0D-862D-ED6E04B17770')
                 INSERT INTO [DefinedValue] ([Id], [IsSystem], [DefinedTypeId], [Order], [Name], [Description], [Guid]) VALUES (36,1,18,5,'PayPal','PayPal','4832DA18-DD18-477F-BFDB-ABFC28FE5743')
                 SET IDENTITY_INSERT [DefinedValue] OFF" );
+
+            //add the pages 1. off of finance page, add financial batch. off financial batch add FB details, then off FB details add transaction
+            AddPage( "7BEB7569-C485-40A0-A609-B0678F6F7240", "Financial Batch", "Manage Financial Batches", "ef65eff2-99ac-4081-8e09-32a04518683a" );
+            AddPage( "ef65eff2-99ac-4081-8e09-32a04518683a", "Financial Batch Detail", "", "606bda31-a8fe-473a-b3f8-a00ecf7e06ec" );
+            AddPage( "606bda31-a8fe-473a-b3f8-a00ecf7e06ec", "Transaction Detail", "", "b67e38cb-2ef1-43ea-863a-37daa1c7340f" );
+
+            // add the block types
+            AddBlockType( "Financial Batch", "~/Blocks/Finance/FinancialBatch.ascx", "Financial Batch", "AB345CE7-5DC6-41AF-BBDC-8D23D52AFE25");
+            AddBlockType( "Financial Batch Details", "~/Blocks/Finance/FinancialBatchDetail.ascx", "Financial Batch Detail", "ce34ce43-2ccf-4568-9aeb-3be203db3470" ); 
+            AddBlockType( "Financial Transaction Details", "~/Blocks/Finance/TransactionBlock.ascx", "Financial Transaction Detail", "1DE16F87-4A49-4A3C-A03E-B8488ECBEEBE" );
+
+            //add block
+            AddBlock( "ef65eff2-99ac-4081-8e09-32a04518683a", "AB345CE7-5DC6-41AF-BBDC-8D23D52AFE25", "Financial Batches","default" , "Content", 0, "B4B7A962-E162-47ED-8499-B7C7A7F41498");
+            AddBlock( "606bda31-a8fe-473a-b3f8-a00ecf7e06ec", "ce34ce43-2ccf-4568-9aeb-3be203db3470", "Financial Batch Details", "default", "Content", 0, "e7c8c398-0e1d-4bce-bc54-a02957228514" );
+            AddBlock( "b67e38cb-2ef1-43ea-863a-37daa1c7340f", "1DE16F87-4A49-4A3C-A03E-B8488ECBEEBE", "Financial Transaction Details", "default", "Content", 0, "f125e7eb-da78-4840-9d00-4c8dd0dd4a27" );
+
+            //( string blockTypeGuid, string fieldTypeGuid, string name, string key, string category, string description, int order, string defaultValue, string guid )
+            AddBlockTypeAttribute( "AB345CE7-5DC6-41AF-BBDC-8D23D52AFE25", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Detail Page Guid", "DetailPageGuid", "Advanced", "", 0, "", "2290806c-9e87-4960-9019-d4d7327591bb" );
+            AddBlockTypeAttribute( "ce34ce43-2ccf-4568-9aeb-3be203db3470", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Detail Page Guid", "DetailPageGuid", "Advanced", "", 0, "", "f70b3c73-3582-4fa4-b988-92880268310c" );
+            AddBlockTypeAttribute( "b67e38cb-2ef1-43ea-863a-37daa1c7340f", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Detail Page Guid", "DetailPageGuid", "Advanced", "", 0, "", "b29aa727-a4ae-46d1-a191-e48cb2fe22f4" );
+
+            // Attrib Value for FinancialBatch:Detail Page Guid
+            //string blockGuid, string attributeGuid, string value
+            AddBlockAttributeValue( "B4B7A962-E162-47ED-8499-B7C7A7F41498", "2290806c-9e87-4960-9019-d4d7327591bb", "ef65eff2-99ac-4081-8e09-32a04518683a" );
+            AddBlockAttributeValue( "e7c8c398-0e1d-4bce-bc54-a02957228514", "f70b3c73-3582-4fa4-b988-92880268310c", "606bda31-a8fe-473a-b3f8-a00ecf7e06ec" );
+            AddBlockAttributeValue( "f125e7eb-da78-4840-9d00-4c8dd0dd4a27", "b29aa727-a4ae-46d1-a191-e48cb2fe22f4", "b67e38cb-2ef1-43ea-863a-37daa1c7340f" );
 
         }
         
@@ -54,6 +76,26 @@ namespace Rock.Migrations
             DropForeignKey("dbo.FinancialBatch", "BatchType_Id", "dbo.DefinedType");
             DropForeignKey("dbo.FinancialBatch", "CampusId", "dbo.Campus");
             DropColumn("dbo.FinancialBatch", "BatchType_Id");
+
+            Sql( "DELETE FROM [dbo].[DefinedType] WHERE [Id] = 18" );
+            Sql( "DELETE FROM [dbo].[DefinedValue] WHERE [DefinedTypeId] = 18" );
+
+            DeleteAttribute( "2290806c-9e87-4960-9019-d4d7327591bb" );
+            DeleteAttribute( "f70b3c73-3582-4fa4-b988-92880268310c" );
+            DeleteAttribute( "b29aa727-a4ae-46d1-a191-e48cb2fe22f4" );
+
+            DeleteBlock( "B4B7A962-E162-47ED-8499-B7C7A7F41498" );
+            DeleteBlock( "e7c8c398-0e1d-4bce-bc54-a02957228514" );
+            DeleteBlock( "f125e7eb-da78-4840-9d00-4c8dd0dd4a27" );
+
+            DeleteBlockType("AB345CE7-5DC6-41AF-BBDC-8D23D52AFE25" );
+            DeleteBlockType("ce34ce43-2ccf-4568-9aeb-3be203db3470" );
+            DeleteBlockType("1DE16F87-4A49-4A3C-A03E-B8488ECBEEBE" );
+
+            DeletePage( "ef65eff2-99ac-4081-8e09-32a04518683a" );
+            DeletePage("606bda31-a8fe-473a-b3f8-a00ecf7e06ec" );
+            DeletePage( "b67e38cb-2ef1-43ea-863a-37daa1c7340f" );
+
         }
     }
 }
