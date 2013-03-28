8234435107d4ce2eeda85f97520a68ee73059640
mikepetersonccv <MikePeterson@ccvonline.com>
2013-03-28 09:51:14 -0700

    Check Scanner - many UI updates, working with MagTek (using RangerOCX for Magtek) and Canon CR50/80 (RangerOCX for Canon)

---
+++ Apps/CheckScannerUtility/AXInteropBuilder/DummyUserControl.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RangerOCXWrapper
+{
+    partial class DummyUserControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DummyUserControl));
+            this.axRanger1 = new AxRANGERLib.AxRanger();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axRanger1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axRanger1
+            // 
+            this.axRanger1.Enabled = true;
+            this.axRanger1.Location = new System.Drawing.Point(8, 48);
+            this.axRanger1.Name = "axRanger1";
+            this.axRanger1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axRanger1.OcxState")));
+            this.axRanger1.Size = new System.Drawing.Size(96, 93);
+            this.axRanger1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(526, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "The only purpose of the RangerAXInteropBuilder project is so the AxInterop.Ranger" +
+    "Lib.Dll will get a clean build";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DummyUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axRanger1);
+            this.Name = "DummyUserControl";
+            this.Size = new System.Drawing.Size(570, 153);
+            ((System.ComponentModel.ISupportInitialize)(this.axRanger1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxRANGERLib.AxRanger axRanger1;
+        private System.Windows.Forms.Label label1;
+    }
+}
