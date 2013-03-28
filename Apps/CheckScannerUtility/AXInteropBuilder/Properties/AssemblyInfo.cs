b88af89d552a3440127eab0f7fc3840efda74518
mikepetersonccv <MikePeterson@ccvonline.com>
2013-03-28 10:51:23 -0700

    added missing copyright headers in files

--- Apps/CheckScannerUtility/AXInteropBuilder/Properties/AssemblyInfo.cs
+++ Apps/CheckScannerUtility/AXInteropBuilder/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Reflection;
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
--- Rock/DataFilters/EnumPropertyFilter.cs
+++ Rock/DataFilters/EnumPropertyFilter.cs
@@ -1,9 +1,9 @@
-﻿using System;
-//
+﻿//
 // THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
 // SHAREALIKE 3.0 UNPORTED LICENSE:
 // http://creativecommons.org/licenses/by-nc-sa/3.0/
 //
+using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq.Expressions;
