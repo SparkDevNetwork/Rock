d6171e6e2fb0e568a0730564f12513807b575370
David Turner <david@azturners.com>
2013-03-28 11:55:16 -0700

    Fix problem with DateFieldAttribute.  Base class assembly, and class name were not be set correctly.

--- Rock/Attribute/DateFieldAttribute.cs
+++ Rock/Attribute/DateFieldAttribute.cs
@@ -11,8 +11,8 @@ namespace Rock.Attribute
     public class DateFieldAttribute : FieldAttribute
     {
         public DateFieldAttribute( string name, string description = "", bool required = true, string defaultValue = "", string category = "",
-            int order = 0, string key = null, string fieldTypeClass = null)
-            : base( name, description, required, defaultValue, category, order, key, fieldTypeClass, typeof( DateFieldType ).FullName )
+            int order = 0, string key = null)
+            : base( name, description, required, defaultValue, category, order, key, typeof( DateFieldType ).FullName )
         {
         }
     }
