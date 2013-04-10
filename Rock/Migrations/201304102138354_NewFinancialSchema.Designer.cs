60a8d7285039265aefdadf38334b08fce2a9f98f
David Turner <david@azturners.com>
2013-04-10 15:22:10 -0700

    Recreate migration after merge

--- Rock/Migrations/201304102138354_NewFinancialSchema.Designer.cs
+++ Rock/Migrations/201304102138354_NewFinancialSchema.Designer.cs
@@ -11,7 +11,7 @@ namespace Rock.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201304101953596_NewFinancialSchema"; }
+            get { return "201304102138354_NewFinancialSchema"; }
         }
         
         string IMigrationMetadata.Source
