5c6cebe20d0b432133af32639ba925c4eafcddb5
Mike Peterson <mike.peterson@ccvonline.com>
2013-04-24 14:54:41 -0700

    added missing copyright headers

--- Rock/Security/Encryption.cs
+++ Rock/Security/Encryption.cs
@@ -1,3 +1,8 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
 using System;
 using System.Collections.Generic;
 using System.Configuration;
