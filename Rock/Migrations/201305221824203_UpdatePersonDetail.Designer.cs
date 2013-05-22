be252d3959e693c03e550b0ec4c3dac7025fd4b4
David Turner <david@azturners.com>
2013-05-22 12:18:41 -0700

    Update migration after merging latest

--- Rock/Migrations/201305221824203_UpdatePersonDetail.Designer.cs
+++ Rock/Migrations/201305221824203_UpdatePersonDetail.Designer.cs
@@ -11,7 +11,7 @@ namespace Rock.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201305201830290_UpdatePersonDetail"; }
+            get { return "201305221824203_UpdatePersonDetail"; }
         }
         
         string IMigrationMetadata.Source
