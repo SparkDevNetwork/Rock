8680e2c6f6640f53a1db424421d5cb099f1a24e9
Nick Airdo <nairdo@gmail.com>
2013-05-30 10:02:12 -0700

    Added the <none> option to the DefinedValueFieldType when using the default (non-checkboxlist) and changed checkboxlist to use new "checkboxlist-group" instead of "well".

--- Rock/Web/UI/Adapters/CheckBoxListAdapter.cs
+++ Rock/Web/UI/Adapters/CheckBoxListAdapter.cs
@@ -22,10 +22,11 @@ namespace Rock.Web.UI.Adapters
         /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> containing methods to render the target-specific output.</param>
         protected override void RenderBeginTag( System.Web.UI.HtmlTextWriter writer )
         {
+            // Preserve any classes that a developer put on the control (such as a "well") by wrapping it in a <div>.
             CheckBoxList cbl = Control as CheckBoxList;
-            if ( cbl != null && cbl.CssClass.Contains( "well" ) )
+            if ( cbl != null && ! string.IsNullOrEmpty( cbl.CssClass ) )
             {
-                writer.AddAttribute( "class", "well" );
+                writer.AddAttribute( "class", cbl.CssClass );
                 writer.RenderBeginTag( HtmlTextWriterTag.Div );
             }
         }
@@ -36,8 +37,9 @@ namespace Rock.Web.UI.Adapters
         /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> containing methods to render the target-specific output.</param>
         protected override void RenderEndTag( System.Web.UI.HtmlTextWriter writer )
         {
+            // Close the <div> tag we may have started in the BeginTag above.
             CheckBoxList cbl = Control as CheckBoxList;
-            if ( cbl != null && cbl.CssClass.Contains( "well" ) )
+            if ( cbl != null && !string.IsNullOrEmpty( cbl.CssClass ) )
             {
                 writer.RenderEndTag();
             }
