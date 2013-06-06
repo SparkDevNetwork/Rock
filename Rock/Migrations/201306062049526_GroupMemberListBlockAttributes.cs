0bdf611b45f3602ab6592f575cc8ee421aac6cd2
Mike Peterson <mike.peterson@ccvonline.com>
2013-06-06 15:08:59 -0700

    fix <none> to &lt;none&gt; in block attribute discription

--- Rock/Migrations/201306062049526_GroupMemberListBlockAttributes.cs
+++ Rock/Migrations/201306062049526_GroupMemberListBlockAttributes.cs
@@ -19,7 +19,7 @@ namespace Rock.Migrations
         public override void Up()
         {
             // Attrib for BlockType: Crm - Group Member List:Group
-            AddBlockTypeAttribute( "88B7EFA9-7419-4D05-9F88-38B936E61EDD", "F4399CEF-827B-48B2-A735-F7806FCFE8E8", "Group", "Group", "", "Either pick a specific group or choose <none> to have group be determined by the groupId page parameter", 0, "", "9F2D3674-B780-4CD3-B4AB-3DF3EA21905A" );
+            AddBlockTypeAttribute( "88B7EFA9-7419-4D05-9F88-38B936E61EDD", "F4399CEF-827B-48B2-A735-F7806FCFE8E8", "Group", "Group", "", "Either pick a specific group or choose &lt;none&gt; to have group be determined by the groupId page parameter", 0, "", "9F2D3674-B780-4CD3-B4AB-3DF3EA21905A" );
         }
         
         /// <summary>
