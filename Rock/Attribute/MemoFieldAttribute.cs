936fde67e764ae697ac686b33b571b438efc8380
dcs619 <dcs619@yahoo.com>
2013-06-17 12:06:38 -0400

    Added new block attribute for multi-line text

---
+++ Rock/Attribute/MemoFieldAttribute.cs
@@ -0,0 +1,35 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+using System.Linq;
+
+namespace Rock.Attribute
+{
+    /// <summary>
+    /// A class Attribute that can be used by any oject that inherits from <see cref="Rock.Attribute.IHasAttributes"/> to specify what attributes it needs.  The 
+    /// Framework provides methods in the <see cref="Rock.Attribute.Helper"/> class to create, read, and update the attributes
+    /// </summary>
+    /// <remarks>
+    /// If using a custom <see cref="Rock.Field.IFieldType"/> make sure that the fieldtype has been added to Rock.
+    /// </remarks>
+    [AttributeUsage( AttributeTargets.Class, AllowMultiple = true, Inherited = true )]
+    public class MemoFieldAttribute : FieldAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextFieldAttribute" /> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="required">if set to <c>true</c> [required].</param>
+        /// <param name="order">The order.</param>
+        public MemoFieldAttribute( string name, string description = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null)
+            : base( name, description, required, defaultValue, category, order, key)
+        {
+        }
+
+    }
+}
