eae1479e8557d5d9b7c6acbef9fdb92fe3c559e1
David Turner <david@azturners.com>
2013-07-01 10:17:27 -0700

    Update migration for new family block

--- Rock/Migrations/201306282349062_NewFamilyPage.cs
+++ Rock/Migrations/201306282349062_NewFamilyPage.cs
@@ -18,10 +18,33 @@ namespace Rock.Migrations
         /// </summary>
         public override void Up()
         {
-            AddPage( "08DBD8A5-2C35-4146-B4A8-0F7652348B25", "New Family", "", "Default", "6A11A13D-05AB-4982-A4C2-67A8B1950C74", "" );
+            // Convert Defined Value field types to use Guid instead of id
+            Sql( @"
+    UPDATE AV	
+	    SET [Value] = CAST(DV.[Guid] AS varchar(100))
+    FROM [Attribute] A
+    INNER JOIN [FieldType] F 
+	    ON F.[Id] = A.[FieldTypeId]
+	    AND F.[Class] = 'Rock.Field.Types.DefinedValueFieldType'
+    INNER JOIN [AttributeValue] AV
+	    ON AV.[AttributeId] = A.[Id]
+    INNER JOIN [DefinedValue] DV
+	    ON DV.[Id] = CAST(AV.[Value] AS int)
 
-//TODO: this should have a diff parent page.
-            AddPage( "91CCB1C9-5F9F-44F5-8BE2-9EC3A3CFD46F", "Attribute Categories", "", "Default", "220D72F5-B589-4378-9852-BBB6F145AD7F", "" );
+    -- Update existing attribute value that had previusly used ID
+    UPDATE AV	
+	    SET [Value] = '283999EC-7346-42E3-B807-BCE9B2BABB49'
+    FROM [attribute] A
+    INNER JOIN [AttributeValue] AV
+    ON AV.[AttributeId] = A.[Id]
+    WHERE A.[Guid] = '88021EF1-853C-4FF7-859B-FDDB4C5E9DDC'
+" );
+
+            UpdateFieldType( "Categories Field Type", "", "Rock", "Rock.Field.Types.CategoriesFieldType", "775899FB-AC17-4C2C-B809-CF3A1D2AA4E1" );
+
+            AddPage( "08DBD8A5-2C35-4146-B4A8-0F7652348B25", "New Family", "", "Default", "6A11A13D-05AB-4982-A4C2-67A8B1950C74", "" );
+            AddPage( "0B213645-FA4E-44A5-8E4C-B2D8EF054985", "Attribute Categories", "Categorize attributes based on their associated entity type", "Default", "220D72F5-B589-4378-9852-BBB6F145AD7F", "icon-folder-close" );
+            AddPageRoute( "6A11A13D-05AB-4982-A4C2-67A8B1950C74", "NewFamily" );
 
             AddBlockType( "Administration - Attribute Categories", "", "~/Blocks/Administration/AttributeCategories.ascx", "1FC50941-A883-47A2-ABE9-13528BCC4D1B" );
             AddBlockType( "CRM - Person Detail - Add Family", "", "~/Blocks/CRM/PersonDetail/AddFamily.ascx", "DE156975-597A-4C55-A649-FE46712F91C3" );
@@ -38,6 +61,16 @@ namespace Rock.Migrations
             AddBlockTypeAttribute( "DE156975-597A-4C55-A649-FE46712F91C3", "1EDAFDED-DFE6-4334-B019-6EECBA89E05A", "Require Gender", "RequireGender", "", "Should Gender be required", 0, "False", "9BBA1555-9809-4A30-9B79-D7501CE1A864" );
             // Attrib for BlockType: CRM - Person Detail - Add Family:Location Type
             AddBlockTypeAttribute( "DE156975-597A-4C55-A649-FE46712F91C3", "59D5A94C-94A0-4630-B80A-BB25697D74C7", "Location Type", "LocationType", "", "The type of location that address should use", 0, "", "687E6973-0259-4DB4-B4EB-73708D98EFCD" );
+
+            // Attrib Value for Add Family:Category
+            AddBlockAttributeValue( "613536BE-86BC-4755-B815-807C236B92E6", "CF150193-1568-42D8-8E28-5BFCD4954C6F", "" );
+            // Attrib Value for Add Family:Require Gender
+            AddBlockAttributeValue( "613536BE-86BC-4755-B815-807C236B92E6", "9BBA1555-9809-4A30-9B79-D7501CE1A864", "True" );
+            // Attrib Value for Add Family:Require Grade
+            AddBlockAttributeValue( "613536BE-86BC-4755-B815-807C236B92E6", "090099D5-D8C4-4C72-B1B0-ACEBA231E68F", "True" );
+            // Attrib Value for Add Family:Location Type
+            AddBlockAttributeValue( "613536BE-86BC-4755-B815-807C236B92E6", "687E6973-0259-4DB4-B4EB-73708D98EFCD", "8C52E53C-2A66-435A-AE6E-5EE307D9A0DC" );
+
         }
         
         /// <summary>
@@ -63,6 +96,20 @@ namespace Rock.Migrations
 
             DeletePage( "220D72F5-B589-4378-9852-BBB6F145AD7F" ); // Attribute Categories
             DeletePage( "6A11A13D-05AB-4982-A4C2-67A8B1950C74" ); // New Family
+
+            // Convert Defined Value field types to use Id instead of Guid
+            Sql( @"
+    UPDATE AV	
+	    SET [Value] = CAST(DV.[Id] AS varchar)
+    FROM [Attribute] A
+    INNER JOIN [FieldType] F 
+	    ON F.[Id] = A.[FieldTypeId]
+	    AND F.[Class] = 'Rock.Field.Types.DefinedValueFieldType'
+    INNER JOIN [AttributeValue] AV
+	    ON AV.[AttributeId] = A.[Id]
+    INNER JOIN [DefinedValue] DV
+	    ON CAST(DV.[Guid] AS varchar(100)) = AV.[Value]
+" );
         }
     }
 }
