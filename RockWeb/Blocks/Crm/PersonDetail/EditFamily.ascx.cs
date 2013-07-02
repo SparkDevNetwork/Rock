4bc32b0f28c925480d81727336fe5e4de43dd360
David Turner <david@azturners.com>
2013-07-02 11:29:26 -0700

    + Update FamilyMembers block to render controls (w/ postback functionality) rather than using XSLT + Created Initial EditFamily block

---
+++ RockWeb/Blocks/Crm/PersonDetail/EditFamily.ascx.cs
@@ -0,0 +1,155 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+using Rock.Web.Cache;
+
+namespace RockWeb.Blocks.Crm.PersonDetail
+{
+    /// <summary>
+    /// The main Person Profile blockthe main information about a peron 
+    /// </summary>
+    public partial class EditFamily : PersonBlock
+    {
+        public Group _family = null;
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            int familyId = int.MinValue;
+            if ( int.TryParse( PageParameter( "FamilyId" ), out familyId ) )
+            {
+                _family = new GroupService().Get( familyId );
+                if ( _family != null && string.Compare( _family.GroupType.Guid.ToString(), Rock.SystemGuid.GroupType.GROUPTYPE_FAMILY, true ) != 0 )
+                {
+                    nbNotice.Heading = "Invalid Family";
+                    nbNotice.Text = "Sorry, but the group selected is not a Family group";
+                    nbNotice.NotificationBoxType = NotificationBoxType.Error;
+                    nbNotice.Visible = true;
+
+                    _family = null;
+                    return;
+                }
+            }
+
+            ddlRecordStatus.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_RECORD_STATUS ) ) );
+            ddlReason.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_RECORD_STATUS_REASON ) ), true );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack && _family != null )
+            {
+                ShowDetails();
+            }
+        }
+
+        /// <summary>
+        /// Handles the SelectedIndexChanged event of the ddlRecordStatus control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void ddlRecordStatus_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            ddlReason.Visible = ( ddlRecordStatus.SelectedValueAsInt() == DefinedValueCache.Read( new Guid( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_INACTIVE ) ).Id );
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnSave control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void btnSave_Click( object sender, EventArgs e )
+        {
+            using ( new UnitOfWorkScope() )
+            {
+                var service = new GroupService();
+                _family = service.Get( _family.Id );
+                _family.Name = tbFamilyName.Text;
+                _family.CampusId = cpCampus.SelectedValueAsInt();
+                service.Save( _family, CurrentPersonId );
+
+                var phoneNumberService = new PhoneNumberService();
+                int primaryPhoneId = DefinedValueCache.Read(new Guid(Rock.SystemGuid.DefinedValue.PERSON_PHONE_TYPE_PRIMARY)).Id;
+                foreach( var person in _family.Members.Select( m => m.Person) )
+                {
+                    var phoneNumber = person.PhoneNumbers.Where( n => n.NumberTypeValueId == primaryPhoneId).FirstOrDefault();
+                    if (phoneNumber == null)
+                    {
+                        phoneNumber = new PhoneNumber();
+                        phoneNumber.PersonId = person.Id;
+                        phoneNumber.NumberTypeValueId = primaryPhoneId;
+                        phoneNumberService.Add(phoneNumber, CurrentPersonId);
+                    }
+                    phoneNumber.Number = tbPhone.Text;
+                    phoneNumber.IsUnlisted = cbUnlisted.Checked;
+                    phoneNumberService.Save(phoneNumber, CurrentPersonId);
+                }
+
+                if ( _family.Members.Any( m => m.PersonId == Person.Id ) )
+                {
+                    Response.Redirect( string.Format( "~/Person/{0}", Person.Id ), false );
+                }
+                else
+                {
+                    var fm = _family.Members.FirstOrDefault();
+                    if ( fm != null )
+                    {
+                        Response.Redirect( string.Format( "~/Person/{0}", fm.PersonId ), false );
+                    }
+                    else
+                    {
+                        Response.Redirect( "~", false );
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnCancel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void btnCancel_Click( object sender, EventArgs e )
+        {
+            Response.Redirect( string.Format( "~/Person/{0}", Person.Id ), false );
+        }
+
+        /// <summary>
+        /// Shows the details.
+        /// </summary>
+        private void ShowDetails()
+        {
+            tbFamilyName.Text = _family.Name;
+            cpCampus.SelectedCampusId = _family.CampusId;
+        }
+
+    }
+}
--- RockWeb/Blocks/Crm/PersonDetail/Relationships.ascx.cs
+++ RockWeb/Blocks/Crm/PersonDetail/Relationships.ascx.cs
@@ -42,68 +42,71 @@ namespace RockWeb.Blocks.Crm.PersonDetail
         /// </summary>
         private void BindData()
         {
-            Group group = null;
-
-            Guid roleGuid = Guid.Empty;
-            if ( Guid.TryParse( GetAttributeValue( "GroupType/RoleFilter" ), out roleGuid ) )
+            if ( Person != null && Person.Id > 0 )
             {
-                using ( new Rock.Data.UnitOfWorkScope() )
+                Group group = null;
+
+                Guid roleGuid = Guid.Empty;
+                if ( Guid.TryParse( GetAttributeValue( "GroupType/RoleFilter" ), out roleGuid ) )
                 {
-                    var memberService = new GroupMemberService();
-                    group = memberService.Queryable()
-                        .Where( m =>
-                            m.PersonId == Person.Id &&
-                            m.GroupRole.Guid == roleGuid
-                        )
-                        .Select( m => m.Group )
-                        .FirstOrDefault();
-
-                    if ( group == null && bool.Parse( GetAttributeValue( "CreateGroup" ) ) )
+                    using ( new Rock.Data.UnitOfWorkScope() )
                     {
-                        var role = new GroupRoleService().Get( roleGuid );
-                        if ( role != null && role.GroupTypeId.HasValue )
+                        var memberService = new GroupMemberService();
+                        group = memberService.Queryable()
+                            .Where( m =>
+                                m.PersonId == Person.Id &&
+                                m.GroupRole.Guid == roleGuid
+                            )
+                            .Select( m => m.Group )
+                            .FirstOrDefault();
+
+                        if ( group == null && bool.Parse( GetAttributeValue( "CreateGroup" ) ) )
                         {
-                            var groupMember = new GroupMember();
-                            groupMember.PersonId = Person.Id;
-                            groupMember.GroupRoleId = role.Id;
-
-                            group = new Group();
-                            group.Name = role.GroupType.Name;
-                            group.GroupTypeId = role.GroupTypeId.Value;
-                            group.Members.Add( groupMember );
-
-                            var groupService = new GroupService();
-                            groupService.Add( group, CurrentPersonId );
-                            groupService.Save( group, CurrentPersonId );
-
-                            group = groupService.Get( group.Id );
-                        }
-                    }
-
-                    if ( group != null )
-                    {
-                        if ( group.IsAuthorized( "View", CurrentPerson ) )
-                        {
-                            if ( !string.IsNullOrWhiteSpace( group.GroupType.IconCssClass ) )
+                            var role = new GroupRoleService().Get( roleGuid );
+                            if ( role != null && role.GroupTypeId.HasValue )
                             {
-                                phGroupTypeIcon.Controls.Add(
-                                    new LiteralControl(
-                                        string.Format( "<i class='{0}'></i>", group.GroupType.IconCssClass ) ) );
+                                var groupMember = new GroupMember();
+                                groupMember.PersonId = Person.Id;
+                                groupMember.GroupRoleId = role.Id;
+
+                                group = new Group();
+                                group.Name = role.GroupType.Name;
+                                group.GroupTypeId = role.GroupTypeId.Value;
+                                group.Members.Add( groupMember );
+
+                                var groupService = new GroupService();
+                                groupService.Add( group, CurrentPersonId );
+                                groupService.Save( group, CurrentPersonId );
+
+                                group = groupService.Get( group.Id );
                             }
+                        }
 
-                            lGroupName.Text = group.Name;
+                        if ( group != null )
+                        {
+                            if ( group.IsAuthorized( "View", CurrentPerson ) )
+                            {
+                                if ( !string.IsNullOrWhiteSpace( group.GroupType.IconCssClass ) )
+                                {
+                                    phGroupTypeIcon.Controls.Add(
+                                        new LiteralControl(
+                                            string.Format( "<i class='{0}'></i>", group.GroupType.IconCssClass ) ) );
+                                }
 
-                            phEditActions.Visible = group.IsAuthorized( "Edit", CurrentPerson );
+                                lGroupName.Text = group.Name;
 
-                            // TODO: How many implied relationships should be displayed
+                                phEditActions.Visible = group.IsAuthorized( "Edit", CurrentPerson );
 
-                            rGroupMembers.DataSource = new GroupMemberService().GetByGroupId( group.Id )
-                                .Where( m => m.PersonId != Person.Id )
-                                .OrderBy( m => m.Person.LastName )
-                                .ThenBy( m => m.Person.FirstName )
-                                .Take( 50 )
-                                .ToList();
-                            rGroupMembers.DataBind();
+                                // TODO: How many implied relationships should be displayed
+
+                                rGroupMembers.DataSource = new GroupMemberService().GetByGroupId( group.Id )
+                                    .Where( m => m.PersonId != Person.Id )
+                                    .OrderBy( m => m.Person.LastName )
+                                    .ThenBy( m => m.Person.FirstName )
+                                    .Take( 50 )
+                                    .ToList();
+                                rGroupMembers.DataBind();
+                            }
                         }
                     }
                 }
