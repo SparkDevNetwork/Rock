ff61e4a0feb05a0fa6bbe6aa9f058d79f51f988b
mikepetersonccv <MikePeterson@ccvonline.com>
2013-07-12 01:05:58 -0700

    Update CodeGenerator so that all Services can have a constructor that accepts RockContext so that two services can share a database connection.  This is mostly just handy when doing Joins.

--- Rock/Model/CodeGenerated/FinancialAccountService.cs
+++ Rock/Model/CodeGenerated/FinancialAccountService.cs
@@ -33,7 +33,16 @@ namespace Rock.Model
         /// <summary>
         /// Initializes a new instance of the <see cref="FinancialAccountService"/> class
         /// </summary>
+        /// <param name="repository">The repository.</param>
         public FinancialAccountService(IRepository<FinancialAccount> repository) : base(repository)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FinancialAccountService"/> class
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public FinancialAccountService(RockContext context) : base(context)
         {
         }
 
--- Rock/Model/CodeGenerated/FinancialTransactionScannedCheckService.cs
+++ Rock/Model/CodeGenerated/FinancialTransactionScannedCheckService.cs
@@ -33,7 +33,16 @@ namespace Rock.Model
         /// <summary>
         /// Initializes a new instance of the <see cref="FinancialTransactionScannedCheckService"/> class
         /// </summary>
+        /// <param name="repository">The repository.</param>
         public FinancialTransactionScannedCheckService(IRepository<FinancialTransactionScannedCheck> repository) : base(repository)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FinancialTransactionScannedCheckService"/> class
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public FinancialTransactionScannedCheckService(RockContext context) : base(context)
         {
         }
     }
