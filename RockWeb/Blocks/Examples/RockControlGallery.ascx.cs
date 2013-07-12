95fe4f5d1d2a27f83c19e19cd0db6a6252cd4223
Mike Peterson <mike.peterson@ccvonline.com>
2013-07-12 16:10:58 -0700

    new ColorField grid control

--- RockWeb/Blocks/Examples/RockControlGallery.ascx.cs
+++ RockWeb/Blocks/Examples/RockControlGallery.ascx.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using Rock.Web.UI;
 using Rock.Web.UI.Controls;
+using Rock;
 
 namespace RockWeb.Blocks.Examples
 {
@@ -13,6 +15,20 @@ namespace RockWeb.Blocks.Examples
     /// </summary>
     public partial class RockControlGallery : RockBlock
     {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            gExample.DataKeyNames = new string[] { "id" };
+            gExample.GridRebind += gExample_GridRebind;
+        }
+
+
+
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
         /// </summary>
@@ -29,11 +45,13 @@ namespace RockWeb.Blocks.Examples
                 bddlExample.Items.Add( new ListItem( "Ketchup", "150" ) );
                 bddlExample.Items.Add( new ListItem( "Mustard", "654" ) );
                 bddlExample.SelectedValue = "44";
-                
-                ddlDataExample.Items.AddRange(bddlExample.Items.OfType<ListItem>().ToArray());
+
+                ddlDataExample.Items.AddRange( bddlExample.Items.OfType<ListItem>().ToArray() );
 
                 labeledCheckBoxList.Items.AddRange( bddlExample.Items.OfType<ListItem>().ToArray() );
                 labeledRadioButtonList.Items.AddRange( bddlExample.Items.OfType<ListItem>().ToArray() );
+
+                BindGrid();
             }
         }
 
@@ -106,7 +124,7 @@ namespace RockWeb.Blocks.Examples
                         hg.Visible = !hg.Visible;
                     }
                 }
-                
+
             }
         }
 
@@ -119,7 +137,7 @@ namespace RockWeb.Blocks.Examples
         {
             var date = monthYearPicker.SelectedDate;
         }
-        
+
         /// <summary>
         /// Handles the TextChanged event of the monthDayPicker control.
         /// </summary>
@@ -129,5 +147,82 @@ namespace RockWeb.Blocks.Examples
         {
             var date = monthDayPicker.SelectedDate;
         }
-}
+
+        /// <summary>
+        /// Handles the GridRebind event of the gExample control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        void gExample_GridRebind( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private class ExampleDataItem
+        {
+            /// <summary>
+            /// Gets or sets the id.
+            /// </summary>
+            /// <value>
+            /// The id.
+            /// </value>
+            public int Id { get; set; }
+
+            /// <summary>
+            /// Gets or sets the color of the defined value.
+            /// </summary>
+            /// <value>
+            /// The color of the defined value.
+            /// </value>
+            public string DefinedValueColor { get; set; }
+
+            /// <summary>
+            /// Gets or sets the name of the defined value type.
+            /// </summary>
+            /// <value>
+            /// The name of the defined value type.
+            /// </value>
+            public string DefinedValueTypeName { get; set; }
+
+            /// <summary>
+            /// Gets or sets some date time.
+            /// </summary>
+            /// <value>
+            /// Some date time.
+            /// </value>
+            public DateTime SomeDateTime { get; set; }
+
+            /// <summary>
+            /// Gets or sets some boolean.
+            /// </summary>
+            /// <value>
+            /// Some boolean.
+            /// </value>
+            public Boolean SomeBoolean { get; set; }
+        }
+
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            Random random = new Random();
+
+            List<ExampleDataItem> dataList = new List<ExampleDataItem>();
+            dataList.Add( new ExampleDataItem { Id = 1, DefinedValueColor = "green", DefinedValueTypeName = "Pickles", SomeDateTime = DateTime.Now, SomeBoolean = random.Next( 2 ) == 1 } );
+            dataList.Add( new ExampleDataItem { Id = 2, DefinedValueColor = "#ff0000", DefinedValueTypeName = "Ketchup", SomeDateTime = DateTime.Now, SomeBoolean = random.Next( 2 ) == 1 } );
+            dataList.Add( new ExampleDataItem { Id = 3, DefinedValueColor = "white", DefinedValueTypeName = "Onions", SomeDateTime = DateTime.Now, SomeBoolean = random.Next( 2 ) == 1 } );
+            dataList.Add( new ExampleDataItem { Id = 4, DefinedValueColor = "rgb(255,255,0)", DefinedValueTypeName = "Mustard", SomeDateTime = DateTime.Now, SomeBoolean = random.Next( 2 ) == 1 } );
+
+            SortProperty sortProperty = gExample.SortProperty ?? new SortProperty( new GridViewSortEventArgs( "DefinedValueTypeName", SortDirection.Ascending ) );
+
+            gExample.DataSource = dataList.AsQueryable().Sort(sortProperty).ToList();
+            gExample.DataBind();
+        }
+
+
+    }
 }
