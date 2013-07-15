c2577386c2ba553fb064c551713da6c214914da4
David Stevens <david.stevens@newspring.cc>
2013-07-15 17:13:41 -0400

    Added workflow action for Last Attended, edited workflow and Confirm page, cleaned up comments

--- Rock/Workflow/Action/CheckIn/AttendedLoadGroupTypes.cs
+++ Rock/Workflow/Action/CheckIn/AttendedLoadGroupTypes.cs
@@ -42,17 +42,6 @@ namespace Rock.Workflow.Action.CheckIn
                 {
                     foreach ( var person in family.People )
                     {
-                        //foreach ( var kioskGroupType in checkInState.Kiosk.KioskGroupTypes.Where( g => g.KioskLocations.Any( l => l.Location.IsActive ) ) )
-                        //{
-                        //    if ( !person.GroupTypes.Any( g => g.GroupType.Id == kioskGroupType.GroupType.Id ) )
-                        //    {
-                        //        var checkinGroupType = new CheckInGroupType();
-                        //        checkinGroupType.GroupType = kioskGroupType.GroupType.Clone( false );
-                        //        checkinGroupType.GroupType.CopyAttributesFrom( kioskGroupType.GroupType );
-                        //        person.GroupTypes.Add( checkinGroupType );
-                        //    }
-                        //}
-                        
                         foreach ( var roomGroupType in CheckInBlock.CurrentRoomGroupTypes )
                         {
                             if ( !person.GroupTypes.Any( g => g.GroupType.Id == roomGroupType.Id ) )
