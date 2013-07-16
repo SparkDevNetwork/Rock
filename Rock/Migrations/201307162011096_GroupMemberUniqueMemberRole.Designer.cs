6cd23697666f679ef603aba850afa0c619283da1
chrisfunk <cfunk@secc.org>
2013-07-16 16:16:46 -0400

    Migration Updates

--- Rock/Migrations/201307162011096_GroupMemberUniqueMemberRole.Designer.cs
+++ Rock/Migrations/201307162011096_GroupMemberUniqueMemberRole.Designer.cs
@@ -11,7 +11,7 @@ namespace Rock.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201307152115127_GroupMemberUniqueMemberRole"; }
+            get { return "201307162011096_GroupMemberUniqueMemberRole"; }
         }
         
         string IMigrationMetadata.Source
