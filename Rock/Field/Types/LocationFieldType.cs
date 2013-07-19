3addee9b81c0277112f55293f13ebda8011b25e1
David Turner <david@azturners.com>
2013-07-19 12:46:08 -0700

    + Change the Organization Address global attribute to use a location field instead of free text + Modified some location fields to be more consistent

---
+++ Rock/Field/Types/LocationFieldType.cs
@@ -0,0 +1,22 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+
+using System;
+
+namespace Rock.Field.Types
+{
+    /// <summary>
+    /// Field used to save and dispaly a location value
+    /// TODO: For now this just renders a textbox for entering the guid of the location.  Will eventually
+    /// need to render a LocationPicker
+    /// </summary>
+    [Serializable]
+    public class LocationFieldType : FieldType
+    {
+
+
+    }
+}
