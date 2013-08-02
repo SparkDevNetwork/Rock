daecfd43a9b39a694d8b52c6f743ecbac57561a2
Mike Peterson <mike.peterson@ccvonline.com>
2013-08-01 17:36:00 -0700

    update Dev Tool for TreeView to at least display stuff with correct parent-child relationship! oops

--- Dev Tools/Sql/View_Pages_As_Ascii_Tree.sql
+++ Dev Tools/Sql/View_Pages_As_Ascii_Tree.sql
@@ -1,14 +1,15 @@
 -- generates an ASCII treeview of the Pages
 -- either copy and paste the grid results to a text editor, or do Right-Click | Results To | Text
+-- NOTE: This does not sort pages by order, but the parent-child relation is displayed correctly
 
 with CTE as (
-select *, RIGHT('000000' + cast([Order] as nvarchar(max)), 6) [Sequence], 0 [Level] from [Page] where [ParentPageId] is null
+select *, RIGHT('000000' + cast([Id] as nvarchar(max)), 6) [Sequence], 1 [Level] from [Page] where [ParentPageId] is null
 union all
-select [a].*, pcte.Sequence + '/' + RIGHT('000000' + cast(a.[Order] as nvarchar(max)), 6) [Sequence], pcte.Level + 1 [Level] from [Page] [a]
+select [a].*, pcte.Sequence + '/' + RIGHT('000000' + cast(a.[Id] as nvarchar(max)), 6) [Sequence], pcte.Level + 1 [Level] from [Page] [a]
 inner join CTE pcte on pcte.Id = [a].[ParentPageId]
 )
 
 select 
-  REPLICATE('- ', [Level]) + [Name]
+  REPLICATE('| ', [Level]-1) + [Name], [Order], [Level], [Guid], [Sequence]
 from CTE
 order by [Sequence], [Order], [Name]
