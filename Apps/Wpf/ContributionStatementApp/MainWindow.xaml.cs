552b5f36822e6c154e40b0ce360e9731ae7f968f
mikepetersonccv <MikePeterson@ccvonline.com>
2013-09-18 13:15:15 -0700

    Contributions now create new Migradoc Table per Person.  Can do 2000+ pages without crashing, but crashes at ~3000 or so

--- Apps/Wpf/ContributionStatementApp/MainWindow.xaml.cs
+++ Apps/Wpf/ContributionStatementApp/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using Rock.Data;
 using System.Data.Entity;
 using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace ContributionStatementApp
 {
@@ -27,24 +29,83 @@ namespace ContributionStatementApp
         }
 
         /// <summary>
+        /// Toggles the fade.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="speed">The speed.</param>
+        public void FadeIn( Control control, int speed = 0 )
+        {
+            // TODO Move this to a shared dll
+            control.Opacity = 0;
+            control.Visibility = Visibility.Visible;
+            Storyboard storyboard = new Storyboard();
+            TimeSpan duration = new TimeSpan( 0, 0, 0, 0, (int)speed );
+            DoubleAnimation fadeInAnimation = new DoubleAnimation { From = 0.0, To = 1.0, Duration = new Duration( duration ) };
+            Storyboard.SetTargetName( fadeInAnimation, control.Name );
+            Storyboard.SetTargetProperty( fadeInAnimation, new PropertyPath( "Opacity", 1 ) );
+            storyboard.Children.Add( fadeInAnimation );
+            storyboard.Begin( control );
+        }
+
+        /// <summary>
+        /// Fades the out.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="speed">The speed.</param>
+        public void FadeOut( Control control, int speed = 2000 )
+        {
+            // TODO Move this to a shared dll
+            Storyboard storyboard = new Storyboard();
+            TimeSpan duration = new TimeSpan( 0, 0, 0, 0, (int)speed );
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation { From = 1.0, To = 0.0, Duration = new Duration( duration ) };
+            Storyboard.SetTargetName( fadeOutAnimation, control.Name );
+            Storyboard.SetTargetProperty( fadeOutAnimation, new PropertyPath( "Opacity", 0 ) );
+            storyboard.Children.Add( fadeOutAnimation );
+
+            EventHandler handleCompleted = new EventHandler( ( sender, e ) =>
+            {
+                control.Visibility = Visibility.Collapsed;
+            } );
+
+            storyboard.Completed += handleCompleted;
+            storyboard.Begin( control );
+        }
+
+        /// <summary>
         /// Handles the Click event of the btnShowStatement control.
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void btnShowStatement_Click( object sender, RoutedEventArgs e )
         {
-
+            btnShowStatement.IsEnabled = false;
+            lblReportProgress.Visibility = System.Windows.Visibility.Hidden;
+            lblReportProgress.Content = "Progress - Creating Statements";
+            FadeIn( lblReportProgress, 2000 );
             BackgroundWorker bw = new BackgroundWorker();
             bw.DoWork += bw_DoWork;
             bw.RunWorkerCompleted += bw_RunWorkerCompleted;
             bw.RunWorkerAsync();
         }
 
+        /// <summary>
+        /// Handles the RunWorkerCompleted event of the bw control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RunWorkerCompletedEventArgs"/> instance containing the event data.</param>
         void bw_RunWorkerCompleted( object sender, RunWorkerCompletedEventArgs e )
         {
-            MessageBox.Show( "Done" );
+            btnShowStatement.IsEnabled = true;
+
+            lblReportProgress.Content = "Done";
+            FadeOut( lblReportProgress );
         }
 
+        /// <summary>
+        /// Handles the DoWork event of the bw control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DoWorkEventArgs"/> instance containing the event data.</param>
         void bw_DoWork( object sender, DoWorkEventArgs e )
         {
             ContributionForm contributionForm = new ContributionForm();
@@ -53,8 +114,8 @@ namespace ContributionStatementApp
             Database.SetInitializer<Rock.Data.RockContext>( null );
 
             // TODO - Get actual date range
-            DateTime startDate = new DateTime( 2013, 1, 1 );
-            DateTime endDate = new DateTime( 2013, 12, 1 );
+            DateTime startDate = new DateTime( 2013, 2, 1 );
+            DateTime endDate = new DateTime( 2013, 9, 1 );
 
             FinancialTransactionService financialTransactionService = new FinancialTransactionService();
 
@@ -70,20 +131,49 @@ namespace ContributionStatementApp
             pdfDocumentRenderer.Document = document;
             pdfDocumentRenderer.DocumentRenderer.PrepareDocumentProgress += DocumentRenderer_PrepareDocumentProgress;
 
-            //pdfDocumentRenderer.DocumentRenderer.WorkingDirectory = "c:\temp";
-            //pdfDocumentRenderer.PrepareRenderPages();
             pdfDocumentRenderer.RenderDocument();
             string fileName = "Statement_" + Guid.NewGuid().ToString( "N" ) + ".pdf";
             pdfDocumentRenderer.Save( fileName );
 
             Process.Start( fileName );
-            
+
         }
 
+        /// <summary>
+        /// The current progress percent
+        /// </summary>
+        private decimal currentProgressPercent = 0;
+
+        /// <summary>
+        /// Handles the PrepareDocumentProgress event of the DocumentRenderer control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="DocumentRenderer.PrepareDocumentProgressEventArgs"/> instance containing the event data.</param>
         void DocumentRenderer_PrepareDocumentProgress( object sender, DocumentRenderer.PrepareDocumentProgressEventArgs e )
         {
-            var progress = e.Value/e.Maximum;
-            
+
+            decimal percentProgress = decimal.Divide( e.Value, e.Maximum ) * 100;
+            if ( percentProgress != currentProgressPercent )
+            {
+                currentProgressPercent = percentProgress;
+                Dispatcher.Invoke( new Action( UpdateProgressBar ) );
+            }
+
+        }
+
+        /// <summary>
+        /// Updates the progress bar.
+        /// </summary>
+        private void UpdateProgressBar()
+        {
+            if ( currentProgressPercent == 100 )
+            {
+                lblReportProgress.Content = "Render Complete - Preparing to save document";
+            }
+            else
+            {
+                lblReportProgress.Content = "Render Progress " + currentProgressPercent.ToString();
+            }
         }
     }
 }
