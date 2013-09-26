011bb75d7cb430cd820b8540154ca40dae87272b
David Turner <david@azturners.com>
2013-09-26 05:47:31 -0700

    Update Marketing Campaign Ads to use Liquid instead of XSLT

--- RockWeb/Blocks/Cms/MarketingCampaignAds.ascx
+++ RockWeb/Blocks/Cms/MarketingCampaignAds.ascx
@@ -1,4 +1,4 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="MarketingCampaignAdsXslt.ascx.cs" Inherits=" RockWeb.Blocks.Cms.MarketingCampaignAdsXslt" %>
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="MarketingCampaignAds.ascx.cs" Inherits=" RockWeb.Blocks.Cms.MarketingCampaignAds" %>
 <asp:UpdatePanel ID="upContent" runat="server">
     <ContentTemplate>
         <asp:PlaceHolder ID="phContent" runat="server"></asp:PlaceHolder>
--- RockWeb/Blocks/Cms/MarketingCampaignAds.ascx.cs
+++ RockWeb/Blocks/Cms/MarketingCampaignAds.ascx.cs
@@ -5,13 +5,11 @@
 //
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.UI;
-using System.Xml.Linq;
-using System.Xml.Xsl;
+using DotLiquid;
 using Rock;
 using Rock.Attribute;
 using Rock.Model;
@@ -22,11 +20,16 @@ using Rock.Web.UI;
 namespace RockWeb.Blocks.Cms
 {
     [IntegerField( "Max Items", "", true, int.MinValue, "", 0 )]
-    [LinkedPage ("Detail Page", "", false, "", "", 1)]
-    [CustomCheckboxListField("Image Types", "The types of images to display",  
+    [LinkedPage( "Detail Page", "", false, "", "", 1 )]
+    [CustomCheckboxListField( "Image Types", "The types of images to display",
         "SELECT A.[name] AS [Text], A.[key] AS [Value] FROM [EntityType] E INNER JOIN [attribute] a ON A.[EntityTypeId] = E.[Id] INNER JOIN [FieldType] F ON F.Id = A.[FieldTypeId]	AND F.Guid = '" +
-        Rock.SystemGuid.FieldType.IMAGE + "' WHERE E.Name = 'Rock.Model.MarketingCampaignAd' ORDER BY [Key]", false, "", "", 2)]
-    [TextField( "XSLT File", "The path to the XSLT File ", true, "~/Assets/XSLT/AdList.xslt", "", 3 )]
+        Rock.SystemGuid.FieldType.IMAGE + "' WHERE E.Name = 'Rock.Model.MarketingCampaignAd' ORDER BY [Key]", false, "", "", 2 )]
+
+    [MemoField( "Template", "The liquid template to use for rendering", true, @"
+<div class='ad-list'>
+    {% include 'AdList' with Ads %}
+</div>
+", "", 3 )]
 
     [CampusesField( "Campuses", "Display Ads for selected campus", false, "", "Filter", 4 )]
     [CustomCheckboxListField( "Ad Types", "Types of Ads to display",
@@ -34,14 +37,11 @@ namespace RockWeb.Blocks.Cms
     [DefinedValueField( Rock.SystemGuid.DefinedType.MARKETING_CAMPAIGN_AUDIENCE_TYPE, "Audience", "The Audience", false, "", "Filter", 6 )]
     [CustomCheckboxListField( "Audience Primary Secondary", "Primary or Secondary Audience", "1:Primary,2:Secondary", false, "1,2", "Filter", 7 )]
 
-    [BooleanField( "Show Debug", "Output the XML to be transformed.", false, "Advanced", 8 )]
+    [IntegerField( "Image Width", "Width that the image should be resized to. Leave height/width blank to get original size.", false, int.MinValue, "", 8 )]
+    [IntegerField( "Image Height", "Height that the image should be resized to. Leave height/width blank to get original size.", false, int.MinValue, "", 9 )]
 
-    [IntegerField("Image Width", "Width that the image should be resized to. Leave height/width blank to get original size.", false, int.MinValue, "", 9)]
-    [IntegerField("Image Height", "Height that the image should be resized to. Leave height/width blank to get original size.", false, int.MinValue, "", 10)]
-    
-    
-    [ContextAware( typeof(Campus) )]
-    public partial class MarketingCampaignAdsXslt : RockBlock
+    [ContextAware( typeof( Campus ) )]
+    public partial class MarketingCampaignAds : RockBlock
     {
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
@@ -50,25 +50,14 @@ namespace RockWeb.Blocks.Cms
         protected override void OnInit( EventArgs e )
         {
             base.OnInit( e );
-
-            // ensure xslt file exists
-            string xsltFile = Server.MapPath(GetAttributeValue("XSLTFile"));
-            if (System.IO.File.Exists( xsltFile ))
-            {
-                TransformXml(xsltFile);
-            }
-            else
-            {
-                string errorMessage = "<div class='alert warning' style='margin: 24px auto 0 auto; max-width: 500px;' ><strong>Warning!</strong><p>The XSLT file required to process the ad list could not be found.</p><p><em>" + xsltFile + "</em></p>";
-                phContent.Controls.Add(new LiteralControl(errorMessage));
-            }
+            Render();
         }
 
 
         /// <summary>
-        /// Transforms the XML.
+        /// Renders the Ads using Liquid.
         /// </summary>
-        private void TransformXml(string xsltFile)
+        private void Render()
         {
             MarketingCampaignAdService marketingCampaignAdService = new MarketingCampaignAdService();
             var qry = marketingCampaignAdService.Queryable();
@@ -148,7 +137,7 @@ namespace RockWeb.Blocks.Cms
                 // limit to ads that are targeted to the current campus context
                 qry = qry.Where( a => a.MarketingCampaign.MarketingCampaignCampuses.Any( x => x.Id.Equals( campusContext.Id ) ) );
             }
-            
+
 
             // Max Items
             string maxItems = GetAttributeValue( "MaxItems" );
@@ -173,14 +162,12 @@ namespace RockWeb.Blocks.Cms
                 marketingCampaignAdList = qry.Take( maxAdCount.Value ).ToList();
             }
 
-            // build Xml doc
-            XDocument doc = new XDocument();
-            XElement rootNode = new XElement( "Ads" );
-            doc.Add( rootNode );
+            // build dictionary for liquid
+            var ads = new List<Dictionary<string, object>>();
 
             foreach ( var marketingCampaignAd in marketingCampaignAdList )
             {
-                XElement xmlElement = new XElement( "Ad" );
+                var ad = new Dictionary<string, object>();
 
                 // DetailPage
                 string detailPageUrl = string.Empty;
@@ -199,39 +186,37 @@ namespace RockWeb.Blocks.Cms
                 string eventGroupName = marketingCampaignAd.MarketingCampaign.EventGroup != null ? marketingCampaignAd.MarketingCampaign.EventGroup.Name : string.Empty;
 
                 // Marketing Campaign Fields
-                xmlElement.Add(
-                    new XAttribute( "Title", marketingCampaignAd.MarketingCampaign.Title ),
-                    new XAttribute( "ContactEmail", marketingCampaignAd.MarketingCampaign.ContactEmail ),
-                    new XAttribute( "ContactFullName", marketingCampaignAd.MarketingCampaign.ContactFullName ),
-                    new XAttribute( "ContactPhoneNumber", marketingCampaignAd.MarketingCampaign.ContactPhoneNumber ),
-                    new XAttribute( "LinkedEvent", eventGroupName ) );
+                ad.Add( "Title", marketingCampaignAd.MarketingCampaign.Title );
+                ad.Add( "ContactEmail", marketingCampaignAd.MarketingCampaign.ContactEmail );
+                ad.Add( "ContactFullName", marketingCampaignAd.MarketingCampaign.ContactFullName );
+                ad.Add( "ContactPhoneNumber", marketingCampaignAd.MarketingCampaign.ContactPhoneNumber );
+                ad.Add( "LinkedEvent", eventGroupName );
 
                 // Specific Ad Fields
-                xmlElement.Add(
-                    new XAttribute( "AdType", marketingCampaignAd.MarketingCampaignAdType.Name ),
-                    new XAttribute( "StartDate", marketingCampaignAd.StartDate.ToString() ),
-                    new XAttribute( "EndDate", marketingCampaignAd.EndDate.ToString() ),
-                    new XAttribute( "Priority", marketingCampaignAd.Priority ),
-                    new XAttribute( "Url", marketingCampaignAd.Url ),
-                    new XAttribute( "DetailPageUrl", detailPageUrl ) );
+                ad.Add( "AdType", marketingCampaignAd.MarketingCampaignAdType.Name );
+                ad.Add( "StartDate", marketingCampaignAd.StartDate.ToString() );
+                ad.Add( "EndDate", marketingCampaignAd.EndDate.ToString() );
+                ad.Add( "Priority", marketingCampaignAd.Priority );
+                ad.Add( "Url", marketingCampaignAd.Url );
+                ad.Add( "DetailPageUrl", detailPageUrl );
 
                 // Ad Attributes
-                XElement attribsNode = new XElement( "Attributes" );
-                xmlElement.Add( attribsNode );
+                var attributes = new List<Dictionary<string, object>>();
+                ad.Add( "Attributes", attributes );
 
                 marketingCampaignAd.LoadAttributes();
                 Rock.Attribute.Helper.AddDisplayControls( marketingCampaignAd, phContent );
-                
+
                 // create image resize width/height from block settings
                 Dictionary<string, Rock.Field.ConfigurationValue> imageConfig = new Dictionary<string, Rock.Field.ConfigurationValue>();
-                if (!string.IsNullOrWhiteSpace(GetAttributeValue("ImageWidth"))
-                    && Int32.Parse(GetAttributeValue("ImageWidth")) != Int16.MinValue)
-                    imageConfig.Add("width", new Rock.Field.ConfigurationValue(GetAttributeValue("ImageWidth")));
+                if ( !string.IsNullOrWhiteSpace( GetAttributeValue( "ImageWidth" ) )
+                    && Int32.Parse( GetAttributeValue( "ImageWidth" ) ) != Int16.MinValue )
+                    imageConfig.Add( "width", new Rock.Field.ConfigurationValue( GetAttributeValue( "ImageWidth" ) ) );
 
-                if (!string.IsNullOrWhiteSpace(GetAttributeValue("ImageHeight"))
-                    && Int32.Parse(GetAttributeValue("ImageHeight")) != Int16.MinValue)
-                    imageConfig.Add("height", new Rock.Field.ConfigurationValue(GetAttributeValue("ImageHeight")));
-                
+                if ( !string.IsNullOrWhiteSpace( GetAttributeValue( "ImageHeight" ) )
+                    && Int32.Parse( GetAttributeValue( "ImageHeight" ) ) != Int16.MinValue )
+                    imageConfig.Add( "height", new Rock.Field.ConfigurationValue( GetAttributeValue( "ImageHeight" ) ) );
+
                 foreach ( var item in marketingCampaignAd.Attributes )
                 {
                     AttributeCache attribute = item.Value;
@@ -239,82 +224,94 @@ namespace RockWeb.Blocks.Cms
                     foreach ( AttributeValue attributeValue in attributeValues )
                     {
                         string valueHtml = string.Empty;
-                        
+
                         // If Block Attributes limit image types, limit images 
-                        if (attribute.FieldType.Guid.Equals(new Guid(Rock.SystemGuid.FieldType.IMAGE)))
+                        if ( attribute.FieldType.Guid.Equals( new Guid( Rock.SystemGuid.FieldType.IMAGE ) ) )
                         {
-                            if (imageTypeFilter != null)
+                            if ( imageTypeFilter != null )
                             {
-                                if (!imageTypeFilter.Contains(attribute.Key))
+                                if ( !imageTypeFilter.Contains( attribute.Key ) )
                                 {
                                     // skip to next attribute if this is an image attribute and it doesn't match the image key filter
                                     continue;
                                 }
                                 else
                                 {
-                                    valueHtml = attribute.FieldType.Field.FormatValue(this, attributeValue.Value, imageConfig, false);
+                                    valueHtml = attribute.FieldType.Field.FormatValue( this, attributeValue.Value, imageConfig, false );
                                 }
                             }
                         }
                         else
                         {
-                            valueHtml = attribute.FieldType.Field.FormatValue(this, attributeValue.Value, attribute.QualifierValues, false);
+                            valueHtml = attribute.FieldType.Field.FormatValue( this, attributeValue.Value, attribute.QualifierValues, false );
                         }
 
-                        XElement valueNode = new XElement( "Attribute" );
-                        valueNode.Add( new XAttribute( "Key", attribute.Key ) );
-                        valueNode.Add( new XAttribute( "Name", attribute.Name ) );
-                        valueNode.Add( new XAttribute( "Value", valueHtml ) );
-                        attribsNode.Add( valueNode );
+                        var valueNode = new Dictionary<string, object>();
+                        valueNode.Add( "Key", attribute.Key );
+                        valueNode.Add( "Name", attribute.Name );
+                        valueNode.Add( "Value", valueHtml );
+                        attributes.Add( valueNode );
                     }
                 }
 
-                rootNode.Add( xmlElement );
+                ads.Add( ad );
             }
+
+            var data = new Dictionary<string, object>();
+            data.Add( "Ads", ads );
+            data.Add( "ApplicationPath", HttpRuntime.AppDomainAppVirtualPath );
 
             string showDebugValue = GetAttributeValue( "ShowDebug" ) ?? string.Empty;
             bool showDebug = showDebugValue.Equals( "true", StringComparison.OrdinalIgnoreCase );
 
-            
-            string outputXml;
-            if ( showDebug || string.IsNullOrWhiteSpace( xsltFile ) )
+            string content;
+            try
             {
-                outputXml = "<pre><code>" + HttpUtility.HtmlEncode( doc.ToString() ) + "</code></pre>";
+                content = GetTemplate().Render( Hash.FromDictionary( data ) );
             }
-            else
+            catch ( Exception ex )
             {
-                StringBuilder sb = new StringBuilder();
-                TextWriter tw = new StringWriter( sb );
+                // xslt compile error
+                string exMessage = "An excception occurred while compiling the XSLT template.";
 
-                // try compiling the XSLT
-                try
-                {
-                    XslCompiledTransform xslTransformer = new XslCompiledTransform();
+                if ( ex.InnerException != null )
+                    exMessage += "<br /><em>" + ex.InnerException.Message + "</em>";
 
-                    // pass in xslt parms
-                    XsltArgumentList xsltArgs = new XsltArgumentList();
-                    xsltArgs.AddParam("application_path", "", HttpRuntime.AppDomainAppVirtualPath);
-                    // todo add theme path JME
-                                        
-                    xslTransformer.Load(xsltFile);
-                    xslTransformer.Transform(doc.CreateReader(), xsltArgs, tw);
-                    outputXml = sb.ToString();
-                }
-                catch (Exception ex)
-                {
-                    // xslt compile error
-                    string exMessage = "An excception occurred while compiling the XSLT template.";
-                    
-                    if (ex.InnerException != null)
-                        exMessage += "<br /><em>" + ex.InnerException.Message + "</em>";
-
-                    outputXml = "<div class='alert warning' style='margin: 24px auto 0 auto; max-width: 500px;' ><strong>XSLT Compile Error</strong><p>" + exMessage + "</p></div>";
-                }
+                content = "<div class='alert warning' style='margin: 24px auto 0 auto; max-width: 500px;' ><strong>XSLT Compile Error</strong><p>" + exMessage + "</p></div>";
             }
 
             phContent.Controls.Clear();
+            phContent.Controls.Add( new LiteralControl( content ) );
+        }
 
-            phContent.Controls.Add( new LiteralControl( outputXml ) );
+        private string CacheKey()
+        {
+            return string.Format( "Rock:MarketingCampaignAds:{0}", CurrentBlock.Id );
+        }
+
+        private Template GetTemplate()
+        {
+            string cacheKey = CacheKey();
+
+            ObjectCache cache = MemoryCache.Default;
+            Template template = cache[cacheKey] as Template;
+
+            if ( template != null )
+            {
+                return template;
+            }
+            else
+            {
+                Template.NamingConvention = new DotLiquid.NamingConventions.CSharpNamingConvention();
+                //TODO: This should probably use the theme assets folder
+                Template.FileSystem = new DotLiquid.FileSystems.LocalFileSystem( System.Web.HttpContext.Current.Server.MapPath( "~/Assets/Liquid" ) );
+                template = Template.Parse( GetAttributeValue( "Template" ) );
+
+                var cachePolicy = new CacheItemPolicy();
+                cache.Set( cacheKey, template, cachePolicy );
+
+                return template;
+            }
         }
     }
 }
