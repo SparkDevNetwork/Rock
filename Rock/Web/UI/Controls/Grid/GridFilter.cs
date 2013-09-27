02cfc732ecaa35bdd154701b0da986fc0f823823
edmistj <jonedmiston@ccvonline.com>
2013-09-27 15:05:53 -0700

    Clean up a type in a comment

--- Rock/Web/UI/Controls/Grid/GridFilter.cs
+++ Rock/Web/UI/Controls/Grid/GridFilter.cs
@@ -218,7 +218,7 @@ Sys.Application.add_load(function () {
 
                     if ( child != _lbFilter && child != _hfVisible )
                     {
-                        // add row column
+                        // add column
                         writer.AddAttribute("class", "col-md-4");
                         writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
