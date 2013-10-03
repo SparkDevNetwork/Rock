215e5a213e71483d5fdc45235566ed6f69a6e8d7
David Turner <david@azturners.com>
2013-10-03 08:31:53 -0700

    Clean up some compiler warnings

--- Rock/Model/PersonService.Partial.cs
+++ Rock/Model/PersonService.Partial.cs
@@ -243,6 +243,7 @@ namespace Rock.Model
             );
         }
 
+        /// <summary>
         /// Gets the families.
         /// </summary>
         /// <param name="person">The person.</param>
