f0697de3b810ce018ad7c6eeb9d284bf8e56d0b8
David Turner <david@azturners.com>
2013-10-30 04:39:37 -0700

    Update DefinedValue list to add custom columns before the delete column instead of the re-order column. Fixes #170

--- RockWeb/Blocks/Administration/DefinedValueList.ascx.cs
+++ RockWeb/Blocks/Administration/DefinedValueList.ascx.cs
@@ -270,7 +270,7 @@ namespace RockWeb.Blocks.Administration
                     boundField.DataField = dataFieldExpression;
                     boundField.HeaderText = item.Name;
                     boundField.SortExpression = string.Empty;
-                    int insertPos = gDefinedValues.Columns.IndexOf( gDefinedValues.Columns.OfType<ReorderField>().First());
+                    int insertPos = gDefinedValues.Columns.IndexOf( gDefinedValues.Columns.OfType<DeleteField>().First());
                     gDefinedValues.Columns.Insert(insertPos, boundField );
                 }
             }
