b3645a28df59be916925ad374f3ab589631cf5b5
Mike Peterson <MikePeterson@ccvonline.com>
2013-11-13 17:52:03 -0700

    Updated PanelWidget to have an optional ReorderIcon and Delete, and also did some StyleCop

--- Rock/Web/UI/Controls/PanelWidget.cs
+++ Rock/Web/UI/Controls/PanelWidget.cs
@@ -4,7 +4,8 @@
 // http://creativecommons.org/licenses/by-nc-sa/3.0/
 //
 
-using System.ComponentModel;
+using System;
+using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -21,7 +22,9 @@ namespace Rock.Web.UI.Controls
         /// <summary>
         /// The hidden field for tracking expanded
         /// </summary>
-        protected HiddenField hfExpanded;
+        private HiddenField _hfExpanded;
+
+        private LinkButton _lbDelete;
 
         /// <summary>
         /// Gets or sets the Title text.
@@ -33,6 +36,44 @@ namespace Rock.Web.UI.Controls
         {
             get { return ViewState["Title"] as string ?? string.Empty; }
             set { ViewState["Title"] = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [show reorder icon].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [show reorder icon]; otherwise, <c>false</c>.
+        /// </value>
+        public bool ShowReorderIcon
+        {
+            get
+            {
+                return ViewState["ShowReorderIcon"] as bool? ?? false;
+            }
+
+            set
+            {
+                ViewState["ShowReorderIcon"] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [show delete button].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [show delete button]; otherwise, <c>false</c>.
+        /// </value>
+        public bool ShowDeleteButton
+        {
+            get
+            {
+                return ViewState["ShowDeleteButton"] as bool? ?? false;
+            }
+
+            set
+            {
+                ViewState["ShowDeleteButton"] = value;
+            }
         }
 
         /// <summary>
@@ -48,14 +89,18 @@ namespace Rock.Web.UI.Controls
                 EnsureChildControls();
 
                 bool expanded = false;
-                if ( !bool.TryParse( hfExpanded.Value, out expanded ) )
+                if ( !bool.TryParse( _hfExpanded.Value, out expanded ) )
+                {
                     expanded = false;
+                }
+
                 return expanded;
             }
+
             set
             {
                 EnsureChildControls();
-                hfExpanded.Value = value.ToString();
+                _hfExpanded.Value = value.ToString();
             }
         }
 
@@ -63,6 +108,7 @@ namespace Rock.Web.UI.Controls
 
         protected override void OnInit( System.EventArgs e )
         {
+            EnsureChildControls();
             base.OnInit( e );
 
             string script = @"
@@ -76,20 +122,66 @@ $('.rock-panel-widget > header').click(function () {
     $('a.view-state > i', this).toggleClass('icon-chevron-down');
     $('a.view-state > i', this).toggleClass('icon-chevron-up');
 });
+
+
+// fix so that the Remove button will fire its event, but not the parent event 
+$('.rock-panel-widget a.btn-danger').click(function (event) {
+    event.stopImmediatePropagation();
+});
+
+// fix so that the Reorder button will fire its event, but not the parent event 
+$('.rock-panel-widget a.panel-widget-reorder').click(function (event) {
+    event.stopImmediatePropagation();
+});
+
 ";
             ScriptManager.RegisterStartupScript( this, this.GetType(), "RockPanelWidgetScript", script, true );
         }
 
+        /// <summary>
+        /// Called by the ASP.NET page framework to notify server controls that use composition-based implementation to create any child controls they contain in preparation for posting back or rendering.
+        /// </summary>
         protected override void CreateChildControls()
         {
             base.CreateChildControls();
 
-            hfExpanded = new HiddenField();
-            Controls.Add( hfExpanded );
-            hfExpanded.ID = this.ID + "_hfExpanded";
-            hfExpanded.Value = "False";
+            _hfExpanded = new HiddenField();
+            Controls.Add( _hfExpanded );
+            _hfExpanded.ID = this.ID + "_hfExpanded";
+            _hfExpanded.Value = "False";
+
+            _lbDelete = new LinkButton();
+            _lbDelete.CausesValidation = false;
+            _lbDelete.ID = this.ID + "_lbDelete";
+            _lbDelete.CssClass = "btn btn-xs btn-danger";
+            _lbDelete.Click += lbDelete_Click;
+            _lbDelete.Controls.Add( new LiteralControl { Text = "<i class='icon-remove'></i>" } );
+
+            Controls.Add( _lbDelete );
         }
 
+        /// <summary>
+        /// Handles the Click event of the lbDelete control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void lbDelete_Click( object sender, System.EventArgs e )
+        {
+            if ( DeleteClick != null )
+            {
+                DeleteClick( this, e );
+            }
+        }
+
+        /// <summary>
+        /// Occurs when [delete click].
+        /// </summary>
+        public event EventHandler DeleteClick;
+
+        /// <summary>
+        /// Outputs server control content to a provided <see cref="T:System.Web.UI.HtmlTextWriter" /> object and stores tracing information about the control if tracing is enabled.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> object that receives the control content.</param>
         public override void RenderControl( HtmlTextWriter writer )
         {
             if ( this.Visible )
@@ -104,7 +196,7 @@ $('.rock-panel-widget > header').click(function () {
 
                 // Hidden Field to track expansion
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, "filter-expanded" );
-                hfExpanded.RenderControl( writer );
+                _hfExpanded.RenderControl( writer );
 
                 // Title
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, "pull-left" );
@@ -114,44 +206,69 @@ $('.rock-panel-widget > header').click(function () {
                 writer.RenderEndTag();
                 writer.RenderEndTag();
 
-                // Chevron
+                // Panel Controls
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, "pull-right" );
                 writer.RenderBeginTag( HtmlTextWriterTag.Div );
+
+                if ( ShowReorderIcon )
+                {
+                    // Reorder Icon
+                    writer.AddAttribute( HtmlTextWriterAttribute.Class, "btn btn-xs panel-widget-reorder" );
+                    writer.RenderBeginTag( HtmlTextWriterTag.A );
+                    writer.AddAttribute( HtmlTextWriterAttribute.Class, "icon-reorder" );
+                    writer.RenderBeginTag( HtmlTextWriterTag.I );
+                    writer.RenderEndTag();
+                    writer.RenderEndTag();
+                }
+
+                // Chevron up/down Button
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, "btn btn-xs view-state" );
                 writer.RenderBeginTag( HtmlTextWriterTag.A );
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, Expanded ? "icon-chevron-up" : "icon-chevron-down" );
                 writer.RenderBeginTag( HtmlTextWriterTag.I );
                 writer.RenderEndTag();
                 writer.RenderEndTag();
-                writer.RenderEndTag();
 
-                writer.RenderEndTag();  // Header                
-                
+                if ( ShowDeleteButton )
+                {
+                    _lbDelete.Visible = true;
+                    _lbDelete.RenderControl( writer );
+                }
+                else
+                {
+                    _lbDelete.Visible = false;
+                }
+
+                writer.RenderEndTag(); // pull-right
+
+                writer.RenderEndTag(); // Header                
+
                 // Body
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, "panel-body" );
                 if ( !Expanded )
                 {
                     writer.AddStyleAttribute( HtmlTextWriterStyle.Display, "none" );
-                } 
+                }
+
                 writer.RenderBeginTag( HtmlTextWriterTag.Div );
 
                 // Render placeholder's child controls
                 if ( this.Controls != null )
                 {
+                    var builtInControls = new Control[] { _hfExpanded, _lbDelete };
                     foreach ( Control child in this.Controls )
                     {
-                        if ( child != hfExpanded )
+                        if ( !builtInControls.Contains( child ) )
                         {
                             child.RenderControl( writer );
                         }
                     }
                 }
 
-                writer.RenderEndTag();  
+                writer.RenderEndTag();
 
                 writer.RenderEndTag();  // Section
             }
         }
-
     }
 }
