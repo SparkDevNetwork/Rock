c5de3066c2cc9115b9bccbdf38fad333b4a9515c
Mike Peterson <MikePeterson@ccvonline.com>
2013-11-14 17:28:01 -0700

    + Added DataSelectComponent/DataSelectContainer for Report "Additional Columns" MEF components + Started on LastContributionSelect DataSelectComponent

---
+++ Rock/Reporting/DataSelectComponent.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.UI;
+using Rock.Extension;
+
+namespace Rock.Reporting
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class DataSelectComponent : Component
+    {
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public abstract string Title { get; }
+
+        /// <summary>
+        /// Gets the name of the entity type.
+        /// </summary>
+        /// <value>
+        /// The name of the entity type.
+        /// </value>
+        public abstract string EntityTypeName { get; }
+
+        /// <summary>
+        /// Gets the section.
+        /// </summary>
+        /// <value>
+        /// The section.
+        /// </value>
+        public virtual string Section
+        {
+            get { return "Additional Columns"; }
+        }
+
+        /// <summary>
+        /// Gets the attribute value defaults.
+        /// </summary>
+        /// <value>
+        /// The attribute defaults.
+        /// </value>
+        public override Dictionary<string, string> AttributeValueDefaults
+        {
+            get
+            {
+                var defaults = new Dictionary<string, string>();
+                defaults.Add( "Active", "True" );
+                return defaults;
+            }
+        }
+
+        /// <summary>
+        /// Formats the selection on the client-side.  When the widget is collapsed by the user, the Filterfield control
+        /// will set the description of the filter to whatever is returned by this property.  If including script, the
+        /// controls parent container can be referenced through a '$content' variable that is set by the control before 
+        /// referencing this property.
+        /// </summary>
+        /// <value>
+        /// The client format script.
+        /// </value>
+        public virtual string GetClientFormatSelection( Type entityType )
+        {
+            return this.Title;
+        }
+
+        /// <summary>
+        /// Formats the selection.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="selection">The selection.</param>
+        /// <returns></returns>
+        public virtual string FormatSelection( string selection )
+        {
+            return this.Title;
+        }
+
+        /// <summary>
+        /// Creates the child controls.
+        /// </summary>
+        /// <returns></returns>
+        public virtual Control[] CreateChildControls( Type entityType, Control parentControl )
+        {
+            return new Control[0];
+        }
+
+        /// <summary>
+        /// Renders the controls.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="filterControl">The filter control.</param>
+        /// <param name="writer">The writer.</param>
+        /// <param name="controls">The controls.</param>
+        public virtual void RenderControls( Type entityType, Control parentControl, HtmlTextWriter writer, Control[] controls )
+        {
+            foreach ( var control in controls )
+            {
+                control.RenderControl( writer );
+                writer.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Gets the selection.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="controls">The controls.</param>
+        /// <returns></returns>
+        public virtual string GetSelection( Type entityType, Control[] controls )
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Sets the selection.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="controls">The controls.</param>
+        /// <param name="selection">The selection.</param>
+        public virtual void SetSelection( Type entityType, Control[] controls, string selection )
+        {
+            string[] selectionValues = selection.Split( '|' );
+        }
+
+        /// <summary>
+        /// Gets the data columns.
+        /// </summary>
+        /// <value>
+        /// The data columns.
+        /// </value>
+        public abstract List<DataColumn> DataColumns { get; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class DataSelectComponent<T> : DataSelectComponent
+    {
+        /// <summary>
+        /// Gets the data column values.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
+        public abstract List<object> GetDataColumnValues( T item );
+    }
+}
