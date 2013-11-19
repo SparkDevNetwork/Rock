c8ba098cdef0cc4de3e917bc9e1067404e20c2cb
David Turner <david@azturners.com>
2013-11-19 09:57:49 -0700

    Fix block move after changing zones to use name instead of id.

--- Rock/Web/UI/RockPage.cs
+++ Rock/Web/UI/RockPage.cs
@@ -1164,7 +1164,7 @@ namespace Rock.Web.UI
             ddlZones.ID = "block-move-zone";
             ddlZones.Label = "Zone";
             foreach ( var zone in Zones )
-                ddlZones.Items.Add( new ListItem( zone.Value.Key, zone.Key ) );
+                ddlZones.Items.Add( new ListItem( zone.Value.Key, zone.Key.ToLower() ) );
             fsZoneSelect.Controls.Add( ddlZones );
 
             RockRadioButtonList rblLocation = new RockRadioButtonList();
