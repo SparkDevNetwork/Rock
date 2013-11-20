c7ba9e3edbb851efaac921c09673728a3045df80
David Turner <david@azturners.com>
2013-11-20 11:44:11 -0700

    Update field types to use the base fieldtype's FormatValue function since it will HtmlEncode results when displaying the condensed value in a grid

--- Rock/Field/Types/PersonFieldType.cs
+++ Rock/Field/Types/PersonFieldType.cs
@@ -29,17 +29,19 @@ namespace Rock.Field.Types
         /// <returns></returns>
         public override string FormatValue( System.Web.UI.Control parentControl, string value, Dictionary<string, ConfigurationValue> configurationValues, bool condensed )
         {
+            string formattedValue = string.Empty;
+
             if ( !string.IsNullOrWhiteSpace( value ) )
             {
                 var service = new PersonService();
                 var person = service.Get( new Guid( value ) );
                 if ( person != null )
                 {
-                    return person.FullName;
+                    formattedValue = person.FullName;
                 }
             }
 
-            return string.Empty;
+            return base.FormatValue( parentControl, formattedValue, null, condensed );
         }
 
         /// <summary>
