bf45566c3e42295f8b2b59ae4216cb43614a9f51
David Turner <david@azturners.com>
2013-12-04 11:56:52 -0700

    +Update SiteDetail to only require a DefaultPage when editing an existing site.  If creating a new site and DefaultPage is left blank, a default page will get created automatically. +Updated Page Properties so that a different site can be selected

--- RockWeb/Blocks/Cms/SiteDetail.ascx.cs
+++ RockWeb/Blocks/Cms/SiteDetail.ascx.cs
@@ -176,9 +176,10 @@ namespace RockWeb.Blocks.Cms
                     site.FacebookAppId = tbFacebookAppId.Text;
                     site.FacebookAppSecret = tbFacebookAppSecret.Text;
 
-                    if (!site.DefaultPageId.HasValue)
+                    if (!site.DefaultPageId.HasValue && !newSite)
                     {
                         ppDefaultPage.ShowErrorMessage( "Default Page is required." );
+                        return;
                     }
 
                     if ( !site.IsValid )
@@ -198,6 +199,41 @@ namespace RockWeb.Blocks.Cms
                     } );
 
                     SiteCache.Flush( site.Id );
+
+                    // Create the default page is this is a new site
+                    if ( !site.DefaultPageId.HasValue && newSite )
+                    {
+                        var siteCache = SiteCache.Read( site.Id );
+
+                        // Create the layouts for the site, and find the first one
+                        var layoutService = new LayoutService();
+                        layoutService.RegisterLayouts( Request.MapPath( "~" ), siteCache, CurrentPersonId );
+                        var layout = layoutService.GetBySiteId( siteCache.Id ).FirstOrDefault();
+                        if ( layout != null )
+                        {
+                            var pageService = new PageService();
+                            var page = new Page();
+                            page.LayoutId = layout.Id;
+                            page.Title = siteCache.Name + " Home Page";
+                            page.Name = page.Title;
+                            page.EnableViewState = true;
+                            page.IncludeAdminFooter = true;
+
+                            var lastPage = pageService.GetByParentPageId( null ).
+                                OrderByDescending( b => b.Order ).FirstOrDefault();
+
+                            page.Order = lastPage != null ? lastPage.Order + 1 : 0;
+                            pageService.Add( page, CurrentPersonId );
+                            pageService.Save( page, CurrentPersonId );
+
+                            site = siteService.Get( siteCache.Id );
+                            site.DefaultPageId = page.Id;
+                            siteService.Save( site, CurrentPersonId );
+
+                            SiteCache.Flush( site.Id );
+                        }
+                    }
+
                 }
 
                 var qryParams = new Dictionary<string, string>();
