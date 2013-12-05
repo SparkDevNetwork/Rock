aa4e760d14c1d3335084ca6181669260fae123b1
David Turner <david@azturners.com>
2013-12-05 16:57:02 -0700

    Redo validation script so that it works with Chrome

--- RockWeb/Scripts/Rock/Validate/validation.js
+++ RockWeb/Scripts/Rock/Validate/validation.js
@@ -1,39 +1,39 @@
 (function (Sys) {
     'use strict';
     Sys.Application.add_load(function () {
+          
+        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        var myObserver = new MutationObserver(mutationHandler);
+        var obsConfig = { attributes: true };
 
-        // Monitors validation controls to watch for them becoming invalid.  Once
-        // They are invalid, the validated control's parent <dl> tag will have the 'error'
-        // css class added.  This requires that the validation control's display be set to
-        // dynamic.  This script will also clear the error text display by the control (requires
-        // that a validation summary control be used.
-
-        $("span.validation-error").bind("DOMAttrModified propertychange", function (e) {
-
-            // Exit early if IE because it throws this event lots more
-            if (e.originalEvent.propertyName && e.originalEvent.propertyName != "isvalid") return;
-
-            var controlToValidate = $("#" + this.controltovalidate);
-            if (controlToValidate == null) return;
-
-            var isValid = true;
-            
-            $(controlToValidate).each(function () {
-                $(this.Validators).each(function () {
-                    if (this.isvalid !== true) {
-                        isValid = false;
-                    }
-                });
-            });
-
-            this.innerHTML = '';
-
-            if (isValid) {
-                controlToValidate.parents('div.form-group').removeClass("has-error");
-            } else {
-                controlToValidate.parents('div.form-group').addClass("has-error");
-            }
+        $("span.validation-error").each(function () {
+            myObserver.observe(this, obsConfig);
         });
+
+        function mutationHandler(mutationRecords) {
+            mutationRecords.forEach(function (mutation) {
+
+                var controlToValidate = $("#" + mutation.target.controltovalidate);
+
+                var isValid = true;
+
+                $(controlToValidate).each(function () {
+                    $(this.Validators).each(function () {
+                        if (this.isvalid !== true) {
+                            isValid = false;
+                        }
+                    });
+                });
+
+                mutation.target.innerHTML = '';
+
+                if (isValid) {
+                    controlToValidate.parents('div.form-group').removeClass("has-error");
+                } else {
+                    controlToValidate.parents('div.form-group').addClass("has-error");
+                }
+            })
+        }
 
     });
 }(Sys));
