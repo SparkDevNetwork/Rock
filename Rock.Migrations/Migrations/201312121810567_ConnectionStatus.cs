1a76a69b14d5620cd06d3a5fee6f614c3e89feca
David Turner <david@azturners.com>
2013-12-12 11:50:50 -0700

    Rename PersonStatus Badge to ConnectionStatus

--- Rock.Migrations/Migrations/201312121810567_ConnectionStatus.cs
+++ Rock.Migrations/Migrations/201312121810567_ConnectionStatus.cs
@@ -27,6 +27,17 @@ namespace Rock.Migrations
 
             Sql ( @"
     UPDATE [Attribute] SET [Key] = 'DefaultConnectionStatus' WHERE [key] = 'DefaultPersonStatus'
+
+    -- Delete values that may have been created by framework before running migration
+    DELETE [Attribute] WHERE [EntityTypeId] IN (
+	    SELECT [Id] FROM [EntityType] WHERE [Name] = 'Rock.PersonProfile.Badge.ConnectionStatus'   
+    )
+    DELETE [EntityType] WHERE [Name] = 'Rock.PersonProfile.Badge.ConnectionStatus'   
+    UPDATE [EntityType] SET 
+	    [Name] = 'Rock.PersonProfile.Badge.ConnectionStatus',
+	    [AssemblyName] = 'Rock.PersonProfile.Badge.ConnectionStatus, Rock, Version=0.0.1.0, Culture=neutral, PublicKeyToken=null',
+	    [FriendlyName] = 'Connection Status'
+    WHERE [Name] = 'Rock.PersonProfile.Badge.PersonStatus'
 " );
         }
         
@@ -37,6 +48,17 @@ namespace Rock.Migrations
         {
             Sql( @"
     UPDATE [Attribute] SET [Key] = 'DefaultPersonStatus' WHERE [key] = 'DefaultConnectionStatus'
+
+    -- Delete values that may have been created by framework before running migration
+    DELETE [Attribute] WHERE [EntityTypeId] IN (
+	    SELECT [Id] FROM [EntityType] WHERE [Name] = 'Rock.PersonProfile.Badge.PersonStatus'   
+    )
+    DELETE [EntityType] WHERE [Name] = 'Rock.PersonProfile.Badge.PersonStatus'   
+    UPDATE [EntityType] SET 
+	    [Name] = 'Rock.PersonProfile.Badge.PersonStatus',
+	    [AssemblyName] = 'Rock.PersonProfile.Badge.PersonStatus, Rock, Version=0.0.1.0, Culture=neutral, PublicKeyToken=null',
+	    [FriendlyName] = 'Person Status'
+    WHERE [Name] = 'Rock.PersonProfile.Badge.ConnectionStatus'
 " ); 
             AddColumn( "dbo.Person", "PersonStatusValueId", c => c.Int() );
             DropForeignKey("dbo.Person", "ConnectionStatusValueId", "dbo.DefinedValue");
