1fc946cd8d847b885e7a44ded91b039699d6b951
Mike Peterson <MikePeterson@ccvonline.com>
2013-12-13 18:27:06 -0700

    started on the HtmlEditor plugins

---
+++ RockWeb/Scripts/ckeditor/plugins/rockdocumentbrowser/plugin.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    CKEDITOR.plugins.add('rockdocumentbrowser', {
+        init: function (editor) {
+            editor.addCommand('rockdocumentbrowser', {
+                exec: function (editor) {
+                    var timestamp = new Date();
+                    editor.insertHtml('##TODO: dialog to pick/upload documents##');
+                }
+            });
+            editor.ui.addButton && editor.ui.addButton('rockdocumentbrowser', {
+                label: 'Insert rockdocumentbrowser',
+                command: 'rockdocumentbrowser',
+                icon: this.path + 'rockdocumentbrowser.png'
+            });
+        }
+    });
+})()
