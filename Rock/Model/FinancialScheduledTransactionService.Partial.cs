02ede22b4ee80880321c73c89727bd7da7d9248e
David Turner <david@azturners.com>
2013-12-16 13:30:32 -0700

    Update GivingProfileList to work with person context

---
+++ Rock/Model/FinancialScheduledTransactionService.Partial.cs
@@ -0,0 +1,51 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Rock.Data;
+
+namespace Rock.Model
+{
+    /// <summary>
+    /// Service/Data access class for <see cref="Rock.Model.FinancialScheduledTransaction"/> entity objects.
+    /// </summary>
+    public partial class FinancialScheduledTransactionService 
+    {
+        /// <summary>
+        /// Gets schedule transactions associated to a person.  Includes any transactions associated to person
+        /// or any other perosn with same giving group id
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <param name="givingGroupId">The giving group identifier.</param>
+        /// <returns>
+        /// The <see cref="Rock.Model.FinancialTransaction"/> that matches the transaction code, this value will be null if a match is not found.
+        /// </returns>
+        public IQueryable<FinancialScheduledTransaction> Get( int personId, int? givingGroupId, bool includeInactive )
+        {
+            if ( givingGroupId.HasValue )
+            {
+                return Repository.AsQueryable()
+                    .Where( t => 
+                        t.AuthorizedPerson.GivingGroupId == givingGroupId.Value &&
+                        (t.IsActive || includeInactive) )
+                    .OrderByDescending(t => t.IsActive)
+                    .ThenByDescending(t => t.StartDate);
+            }
+            else
+            {
+                return Repository.AsQueryable()
+                    .Where( t =>
+                        t.AuthorizedPersonId == personId &&
+                        ( t.IsActive || includeInactive ) )
+                    .OrderByDescending( t => t.IsActive )
+                    .ThenByDescending( t => t.StartDate );
+            }
+            return null;
+        }
+    }
+}
