693e87f562c26c0bf9b471356aeb45c4a429d3a7
Mike Peterson <mpeterson@CCV13-L004.ccvonline.com>
2013-12-18 18:45:02 -0700

    a bunch of progress on the RockImageBrowser/RockDocumentBrowser (will probably be combined into RockFileBrowser). It is just barely functional.

--- RockWeb/Scripts/ckeditor/plugins/rockimagebrowser/plugin.js
+++ RockWeb/Scripts/ckeditor/plugins/rockimagebrowser/plugin.js
@@ -1,17 +1,16 @@
 ﻿(function () {
     CKEDITOR.plugins.add('rockimagebrowser', {
         init: function (editor) {
-            editor.addCommand('rockimagebrowser', {
-                exec: function (editor) {
-                    var timestamp = new Date();
-                    editor.insertHtml('##TODO Dialog to pick/upload images##');
-                }
-            });
+
+            editor.addCommand('rockimagebrowserDialog', new CKEDITOR.dialogCommand('rockimagebrowserDialog'));
+
             editor.ui.addButton && editor.ui.addButton('rockimagebrowser', {
-                label: 'Insert rockimagebrowser',
-                command: 'rockimagebrowser',
+                label: 'Image Browser',
+                command: 'rockimagebrowserDialog',
                 icon: this.path + 'rockimagebrowser.png'
             });
+
+            CKEDITOR.dialog.add('rockimagebrowserDialog', this.path + 'dialogs/rockimagebrowser.js');
         }
     });
 })()
