e1a079267049efb30fadb24a2acccd42b8db126e
David Turner <david@azturners.com>
2013-12-23 11:20:46 -0700

    Fixed a routing problem that occurred when search term included a period

--- RockWeb/Scripts/Rock/Controls/searchField.js
+++ RockWeb/Scripts/Rock/Controls/searchField.js
@@ -37,7 +37,7 @@
                         var keyVal = self.$el.parents('.smartsearch').find('input:hidden').val(),
                             $li = self.$el.parents('.smartsearch').find('li[data-key="' + keyVal + '"]'),
                             targetUrl = $li.attr('data-target'),
-                            url = Rock.settings.get('baseUrl') + targetUrl.replace('{0}', encodeURIComponent(term));
+                            url = Rock.settings.get('baseUrl') + targetUrl.replace('{0}', encodeURIComponent(term)).replace('.', '');
 
                         window.location = url;
                     };
