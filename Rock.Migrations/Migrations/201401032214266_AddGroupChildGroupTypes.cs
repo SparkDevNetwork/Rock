776dbdeb5367fce149751b158df5ef03960979e9
Nick Airdo <nairdo@gmail.com>
2014-01-03 15:52:39 -0700

    Closes #262 to allow Serving Team and Event GroupTypes to have child groups.

---
+++ Rock.Migrations/Migrations/201401032214266_AddGroupChildGroupTypes.cs
@@ -0,0 +1,34 @@
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+namespace Rock.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    /// <summary>
+    ///
+    /// </summary>
+    public partial class AddGroupChildGroupTypes : Rock.Migrations.RockMigration
+    {
+        /// <summary>
+        /// Operations to be performed during the upgrade process.
+        /// </summary>
+        public override void Up()
+        {
+            Sql( "IF NOT EXISTS (SELECT 1 FROM [GroupTypeAssociation] WHERE [GroupTypeId] = 13 AND [ChildGroupTypeId] = 13) INSERT INTO [GroupTypeAssociation] ([GroupTypeId], [ChildGroupTypeId]) VALUES (13, 13)" );
+            Sql( "IF NOT EXISTS (SELECT 1 FROM [GroupTypeAssociation] WHERE [GroupTypeId] = 23 AND [ChildGroupTypeId] = 23) INSERT INTO [GroupTypeAssociation] ([GroupTypeId], [ChildGroupTypeId]) VALUES (23, 23)" );
+        }
+        
+        /// <summary>
+        /// Operations to be performed during the downgrade process.
+        /// </summary>
+        public override void Down()
+        {
+            Sql( @"DELETE FROM [GroupTypeAssociation] WHERE [GroupTypeId] = 13 AND [ChildGroupTypeId] = 13" );
+            Sql( @"DELETE FROM [GroupTypeAssociation] WHERE [GroupTypeId] = 23 AND [ChildGroupTypeId] = 23" );
+        }
+    }
+}
