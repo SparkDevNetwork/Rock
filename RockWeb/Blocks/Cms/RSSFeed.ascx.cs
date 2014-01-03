4c77bf5bd58a3b85a151cf25385080080aafd946
chrisfunk <cfunk@secc.org>
2014-01-03 16:58:21 -0500

    RSS Feed initial

---
+++ RockWeb/Blocks/Cms/RSSFeed.ascx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Cms
+{
+    public partial class RSSFeed : RockBlock
+    {
+        #region Control Methods
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+        }
+        #endregion
+
+        #region Internal Methods
+
+        #endregion
+    }
+}
