560b63376a829dc8b9a07d18d49eaaf14bc8d31e
Jon Edmiston <jon@sparkdevnetwork.org>
2014-01-06 15:17:30 -0700

    Added migration to rename 'Adult Parent' and 'Adult Child' to just 'Parent' and Child' and to add a new role called "Former Spouse"

---
+++ Rock.Migrations/Migrations/201401062203180_RenameAdultChildParentRoles.cs
@@ -0,0 +1,60 @@
+//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+namespace Rock.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    /// <summary>
+    ///
+    /// </summary>
+    public partial class RenameAdultChildParentRoles : Rock.Migrations.RockMigration
+    {
+        /// <summary>
+        /// Operations to be performed during the upgrade process.
+        /// </summary>
+        public override void Up()
+        {
+            Sql(@"
+                UPDATE 
+                    [GroupTypeRole]
+                SET 
+                    [Name] = 'Child'
+                WHERE 
+                    [Guid] = 'F87DF00F-E86D-4771-A3AE-DBF79B78CF5D'");
+
+            Sql(@"
+                UPDATE 
+                    [GroupTypeRole]
+                SET 
+                    [Name] = 'Parent'
+                WHERE 
+                    [Guid] = '6F3FADC4-6320-4B54-9CF6-02EF9586A660'");
+        }
+        
+        /// <summary>
+        /// Operations to be performed during the downgrade process.
+        /// </summary>
+        public override void Down()
+        {
+            Sql(@"
+                UPDATE 
+                    [GroupTypeRole]
+                SET 
+                    [Name] = 'Adult Child'
+                WHERE 
+                    [Guid] = 'F87DF00F-E86D-4771-A3AE-DBF79B78CF5D'");
+
+            Sql(@"
+                UPDATE 
+                    [GroupTypeRole]
+                SET 
+                    [Name] = 'Adult Parent Of'
+                WHERE 
+                    [Guid] = '6F3FADC4-6320-4B54-9CF6-02EF9586A660'");
+        }
+    }
+}
