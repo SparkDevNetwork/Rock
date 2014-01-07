d7ed396484b95e3d10b5fba5983a23eb33df57f7
Mike Peterson <MikePeterson@ccvonline.com>
2014-01-07 15:06:29 -0700

    + combine rockimagebrowser/rockdocumentbrowser into just one rockfilebrowser for now since they will be nearly identical + added developer.css so that rockfilebrowser images have a width/height for both regular and svg imagefiles

---
+++ RockWeb/Scripts/ckeditor/plugins/rockfilebrowser/plugin.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    CKEDITOR.plugins.add('rockfilebrowser', {
+        init: function (editor) {
+
+            editor.addCommand('rockfilebrowserDialog', new CKEDITOR.dialogCommand('rockfilebrowserDialog'));
+
+            editor.ui.addButton && editor.ui.addButton('rockfilebrowser', {
+                label: 'File Browser',
+                command: 'rockfilebrowserDialog',
+                icon: this.path + 'rockfilebrowser.png'
+            });
+
+            CKEDITOR.dialog.add('rockfilebrowserDialog', this.path + 'dialogs/rockfilebrowser.js');
+        }
+    });
+})()
