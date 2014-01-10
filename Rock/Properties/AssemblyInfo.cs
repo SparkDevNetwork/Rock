9d91402dd83f8a75b2d3af4d3e83a4980594c92b
David Turner <david@azturners.com>
2014-01-09 22:29:32 -0700

    Update version numbers to Humphreys 1.1 (0.1.1.0)

--- Rock/Properties/AssemblyInfo.cs
+++ Rock/Properties/AssemblyInfo.cs
@@ -10,22 +10,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rock")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Spark Development Network")]
-[assembly: AssemblyProduct("Rock")]
-[assembly: AssemblyCopyright("Copyright © Spark Development Network 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "Rock" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Spark Development Network" )]
+[assembly: AssemblyProduct( "Rock" )]
+[assembly: AssemblyCopyright( "Copyright © Spark Development Network 2011-2014" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2b98b90-6dcb-4049-ad04-353c9b46a113")]
+[assembly: Guid( "a2b98b90-6dcb-4049-ad04-353c9b46a113" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 //[assembly: AssemblyVersion( "0.0.0.*" )]    // I've read some things that make me think we should not do this... comments?
-[assembly: AssemblyVersion( "0.1.0.0" )]
-[assembly: AssemblyFileVersion( "0.1.0.0" )]
+[assembly: AssemblyVersion( "0.1.1.0" )]
+[assembly: AssemblyFileVersion( "0.1.1.0" )]
 
 
