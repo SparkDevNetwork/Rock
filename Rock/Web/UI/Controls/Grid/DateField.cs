190e36ec6966e8ae4095feaf6b7b45595c1ef883
David Turner <david@azturners.com>
2014-01-13 16:35:45 -0700

    Added a column to GroupMember list that will display how long ago a person was added to the group

--- Rock/Web/UI/Controls/Grid/DateField.cs
+++ Rock/Web/UI/Controls/Grid/DateField.cs
@@ -3,9 +3,11 @@
 // SHAREALIKE 3.0 UNPORTED LICENSE:
 // http://creativecommons.org/licenses/by-nc-sa/3.0/
 //
-
+using System;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
+using Rock;
 
 namespace Rock.Web.UI.Controls
 {
@@ -16,6 +18,18 @@ namespace Rock.Web.UI.Controls
     public class DateField : BoundField
     {
         /// <summary>
+        /// Gets or sets a value indicating whether value should be displayed as an elapsed time (i.e. "3 days ago").
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if [format as elapsed time]; otherwise, <c>false</c>.
+        /// </value>
+        public bool FormatAsElapsedTime
+        {
+            get { return ViewState["FormatAsElapsedTime"] as bool? ?? false; }
+            set { ViewState["FormatAsElapsedTime"] = value; }
+        }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="DateField" /> class.
         /// </summary>
         public DateField()
@@ -24,6 +38,32 @@ namespace Rock.Web.UI.Controls
             // Let the Header be left aligned (that's how Bootstrap wants it), but have the item be right-aligned
             this.ItemStyle.HorizontalAlign = HorizontalAlign.Right;
             this.DataFormatString = "{0:d}";
+        }
+
+        /// <summary>
+        /// Formats the specified field value for a cell in the <see cref="T:System.Web.UI.WebControls.BoundField" /> object.
+        /// </summary>
+        /// <param name="dataValue">The field value to format.</param>
+        /// <param name="encode">true to encode the value; otherwise, false.</param>
+        /// <returns>
+        /// The field value converted to the format specified by <see cref="P:System.Web.UI.WebControls.BoundField.DataFormatString" />.
+        /// </returns>
+        protected override string FormatDataValue( object dataValue, bool encode )
+        {
+            if ( FormatAsElapsedTime )
+            {
+                if ( dataValue is DateTime )
+                {
+                    return ( (DateTime)dataValue ).ToElapsedString( false, false );
+                }
+
+                if ( dataValue is DateTime? )
+                {
+                    return ( (DateTime)dataValue ).ToElapsedString( false, false );
+                }
+            }
+
+            return base.FormatDataValue( dataValue, encode );
         }
     }
 }
