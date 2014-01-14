dd858607bef68880c745bc147149fd7601774e78
richarddubay <richardtdubayjr@gmail.com>
2014-01-14 15:04:22 -0500

    Created a tabbed interface for Site Details to switch between showing the site's layouts and the site's pages. In order to do this we've: + added a PageLiquid menu in between the SiteDetail and LayoutList blocks on the SiteDetail page + created a second SiteDetail page to hold three blocks: SiteDetail, PageLiquid and a new PageList block. + created the PageList block to show the Pages associated with a site and it's layouts. Clicking on an item in the grid will take you to that particular page.

---
+++ RockWeb/Blocks/Cms/PageList.ascx.cs
@@ -0,0 +1,179 @@
+﻿//
+// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
+// SHAREALIKE 3.0 UNPORTED LICENSE:
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Cms
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DisplayName( "Page List" )]
+    [Category( "CMS" )]
+    [Description( "Lists pages for a site." )]
+    public partial class PageList : RockBlock, ISecondaryBlock
+    {
+        #region Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            gPages.DataKeyNames = new string[] { "Guid" };
+            gPages.GridRebind += gPages_GridRebind;
+            gPagesFilter.ApplyFilterClick += gPagesFilter_ApplyFilterClick;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+            if ( !Page.IsPostBack )
+            {
+                BindFilter();
+                BindPagesGrid();
+            }
+        }
+
+        #endregion
+
+        #region Edit Events
+
+        /// <summary>
+        /// Handles the Edit event of the gPages control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void gPages_Edit( object sender, RowEventArgs e )
+        {
+            var queryString = new Dictionary<string, string>();
+            NavigateToPage( new Guid( e.RowKeyValue.ToString() ), queryString );
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the gPages control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        void gPages_GridRebind( object sender, EventArgs e )
+        {
+            BindPagesGrid();
+        }
+
+        /// <summary>
+        /// Handles the ApplyFilterClick event of the gPagesFilter control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        void gPagesFilter_ApplyFilterClick( object sender, EventArgs e )
+        {
+            if ( ddlLayoutFilter.SelectedIndex > 0 )
+            {
+                gPagesFilter.SaveUserPreference( "Layout", ddlLayoutFilter.SelectedValue );
+            }
+            else
+            {
+                gPagesFilter.SaveUserPreference( "Layout", "" );
+            }
+
+            BindPagesGrid();
+        }
+
+        #endregion
+
+        #region Internal Methods
+
+        /// <summary>
+        /// Binds the filter.
+        /// </summary>
+        private void BindFilter()
+        {
+            int siteId = PageParameter( "siteId" ).AsInteger() ?? 0;
+            LayoutService layoutService = new LayoutService();
+            layoutService.RegisterLayouts( Request.MapPath( "~" ), SiteCache.Read( siteId ), CurrentPersonId );
+            var layouts = layoutService.Queryable().Where( a => a.SiteId.Equals( siteId ) ).ToList();
+            ddlLayoutFilter.DataSource = layouts;
+            ddlLayoutFilter.DataBind();
+            ddlLayoutFilter.Items.Insert( 0, Rock.Constants.All.ListItem );
+            ddlLayoutFilter.Visible = layouts.Any();
+            ddlLayoutFilter.SetValue( gPagesFilter.GetUserPreference( "Layout" ) );
+        }
+
+        /// <summary>
+        /// Binds the pages grid.
+        /// </summary>
+        protected void BindPagesGrid()
+        {
+            pnlPages.Visible = false;
+            int siteId = PageParameter( "siteId" ).AsInteger() ?? 0;
+            if ( siteId == 0 )
+            {
+                // quit if the siteId can't be determined
+                return;
+            }
+
+            hfSiteId.SetValue( siteId );
+            pnlPages.Visible = true;
+
+            LayoutService layoutService = new LayoutService();
+            layoutService.RegisterLayouts( Request.MapPath( "~" ), SiteCache.Read( siteId ), CurrentPersonId );
+            var layouts = layoutService.Queryable().Where( a => a.SiteId.Equals( siteId ) ).Select( a => a.Id ).ToList();
+
+            var siteService = new SiteService();
+            var pageId = siteService.Get( siteId ).DefaultPageId;
+
+            var pageService = new PageService();
+            var qry = pageService.GetAllDescendents( (int)pageId ).AsQueryable().Where( a => layouts.Contains(a.LayoutId) );
+
+            string layoutFilter = gPagesFilter.GetUserPreference( "Layout" );
+            if ( !string.IsNullOrWhiteSpace( layoutFilter ) && layoutFilter != Rock.Constants.All.Text )
+            {
+                qry = qry.Where( a => a.Layout.ToString() == layoutFilter );
+            }
+
+            SortProperty sortProperty = gPages.SortProperty;
+            if ( sortProperty != null )
+            {
+                qry = qry.Sort( sortProperty );
+            }
+            else
+            {
+                qry = qry.OrderBy( q => q.Id );
+            }
+
+            gPages.DataSource = qry.ToList();
+            gPages.DataBind();
+        }
+
+        /// <summary>
+        /// Sets the visible.
+        /// </summary>
+        /// <param name="visible">if set to <c>true</c> [visible].</param>
+        public void SetVisible( bool visible )
+        {
+            pnlContent.Visible = visible;
+        }
+
+        #endregion
+    }
+}
