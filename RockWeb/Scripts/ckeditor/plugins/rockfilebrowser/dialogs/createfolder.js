8f896db28701de60a375debe579cb8868e8def66
Mike Peterson <MikePeterson@ccvonline.com>
2014-01-13 17:16:46 -0700

    rockfilebrowser - Create/Delete/Rename folder slow progress, but fixed a couple of issues when loading the dialogs multiple times

--- RockWeb/Scripts/ckeditor/plugins/rockfilebrowser/dialogs/createfolder.js
+++ RockWeb/Scripts/ckeditor/plugins/rockfilebrowser/dialogs/createfolder.js
@@ -1,6 +1,10 @@
 ﻿CKEDITOR.dialog.add('createfolderDialog', function (editor) {
     return {
         title: "Create Folder",
+        editorId: editor.id,
+        minWidth: 200,
+        minHeight: 60,
+        resizable: false,
         contents: [
             {
                 id: 'tab0',
@@ -11,7 +15,7 @@
                         type: 'text',
                         id: 'createFolderId',
                         label: 'Folder Name',
-                        validate:function () {
+                        validate: function () {
                             if (!this.getValue()) {
                                 debugger
                                 return false;
@@ -21,10 +25,28 @@
                 ]
             }
         ],
-        onOk: function (sender) {
-            var dialog = this;
-            debugger;
-            //editor
+        onOk: function (eventParam) {
+            var foldersControlId = 'file-browser-folder-tree_' + eventParam.sender.definition.editorId;
+            var foldersRockTree = $('#' + foldersControlId).find('.treeview').data('rockTree');
+            var selectedFolderPath = foldersRockTree.$el.find('.selected').closest('.rocktree-item').attr('data-id');
+            var newFolderName = this.getValueOf('tab0', 'createFolderId');
+            var newFolderPath = selectedFolderPath + "\\" + newFolderName;
+            var restUrl = Rock.settings.get('baseUrl') + 'api/FileBrowser/CreateFolder?relativeFolderPath=' + encodeURIComponent(newFolderPath);
+            return $.ajax({
+                type: 'POST',
+                url: restUrl,
+                context: {
+                    RockTree: foldersRockTree,
+                    FolderPath: newFolderPath
+                }
+            }).done(function (data, textStatus, jqXHR) {
+                
+                // TODO figure out how to best refresh the tree
+                //var rockTree = this.RockTree;
+                //var folderId = this.FolderPath;
+                //var rockTreeOptions = rockTree.settings;
+                //rockTree.rebuild();
+            });
         }
     }
 });
