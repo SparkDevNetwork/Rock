3cfcbf65581387e410ac4a4095d4eb52fca0e489
Mike Peterson <MikePeterson@ccvonline.com>
2014-01-17 11:51:10 -0700

    Update Copyright to Apache on all files

--- Rock/Model/DiscService.cs
+++ Rock/Model/DiscService.cs
@@ -1,21 +1,19 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
 //
-// THIS WORK IS LICENSED UNDER A CREATIVE COMMONS ATTRIBUTION-NONCOMMERCIAL-
-// SHAREALIKE 3.0 UNPORTED LICENSE:
-// http://creativecommons.org/licenses/by-nc-sa/3.0/
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-
-//********************************************************************************************
-// DISC Personality Profile Assessment - This is basically a javascript to C# port 
-//  with a few changes to make it more readable.
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-// Source Material from http://www.gregwiens.com/scid/
-// Used By Permission
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
 //
-// Written By: Derek Mangrum
-//       Date: 2012-04-25
-//
-//********************************************************************************************
-
 using System;
 using System.Linq;
 using System.Collections.Generic;
