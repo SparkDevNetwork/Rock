4b3d0e5644afcbdc1925e898d081ddb8889b09ec
David Turner <david@azturners.com>
2014-03-06 10:14:22 -0700

    - Clean up compiler warnings

--- Rock/Web/RequestValidator.cs
+++ Rock/Web/RequestValidator.cs
@@ -8,8 +8,22 @@ using System.Web.Util;
 
 namespace Rock.Web
 {
+    /// <summary>
+    /// Validates all http request values
+    /// </summary>
     public class RequestValidator : System.Web.Util.RequestValidator
     {
+        /// <summary>
+        /// Validates a string that contains HTTP request data.
+        /// </summary>
+        /// <param name="context">The context of the current request.</param>
+        /// <param name="value">The HTTP request data to validate.</param>
+        /// <param name="requestValidationSource">An enumeration that represents the source of request data that is being validated. The following are possible values for the enumeration:QueryStringForm CookiesFilesRawUrlPathPathInfoHeaders</param>
+        /// <param name="collectionKey">The key in the request collection of the item to validate. This parameter is optional. This parameter is used if the data to validate is obtained from a collection. If the data to validate is not from a collection, <paramref name="collectionKey" /> can be null.</param>
+        /// <param name="validationFailureIndex">When this method returns, indicates the zero-based starting point of the problematic or invalid text in the request collection. This parameter is passed uninitialized.</param>
+        /// <returns>
+        /// true if the string to be validated is valid; otherwise, false.
+        /// </returns>
         protected override bool IsValidRequestString( HttpContext context, string value, RequestValidationSource requestValidationSource, string collectionKey, out int validationFailureIndex )
         {
             if ( requestValidationSource == RequestValidationSource.Form )
