7f4512aac7a67a2be8be5dfaeb897e03fd3c0e00
David Turner <david@azturners.com>
2014-03-07 16:30:50 -0700

    - Update to version 1.0.1.0

--- Rock.Migrations/Properties/AssemblyInfo.cs
+++ Rock.Migrations/Properties/AssemblyInfo.cs
@@ -48,8 +48,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.1.10.0" )]
-[assembly: AssemblyFileVersion( "0.1.10.0" )]
+[assembly: AssemblyVersion( "1.0.1.0" )]
+[assembly: AssemblyFileVersion( "1.0.1.0" )]
 
 // required for EntityFramework RockMigrationGenerator's override of CSharpMigrationCodeGenerator (Specifically in CSharpMigrationCodeGenerator.WriteClassStart)
-[assembly: AssemblyInformationalVersion( "Rock Humphreys 1.10 (alpha)" )]
+[assembly: AssemblyInformationalVersion( "Rock McKinley 0.1 (beta)" )]
