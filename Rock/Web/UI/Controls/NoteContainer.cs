f90ac6e2a1289670189fe3ea43391bcf097befcd
David Turner <david@azturners.com>
2014-03-12 06:39:11 -0700

    + Update the Note control so that when user cancels adding a note, the contents of the note are cleared prior to adding another note (Fixes #367).

--- Rock/Web/UI/Controls/NoteContainer.cs
+++ Rock/Web/UI/Controls/NoteContainer.cs
@@ -345,7 +345,10 @@ namespace Rock.Web.UI.Controls
             base.OnInit( e );
             string script = @"
     $('a.add-note').click(function () {
-        $(this).closest('.panel-note').find('.note-new > .note').children().slideToggle(""slow"");
+        var $newNotePanel = $(this).closest('.panel-note').find('.note-new > .note');
+        $newNotePanel.find('textarea').val('');
+        $newNotePanel.find('input:checkbox').prop('checked', false);
+        $newNotePanel.children().slideToggle(""slow"");
     });
 ";
 
