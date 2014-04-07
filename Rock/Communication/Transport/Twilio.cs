b23e57483f647ce1cfb4c1aaf6d8d51101275191
Jon Edmiston <jon@sparkdevnetwork.org>
2014-04-07 13:57:30 -0700

    - fixed issue with getting the correct fromPhone in Twillio transport

--- Rock/Communication/Transport/Twilio.cs
+++ Rock/Communication/Transport/Twilio.cs
@@ -65,7 +65,7 @@ namespace Rock.Communication.Transport
                 int fromValueId = int.MinValue;
                 if ( int.TryParse( fromValue, out fromValueId ) )
                 {
-                    fromPhone = DefinedValueCache.Read( fromValueId ).Description;
+                    fromPhone = DefinedValueCache.Read( fromValueId ).Name;
                 }
 
                 if ( !string.IsNullOrWhiteSpace( fromPhone ) )
