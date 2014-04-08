e2ff7de9730c5385063612216d6ef9b536f18839
David Turner <david@azturners.com>
2014-04-08 08:45:31 -0700

    - Rock Plugin Project

---
+++ Rock.Plugin/RockVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Plugin
+{
+    [AttributeUsage( AttributeTargets.Method, AllowMultiple=false)]
+    public class RockVersion : System.Attribute
+    {
+        public string Version { get; set; }
+        public RockVersion( string version )
+            : base()
+        {
+            Version = version;
+        }
+    }
+}
