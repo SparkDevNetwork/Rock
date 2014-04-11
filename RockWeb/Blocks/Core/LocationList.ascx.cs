d79827fdee4c97625d7b44b89c7b5d5b88c09edc
Jon Edmiston <jon@sparkdevnetwork.org>
2014-04-10 22:58:32 -0700

    - start of a location list block

---
+++ RockWeb/Blocks/Core/LocationList.ascx.cs
@@ -0,0 +1,192 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Core
+{
+    /// <summary>
+    /// Block for viewing list of tags
+    /// </summary>
+    [DisplayName( "Location List" )]
+    [Category( "Core" )]
+    [Description( "Block for viewing a list of locations." )]
+
+    [LinkedPage( "Detail Page" )]
+    public partial class LocationList : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+
+        #endregion
+
+        #region Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            rFilter.ApplyFilterClick += rFilter_ApplyFilterClick;
+            rFilter.DisplayFilterValue += rFilter_DisplayFilterValue;
+
+            rGrid.DataKeyNames = new string[] { "Id" };
+            rGrid.Actions.ShowAdd = false;
+            rGrid.GridRebind += rGrid_GridRebind;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            if ( !Page.IsPostBack )
+            {
+                BindFilter();
+                BindGrid();
+            }
+
+            base.OnLoad( e );
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the Edit event of the rGrid control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void rGrid_Edit( object sender, RowEventArgs e )
+        {
+            var parms = new Dictionary<string, string>();
+            parms.Add( "LocationId", e.RowKeyValue.ToString() );
+            NavigateToLinkedPage( "DetailPage", parms );
+        }
+
+
+        /// <summary>
+        /// Handles the GridRebind event of the rGrid control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void rGrid_GridRebind( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+
+        /// <summary>
+        /// Rs the filter_ display filter value.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        protected void rFilter_DisplayFilterValue( object sender, GridFilter.DisplayFilterValueArgs e )
+        {
+            
+        }
+
+        /// <summary>
+        /// Handles the ApplyFilterClick event of the rFilter control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void rFilter_ApplyFilterClick( object sender, EventArgs e )
+        {
+            rFilter.SaveUserPreference( "Street Address", txtStreetAddress1.Text );
+
+            BindGrid();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Binds the filter.
+        /// </summary>
+        private void BindFilter()
+        {
+            if ( string.IsNullOrWhiteSpace( rFilter.GetUserPreference( "Street Address" ) ) )
+            {
+                txtStreetAddress1.Text = rFilter.GetUserPreference( "Street Address" );
+            }
+        }
+
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            //rGrid.Columns[0].Visible = true;
+
+            var locations = GetLocations();
+            if (locations != null)
+            {
+                rGrid.DataSource = locations.Select( t => new
+                {
+                    Id = t.Id,
+                    t.Street1,
+                    t.City,
+                    t.State,
+                    t.Zip,
+                    t.Country
+                } ).ToList();
+
+                rGrid.DataBind();
+            }
+        }
+
+        /// <summary>
+        /// Gets the tags.
+        /// </summary>
+        /// <returns></returns>
+        private IQueryable<Location> GetLocations()
+        {
+
+
+            if ( !string.IsNullOrWhiteSpace(rFilter.GetUserPreference( "Street Address" )) )
+            {
+                string streetAddress1 = rFilter.GetUserPreference( "Street Address" );
+                var queryable = new Rock.Model.LocationService( new RockContext() ).Queryable().
+                    Where( l => l.Street1.StartsWith( streetAddress1 ) );
+                return queryable;
+
+            }
+
+            return null;
+        }
+
+        #endregion
+
+}
+}
