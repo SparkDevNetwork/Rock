be5bb7b3b8c443da747976a1c3620764f8f5f1f3
Nick Airdo <nairdo@gmail.com>
2014-04-15 16:11:53 -0700

    - Adding doc version number to copyright footer of SHFB

--- Build/buildrockdocs.bat
+++ Build/buildrockdocs.bat
@@ -6,4 +6,4 @@ set DXROOT=c:\Program Files (x86)\EWSoftware\Sandcastle Help File Builder
 set SHFBROOT=%DXROOT%
 set LANGUAGE=%SHFBROOT%\SandcastleBuilderGUI.exe
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Build\Rock.shfbproj
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /p:CopyrightText="Copyright \xa9 2012-2014 - ver. %BUILD_ID%" Build\Rock.shfbproj
