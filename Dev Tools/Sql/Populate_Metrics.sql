c09a29c817a4b3f8c3a2c05089cf8085dabf5dd0
Mike Peterson <MikePeterson@ccvonline.com>
2014-04-18 15:49:38 -0700

    - added MetricValueList block

--- Dev Tools/Sql/Populate_Metrics.sql
+++ Dev Tools/Sql/Populate_Metrics.sql
@@ -5,7 +5,8 @@ declare
    @iconCssClass nvarchar(max) = null,
    @categoryId int = null,
    @order int = 0,
-   @metricGuid uniqueidentifier = null; 
+   @metricGuid uniqueidentifier = null,
+   @metricId int = null;
 begin
 
     delete from MetricValue   
@@ -37,13 +38,15 @@ begin
      VALUES 
         (0,'First Time Visitors Per Week','First Time Visitors counts broken down into weeks', 'fa fa-gift', 0, @metricGuid)
 
+    set @metricId = (select Id from Metric where Guid = @metricGuid);
+
     set @categoryId = (select id from Category where Name = 'Person Metrics' and EntityTypeId = @entityTypeIdMetricCategoryType )    
     insert into [MetricCategory] ([MetricId], [CategoryId], [Order], [Guid])
-      values ( (select Id from Metric where Guid = @metricGuid), @categoryId, 0, NEWID())
+      values ( @metricId, @categoryId, 0, NEWID())
 
     set @categoryId = (select id from Category where Name = 'Group Metrics' and EntityTypeId = @entityTypeIdMetricCategoryType )    
     insert into [MetricCategory] ([MetricId], [CategoryId], [Order], [Guid])
-      values ( (select Id from Metric where Guid = @metricGuid), @categoryId, 0, NEWID())
+      values ( @metricId, @categoryId, 0, NEWID())
 
     -- example metric    
     set @categoryId = (select id from Category where Name = 'Person Metrics' and EntityTypeId = @entityTypeIdMetricCategoryType )    
@@ -51,9 +54,36 @@ begin
     INSERT INTO [Metric]([IsSystem],[Title],[Description],[IconCssClass], [IsCumulative],[Guid])
      VALUES 
         (0,'Free Lunches','Number of Free Lunches per day', 'fa fa-thumbs-up', 1, @metricGuid)
+
+    set @metricId = (select Id from Metric where Guid = @metricGuid);
     
     insert into [MetricCategory] ([MetricId], [CategoryId], [Order], [Guid])
-      values ( (select Id from Metric where Guid = @metricGuid), @categoryId, 0, NEWID())
+        values ( @metricId, @categoryId, 0, NEWID())
+
+    INSERT INTO [MetricValue] ([MetricId], [MetricValueType], [XValue], [YValue], [Order], [Guid])
+        values 
+( @metricId, 1, '12/11/2013', 500, 0, NEWID()),
+( @metricId, 1, '12/18/2013', 200, 0, NEWID()),
+( @metricId, 1, '12/25/2013', 0, 0, NEWID()),
+( @metricId, 1, '01/03/2013', 0, 0, NEWID()),
+( @metricId, 1, '01/10/2013', 999, 0, NEWID()),
+( @metricId, 1, '01/17/2013', 5000, 0, NEWID()),
+( @metricId, 1, '01/24/2013', 12, 0, NEWID()),
+( @metricId, 1, '02/01/2013', 45, 0, NEWID()),
+( @metricId, 1, '02/05/2013', 10, 0, NEWID()),
+( @metricId, 1, '02/10/2013', 200, 0, NEWID()),
+( @metricId, 1, '02/18/2013', 300, 0, NEWID()),
+( @metricId, 1, '03/01/2013', 400, 0, NEWID()),
+( @metricId, 1, '04/01/2013', 500, 0, NEWID()),
+( @metricId, 1, '05/01/2013', 600, 0, NEWID()),
+( @metricId, 1, '06/01/2013', 700, 0, NEWID()),
+( @metricId, 1, '07/01/2013', 300, 0, NEWID()),
+( @metricId, 1, '08/01/2013', 200, 0, NEWID()),
+( @metricId, 1, '09/01/2013', 100, 0, NEWID()),
+( @metricId, 1, '10/01/2013', 50, 0, NEWID()),
+( @metricId, 1, '10/02/2013', 1.9199065065406540, 0, NEWID()),
+( @metricId, 1, '11/01/2013', 9, 0, NEWID())
+
    
 
 end
