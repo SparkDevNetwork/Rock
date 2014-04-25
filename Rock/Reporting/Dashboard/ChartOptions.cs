83ef29605f90f194fc113097f84acc9acb768d96
Mike Peterson <MikePeterson@ccvonline.com>
2014-04-24 17:20:35 -0700

    - added additional settings to ChartOptions - updated LineChart example use same colors and options as ExceptionList

--- Rock/Reporting/Dashboard/ChartOptions.cs
+++ Rock/Reporting/Dashboard/ChartOptions.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +28,71 @@ namespace Rock.Reporting.Dashboard
     public class ChartOptions
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="ChartOptions"/> class.
+        /// </summary>
+        public ChartOptions()
+        {
+            titleTextStyle = new System.Dynamic.ExpandoObject();
+            vAxis = new System.Dynamic.ExpandoObject();
+            hAxis = new System.Dynamic.ExpandoObject();
+            legend = new System.Dynamic.ExpandoObject();
+        }
+
+        /// <summary>
         /// Gets or sets the title.
         /// </summary>
         /// <value>
         /// The title.
         /// </value>
         public string title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title text style.
+        /// </summary>
+        /// <value>
+        /// The title text style.
+        /// </value>
+        public dynamic titleTextStyle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color of the background.
+        /// </summary>
+        /// <value>
+        /// The color of the background.
+        /// </value>
+        public string backgroundColor { get; set; }
+
+        /// <summary>
+        /// Gets or sets the colors.
+        /// </summary>
+        /// <value>
+        /// The colors.
+        /// </value>
+        public string[] colors { get; set; }
+
+        /// <summary>
+        /// Gets or sets the v axis.
+        /// </summary>
+        /// <value>
+        /// The v axis.
+        /// </value>
+        public dynamic vAxis { get; set; }
+
+        /// <summary>
+        /// Gets or sets the h axis.
+        /// </summary>
+        /// <value>
+        /// The h axis.
+        /// </value>
+        public dynamic hAxis { get; set; }
+
+        /// <summary>
+        /// Gets or sets the legend.
+        /// </summary>
+        /// <value>
+        /// The legend.
+        /// </value>
+        public dynamic legend { get; set; }
 
         /// <summary>
         /// Gets or sets the width.
@@ -109,12 +184,12 @@ namespace Rock.Reporting.Dashboard
         /// The date
         /// </summary>
         date,
-        
+
         /// <summary>
         /// The number
         /// </summary>
         number,
-        
+
         /// <summary>
         /// The string
         /// </summary>
