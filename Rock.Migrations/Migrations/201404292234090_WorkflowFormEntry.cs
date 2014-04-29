8b92eb5ab706670bff0f8ba561abf0ad7ee848d1
David Turner <david@azturners.com>
2014-04-29 16:07:49 -0700

    - Update migration for workflow form entry

--- Rock.Migrations/Migrations/201404292234090_WorkflowFormEntry.cs
+++ Rock.Migrations/Migrations/201404292234090_WorkflowFormEntry.cs
@@ -22,13 +22,18 @@ namespace Rock.Migrations
     /// <summary>
     ///
     /// </summary>
-    public partial class WorkflowForm : Rock.Migrations.RockMigration
+    public partial class WorkflowFormEntry : Rock.Migrations.RockMigration
     {
         /// <summary>
         /// Operations to be performed during the upgrade process.
         /// </summary>
         public override void Up()
         {
+            // Update checklist block to only be visible by Rock Administrators
+            DeleteSecurityAuthForBlock( "62B1DBE6-B3D9-4C0B-BD12-1DD8C4F2C6EB" );
+            AddSecurityAuthForBlock( "62B1DBE6-B3D9-4C0B-BD12-1DD8C4F2C6EB", 0, "View", true, "628C51A8-4613-43ED-A18D-4A6FB999273E", Model.SpecialRole.None, "B26794E2-7AB6-4F14-9E18-FEBA8A037C5F" );
+            AddSecurityAuthForBlock( "62B1DBE6-B3D9-4C0B-BD12-1DD8C4F2C6EB", 1, "View", false, null, Model.SpecialRole.AllUsers, "DF120B3D-7AD2-4B16-A79A-D2156CC1DD9F" );
+
             CreateTable(
                 "dbo.WorkflowActionFormAttribute",
                 c => new
@@ -85,6 +90,93 @@ namespace Rock.Migrations
             AddColumn("dbo.WorkflowActionType", "WorkflowFormId", c => c.Int());
             CreateIndex("dbo.WorkflowActionType", "WorkflowFormId");
             AddForeignKey("dbo.WorkflowActionType", "WorkflowFormId", "dbo.WorkflowActionForm", "Id");
+
+            // Remove workflow list block from workflow type detail page
+            DeleteBlock( "B8DE3F9A-5C84-4D13-8B90-F1E768740269" );
+
+            Sql( @"
+
+    -- remove the 'IsSystem' from sample groups in install
+    UPDATE [Group] SET
+    [IsSystem] = 0
+    WHERE [Guid] IN ('C05E60C4-6DFC-420D-8DBA-28DBB5F0E3F9', '3CBC1A3F-DD26-430A-9B53-B476CB385ABC')
+
+    -----------------------------------------------------------------------------------------------
+    -- START script for DataView: Pending People
+    -----------------------------------------------------------------------------------------------
+    DECLARE @PropertyFilterEntityTypeId INT
+    SET @PropertyFilterEntityTypeId = (SELECT [Id] FROM [EntityType] WHERE [Guid] = '03F0D6AC-D181-48B6-B4BC-1F2652B55323')
+
+    -- parent DataViewFilter for DataView: Pending People
+    DECLARE @ParentDataViewFilterId INT
+    INSERT INTO [DataViewFilter]
+        ([ExpressionType]
+        ,[Guid]
+        ,[CreatedDateTime]
+        ,[ModifiedDateTime])
+    VALUES
+        (1
+        ,'9eb9455c-adba-435b-869b-264178484944'
+        ,'4/29/2014 12:26:07 PM'
+        ,'4/29/2014 12:26:07 PM')
+    SET @ParentDataViewFilterId = SCOPE_IDENTITY()
+
+    
+        -- child DataViewFilters for DataView: Pending People
+        INSERT INTO [DataViewFilter]
+            ([ExpressionType]
+            ,[ParentId]
+            ,[EntityTypeId]
+            ,[Selection]
+            ,[Guid]
+            ,[CreatedDateTime]
+            ,[ModifiedDateTime])
+        VALUES
+            (0
+            ,@ParentDataViewFilterId
+            ,@PropertyFilterEntityTypeId
+            ,N'[
+  ""RecordStatusValueId"",
+  ""[\r\n  \""5\""\r\n]""
+]'
+            ,'a3e1eb8e-cb2a-4ee0-9a27-3499392d5a1d'
+            ,'4/29/2014 12:26:07 PM'
+            ,'4/29/2014 12:26:07 PM')
+            
+    -- add DataView: Pending People
+    DECLARE @CategoryId INT
+    SET @CategoryId = (SELECT [Id] FROM [Category] WHERE [Guid] = 'bdd2c36f-7575-48a8-8b70-3a566e3811ed')
+
+    DECLARE @DataViewId INT
+    INSERT INTO [DataView]
+        ([IsSystem]
+        ,[Name]
+        ,[Description]
+        ,[CategoryId]
+        ,[EntityTypeId]
+        ,[DataViewFilterId]
+        ,[TransformEntityTypeId]
+        ,[Guid]
+        ,[CreatedDateTime]
+        ,[ModifiedDateTime])
+    VALUES
+        (0
+        ,N'Pending People'
+        ,N'Includes all people who have a status of Pending.'
+        ,@CategoryId
+        ,15
+        ,@ParentDataViewFilterId
+        ,NULL
+        ,'6374c50d-4bbf-4b12-aa2c-9c2c92e5f32e'
+        ,'4/29/2014 12:26:07 PM'
+        ,'4/29/2014 12:26:07 PM')
+    SET @DataViewId = SCOPE_IDENTITY()
+    
+    -----------------------------------------------------------------------------------------------
+    -- END script for DataView: Pending People
+    -----------------------------------------------------------------------------------------------
+" );
+
         }
         
         /// <summary>
@@ -92,6 +184,14 @@ namespace Rock.Migrations
         /// </summary>
         public override void Down()
         {
+            Sql( @"
+-----------------------------------------------------------------------------------------------
+    -- Uninstall DataView: Pending People
+    DELETE [DataView] WHERE [Guid] = '6374c50d-4bbf-4b12-aa2c-9c2c92e5f32e'
+    DELETE [DataViewFilter] WHERE [Guid] = 'a3e1eb8e-cb2a-4ee0-9a27-3499392d5a1d'
+    DELETE [DataViewFilter] WHERE [Guid] = '9eb9455c-adba-435b-869b-264178484944'
+-----------------------------------------------------------------------------------------------
+" );
             DropForeignKey("dbo.WorkflowActionType", "WorkflowFormId", "dbo.WorkflowActionForm");
             DropForeignKey("dbo.WorkflowActionFormAttribute", "WorkflowActionFormId", "dbo.WorkflowActionForm");
             DropForeignKey("dbo.WorkflowActionForm", "ModifiedByPersonAliasId", "dbo.PersonAlias");
