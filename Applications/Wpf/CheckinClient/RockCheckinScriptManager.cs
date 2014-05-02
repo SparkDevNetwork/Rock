15ecc1476cc7a08592158edc31e264b396892a03
Jon Edmiston <jon@sparkdevnetwork.org>
2014-05-02 16:55:52 -0700

    - initial checkin of the windows check-in client

---
+++ Applications/Wpf/CheckinClient/RockCheckinScriptManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckinClient
+{
+    [PermissionSet( SecurityAction.Demand, Name = "FullTrust" )]
+    [System.Runtime.InteropServices.ComVisibleAttribute( true )]
+    public class RockCheckinScriptManager
+    {
+        MainWindow mainWindow;
+
+        public RockCheckinScriptManager( Window w )
+        {
+            this.mainWindow = (MainWindow)w;
+        }
+
+        public void PrintLabels( string labelData )
+        {
+            string something = "";
+        }
+    }
+}
