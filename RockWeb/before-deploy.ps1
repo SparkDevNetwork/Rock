1527d3b16a869a08fcfad445bd716f52bbd6f0b2
Jon Edmiston <jonedmiston@ccvonline.com>
2014-05-28 11:12:11 -0700

    + updated the deploy scripts to stop/start IIS to release the SqlServerSpatial assemblies

--- RockWeb/before-deploy.ps1
+++ RockWeb/before-deploy.ps1
@@ -1,4 +1,8 @@
 # This script is run by AppVeyor's deploy agent before the deploy
+Import-Module WebAdministration
+
+# stop website - needed to allow the deploy to overwrite the sql server spatial types
+Stop-WebSite 'RockRMS'
  
- # move content folder to temp
- Move-Item c:\webdata\rock.ccvonline.com\docs\Content c:\webdata\rock.ccvonline.com\temp
+# move content folder to temp
+Move-Item c:\webdata\rock.ccvonline.com\docs\Content c:\webdata\rock.ccvonline.com\temp
--- RockWeb/deploy.ps1
+++ RockWeb/deploy.ps1
@@ -1,7 +1,11 @@
 # This script is run by AppVeyor's deploy agent after the deploy
+Import-Module WebAdministration
  
- # move content directory back from temp
- Move-Item c:\webdata\rock.ccvonline.com\temp\Content c:\webdata\rock.ccvonline.com\docs 
- 
- # copy new connection string file
- Copy-Item c:\webdata\rock.ccvonline.com\config\web.ConnectionStrings.config c:\webdata\rock.ccvonline.com\docs
+# start website
+Start-WebSite 'RockRMS'
+
+# move content directory back from temp
+Move-Item c:\webdata\rock.ccvonline.com\temp\Content c:\webdata\rock.ccvonline.com\docs 
+
+# copy new connection string file
+Copy-Item c:\webdata\rock.ccvonline.com\config\web.ConnectionStrings.config c:\webdata\rock.ccvonline.com\docs
