f797ca102e321b6c156726d9045b33ad1cdbe2a2
Jon Edmiston <jonedmiston@ccvonline.com>
2014-05-28 11:24:09 -0700

    - added messages to deploy steps

--- RockWeb/before-deploy.ps1
+++ RockWeb/before-deploy.ps1
@@ -2,7 +2,9 @@
 Import-Module WebAdministration
 
 # stop website - needed to allow the deploy to overwrite the sql server spatial types
+Write-Host "Stopping IIS Website"
 Stop-WebSite 'RockRMS'
  
 # move content folder to temp
+Write-Host "Moving content folder to temp directory"
 Move-Item c:\webdata\rock.ccvonline.com\docs\Content c:\webdata\rock.ccvonline.com\temp
--- RockWeb/deploy.ps1
+++ RockWeb/deploy.ps1
@@ -2,10 +2,13 @@
 Import-Module WebAdministration
  
 # start website
+Write-Host "Starting IIS website"
 Start-WebSite 'RockRMS'
 
 # move content directory back from temp
+Write-Host "Moving Contents folder back from temp directory"
 Move-Item c:\webdata\rock.ccvonline.com\temp\Content c:\webdata\rock.ccvonline.com\docs 
 
 # copy new connection string file
+Write-Host "Copying new web.ConnectionStrings.config to web dir"
 Copy-Item c:\webdata\rock.ccvonline.com\config\web.ConnectionStrings.config c:\webdata\rock.ccvonline.com\docs
