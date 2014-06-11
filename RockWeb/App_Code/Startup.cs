32ef08c059241595371089eb1f25367fee3140a0
Jon Edmiston <jon@sparkdevnetwork.org>
2014-06-11 15:05:15 -0700

    - added missing OWIN startup file for SignalR

---
+++ RockWeb/App_Code/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using System.Web;
+
+
+public class Startup
+{
+    public void Configuration( IAppBuilder app )
+    {
+        app.MapSignalR();
+    }
+}
