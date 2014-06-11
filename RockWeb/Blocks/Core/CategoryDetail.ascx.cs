45dbc4c374b820921923106e6b6078ebb9f04e20
David Turner <david@azturners.com>
2014-06-11 10:30:35 -0700

    - Add UI for editing the category Highlight Color

--- RockWeb/Blocks/Core/CategoryDetail.ascx.cs
+++ RockWeb/Blocks/Core/CategoryDetail.ascx.cs
@@ -230,6 +230,7 @@ namespace RockWeb.Blocks.Core
             category.Name = tbName.Text;
             category.ParentCategoryId = cpParentCategory.SelectedValueAsInt();
             category.IconCssClass = tbIconCssClass.Text;
+            category.HilightColor = tbHighlightColor.Text;
 
             List<int> orphanedBinaryFileIdList = new List<int>();
 
@@ -395,6 +396,7 @@ namespace RockWeb.Blocks.Core
             lblEntityTypeQualifierValue.Visible = !string.IsNullOrWhiteSpace( category.EntityTypeQualifierValue );
             lblEntityTypeQualifierValue.Text = category.EntityTypeQualifierValue;
             tbIconCssClass.Text = category.IconCssClass;
+            tbHighlightColor.Text = category.HilightColor;
         }
 
         /// <summary>
