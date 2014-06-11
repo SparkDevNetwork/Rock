05e461b7f5f0f1fd293ff9350114bfe3f113d5c3
Mike Peterson <MikePeterson@ccvonline.com>
2014-06-10 17:15:33 -0700

    - delete unneeded CodeGen sql

--- Dev Tools/Sql/CodeGen_DeleteDataFromAllTables.sql
+++
@@ -1,8 +0,0 @@
-select 'alter table [' + TABLE_NAME + '] nocheck constraint all' [Code], 0 [ScriptOrder], [TABLE_NAME]  from INFORMATION_SCHEMA.TABLES where TABLE_NAME not in ( 'sysdiagrams')
-union
-select 'delete from [' + TABLE_NAME + ']'  [Code], 1 [ScriptOrder], [TABLE_NAME] from INFORMATION_SCHEMA.TABLES where TABLE_NAME not in ( 'sysdiagrams')
-union
-select 'alter table [' + TABLE_NAME + '] check constraint all' [Code], 2 [ScriptOrder], [TABLE_NAME] from INFORMATION_SCHEMA.TABLES where TABLE_NAME not in ( 'sysdiagrams')
-order by ScriptOrder, TABLE_NAME
-
-
