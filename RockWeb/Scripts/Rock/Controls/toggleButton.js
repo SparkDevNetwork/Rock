3600c895e6c0dd3cb95525f37fbb4a5406673d69
Mike Peterson <MikePeterson@ccvonline.com>
2014-06-16 13:28:09 -0700

    - convert Toggle to use bootply pattern

---
+++ RockWeb/Scripts/Rock/Controls/toggleButton.js
@@ -0,0 +1,35 @@
+﻿(function ($) {
+    'use strict';
+    window.Rock = window.Rock || {};
+    Rock.controls = Rock.controls || {};
+
+    Rock.controls.toggleButton = (function () {
+        var exports = {
+            initialize: function (options) {
+                if (!options.id) {
+                    throw 'id is required';
+                }
+
+                // uses pattern from http://www.bootply.com/92189
+
+                $('#' + options.id).click(function () {
+                    $(this).find('.btn').toggleClass('active');
+
+                    if ($(this).find('.' + options.activeButtonCssClass).size() > 0) {
+                        $(this).find('.btn').toggleClass(options.activeButtonCssClass);
+                    }
+
+                    $(this).find('.btn').toggleClass('btn-default');
+                    
+                    $(this).find('.js-toggle-checked').val($(this).find('.js-toggle-on').hasClass('active'));
+                });
+
+            }
+        };
+
+        return exports;
+    }());
+}(jQuery));
+
+
+
