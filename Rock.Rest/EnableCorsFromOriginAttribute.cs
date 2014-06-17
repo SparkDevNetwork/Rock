665c2c74bf8713bf3cb446790a770906305f7ea5
Mike Peterson <MikePeterson@ccvonline.com>
2014-06-16 18:13:58 -0700

    - updated missing/incorrect cs copyright headers

--- Rock.Rest/EnableCorsFromOriginAttribute.cs
+++ Rock.Rest/EnableCorsFromOriginAttribute.cs
@@ -1,4 +1,6 @@
-﻿//
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
