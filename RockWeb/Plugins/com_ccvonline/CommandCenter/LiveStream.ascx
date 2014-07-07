e28c51bb034ba7cf91d9dca78b75e2489361efa0
mkinyon <mavhuntrx@gmail.com>
2014-07-07 15:57:08 -0700

    Block for playing live streams.

---
+++ RockWeb/Plugins/com_ccvonline/CommandCenter/LiveStream.ascx
@@ -0,0 +1,28 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="LiveStream.ascx.cs" Inherits="RockWeb.Plugins.com_ccvonline.CommandCenter.LiveStream" %>
+
+<br />
+
+<asp:Repeater ID="rptvideostreams" runat="server" >
+    <ItemTemplate>
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <h3 class="panel-heading"><%# Eval("[0]") %></h3>
+                <div class="panel-body">
+                    <div class="videocontent">
+                        <video id='<%# Eval("[0]") %>' class="video-js vjs-default-skin vjs-live vjs-big-play-centered" controls
+                                preload="none" width="auto" height="330" poster="<%# ResolveRockUrl("~~/Assets/images/" + Eval("[0]") + "poster.jpg") %>" data-setup='{ "techOrder": ["flash"] }'>
+                            <source src='<%# Eval("[1]") %>' type='rtmp/mp4'>
+                            <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+                        </video>
+                        <br />
+                        <div style="text-align:center;">
+                            <button type="button" class="btn btn-lg btn-primary"><i class="fa fa-microphone"></i> Mute</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </ItemTemplate>
+</asp:Repeater>
+
+
---
+++ RockWeb/Plugins/com_ccvonline/CommandCenter/LiveStream.ascx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.com_ccvonline.CommandCenter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    
+    [DisplayName("Live Stream")]
+    [Category( "CCV" )]
+    [CategoryField("Command Center > Live Stream")]
+    [Description("Used for viewing live streams.")]
+
+    //todo: choose stream type block setting
+
+    public partial class LiveStream : RockBlock
+    {
+        #region Base Control Methods
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CampusService campusService = new CampusService( new RockContext() );
+            List<Campus> campuses = new List<Campus>();
+
+            List<string[]> dataSource = new List<string[]>();
+
+            foreach ( var campus in campusService.Queryable().OrderBy( c => c.Name ) )
+            {
+                 campus.LoadAttributes();
+                string attributeValue = campus.GetAttributeValue( "VenueStreams" );
+                if ( !string.IsNullOrWhiteSpace( attributeValue ) )
+                {
+                    string[] nameValues = attributeValue.Split( new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries );
+                    foreach ( string nameValue in nameValues )
+                    {
+                        string[] nameAndValue = nameValue.Split( new char[] { '^' }, StringSplitOptions.RemoveEmptyEntries );
+
+                        dataSource.Add( new string[] { campus.Name.ToString(), nameAndValue[1].ToString() } );                        
+                    }
+                }
+            }
+
+            rptvideostreams.DataSource = dataSource.ToList();
+            rptvideostreams.DataBind();
+        }
+
+        #endregion
+        
+    }
+}
