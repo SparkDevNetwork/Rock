3eaff736dca7b4a8cf487ae10253abf61abcabd6
mkinyon <mavhuntrx@gmail.com>
2014-07-15 17:00:44 -0700

    Added DVR recording blocks.

---
+++ RockWeb/Plugins/com_ccvonline/CommandCenter/DVRRecordingDetail.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="DVRRecordingDetail.ascx.cs" Inherits="RockWeb.Plugins.com_ccvonline.CommandCenter.DVRRecordingDetail" %>
+
+<asp:UpdatePanel ID="upRecordings" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlDetails" runat="server">
+
+
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ com.ccvonline.CommandCenter/Migrations/003_AddVenueType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rock.Plugin;
+
+namespace com.ccvonline.CommandCenter.Migrations
+{
+    [MigrationNumber( 3, "1.0.9" )]
+    class AddVenueType : Migration
+    {
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+            Sql( @"
+                    ALTER TABLE dbo._com_ccvonline_CommandCenter_Recording
+                    ADD VenueType VARCHAR(50) NULL
+                " );
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+            Sql( @"
+                    ALTER TABLE dbo._com_ccvonline_CommandCenter_Recording
+                    DROP COLUMN VenueType 
+                " );
+        }
+    }
+}
