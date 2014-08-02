8ad83e57797a10dbb40819fb3598e542f2dfe9ba
nairdo <nairdo@gmail.com>
2014-08-01 18:55:14 -0700

    - [DevDoc] Added new migration helper methods for update-add new grouptype, grouptyperole, and group.

--- Rock/SystemGuid/GroupType.cs
+++ Rock/SystemGuid/GroupType.cs
@@ -67,8 +67,8 @@ namespace Rock.SystemGuid
         public const string GROUPTYPE_SMALL_GROUP = "50FCFB30-F51A-49DF-86F4-2B176EA1820B";
 
         /// <summary>
-        /// System Groups group type guid
+        /// Application Group group type guid
         /// </summary>
-        public const string GROUPTYPE_SYSTEM_GROUPS = "3981CF6D-7D15-4B57-AACE-C0E25D28BD49";
+        public const string GROUPTYPE_APPLICATION_GROUP = "3981CF6D-7D15-4B57-AACE-C0E25D28BD49";
     }
 }
