2f73eac9957cf0e2bc9565605a9acf11e6e394c5
Mike Peterson <MikePeterson@ccvonline.com>
2014-08-04 11:08:59 -0700

    - added Block Properties "Show Legend" and "Legend Position" to LineChart and BarChart

--- Rock/Reporting/Dashboard/LineBarPointsChartDashboardWidget.cs
+++ Rock/Reporting/Dashboard/LineBarPointsChartDashboardWidget.cs
@@ -35,7 +35,9 @@ namespace Rock.Reporting.Dashboard
     [CustomCheckboxListField( "Metric Value Types", "Select which metric value types to display in the chart", "Goal,Measure", false, "Measure", Order = 4 )]
     [MetricEntityField( "Metric", "Select the metric and the filter", Order = 5 )]
     [SlidingDateRangeField( "Date Range", Key = "SlidingDateRange", DefaultValue = "1||4||", Order = 6 )]
-    [LinkedPage( "Detail Page", "Select the page to navigate to when the chart is clicked", Order = 7 )]
+    [BooleanField( "Show Legend", "", true, Order = 7 )]
+    [CustomDropdownListField( "Legend Position", "Select the position of the Legend (corner)", "ne,nw,se,sw", false, "ne", Order = 8)]
+    [LinkedPage( "Detail Page", "Select the page to navigate to when the chart is clicked", Order = 9 )]
     public abstract class LineBarPointsChartDashboardWidget : DashboardWidget
     {
         /// <summary>
@@ -53,7 +55,7 @@ namespace Rock.Reporting.Dashboard
         /// The metric warning control.
         /// </value>
         public abstract NotificationBox MetricWarningControl { get; }
-        
+
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
         /// </summary>
@@ -295,6 +297,11 @@ namespace Rock.Reporting.Dashboard
             }
 
             FlotChartControl.Options.SetChartStyle( this.ChartStyleDefinedValueGuid );
+
+            FlotChartControl.Options.legend = FlotChartControl.Options.legend ?? new Legend();
+            FlotChartControl.Options.legend.show = this.GetAttributeValue( "ShowLegend" ).AsBooleanOrNull();
+            FlotChartControl.Options.legend.position = this.GetAttributeValue( "LegendPosition" );
+            FlotChartControl.ShowDebug = true;
 
             MetricWarningControl.Visible = !this.MetricId.HasValue;
         }
