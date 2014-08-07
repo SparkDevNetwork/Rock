d699ddd1af323769a6408859e657c43c84b7ba37
Mike Peterson <MikePeterson@ccvonline.com>
2014-08-07 16:01:59 -0700

    - updated person duplicate finder proc to do Email and Name

--- database/Procedures/spCrm_PersonDuplicateFinder.sql
+++ database/Procedures/spCrm_PersonDuplicateFinder.sql
@@ -13,74 +13,196 @@
 	</code>
 </doc>
 */
-
 SET STATISTICS TIME ON
+GO
 
-/*
 ALTER PROCEDURE [dbo].[spCrm_PersonDuplicateFinder]
 AS
-*/
 BEGIN
- 
-/* Find Duplicates by looking at people with the exact same email address  */   
-    DECLARE @PersonDuplicateByEmailTable TABLE  (
-      Id INT NOT NULL IDENTITY(1,1),
-      Email NVARCHAR(MAX) NOT NULL,
-      PersonAliasId INT NOT NULL
-      );   
+    BEGIN TRANSACTION
 
-    INSERT INTO @PersonDuplicateByEmailTable
-    SELECT 
-        e.Email [Email] 
-        ,pa.Id [PersonAliasId]
-    FROM
-    (
-        SELECT 
-            [a].[Email]
-        FROM
-        (
-            SELECT 
-                [Email]
+    -- prevent stored proc from running simultaneously since we are using #temp tables
+    EXEC sp_getapplock 'spCrm_PersonDuplicateFinder'
+        ,'Exclusive'
+        ,'Transaction'
+        ,0
+
+    DECLARE @cScoreWeightEmail INT = 10
+        ,@cScoreWeightNameMatch INT = 3
+        ,@processDateTime DATETIME = sysdatetime()
+
+    /* Find Duplicates by looking at people with the exact same email address  */
+    CREATE TABLE #PersonDuplicateByEmailTable (
+        Id INT NOT NULL IDENTITY(1, 1)
+        ,Email NVARCHAR(75) NOT NULL
+        ,PersonAliasId INT NOT NULL
+        ,CONSTRAINT [pk_PersonDuplicateByEmailTable] PRIMARY KEY CLUSTERED (Id)
+        );
+
+    --CREATE INDEX IX_EMAIL on #PersonDuplicateByEmailTable (Email);
+    INSERT INTO #PersonDuplicateByEmailTable
+    SELECT [e].[Email] [Email]
+        ,[pa].[Id] [PersonAliasId]
+    FROM (
+        SELECT [a].[Email]
+        FROM (
+            SELECT [Email]
                 ,COUNT(*) [EmailCount]
-            FROM 
-                [Person] [p]
-            WHERE 
-                [Email] IS NOT NULL 
-            AND
-                [Email] != ''
-            GROUP BY 
-                [Email]
-        ) [a]
+            FROM [Person] [p]
+            WHERE [Email] IS NOT NULL
+                AND [Email] != ''
+            GROUP BY [Email]
+            ) [a]
         WHERE [a].[EmailCount] > 1
-    ) [e]
-    JOIN Person p on p.Email = e.Email
-    JOIN PersonAlias pa on pa.PersonId = p.Id
-    order by Email
+        ) [e]
+    JOIN [Person] [p] ON [p].[Email] = [e].[Email]
+    JOIN [PersonAlias] [pa] ON [pa].[PersonId] = [p].[Id]
+    ORDER BY [Email]
 
-    MERGE [PersonDuplicate] as target
+    /* Find Duplicates by looking at people with the exact same lastname and same first 2 chars of First/Nick name */
+    CREATE TABLE #PersonDuplicateByNameTable (
+        Id INT NOT NULL IDENTITY(1, 1)
+        ,NameMatch NVARCHAR(152) NOT NULL
+        ,PersonAliasId INT NOT NULL
+        ,CONSTRAINT [pk_PersonDuplicateByNameTable] PRIMARY KEY CLUSTERED (Id)
+        );
+
+    --CREATE INDEX IX_NameMatch on #PersonDuplicateByNameTable (NameMatch);
+    --CREATE INDEX IX_PersonAliasId on #PersonDuplicateByNameTable (PersonAliasId);
+    INSERT INTO #PersonDuplicateByNameTable
+    SELECT [e].[LastName] + ' ' + [e].[First2] [NameMatch]
+        ,[pa].[Id] [PersonAliasId]
+    FROM (
+        SELECT [a].[First2]
+            ,[a].[LastName]
+        FROM (
+            SELECT SUBSTRING([FirstName], 1, 2) [First2]
+                ,[LastName]
+                ,COUNT(*) [MatchCount]
+            FROM [Person] [p]
+            WHERE isnull([LastName], '') != ''
+                AND [FirstName] IS NOT NULL
+                AND LEN([FirstName]) >= 2
+            GROUP BY [LastName]
+                ,SUBSTRING([FirstName], 1, 2)
+            ) [a]
+        WHERE [a].[MatchCount] > 1
+        ) [e]
+    JOIN [Person] [p] ON [p].[LastName] = [e].[LastName]
+        AND [p].[FirstName] LIKE (e.First2 + '%')
+    JOIN [PersonAlias] [pa] ON [pa].[PersonId] = [p].[Id]
+    ORDER BY [NameMatch]
+
+    /* Reset Scores for everybody. Preserve records where [IsConfirmedAsNotDuplicate] */
+    UPDATE [PersonDuplicate]
+    SET [Score] = 0
+        ,[ScoreDetail] = '';
+
+    /* Update PersonDuplicate table with results of email match */
+    MERGE [PersonDuplicate] AS TARGET
     USING (
-        SELECT 
-            a.PersonAliasId,
-            b.PersonAliasId [DuplicatePersonAliasId],
-            a.Email [Email] 
-        FROM 
-            @PersonDuplicateByEmailTable a
-        join @PersonDuplicateByEmailTable b on a.email = b.email
-        and b.Id != a.Id  
-        ) AS source (PersonAliasId, DuplicatePersonAliasId, Email)
-    ON (target.PersonAliasId = source.PersonAliasId) and (target.DuplicatePersonAliasId = source.DuplicatePersonAliasId)
-    WHEN MATCHED THEN
-        UPDATE SET [Score] = 2, [ScoreDetail] = 'Email:' + source.Email
-        WHEN NOT MATCHED THEN
-            INSERT (PersonAliasId, DuplicatePersonAliasId, Score, ScoreDetail, IsConfirmedAsNotDuplicate, [Guid])
-            VALUES (source.PersonAliasId, source.DuplicatePersonAliasId, 2, 'Email:' + source.Email, 0, NEWID());
+        SELECT [e1].[PersonAliasId] [PersonAliasId]
+            ,[e2].[PersonAliasId] [DuplicatePersonAliasId]
+            ,[e1].[Email] [Email]
+        FROM #PersonDuplicateByEmailTable [e1]
+        JOIN #PersonDuplicateByEmailTable [e2] ON [e1].[email] = [e2].[email]
+            AND [e1].[Id] != [e2].[Id]
+        ) AS source(PersonAliasId, DuplicatePersonAliasId, Email)
+        ON (target.PersonAliasId = source.PersonAliasId)
+            AND (target.DuplicatePersonAliasId = source.DuplicatePersonAliasId)
+    WHEN MATCHED
+        THEN
+            UPDATE
+            SET [Score] = [Score] + @cScoreWeightEmail
+                ,[ScoreDetail] = [ScoreDetail] + '| Email:' + source.Email
+                ,[ModifiedDateTime] = @processDateTime
+    WHEN NOT MATCHED
+        THEN
+            INSERT (
+                PersonAliasId
+                ,DuplicatePersonAliasId
+                ,Score
+                ,ScoreDetail
+                ,IsConfirmedAsNotDuplicate
+                ,ModifiedDateTime
+                ,CreatedDateTime
+                ,[Guid]
+                )
+            VALUES (
+                source.PersonAliasId
+                ,source.DuplicatePersonAliasId
+                ,@cScoreWeightEmail
+                ,'| Email:' + source.Email
+                ,0
+                ,@processDateTime
+                ,@processDateTime
+                ,NEWID()
+                );
 
+    /* Update PersonDuplicate table with results of name match */
+    MERGE [PersonDuplicate] AS TARGET
+    USING (
+        SELECT [e1].[PersonAliasId] [PersonAliasId]
+            ,[e2].[PersonAliasId] [DuplicatePersonAliasId]
+            ,[e1].[NameMatch] [NameMatch]
+        FROM #PersonDuplicateByNameTable [e1]
+        JOIN #PersonDuplicateByNameTable [e2] ON [e1].[NameMatch] = [e2].[NameMatch]
+            AND [e1].[Id] != [e2].[Id]
+        ) AS source(PersonAliasId, DuplicatePersonAliasId, NameMatch)
+        ON (target.PersonAliasId = source.PersonAliasId)
+            AND (target.DuplicatePersonAliasId = source.DuplicatePersonAliasId)
+    WHEN MATCHED
+        THEN
+            UPDATE
+            SET [Score] = [Score] + @cScoreWeightNameMatch
+                ,[ScoreDetail] = [ScoreDetail] + '| NameMatch:' + source.NameMatch
+                ,[ModifiedDateTime] = @processDateTime
+    WHEN NOT MATCHED
+        THEN
+            INSERT (
+                PersonAliasId
+                ,DuplicatePersonAliasId
+                ,Score
+                ,ScoreDetail
+                ,IsConfirmedAsNotDuplicate
+                ,ModifiedDateTime
+                ,CreatedDateTime
+                ,[Guid]
+                )
+            VALUES (
+                source.PersonAliasId
+                ,source.DuplicatePersonAliasId
+                ,@cScoreWeightNameMatch
+                ,'| NameMatch:' + source.NameMatch
+                ,0
+                ,@processDateTime
+                ,@processDateTime
+                ,NEWID()
+                );
 
+    /* Clean up records that no longer are duplicates */
+    DECLARE @staleCount INT;
 
+    SELECT @staleCount = count(*)
+    FROM [PersonDuplicate]
+    WHERE [ModifiedDateTime] < @processDateTime
 
+    IF (@staleCount != 0)
+    BEGIN
+        DELETE
+        FROM [PersonDuplicate]
+        WHERE [ModifiedDateTime] < @processDateTime
+    END
+
+    DROP TABLE #PersonDuplicateByEmailTable;
+
+    DROP TABLE #PersonDuplicateByNameTable;
+
+    COMMIT
 END
+GO
 
-/*
-select * from PersonDuplicate order by ScoreDetail
+/* DEBUG
+EXEC [dbo].[spCrm_PersonDuplicateFinder]
+GO
 */
-
