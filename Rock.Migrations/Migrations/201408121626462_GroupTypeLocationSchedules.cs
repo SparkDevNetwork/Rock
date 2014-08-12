6c422676bf66f23d6e92f2a75e9302cdfeccf6dd
David Turner <david@azturners.com>
2014-08-12 09:53:57 -0700

    - Update previous migration to also update locations associated with central kiosk

--- Rock.Migrations/Migrations/201408121626462_GroupTypeLocationSchedules.cs
+++ Rock.Migrations/Migrations/201408121626462_GroupTypeLocationSchedules.cs
@@ -92,6 +92,27 @@ namespace Rock.Migrations
     INSERT INTO [AttributeQualifier] ( [IsSystem], [AttributeId], [Key], [Value], [Guid] )
     VALUES ( 1, @AttributeId, 'binaryFileType', CAST( @CheckInLabelFileTypeId AS VARCHAR ), '06401D07-102F-4FEC-8F2C-ED4F4E177184' )
     
+    -- Update the Central Kiosk device to be associated with campus location instead of each room (change to checking to support this is coming)
+    DECLARE @KioskId int = ( SELECT TOP 1 [Id] FROM [Device] WHERE [Guid] = '61111232-01D7-427D-9C1F-D45CF4D3F7CB' )
+    DECLARE @CampusLocationId int = ( SELECT TOP 1 [Id] FROM [Location] WHERE [Guid] = 'D5171D44-C801-4B7D-9335-D23FB4EA0E60' )
+    
+    DELETE DL
+    FROM [DeviceLocation] DL
+    INNER JOIN [Location] L on L.[Id] = DL.[LocationId]
+    WHERE L.[Guid] IN (
+          '844336F4-88B4-4894-B416-769C95A4702D'
+        , 'C07B25A6-EB58-4F77-8ED3-952C5A4DEE9F'
+        , '0BD7BF34-647D-4DC1-8C61-DD154B403687'
+        , 'DFCBFE8A-D1E8-4E95-9561-81826C1C2783'
+        , 'E847E31E-ABA2-415B-A0E1-770FF1B64425'
+        , 'F9F33251-F829-4618-904E-42A1EDA2B58E'
+        , '40AABD2F-40A0-4F95-96CD-1ACACA7460B0'
+        , '2459095D-78DF-405E-8974-DDBDBD053AEE'    
+    )
+
+    INSERT INTO [DeviceLocation] ( [DeviceId], [LocationId] )
+    VALUES ( @KioskId, @CampusLocationId )
+    
 " );
         }
         
