6718d7876510b5ab95fd451b31f1ca030d2f5e07
David Turner <david@azturners.com>
2014-08-14 16:28:13 -0700

    + Add block for updating several people at once

---
+++ RockWeb/Blocks/Crm/BulkUpdate.ascx.cs
@@ -0,0 +1,444 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Communication;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls.Communication;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Crm
+{
+    /// <summary>
+    /// User control for creating a new communication.  This block should be used on same page as the CommunicationDetail block and only visible when editing a new or transient communication
+    /// </summary>
+    [DisplayName( "Bulk Update" )]
+    [Category( "CRM" )]
+    [Description( "Used for updating information about several individuals at once." )]
+
+    [IntegerField( "Display Count", "The initial number of individuals to display prior to expanding list", false, 0, "", 3  )]
+    public partial class BulkUpdate : RockBlock
+    {
+        #region Fields
+
+        DateTime _gradeTransitionDate = new DateTime( RockDateTime.Today.Year, 6, 1 );
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the individuals.
+        /// </summary>
+        /// <value>
+        /// The individual ids.
+        /// </value>
+        protected List<Individual> Individuals
+        {
+            get 
+            { 
+                var individuals = ViewState["Individuals"] as List<Individual>;
+                if ( individuals == null )
+                {
+                    individuals = new List<Individual>();
+                    ViewState["Individuals"] = individuals;
+                }
+                return individuals;
+            }
+
+            set { ViewState["Individuals"] = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [show all individuals].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [show all individuals]; otherwise, <c>false</c>.
+        /// </value>
+        protected bool ShowAllIndividuals
+        {
+            get { return ViewState["ShowAllIndividuals"] as bool? ?? false; }
+            set { ViewState["ShowAllIndividuals"] = value; }
+        }
+            
+        #endregion
+
+        #region Base Control Methods
+
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            ddlTitle.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_TITLE ) ), true );
+            ddlStatus.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_CONNECTION_STATUS ) ), true );
+            ddlMaritalStatus.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_MARITAL_STATUS ) ), true );
+            ddlSuffix.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_SUFFIX ) ), true );
+            ddlRecordStatus.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_RECORD_STATUS ) ), true );
+            ddlReason.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_RECORD_STATUS_REASON ) ), true );
+            ddlReviewReason.BindToDefinedType( DefinedTypeCache.Read( new Guid( Rock.SystemGuid.DefinedType.PERSON_REVIEW_REASON ) ), true );
+
+            DateTime? gradeTransitionDate = GlobalAttributesCache.Read().GetValue( "GradeTransitionDate" ).AsDateTime();
+            if ( gradeTransitionDate.HasValue )
+            {
+                _gradeTransitionDate = gradeTransitionDate.Value;
+            }
+
+            ddlGrade.Items.Clear();
+            ddlGrade.Items.Add( new ListItem( "", "" ) );
+            ddlGrade.Items.Add( new ListItem( "K", "0" ) );
+            ddlGrade.Items.Add( new ListItem( "1st", "1" ) );
+            ddlGrade.Items.Add( new ListItem( "2nd", "2" ) );
+            ddlGrade.Items.Add( new ListItem( "3rd", "3" ) );
+            ddlGrade.Items.Add( new ListItem( "4th", "4" ) );
+            ddlGrade.Items.Add( new ListItem( "5th", "5" ) );
+            ddlGrade.Items.Add( new ListItem( "6th", "6" ) );
+            ddlGrade.Items.Add( new ListItem( "7th", "7" ) );
+            ddlGrade.Items.Add( new ListItem( "8th", "8" ) );
+            ddlGrade.Items.Add( new ListItem( "9th", "9" ) );
+            ddlGrade.Items.Add( new ListItem( "10th", "10" ) );
+            ddlGrade.Items.Add( new ListItem( "11th", "11" ) );
+            ddlGrade.Items.Add( new ListItem( "12th", "12" ) );
+
+            int gradeFactorReactor = ( RockDateTime.Now < _gradeTransitionDate ) ? 12 : 13;
+
+            string script = string.Format( @"
+    $('#{0}').change(function(){{
+        if ($(this).val() != '') {{
+            $('#{1}').val( {2} + ( {3} - parseInt( $(this).val() ) ) );
+    
+        }}
+    }});
+
+    $('#{1}').change(function(){{
+        if ($(this).val() == '') {{
+            $('#{0}').val('');
+        }} else {{
+            var grade = {3} - ( parseInt( $(this).val() ) - {4} );
+            if (grade >= 0 && grade <= 12) {{
+                $('#{0}').val(grade.toString());
+            }} else {{
+                $('#{0}').val('');
+            }}
+        }}
+    }});
+
+", ddlGrade.ClientID, ypGraduation.ClientID, _gradeTransitionDate.Year, gradeFactorReactor, RockDateTime.Now.Year );
+            ScriptManager.RegisterStartupScript( ddlGrade, ddlGrade.GetType(), "grade-selection-" + BlockId.ToString(), script, true );
+
+            script = @"
+    $('a.remove-all-individuals').click(function( e ){
+        e.preventDefault();
+        Rock.dialogs.confirm('Are you sure you want to remove all of the individuals from this update?', function (result) {
+            if (result) {
+                eval(e.target.href);
+            }
+        });
+    });
+";
+            ScriptManager.RegisterStartupScript( lbRemoveAllIndividuals, lbRemoveAllIndividuals.GetType(), "confirm-remove-all-" + BlockId.ToString(), script, true );
+        }
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                var campusi = new CampusService( new RockContext() ).Queryable().OrderBy( a => a.Name ).ToList();
+                cpCampus.Campuses = campusi;
+                cpCampus.Required = false;
+
+                ShowAllIndividuals = false;
+                ShowDetail();
+            }
+            else
+            {
+                BuildGroupControls( false );
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.PreRender" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnPreRender( EventArgs e )
+        {
+            BindIndividuals();
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the SelectPerson event of the ppAddPerson control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void ppAddPerson_SelectPerson( object sender, EventArgs e )
+        {
+            if ( ppAddPerson.PersonId.HasValue )
+            {
+                if ( !Individuals.Any( r => r.PersonId == ppAddPerson.PersonId.Value ) )
+                {
+                    var Person = new PersonService( new RockContext() ).Get( ppAddPerson.PersonId.Value );
+                    if ( Person != null )
+                    {
+                        Individuals.Add( new Individual( Person ) );
+                        ShowAllIndividuals = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the ItemCommand event of the rptIndividuals control.
+        /// </summary>
+        /// <param name="source">The source of the event.</param>
+        /// <param name="e">The <see cref="RepeaterCommandEventArgs" /> instance containing the event data.</param>
+        protected void rptIndividuals_ItemCommand( object source, RepeaterCommandEventArgs e )
+        {
+            int personId = int.MinValue;
+            if ( int.TryParse( e.CommandArgument.ToString(), out personId ) )
+            {
+                Individuals = Individuals.Where( r => r.PersonId != personId ).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbShowAllIndividuals control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void lbShowAllIndividuals_Click( object sender, EventArgs e )
+        {
+            ShowAllIndividuals = true;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbRemoveAllIndividuals control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+        protected void lbRemoveAllIndividuals_Click( object sender, EventArgs e )
+        {
+            Individuals = new List<Individual>();
+        }
+
+        /// <summary>
+        /// Handles the ServerValidate event of the valIndividuals control.
+        /// </summary>
+        /// <param name="source">The source of the event.</param>
+        /// <param name="args">The <see cref="ServerValidateEventArgs" /> instance containing the event data.</param>
+        protected void valIndividuals_ServerValidate( object source, ServerValidateEventArgs args )
+        {
+            args.IsValid = Individuals.Any();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnComplete control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnComplete_Click( object sender, EventArgs e )
+        {
+            if ( Page.IsValid )
+            {
+                string message = string.Empty;
+                ShowResult( message );
+            }
+        }
+
+        /// <summary>
+        /// Handles the SelectedIndexChanged event of the ddlRecordStatus control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void ddlRecordStatus_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            ddlReason.Visible = ( ddlRecordStatus.SelectedValueAsInt() == DefinedValueCache.Read( new Guid( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_INACTIVE ) ).Id );
+            tbInactiveReason.Visible = ddlReason.Visible;
+        }
+
+        /// <summary>
+        /// Handles the SelectItem event of the gpGroup control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void gpGroup_SelectItem( object sender, EventArgs e )
+        {
+            BuildGroupControls( true );
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Shows the detail.
+        /// </summary>
+        /// <param name="communication">The communication.</param>
+        private void ShowDetail ()
+        {
+            Individuals.Clear();
+        }
+
+        /// <summary>
+        /// Binds the individuals.
+        /// </summary>
+        private void BindIndividuals()
+        {
+            int individualCount = Individuals.Count();
+            lNumIndividuals.Text = individualCount.ToString( "N0" ) +
+                (individualCount == 1 ? " Person" : " People");
+
+            ppAddPerson.PersonId = Rock.Constants.None.Id;
+            ppAddPerson.PersonName = "Add Person";
+
+            int displayCount = int.MaxValue;
+
+            if ( !ShowAllIndividuals )
+            {
+                int.TryParse( GetAttributeValue( "DisplayCount" ), out displayCount );
+            }
+
+            if ( displayCount > 0 && displayCount < Individuals.Count )
+            {
+                rptIndividuals.DataSource = Individuals.Take( displayCount ).ToList();
+                lbShowAllIndividuals.Visible = true;
+            }
+            else
+            {
+                rptIndividuals.DataSource = Individuals.ToList();
+                lbShowAllIndividuals.Visible = false;
+            }
+
+            rptIndividuals.DataBind();
+        }
+
+        private void BuildGroupControls( bool setValues )
+        {
+            ddlGroupRole.Items.Clear();
+            ddlGroupMemberStatus.Items.Clear();
+            phAttributes.Controls.Clear();
+
+            var rockContext = new RockContext();
+            Group group = null;
+
+            int? groupId = gpGroup.SelectedValueAsId();
+            if (groupId.HasValue)
+            {
+                group = new GroupService( rockContext ).Get( groupId.Value );
+            }
+
+            if (group != null)
+            {
+                var groupType = GroupTypeCache.Read( group.GroupTypeId );
+                ddlGroupRole.DataSource = groupType.Roles.OrderBy( r => r.Order ).ToList();
+                ddlGroupRole.DataBind();
+
+                ddlGroupMemberStatus.Items.Add( new ListItem( "Active", "1") );
+                ddlGroupMemberStatus.Items.Add( new ListItem( "Pending", "2" ) );
+                ddlGroupMemberStatus.Items.Add( new ListItem( "Inactive", "0") );
+
+                var groupMember = new GroupMember();
+                groupMember.Group = group;
+                groupMember.GroupId = group.Id;
+                Rock.Attribute.Helper.AddEditControls( groupMember, phAttributes, setValues, "", true );
+            }
+            else
+            {
+                ddlGroupRole.Items.Add( new ListItem( string.Empty, string.Empty ) );
+                ddlGroupMemberStatus.Items.Add( new ListItem( string.Empty, string.Empty ) );
+            }
+        }
+
+        /// <summary>
+        /// Shows the result.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        private void ShowResult( string message )
+        {
+            pnlEntry.Visible = false;
+
+            nbResult.Text = message;
+
+            pnlResult.Visible = true;
+        }
+
+        #endregion
+
+        #region Helper Classes
+
+        /// <summary>
+        /// Helper class used to maintain state of individuals
+        /// </summary>
+        [Serializable]
+        protected class Individual
+        {
+            /// <summary>
+            /// Gets or sets the person id.
+            /// </summary>
+            /// <value>
+            /// The person id.
+            /// </value>
+            public int PersonId { get; set; }
+
+            /// <summary>
+            /// Gets or sets the name of the person.
+            /// </summary>
+            /// <value>
+            /// The name of the person.
+            /// </value>
+            public string PersonName { get; set; }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Individual" /> class.
+            /// </summary>
+            /// <param name="personId">The person id.</param>
+            /// <param name="personName">Name of the person.</param>
+            /// <param name="status">The status.</param>
+            public Individual( Person person )
+            {
+                PersonId = person.Id;
+                PersonName = person.FullName;
+            }
+
+        }
+
+        #endregion
+
+}
+}
