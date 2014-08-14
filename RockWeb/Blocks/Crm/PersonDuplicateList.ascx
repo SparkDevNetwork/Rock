a2847c2297807660431119e0a3bf20756e19a30d
Mike Peterson <MikePeterson@ccvonline.com>
2014-08-14 15:55:04 -0700

    - Progress on PersonDuplicate List/Detail blocks

--- RockWeb/Blocks/Crm/PersonDuplicateList.ascx
+++ RockWeb/Blocks/Crm/PersonDuplicateList.ascx
@@ -4,24 +4,33 @@
     <ContentTemplate>
 
         <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
-        
+
             <div class="panel-heading">
-                <h1 class="panel-title"><i class="fa fa-star"></i> Person Duplicate List</h1>
+                <h1 class="panel-title"><i class="fa fa-files-o"></i> Person Duplicate List</h1>
             </div>
             <div class="panel-body">
 
                 <div class="grid grid-panel">
-                    <Rock:Grid ID="gList" runat="server" PersonIdField="PersonId">
+                    <Rock:Grid ID="gList" runat="server" DataKeyNames="PersonId" AllowSorting="true" OnRowSelected="gList_RowSelected">
                         <Columns>
-                            <asp:BoundField DataField="FullName" HeaderText="Name" SortExpression="FullName" />
-                            <asp:BoundField DataField="MatchCount" HeaderText="Match Count" SortExpression="MatchCount" />
-                            <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                            <asp:BoundField DataField="FirstName" HeaderText="First Name" SortExpression="FirstName, LastName" />
+                            <asp:BoundField DataField="LastName" HeaderText="Last Name" SortExpression="LastName, FirstName" />
+                            <asp:BoundField DataField="MatchCount" HeaderText="Match Count" SortExpression="MatchCount" ItemStyle-HorizontalAlign="Right" />
+
+                            <asp:TemplateField HeaderText="Max Score" ItemStyle-HorizontalAlign="Right" SortExpression="Max">
+                                <ItemTemplate>
+                                    <span class='<%# GetMatchLabelClass((decimal)Eval("MaxScorePercent"))  %>'><%# ((decimal)Eval("MaxScorePercent")/100).ToString("P") %></span>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                            <Rock:DateTimeField DataField="PersonModifiedDateTime" HeaderText="Modified" SortExpression="PersonModifiedDateTime" />
+                            <asp:BoundField DataField="CreatedByPerson" HeaderText="Created By" SortExpression="CreatedByPerson" />
                         </Columns>
                     </Rock:Grid>
                 </div>
 
             </div>
-        
+
         </asp:Panel>
 
     </ContentTemplate>
--- RockWeb/Blocks/Crm/PersonDuplicateList.ascx.cs
+++ RockWeb/Blocks/Crm/PersonDuplicateList.ascx.cs
@@ -17,44 +17,52 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 using Rock;
+using Rock.Attribute;
 using Rock.Data;
 using Rock.Model;
-using Rock.Web.Cache;
-using Rock.Web.UI.Controls;
-using Rock.Attribute;
 using Rock.Web.UI;
+using Rock.Web.UI.Controls;
 
 namespace RockWeb.Blocks.Crm
 {
     /// <summary>
-    /// Template block for developers to use to start a new block.
+    /// List of person records that have possible duplicates
     /// </summary>
     [DisplayName( "Person Duplicate List" )]
     [Category( "CRM" )]
     [Description( "List of person records that have possible duplicates" )]
+
+    [DecimalField( "Match Percent High", "The minimum percent score required to be considered a likely match", true, 80.00 )]
+    [DecimalField( "Match Percent Low", "The max percent score required to be considered an unlikely match", true, 40.00 )]
+    [LinkedPage("Detail Page")]
     public partial class PersonDuplicateList : RockBlock
     {
-        #region Fields
-
-        // used for private variables
-
-        #endregion
-
-        #region Properties
-
-        // used for public / protected properties
-
-        #endregion
+        /// <summary>
+        /// Gets the match label class.
+        /// </summary>
+        /// <param name="score">The score.</param>
+        /// <returns></returns>
+        public string GetMatchLabelClass( decimal percent )
+        {
+            if ( percent >= this.GetAttributeValue( "MatchPercentHigh" ).AsDecimal() )
+            {
+                return "label label-success";
+            }
+            else if ( percent <= this.GetAttributeValue( "MatchPercentLow" ).AsDecimal() )
+            {
+                return "label label-default";
+            }
+            else
+            {
+                return "label label-warning";
+            }
+        }
 
         #region Base Control Methods
-
-        //  overrides of the base RockBlock methods (i.e. OnInit, OnLoad)
 
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
@@ -64,9 +72,9 @@ namespace RockWeb.Blocks.Crm
         {
             base.OnInit( e );
 
-            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
-            this.BlockUpdated += Block_BlockUpdated;
-            this.AddConfigurationUpdateTrigger( upnlContent );
+            gList.Actions.ShowAdd = false;
+            gList.DataKeyNames = new string[] { "PersonId" };
+            gList.GridRebind += gList_GridRebind;
         }
 
         /// <summary>
@@ -87,16 +95,24 @@ namespace RockWeb.Blocks.Crm
 
         #region Events
 
-        // handlers called by the controls on your block
-
         /// <summary>
-        /// Handles the BlockUpdated event of the control.
+        /// Handles the GridRebind event of the gList control.
         /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void Block_BlockUpdated( object sender, EventArgs e )
+        protected void gList_GridRebind( object sender, EventArgs e )
         {
+            BindGrid();
+        }
 
+        /// <summary>
+        /// Handles the RowSelected event of the gList control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void gList_RowSelected( object sender, RowEventArgs e )
+        {
+            NavigateToLinkedPage( "DetailPage", "PersonId", e.RowKeyId );
         }
 
         #endregion
@@ -109,12 +125,32 @@ namespace RockWeb.Blocks.Crm
         private void BindGrid()
         {
             RockContext rockContext = new RockContext();
-            PersonService personService = new PersonService( rockContext );
-            
-            // sample query to display a few people
-            var qry = personService.Queryable()
-                        .Where( p => p.Gender == Gender.Male)
-                        .Take(10);
+            var personDuplicateService = new PersonDuplicateService( rockContext );
+
+            var personDuplicateQry = personDuplicateService.Queryable();
+
+            var groupByQry = personDuplicateQry.GroupBy( a => a.PersonAlias.Person );
+
+            var qry = groupByQry.Select( a => new
+            {
+                PersonId = a.Key.Id,
+                LastName = a.Key.LastName,
+                FirstName = a.Key.FirstName,
+                MatchCount = a.Count(),
+                MaxScorePercent = a.Max( s => s.Score * 100 / (decimal)s.Capacity ),
+                PersonModifiedDateTime = a.Key.ModifiedDateTime,
+                CreatedByPerson = a.Key.CreatedByPersonAlias.Person.FirstName + " " + a.Key.CreatedByPersonAlias.Person.LastName
+            } );
+
+            SortProperty sortProperty = gList.SortProperty;
+            if ( sortProperty != null )
+            {
+                qry = qry.Sort( sortProperty );
+            }
+            else
+            {
+                qry = qry.OrderByDescending( a => a.MaxScorePercent ).ThenBy( a => a.LastName ).ThenBy( a => a.FirstName );
+            }
 
             gList.DataSource = qry.ToList();
             gList.DataBind();
