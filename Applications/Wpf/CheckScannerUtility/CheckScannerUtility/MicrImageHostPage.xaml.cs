06d4d21c04edf6f670c11a2157e57583de80f5f6
Mike Peterson <MikePeterson@ccvonline.com>
2014-09-03 14:29:05 -0700

    - Updated CheckScanner to work with either Both, One or No Scanner drivers installed.  If None are installed, a message will be shown explaining that there are no drivers installed. - When starting up, if unable to connect to the configured scanner, OptionsPage will be shown so that user can select which scanner type (if any) to use

---
+++ Applications/Wpf/CheckScannerUtility/CheckScannerUtility/MicrImageHostPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace Rock.Apps.CheckScannerUtility
+{
+    /// <summary>
+    /// Interaction logic for MicrImageHostPage.xaml
+    /// </summary>
+    public partial class MicrImageHostPage : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MicrImageHostPage" /> class.
+        /// </summary>
+        public MicrImageHostPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Applications/Wpf/CheckScannerUtility/CheckScannerUtility/RangerScannerHostPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Rock.Apps.CheckScannerUtility
+{
+    /// <summary>
+    /// Interaction logic for RangerScannerHostPage.xaml
+    /// </summary>
+    public partial class RangerScannerHostPage : Page
+    {
+        public RangerScannerHostPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
