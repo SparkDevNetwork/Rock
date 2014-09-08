65d2d7cbc1192e0fd275016e32d1df26ce291129
Jon Edmiston <jon@sparkdevnetwork.org>
2014-09-07 20:48:36 -0700

    - added logic to generate unique machine key to installer

--- Installers/RockInstaller/RockInstallTools/RockInstallUtilities.cs
+++ Installers/RockInstaller/RockInstallTools/RockInstallUtilities.cs
@@ -23,14 +23,25 @@ namespace RockInstallTools
             return Convert.ToBase64String( uniqueHash.ComputeHash( hash.ComputeHash( Encoding.Unicode.GetBytes( password ) ) ) );
         }
         
-        public static string GenerateRandomDataEncryptionKey()
+        public static string GeneratePasswordKey( int length )
         {
             var rng = System.Security.Cryptography.RNGCryptoServiceProvider.Create();
-            byte[] randomBytes = new byte[128];
+            byte[] randomBytes = new byte[length];
             rng.GetNonZeroBytes( randomBytes );
             string dataEncryptionKey = Convert.ToBase64String( randomBytes );
 
             return dataEncryptionKey;
+        }
+
+        public static string GenerateMachineKey( int length )
+        {
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[length];
+            rngCsp.GetBytes( buff );
+            StringBuilder sb = new StringBuilder( buff.Length * 2 );
+            for ( int i = 0; i < buff.Length; i++ )
+                sb.Append( string.Format( "{0:X2}", buff[i] ) );
+            return sb.ToString();
         }
 
         private static byte[] HexToByte( string hexString )
