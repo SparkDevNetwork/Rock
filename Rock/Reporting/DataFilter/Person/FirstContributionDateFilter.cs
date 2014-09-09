d346d69969584a342d8f3fff7f2f6ee82b6cb578
Mike Peterson <MikePeterson@ccvonline.com>
2014-09-09 13:50:59 -0700

    + moved settings for Full and Light CKEditor toolbars to RockWeb/Scripts/Rock/Controls/htmlEditor.js - stylecop FirstContributionDateFilter

--- Rock/Reporting/DataFilter/Person/FirstContributionDateFilter.cs
+++ Rock/Reporting/DataFilter/Person/FirstContributionDateFilter.cs
@@ -15,13 +15,11 @@
 // </copyright>
 //
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using Rock.Data;
 using Rock.Model;
 using Rock.Web.UI.Controls;
@@ -239,7 +237,7 @@ function() {
             int transactionTypeContributionId = Rock.Web.Cache.DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.TRANSACTION_TYPE_CONTRIBUTION.AsGuid() ).Id;
 
             var firstContributionDateQry = new FinancialTransactionService( rockContext ).Queryable()
-                .Where( xx => xx.TransactionTypeValueId == transactionTypeContributionId)
+                .Where( xx => xx.TransactionTypeValueId == transactionTypeContributionId )
                 .Where( xx => !limitToAccounts || xx.TransactionDetails.Any( a => accountIdList.Contains( a.AccountId ) ) )
                 .GroupBy( xx => xx.AuthorizedPersonAlias.PersonId )
                 .Select( ss => new
