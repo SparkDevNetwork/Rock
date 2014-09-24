b1ed09f47a05d621a59f6e20093387eab4c96d35
Jon Edmiston <jon@sparkdevnetwork.org>
2014-09-23 23:24:35 -0700

    - added Humanizer library to Rock for use in some new liquid filters and in Rock core.

---
+++ packages/Humanizer.1.29.0/src/DateHumanizeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Humanizer.Configuration;
+
+namespace Humanizer
+{
+    /// <summary>
+    /// Humanizes DateTime into human readable sentence
+    /// </summary>
+    public static class DateHumanizeExtensions
+    {
+        /// <summary>
+        /// Turns the current or provided date into a human readable sentence
+        /// </summary>
+        /// <param name="input">The date to be humanized</param>
+        /// <param name="utcDate">Boolean value indicating whether the date is in UTC or local</param>
+        /// <param name="dateToCompareAgainst">Date to compare the input against. If null, current date is used as base</param>
+        /// <param name="culture">Culture to use. If null, current thread's UI culture is used.</param>
+        /// <returns>distance of time in words</returns>
+        public static string Humanize(this DateTime input, bool utcDate = true, DateTime? dateToCompareAgainst = null, CultureInfo culture = null)
+        {
+            var comparisonBase = dateToCompareAgainst ?? DateTime.UtcNow;
+
+            if (!utcDate)
+                comparisonBase = comparisonBase.ToLocalTime();
+
+            return Configurator.DateTimeHumanizeStrategy.Humanize(input, comparisonBase, culture);
+        }
+    }
+}
