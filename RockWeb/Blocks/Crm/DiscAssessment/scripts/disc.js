421f50702835fb01eec78278e434b71d21fa3bba
nairdo <nairdo@gmail.com>
2014-09-29 17:11:15 -0700

    - DISC code from Derek and Nick pending coding conventions consistency refactor.

--- RockWeb/Blocks/Crm/DiscAssessment/scripts/disc.js
+++ RockWeb/Blocks/Crm/DiscAssessment/scripts/disc.js
@@ -3,57 +3,26 @@
 
 $(document).ready(function () {
     "use strict";
-    $('[id$="btnScoreTest"]').attr('disabled', 'disabled');
+    // $('[id$="btnScoreTest"]').attr('disabled', 'disabled');
     // Checks to see if the test has been scored
     // the isScored variable is passed in from the code-behind file
     if (isScored) {
         $('[id$="navTabs"] a:last').tab('show'); // Select last tab
     }
+
+    ///<summary>
+    /// Unchecks the corresponding item from the other rbl
+    ///<summary>
+    $('#questions input[type=radio]').change(function () {
+        var selectedIdx = this.id.charAt(this.id.length - 1);
+        var $parentRow = $(this).closest(".row");        
+        var moreOrLess = (this.name.substr(length - 8, 7) == "rblMore") ? "_rblLess_" : "_rblMore_";
+        var selector = 'input[type=radio][id$="' + moreOrLess + selectedIdx + '"]';
+
+        // uncheck the corresponding inverse variable
+        var $correspondingItem = $parentRow.find(selector);
+        if ($correspondingItem) {
+            $correspondingItem.removeAttr('checked');
+        }
+    });
 });
-
-function moveOn(curQ) {
-    "use strict";
-    var qmTxt = curQ + "m",
-		qlTxt = curQ + "l",
-		mResponse = "",
-		lResponse = "",
-		qm = $('input[name$="' + qmTxt + '"]:radio'),
-		ql = $('input[name$="' + qlTxt + '"]:radio'),
-		mDone = 0,
-		lDone = 0,
-		x = 0,
-		hdr = $('#page-header');
-
-    for (x = 0; x < qm.length; x++) {
-        if (qm[x].checked) {
-            mDone = 1;
-            mResponse = qm[x].id.substr(-4, 3);
-            break;
-        }
-    }
-
-    for (x = 0; x < ql.length; x++) {
-        if (ql[x].checked) {
-            lDone = 1;
-            lResponse = ql[x].id.substr(-4, 3);
-            break;
-        }
-    }
-
-    if (mDone && lDone && (mResponse !== lResponse)) {
-        if (curQ < 30) {
-            curQ++;
-            $(window).scrollTop($('td[id$="q' + pad2(curQ) + '"]').offset().top - hdr.height() - 20);
-        }
-        numberOfQuestionsAnswered++;
-        if (numberOfQuestionsAnswered >= 30) {
-            $('[id$="btnScoreTest"]').removeAttr('disabled');
-        }
-    }
-    return true;
-}
-
-function pad2(number) {
-    "use strict";
-    return (number < 10 ? '0' : '') + number;
-}
