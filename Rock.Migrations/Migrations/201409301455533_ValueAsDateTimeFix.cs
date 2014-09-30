64e77d8e5fe6c6229895c800c5aa6dd3702745a4
Mike Peterson <mikepeterson@ccvonline.com>
2014-09-30 11:16:16 -0700

    - update migration to ensure all person records have values for ConnectionStatus, RecordType, MaritalStatus and IsEmailActive - update migration to ensure all person records have a PersonAlias

--- Rock.Migrations/Migrations/201409301455533_ValueAsDateTimeFix.cs
+++ Rock.Migrations/Migrations/201409301455533_ValueAsDateTimeFix.cs
@@ -33,6 +33,24 @@ namespace Rock.Migrations
             DropColumn( "dbo.AttributeValue", "ValueAsNumeric" );
             Sql( @"alter table AttributeValue add ValueAsDateTime as case when (len(value) < 50 and ISDATE( value) = 1) then convert(datetime, value) else null end" );
             Sql( @"alter table AttributeValue add ValueAsNumeric as case when (len(value) < 100 and ISNUMERIC( value) = 1 and value not like '%[^0-9.]%') then convert(numeric(38,10), value ) else null end" );
+
+            // Clean up Person records that have null values for some things
+            Sql( @"
+declare
+  @ConnectionStatusValueId int = (select top 1 [Id] from [DefinedValue] where [Guid] = 'B91BA046-BC1E-400C-B85D-638C1F4E0CE2' /* Visitor */) 
+  ,@RecordTypePersonId int = (select top 1 [Id] from [DefinedValue] where [Guid] = '36CF10D6-C695-413D-8E7C-4546EFEF385E' /* Person */) 
+  ,@MaritalStatusValueId int = (select top 1 [Id] from [DefinedValue] where [Guid] = 'D9CFD343-6A56-45F6-9E26-3269BA4FBC02' /* Unknown */) 
+  ,@IsEmailActive int = 0
+
+update Person set ConnectionStatusValueId = @ConnectionStatusValueId where ConnectionStatusValueId is null;
+update Person set RecordTypeValueId = @RecordTypePersonId where RecordTypeValueId is null;
+update Person set MaritalStatusValueId = @MaritalStatusValueId where MaritalStatusValueId is null;
+update Person set IsEmailActive = @IsEmailActive where IsEmailActive is null;
+" );
+
+            // ensure Person records have a PersonAlias
+            Sql( @"INSERT INTO [PersonAlias] ([PersonId],[AliasPersonId],[AliasPersonGuid],[Guid])
+    SELECT [Id], [Id], [Guid], NEWID() FROM [Person] p where p.Id not in (select PersonId from PersonAlias where PersonId = AliasPersonId)" );
         }
         
         /// <summary>
