e5e90f744bb6ba218b2a88f054bb518408fa4028
Jon Edmiston <jonedmiston@ccvonline.com>
2014-10-06 15:34:25 -0700

    - updated deployment scripts

--- RockWeb/before-deploy.ps1
+++ RockWeb/before-deploy.ps1
@@ -1,6 +1,9 @@
 # This script is run by AppVeyor's deploy agent before the deploy
 Import-Module WebAdministration
 
+# stop execution of the deploy if the moves fail
+$ErrorActionPreference = "Stop"
+
 # stop web publishing service - needed to allow the deploy to overwrite the sql server spatial types
 Write-Host "Stopping Web Publishing Service"
 stop-service -servicename w3svc
--- RockWeb/deploy.ps1
+++ RockWeb/deploy.ps1
@@ -1,10 +1,6 @@
 # This script is run by AppVeyor's deploy agent after the deploy
 Import-Module WebAdministration
  
-# start web publishing service
-Write-Host "Starting Web Publishing Service"
-start-service -servicename w3svc
-
 # move content directory back from temp
 Write-Host "Moving Contents folder back from temp directory"
 Move-Item c:\webdata\rock.ccvonline.com\temp\Content c:\webdata\rock.ccvonline.com\docs 
@@ -12,3 +8,24 @@ Move-Item c:\webdata\rock.ccvonline.com\temp\Content c:\webdata\rock.ccvonline.c
 # copy new connection string file
 Write-Host "Copying new web.ConnectionStrings.config to web dir"
 Copy-Item c:\webdata\rock.ccvonline.com\config\web.ConnectionStrings.config c:\webdata\rock.ccvonline.com\docs
+
+# start web publishing service
+Write-Host "Starting Web Publishing Service"
+start-service -servicename w3svc
+
+# create empty migration flag
+New-Item c:\webdata\rock.ccvonline.com\docs\App_Data\Run.Migration -type file -force
+
+# delete deploy scripts
+If (Test-Path c:\webdata\rock.ccvonline.com\docs\deploy.ps1){
+	Remove-Item c:\webdata\rock.ccvonline.com\docs\deploy.ps1
+}
+
+If (Test-Path c:\webdata\rock.ccvonline.com\docs\before-deploy.ps1){
+	Remove-Item c:\webdata\rock.ccvonline.com\docs\before-deploy.ps1
+}
+
+# delete the appveyor deploy cache
+If (Test-Path c:\appveyor){
+	Remove-Item c:\appveyor -Force -Confirm:$False -Recurse
+}
