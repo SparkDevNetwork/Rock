23ff41288734938c637c8c40c56e6329e70de8fd
Jon Edmiston <jon@sparkdevnetwork.org>
2014-10-14 12:40:28 -0700

    - small update to the DefinedValueListLiquid block

--- RockWeb/Blocks/Core/DefinedValueListLiquid.ascx.cs
+++ RockWeb/Blocks/Core/DefinedValueListLiquid.ascx.cs
@@ -124,7 +124,6 @@ namespace RockWeb.Blocks.Core
 
                 foreach ( var item in dtItem.DefinedValues )
                 {
-                    //item.LoadAttributes(new RockContext());
                     definedValues.Add( item );
                 }
 
