226d1aa8db05fa14fa7d4deb7a2ada3720438d50
Mike Peterson <mikepeterson@ccvonline.com>
2014-10-14 13:55:58 -0700

    - remove debug code that was included in last commit

--- Rock/Workflow/Action/AssignActivityToGroup.cs
+++ Rock/Workflow/Action/AssignActivityToGroup.cs
@@ -32,8 +32,7 @@ namespace Rock.Workflow.Action
     [Export(typeof(ActionComponent))]
     [ExportMetadata( "ComponentName", "Assign Activity to Group" )]
 
-    //[GroupTypeGroupField( "Group", "Select group type, then group, to set the group to assign this activity to.", "Group")] 
-    [GroupField( "Group", "The group to assign this activity to." )]
+    [GroupTypeGroupField( "Group", "Select group type, then group, to set the group to assign this activity to.", "Group")] 
     public class AssignActivityToGroup : ActionComponent
     {
         /// <summary>
