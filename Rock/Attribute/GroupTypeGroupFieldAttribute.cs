e41ea33c6c83186a4e932619394e16c487014e46
Mike Peterson <mikepeterson@ccvonline.com>
2014-10-14 13:47:19 -0700

    - changed AssignActivityToGroup to use GroupTypeGroupFieldAttribute so that Groups that have a GroupType with ShowInList=True can be picked - added Config to GroupTypeGroupFieldType/Attribute so that GroupPickerLabel can be configured - fixed bug in GroupContextSetter that was causing an exception when it loaded

--- Rock/Attribute/GroupTypeGroupFieldAttribute.cs
+++ Rock/Attribute/GroupTypeGroupFieldAttribute.cs
@@ -38,9 +38,10 @@ namespace Rock.Attribute
         /// <param name="category">The category.</param>
         /// <param name="order">The order.</param>
         /// <param name="key">The key.</param>
-        public GroupTypeGroupFieldAttribute( string name = "", string description = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null )
+        public GroupTypeGroupFieldAttribute( string name = "", string description = "", string groupPickerLabel = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null )
             : base( name, description, required, defaultValue, category, order, key, typeof( Rock.Field.Types.GroupTypeGroupFieldType ).FullName )
         {
+            FieldConfigurationValues.Add( Rock.Field.Types.GroupTypeGroupFieldType.CONFIG_GROUP_PICKER_LABEL, new Field.ConfigurationValue( groupPickerLabel ) );
         }
     }
 }
