174c8591966b19ed5333db2e3b5be23b4f629359
Mike Peterson <MikePeterson@ccvonline.com>
2014-11-18 10:59:19 -0700

    - fix up CkEditor so that it only adds the CkEditor.js lib to the page as needed instead of for every page (fixes #633)

--- Rock/Web/UI/RockPage.cs
+++ Rock/Web/UI/RockPage.cs
@@ -471,9 +471,6 @@ namespace Rock.Web.UI
             // wire up navigation event
             _scriptManager.Navigate += new EventHandler<HistoryEventArgs>( scriptManager_Navigate );
 
-            // add ckeditor (doesn't like to be added during an async postback)
-            _scriptManager.Scripts.Add( new ScriptReference( ResolveRockUrl( "~/Scripts/ckeditor/ckeditor.js", true ) ) );
-
             // Add library and UI bundles during init, that way theme developers will only
             // need to worry about registering any custom scripts or script bundles they need
             _scriptManager.Scripts.Add( new ScriptReference( "~/Bundles/WebFormsJs" ) );
