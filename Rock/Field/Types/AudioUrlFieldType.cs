3f1c33953deb302a4082632999ee1011e0c06766
Mike Peterson <MikePeterson@ccvonline.com>
2014-11-20 17:14:05 -0700

    - fix Video field not showing correctly after partial postback - updated mediaPlayer so that js and css load into the page only as needed - VideoUrl field now supports youtube (but only works on Chrome and Safari)

--- Rock/Field/Types/AudioUrlFieldType.cs
+++ Rock/Field/Types/AudioUrlFieldType.cs
@@ -47,23 +47,19 @@ namespace Rock.Field.Types
                 }
                 else
                 {
-                    string controlId = string.Format( "player_{0}", Guid.NewGuid().ToString( "N" ) );
                     string htmlFormat = @"
 <audio
     src='{0}'
     class='img img-responsive' 
-    id='{1}'
-    controls='true'
+    controls
 >
 </audio>
                     
 <script>
-    $(document).ready(function() {{
-        Rock.controls.mediaPlayer.initialize({{ id: '{1}' }});
-    }});
+    Rock.controls.mediaPlayer.initialize();
 </script>
 ";
-                    var html = string.Format( htmlFormat, videoUrl, controlId );
+                    var html = string.Format( htmlFormat, videoUrl );
                     return html;
                 }
 
