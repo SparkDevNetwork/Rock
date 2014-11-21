d49e104e7a973f9897a83bb8c56c23dfb146bcb2
Mike Peterson <MikePeterson@ccvonline.com>
2014-11-21 14:46:52 -0700

    - fixed up Model.cs in plugins to be abstract (to be consistent with Rock.Data.Model and so that Rock doesn't think they are real entity types)

--- com.ccvonline.Residency/Data/Model.cs
+++ com.ccvonline.Residency/Data/Model.cs
@@ -3,7 +3,7 @@ using System.Runtime.Serialization;
 namespace com.ccvonline.Residency.Data
 {
     [DataContract]
-    public class Model<T> : Rock.Data.Model<T> where T : Rock.Data.Model<T>, Rock.Security.ISecured, new()
+    public abstract class Model<T> : Rock.Data.Model<T> where T : Rock.Data.Model<T>, Rock.Security.ISecured, new()
     {
     }
 }
