b619471a03f59fcac855450ed3cdd287c883d3ba
David Turner <david@azturners.com>
2014-12-12 16:41:25 -0700

    + Fixed Attended Duration badge to correctly include Sunday attendances

--- Rock.Migrations/Migrations/Version 2.0/Version 1.2.0/201412061329400_AddMigrationRollups.cs
+++ Rock.Migrations/Migrations/Version 2.0/Version 1.2.0/201412061329400_AddMigrationRollups.cs
@@ -84,6 +84,52 @@ namespace Rock.Migrations
             {
                 // ignore if the SQL failed
             }
+
+            Sql( @"
+    -- create stored proc for attendance duration
+    /*
+    <doc>
+	    <summary>
+ 		    This function returns the number of weekends a member of a family has attended a weekend service
+		    in the last X weeks.
+	    </summary>
+
+	    <returns>
+		    * Number of weeks
+	    </returns>
+	    <param name=""PersonId"" datatype=""int"">The person id to use</param>
+	    <param name=""WeekDuration"" datatype=""int"">The number of weeks to use as the duration (default 16)</param>
+	    <remarks>	
+	    </remarks>
+	    <code>
+		    EXEC [dbo].[spCheckin_WeeksAttendedInDuration] 2 -- Ted Decker
+	    </code>
+    </doc>
+    */
+
+    ALTER PROCEDURE [dbo].[spCheckin_WeeksAttendedInDuration]
+	    @PersonId int
+	    ,@WeekDuration int = 16
+    AS
+    BEGIN
+	
+        DECLARE @LastSunday datetime 
+
+        SET @LastSunday = [dbo].[ufnUtility_GetPreviousSundayDate]()
+
+        SELECT 
+	        COUNT(DISTINCT dbo.ufnUtility_GetSundayDate(a.[StartDateTime]) )
+        FROM
+	        [Attendance] a
+	        INNER JOIN [PersonAlias] pa ON pa.[Id] = a.[PersonAliasId]
+        WHERE 
+	        [GroupId] IN (SELECT [Id] FROM [dbo].[ufnCheckin_WeeklyServiceGroups]())
+	        AND pa.[PersonId] IN (SELECT [Id] FROM [dbo].[ufnCrm_FamilyMembersOfPersonId](@PersonId))
+	        AND a.[StartDateTime] BETWEEN DATEADD(WEEK, (@WeekDuration * -1), @LastSunday) AND DATEADD(DAY, 1, @LastSunday)
+
+    END
+" );
+
         }
         
         /// <summary>
