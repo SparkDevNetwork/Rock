17a8234b1571c8ea31395acf8663e833e29119ae
Jon Edmiston <jon@sparkdevnetwork.org>
2014-12-15 07:26:40 -0700

    - updates to the package details

---
+++ Rock/Store/PackageVersionService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+using System.Configuration;
+using System.IO;
+
+
+namespace Rock.Store
+{
+    /// <summary>
+    /// Service class for the store category model.
+    /// </summary>
+    public class PackageVersionService : StoreService
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryService"/> class.
+        /// </summary>
+        public PackageVersionService() :base()
+        {}
+
+        /// <summary>
+        /// Gets a package version.
+        /// </summary>
+        /// <returns>a <see cref="T:PackageVersion"/> package version.</returns>
+        public PackageVersion GetPackageVersion(int versionId)
+        {
+            // setup REST call
+            var client = new RestClient( _rockStoreUrl );
+            var request = new RestRequest();
+            request.Method = Method.GET;
+            request.Resource = string.Format( "api/Packages/GetPackageVersionDetails/{0}", versionId.ToString() );
+
+            var version = client.Execute<PackageVersion>( request ).Data;
+            return version;
+        }
+    }
+}
