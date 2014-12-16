6597507522c07f781438447b1cc0899c48071f2b
David Turner <david@azturners.com>
2014-12-16 10:04:27 -0700

    + Updated the Workflow Entry block so that it will interrogate the query string parameters and set any workflow attributes with same key to the value passed in query string. This results in the Activate Workflow block no longer being needed, so it has been deprecated and will be removed in a future update.

--- Rock.Migrations/Migrations/201412160006127_BinaryFileUrl.cs
+++ Rock.Migrations/Migrations/201412160006127_BinaryFileUrl.cs
@@ -91,6 +91,17 @@ BEGIN
         (bf.[Guid] = @Guid)
 END
 " );
+            // Update references to ActivateWorkflow to use WorkflowEntry instead
+            RockMigrationHelper.AddBlockAttributeValue( "2C330A26-1A1C-4B36-80FA-4CB96198F985", "8023344B-C247-4A23-A191-880ABF391A34", "0550d2aa-a705-4400-81ff-ab124fdf83d7" );
+            RockMigrationHelper.UpdateBlockTypeAttribute( "0F5922BB-CD68-40AC-BF3C-4AAB1B98760C", "1D0D3794-C210-48A8-8C68-3FBEC08A6BA5", "Additional Custom Actions", "Actions", "", @"
+Additional custom actions (will be displayed after the list of workflow actions). Any instance of '{0}' will be replaced with the current person's id.
+Because the contents of this setting will be rendered inside a &lt;ul&gt; element, it is recommended to use an 
+&lt;li&gt; element for each available action.  Example:
+<pre>
+    &lt;li&gt;&lt;a href='~/WorkflowEntry/4?PersonId={0}' tabindex='0'&gt;Fourth Action&lt;/a&gt;&lt;/li&gt;
+</pre>
+", 2, "", "35F69669-48DE-4182-B828-4EC9C1C31B08" );
+
         }
         
         /// <summary>
