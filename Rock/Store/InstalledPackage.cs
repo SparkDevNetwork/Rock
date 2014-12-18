e78411b051711a046be3d4107c825304e620e517
Jon Edmiston <jon@sparkdevnetwork.org>
2014-12-17 23:27:01 -0700

    - created model to store info on installed packages (used for serializing to JSON)

---
+++ Rock/Store/InstalledPackage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Store
+{
+    /// <summary>
+    /// Represents a store promo.
+    /// </summary>
+    public class InstalledPackage : StoreModel
+    {
+        /// <summary>
+        /// Gets or sets the PackageId of the installation. 
+        /// </summary>
+        /// <value>
+        /// A <see cref="System.Int32"/> representing the PackageId.
+        /// </value>
+        public int PackageId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the package label of the installation. 
+        /// </summary>
+        /// <value>
+        /// A <see cref="System.String"/> representing the package label.
+        /// </value>
+        public string PackageLabel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the VersionId of the installation. 
+        /// </summary>
+        /// <value>
+        /// A <see cref="System.Int32"/> representing the VersionId.
+        /// </value>
+        public int VersionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version label of the installation. 
+        /// </summary>
+        /// <value>
+        /// A <see cref="System.String"/> representing the version label.
+        /// </value>
+        public string VersionLabel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the install date/time. 
+        /// </summary>
+        /// <value>
+        /// A <see cref="System.DateTime"/> representing the install date/time.
+        /// </value>
+        public DateTime InstallDateTime { get; set; }
+       
+    }
+}
