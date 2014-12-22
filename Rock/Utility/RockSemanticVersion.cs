bbf6112e6f3411455f05760f045438f8e40dbf27
David Turner <david@azturners.com>
2014-12-21 17:19:11 -0700

    - Fixed a few compiler warnings

--- Rock/Utility/RockSemanticVersion.cs
+++ Rock/Utility/RockSemanticVersion.cs
@@ -411,6 +411,12 @@ namespace Rock.Utility
             }
         }
 
+        /// <summary>
+        /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo" /> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> to populate with data.</param>
+        /// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext" />) for this serialization.</param>
+        /// <exception cref="System.ArgumentNullException">info</exception>
         [SecurityPermission( SecurityAction.Demand, SerializationFormatter = true )]
         public void GetObjectData( SerializationInfo info, StreamingContext context )
         {
