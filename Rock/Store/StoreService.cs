bee4a1c46d11d2a2515bdb1e861c49e05205c409
Jon Edmiston <jon@sparkdevnetwork.org>
2015-01-06 22:25:02 -0700

    - add purchase rest method and return models

--- Rock/Store/StoreService.cs
+++ Rock/Store/StoreService.cs
@@ -52,6 +52,39 @@ namespace Rock.Store
             }
         }
 
+        public PurchaseResponse Purchase( string username, string password, int packageId )
+        {
+            string errorResponse = string.Empty;
+            return Purchase( username, password, packageId, out errorResponse );
+        }
+
+        public PurchaseResponse Purchase( string username, string password, int packageId, out string errorResponse )
+        {
+            errorResponse = string.Empty;
+
+            // get organization key
+            string organizationKey = GetOrganizationKey();
+
+            // setup REST call
+            var client = new RestClient( _rockStoreUrl );
+            client.Timeout = _clientTimeout;
+            string requestUrl = string.Format( "api/Store/Purchase/{0}/{1}/{2}/{3}", username, password, organizationKey, packageId.ToString() );
+            var request = new RestRequest( requestUrl, Method.GET );
+
+            // deserialize to list of packages
+            var response = client.Execute<PurchaseResponse>( request );
+
+            if ( response.ResponseStatus == ResponseStatus.Completed )
+            {
+                return response.Data;
+            }
+            else
+            {
+                errorResponse = response.ErrorMessage;
+                return new PurchaseResponse();
+            }
+        }
+
         public static bool OrganizationIsConfigured()
         {
             var globalAttributes = Rock.Web.Cache.GlobalAttributesCache.Read();
