7dabdd965002bbd5eb6603cb231dbb07808efcf2
David Turner <david@azturners.com>
2015-01-08 11:49:19 -0700

    + Updated block rendering so that the Pre/Post text is rendered outside of all the block wrapping divs (reverted from commit 74eb043a2bdd314f6cdb56c81022c1c08fb34f17)

--- RockWeb/Blocks/Administration/PageProperties.ascx.cs
+++ RockWeb/Blocks/Administration/PageProperties.ascx.cs
@@ -110,7 +110,7 @@ namespace RockWeb.Blocks.Administration
                             var blockControl = TemplateControl.LoadControl( block.BlockType.Path ) as RockBlock;
                             if ( blockControl != null )
                             {
-                                blockControl.SetBlock( pageCache, block );
+                                blockControl.SetBlock( block );
                                 foreach ( var context in blockControl.ContextTypesRequired )
                                 {
                                     if ( !blockContexts.ContainsKey( context.Name ) )
