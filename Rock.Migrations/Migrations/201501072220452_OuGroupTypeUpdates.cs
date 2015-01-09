3e979c7c1d3aabf5a876857a5d17d33ceb0a1db7
Jon Edmiston <jon@sparkdevnetwork.org>
2015-01-09 14:24:57 -0700

    - removed the staff purpose migration

--- Rock.Migrations/Migrations/201501072220452_OuGroupTypeUpdates.cs
+++ Rock.Migrations/Migrations/201501072220452_OuGroupTypeUpdates.cs
@@ -29,15 +29,6 @@ namespace Rock.Migrations
         /// </summary>
         public override void Up()
         {
-            // create new purpose
-            RockMigrationHelper.AddDefinedValue( "B23F1E45-BC26-4E82-BEB3-9B191FE5CCC3", "Staff", "Used to designate that a group type contains staff members.", "B76244EB-4F61-4B7C-9595-41D23650386F" );
-        
-            // update ou group type to assign purpose
-            Sql( @"DECLARE @PurposeValueId int = (SELECT [Id] FROM [DefinedValue] WHERE [Guid] = 'B76244EB-4F61-4B7C-9595-41D23650386F')
-
-                UPDATE [GroupType] SET
-                [GroupTypePurposeValueId] = @PurposeValueId
-                WHERE [Guid] = 'AAB2E9F4-E828-4FEE-8467-73DC9DAB784C'" );
 
             // update missing block setting on Org Chart page
             RockMigrationHelper.AddBlockAttributeValue( "3FB66841-811D-4298-823B-06F8AFC95047", "ADCC4391-8D8B-4A28-80AF-24CD6D3F77E2", "C3909F1A-6908-4035-BB93-EC4FBFDCC536" );
