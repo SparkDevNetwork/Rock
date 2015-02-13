5dccd5d47f359182d96c494a4317e74b67935503
Jon Edmiston <jon@sparkdevnetwork.org>
2015-02-12 17:01:22 -0700

    - kiosk progress

--- RockWeb/Blocks/Finance/KioskTransactionEntry.ascx
+++ RockWeb/Blocks/Finance/KioskTransactionEntry.ascx
@@ -6,88 +6,182 @@
         <script>
         
             Sys.Application.add_load(function () {
-                $('.tenkey a.digit').click(function () {
-                    $phoneNumber = $("input[id$='tbPhone']");
-                    $phoneNumber.val($phoneNumber.val() + $(this).html());
-                });
-                $('.tenkey a.back').click(function () {
-                    $phoneNumber = $("input[id$='tbPhone']");
-                    $phoneNumber.val($phoneNumber.val().slice(0,-1));
-                });
-                $('.tenkey a.clear').click(function () {
-                    $phoneNumber = $("input[id$='tbPhone']");
-                    $phoneNumber.val('');
-                });
 
-                // set focus to the input unless on a touch device
-                var isTouchDevice = 'ontouchstart' in document.documentElement;
-                if (!isTouchDevice) {
-                    $('.checkin-phone-entry').focus();
+                //
+                // setup keyboard for phone search
+                //
+                if ($(".js-pnlsearch").length) {
+
+                    $('.js-pnlsearch .tenkey a.digit').click(function () {
+                        $phoneNumber = $("input[id$='tbPhone']");
+                        $phoneNumber.val($phoneNumber.val() + $(this).html());
+                        return false;
+                    });
+                    $('.js-pnlsearch .tenkey a.back').click(function () {
+                        $phoneNumber = $("input[id$='tbPhone']");
+                        $phoneNumber.val($phoneNumber.val().slice(0, -1));
+                        return false;
+                    });
+                    $('.js-pnlsearch .tenkey a.clear').click(function () {
+                        $phoneNumber = $("input[id$='tbPhone']");
+                        $phoneNumber.val('');
+                        return false;
+                    });
+
+                    // set focus to the input unless on a touch device
+                
+                    var isTouchDevice = 'ontouchstart' in document.documentElement;
+                    if (!isTouchDevice) {
+                        $('.kiosk-phoneentry').focus();
+                    }
+                }
+
+                //
+                // setup keyboard for account entry
+                //
+                if ($(".js-pnlaccountentry").length) {
+
+                    $('.js-pnlaccountentry .tenkey a.digit').click(function () {
+                        $amount = $(".input-group.active .form-control");
+                        $amount.val($amount.val() + $(this).html());
+                        return false;
+                    });
+                    $('.js-pnlaccountentry .tenkey a.clear').click(function () {
+                        $amount = $(".input-group.active .form-control");
+                        $amount.val('');
+                        return false;
+                    });
+
+                    $('.form-control').click(function () {
+                        $('.input-group').removeClass("active");
+                        $(this).closest('.input-group').addClass("active");
+                    });
                 }
             });
         
         </script>
 
-        <asp:Panel ID="pnlSearch" runat="server">
+        <asp:Panel ID="pnlSearch" runat="server" CssClass="js-pnlsearch">
+            <header>
+                <h1>Please Enter Your Phone Number</h1>
+            </header>
 
-            <div class="row">
-                <div class="col-md-8">
+            <main>
+                <div class="row">
+                    <div class="col-md-8 margin-b-lg">
+                        <Rock:NotificationBox ID="nbSearch" runat="server" NotificationBoxType="Warning"></Rock:NotificationBox>
                     
-                    <h1>Please Enter Your Phone Number</h1>
+                        <Rock:RockTextBox ID="tbPhone" type="number" CssClass="kiosk-phoneentry" runat="server" Label="Phone Number" />
 
-                    <Rock:NotificationBox ID="nbSearch" runat="server" NotificationBoxType="Warning"></Rock:NotificationBox>
-                    
-                    <Rock:RockTextBox ID="tbPhone" MaxLength="10" CssClass="checkin-phone-entry" runat="server" Label="Phone Number" />
-
-                    <div class="tenkey kiosk-phone-keypad">
-                        <div>
-                            <a href="#" class="btn btn-default btn-lg digit">1</a>
-                            <a href="#" class="btn btn-default btn-lg digit">2</a>
-                            <a href="#" class="btn btn-default btn-lg digit">3</a>
-                        </div>
-                        <div>
-                            <a href="#" class="btn btn-default btn-lg digit">4</a>
-                            <a href="#" class="btn btn-default btn-lg digit">5</a>
-                            <a href="#" class="btn btn-default btn-lg digit">6</a>
-                        </div>
-                        <div>
-                            <a href="#" class="btn btn-default btn-lg digit">7</a>
-                            <a href="#" class="btn btn-default btn-lg digit">8</a>
-                            <a href="#" class="btn btn-default btn-lg digit">9</a>
-                        </div>
-                        <div>
-                            <a href="#" class="btn btn-default btn-lg command back">Back</a>
-                            <a href="#" class="btn btn-default btn-lg digit">0</a>
-                            <a href="#" class="btn btn-default btn-lg command clear">Clear</a>
+                        <div class="tenkey kiosk-phone-keypad">
+                            <div>
+                                <a href="#" class="btn btn-default btn-lg digit">1</a>
+                                <a href="#" class="btn btn-default btn-lg digit">2</a>
+                                <a href="#" class="btn btn-default btn-lg digit">3</a>
+                            </div>
+                            <div>
+                                <a href="#" class="btn btn-default btn-lg digit">4</a>
+                                <a href="#" class="btn btn-default btn-lg digit">5</a>
+                                <a href="#" class="btn btn-default btn-lg digit">6</a>
+                            </div>
+                            <div>
+                                <a href="#" class="btn btn-default btn-lg digit">7</a>
+                                <a href="#" class="btn btn-default btn-lg digit">8</a>
+                                <a href="#" class="btn btn-default btn-lg digit">9</a>
+                            </div>
+                            <div>
+                                <a href="#" class="btn btn-default btn-lg command back">Back</a>
+                                <a href="#" class="btn btn-default btn-lg digit">0</a>
+                                <a href="#" class="btn btn-default btn-lg command clear">Clear</a>
+                            </div>
                         </div>
                     </div>
+                    <div class="col-md-4">
+                        <asp:LinkButton ID="lbSearchNext" runat="server" OnClick="lbSearchNext_Click" CssClass="btn btn-primary btn-kiosk btn-kiosk-lg">Next</asp:LinkButton>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <asp:LinkButton ID="lbSearchNext" runat="server" OnClick="lbSearchNext_Click" CssClass="btn btn-primary btn-lg">Next</asp:LinkButton>
-                </div>
-            </div>
-            
+            </main>
+
             <footer>
-                <asp:LinkButton ID="lbSearchCancel" runat="server" OnClick="lbSearchCancel_Click" CssClass="btn btn-default">Cancel</asp:LinkButton>
+                <div class="container">
+                    <asp:LinkButton ID="lbSearchCancel" runat="server" OnClick="lbSearchCancel_Click" CssClass="btn btn-default btn-kiosk">Cancel</asp:LinkButton>
+                </div>
             </footer>
             
         </asp:Panel>
 
-        <asp:Panel ID="pnlFamilySelect" runat="server" Visible="false">
-            <h1>Select Your Family</h1>
-            family select
+        <asp:Panel ID="pnlGivingUnitSelect" runat="server" Visible="false" CssClass="js-pnlgivingunitselect">
+            <header>
+                <h1>Select Your Family</h1>
+            </header>
+
+            <main class="clearfix">
+                <div class="scrollpanel">
+                    <div class="scroller">
+                        <asp:PlaceHolder ID="phGivingUnits" runat="server"></asp:PlaceHolder>
+                    </div>
+                </div>
+            </main>
 
             <footer>
-                <asp:LinkButton ID="lbFamilySelectBack" runat="server" OnClick="lbFamilySelectBack_Click" CssClass="btn btn-default">Back</asp:LinkButton>
-                <asp:LinkButton ID="lbFamilySelectCancel" runat="server" OnClick="lbFamilySelectCancel_Click" CssClass="btn btn-default">Cancel</asp:LinkButton>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <asp:LinkButton ID="lbGivingUnitSelectBack" runat="server" OnClick="lbGivingUnitSelectBack_Click" CssClass="btn btn-default btn-kiosk">Back</asp:LinkButton>
+                            <asp:LinkButton ID="lbGivingUnitSelectCancel" runat="server" OnClick="lbGivingUnitSelectCancel_Click" CssClass="btn btn-default btn-kiosk">Cancel</asp:LinkButton>
+                        </div>
+                        <div class="col-md-4">
+                            <asp:LinkButton ID="lbRegisterFamily" runat="server" OnClick="lbRegisterFamily_Click" CssClass="btn btn-default btn-kiosk btn-kiosk-lg pull-right">Register</asp:LinkButton>
+                        </div>
+                    </div>
+                </div>
             </footer>
         </asp:Panel>
 
         <asp:Panel ID="pnlRegister" runat="server" Visible="false">
-
+            Register Page
         </asp:Panel>
 
-        <asp:Panel ID="pnlAccountEntry" runat="server" Visible="false">
+        <asp:Panel ID="pnlAccountEntry" runat="server" CssClass="js-pnlaccountentry" Visible="false">
+            <header>
+                <h1>Enter An Amount</h1>
+            </header>
+            
+            <main>
+                <asp:Label ID="lblGivingAs" runat="server"></asp:Label>
+
+                <asp:PlaceHolder id="phAccounts" runat="server"></asp:PlaceHolder>
+
+                <div class="tenkey kiosk-phone-keypad">
+                    <div>
+                        <a href="#" class="btn btn-default btn-lg digit">1</a>
+                        <a href="#" class="btn btn-default btn-lg digit">2</a>
+                        <a href="#" class="btn btn-default btn-lg digit">3</a>
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-default btn-lg digit">4</a>
+                        <a href="#" class="btn btn-default btn-lg digit">5</a>
+                        <a href="#" class="btn btn-default btn-lg digit">6</a>
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-default btn-lg digit">7</a>
+                        <a href="#" class="btn btn-default btn-lg digit">8</a>
+                        <a href="#" class="btn btn-default btn-lg digit">9</a>
+                    </div>
+                    <div>
+                        <a href="#" class="btn btn-default btn-lg digit">.</a>
+                        <a href="#" class="btn btn-default btn-lg digit">0</a>
+                        <a href="#" class="btn btn-default btn-lg command clear">Clear</a>
+                    </div>
+                </div>
+            </main>
+            
+            <footer>
+                <div class="container">
+                    <asp:LinkButton ID="lbAccountEntryBack" runat="server" OnClick="lbAccountEntryBack_Click" CssClass="btn btn-default btn-kiosk">Back</asp:LinkButton>
+                    <asp:LinkButton ID="lbAccountEntryCancel" runat="server" OnClick="lbAccountEntryCancel_Click" CssClass="btn btn-default btn-kiosk">Cancel</asp:LinkButton>
+                </div>
+            </footer>
 
         </asp:Panel>
 
--- RockWeb/Blocks/Finance/KioskTransactionEntry.ascx.cs
+++ RockWeb/Blocks/Finance/KioskTransactionEntry.ascx.cs
@@ -21,6 +21,8 @@ using System.IO;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Text.RegularExpressions;
 
 using Rock;
 using Rock.Data;
@@ -47,6 +49,9 @@ namespace RockWeb.Blocks.Finance
     [LinkedPage( "Homepage", "Homepage of the kiosk.", true, "", "", 2 )]
     [PersonField("Anonymous Person", "Person in the database to assign anonymous giving to.", true, "", "", 3)]
     [DefinedValueField( Rock.SystemGuid.DefinedType.PERSON_CONNECTION_STATUS, "Connection Status", "The connection status to use when creating a new individual.", true, false, Rock.SystemGuid.DefinedValue.PERSON_CONNECTION_STATUS_PARTICIPANT, "", 4 )]
+    [IntegerField( "Minimum Phone Number Length", "Minimum length for phone number searches (defaults to 4).", false, 4,"", 5 )]
+    [IntegerField( "Maximum Phone Number Length", "Maximum length for phone number searches (defaults to 10).", false, 10, "", 6 )]
+    [TextField( "Search Regex", "Regular Expression to run the search input through before searching. Useful for stripping off characters.", false, "", "", 7 )]
     #endregion
 
     public partial class KioskTransactionEntry : Rock.Web.UI.RockBlock
@@ -54,7 +59,23 @@ namespace RockWeb.Blocks.Finance
         #region Fields
 
         // used for private variables
+        private List<GivingUnit> GivingUnits
+        {
+            get { return (List<GivingUnit>)ViewState["GivingUnits"]; }
+            set { ViewState["GivingUnits"] = value; }
+        }
 
+        private GivingUnit SelectedGivingUnit
+        {
+            get { return (GivingUnit)ViewState["SelectedGivingUnit"]; }
+            set { ViewState["SelectedGivingUnit"] = value; }
+        }
+
+        private Dictionary<int, decimal> Amounts
+        {
+            get { return ViewState["Amounts"] as Dictionary<int, decimal>; }
+            set { ViewState["Amounts"] = value; }
+        }
         #endregion
 
         #region Properties
@@ -78,6 +99,9 @@ namespace RockWeb.Blocks.Finance
             // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
             this.BlockUpdated += Block_BlockUpdated;
             this.AddConfigurationUpdateTrigger( upnlContent );
+
+            RockPage.AddScriptLink( "~/Scripts/iscroll.js" );
+            RockPage.AddScriptLink( "~/Scripts/Kiosk/kiosk-core.js" );
         }
 
         /// <summary>
@@ -90,29 +114,30 @@ namespace RockWeb.Blocks.Finance
 
             if ( !Page.IsPostBack )
             {
-                // added for your convenience
+                // set max length of phone
+                int maxLength = int.Parse( GetAttributeValue( "MaximumPhoneNumberLength" ) );
+                tbPhone.MaxLength = maxLength;
+            }
+            else
+            {
+                if ( pnlGivingUnitSelect.Visible )
+                {
+                    BuildGivingUnitControls();
+                }
             }
         }
 
         #endregion
 
         #region Events
-
-        // handlers called by the controls on your block
+                
+        //
+        // Search Events
+        //
 
         protected void lbSearchNext_Click( object sender, EventArgs e )
         {
-            if ( tbPhone.Text.Length < 4 )
-            {
-                nbSearch.Text = "You must enter at least 4 numbers of your phone.";
-                nbSearch.NotificationBoxType = NotificationBoxType.Warning;
-            }
-            else
-            {
-                // todo search for families
-                HidePanels();
-                pnlFamilySelect.Visible = true;
-            }
+            ShowGivingUnitSelectPanel();
         }
 
         protected void lbSearchCancel_Click( object sender, EventArgs e )
@@ -120,15 +145,49 @@ namespace RockWeb.Blocks.Finance
             GoHome();
         }
 
-        protected void lbFamilySelectCancel_Click( object sender, EventArgs e )
+        //
+        // Giving Unit Select Events
+        //
+
+        // called when a giving unit is selected
+        void unitName_Click( object sender, EventArgs e )
+        {
+            LinkButton lb = (LinkButton)sender;
+            this.SelectedGivingUnit = new GivingUnit( lb.CommandArgument );
+
+            ShowAccountPanel();
+        }
+
+        protected void lbGivingUnitSelectCancel_Click( object sender, EventArgs e )
         {
             GoHome();
         }
 
-        protected void lbFamilySelectBack_Click( object sender, EventArgs e )
+        protected void lbGivingUnitSelectBack_Click( object sender, EventArgs e )
         {
             HidePanels();
             pnlSearch.Visible = true;
+        }
+
+        //
+        // Account Entry Events
+        //
+
+        protected void lbAccountEntryBack_Click( object sender, EventArgs e )
+        {
+            HidePanels();
+            pnlGivingUnitSelect.Visible = true;
+        }
+        protected void lbAccountEntryCancel_Click( object sender, EventArgs e )
+        {
+            GoHome();
+        }
+
+        // used to show the new registration page
+        protected void lbRegisterFamily_Click( object sender, EventArgs e )
+        {
+            HidePanels();
+            pnlRegister.Visible = true;
         }
 
         /// <summary>
@@ -145,11 +204,179 @@ namespace RockWeb.Blocks.Finance
 
         #region Methods
 
+        // show giving unit select panel
+        private void ShowGivingUnitSelectPanel()
+        {
+            int minLength = int.Parse( GetAttributeValue( "MinimumPhoneNumberLength" ) );
+            int maxLength = int.Parse( GetAttributeValue( "MaximumPhoneNumberLength" ) );
+
+            if ( tbPhone.Text.Length >= minLength && tbPhone.Text.Length <= maxLength )
+            {
+                // run regex expression on input if provided
+                string searchInput = tbPhone.Text;
+                if ( !string.IsNullOrWhiteSpace( GetAttributeValue( "SearchRegex" ) ) )
+                {
+                    Regex regex = new Regex( GetAttributeValue( "SearchRegex" ) );
+                    Match match = regex.Match( searchInput );
+                    if ( match.Success )
+                    {
+                        if ( match.Groups.Count == 2 )
+                        {
+                            searchInput = match.Groups[1].ToString();
+                        }
+                    }
+                }
+
+                var searchResults = new List<GivingUnit>();
+
+                RockContext rockContext = new RockContext();
+                PersonService personService = new PersonService( rockContext );
+                var people = personService.GetByPhonePartial( searchInput, false, true );
+
+                foreach ( var person in people.ToList() )
+                {
+                    if ( person.GivingGroupId == null )
+                    {
+                        // giving as an individuals
+                        searchResults.Add( new GivingUnit(person.PrimaryAliasId.Value, person.LastName, person.FirstName));
+                    }
+                    else
+                    {
+                        var givingGroupMembers = person.GivingGroup.Members
+                                                    .Where( g => g.Person.GivingGroupId == g.GroupId )
+                                                    .OrderBy( g => g.GroupRole.Order )
+                                                    .ThenBy( g => g.Person.Gender )
+                                                    .ThenBy( g => g.Person.Age );
+
+                        if ( givingGroupMembers.ToList().Count == 1 )
+                        {
+                            // only one person in the giving group display as an individual
+                            searchResults.Add( new GivingUnit( person.PrimaryAliasId.Value, person.LastName, person.FirstName ) );
+                        }
+                        else
+                        {
+                            // display as a family
+                            string firstNameList = string.Join( ", ", givingGroupMembers.Select( g => g.Person.NickName ) ).ReplaceLastOccurrence(",", " &");
+                            int headOfHousePersonAliasId = givingGroupMembers.Select( g => g.Person.PrimaryAliasId.Value ).FirstOrDefault();
+                            string lastName = givingGroupMembers.Select( g => g.Person.LastName ).FirstOrDefault();
+                            searchResults.Add( new GivingUnit( headOfHousePersonAliasId, person.LastName, firstNameList ) );
+                        }
+                    }
+                }
+
+                this.GivingUnits = searchResults;
+
+                BuildGivingUnitControls();
+
+                HidePanels();
+                pnlGivingUnitSelect.Visible = true;
+            }
+            else
+            {
+                if ( tbPhone.Text.Length < minLength )
+                {
+                    nbSearch.Text = String.Format( "Please enter at least {0} numbers of your phone.", minLength.ToString() );
+                }
+                else
+                {
+                    nbSearch.Text = String.Format( "Please enter no more than {0} numbers to search on.", maxLength.ToString() );
+                }
+            }
+        }
+
+        // show accounts panel 
+        private void ShowAccountPanel()
+        {
+            lblGivingAs.Text = String.Format( "Giving as {0} {1}", this.SelectedGivingUnit.FirstNames, this.SelectedGivingUnit.LastName );
+            
+            // get accounts
+            BuildAccountControls();
+
+            // show panels
+            HidePanels();
+            pnlAccountEntry.Visible = true;
+
+
+        }
+
+        // displays accounts
+        private void BuildAccountControls()
+        {
+            // get list of selected accounts filtered by the current campus
+            RockContext rockContext = new RockContext();
+            FinancialAccountService accountService = new FinancialAccountService( rockContext );
+            
+            Guid[] selectedAccounts = GetAttributeValue( "Accounts" ).Split( ',' ).Select( s => Guid.Parse( s ) ).ToArray(); ;
+            int campusContext = 1;
+
+            var accounts = accountService.Queryable()
+                            .Where( a => selectedAccounts.Contains( a.Guid ) && (a.CampusId.Value == campusContext || a.CampusId == null) )
+                            .ToList();
+
+            if ( accounts.Count > 0 )
+            {
+                bool firstAccount = true;
+                
+                foreach ( var account in accounts )
+                {
+                    HtmlGenericControl formGroup = new HtmlGenericControl( "div" );
+                    formGroup.AddCssClass( "form-group" );
+                    phAccounts.Controls.Add( formGroup );
+
+                    RockTextBox tb = new RockTextBox();
+                    tb.PrependText = "<i class='fa fa-dollar'></i>";
+                    tb.ID = "tbAccount_" + account.Id;
+                    tb.Attributes.Add( "name", tb.ID );
+                    //tb.Attributes.Add( "placeholder", "$0.00" );
+                    tb.Attributes.Add( "type", "number" );
+
+                    if ( firstAccount )
+                    {
+                        tb.CssClass = "active";
+                        firstAccount = false;
+                    }
+
+                    Label label = new Label();
+                    label.AssociatedControlID = tb.ID;
+                    label.ID = "labelFund_" + account.Id;
+                    label.Text = account.PublicName;
+
+                    formGroup.Controls.Add( label );
+                    formGroup.Controls.Add( tb );
+                }
+            }
+        }
+
+        // displays giving units
+        private void BuildGivingUnitControls()
+        {           
+            // display results
+            if ( this.GivingUnits.Count > 0 )
+            {
+
+                foreach ( var unit in this.GivingUnits )
+                {
+                    LinkButton lb = new LinkButton();
+                    lb.ID = "lbUnit_" + unit.PersonAliasId.ToString();
+                    lb.CssClass = "btn btn-primary btn-kioskselect";
+                    phGivingUnits.Controls.Add( lb );
+                    lb.CommandArgument = unit.CommandArg;
+                    lb.Click += new EventHandler( unitName_Click );
+                    lb.Text = string.Format("{0} <small>{1}</small>", unit.LastName, unit.FirstNames);
+                }
+            }
+            else
+            {
+                phGivingUnits.Controls.Add( new LiteralControl(
+                    "<div class='alert alert-danger'>There were not any families found with the phone number you entered. You can add your family using the 'Register Your Family' button below.</div>" ) );
+            }
+        }
+
         // hides all panels
         private void HidePanels()
         {
             pnlSearch.Visible = false;
-            pnlFamilySelect.Visible = false;
+            pnlGivingUnitSelect.Visible = false;
             pnlRegister.Visible = false;
             pnlAccountEntry.Visible = false;
             pnlSwipe.Visible = false;
@@ -166,7 +393,34 @@ namespace RockWeb.Blocks.Finance
         }
 
         #endregion
-
-        
 }
+
+    [Serializable]
+    class GivingUnit
+    {
+        public int PersonAliasId { get; set; }
+        public string LastName { get; set; }
+        public string FirstNames { get; set; }
+
+        public string CommandArg
+        {
+            get { return string.Format( "{0}|{1}|{2}", PersonAliasId, LastName, FirstNames ); }
+        }
+
+        public GivingUnit( int personAliasId, string lastName, string firstNames )
+        {
+            PersonAliasId = personAliasId;
+            LastName = lastName;
+            FirstNames = firstNames;
+        }
+
+        public GivingUnit( string commandArg )
+        {
+            string[] parts = commandArg.Split( '|' );
+
+            PersonAliasId = Convert.ToInt32( parts[0] );
+            LastName = parts[1];
+            FirstNames = parts[2];
+        }
+    }
 }
