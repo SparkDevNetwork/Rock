b6ea7ffa572ced454b2d6b940c5997485714f6be
Mike Peterson <MikePeterson@ccvonline.com>
2015-02-18 15:33:59 -0700

    - code-gen'd Rock.Client after adding Attributes/AttributeValues

--- Rock.Client/CodeGenerated/PersonDuplicate.cs
+++ Rock.Client/CodeGenerated/PersonDuplicate.cs
@@ -79,5 +79,12 @@ namespace Rock.Client
         /// <summary />
         public string ForeignId { get; set; }
 
+        /// <summary />
+        public Dictionary<string, Rock.Client.Attribute> Attributes { get; set; }
+
+
+        /// <summary />
+        public Dictionary<string, Rock.Client.AttributeValue> AttributeValues { get; set; }
+
     }
 }
