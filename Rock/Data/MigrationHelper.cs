b582ddd8679bb2d78b6c0fad20124913449c99d9
David Turner <david@azturners.com>
2015-02-28 07:22:57 -0700

    - Add migration for attendance reminder job and system email

--- Rock/Data/MigrationHelper.cs
+++ Rock/Data/MigrationHelper.cs
@@ -765,6 +765,43 @@ namespace Rock.Data
             );
         }
 
+        public void UpdateCategoryByName( string entityTypeGuid, string name, string iconCssClass, string description, string guid, int order = 0 )
+        {
+            Migration.Sql( string.Format( @"
+                
+                DECLARE @EntityTypeId int
+                SET @EntityTypeId = (SELECT [Id] FROM [EntityType] WHERE [Guid] = '{0}')
+
+                IF EXISTS (
+                    SELECT [Id] 
+                    FROM [Category] 
+                    WHERE [EntityTypeId] = @EntityTypeId
+                    AND [Name] = '{1}' )
+                BEGIN
+                    UPDATE [Category] SET
+                        [IsSystem] = 1,
+                        [IconCssClass] = '{2}',
+                        [Description] = '{3}',
+                        [Order] = {5},
+                        [Guid] = '{4}'
+                    WHERE [EntityTypeId] = @EntityTypeId
+                    AND [Name] = '{1}'
+                END
+                ELSE
+                BEGIN
+                    INSERT INTO [Category] ( [IsSystem],[EntityTypeId],[Name],[IconCssClass],[Description],[Order],[Guid] )
+                    VALUES( 1,@EntityTypeId,'{1}','{2}','{3}',{5},'{4}' )  
+                END
+",
+                    entityTypeGuid,
+                    name,
+                    iconCssClass,
+                    description.Replace( "'", "''" ),
+                    guid,
+                    order )
+            );
+        }
+
         /// <summary>
         /// Deletes the category.
         /// </summary>
