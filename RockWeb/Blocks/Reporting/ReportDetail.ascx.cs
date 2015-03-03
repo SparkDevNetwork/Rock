f80a35b6e34d67b654dda9c857f199af01b34796
Mike Peterson <MikePeterson@ccvonline.com>
2015-03-03 10:13:32 -0700

    - added DataViewPicker (as a RockDropDownList) and updated DataView and Report UI to use it.

--- RockWeb/Blocks/Reporting/ReportDetail.ascx.cs
+++ RockWeb/Blocks/Reporting/ReportDetail.ascx.cs
@@ -546,27 +546,8 @@ namespace RockWeb.Blocks.Reporting
         /// <param name="entityTypeId">The entity type identifier.</param>
         private void LoadDropdownsForEntityType( int? entityTypeId )
         {
-            if ( entityTypeId.HasValue )
-            {
-                var rockContext = new RockContext();
-                ddlDataView.Enabled = true;
-                ddlDataView.Items.Clear();
-
-                foreach ( var dataView in new DataViewService( new RockContext() ).GetByEntityTypeId( entityTypeId.Value ).ToList() )
-                {
-                    if ( dataView.IsAuthorized( Authorization.VIEW, this.CurrentPerson, rockContext ) )
-                    {
-                        ddlDataView.Items.Add( new ListItem( dataView.Name, dataView.Id.ToString() ) );
-                    }
-                }
-
-                ddlDataView.Items.Insert( 0, new ListItem( string.Empty, "0" ) );
-            }
-            else
-            {
-                ddlDataView.Enabled = false;
-                ddlDataView.Items.Clear();
-            }
+            ddlDataView.EntityTypeId = entityTypeId;
+            ddlDataView.Enabled = entityTypeId.HasValue;
         }
 
         /// <summary>
