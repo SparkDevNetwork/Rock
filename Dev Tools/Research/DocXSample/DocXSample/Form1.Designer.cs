c4dddcfb568cbc9844180c95a11ffe3e22b5b57a
Mike Peterson <MikePeterson@ccvonline.com>
2015-03-04 17:26:55 -0700

    - temporarily add sample project for researching DocX and OpenXML Docs

---
+++ Dev Tools/Research/DocXSample/DocXSample/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace DocXSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTableDocX = new System.Windows.Forms.Button();
+            this.btnLabelsDocX = new System.Windows.Forms.Button();
+            this.btnMakeDocumentDocX = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnMakeTableOpenXML = new System.Windows.Forms.Button();
+            this.btnLabelsOpenXML = new System.Windows.Forms.Button();
+            this.btnMakeDocumentOpenXML = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTableDocX);
+            this.groupBox1.Controls.Add(this.btnLabelsDocX);
+            this.groupBox1.Controls.Add(this.btnMakeDocumentDocX);
+            this.groupBox1.Location = new System.Drawing.Point(12, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 300);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DocX";
+            // 
+            // btnTableDocX
+            // 
+            this.btnTableDocX.Location = new System.Drawing.Point(17, 87);
+            this.btnTableDocX.Name = "btnTableDocX";
+            this.btnTableDocX.Size = new System.Drawing.Size(105, 23);
+            this.btnTableDocX.TabIndex = 5;
+            this.btnTableDocX.Text = "Make Table";
+            this.btnTableDocX.UseVisualStyleBackColor = true;
+            this.btnTableDocX.Click += new System.EventHandler(this.btnTable_Click);
+            // 
+            // btnLabelsDocX
+            // 
+            this.btnLabelsDocX.Location = new System.Drawing.Point(17, 58);
+            this.btnLabelsDocX.Name = "btnLabelsDocX";
+            this.btnLabelsDocX.Size = new System.Drawing.Size(105, 23);
+            this.btnLabelsDocX.TabIndex = 4;
+            this.btnLabelsDocX.Text = "Make Labels";
+            this.btnLabelsDocX.UseVisualStyleBackColor = true;
+            this.btnLabelsDocX.Click += new System.EventHandler(this.btnLabels_Click);
+            // 
+            // btnMakeDocumentDocX
+            // 
+            this.btnMakeDocumentDocX.Location = new System.Drawing.Point(17, 29);
+            this.btnMakeDocumentDocX.Name = "btnMakeDocumentDocX";
+            this.btnMakeDocumentDocX.Size = new System.Drawing.Size(105, 23);
+            this.btnMakeDocumentDocX.TabIndex = 3;
+            this.btnMakeDocumentDocX.Text = "Make Document";
+            this.btnMakeDocumentDocX.UseVisualStyleBackColor = true;
+            this.btnMakeDocumentDocX.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnMakeTableOpenXML);
+            this.groupBox2.Controls.Add(this.btnLabelsOpenXML);
+            this.groupBox2.Controls.Add(this.btnMakeDocumentOpenXML);
+            this.groupBox2.Location = new System.Drawing.Point(248, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(214, 300);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "OpenXML";
+            // 
+            // btnMakeTableOpenXML
+            // 
+            this.btnMakeTableOpenXML.Location = new System.Drawing.Point(17, 87);
+            this.btnMakeTableOpenXML.Name = "btnMakeTableOpenXML";
+            this.btnMakeTableOpenXML.Size = new System.Drawing.Size(105, 23);
+            this.btnMakeTableOpenXML.TabIndex = 5;
+            this.btnMakeTableOpenXML.Text = "Make Table";
+            this.btnMakeTableOpenXML.UseVisualStyleBackColor = true;
+            this.btnMakeTableOpenXML.Click += new System.EventHandler(this.btnMakeTableOpenXML_Click);
+            // 
+            // btnLabelsOpenXML
+            // 
+            this.btnLabelsOpenXML.Location = new System.Drawing.Point(17, 58);
+            this.btnLabelsOpenXML.Name = "btnLabelsOpenXML";
+            this.btnLabelsOpenXML.Size = new System.Drawing.Size(105, 23);
+            this.btnLabelsOpenXML.TabIndex = 4;
+            this.btnLabelsOpenXML.Text = "Make Labels";
+            this.btnLabelsOpenXML.UseVisualStyleBackColor = true;
+            this.btnLabelsOpenXML.Click += new System.EventHandler(this.btnLabelsOpenXML_Click);
+            // 
+            // btnMakeDocumentOpenXML
+            // 
+            this.btnMakeDocumentOpenXML.Location = new System.Drawing.Point(17, 29);
+            this.btnMakeDocumentOpenXML.Name = "btnMakeDocumentOpenXML";
+            this.btnMakeDocumentOpenXML.Size = new System.Drawing.Size(105, 23);
+            this.btnMakeDocumentOpenXML.TabIndex = 3;
+            this.btnMakeDocumentOpenXML.Text = "Make Document";
+            this.btnMakeDocumentOpenXML.UseVisualStyleBackColor = true;
+            this.btnMakeDocumentOpenXML.Click += new System.EventHandler(this.btnMakeDocumentOpenXML_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 327);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnTableDocX;
+        private System.Windows.Forms.Button btnLabelsDocX;
+        private System.Windows.Forms.Button btnMakeDocumentDocX;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnMakeTableOpenXML;
+        private System.Windows.Forms.Button btnLabelsOpenXML;
+        private System.Windows.Forms.Button btnMakeDocumentOpenXML;
+    }
+}
+
---
+++ Dev Tools/Research/DocXSample/DocXSample/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Novacode;
+using OpenXmlPowerTools;
+
+namespace DocXSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // docs
+        private string[] relations = { "son", "daughter", "uncle", "aunt" };
+        private string[] seasons = { "spring", "summer", "fall", "winter is a great season that lasts a long time in many areas of the world, especially in the north" };
+
+        // labels
+        string[] companies = { "Time Warner", "Apple", "IBM", "CCV", "Honeywell", "Amex" };
+        string[] addresses = { "123 W Elm St", "352 Monroe Blvd", "2321 W Washington Ave", "1231 24th St", "3426 E Warner Rd", "211 Peterson St" };
+        string[] cities = { "Phoenix", "Glendale", "Mesa", "Scottsdale", "Peoria", "Chandler" };
+        string[] states = { "AZ", "AZ", "AZ", "AZ", "AZ", "AZ" };
+        string[] zips = { "85383", "85697", "85452", "85452", "85741", "85732" };
+
+        /// <summary>
+        /// Merges the Letter Template using DOCX
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnGo_Click( object sender, EventArgs e )
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            // Create a new document using DocX Library
+            using ( DocX document = DocX.Create( path + @"\LetterOut.docx" ) )
+            {
+
+                for ( int i = 0; i < 4; i++ )
+                {
+                    DocX template = DocX.Load( path + @"\letter-template - insane formatting2.docx" );
+                    template.ReplaceText( "<<Relation>>", relations[i], false );
+                    template.ReplaceText( "<<Season>>", seasons[i], false );
+                    document.InsertDocument( template );
+
+                    // insert page break
+                    if ( i != 3 )
+                    {
+                        Novacode.Paragraph p1 = document.InsertParagraph( "", false );
+                        p1.InsertPageBreakAfterSelf();
+                    }
+
+                    template.Dispose();
+                }
+
+                document.Save();
+            }
+        }
+
+
+
+        private void btnLabels_Click( object sender, EventArgs e )
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            int companyCount = 0;
+
+            // Create a new document.
+            using ( DocX document = DocX.Create( path + @"\LabelsOut.docx" ) )
+            {
+
+                DocX template = DocX.Load( path + @"\label-template.docx" );
+
+                foreach ( Novacode.Table t in template.Tables )
+                {
+                    foreach ( Row r in t.Rows )
+                    {
+                        foreach ( Cell c in r.Cells )
+                        {
+                            if ( companyCount == ( companies.Length - 1 ) )
+                            {
+                                // out of data so hide cell contents
+                                foreach ( Novacode.Paragraph p in c.Paragraphs )
+                                {
+                                    p.Hide();
+                                }
+                            }
+                            else
+                            {
+                                // make sure cell has merge fields
+                                List<int> fields = c.FindAll( "{{" );
+                                if ( fields.Count > 0 )
+                                {
+                                    c.ReplaceText( "{{CompanyName}}", companies[companyCount] );
+                                    c.ReplaceText( "{{Address}}", addresses[companyCount] );
+                                    c.ReplaceText( "{{City}}", cities[companyCount] );
+                                    c.ReplaceText( "{{State}}", states[companyCount] );
+                                    c.ReplaceText( "{{ZipCode}}", zips[companyCount] );
+
+                                    companyCount++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                document.InsertDocument( template );
+                template.Dispose();
+                document.Save();
+            }
+        }
+
+        private void btnTable_Click( object sender, EventArgs e )
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            string[] names = { "Tom Selleck", "Tom Cruise", "Jon Edmiston" };
+            string[] emails = { "ts@higgins.com", "tom@topgun.com", "jonathan.edmiston@gmail.com" };
+            string[] coolness = { "Pretty Cool", "Kinda Cool", "Exteremely Cool" };
+
+            // Create a new document.
+            using ( DocX document = DocX.Create( path + @"\TableOut.docx" ) )
+            {
+                DocX template = DocX.Load( path + @"\table-template.docx" );
+
+                /*foreach (Table t in template.Tables)
+                {
+                    
+                    for (int r = 0; r < 3; r++)
+                    {
+                        // check if this is a template row
+                        List<int> commands = t.Rows[r].Cells[0].FindAll("{%tr%}", System.Text.RegularExpressions.RegexOptions.IgnorePatternWhitespace);
+                        if (commands.Count > 0)
+                        {
+                            // we have a command row
+
+                            // remove command
+                            t.Rows[r].ReplaceText("{%tr%}", "");
+
+                            for (int i = 0; i < emails.Count(); i++)
+                            {
+                                
+                                
+                                //Row newRow =  t.InsertRow(r + 1);
+                                //newRow.Xml = t.Rows[r].Xml;
+                                //newRow.ReplaceText("{{Name}}", names[i]);
+                                //t.InsertRow(newRow, r);
+                            }
+
+                            
+                            
+                        }
+                    }
+                }*/
+
+                document.InsertDocument( template );
+
+
+                template.Dispose();
+                document.Save();
+            }
+        }
+
+        /*
+         
+         Using OPEN XML 
+         
+         */
+
+        /// <summary>
+        /// Merges the Letter Template using OpenXML
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnMakeDocumentOpenXML_Click( object sender, EventArgs e )
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            MemoryStream outputDocStream = new MemoryStream();
+
+            using ( var letterTemplateStream = new FileStream( path + @"\letter-template - insane formatting3.docx", FileMode.Open, FileAccess.ReadWrite ) )
+            {
+                // Start by creating a new document with the contents of the Template (so that Styles, etc get included)
+                letterTemplateStream.CopyTo( outputDocStream );
+                outputDocStream.Seek( 0, SeekOrigin.Begin );
+
+                using ( WordprocessingDocument outputDoc = WordprocessingDocument.Open( outputDocStream, true ) )
+                {
+                    var newDocBody = outputDoc.MainDocumentPart.Document.Body;
+
+                    // start with a clean body
+                    newDocBody.RemoveAllChildren();
+
+                    // loop thru each merge item, using the template
+                    for ( int i = 0; i < 4; i++ )
+                    {
+                        // create our replacement dictionary
+                        Dictionary<string, string> replacements = new Dictionary<string, string>();
+                        replacements.Add( "<<Relation>>", relations[i] );
+                        replacements.Add( "<<Season>>", seasons[i] );
+                        replacements.Add( @"<<LAVA>>.<<LAVA/>>", @"block of lava" );
+                        replacements.Add( @".*\{\{.+\}\}.*", @"chunk of lava" );
+
+                        var tempMergeDocStream = new MemoryStream();
+                        letterTemplateStream.Position = 0;
+                        letterTemplateStream.CopyTo( tempMergeDocStream );
+                        tempMergeDocStream.Position = 0;
+                        var tempMergeDoc = WordprocessingDocument.Open( tempMergeDocStream, true );
+
+                        var xdoc = tempMergeDoc.MainDocumentPart.GetXDocument();
+                        foreach ( var r in replacements )
+                        {
+                            OpenXmlRegex.Match( xdoc.Nodes().OfType<XElement>(), new Regex( r.Key, RegexOptions.Multiline ), ( x, m ) =>
+                            {
+                                string todo = "hello";
+                            } );
+
+                            OpenXmlRegex.Replace( xdoc.Nodes().OfType<XElement>(), new Regex( r.Key ), r.Value, ( x, m ) =>
+                                            {
+                                                return true;
+                                            } );
+
+                        }
+
+                        tempMergeDoc.MainDocumentPart.PutXDocument();
+
+                        foreach ( var childBodyItem in tempMergeDoc.MainDocumentPart.Document.Body )
+                        {
+                            var clonedChild = childBodyItem.CloneNode( true );
+                            newDocBody.AppendChild( clonedChild );
+                        }
+
+                        // add page break
+                        newDocBody.AppendChild( new DocumentFormat.OpenXml.Wordprocessing.Break() { Type = BreakValues.Page } );
+                    }
+
+                    // remove last page break
+                    var lastBr = newDocBody.LastChild as DocumentFormat.OpenXml.Wordprocessing.Break;
+                    if ( lastBr != null )
+                    {
+                        newDocBody.RemoveChild( lastBr );
+                    }
+                }
+            }
+
+            // Save to disk
+            File.WriteAllBytes( path + @"\LetterOut_OpenXML.docx", outputDocStream.ToArray() );
+        }
+
+        /// <summary>
+        /// Make Labels using OpenXML
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnLabelsOpenXML_Click( object sender, EventArgs e )
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            // see HowTos at https://msdn.microsoft.com/EN-US/library/office/cc850849.aspx
+
+            // Start by creating a new document with the contents of the Template (so that Styles, etc get included)
+            MemoryStream outputDocStream = new MemoryStream();
+            var letterTemplateStream = File.OpenRead( path + @"\label-template.docx" );
+            letterTemplateStream.CopyTo( outputDocStream );
+            outputDocStream.Seek( 0, SeekOrigin.Begin );
+
+            int companyCount = 0;
+
+            using ( WordprocessingDocument outputDoc = WordprocessingDocument.Open( outputDocStream, true ) )
+            {
+                var newDocBody = outputDoc.MainDocumentPart.Document.Body;
+                var tables = newDocBody.ChildElements.OfType<DocumentFormat.OpenXml.Wordprocessing.Table>();
+
+                foreach ( var table in tables )
+                {
+                    foreach ( var row in table.ChildElements.OfType<TableRow>() )
+                    {
+                        foreach ( var cell in row.ChildElements.OfType<TableCell>().Where( a => a.InnerText.Contains( "{{" ) ).ToList() )
+                        {
+                            if ( companyCount >= ( companies.Length - 1 ) )
+                            {
+                                // out of data so hide cell contents
+                                var emptyCell = new TableCell( new DocumentFormat.OpenXml.Wordprocessing.Paragraph[] { new DocumentFormat.OpenXml.Wordprocessing.Paragraph() } );
+                                cell.Parent.ReplaceChild( emptyCell, cell );
+                            }
+                            else
+                            {
+                                XElement[] xe = new XElement[] { XElement.Parse( cell.OuterXml ) };
+
+                                Dictionary<string, string> replacements = new Dictionary<string, string>();
+
+                                replacements.Add( "{{CompanyName}}", companies[companyCount] );
+                                replacements.Add( "{{Address}}", addresses[companyCount] );
+                                replacements.Add( "{{City}}", cities[companyCount] );
+                                replacements.Add( "{{State}}", states[companyCount] );
+                                replacements.Add( "{{ZipCode}}", zips[companyCount] );
+
+                                foreach ( var r in replacements )
+                                {
+
+                                    OpenXmlRegex.Replace( xe, new Regex( r.Key ), r.Value, ( x, m ) =>
+                                    {
+                                        return true;
+                                    } );
+                                }
+
+                                XDocument xdoc = XDocument.Parse( cell.OuterXml );
+
+                                var newCell = new TableCell( xe[0].ToString() );
+
+                                cell.Parent.ReplaceChild( newCell, cell );
+
+                                companyCount++;
+                            }
+
+
+                        }
+                    }
+                }
+            }
+
+            // Save to disk
+            File.WriteAllBytes( path + @"\LabelOut_OpenXML.docx", outputDocStream.ToArray() );
+        }
+
+        private void btnMakeTableOpenXML_Click( object sender, EventArgs e )
+        {
+            //
+        }
+    }
+}
+
+
