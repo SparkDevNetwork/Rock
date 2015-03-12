f14e828cfda7397e6c47cf8f12370525b5d3571c
Nick Sheck <nsheck@gmail.com>
2015-03-12 15:50:14 -0700

    Campus modal campus selection

--- RockWeb/Themes/church_ccv_External_v7/Scripts/map-campus-modal.js
+++ RockWeb/Themes/church_ccv_External_v7/Scripts/map-campus-modal.js
@@ -16,6 +16,18 @@
 
 var campusModalMap = new CCV.infoWindowMap(document.getElementById('campusmodal-holder'))
 
+campusModalMap.selectCampus = function (campusId) {
+  Rock.utility.setContext('campuses', campusId)
+
+  // wait until API adds the cookie
+  $(document).ajaxComplete(function( event, xhr, settings ) {
+    if (settings.url.indexOf('/api/campuses/SetContext/') > -1) {
+      // reloads page from cache
+      location.reload();
+    }
+  })
+}
+
 campusModalMap.draw()
 
 $('document').ready(function(){
