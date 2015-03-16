1553afadc98a806668fbd583a981c13ee977de30
Nick Sheck <nsheck@gmail.com>
2015-03-16 16:57:25 -0700

    Load google maps API dynamically for campus modal

--- RockWeb/Themes/church_ccv_External_v7/Scripts/map-campus-modal.js
+++ RockWeb/Themes/church_ccv_External_v7/Scripts/map-campus-modal.js
@@ -10,26 +10,34 @@
 
 // @prepros-prepend "_map.js"
 
+// Load the Google maps api dynamically to avoid errors when double loading
+// The URL string is from https://github.com/SparkDevNetwork/Rock/blob/582e01a72f41ab33ad41335c01c365523b35be5b/Rock/Web/UI/RockPage.cs#L1045
+Rock.controls.util.loadGoogleMapsApi('https://maps.googleapis.com/maps/api/js?sensor=false&libraries=drawing')
+
 
 // Map Object
 // -------------------------
 
-var campusModalMap = new CCV.infoWindowMap(document.getElementById('campusmodal-holder'))
+$(window).on('googleMapsIsLoaded', function(){
 
-campusModalMap.selectCampus = function (campusId) {
-  Rock.utility.setContext('campuses', campusId)
+  window.campusModalMap = new CCV.infoWindowMap(document.getElementById('campusmodal-holder'))
 
-  // wait until API adds the cookie
-  $(document).ajaxComplete(function( event, xhr, settings ) {
-    if (settings.url.indexOf('/api/campuses/SetContext/') > -1) {
-      // reloads page from cache
-      location.reload();
-    }
+  campusModalMap.selectCampus = function (campusId) {
+    Rock.utility.setContext('campuses', campusId)
+
+    // wait until API adds the cookie
+    $(document).ajaxComplete(function( event, xhr, settings ) {
+      if (settings.url.indexOf('/api/campuses/SetContext/') > -1) {
+        // reloads page from cache
+        location.reload();
+      }
+    })
+  }
+
+  campusModalMap.draw()
+
+  $('document').ready(function(){
+    $('body').addClass('modal-open')
   })
-}
 
-campusModalMap.draw()
-
-$('document').ready(function(){
-  $('body').addClass('modal-open')
 })
