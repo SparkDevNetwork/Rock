41605a013f81624e8a78f14a3874fb55fde6b34f
Mike Peterson <MikePeterson@ccvonline.com>
2015-03-26 13:09:35 -0700

    - FormatTSQL on ufnCrm_GetFamilyTitle.sql

--- database/Functions/ufnCrm_GetFamilyTitle.sql
+++ database/Functions/ufnCrm_GetFamilyTitle.sql
@@ -24,108 +24,103 @@
 	</code>
 </doc>
 */
-ALTER FUNCTION [dbo].[ufnCrm_GetFamilyTitle] 
-( 
-    @PersonId int
-    , @GroupId int
-    , @GroupPersonIds varchar(max) = null
-)
-RETURNS @PersonNamesTable TABLE ( PersonNames varchar(max))
+ALTER FUNCTION [dbo].[ufnCrm_GetFamilyTitle] (
+    @PersonId INT
+    ,@GroupId INT
+    ,@GroupPersonIds VARCHAR(max) = NULL
+    )
+RETURNS @PersonNamesTable TABLE (PersonNames VARCHAR(max))
 AS
 BEGIN
-    DECLARE @PersonNames varchar(max); 
-    DECLARE @AdultLastNameCount int;
-    DECLARE @GroupFirstNames varchar(max) = '';
-    DECLARE @GroupLastName varchar(max);
-    DECLARE @GroupAdultFullNames varchar(max) = '';
-    DECLARE @GroupNonAdultFullNames varchar(max) = '';
-    DECLARE @GroupMemberTable table (LastName varchar(max), FirstName varchar(max), FullName varchar(max), Gender int, GroupRoleGuid uniqueidentifier );
-    DECLARE @cGROUPTYPEROLE_FAMILY_MEMBER_ADULT uniqueidentifier = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42';
+    DECLARE @PersonNames VARCHAR(max);
+    DECLARE @AdultLastNameCount INT;
+    DECLARE @GroupFirstNames VARCHAR(max) = '';
+    DECLARE @GroupLastName VARCHAR(max);
+    DECLARE @GroupAdultFullNames VARCHAR(max) = '';
+    DECLARE @GroupNonAdultFullNames VARCHAR(max) = '';
+    DECLARE @GroupMemberTable TABLE (
+        LastName VARCHAR(max)
+        ,FirstName VARCHAR(max)
+        ,FullName VARCHAR(max)
+        ,Gender INT
+        ,GroupRoleGuid UNIQUEIDENTIFIER
+        );
+    DECLARE @cGROUPTYPEROLE_FAMILY_MEMBER_ADULT UNIQUEIDENTIFIER = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42';
 
-    IF (@PersonId is not null) 
+    IF (@PersonId IS NOT NULL)
     BEGIN
         -- just getting the Person Names portion of the address for an individual person
-        SELECT 
-            @PersonNames = ISNULL([p].[NickName],'') + ' ' + ISNULL([p].[LastName],'') + ' ' + ISNULL([dv].[Value], '')
-        FROM
-            [Person] [p]
-        LEFT OUTER JOIN 
-            [DefinedValue] [dv] ON [dv].[Id] = [p].[SuffixValueId]
-        WHERE 
-            [p].[Id] = @PersonId;
+        SELECT @PersonNames = ISNULL([p].[NickName], '') + ' ' + ISNULL([p].[LastName], '') + ' ' + ISNULL([dv].[Value], '')
+        FROM [Person] [p]
+        LEFT OUTER JOIN [DefinedValue] [dv] ON [dv].[Id] = [p].[SuffixValueId]
+        WHERE [p].[Id] = @PersonId;
     END
     ELSE
     BEGIN
         -- populate a table variable with the data we'll need for the different cases
         -- if GroupPersonIds is set (comma-delimited) only a subset of the family members should be combined
-        INSERT INTO @GroupMemberTable 
-        SELECT 
-            [p].[LastName] 
-            , [p].[FirstName] 
-            , ISNULL([p].[NickName],'') + ' ' + ISNULL([p].[LastName],'') + ' ' + ISNULL([dv].[Value], '') as [FullName] 
-            , [p].Gender
-            , [gr].[Guid]
-        FROM 
-            [GroupMember] [gm] 
-        JOIN 
-            [Person] [p] 
-        ON 
-            [p].[Id] = [gm].[PersonId] 
-        LEFT OUTER JOIN 
-            [DefinedValue] [dv] ON [dv].[Id] = [p].[SuffixValueId]
-        JOIN 
-            [GroupTypeRole] [gr] ON [gm].[GroupRoleId] = [gr].[Id]
-        WHERE 
-            [GroupId] = @GroupId
-        AND 
-            (ISNULL(@GroupPersonIds, '') = '' OR (p.[Id] in (select * from ufnUtility_CsvToTable(@GroupPersonIds))) )
+        INSERT INTO @GroupMemberTable
+        SELECT [p].[LastName]
+            ,[p].[FirstName]
+            ,ISNULL([p].[NickName], '') + ' ' + ISNULL([p].[LastName], '') + ' ' + ISNULL([dv].[Value], '') AS [FullName]
+            ,[p].Gender
+            ,[gr].[Guid]
+        FROM [GroupMember] [gm]
+        JOIN [Person] [p] ON [p].[Id] = [gm].[PersonId]
+        LEFT OUTER JOIN [DefinedValue] [dv] ON [dv].[Id] = [p].[SuffixValueId]
+        JOIN [GroupTypeRole] [gr] ON [gm].[GroupRoleId] = [gr].[Id]
+        WHERE [GroupId] = @GroupId
+            AND (
+                ISNULL(@GroupPersonIds, '') = ''
+                OR (
+                    p.[Id] IN (
+                        SELECT *
+                        FROM ufnUtility_CsvToTable(@GroupPersonIds)
+                        )
+                    )
+                )
 
-        
         -- determine adultCount and if we can use the same lastname for all adults, and get lastname while we are at it
-        SELECT 
-            @AdultLastNameCount = count(distinct [LastName])
-            , @GroupLastName = max([LastName])
-        FROM 
-            @GroupMemberTable
-        WHERE
-            [GroupRoleGuid] = @cGROUPTYPEROLE_FAMILY_MEMBER_ADULT;  
+        SELECT @AdultLastNameCount = count(DISTINCT [LastName])
+            ,@GroupLastName = max([LastName])
+        FROM @GroupMemberTable
+        WHERE [GroupRoleGuid] = @cGROUPTYPEROLE_FAMILY_MEMBER_ADULT;
 
-        IF @AdultLastNameCount > 0 
+        IF @AdultLastNameCount > 0
         BEGIN
             -- get the FirstNames and Adult FullNames for use in the cases of families with Adults
-            SELECT 
-                @GroupFirstNames = @GroupFirstNames + [FirstName] + ' & '
-                , @GroupAdultFullNames = @GroupAdultFullNames + [FullName] + ' & '
-            FROM      
-                @GroupMemberTable g
-            WHERE
-                g.[GroupRoleGuid] = @cGROUPTYPEROLE_FAMILY_MEMBER_ADULT
-            ORDER BY g.Gender, g.FirstName
+            SELECT @GroupFirstNames = @GroupFirstNames + [FirstName] + ' & '
+                ,@GroupAdultFullNames = @GroupAdultFullNames + [FullName] + ' & '
+            FROM @GroupMemberTable g
+            WHERE g.[GroupRoleGuid] = @cGROUPTYPEROLE_FAMILY_MEMBER_ADULT
+            ORDER BY g.Gender
+                ,g.FirstName
 
             -- cleanup the trailing ' &'s
-            IF len(@GroupFirstNames) > 2 BEGIN
+            IF len(@GroupFirstNames) > 2
+            BEGIN
                 -- trim the extra ' &' off the end 
                 SET @GroupFirstNames = SUBSTRING(@GroupFirstNames, 0, len(@GroupFirstNames) - 1)
-            END 
-
-            IF len(@GroupAdultFullNames) > 2 BEGIN
-                -- trim the extra ' &' off the end 
-                SET @GroupAdultFullNames = SUBSTRING(@GroupAdultFullNames, 0, len(@GroupAdultFullNames) - 1)  
             END
-        END             
 
-        IF @AdultLastNameCount = 0        
+            IF len(@GroupAdultFullNames) > 2
+            BEGIN
+                -- trim the extra ' &' off the end 
+                SET @GroupAdultFullNames = SUBSTRING(@GroupAdultFullNames, 0, len(@GroupAdultFullNames) - 1)
+            END
+        END
+
+        IF @AdultLastNameCount = 0
         BEGIN
             -- get the NonAdultFullNames for use in the case of families without adults 
-            SELECT 
-                @GroupNonAdultFullNames = @groupNonAdultFullNames + [FullName] + ' & '
-            FROM 
-                @GroupMemberTable
+            SELECT @GroupNonAdultFullNames = @groupNonAdultFullNames + [FullName] + ' & '
+            FROM @GroupMemberTable
             ORDER BY [FullName]
 
-            IF len(@GroupNonAdultFullNames) > 2 BEGIN
+            IF len(@GroupNonAdultFullNames) > 2
+            BEGIN
                 -- trim the extra ' &' off the end 
-                SET @GroupNonAdultFullNames = SUBSTRING(@GroupNonAdultFullNames, 0, len(@GroupNonAdultFullNames) - 1)  
+                SET @GroupNonAdultFullNames = SUBSTRING(@GroupNonAdultFullNames, 0, len(@GroupNonAdultFullNames) - 1)
             END
         END
 
@@ -136,17 +131,18 @@ BEGIN
         END
         ELSE IF (@AdultLastNameCount = 0)
         BEGIN
-             -- no adults in family, list all members of the family in 'Fullname & FullName & ...' format
+            -- no adults in family, list all members of the family in 'Fullname & FullName & ...' format
             SET @PersonNames = @GroupNonAdultFullNames;
         END
         ELSE
         BEGIN
             -- multiple adult lastnames
             SET @PersonNames = @GroupAdultFullNames;
-        END 
+        END
     END
 
-    INSERT INTO @PersonNamesTable ( [PersonNames] ) VALUES ( @PersonNames);
+    INSERT INTO @PersonNamesTable ([PersonNames])
+    VALUES (@PersonNames);
 
-  RETURN
+    RETURN
 END
