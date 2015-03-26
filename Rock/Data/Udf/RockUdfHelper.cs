38efe3e6088971fd100756f63451064d0d585324
Mike Peterson <MikePeterson@ccvonline.com>
2015-03-26 15:48:12 -0700

    - fixed up RockUdfHelper.ufnCrm_GetFamilyTitle to match updated ufnCrm_GetFamilyTitle database function

--- Rock/Data/Udf/RockUdfHelper.cs
+++ Rock/Data/Udf/RockUdfHelper.cs
@@ -14,7 +14,7 @@
 // limitations under the License.
 // </copyright>
 //
-using System.Collections.Generic;
+using System;
 using System.Data;
 using System.Data.Entity;
 using System.Data.SqlClient;
@@ -30,11 +30,12 @@ namespace Rock.Data
     public static class RockUdfHelper
     {
         /// <summary>
-        /// calls database function ufnCrm_GetAddress 
+        /// when used in a Linq Query, calls database function ufnCrm_GetAddress 
+        /// Example: qry.Select( a => RockUdfHelper.ufnCrm_GetAddress(a.PersonId, "Home", "Full"))
         /// </summary>
         /// <param name="PersonId">The person identifier.</param>
         /// <param name="AddressType">Type of the address.  Can either be text "Home", "Work", "Previous", or the Id of the GroupLocationTypeValue </param>
-        /// <param name="AddressComponent">The address component  Can be 'Street1', 'Street2', 'City', 'State', 'PostalCode', 'Country'.</param>
+        /// <param name="AddressComponent">The address component  Can be 'Full','Street1', 'Street2', 'City', 'State', 'PostalCode', 'Country'.</param>
         /// <returns></returns>
         [DbFunction( "CodeFirstDatabaseSchema", "ufnCrm_GetAddress" )]
         public static string ufnCrm_GetAddress( int? PersonId, string AddressType, string AddressComponent )
@@ -44,22 +45,27 @@ namespace Rock.Data
         }
 
         /// <summary>
-        /// calls database function ufnCrm_GetFamilyTitle.  
+        /// calls database TVF function ufnCrm_GetFamilyTitle.
+        /// Usage: string familyTitle = RockUdfHelper.ufnCrm_GetFamilyTitle( rockContext, personId, groupId, commaPersonIds, true );
         /// </summary>
+        /// <param name="rockContext">The rock context.</param>
         /// <param name="PersonId">The PersonId. NULL means use GroupId parameter</param>
         /// <param name="GroupId">The GroupId of the Family. NULL means use PersonId parameter</param>
-        /// <param name="GroupPersonIds">If GroupId is specified, set this as a comma-delimited list of PersonIds that you want to limit the family members to. NULL means don't restrict. </param>
+        /// <param name="GroupPersonIds">If GroupId is specified, set this as a comma-delimited list of PersonIds that you want to limit the family members to. NULL means don't restrict.</param>
+        /// <param name="UseNickName">if set to <c>true</c> [use nick name].</param>
         /// <returns></returns>
-        public static string ufnCrm_GetFamilyTitle( RockContext rockContext, int? PersonId, int? GroupId, string GroupPersonIds )
+        public static string ufnCrm_GetFamilyTitle( RockContext rockContext, int? PersonId, int? GroupId, string GroupPersonIds, bool UseNickName )
         {
             var result = rockContext.Database.SqlQuery(
                 typeof( string ),
-                "SELECT TOP 1 [PersonNames] FROM dbo.ufnCrm_GetFamilyTitle(null, @GroupId, @GroupPersonIds)",
-                //new SqlParameter( "@PersonId", PersonId ) { SqlDbType = SqlDbType.Int, IsNullable = true },
-                new SqlParameter( "@GroupId", GroupId ) { SqlDbType = SqlDbType.Int, IsNullable = true },
-                new SqlParameter( "@GroupPersonIds", GroupPersonIds )
-                ).OfType<string>().FirstOrDefault();
-            return result;
+                "SELECT TOP 1 [PersonNames] FROM dbo.ufnCrm_GetFamilyTitle(@PersonId, @GroupId, @GroupPersonIds, @UseNickName)",
+                new SqlParameter( "@PersonId", PersonId.HasValue ? (object)PersonId.Value : DBNull.Value ) { SqlDbType = SqlDbType.Int, IsNullable = true },
+                new SqlParameter( "@GroupId", GroupId.HasValue ? (object)GroupId.Value : DBNull.Value ) { SqlDbType = SqlDbType.Int, IsNullable = true },
+                new SqlParameter( "@GroupPersonIds", string.IsNullOrWhiteSpace( GroupPersonIds ) ? DBNull.Value : (object)GroupPersonIds ) { SqlDbType = SqlDbType.Text, IsNullable = true },
+                new SqlParameter( "@UseNickName", UseNickName ));
+
+            // NOTE: ufnCrm_GetFamilyTitle is a Table Valued Function, but it only returns one ROW
+            return result.OfType<string>().FirstOrDefault();
         }
     }
 }
