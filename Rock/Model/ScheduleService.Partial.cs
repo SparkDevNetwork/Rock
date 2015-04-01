ca7f5edac99f2dd7a922e1a9d55a813558ac3d3a
David Turner <david@azturners.com>
2015-04-01 04:31:20 -0700

    + Updated group attendance list/detail blocks to support groups with multiple locations and schedules (i.e. groups used for check-in)

--- Rock/Model/ScheduleService.Partial.cs
+++ Rock/Model/ScheduleService.Partial.cs
@@ -33,137 +33,210 @@ namespace Rock.Model
         /// Gets occurrence data for the selected group
         /// </summary>
         /// <param name="group">The group.</param>
+        /// <param name="dateTime">The date time.</param>
+        /// <param name="scheduleId">The schedule identifier. Use a value of 0 to limit occurrences to those without a schedule ( a null value will not filter by schedule ).</param>
+        /// <param name="locationId">The location identifier. Use a value of 0 to limit occurrences to those without a location ( a null value will not filter by location ).</param>
         /// <param name="loadAttendanceData">if set to <c>true</c> [load attendance data].</param>
         /// <returns></returns>
-        public List<ScheduleOccurrence> GetGroupOccurrences( Group group, bool loadAttendanceData = true )
+        public List<ScheduleOccurrence> GetGroupOccurrences( Group group, DateTime? fromDateTime = null, DateTime? toDateTime = null, 
+            int? locationId = null, int? scheduleId = null, bool loadAttendanceData = true )
         {
             var occurrences = new List<ScheduleOccurrence>();
 
             if ( group != null )
             {
                 var rockContext = (RockContext)this.Context;
-
                 var attendanceService = new AttendanceService( rockContext );
+                var scheduleService = new ScheduleService( rockContext );
 
-                var uniqueStartDates = attendanceService
+                // Get existing 'occurrences'
+                var qry = attendanceService
                     .Queryable().AsNoTracking()
-                    .Where( a => a.GroupId == group.Id )
-                    .Select( a => a.StartDateTime )
-                    .Distinct()
-                    .ToList()
-                    .Select( dt => dt.Date )
-                    .Distinct()
-                    .ToList();
+                    .Where( a => a.GroupId == group.Id );
 
-                Schedule schedule = null;
-                if ( group.ScheduleId.HasValue )
+                if ( fromDateTime.HasValue )
                 {
-                    schedule = group.Schedule;
-                    if ( schedule == null )
+                    var fromDate = fromDateTime.Value.Date;
+                    qry = qry.Where( a => DbFunctions.TruncateTime( a.StartDateTime ) >= ( fromDate ) );
+                }
+
+                if ( toDateTime.HasValue )
+                {
+                    var toDate = toDateTime.Value.Date;
+                    qry = qry.Where( a => DbFunctions.TruncateTime( a.StartDateTime ) < ( toDate ) );
+                }
+
+                if ( locationId.HasValue )
+                {
+                    qry = qry.Where( a =>
+                        ( a.LocationId.HasValue && a.LocationId.Value == locationId.Value ) ||
+                        ( !a.LocationId.HasValue && locationId.Value == 0 ) );
+                }
+
+                if ( scheduleId.HasValue )
+                {
+                    qry = qry.Where( a =>
+                        ( a.ScheduleId.HasValue && a.ScheduleId.Value == scheduleId.Value ) ||
+                        ( !a.ScheduleId.HasValue && scheduleId.Value == 0 ) );
+                }
+
+                foreach ( var occurrence in qry
+                    .Select( a => new
                     {
-                        schedule = new ScheduleService( rockContext ).Get( group.ScheduleId.Value );
+                        a.LocationId,
+                        LocationName = a.Location != null ? a.Location.Name : "",
+                        a.ScheduleId,
+                        ScheduleName = a.Schedule != null ? a.Schedule.Name : "",
+                        Date = DbFunctions.TruncateTime( a.StartDateTime )
+                    } )
+                    .Distinct()
+                    .ToList() )
+                {
+                    if ( occurrence.Date.HasValue )
+                    {
+                        occurrences.Add(
+                            new ScheduleOccurrence(
+                                occurrence.Date.Value,
+                                occurrence.Date.Value.AddDays( 1 ),
+                                occurrence.ScheduleId,
+                                occurrence.ScheduleName,
+                                occurrence.LocationId, 
+                                occurrence.LocationName ) );
                     }
                 }
 
-                if ( schedule != null )
+                // Load the attendance data for each occurrence
+                if ( loadAttendanceData && occurrences.Any())
                 {
-                    var endDate = RockDateTime.Today.AddDays( 1 );
+                    var minStartValue = occurrences.Min( o => o.StartDateTime );
+                    var maxEndValue = occurrences.Max( o => o.EndDateTime );
 
-                    DDay.iCal.Event calEvent = schedule.GetCalenderEvent();
+                    var attendanceQry = attendanceService
+                        .Queryable( "PersonAlias" ).AsNoTracking()
+                        .Where( a =>
+                            a.GroupId == group.Id &&
+                            a.StartDateTime >= minStartValue &&
+                            a.StartDateTime < maxEndValue );
+
+                    var attendanceData = attendanceQry.ToList();
+
+                    foreach ( var occurrence in occurrences )
+                    {
+                        occurrence.Attendance = attendanceData
+                            .Where( a =>
+                                a.StartDateTime >= occurrence.StartDateTime &&
+                                a.StartDateTime < occurrence.EndDateTime )
+                            .ToList()
+                            .Where( a => 
+                                a.LocationId.Equals( occurrence.LocationId ) &&
+                                a.ScheduleId.Equals( occurrence.ScheduleId ) )
+                            .ToList();
+                    }
+                }
+
+                // Create any missing occurrences from the group's schedule (not location schedules)
+                Schedule groupSchedule = null;
+                if ( group.ScheduleId.HasValue )
+                {
+                    groupSchedule = group.Schedule;
+                    if ( groupSchedule == null )
+                    {
+                        groupSchedule = new ScheduleService( rockContext ).Get( group.ScheduleId.Value );
+                    }
+                }
+
+                if ( groupSchedule != null )
+                {
+                    var newOccurrences = new List<ScheduleOccurrence>();
+
+                    var existingDates = occurrences
+                        .Where( o => o.ScheduleId.Equals( groupSchedule.Id ) )
+                        .Select( o => o.StartDateTime.Date )
+                        .Distinct()
+                        .ToList();
+
+                    var startDate = fromDateTime.HasValue ? fromDateTime.Value : RockDateTime.Today.AddMonths( -2 );
+                    var endDate = toDateTime.HasValue ? toDateTime.Value : RockDateTime.Today.AddDays( 1 );
+
+                    DDay.iCal.Event calEvent = groupSchedule.GetCalenderEvent();
                     if ( calEvent != null )
                     {
                         // If schedule has an iCal schedule, get all the past occurrences 
-                        foreach ( var occurrence in calEvent.GetOccurrences( DateTime.MinValue, endDate ) )
+                        foreach ( var occurrence in calEvent.GetOccurrences( startDate, endDate ) )
                         {
-                            occurrences.Add( new ScheduleOccurrence( occurrence, schedule.Id ) );
+                            var scheduleOccurrence = new ScheduleOccurrence( occurrence, groupSchedule.Id, groupSchedule.Name );
+                            if ( !existingDates.Contains( scheduleOccurrence.StartDateTime.Date ) )
+                            {
+                                newOccurrences.Add( scheduleOccurrence );
+                            }
                         }
                     }
                     else
                     {
                         // if schedule does not have an iCal, then check for weekly schedule and calculate occurrences starting with first attendance or current week
-                        if ( schedule.WeeklyDayOfWeek.HasValue )
+                        if ( groupSchedule.WeeklyDayOfWeek.HasValue )
                         {
+
                             // default to start with date 2 months earlier
-                            DateTime startDateTime = RockDateTime.Today.AddMonths( -2 );
-                            if ( uniqueStartDates.Any( d => d < startDateTime ) )
+                            startDate = fromDateTime.HasValue ? fromDateTime.Value : RockDateTime.Today.AddMonths( -2 );
+                            if ( existingDates.Any( d => d < startDate ) )
                             {
-                                startDateTime = uniqueStartDates.Min();
+                                startDate = existingDates.Min();
                             }
 
                             // Back up start time to the correct day of week
-                            while ( startDateTime.DayOfWeek != schedule.WeeklyDayOfWeek.Value )
+                            while ( startDate.DayOfWeek != groupSchedule.WeeklyDayOfWeek.Value )
                             {
-                                startDateTime = startDateTime.AddDays( -1 );
+                                startDate = startDate.AddDays( -1 );
                             }
 
                             // Add the start time
-                            if ( schedule.WeeklyTimeOfDay.HasValue )
+                            if ( groupSchedule.WeeklyTimeOfDay.HasValue )
                             {
-                                startDateTime = startDateTime.Add( schedule.WeeklyTimeOfDay.Value );
+                                startDate = startDate.Add( groupSchedule.WeeklyTimeOfDay.Value );
                             }
 
                             // Create occurrences up to current time
-                            while ( startDateTime < endDate )
+                            while ( startDate < endDate )
                             {
-                                occurrences.Add( new ScheduleOccurrence( startDateTime, startDateTime.AddDays( 1 ), schedule.Id ) );
-                                startDateTime = startDateTime.AddDays( 7 );
+                                if ( !existingDates.Contains( startDate.Date ) )
+                                {
+                                    var scheduleOccurrence = new ScheduleOccurrence( startDate, startDate.AddDays( 1 ), groupSchedule.Id, groupSchedule.Name );
+                                    newOccurrences.Add( scheduleOccurrence );
+                                }
+
+                                startDate = startDate.AddDays( 7 );
                             }
                         }
                     }
-                }
 
-                // Add occurrences for any attendance dates that do not fall into a logical occurrence based on the schedule
-                var occurrenceDates = occurrences.Select( o => o.StartDateTime.Date ).ToList();
-                foreach( var date in uniqueStartDates.Where( d => !occurrenceDates.Contains( d ) ) )
-                {
-                    occurrences.Add( new ScheduleOccurrence( date, date.AddDays( 1 ) ) );
-                }
-
-                if ( occurrences.Any() )
-                {
-                    // Filter Exclusions
-                    var groupType = GroupTypeCache.Read( group.GroupTypeId );
-                    foreach ( var exclusion in groupType.GroupScheduleExclusions )
+                    if ( newOccurrences.Any() )
                     {
-                        if ( exclusion.Start.HasValue && exclusion.End.HasValue )
+                        // Filter Exclusions
+                        var groupType = GroupTypeCache.Read( group.GroupTypeId );
+                        foreach ( var exclusion in groupType.GroupScheduleExclusions )
                         {
-                            foreach ( var occurrence in occurrences.ToList() )
+                            if ( exclusion.Start.HasValue && exclusion.End.HasValue )
                             {
-                                if ( occurrence.StartDateTime >= exclusion.Start.Value &&
-                                    occurrence.StartDateTime < exclusion.End.Value.AddDays( 1 ) )
+                                foreach ( var occurrence in newOccurrences.ToList() )
                                 {
-                                    occurrences.Remove( occurrence );
+                                    if ( occurrence.StartDateTime >= exclusion.Start.Value &&
+                                        occurrence.StartDateTime < exclusion.End.Value.AddDays( 1 ) )
+                                    {
+                                        newOccurrences.Remove( occurrence );
+                                    }
                                 }
                             }
                         }
                     }
 
-
-                    if ( loadAttendanceData )
+                    foreach( var occurrence in newOccurrences )
                     {
-                        var minStartValue = occurrences.Min( o => o.StartDateTime );
-                        var maxEndValue = occurrences.Max( o => o.EndDateTime );
-
-                        var attendanceQry = attendanceService
-                            .Queryable( "PersonAlias" ).AsNoTracking()
-                            .Where( a =>
-                                a.GroupId == group.Id &&
-                                a.StartDateTime >= minStartValue &&
-                                a.StartDateTime < maxEndValue );
-
-                        var attendanceData = attendanceQry.ToList();
-
-                        foreach ( var occurrence in occurrences )
-                        {
-                            occurrence.Attendance = attendanceData
-                                .Where( a =>
-                                    a.StartDateTime >= occurrence.StartDateTime &&
-                                    a.StartDateTime < occurrence.EndDateTime )
-                                .ToList();
-                        }
+                        occurrence.Attendance = new List<Attendance>();
+                        occurrences.Add( occurrence );
                     }
-                }
 
+                }
             }
 
             return occurrences;
@@ -185,6 +258,10 @@ namespace Rock.Model
                         a.GroupId == group.Id &&
                         a.StartDateTime >= occurrence.StartDateTime &&
                         a.StartDateTime < occurrence.EndDateTime )
+                    .ToList()
+                    .Where( a =>
+                        a.LocationId.Equals( occurrence.LocationId ) &&
+                        a.ScheduleId.Equals( occurrence.ScheduleId ) )
                     .ToList();
 
                 occurrence.Attendance = attendanceData;
