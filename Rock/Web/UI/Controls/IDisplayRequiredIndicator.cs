2e7734ede026384cc05b555784894e95e9db5973
Mike Peterson <mikepeterson@ccvonline.com>
2015-04-13 11:39:40 -0700

    - added new Add Person To Group workflow action where the Group and Role are specified using a Group/Role picker - new Group/Role Picker (GroupAndRolePicker) where you first select a Group Type then the Group and optionally the Group Role - Added new IDisplayRequiredIndicator to the small number of RockControls that have a "DisplayRequiredIndicator"

---
+++ Rock/Web/UI/Controls/IDisplayRequiredIndicator.cs
@@ -0,0 +1,35 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Rock.Web.UI.Controls
+{
+    /// <summary>
+    /// Interface for RockControls that have a DisplayRequiredIndicator option
+    /// </summary>
+    public interface IDisplayRequiredIndicator : IRockControl
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether to show the Required indicator when Required=true
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if [display required indicator]; otherwise, <c>false</c>.
+        /// </value>
+        bool DisplayRequiredIndicator { get; set; }
+    }
+}
