f927c867e09c6bd7c33afed4a5f31d54377d5b9c
David Turner <david@sparkdevnetwork.org>
2015-04-13 05:13:38 -0700

    Add foreignid indexes to custom tables

---
+++ church.ccv.CommandCenter/Migrations/005_AddForeignIdIndex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rock.Plugin;
+
+namespace church.ccv.CommandCenter.Migrations
+{
+    [MigrationNumber( 5, "1.3.1" )]
+    class AddForeignIdIndex : Migration
+    {
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+            Sql( "ALTER TABLE [dbo].[_church_ccv_CommandCenter_Recording] ALTER COLUMN [ForeignId] NVARCHAR(100)" );
+            Sql( "CREATE NONCLUSTERED INDEX [IX_ForeignId] ON [dbo].[_church_ccv_CommandCenter_Recording] ([ForeignId] ASC)" );
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ church.ccv.SampleProject/Migrations/004_AddForeignIdIndex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rock.Plugin;
+
+namespace church.ccv.SampleProject.Migrations
+{
+    [MigrationNumber( 4, "1.3.1" )]
+    class AddForeignIdIndex : Migration
+    {
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+            Sql( "ALTER TABLE [dbo].[_church_ccv_SampleProject_ReferralAgency] ALTER COLUMN [ForeignId] NVARCHAR(100)" );
+            Sql( "CREATE NONCLUSTERED INDEX [IX_ForeignId] ON [dbo].[_church_ccv_SampleProject_ReferralAgency] ([ForeignId] ASC)" );
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+        }
+    }
+}
