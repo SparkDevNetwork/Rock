b3f0a5f7617f7fc6f60c8ea299d71185ab2b8de1
nairdo <nairdo@gmail.com>
2015-05-09 11:49:02 -0700

    - Added a tiny SignalR message hub which the SampleData block now uses to report realtime feedback as the data is being loaded (if the block's stopwatch is enabled).  I'm planning on following this pattern for the RockUpdate block to provide update-details in realtime.

---
+++ RockWeb/App_Code/RockMessageHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+/// <summary>
+/// This is the hub for sending/receiving messages. Javascript clients need to
+/// implement a receiveNotification handler:
+/// <code>
+///     proxy.client.receiveNotification = function (message) {
+///            //do something here...
+///        }
+/// </code>
+/// </summary>
+namespace RockWeb
+{
+    [HubName( "rockMessageHub" )]
+    public class RockMessageHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.receiveNotification( message );
+        }
+    }
+}
