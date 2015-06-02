5a89a205749450af92cf3bbd9a032ddb95f18567
Jon Edmiston <jonedmiston@ccvonline.com>
2015-06-02 08:29:26 -0700

    - start of era loss report

---
+++ RockWeb/Plugins/church_ccv/Era/EraLossList.ascx
@@ -0,0 +1,28 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="EraLossList.ascx.cs" Inherits="RockWeb.Plugins.church_ccv.Era.EraLossList" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
+        
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-star"></i> Blank List Block</h1>
+            </div>
+            <div class="panel-body">
+
+                <div class="grid grid-panel">
+                    <Rock:Grid ID="gList" runat="server" AllowSorting="true">
+                        <Columns>
+                            <Rock:RockBoundField DataField="FullName" HeaderText="Name" SortExpression="FullName" />
+                            <Rock:RockBoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                            <Rock:RockBoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                        </Columns>
+                    </Rock:Grid>
+                </div>
+
+            </div>
+        
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/church_ccv/Era/EraLossList.ascx.cs
@@ -0,0 +1,135 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+using church.ccv.Datamart.Model;
+using church.ccv.Datamart.Data;
+
+namespace RockWeb.Plugins.church_ccv.Era
+{
+    /// <summary>
+    /// Template block for developers to use to start a new block.
+    /// </summary>
+    [DisplayName( "eRA Loss List" )]
+    [Category("CCV > eRA")]
+    [Description( "This block lists eRA losses based on filters." )]
+    public partial class EraLossList : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+        // used for private variables
+
+        #endregion
+
+        #region Properties
+
+        // used for public / protected properties
+
+        #endregion
+
+        #region Base Control Methods
+
+        //  overrides of the base RockBlock methods (i.e. OnInit, OnLoad)
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            gList.GridRebind += gList_GridRebind;
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                BindGrid();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the gPledges control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void gList_GridRebind( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            DatamartContext datamartContext = new DatamartContext();
+            //DatamartEstimatedRegularAttendeesService dtmtEraService = new DatamartEstimatedRegularAttendeesService(datamartContext);
+
+            // sample query to display a few people
+            //var qry = dtmtEraService.Queryable().Where( d => d.Los)
+
+            //gList.DataSource = qry.ToList();
+            //gList.DataBind();
+        }
+
+        #endregion
+    }
+}
