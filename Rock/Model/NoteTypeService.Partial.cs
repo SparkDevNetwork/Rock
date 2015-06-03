6fa82e851d729c7c59a9b4b885c5325846cf933c
David Turner <david@azturners.com>
2015-06-03 08:05:54 -0700

    - Add back note type service method that will get (and add if needed) a note type based on the entity type and a name (used by sample data).

--- Rock/Model/NoteTypeService.Partial.cs
+++ Rock/Model/NoteTypeService.Partial.cs
@@ -29,6 +29,46 @@ namespace Rock.Model
         /// Gets the specified entity type identifier.
         /// </summary>
         /// <param name="entityTypeId">The entity type identifier.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="create">if set to <c>true</c> [create].</param>
+        /// <returns></returns>
+        public NoteType Get( int entityTypeId, string name, bool create = true )
+        {
+            var noteTypes = Get( entityTypeId, string.Empty, string.Empty ).ToList();
+            var noteType = noteTypes.Where( t => t.Name == name ).FirstOrDefault();
+
+            if ( noteType == null && create )
+            {
+                noteType = new NoteType();
+                noteType.IsSystem = false;
+                noteType.EntityTypeId = entityTypeId;
+                noteType.EntityTypeQualifierColumn = string.Empty;
+                noteType.EntityTypeQualifierValue = string.Empty;
+                noteType.Name = name;
+                noteType.UserSelectable = true;
+                noteType.IconCssClass = string.Empty;
+                noteType.CssClass = string.Empty;
+                noteType.Order = noteTypes.Any() ? noteTypes.Max( t => t.Order ) + 1 : 0;
+
+                // Create a new context/service so that save does not affect calling method's context
+                using ( var rockContext = new RockContext() )
+                {
+                    var noteTypeService = new NoteTypeService( rockContext );
+                    noteTypeService.Add( noteType );
+                    rockContext.SaveChanges();
+                }
+
+                // requery using calling context
+                noteType = Get( noteType.Id );
+            }
+
+            return noteType;
+        }
+
+        /// <summary>
+        /// Gets the specified entity type identifier.
+        /// </summary>
+        /// <param name="entityTypeId">The entity type identifier.</param>
         /// <param name="entityTypeQualifierColumn">The entity type qualifier column.</param>
         /// <param name="entityTypeQualifierValue">The entity type qualifier value.</param>
         /// <returns></returns>
