SparkDevNetwork/Rock

commit 7af580fb689cebd742ffb3e5029b918f098ee9a3

+ User Logins can now be added from the Security > User Accounts page (Fixes #888).

RockWeb/Blocks/Security/UserLoginList.ascx.cs
@@ -67,6 +67,7 @@ namespace RockWeb.Blocks.Security
 
                 // Hide the person name column
                 gUserLogins.Columns[1].Visible = false;
+                ppPerson.Visible = false;
             }
 
             _canEdit = IsUserAuthorized( Authorization.EDIT );
@@ -75,7 +76,7 @@ namespace RockWeb.Blocks.Security
             gfSettings.DisplayFilterValue += gfSettings_DisplayFilterValue;
 
             gUserLogins.DataKeyNames = new string[] { "Id" };
-            gUserLogins.Actions.ShowAdd = _personId.HasValue && _canEdit;
+            gUserLogins.Actions.ShowAdd = _canEdit;
             gUserLogins.Actions.AddClick += gUserLogins_Add;
             gUserLogins.IsDeleteEnabled = _canEdit;
             gUserLogins.GridRebind += gUserLogins_GridRebind;
@@ -267,10 +268,14 @@ namespace RockWeb.Blocks.Security
                     {
                         userLogin.PersonId = _personId;
                     }
+                    else if ( ppPerson.PersonId.HasValue )
+                    {
+                        userLogin.PersonId = ppPerson.PersonId.Value;
+                    }
                     else
                     {
                         nbErrorMessage.Title = "Invalid Situation";
-                        nbErrorMessage.Text = "The person you are editing has no person Id.";
+                        nbErrorMessage.Text = "No person selected, or the person you are editing has no person Id.";
                         nbErrorMessage.Visible = true;
                         return;
                     }
@@ -445,17 +450,17 @@ namespace RockWeb.Blocks.Security
             gUserLogins.EntityTypeId = EntityTypeCache.Read<UserLogin>().Id;
             gUserLogins.DataSource = qry.Sort( sortProperty )
                 .Select( l => new
-                    {
-                        Id = l.Id,
-                        UserName = l.UserName,
-                        PersonId = l.PersonId,
-                        PersonName = l.Person.LastName + ", " + l.Person.NickName,
-                        ProviderName = l.EntityType.FriendlyName,
-                        CreatedDateTime = l.CreatedDateTime,
-                        LastLoginDateTime = l.LastLoginDateTime,
-                        IsConfirmed = l.IsConfirmed,
-                        IsLockedOut = l.IsLockedOut
-                    } ).ToList();
+                {
+                    Id = l.Id,
+                    UserName = l.UserName,
+                    PersonId = l.PersonId,
+                    PersonName = l.Person.LastName + ", " + l.Person.NickName,
+                    ProviderName = l.EntityType.FriendlyName,
+                    CreatedDateTime = l.CreatedDateTime,
+                    LastLoginDateTime = l.LastLoginDateTime,
+                    IsConfirmed = l.IsConfirmed,
+                    IsLockedOut = l.IsLockedOut
+                } ).ToList();
             gUserLogins.DataBind();
         }
 
@@ -466,9 +471,21 @@ namespace RockWeb.Blocks.Security
         protected void ShowEdit( int userLoginId )
         {
             UserLogin userLogin = null;
+            ppPerson.Visible = false;
+
             if ( userLoginId > 0 )
             {
                 userLogin = new UserLoginService( new RockContext() ).Get( userLoginId );
+                ppPerson.PersonId = userLogin.PersonId;
+            }
+            else
+            {
+                // Shows the person picker when adding a new login outside of a person context
+                ppPerson.SetValue( null );
+                if ( !_personId.HasValue )
+                {
+                    ppPerson.Visible = true;
+                }
             }
 
             if ( userLogin == null )
@@ -500,6 +517,10 @@ namespace RockWeb.Blocks.Security
             tbPasswordConfirm.Enabled = false;
             tbPassword.Required = false;
             tbPasswordConfirm.Required = false;
+
+            // This is to clear any previous password that is usually taken from the RockTextBox's RenderBaseControl function.
+            tbPassword.Text = String.Empty;
+            tbPasswordConfirm.Text = String.Empty;
 
             var entityType = EntityTypeCache.Read( compProvider.SelectedValue.AsGuid() );
             if ( entityType != null )

RockWeb/Blocks/Security/UserLoginList.ascx
@@ -23,12 +23,12 @@
                         <Rock:ComponentPicker ID="compProviderFilter" runat="server" Label="Authentication Provider" ContainerType="Rock.Security.AuthenticationContainer, Rock" />
                         <Rock:DateRangePicker ID="drpCreated" runat="server" Label="Created" />
                         <Rock:DateRangePicker ID="drpLastLogin" runat="server" Label="Last Login" />
-                        <Rock:RockDropDownList ID="ddlIsConfirmedFilter" runat="server" Label="Is Confirmed" >
+                        <Rock:RockDropDownList ID="ddlIsConfirmedFilter" runat="server" Label="Is Confirmed">
                             <asp:ListItem Value="" Text="" />
                             <asp:ListItem Value="true" Text="Yes" />
                             <asp:ListItem Value="false" Text="No" />
                         </Rock:RockDropDownList>
-                        <Rock:RockDropDownList ID="ddlLockedOutFilter" runat="server" Label="Is Locked Out" >
+                        <Rock:RockDropDownList ID="ddlLockedOutFilter" runat="server" Label="Is Locked Out">
                             <asp:ListItem Value="" Text="" />
                             <asp:ListItem Value="true" Text="Yes" />
                             <asp:ListItem Value="false" Text="No" />
@@ -47,10 +47,10 @@
                             <Rock:DeleteField OnClick="gUserLogins_Delete" />
                         </Columns>
                     </Rock:Grid>
-            </div>
+                </div>
             </div>
 
-            
+
 
         </asp:Panel>
 
@@ -62,7 +62,13 @@
                 <asp:HiddenField ID="hfIdValue" runat="server" />
 
                 <Rock:NotificationBox ID="nbErrorMessage" runat="server" NotificationBoxType="Danger" />
-
+                <div class="row">
+                    <div class="col-md-6">
+                        <Rock:PersonPicker ID="ppPerson" runat="server" Label="Person" />
+                    </div>
+                    <div class="col-md-6">
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-md-6">
                         <Rock:DataTextBox ID="tbUserName" runat="server" SourceTypeName="Rock.Model.UserLogin, Rock" PropertyName="UserName" />

commit 4af417073e2f1ea59c4aec87e35bbe135ae82de9

- Fixes the password verification bug introduced in Commit 7af580fb689cebd742ffb3e5029b918f098ee9a3

Rock/Web/UI/Controls/RockTextBox.cs
@@ -411,6 +411,16 @@ namespace Rock.Web.UI.Controls
         }
 
         /// <summary>
+        /// Clears the password.
+        /// </summary>
+        public void ClearPassword()
+        {
+            Password = string.Empty;
+            this.Text = string.Empty;
+            ViewState["Password"] = null;
+        }
+
+        /// <summary>
         /// Gets or sets the text content of the <see cref="T:System.Web.UI.WebControls.TextBox" /> control.
         /// </summary>
         /// <returns>The text displayed in the <see cref="T:System.Web.UI.WebControls.TextBox" /> control. The default is an empty string ("").</returns>

RockWeb/Blocks/Security/UserLoginList.ascx.cs
@@ -470,6 +470,8 @@ namespace RockWeb.Blocks.Security
         /// <param name="attributeId">The attribute id.</param>
         protected void ShowEdit( int userLoginId )
         {
+            tbPassword.ClearPassword();
+            tbPasswordConfirm.ClearPassword();
             UserLogin userLogin = null;
             ppPerson.Visible = false;
 
@@ -517,10 +519,6 @@ namespace RockWeb.Blocks.Security
             tbPasswordConfirm.Enabled = false;
             tbPassword.Required = false;
             tbPasswordConfirm.Required = false;
-
-            // This is to clear any previous password that is usually taken from the RockTextBox's RenderBaseControl function.
-            tbPassword.Text = String.Empty;
-            tbPasswordConfirm.Text = String.Empty;
 
             var entityType = EntityTypeCache.Read( compProvider.SelectedValue.AsGuid() );
             if ( entityType != null )