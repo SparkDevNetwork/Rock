d4ce8f9e999f2192a7d7315b11ba27fa3c67318d
Mike Peterson <MikePeterson@ccvonline.com>
2015-06-04 16:36:24 -0700

    - cleaned up some of the reporting javascript which was getting rendered redundantly multiple times - fixed issue where datafilter javascript wouldn't work on a full page postback. This fixes an issue where ReportData block and DataView block would sometimes show extra controls when a compare to was "Is Blank/Is Not Blank", etc

--- Rock/Reporting/DataFilterComponent.cs
+++ Rock/Reporting/DataFilterComponent.cs
@@ -219,21 +219,7 @@ namespace Rock.Reporting
         /// </value>
         public void RegisterFilterCompareChangeScript( FilterField filterControl )
         {
-            string filterComparescript = @"
-            $('.js-filter-compare').change( function () {
-        var $fieldCriteriaRow = $(this).closest('.field-criteria');
-        var compareValue = $(this).val();
-        var isNullCompare = (compareValue == 32 || compareValue == 64);
-        if (isNullCompare) {
-            $fieldCriteriaRow.find('.js-filter-control').hide();
-        }
-        else {
-            $fieldCriteriaRow.find('.js-filter-control').show();
-        }
-    });
-";
-            // only need this script once per page
-            ScriptManager.RegisterStartupScript( filterControl.Page, filterControl.Page.GetType(), "js-filter-compare-change-script", filterComparescript, true );
+            ReportingHelper.RegisterJavascriptInclude( filterControl );
         }
 
         #endregion
--- Rock/Reporting/ReportingHelper.cs
+++ Rock/Reporting/ReportingHelper.cs
@@ -386,5 +386,14 @@ namespace Rock.Reporting
 
             return filter;
         }
+
+        /// <summary>
+        /// Registers the javascript include needed for reporting client controls
+        /// </summary>
+        /// <param name="rockBlock">The rock block.</param>
+        public static void RegisterJavascriptInclude( FilterField filterField )
+        {
+            ScriptManager.RegisterClientScriptInclude( filterField, filterField.GetType(), "reporting-include", filterField.RockBlock().RockPage.ResolveRockUrl( "~/Scripts/Rock/reportingInclude.js", true ) );
+        }
     }
 }
