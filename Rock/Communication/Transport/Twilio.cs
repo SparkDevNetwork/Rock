ba49cf2b7786058e74cd590d7a63df0ffd0cbc4f
Jon Edmiston <jon@sparkdevnetwork.org>
2015-06-09 10:18:50 -0700

    - email update to include sending emails with a from name

--- Rock/Communication/Transport/Twilio.cs
+++ Rock/Communication/Transport/Twilio.cs
@@ -295,5 +295,22 @@ namespace Rock.Communication.Transport
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Sends the specified recipients.
+        /// </summary>
+        /// <param name="recipients">The recipients.</param>
+        /// <param name="from">From.</param>
+        /// <param name="fromName">From name.</param>
+        /// <param name="subject">The subject.</param>
+        /// <param name="body">The body.</param>
+        /// <param name="appRoot">The application root.</param>
+        /// <param name="themeRoot">The theme root.</param>
+        /// <param name="attachments">The attachments.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void Send( List<string> recipients, string from, string fromName, string subject, string body, string appRoot = null, string themeRoot = null, List<Attachment> attachments = null )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
