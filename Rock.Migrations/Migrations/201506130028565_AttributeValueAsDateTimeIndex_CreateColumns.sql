00ad04d4c5b65f785132140c08b9a9eb1f9a9339
Mike Peterson <MikePeterson@ccvonline.com>
2015-06-15 12:32:33 -0700

    - fix up new Attribute.ValueAsDateTime to use regular column + trigger instead of Computed column due to bug in SQLSERVER2012 that isn't fixed until SP2

---
+++ Rock.Migrations/Migrations/201506130028565_AttributeValueAsDateTimeIndex_CreateColumns.sql
@@ -0,0 +1,18 @@
+﻿ALTER TABLE AttributeValue DROP COLUMN ValueAsNumeric
+
+ALTER TABLE AttributeValue ADD [ValueAsNumeric] AS (
+    CASE 
+        WHEN len([value]) < (100)
+            AND isnumeric([value]) = (1)
+            AND NOT [value] LIKE '%[^0-9.]%'
+            AND NOT [value] LIKE '%[.]%'
+            THEN TRY_CONVERT([numeric](38, 10), [value])
+        END
+    ) persisted
+
+CREATE INDEX IX_ValueAsNumeric on AttributeValue (ValueAsNumeric)
+
+
+ALTER TABLE AttributeValue DROP COLUMN ValueAsDateTime
+ALTER TABLE AttributeValue ADD ValueAsDateTime datetime;
+CREATE INDEX IX_ValueAsDateTime on AttributeValue (ValueAsDateTime)
