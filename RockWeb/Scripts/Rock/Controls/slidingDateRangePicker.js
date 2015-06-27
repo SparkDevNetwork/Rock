eebf1cf9388523d0f04f9e2cdc1f13a26f117380
Mike Peterson <MikePeterson@ccvonline.com>
2015-06-26 18:06:23 -0700

    + Add 'Between' as an option for all date dataview filters that lets you choose a date range or a sliding date range - moved some more javascript from datafilters into reportingInclude.js - tweaks to SlidingDateRangePicker and DatePicker to help it when used as a filter

--- RockWeb/Scripts/Rock/Controls/slidingDateRangePicker.js
+++ RockWeb/Scripts/Rock/Controls/slidingDateRangePicker.js
@@ -41,8 +41,8 @@
             $picker.find('.js-time-units-singular').toggle(isCurrent);
             $picker.find('.js-time-units-plural').toggle(isLast || isPrevious);
             $picker.find('.js-time-units-date-range').toggle(isDateRange);
-
-            $picker.siblings('.js-slidingdaterange-info').toggle(isLast || isPrevious || isCurrent);
+            var $pickerContainer = $picker.closest('.js-slidingdaterange-container');
+            $pickerContainer.find('.js-slidingdaterange-info').toggle(isLast || isPrevious || isCurrent);
         },
         updateDateRangeInfo = function ($picker) {
             var $select = $picker.find('.js-slidingdaterange-select');
@@ -59,14 +59,16 @@
 
             var numberOf = $picker.find('.js-number').val();
 
+            var $pickerContainer = $picker.closest('.js-slidingdaterange-container');
+
             var getDateRangeUrl = Rock.settings.get('baseUrl') + 'api/Utility/CalculateSlidingDateRange?slidingDateRangeType=' + $select.val() + '&timeUnitType=' + timeUnitType + '&number=' + numberOf;
             $.get(getDateRangeUrl, function (r) {
-                $picker.siblings('.js-slidingdaterange-info').text(r);
+                $pickerContainer.find('.js-slidingdaterange-info').text(r);
             });
 
             var getTextValueUrl = Rock.settings.get('baseUrl') + 'api/Utility/GetSlidingDateRangeTextValue?slidingDateRangeType=' + $select.val() + '&timeUnitType=' + timeUnitType + '&number=' + numberOf;
             $.get(getTextValueUrl, function (r) {
-                $picker.siblings('.js-slidingdaterange-text-value').val(r);
+                $pickerContainer.find('.js-slidingdaterange-text-value').val(r);
             });
         }
 
