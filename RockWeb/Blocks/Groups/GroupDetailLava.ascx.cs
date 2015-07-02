SparkDevNetwork/Rock

commit 5b56a30da091eb7c27f542e7b92486c33283b40d

- order the group members by surname, first name (Fixes #991)

RockWeb/Blocks/Groups/GroupDetailLava.ascx.cs
@@ -525,6 +525,9 @@ namespace RockWeb.Blocks.Groups
                 }
                 var group = qry.FirstOrDefault();
 
+                // order group members by name
+                group.Members = group.Members.OrderBy( m => m.Person.LastName ).ThenBy( m => m.Person.FirstName ).ToList();
+
                 var mergeFields = new Dictionary<string, object>();
                 mergeFields.Add( "Group", group );
 

commit 1dcc6b4d6eba564b23526db73b97ff3e52d2a268

- Fixed the group detail lava block from trying to load a group if no group was passed in the query string (Fixes an exception introduced in 5b56a30da091eb7c27f542e7b92486c33283b40d)

RockWeb/Blocks/Groups/GroupDetailLava.ascx.cs
@@ -508,7 +508,7 @@ namespace RockWeb.Blocks.Groups
         private void DisplayViewGroup()
         {
 
-            if ( _groupId != -1 )
+            if ( _groupId > 0 )
             {
                 RockContext rockContext = new RockContext();
                 GroupService groupService = new GroupService( rockContext );