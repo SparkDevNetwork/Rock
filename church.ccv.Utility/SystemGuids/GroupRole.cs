10ec3c1e0fb7d635abbf56edb000d84245b2dfbf
Jon Edmiston <jonedmiston@ccvonline.com>
2015-07-02 08:52:09 -0700

    - added system guids for ccv group types and roles

---
+++ church.ccv.Utility/SystemGuids/GroupRole.cs
@@ -0,0 +1,65 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace church.ccv.SystemGuid
+{
+    /// <summary>
+    /// Group Role System Guids
+    /// </summary>
+    public static class GroupRole
+    {
+        #region Neighborhoods
+
+        /// <summary>
+        /// Gets neighborhood pastor role
+        /// </summary>
+        public const string GROUPROLE_NEIGHBORHOOD_PASTOR = "AA472582-225E-4DA8-9A49-9B0D6DA20592";
+        
+        /// <summary>
+        /// Gets neighborhood leader role
+        /// </summary>
+        public const string GROUPROLE_NEIGHBORHOOD_LEADER = "C5ECBA4C-17B1-4A1E-8259-2AB85F0E2B80";
+
+        /// <summary>
+        /// Gets neighborhood group leader role
+        /// </summary>
+        public const string GROUPROLE_NEIGHBORHOODGROUP_LEADER = "40DD0959-D7E1-4A60-9D41-69EA003B120A";
+
+        /// <summary>
+        /// Gets neighborhood group attendee role
+        /// </summary>
+        public const string GROUPROLE_NEIGHBORHOODGROUP_ATTENDEE = "4F097DFA-2188-4E7B-8C80-78AD1566150B";
+
+        /// <summary>
+        /// Gets neighborhood group host role
+        /// </summary>
+        public const string GROUPROLE_NEIGHBORHOODGROUP_HOST = "21EC896A-7C9D-4407-90DD-47D0C88469B9";
+
+        /// <summary>
+        /// Gets neighborhood group assistant leader role
+        /// </summary>
+        public const string GROUPROLE_NEIGHBORHOODGROUP_ASSISTANTLEADER = "416A59E5-8781-45C8-8F8D-353703AFAED9";
+
+        #endregion
+
+        
+    }
+}
---
+++ church.ccv.Utility/SystemGuids/GroupType.cs
@@ -0,0 +1,40 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace church.ccv.SystemGuid
+{
+    /// <summary>
+    /// System Group Types
+    /// </summary>
+    public static class GroupType
+    {
+        /// <summary>
+        /// Security Role group type guid
+        /// </summary>
+        public const string GROUPTYPE_NEIGHBORHOOD_AREA = "C3A3EB51-53CA-4EC1-B9B4-BB62E0C61445";
+
+        /// <summary>
+        /// Family group type guid
+        /// </summary>
+        public const string GROUPTYPE_NEIGHBORHOOD_GROUP = "A25F4FF3-5FB0-4837-8E4E-A501AF0BD5D7";
+
+    }
+}
