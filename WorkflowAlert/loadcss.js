f9a8d0506bcfb1c89d7a93e4214232bcc7ad15ca
Brian Kalwat <brian@briankalwat.com>
2015-07-16 11:06:04 -0400

    Updated Path to Workflow Alerts Stylesheet

--- WorkflowAlert/loadcss.js
+++ WorkflowAlert/loadcss.js
@@ -1,5 +1,5 @@
 ﻿var loadCSS = function () {
-    var relPath = '../plugins/cc_newspring/workflowalert/Styles/styles.css';
+    var relPath = '../plugins/cc_newspring/blocks/workflowalert/Styles/styles.css';
     var styleLink = $('<link>').attr('rel', 'stylesheet').attr('href', relPath);
     $('head').append(styleLink);
 }();
