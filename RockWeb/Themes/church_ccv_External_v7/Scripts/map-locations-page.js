a80aeb8201f06ce738c86af100bbe834f5dbb550
Nick Sheck <nsheck@gmail.com>
2015-07-21 16:48:08 -0700

    Location page changes

--- RockWeb/Themes/church_ccv_External_v7/Scripts/map-locations-page.js
+++ RockWeb/Themes/church_ccv_External_v7/Scripts/map-locations-page.js
@@ -12,7 +12,7 @@
 
 // Load the Google maps api dynamically to avoid errors when double loading
 // The URL string is from https://github.com/SparkDevNetwork/Rock/blob/582e01a72f41ab33ad41335c01c365523b35be5b/Rock/Web/UI/RockPage.cs#L1045
-Rock.controls.util.loadGoogleMapsApi('https://maps.googleapis.com/maps/api/js?sensor=false&libraries=drawing')
+Rock.controls.util.loadGoogleMapsApi('https://maps.googleapis.com/maps/api/js?sensor=true&libraries=drawing')
 
 
 // Map Object
@@ -24,16 +24,6 @@ $(window).on('googleMapsIsLoaded', function(){
 
   locationsMap.selectCampus = function (campusId) {
     alert(campusId)
-  }
-
-  locationsMap.bindUi = function() {
-    var _this = this
-    $(window).resize(function() {
-      _this.fitMarkers()
-    })
-    $('.js-find-nearest').click(function(){
-      _this.findNearestCampus(_this.openInfoWindow, this)
-    })
   }
 
   locationsMap.buildInfoWindow = function(campus) {
@@ -53,10 +43,78 @@ $(window).on('googleMapsIsLoaded', function(){
     return result
   }
 
-  locationsMap.useScrollZoom = false;
-  locationsMap.usePanControl = false;
+  // locationsMap.useScrollZoom = false;
+  // locationsMap.usePanControl = false;
+
+  locationsMap.mapOptions = {
+    mapTypeId: 'CCV',
+    scrollwheel: false,
+    panControl: false,
+    zoomControl: false,
+    // zoomControl: true,
+    // zoomControlOptions: {
+    //   style: google.maps.ZoomControlStyle.SMALL,
+    //   position: google.maps.ControlPosition.TOP_RIGHT
+    // },
+    streetViewControl: false,
+    mapTypeControl: false
+  }
 
   locationsMap.draw()
 
+
+  // Find nearest button
+
+  function FindNearest(controlDiv, map) {
+    var controlUI = document.createElement('div');
+    controlUI.className = "btn btn-default btn-primary"
+    controlUI.title = 'Click to find the nearest campus to you';
+    controlUI.innerHTML = '<i class="fa fa-fw fa-location-arrow"></i> Find Nearest Campus';
+    controlDiv.appendChild(controlUI);
+
+    google.maps.event.addDomListener(controlUI, 'click', function() {
+      locationsMap.findNearestCampus(locationsMap.openInfoWindow, this)
+    });
+  }
+  var findNearestDiv = document.createElement('div');
+  findNearestDiv.style.marginTop = '2.5%';
+  findNearestDiv.style.marginLeft = '2.5%';
+  var findNearest = new FindNearest(findNearestDiv, locationsMap.map);
+
+  findNearestDiv.index = 1;
+  locationsMap.map.controls[google.maps.ControlPosition.TOP_LEFT].push(findNearestDiv);
+
+
+  // Custom Zoom buttons
+
+  function CustomZoom(controlDiv, map) {
+    var zoomIn = document.createElement('div');
+    zoomIn.className = "btn btn-default btn-md"
+    zoomIn.title = 'Zoom In';
+    zoomIn.innerHTML = '<i class="fa fa-fw fa-plus"></i>';
+    controlDiv.appendChild(zoomIn);
+
+    google.maps.event.addDomListener(zoomIn, 'click', function() {
+      locationsMap.map.setZoom(locationsMap.map.getZoom() + 1);
+    });
+
+    var zoomOut = document.createElement('div');
+    zoomOut.className = "btn btn-default btn-md"
+    zoomOut.title = 'Zoom Out';
+    zoomOut.innerHTML = '<i class="fa fa-fw fa-minus"></i>';
+    controlDiv.appendChild(zoomOut);
+
+    google.maps.event.addDomListener(zoomOut, 'click', function() {
+      locationsMap.map.setZoom(locationsMap.map.getZoom() - 1);
+    });
+  }
+  var customZoomDiv = document.createElement('div');
+  customZoomDiv.className = "btn-group-vertical"
+  customZoomDiv.style.marginTop = '2.5%';
+  customZoomDiv.style.marginRight = '2.5%';
+  var customZoom = new CustomZoom(customZoomDiv, locationsMap.map);
+
+  customZoomDiv.index = 1;
+  locationsMap.map.controls[google.maps.ControlPosition.RIGHT_TOP].push(customZoomDiv);
 
 })
