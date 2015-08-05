16510820f92610196e5bd976cbd39afb79ea1dd4
dcs619 <dcs619@gmail.com>
2015-08-05 09:06:08 -0400

    Merge remote-tracking branch 'newspring/develop' into develop

--- WorkflowAlert/loadcss.js
+++
@@ -1,5 +0,0 @@
-﻿var loadCSS = function () {
-    var relPath = '../plugins/cc_newspring/blocks/workflowalert/Styles/styles.css';
-    var styleLink = $('<link>').attr('rel', 'stylesheet').attr('href', relPath);
-    $('head').append(styleLink);
-}();
