17b73013d34e081a338ac2496e77c776b9c2613c
David Turner <david@azturners.com>
2015-08-24 04:29:26 -0700

    + Added workflow action to create a connectoin request and added new controls, field types, and attributes for selecting connection opportunity, request, and status.

---
+++ Rock/Field/Types/ConnectionStatusFieldType.cs
@@ -0,0 +1,144 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI.Controls;
+
+namespace Rock.Field.Types
+{
+    /// <summary>
+    /// Field Type used to display a dropdown list of connection opportunities
+    /// Stored as ConnectionStatus.Guid
+    /// </summary>
+    public class ConnectionStatusFieldType : FieldType
+    {
+
+        #region Formatting
+
+        /// <summary>
+        /// Returns the field's current value(s)
+        /// </summary>
+        /// <param name="parentControl">The parent control.</param>
+        /// <param name="value">Information about the value</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="condensed">Flag indicating if the value should be condensed (i.e. for use in a grid column)</param>
+        /// <returns></returns>
+        public override string FormatValue( Control parentControl, string value, Dictionary<string, ConfigurationValue> configurationValues, bool condensed )
+        {
+            string formattedValue = string.Empty;
+
+            Guid? guid = value.AsGuidOrNull();
+            if (guid.HasValue)
+            {
+                var status = new ConnectionStatusService( new RockContext() ).Get( guid.Value );
+                if ( status != null )
+                {
+                    formattedValue = status.Name;
+                }
+            }
+
+            return base.FormatValue( parentControl, formattedValue, configurationValues, condensed );
+        }
+
+        #endregion
+
+        #region Edit Control
+
+        /// <summary>
+        /// Creates the control(s) necessary for prompting user for a new value
+        /// </summary>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="id"></param>
+        /// <returns>
+        /// The control
+        /// </returns>
+        public override Control EditControl( Dictionary<string, ConfigurationValue> configurationValues, string id )
+        {
+            var editControl = new RockDropDownList { ID = id };
+            editControl.Items.Add( new ListItem() );
+
+            var statuses = new ConnectionStatusService( new RockContext() )
+                .Queryable().AsNoTracking()
+                .OrderBy( s => s.ConnectionType.Name )
+                .ThenBy( s => s.Name )
+                .Select( s => new
+                {
+                    s.Guid,
+                    s.Name,
+                    ConnectionTypeName = s.ConnectionType.Name
+                } )
+                .ToList();
+
+            if ( statuses.Any() )
+            {
+                foreach ( var status in statuses )
+                {
+                    var listItem = new ListItem( status.Name, status.Guid.ToString().ToUpper() );
+                    listItem.Attributes.Add( "OptionGroup", status.ConnectionTypeName );
+                    editControl.Items.Add( listItem );
+                }
+
+                return editControl;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Reads new values entered by the user for the field
+        /// </summary>
+        /// <param name="control">Parent control that controls were added to in the CreateEditControl() method</param>
+        /// <param name="configurationValues"></param>
+        /// <returns></returns>
+        public override string GetEditValue( Control control, Dictionary<string, ConfigurationValue> configurationValues )
+        {
+            var picker = control as DropDownList;
+            if (picker != null)
+            {
+                // picker has value as ConnectionStatus.Guid
+                return picker.SelectedValue;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the value.
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="configurationValues"></param>
+        /// <param name="value">The value.</param>
+        public override void SetEditValue( Control control, Dictionary<string, ConfigurationValue> configurationValues, string value )
+        {
+            var picker = control as DropDownList;
+            if ( picker != null )
+            {
+                picker.SelectedValue = value.ToUpper();
+            }
+        }
+
+        #endregion
+
+    }
+}
