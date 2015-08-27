1b5cca203abe5d7bc0635c90ef6f35378ce652b8
Mike Peterson <MikePeterson@ccvonline.com>
2015-08-26 17:50:06 -0700

    - Added CodeGen_AuthMigration that helps create a Migration that adds Auths

--- Dev Tools/Sql/CodeGen_AuthMigration.sql
+++ Dev Tools/Sql/CodeGen_AuthMigration.sql
@@ -1,8 +1,11 @@
+-- list of recently modified Auth records for Page, Block and EntityType to help write a Migration
+
 declare
 @EntityTypeIdPage int = 2
 
 select 
     p.[InternalName] [Page],
+	p.[Guid] [Page.Guid],
     a.[Order],
     a.[Action],
     a.AllowOrDeny,
@@ -17,7 +20,7 @@ select
             '<all un-authenticated users>'
         end
          [Group],
-    a.[Guid]
+    a.[Guid] [Auth.Guid]
     
  from Auth a
 left join [Group] g on a.GroupId = g.Id
@@ -45,10 +48,35 @@ select
             '<all un-authenticated users>'
         end
          [Group],
-    a.[Guid]
+    a.[Guid] [Auth.Guid]
     
  from Auth a
 left join [Group] g on a.GroupId = g.Id
 join [Block] b on a.EntityId = b.Id
 where EntityTypeId = @EntityTypeIdBlock
 order by a.ModifiedDateTime desc
+
+-- Entity Security
+select 
+    b.[Name] + char(9) [EntityType],
+    a.[Order],
+    a.[Action],
+    a.AllowOrDeny,
+    a.SpecialRole,
+    case a.SpecialRole when 0 then
+        concat(g.[Guid], ' ( ', g.Name, ' ), ')
+        when 1 then
+            '<all users>'
+            when 2 then
+            '<all authenticated users>'
+            when 3 then
+            '<all un-authenticated users>'
+        end
+         [Group],
+    a.[Guid] [Auth.Guid]
+    
+ from Auth a
+left join [Group] g on a.GroupId = g.Id
+join [EntityType] b on a.EntityTypeId = b.Id
+where a.EntityId = 0
+order by isnull(a.ModifiedDateTime, '1900-01-01') desc
