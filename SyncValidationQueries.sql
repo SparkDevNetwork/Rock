e0e5cfb0959a042290b83918e33f320a960ffeaa
David Turner <david@sparkdevnetwork.org>
2015-09-01 13:54:28 -0700

    Add check for any invalid contribution image binary file types

--- SyncValidationQueries.sql
+++ SyncValidationQueries.sql
@@ -18,3 +18,15 @@ inner join rockrms.dbo.FinancialTransaction t on t.Id = c.foreign_key
 inner join rockrms.dbo.FinancialBatch rb on rb.id = b.foreign_key
 where t.BatchId is null or t.BatchId <> rb.Id 
 
+-- Check to make sure transaction images are correct (should not return any)
+DECLARE @binaryFileTypeContributionId INT = (
+	SELECT TOP 1 Id
+	FROM rockrms.dbo.BinaryFileType
+	WHERE [Guid] = '6D18A9C4-34AB-444A-B95B-C644019465AC' /* Contribution */
+)
+SELECT * FROM rockrms.dbo.BinaryFile
+WHERE Id IN (
+	SELECT BinaryFileId
+	FROM rockrms.dbo.FinancialTransactionImage
+)
+AND BinaryFileTypeId != @binaryFileTypeContributionId 
