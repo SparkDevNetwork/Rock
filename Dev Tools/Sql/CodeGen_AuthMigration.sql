3846a3485dddf14fff3bf3f3aa8ae0c7ea8c4575
Mike Peterson <MikePeterson@ccvonline.com>
2015-09-04 18:40:06 -0700

    - updated CodeGen_AuthMigration so it'll do the Up and Down for an EntityType Auth migration

--- Dev Tools/Sql/CodeGen_AuthMigration.sql
+++ Dev Tools/Sql/CodeGen_AuthMigration.sql
@@ -106,7 +106,72 @@ WHERE EntityTypeId = @EntityTypeIdBlock
 ORDER BY a.ModifiedDateTime DESC
 
 -- Entity Security
-SELECT b.[Name] + CHAR(9) [EntityType]
+SELECT 
+	a.ModifiedDateTime
+	,CONCAT (
+        'RockMigrationHelper.AddSecurityAuthForEntityType( "'
+        ,et.[Name]
+        ,'", '
+        ,a.[Order]
+        ,', "'
+        ,a.[Action]
+        ,'", '
+        ,CASE a.AllowOrDeny
+            WHEN 'A'
+                THEN 'true'
+            ELSE 'false'
+            END
+        ,', "'
+        ,g.[Guid]
+        ,'", '
+        ,a.SpecialRole
+        ,', "'
+        ,a.[Guid]
+        ,'" );'
+        ,' // EntityType:'
+        ,et.Name
+		,' Group: '
+        ,CASE a.SpecialRole
+            WHEN 0
+                THEN CONCAT (
+                        g.[Guid]
+                        ,' ( '
+                        ,g.NAME
+                        ,' ), '
+                        )
+            WHEN 1
+                THEN '<all users>'
+            WHEN 2
+                THEN '<all authenticated users>'
+            WHEN 3
+                THEN '<all un-authenticated users>'
+            END
+        
+        ) [Up]
+    ,CONCAT (
+        'RockMigrationHelper.DeleteSecurityAuth( "'
+        ,a.[Guid]
+        ,'");'
+        ,' // EntityType:'
+        ,et.Name
+        ,' Group: '
+        ,CASE a.SpecialRole
+            WHEN 0
+                THEN CONCAT (
+                        g.[Guid]
+                        ,' ( '
+                        ,g.NAME
+                        ,' ), '
+                        )
+            WHEN 1
+                THEN '<all users>'
+            WHEN 2
+                THEN '<all authenticated users>'
+            WHEN 3
+                THEN '<all un-authenticated users>'
+            END
+        ) [Down]
+    ,et.[Name] + CHAR(9) [EntityType]
     ,a.[Order]
     ,a.[Action]
     ,a.AllowOrDeny
@@ -129,6 +194,6 @@ SELECT b.[Name] + CHAR(9) [EntityType]
     ,a.[Guid] [Auth.Guid]
 FROM Auth a
 LEFT JOIN [Group] g ON a.GroupId = g.Id
-JOIN [EntityType] b ON a.EntityTypeId = b.Id
+JOIN [EntityType] et ON a.EntityTypeId = et.Id
 WHERE a.EntityId = 0
 ORDER BY isnull(a.ModifiedDateTime, '1900-01-01') DESC
