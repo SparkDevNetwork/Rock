aa09e98a51657d31c7cc9e14cb1a348135ca798a
Mike Peterson <MikePeterson@ccvonline.com>
2015-09-09 17:25:51 -0700

    - more progress on EmployeeTitheImport

--- RockWeb/Plugins/church_ccv/Finance/EmployeeTitheImport.ascx
+++ RockWeb/Plugins/church_ccv/Finance/EmployeeTitheImport.ascx
@@ -17,11 +17,12 @@
                 <%-- Configure Import Panel --%>
                 <asp:Panel ID="pnlConfigure" runat="server" Visible="false">
                     <h4>Account Mapping</h4>
-                    <Rock:Grid ID="gMapAccounts" runat="server" DisplayType="Light" OnRowCreated="gMapAccounts_RowCreated">
+                    <Rock:Grid ID="gMapAccounts" runat="server" DisplayType="Light" OnRowCreated="gMapAccounts_RowCreated" OnRowDataBound="gMapAccounts_RowDataBound">
                         <Columns>
                             <Rock:RockBoundField HeaderText="Column" DataField="CampusCode" />
                             <Rock:RockTemplateField HeaderText="Account">
                                 <ItemTemplate>
+                                    <asp:HiddenField ID="hfCampusCode" runat="server" />
                                     <Rock:RockDropDownList ID="ddlAccount" runat="server" />
                                 </ItemTemplate>
                             </Rock:RockTemplateField>
@@ -31,7 +32,7 @@
 
                     <h4>Currency Type</h4>
                     <Rock:RockDropDownList ID="ddlCurrencyType" runat="server" />
-                    
+
                     <br />
                     <div class="actions pull-right">
                         <asp:LinkButton ID="btnNext" runat="server" AccessKey="n" Text="Next" CssClass="btn btn-primary" OnClick="btnNext_Click" />
@@ -41,6 +42,47 @@
 
                 <%-- Import Preview Panel --%>
                 <asp:Panel ID="pnlImportPreview" runat="server" Visible="false">
+                    <div class="pull-right">
+                        <asp:Literal ID="lUnmatchedRecords" runat="server" Text="" /></div>
+                    <h4>Preview</h4>
+                    <Rock:Grid ID="gImportPreview" runat="server" AllowPaging="false">
+                        <Columns>
+                            <Rock:RockBoundField DataField="EmployeeId" HeaderText="Employee Id" />
+                            <Rock:RockBoundField DataField="ImportPersonName" HeaderText="Import Name" />
+                            <Rock:PersonField DataField="RockPerson" HeaderText="Rock Name" NullDisplayText="<span class='badge badge-danger'>NOT FOUND</span>" />
+                            <Rock:DateField DataField="PayDate" HeaderText="Date" />
+                        </Columns>
+                    </Rock:Grid>
+
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-8 margin-t-md">
+                            <asp:Panel ID="pnlSummary" runat="server" CssClass="panel panel-block">
+                                <div class="panel-heading">
+                                    <h1 class="panel-title">Account Totals</h1>
+                                </div>
+                                <div class="panel-body">
+                                    <asp:Repeater ID="rptAccountSummary" runat="server">
+                                        <ItemTemplate>
+                                            <div class='row'>
+                                                <div class='col-xs-8'><%#Eval("Name")%></div>
+                                                <div class='col-xs-4 text-right'><%#Eval("TotalAmount")%></div>
+                                            </div>
+                                        </ItemTemplate>
+                                    </asp:Repeater>
+                                    <div class='row'>
+                                        <div class='col-xs-8'><b>Total: </div>
+                                        <div class='col-xs-4 text-right'>
+                                            <asp:Literal ID="lGrandTotal" runat="server" /></b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </asp:Panel>
+                        </div>
+                    </div>
+
+                    <div class="actions pull-right">
+                        <asp:LinkButton ID="btnImport" runat="server" AccessKey="i" Text="Import" CssClass="btn btn-primary" OnClick="btnImport_Click" />
+                    </div>
                 </asp:Panel>
 
 
@@ -51,9 +93,6 @@
                         <asp:LinkButton ID="btnViewBatch" runat="server" AccessKey="v" Text="View Batch" CssClass="btn btn-primary" OnClick="btnViewBatch_Click" />
                     </div>
                 </asp:Panel>
-
-
-
 
             </div>
 
--- RockWeb/Plugins/church_ccv/Finance/EmployeeTitheImport.ascx.cs
+++ RockWeb/Plugins/church_ccv/Finance/EmployeeTitheImport.ascx.cs
@@ -28,17 +28,18 @@ using Rock.Model;
 using Rock.Web.Cache;
 using Rock.Web.UI.Controls;
 using Rock.Attribute;
+using Rock.Web.UI;
 
 namespace RockWeb.Plugins.church_ccv.Finance
 {
     /// <summary>
-    /// Template block for developers to use to start a new block.
+    /// Imports Employee Tithe records
     /// </summary>
     [DisplayName( "Employee Tithe Import" )]
     [Category( "CCV > Finance" )]
     [Description( "Imports Employee Tithe records" )]
 
-    public partial class EmployeeTitheImport : Rock.Web.UI.RockBlock
+    public partial class EmployeeTitheImport : RockBlock
     {
         #region Base Control Methods
 
@@ -76,12 +77,11 @@ namespace RockWeb.Plugins.church_ccv.Finance
             pnlConfigure.Visible = true;
             pnlImportPreview.Visible = false;
             pnlDone.Visible = false;
-            int lastNameCol = 0;
-            int firstNameCol = 1;
-            int locationCol = 2;
-            int payDateCol = 3;
-            int employeeNumberCol = 4;
             int campusColStartCol = 5;
+
+            string keyPrefix = string.Format( "employee-tithe-import-{0}-", this.BlockId );
+
+            
 
             var rockContext = new RockContext();
             var binaryFileService = new BinaryFileService( rockContext );
@@ -100,6 +100,7 @@ namespace RockWeb.Plugins.church_ccv.Finance
                         for ( int campusColumnIndex = campusColStartCol; campusColumnIndex < headerLineParts.Count(); campusColumnIndex++ )
                         {
                             var campusAccountMapping = new CampusAccountMapping { CampusCode = headerLineParts[campusColumnIndex] };
+                            campusAccountMapping.FinancialAccountId = this.GetUserPreference( keyPrefix + campusAccountMapping.CampusCode + "_account_id" ).AsIntegerOrNull();
                             campusCodes.Add( campusAccountMapping );
                         }
                     }
@@ -116,6 +117,8 @@ namespace RockWeb.Plugins.church_ccv.Finance
             {
                 ddlCurrencyType.Items.Add( new ListItem( currencyType.Value, currencyType.Id.ToString() ) );
             }
+
+            ddlCurrencyType.SetValue( this.GetUserPreference( keyPrefix + "currency-type-value-id" ) );
         }
 
         /// <summary>
@@ -137,7 +140,58 @@ namespace RockWeb.Plugins.church_ccv.Finance
             /// <value>
             /// The account identifier.
             /// </value>
-            public int? AccountId { get; set; }
+            public int? FinancialAccountId { get; set; }
+
+            /// <summary>
+            /// Gets or sets the financial account.
+            /// </summary>
+            /// <value>
+            /// The financial account.
+            /// </value>
+            public FinancialAccount FinancialAccount { get; set; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class FinancialAccountAmountInfo
+        {
+            /// <summary>
+            /// Gets or sets the financial account.
+            /// </summary>
+            /// <value>
+            /// The financial account.
+            /// </value>
+            public FinancialAccount FinancialAccount { get; set; }
+
+            /// <summary>
+            /// Gets or sets the amount.
+            /// </summary>
+            /// <value>
+            /// The amount.
+            /// </value>
+            public decimal? Amount { get; set; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class ImportRowData
+        {
+            public string EmployeeId { get; set; }
+            public string ImportFirstName { get; set; }
+            public string ImportLastName { get; set; }
+            public DateTime? PayDate { get; set; }
+            public Person RockPerson { get; set; }
+            public List<FinancialAccountAmountInfo> FinancialAccountAmountInfoList { get; set; }
+
+            public string ImportPersonName
+            {
+                get
+                {
+                    return string.Format( "{0} {1}", ImportFirstName, ImportLastName );
+                }
+            }
         }
 
         /// <summary>
@@ -147,7 +201,113 @@ namespace RockWeb.Plugins.church_ccv.Finance
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void btnNext_Click( object sender, EventArgs e )
         {
+            string keyPrefix = string.Format( "employee-tithe-import-{0}-", this.BlockId );
+            foreach ( var row in gMapAccounts.Rows.OfType<GridViewRow>() )
+            {
+                var ddlAccount = row.FindControl( "ddlAccount" ) as DropDownList;
+                var hfCampusCode = row.FindControl( "hfCampusCode" ) as HiddenField;
+                if (ddlAccount != null && hfCampusCode != null)
+                {
+                    this.SetUserPreference( keyPrefix + hfCampusCode.Value + "_account_id", ddlAccount.SelectedValue );
+                }
+            }
 
+            this.SetUserPreference( keyPrefix + "currency-type-value-id", ddlCurrencyType.SelectedValue );
+            
+            pnlStart.Visible = false;
+            pnlConfigure.Visible = false;
+            pnlImportPreview.Visible = true;
+            pnlDone.Visible = false;
+            int lastNameCol = 0;
+            int firstNameCol = 1;
+
+            //// not Used
+            //// int locationCol = 2;
+
+            int payDateCol = 3;
+            int employeeNumberCol = 4;
+            int campusColStartCol = 5;
+            List<CampusAccountMapping> campusAccountMappingList = new List<CampusAccountMapping>();
+
+            var rockContext = new RockContext();
+            var binaryFileService = new BinaryFileService( rockContext );
+            var binaryFile = binaryFileService.Get( fuImport.BinaryFileId ?? 0 );
+            if ( binaryFile != null )
+            {
+                string importData = binaryFile.ContentsToString();
+                var importLines = importData.Split( new string[] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries );
+                string headerLine = importLines[0];
+                var headerLineParts = headerLine.Split( ',' );
+                if ( headerLineParts.Count() > 5 )
+                {
+                    for ( int campusColumnIndex = campusColStartCol; campusColumnIndex < headerLineParts.Count(); campusColumnIndex++ )
+                    {
+                        var campusAccountMapping = new CampusAccountMapping { CampusCode = headerLineParts[campusColumnIndex] };
+                        campusAccountMappingList.Add( campusAccountMapping );
+                    }
+                }
+
+                foreach ( var campusAccountMapping in campusAccountMappingList )
+                {
+                    CallbackField callbackField = new CallbackField();
+                    callbackField.DataField = "FinancialAccountAmountInfoList";
+
+                    callbackField.OnFormatDataValue += ( s, args ) =>
+                    {
+                        var financialAccountAmountInfoList = args.DataValue as List<FinancialAccountAmountInfo>;
+                        if ( financialAccountAmountInfoList != null )
+                        {
+                            var employeeAccountAmount = financialAccountAmountInfoList.FirstOrDefault( a => a.FinancialAccount == campusAccountMapping.FinancialAccount );
+                            if ( employeeAccountAmount != null )
+                            {
+                                args.FormattedValue = employeeAccountAmount.Amount.FormatAsCurrency();
+                            }
+                            else
+                            {
+                                args.FormattedValue = string.Empty;
+                            }
+                        }
+                    };
+
+                    gImportPreview.Columns.Add( callbackField );
+                }
+
+                List<ImportRowData> importDataRows = new List<ImportRowData>();
+
+                var importDataLines = importLines.Skip( 1 ).ToList();
+                foreach ( var importDataLine in importDataLines )
+                {
+                    var importDataLineParts = importDataLine.Split( ',' );
+                    if ( importDataLines.Count >= campusColStartCol )
+                    {
+                        if ( importDataLineParts.Any( a => !string.IsNullOrWhiteSpace( a ) ) )
+                        {
+                            var importDataRow = new ImportRowData();
+                            importDataRow.EmployeeId = importDataLineParts[employeeNumberCol];
+                            importDataRow.ImportLastName = importDataLineParts[lastNameCol];
+                            importDataRow.ImportFirstName = importDataLineParts[firstNameCol];
+                            importDataRow.PayDate = importDataLineParts[payDateCol].AsDateTime();
+                            importDataRow.FinancialAccountAmountInfoList = new List<FinancialAccountAmountInfo>();
+
+                            for ( int campusColumnIndex = campusColStartCol; campusColumnIndex < importDataLineParts.Count(); campusColumnIndex++ )
+                            {
+                                FinancialAccountAmountInfo financialAccountAmountInfo = new FinancialAccountAmountInfo();
+                                financialAccountAmountInfo.Amount = importDataLineParts[campusColumnIndex].AsDecimalOrNull();
+                                importDataRow.FinancialAccountAmountInfoList.Add( financialAccountAmountInfo );
+                            }
+
+                            importDataRows.Add( importDataRow );
+                        }
+                    }
+                }
+
+                int unmatchedCount = importDataRows.Where( a => a.RockPerson == null ).Count();
+                string labelClass = unmatchedCount > 0 ? "danger" : "success";
+                lUnmatchedRecords.Text = string.Format( "<span class='label label-{0}'>Unmatched Records: {1}</span>", labelClass, unmatchedCount );
+
+                gImportPreview.DataSource = importDataRows;
+                gImportPreview.DataBind();
+            }
         }
 
         #endregion
@@ -176,5 +336,33 @@ namespace RockWeb.Plugins.church_ccv.Finance
                 }
             }
         }
-    }
+
+        /// <summary>
+        /// Handles the RowDataBound event of the gMapAccounts control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="GridViewRowEventArgs"/> instance containing the event data.</param>
+        protected void gMapAccounts_RowDataBound( object sender, GridViewRowEventArgs e )
+        {
+            var ddlAccount = e.Row.FindControl( "ddlAccount" ) as RockDropDownList;
+            var hfCampusCode = e.Row.FindControl( "hfCampusCode" ) as HiddenField;
+            CampusAccountMapping campusAccountMapping = e.Row.DataItem as CampusAccountMapping;
+            if ( ddlAccount != null && campusAccountMapping != null && hfCampusCode != null )
+            {
+                ddlAccount.SelectedValue = campusAccountMapping.FinancialAccountId.ToString();
+                hfCampusCode.Value = campusAccountMapping.CampusCode;
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnImport control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnImport_Click( object sender, EventArgs e )
+        {
+
+        }
+        
+}
 }
