df7f4865ea1e3da4af0d8188b5c242329e714ef3
edolyne <ed@edolyne.com>
2015-09-10 12:44:22 -0400

    ROCK-621 Rock Error Styles for NewSpring

--- Metrics/Metrics.ascx.cs
+++ Metrics/Metrics.ascx.cs
@@ -119,6 +119,8 @@ namespace RockWeb.Plugins.cc_newspring.Blocks.Metrics
         {
             base.OnInit( e );
 
+            throw new Exception();
+
             #region Campus Context
 
             var campusEntityType = EntityTypeCache.Read( "Rock.Model.Campus" );
