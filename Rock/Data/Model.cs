2669d554174c08ffbcbcc14f97744ee0471c1807
Mike Peterson <MikePeterson@ccvonline.com>
2015-10-02 10:40:19 -0700

    - updated the ModifiedAuditValuesAlreadyUpdated logic to only apply to ModifiedByPersonAliasId (not ModifiedDateTime). Also, fixed an issue where ModifiedAuditValuesAlreadyUpdated would not stay set as true if PUT thru the Rest API

--- Rock/Data/Model.cs
+++ Rock/Data/Model.cs
@@ -59,7 +59,7 @@ namespace Rock.Data
         /// </value>
         [DataMember]
         [IncludeForReporting]
-        [RockClientInclude( "If you need to set this manually, set ModifiedAuditValuesAlreadyUpdated=True to prevent Rock from setting it" )]
+        [RockClientInclude( "This does not need to be set or changed. Rock will always set this to the current date/time when saved to the database." )]
         public DateTime? ModifiedDateTime { get; set; }
 
         /// <summary>
@@ -178,17 +178,17 @@ namespace Rock.Data
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the ModifiedByPersonAliasId and ModifiedDateTime values have already been 
-        /// updated to reflect who/when model was updated. If this value is false (default) the framework will update 
-        /// these values with the current user and datetime when the model is saved. Set this value to true if this automatic
+        /// Gets or sets a value indicating whether the ModifiedByPersonAliasId value has already been
+        /// updated to reflect who/when model was updated. If this value is false (default) the framework will update
+        /// the value with the current user when the model is saved. Set this value to true if this automatic
         /// update should not be done.
         /// </summary>
         /// <value>
-        /// <c>false</c> if rock should set the ModifiedDateTime to current time and ModifiedByPersonAliasId to current user when saving model; otherwise, <c>true</c>.
+        /// <c>false</c> if rock should set the ModifiedByPersonAliasId to current user when saving model; otherwise, <c>true</c>.
         /// </value>
         [NotMapped]
         [DataMember]
-        [RockClientInclude("If the ModifiedByPersonAliasId and ModifiedDateTime properties are being set manually and should not be overwritten with current time/user when saved, set this value to true")]
+        [RockClientInclude("If the ModifiedByPersonAliasId is being set manually and should not be overwritten with current user when saved, set this value to true")]
         public virtual bool ModifiedAuditValuesAlreadyUpdated { get; set; }
 
         #endregion
