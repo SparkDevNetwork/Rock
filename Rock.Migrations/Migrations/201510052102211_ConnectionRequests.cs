9e9308ac07657fddba500453c7f9269b8caa75a8
Jon Edmiston <jon@sparkdevnetwork.org>
2015-10-05 18:15:46 -0700

    - added group member notes to migration

--- Rock.Migrations/Migrations/201510052102211_ConnectionRequests.cs
+++ Rock.Migrations/Migrations/201510052102211_ConnectionRequests.cs
@@ -87,6 +87,51 @@ SET MediumDataJson = REPLACE(MediumDataJson, '<a href=\""{{ GlobalAttribute.Publ
 WHERE [Guid] = 'AFE2ADD1-5278-441E-8E84-1DC743D99824'
     AND MediumDataJson LIKE '%<a href=\""{{ GlobalAttribute.PublicApplicationRoot }}\"" style=\""color: #2ba6cb; text-decoration: none;\"">{{ GlobalAttribute.OrganizationWebsite }}</a>%'
 " );
+
+            // add new note type
+            Sql( @"IF  NOT EXISTS (SELECT * FROM [NoteType] WHERE [GUID] = 'FFFC3644-60CD-4D14-A714-E8DCC202A0E1')
+  BEGIN
+	DECLARE @GroupMemberEntityTypeId int = (SELECT TOP 1 [Id] FROM [EntityType] WHERE [Guid] = '49668B95-FEDC-43DD-8085-D2B0D6343C48')
+	
+	INSERT INTO [NoteType]
+		([IsSystem], [EntityTypeId], [Name], [Guid], [UserSelectable], [IconCssClass], [EntityTypeQualifierColumn], [EntityTypeQualifierValue])
+	VALUES
+		(1, @GroupMemberEntityTypeId, 'Group Member Note', 'FFFC3644-60CD-4D14-A714-E8DCC202A0E1', 1, 'fa fa-users', '', '')
+  END" );
+            
+            // add group member notes
+            RockMigrationHelper.AddBlock( "3905C63F-4D57-40F0-9721-C60A2F681911", "", "2E9F32D4-B4FC-4A5F-9BE1-B2E3EA624DD3", "Notes", "Main", "", "", 1, "66443F51-D210-48E3-A8A9-063D3806F43F" );
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "F1BCF615-FBCA-4BC2-A912-C35C0DC04174", @"49668b95-fedc-43dd-8085-d2b0d6343c48" ); // Entity Type
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "D68EE1F5-D29F-404B-945D-AD0BE76594C3", @"True" ); // Show Private Checkbox
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "00B6EBFF-786D-453E-8746-119D0B45CB3E", @"True" ); // Show Security Button
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "20243A98-4802-48E2-AF61-83956056AC65", @"True" ); // Show Alert Checkbox
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "3CB0A7DF-996B-4D6C-B3B6-9BBCC40BDC69", @"Group Member Notes" ); // Heading
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "B69937BE-000A-4B94-852F-16DE92344392", @"fa fa-comments-o" ); // Heading Icon CSS Class
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "FD0727DC-92F4-4765-82CB-3A08B7D864F8", @"Note" ); // Note Term
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "5232BFAE-4DC8-4270-B38F-D29E1B00AB5E", @"Full" ); // Display Type
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "C05757C0-E83E-4170-8CBF-C4E1ABEC36E1", @"False" ); // Use Person Icon
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "EB9CBD02-2B0F-4BA3-9112-BC73D54159E7", @"False" ); // Allow Anonymous
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "8E0BDD15-6B92-4BB0-9138-E9382B60F3A9", @"False" ); // Add Always Visible
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "C9FC2C09-1BF5-4711-8F97-0B96633C46B1", @"Descending" ); // Display Order
+
+            RockMigrationHelper.AddBlockAttributeValue( "66443F51-D210-48E3-A8A9-063D3806F43F", "6184511D-CC68-4FF2-90CB-3AD0AFD59D61", @"False" ); // Allow Backdated Notes
+
+            // Add/Update PageContext for Page:Group Member Detail, Entity: Rock.Model.GroupMember, Parameter: GroupMemberId
+            RockMigrationHelper.UpdatePageContext( "3905C63F-4D57-40F0-9721-C60A2F681911", "Rock.Model.GroupMember", "GroupMemberId", "5683CC10-31CA-4D63-8EA6-4E2186055BD1" );
+
+
+
         }
         
         /// <summary>
