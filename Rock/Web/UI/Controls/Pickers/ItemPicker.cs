4369bebc1dea379fe878921efafec85f9cd30529
Mike Peterson <MikePeterson@ccvonline.com>
2015-10-09 11:46:14 -0700

    - fix a couple of other rendered ID's in ItemPicker that should have rendered ClientID instead of ID

--- Rock/Web/UI/Controls/Pickers/ItemPicker.cs
+++ Rock/Web/UI/Controls/Pickers/ItemPicker.cs
@@ -621,7 +621,7 @@ namespace Rock.Web.UI.Controls
                         <b class='fa fa-caret-down pull-right'></b>
                     </a>";
 
-                    writer.Write( pickerLabelHtmlFormat, this.ID, this.ItemName, this.IconCssClass );
+                    writer.Write( pickerLabelHtmlFormat, this.ClientID, this.ItemName, this.IconCssClass );
 
                     writer.WriteLine();
 
@@ -659,13 +659,13 @@ namespace Rock.Web.UI.Controls
                                     </div>
                                 </div>
                             </div>",
-                           this.ID );
+                           this.ClientID );
 
                 // picker actions
                 writer.AddAttribute( "class", "picker-actions" );
                 writer.RenderBeginTag( HtmlTextWriterTag.Div );
                 _btnSelect.RenderControl( writer );
-                writer.Write( "<a class='btn btn-xs btn-link picker-cancel' id='btnCancel_{0}'>Cancel</a>", this.ID );
+                writer.Write( "<a class='btn btn-xs btn-link picker-cancel' id='btnCancel_{0}'>Cancel</a>", this.ClientID );
 
                 // render any additional picker actions that a child class if ItemPicker implements
                 RenderCustomPickerActions( writer );
