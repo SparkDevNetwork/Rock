5ef22dc8710ad27cea843ba9b908a00a8ba45968
David Turner <david@sparkdevnetwork.org>
2015-10-20 17:11:56 -0700

    - Updated the registrant list so that it includes person fields in Lava so that common merge templates (i.e. nametags) can be used

--- Rock/Model/RegistrationRegistrant.cs
+++ Rock/Model/RegistrationRegistrant.cs
@@ -134,6 +134,86 @@ namespace Rock.Model
         private ICollection<RegistrationRegistrantFee> _fees;
 
         /// <summary>
+        /// Gets the name of the nick.
+        /// </summary>
+        /// <value>
+        /// The name of the nick.
+        /// </value>
+        [NotMapped]
+        [LavaInclude]
+        public virtual string NickName 
+        {
+            get 
+            {
+                if ( PersonAlias != null && PersonAlias.Person != null )
+                {
+                    return PersonAlias.Person.NickName;
+                }
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Gets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        [NotMapped]
+        [LavaInclude]
+        public virtual string FirstName 
+        {
+            get 
+            {
+                if ( PersonAlias != null && PersonAlias.Person != null )
+                {
+                    return PersonAlias.Person.FirstName;
+                }
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Gets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        [NotMapped]
+        [LavaInclude]
+        public virtual string LastName
+        {
+            get 
+            {
+                if ( PersonAlias != null && PersonAlias.Person != null )
+                {
+                    return PersonAlias.Person.LastName;
+                }
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Gets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        [NotMapped]
+        [LavaInclude]
+        public virtual string Email
+        {
+            get
+            {
+                if ( PersonAlias != null && PersonAlias.Person != null )
+                {
+                    return PersonAlias.Person.Email;
+                }
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
         /// Gets the cost with fees.
         /// </summary>
         /// <value>
