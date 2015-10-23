77b1a2c1a3726ec793a0240cf578dae8f0f433fa
Mike Peterson <MikePeterson@ccvonline.com>
2015-10-23 16:05:12 -0700

    - fix bug introduced in last commit that caused person name not to show in groupmemberlist

--- RockWeb/Blocks/Groups/GroupMemberList.ascx.cs
+++ RockWeb/Blocks/Groups/GroupMemberList.ascx.cs
@@ -954,7 +954,7 @@ namespace RockWeb.Blocks.Groups
                         m.Guid,
                         m.PersonId,
                         Name =
-                        selectAll ? m.Person.LastName + ", " + m.Person.NickName : ( m.Person.PhotoId.HasValue ? "<i class='fa fa-fw fa-user photo-icon has-photo js-person-popover' personid=" + m.PersonId.ToString() + "></i> " : "<i class='fa fa-fw photo-icon js-person-popover' personid=" + m.PersonId.ToString() + "></i> " +
+                        ( selectAll ? m.Person.LastName + ", " + m.Person.NickName : ( m.Person.PhotoId.HasValue ? "<i class='fa fa-fw fa-user photo-icon has-photo js-person-popover' personid=" + m.PersonId.ToString() + "></i> " : "<i class='fa fa-fw photo-icon js-person-popover' personid=" + m.PersonId.ToString() + "></i> " ) +
                         m.Person.NickName + " " + m.Person.LastName
                             + ( hasGroupRequirements && groupMemberIdsThatLackGroupRequirements.Contains( m.Id )
                                 ? " <i class='fa fa-exclamation-triangle text-warning'></i>"
