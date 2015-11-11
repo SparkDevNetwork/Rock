dbcd1ffe100fe99f2b4980f16080208ec4cf0583
tcavaletto <Taylor.Cavaletto@centralAZ.com>
2015-11-11 13:29:01 -0700

    - Migration fix

--- com.centralaz.Baptism/Migrations/008_BaptismIsDeleted.cs
+++ com.centralaz.Baptism/Migrations/008_BaptismIsDeleted.cs
@@ -15,8 +15,12 @@ namespace com.centralaz.Baptism.Migrations
         public override void Up()
         {
             Sql( @"
+            IF NOT EXISTS(SELECT * FROM sys.columns 
+            WHERE Name = N'IsDeleted' AND Object_ID = Object_ID(N'[_com_centralaz_Baptism_Baptizee]'))
+BEGIN
             ALTER TABLE [_com_centralaz_Baptism_Baptizee]
             ADD IsDeleted BIT
+END
             " );
             //Adding the value for the Linked Page Attribute "Blackout Day Page" on the BaptismCampusDetail block
             RockMigrationHelper.AddBlockAttributeValue( "94256870-5A3A-4BAE-A489-6BA9F94C0ED0", "CB638267-0065-4FFC-A665-BCE475BD022D", @"a3882eae-f086-467b-9f3c-dc0db75403f7" );
