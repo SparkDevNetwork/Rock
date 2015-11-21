dc519b03a6c27d49e5fbb0b22f77a12f2f87b697
Mike Peterson <MikePeterson@ccvonline.com>
2015-11-20 18:01:25 -0700

    - remove stock Image plugin from CKEditor toolbar so it doesn't get confused with our Rock Image plugin

--- RockWeb/Scripts/Rock/Controls/htmlEditor.js
+++ RockWeb/Scripts/Rock/Controls/htmlEditor.js
@@ -10,7 +10,7 @@
             toolbar_RockCustomConfigLight:
 	            [
                     ['Source'],
-                    ['Bold', 'Italic', 'Underline', 'Strike', 'NumberedList', 'BulletedList', 'Link', 'Image', 'PasteFromWord', '-', 'Undo', 'Redo', '-', 'RemoveFormat'],
+                    ['Bold', 'Italic', 'Underline', 'Strike', 'NumberedList', 'BulletedList', 'Link', 'PasteFromWord', '-', 'Undo', 'Redo', '-', 'RemoveFormat'],
                     ['Format'],
                     ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
                     ['rockmergefield', '-', 'rockimagebrowser', 'rockdocumentbrowser']
