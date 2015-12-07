e9143ea361dfedda1e7a3ae176eea7b6585929f2
Mike Peterson <MikePeterson@ccvonline.com>
2015-12-07 14:53:23 -0700

    - added new CCV-specific Person Badge to show their Neighborhood Area(s) based on their address and home campus

---
+++ church.ccv.Badges/Person/GeofencedByCampusGroup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Web.Cache;
+
+namespace church.ccv.Badges.Person
+{
+    /// <summary>
+    /// Campus Badge
+    /// </summary>
+    [Description( "Displays the campus group(s) of a particular type that have a geo-fence location around one or more of the the person's map locations for their campus." )]
+    [Export( typeof( Rock.PersonProfile.BadgeComponent ) )]
+    [ExportMetadata( "ComponentName", "Geofenced By Campus Group" )]
+
+    [GroupTypeField( "Group Type", "The type of group to use.", true )]
+    [TextField( "Badge Color", "The color of the badge (#ffffff).", true, "#0ab4dd" )]
+    public class GeofencedByCampusGroup : Rock.PersonProfile.BadgeComponent
+    {
+        /// <summary>
+        /// Renders the specified writer.
+        /// </summary>
+        /// <param name="badge">The badge.</param>
+        /// <param name="writer">The writer.</param>
+        public override void Render( PersonBadgeCache badge, System.Web.UI.HtmlTextWriter writer )
+        {
+            Guid? groupTypeGuid = GetAttributeValue( badge, "GroupType" ).AsGuid();
+            string badgeColor = GetAttributeValue( badge, "BadgeColor" );
+
+            if ( groupTypeGuid.HasValue && !string.IsNullOrEmpty( badgeColor ) )
+            {
+                writer.Write( string.Format(
+                    "<span class='label badge-geofencing-group badge-id-{0}' style='background-color:{1};display:none' ></span>",
+                    badge.Id,
+                    badgeColor.EscapeQuotes() ) );
+
+                writer.Write( string.Format( 
+                    @"
+                    <script>
+                    Sys.Application.add_load(function () {{
+                                                
+                        $.ajax({{
+                                type: 'GET',
+                                url: Rock.settings.get('baseUrl') + 'api/CCV/Badges/GeofencingCampusGroups/{0}/{1}' ,
+                                statusCode: {{
+                                    200: function (data, status, xhr) {{
+                                        var $badge = $('.badge-geofencing-group.badge-id-{2}');
+                                        var badgeHtml = '';
+
+                                        $.each(data, function() {{
+                                            if ( badgeHtml != '' ) {{ 
+                                                badgeHtml += ' | ';
+                                            }}
+                                            badgeHtml += '<span title=""' + this.LeaderNames + '"" data-toggle=""tooltip"">' + this.GroupName + '</span>';
+                                        }});
+
+                                        if (badgeHtml != '') {{
+                                            $badge.show('fast');
+                                        }} else {{
+                                            $badge.hide();
+                                        }}
+                                        $badge.html(badgeHtml);
+                                        $badge.find('span').tooltip();
+                                    }}
+                                }},
+                        }});
+                    }});
+                    </script>
+                
+                    ",
+                     Person.Id.ToString(),
+                     groupTypeGuid.ToString(),
+                     badge.Id ) );
+            }
+        }
+    }
+}
---
+++ church.ccv.Badges/Rest/Controllers/CCVBadgesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Rest.Filters;
+
+namespace church.ccv.Badges.Rest.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class CCVBadgesController : Rock.Rest.ApiControllerBase
+    {
+        /// <summary>
+        /// Returns groups that are a specified type and geofence a given person for their home campus
+        /// </summary>
+        /// <param name="personId">The person id.</param>
+        /// <returns></returns>
+        [Authenticate, Secured]
+        [HttpGet]
+        [System.Web.Http.Route( "api/CCV/Badges/GeofencingCampusGroups/{personId}/{groupTypeGuid}" )]
+        public List<GroupAndLeaderInfo> GetGeofencingCampusGroups( int personId, Guid groupTypeGuid )
+        {
+            var rockContext = new RockContext();
+            var groupMemberService = new GroupMemberService( rockContext );
+            int? campusId = null;
+            var person = new PersonService( rockContext ).Get( personId );
+            if ( person != null )
+            {
+                var campus = person.GetCampus();
+                if ( campus != null )
+                {
+                    campusId = campus.Id;
+                }
+            }
+
+            var groups = new GroupService( rockContext ).GetGeofencingGroups( personId, groupTypeGuid ).AsNoTracking();
+            var campusGroups = groups.Where( a => a.CampusId == campusId );
+
+            var result = new List<GroupAndLeaderInfo>();
+            foreach ( var group in campusGroups.OrderBy( g => g.Name ) )
+            {
+                var info = new GroupAndLeaderInfo();
+                info.GroupName = group.Name.Trim();
+                info.LeaderNames = groupMemberService
+                    .Queryable().AsNoTracking()
+                    .Where( m =>
+                        m.GroupId == group.Id &&
+                        m.GroupRole.IsLeader )
+                    .Select( m => m.Person.NickName + " " + m.Person.LastName )
+                    .ToList()
+                    .AsDelimited( ", " );
+                result.Add( info );
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Group and Leader name info
+        /// </summary>
+        public class GroupAndLeaderInfo
+        {
+            /// <summary>
+            /// Gets or sets the name of the group.
+            /// </summary>
+            /// <value>
+            /// The name of the group.
+            /// </value>
+            public string GroupName { get; set; }
+
+            /// <summary>
+            /// Gets or sets the leader names.
+            /// </summary>
+            /// <value>
+            /// The leader names.
+            /// </value>
+            public string LeaderNames { get; set; }
+        }
+    }
+}
