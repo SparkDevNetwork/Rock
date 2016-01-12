b12bc140235e27725566dce699b8f2fbf2bdfc72
Mike Peterson <MikePeterson@ccvonline.com>
2016-01-12 16:07:13 -0700

    - added new DatamartPersonFieldSelect that lets you choose any field from DatamartPerson to be shown in a Report - added new DatamartCampusFilter that lets you filter Person by campus using the datamart value of campus (which should be faster then using the core campus filter)

---
+++ church.ccv.Datamart/Reporting/DataFilter/Person/DatamartCampusFilter.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using church.ccv.Datamart.Model;
+
+using Rock.Data;
+using Rock.Model;
+using Rock.Reporting;
+using Rock.Web.UI.Controls;
+
+namespace church.ccv.Datamart.Reporting.DataFilter.Person
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Description( "Filter people that are associated with a specific campus using Datamart." )]
+    [Export( typeof( DataFilterComponent ) )]
+    [ExportMetadata( "ComponentName", "Person Datamart Campus Filter" )]
+    public class DatamartCampusFilter : DataFilterComponent
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the entity type that filter applies to.
+        /// </summary>
+        /// <value>
+        /// The entity that filter applies to.
+        /// </value>
+        public override string AppliesToEntityType
+        {
+            get { return typeof( Rock.Model.Person ).FullName; }
+        }
+
+        /// <summary>
+        /// Gets the section.
+        /// </summary>
+        /// <value>
+        /// The section.
+        /// </value>
+        public override string Section
+        {
+            get { return "Datamart Filters"; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <param name="entityType"></param>
+        /// <returns></returns>
+        /// <value>
+        /// The title.
+        /// </value>
+        public override string GetTitle( Type entityType )
+        {
+            return "Campus";
+        }
+
+        /// <summary>
+        /// Formats the selection on the client-side.  When the filter is collapsed by the user, the Filterfield control
+        /// will set the description of the filter to whatever is returned by this property.  If including script, the
+        /// controls parent container can be referenced through a '$content' variable that is set by the control before 
+        /// referencing this property.
+        /// </summary>
+        /// <value>
+        /// The client format script.
+        /// </value>
+        public override string GetClientFormatSelection( Type entityType )
+        {
+            return @"
+function() {
+    var campusPicker = $('.js-campus-picker', $content);
+    var campusName = $(':selected', campusPicker).text();
+
+    return 'Campus: ' + campusName;
+}
+";
+        }
+
+        /// <summary>
+        /// Formats the selection.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="selection">The selection.</param>
+        /// <returns></returns>
+        public override string FormatSelection( Type entityType, string selection )
+        {
+            string result = "Campus";
+            string[] selectionValues = selection.Split( '|' );
+
+            if ( selectionValues.Length >= 1 )
+            {
+                string campusName = selectionValues[0];
+                result = "Campus: " + campusName;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Creates the child controls.
+        /// </summary>
+        /// <returns></returns>
+        public override Control[] CreateChildControls( Type entityType, FilterField filterControl )
+        {
+            RockDropDownList ddlCampusPicker = new RockDropDownList();
+            ddlCampusPicker.ID = filterControl.ID + "_0";
+            ddlCampusPicker.Label = string.Empty;
+            ddlCampusPicker.CssClass = "js-campus-picker";
+
+            var campusNames = Rock.Web.Cache.CampusCache.All().Select( a => a.Name );
+
+            ddlCampusPicker.Items.Clear();
+            ddlCampusPicker.Items.Add( new ListItem() );
+            ddlCampusPicker.Items.AddRange( campusNames.Select( a => new ListItem( a ) ).ToArray() );
+
+            filterControl.Controls.Add( ddlCampusPicker );
+
+            return new Control[1] { ddlCampusPicker };
+        }
+
+        /// <summary>
+        /// Renders the controls.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="filterControl">The filter control.</param>
+        /// <param name="writer">The writer.</param>
+        /// <param name="controls">The controls.</param>
+        public override void RenderControls( Type entityType, FilterField filterControl, HtmlTextWriter writer, Control[] controls )
+        {
+            base.RenderControls( entityType, filterControl, writer, controls );
+        }
+
+        /// <summary>
+        /// Gets the selection.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="controls">The controls.</param>
+        /// <returns></returns>
+        public override string GetSelection( Type entityType, Control[] controls )
+        {
+            var campusName = ( controls[0] as RockDropDownList ).SelectedValue;
+            return campusName;
+        }
+
+        /// <summary>
+        /// Sets the selection.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="controls">The controls.</param>
+        /// <param name="selection">The selection.</param>
+        public override void SetSelection( Type entityType, Control[] controls, string selection )
+        {
+            string[] selectionValues = selection.Split( '|' );
+            if ( selectionValues.Length >= 1 )
+            {
+                var ddlCampus = controls[0] as RockDropDownList;
+                ddlCampus.SelectedValue = selectionValues[0];
+            }
+        }
+
+        /// <summary>
+        /// Gets the expression.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="serviceInstance">The service instance.</param>
+        /// <param name="parameterExpression">The parameter expression.</param>
+        /// <param name="selection">The selection.</param>
+        /// <returns></returns>
+        public override Expression GetExpression( Type entityType, IService serviceInstance, ParameterExpression parameterExpression, string selection )
+        {
+            var rockContext = (RockContext)serviceInstance.Context;
+
+            string[] selectionValues = selection.Split( '|' );
+            if ( selectionValues.Length >= 1 )
+            {
+                string campusName = selectionValues[0];
+
+                if ( !string.IsNullOrWhiteSpace( campusName ) )
+                {
+                    var datamartPersonService = new Service<DatamartPerson>( rockContext );
+                    var qryDatamartPerson = datamartPersonService.Queryable().Where( a => a.CampusName == campusName );
+
+                    var qry = new PersonService( rockContext ).Queryable()
+                        .Where( p => qryDatamartPerson.Any( xx => xx.PersonId == p.Id ) );
+
+                    Expression extractedFilterExpression = FilterExpressionExtractor.Extract<Rock.Model.Person>( qry, parameterExpression, "p" );
+
+                    return extractedFilterExpression;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ church.ccv.Datamart/Reporting/DataSelect/Person/DatamartPersonFieldSelect.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Linq.Expressions;
+using church.ccv.Datamart.Model;
+using Rock.Data;
+using Rock.Model;
+using Rock.Reporting;
+using Rock.Web.UI.Controls;
+
+namespace church.ccv.Datamart.Reporting.DataSelect.Person
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Description( "Select a field for the person using the value from Datamart Person " )]
+    [Export( typeof( DataSelectComponent ) )]
+    [ExportMetadata( "ComponentName", "Select a field value from Datamart Person" )]
+    public class DatamartPersonFieldSelect : DataSelectComponent
+    {
+        /// <summary>
+        /// Gets the name of the entity type. Filter should be an empty string
+        /// if it applies to all entities
+        /// </summary>
+        /// <value>
+        /// The name of the entity type.
+        /// </value>
+        public override string AppliesToEntityType
+        {
+            get
+            {
+                return typeof( Rock.Model.Person ).FullName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the section that this will appear in in the Field Selector
+        /// </summary>
+        /// <value>
+        /// The section.
+        /// </value>
+        public override string Section
+        {
+            get
+            {
+                return "Datamart";
+            }
+        }
+
+        /// <summary>
+        /// The PropertyName of the property in the anonymous class returned by the SelectExpression
+        /// </summary>
+        /// <value>
+        /// The name of the column property.
+        /// </value>
+        public override string ColumnPropertyName
+        {
+            get
+            {
+                return "DatamartPersonField";
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the column field.
+        /// </summary>
+        /// <value>
+        /// The type of the column field.
+        /// </value>
+        public override Type ColumnFieldType
+        {
+            get { return typeof( object ); }
+        }
+
+        /// <summary>
+        /// Gets the default column header text.
+        /// </summary>
+        /// <value>
+        /// The default column header text.
+        /// </value>
+        public override string ColumnHeaderText
+        {
+            get
+            {
+                return this.ColumnPropertyName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <param name="entityType"></param>
+        /// <returns></returns>
+        /// <value>
+        /// The title.
+        /// </value>
+        public override string GetTitle( Type entityType )
+        {
+            return this.ColumnHeaderText;
+        }
+
+        /// <summary>
+        /// Creates the child controls.
+        /// </summary>
+        /// <param name="parentControl"></param>
+        /// <returns></returns>
+        public override System.Web.UI.Control[] CreateChildControls( System.Web.UI.Control parentControl )
+        {
+            RockDropDownList ddlField = new RockDropDownList();
+            ddlField.Label = "Field Name";
+            ddlField.ID = parentControl.ID + "_ddlField";
+            var datamartFields = EntityHelper.GetEntityFields( typeof( DatamartPerson ) ).Where( a => a.FieldKind == FieldKind.Property ).OrderBy( a => a.Name ).ToList();
+            var supportedTypes = new Type[] { typeof( string ), typeof( bool? ), typeof( DateTime? ), typeof( decimal? ) };
+            foreach ( var field in datamartFields )
+            {
+                if ( supportedTypes.Any( a => a.IsAssignableFrom( field.PropertyType ) ) )
+                {
+                    ddlField.Items.Add( field.Name );
+                }
+            }
+
+            parentControl.Controls.Add( ddlField );
+
+            return new System.Web.UI.Control[] { ddlField };
+        }
+
+        /// <summary>
+        /// Gets the selection.
+        /// </summary>
+        /// <param name="controls">The controls.</param>
+        /// <returns></returns>
+        public override string GetSelection( System.Web.UI.Control[] controls )
+        {
+            if ( controls.Count() == 1 )
+            {
+                RockDropDownList ddlField = controls[0] as RockDropDownList;
+                return string.Format( "{0}", ddlField.SelectedValue );
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Sets the selection.
+        /// </summary>
+        /// <param name="controls">The controls.</param>
+        /// <param name="selection">The selection.</param>
+        public override void SetSelection( System.Web.UI.Control[] controls, string selection )
+        {
+            if ( controls.Count() == 1 )
+            {
+                RockDropDownList ddlField = controls[0] as RockDropDownList;
+                ddlField.SelectedValue = selection;
+            }
+        }
+
+        /// <summary>
+        /// Gets the expression.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="entityIdProperty">The entity identifier property.</param>
+        /// <param name="selection">The selection.</param>
+        /// <returns></returns>
+        public override System.Linq.Expressions.Expression GetExpression( Rock.Data.RockContext context, System.Linq.Expressions.MemberExpression entityIdProperty, string selection )
+        {
+            var datamartPersonService = new Service<DatamartPerson>( context );
+            var personService = new PersonService( context );
+            var qryDatamartPerson = datamartPersonService.Queryable();
+            var qryPerson = personService.Queryable();
+
+            string fieldName = selection;
+            if ( string.IsNullOrWhiteSpace( fieldName ) )
+            {
+                return null;
+            }
+
+            IQueryable qryResult = null;
+
+            var datamartParameterExpression = Expression.Parameter( typeof( DatamartPerson ), "d" );
+            MemberExpression datamartMember = Expression.Property( datamartParameterExpression, fieldName );
+            if ( datamartMember.Type == typeof( bool ) )
+            {
+                var projectionLamba = Expression.Lambda<Func<DatamartPerson, bool>>( datamartMember, new ParameterExpression[] { datamartParameterExpression } );
+                qryResult = qryPerson
+                    .Select( p => qryDatamartPerson.Where( d => d.PersonId == p.Id )
+                      .Select( projectionLamba ).FirstOrDefault() );
+            }
+            else if ( datamartMember.Type == typeof( bool? ) )
+            {
+                var projectionLamba = Expression.Lambda<Func<DatamartPerson, bool?>>( datamartMember, new ParameterExpression[] { datamartParameterExpression } );
+                qryResult = qryPerson
+                    .Select( p => qryDatamartPerson.Where( d => d.PersonId == p.Id )
+                      .Select( projectionLamba ).FirstOrDefault() );
+            }
+            else if ( datamartMember.Type == typeof( DateTime? ) )
+            {
+                var projectionLamba = Expression.Lambda<Func<DatamartPerson, DateTime?>>( datamartMember, new ParameterExpression[] { datamartParameterExpression } );
+                qryResult = qryPerson
+                    .Select( p => qryDatamartPerson.Where( d => d.PersonId == p.Id )
+                      .Select( projectionLamba ).FirstOrDefault() );
+            }
+            else if ( datamartMember.Type == typeof( decimal? ) )
+            {
+                var projectionLamba = Expression.Lambda<Func<DatamartPerson, decimal?>>( datamartMember, new ParameterExpression[] { datamartParameterExpression } );
+                qryResult = qryPerson
+                    .Select( p => qryDatamartPerson.Where( d => d.PersonId == p.Id )
+                      .Select( projectionLamba ).FirstOrDefault() );
+            }
+            else
+            {
+                var projectionLamba = Expression.Lambda<Func<DatamartPerson, string>>( datamartMember, new ParameterExpression[] { datamartParameterExpression } );
+                qryResult = qryPerson
+                    .Select( p => qryDatamartPerson.Where( d => d.PersonId == p.Id )
+                      .Select( projectionLamba ).FirstOrDefault() );
+            }
+
+            return SelectExpressionExtractor.Extract( qryResult, entityIdProperty, "p" );
+        }
+    }
+}
