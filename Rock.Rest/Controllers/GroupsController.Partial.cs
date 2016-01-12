b0f0b4dad2ea12e908907e1b94527bf7909eb5e5
Mike Peterson <MikePeterson@ccvonline.com>
2016-01-12 16:01:46 -0700

    - fix ~/api/Groups/ByLocation and ~/api/Groups/ByLatLong including groups that don't have a GeoPoint (which was the orig behavior)

--- Rock.Rest/Controllers/GroupsController.Partial.cs
+++ Rock.Rest/Controllers/GroupsController.Partial.cs
@@ -335,6 +335,7 @@ namespace Rock.Rest.Controllers
 
         /// <summary>
         /// Gets a list of groups surrounding the specified the location, optionally limited to the specified geofenceGroupTypeId
+        /// If geofenceGroupTypeId is specified, the list of GeoFence groups will be returned with the groups as child groups of that geofence group.
         /// </summary>
         /// <param name="groupTypeId">The group type identifier.</param>
         /// <param name="locationId">The location identifier.</param>
@@ -360,6 +361,7 @@ namespace Rock.Rest.Controllers
 
         /// <summary>
         /// Gets a list of groups surrounding the specified lat/long, optionally limited to the specified geofenceGroupTypeId
+        /// If geofenceGroupTypeId is specified, the list of GeoFence groups will be returned with the groups as child groups of that geofence group.
         /// </summary>
         /// <param name="groupTypeId">The group type identifier.</param>
         /// <param name="latitude">The latitude.</param>
@@ -468,6 +470,9 @@ namespace Rock.Rest.Controllers
                     }
                 }
             }
+
+            // remove groups that don't have a GeoPoint
+            resultGroups = resultGroups.Where( a => a.GroupLocations.Any( x => x.Location.GeoPoint != null ) );
 
             // remove groups that don't have a location within the specified radius
             if ( maxDistanceMiles.HasValue )
