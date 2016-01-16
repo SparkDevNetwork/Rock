2fc497b2ec3854cc70240f7aed3dc404c2732cf5
edmistj <jon@sparkdevnetwork.org>
2016-01-15 21:58:31 -0700

    - color picker control

---
+++ Rock/Web/UI/Controls/ColorPicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.UI;
+
+namespace Rock.Web.UI.Controls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ToolboxData( "<{0}:ColorPicker runat=server></{0}:ColorPicker>" )]
+    public class ColorPicker : RockTextBox
+    {
+        
+
+        /// <summary>
+        /// Outputs server control content to a provided <see cref="T:System.Web.UI.HtmlTextWriter" /> object and stores tracing information about the control if tracing is enabled.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> object that receives the control content.</param>
+        public override void RenderControl( HtmlTextWriter writer )
+        {
+            this.AppendText = "<i></i>";
+            this.AddCssClass( "rock-colorpicker-input" );
+            this.AddCssClass( "form-control" );
+            string script = "$('input.rock-colorpicker-input').colorpicker();";
+            ScriptManager.RegisterStartupScript( this, this.GetType(), "rock-colorpicker", script, true );
+            base.RenderControl( writer );
+
+        }
+    }
+}
