316aa45e04ffa420fe3c64d5b259e49efca732e1
Mike Peterson <MikePeterson@ccvonline.com>
2016-01-29 14:38:02 -0700

    latest from core 2016-1-29

--- RockWeb/Blocks/Groups/GroupMemberList.ascx.cs
+++ RockWeb/Blocks/Groups/GroupMemberList.ascx.cs
@@ -133,6 +133,7 @@ namespace RockWeb.Blocks.Groups
    // $('.js-person-popover').popover('show'); // uncomment for styling
 ";
             ScriptManager.RegisterStartupScript( this, this.GetType(), "person-link-popover", script, true );
+            RockPage.AddScriptLink( ResolveRockUrl( "~/Scripts/jquery.lazyload.min.js" ) );
 
             // if this block has a specific GroupId set, use that, otherwise, determine it from the PageParameters
             Guid groupGuid = GetAttributeValue( "Group" ).AsGuid();
@@ -1014,6 +1015,8 @@ namespace RockWeb.Blocks.Groups
                         connectionStatusField.Visible = _group.GroupType.ShowConnectionStatus;
                     } 
 
+                    string photoFormat = "<div class=\"photo-icon photo-round photo-round-xs pull-left margin-r-sm js-person-popover\" personid=\"{0}\" data-original=\"{1}&w=50\" style=\"background-image: url( '{2}' ); background-size: cover; background-repeat: no-repeat;\"></div>";
+
                     gGroupMembers.DataSource = groupMembersList.Select( m => new
                     {
                         m.Id,
@@ -1022,7 +1025,7 @@ namespace RockWeb.Blocks.Groups
                         m.Person.NickName,
                         m.Person.LastName,
                         Name =
-                        ( selectAll ? m.Person.LastName + ", " + m.Person.NickName : ( m.Person.PhotoId.HasValue ? "<i class='fa fa-fw fa-user photo-icon has-photo js-person-popover' personid=" + m.PersonId.ToString() + "></i> " : "<i class='fa fa-fw photo-icon js-person-popover' personid=" + m.PersonId.ToString() + "></i> " ) +
+                        ( selectAll ? m.Person.LastName + ", " + m.Person.NickName : string.Format(photoFormat, m.PersonId, m.Person.PhotoUrl, ResolveUrl( "~/Assets/Images/person-no-photo-male.svg" ) )  +
                         m.Person.NickName + " " + m.Person.LastName
                             + ( hasGroupRequirements && groupMemberIdsThatLackGroupRequirements.Contains( m.Id )
                                 ? " <i class='fa fa-exclamation-triangle text-warning'></i>"
