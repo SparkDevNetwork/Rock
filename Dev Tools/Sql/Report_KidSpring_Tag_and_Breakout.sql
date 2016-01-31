3187b73b65e2dd1344a90485dbef1f808c2a8275
benwiley <ben.wiley@newspring.cc>
2016-01-31 17:21:14 -0500

    Add sql to the repo so it can be recovered quickly.  The same query is used for breakout and tag reports, just hide different fields.

---
+++ Dev Tools/Sql/Report_KidSpring_Tag_and_Breakout.sql
@@ -0,0 +1,46 @@
+WITH KidSpringAttendeeGroups AS (
+	SELECT
+		g.Id
+	FROM
+		[Group] g
+	WHERE
+		g.GroupTypeId in (756, 757, 758, 759) 
+), LatestSunday AS (
+	SELECT
+		MAX(CONVERT(DATE, StartDateTime)) AS LatestSunday 
+	FROM 
+		Attendance a
+		JOIN KidSpringAttendeeGroups g ON a.GroupID = g.Id
+)
+SELECT 
+    a.StartDateTime AS CheckinTime,
+	g.Name AS [Group], 
+	c.Name AS Campus,
+	s.Name AS Schedule, 
+	p.LastName, 
+	p.FirstName,
+	ac.Code, 
+	ISNULL(legal.Value, '') AS LegalNotes,
+	ISNULL(allergy.Value, '') AS AllergyNotes,
+	ISNULL(bo.Value, '') AS Breakout
+FROM 
+	[Person] p 
+	JOIN [PersonAlias] pa ON p.Id = pa.PersonId
+	JOIN [Attendance] a ON pa.ID = a.PersonAliasId
+	JOIN LatestSunday ls ON ls.LatestSunday = CONVERT(DATE, a.StartDateTime)
+	JOIN [Schedule] s ON a.ScheduleId = s.Id
+	JOIN [AttendanceCode] ac ON a.AttendanceCodeId = ac.Id
+	JOIN [Group] g ON a.GroupID = g.Id
+	JOIN Campus c ON c.Id = a.CampusId
+	LEFT JOIN [AttributeValue] legal ON p.Id = legal.EntityId AND legal.AttributeId = 715
+	LEFT JOIN [AttributeValue] allergy ON p.Id = allergy.EntityId AND allergy.AttributeId = 676
+	LEFT JOIN [AttributeValue] bo ON p.Id = bo.EntityId AND bo.AttributeId = 2321
+WHERE 
+	g.GroupTypeId in (756, 757, 758, 759) 
+	AND a.DidAttend = 1
+ORDER BY 
+	c.Name,
+	g.Name, 
+	Schedule, 
+	p.LastName,
+	p.FirstName;
