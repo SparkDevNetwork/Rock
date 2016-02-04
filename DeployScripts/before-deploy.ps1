882ffcad1c6fef6bcaae3be952921ed0c6019b9f
Kyle Craviotto <im.kyle@gmail.com>
2016-02-03 16:47:32 -0800

    Add deployscripts

---
+++ DeployScripts/before-deploy.ps1
@@ -0,0 +1,43 @@
+# This script is run by AppVeyor's deploy agent before the deploy
+Import-Module WebAdministration
+
+# Get the application (web root) and the root folder
+$webroot = $env:RockSandboxRootPath
+$rootfolder = Split-Path -Parent $webroot
+
+Write-Output "Running pre-deploy script"
+Write-Output "--------------------------------------------------"
+Write-Output "Root folder: $rootfolder"
+Write-Output "Web root folder: $webroot"
+Write-Output "Running script as: $env:userdomain\$env:username"
+
+# stop execution of the deploy if the moves fail
+$ErrorActionPreference = "Stop"
+
+# stop web site and app pool
+$Site = Get-WebSite -Name "$env:APPLICATION_SITE_NAME"
+If ( $Site.State -eq "Started" ) {
+	Write-Host "Stopping Website"
+	Stop-Website -Name "$env:APPLICATION_SITE_NAME"
+}
+If ( (Get-WebAppPoolState -Name $Site.applicationPool).Value -eq "Started" ) {
+	Write-Host "Stopping ApplicationPool"
+	Stop-WebAppPool -Name $Site.applicationPool
+}
+
+# wait for 10 seconds before continuing
+Start-Sleep 10
+
+# delete the contents of the temp directory
+If (Test-Path "$rootfolder\temp"){
+	Remove-Item "$rootfolder\temp\*" -Force -Confirm:$False -Recurse
+} Else {
+  New-Item -ItemType Directory -Force -Path "$rootfolder\temp"
+}
+
+# Move any files or folders that are not in your git repo like this:
+
+# move a robots file if it exists
+If (Test-Path "$webroot\robots.txt"){
+	Move-Item "$webroot\robots.txt" "$rootfolder\temp"
+}
---
+++ DeployScripts/deploy.ps1
@@ -0,0 +1,55 @@
+# This script is run by AppVeyor's deploy agent after the deploy
+Import-Module WebAdministration
+
+# Get the application (web root) and the root folder
+$webroot = $env:RockSandboxRootPath
+$rootfolder = Split-Path -Parent $webroot
+
+Write-Output "Running post-deploy script"
+Write-Output "--------------------------------------------------"
+Write-Output "Root folder: $rootfolder"
+Write-Output "Web root folder: $webroot"
+Write-Output "Running script as: $env:userdomain\$env:username"
+
+# ensure that the compilation debug is false
+(Get-Content "$webroot\web.config").Replace('<compilation debug="true"', '<compilation debug="false"') | Set-Content "$webroot\web.config"
+
+# Now move your custom stuff back from the temp folder like this:
+
+# move a robots file back from temp if it exists
+If (Test-Path "$rootfolder\temp\robots.txt"){
+	Write-Host "Moving robots.txt file back from temp directory"
+	Move-Item "$rootfolder\temp\robots.txt" "$webroot"
+}
+
+# copy new connection string file
+Write-Host "Copying new web.ConnectionStrings.config to web dir"
+Copy-Item "$rootfolder\config\web.ConnectionStrings.config" $webroot -force
+
+# start web site and app pool
+Write-Host "Starting ApplicationPool and Website"
+Start-WebAppPool -Name (Get-Website -Name "$env:APPLICATION_SITE_NAME").applicationPool
+Start-Website -Name "$env:APPLICATION_SITE_NAME"
+
+# create empty migration flag
+New-Item "$webroot\App_Data\Run.Migration" -type file -force
+
+# set acl on migration flag file so the app has permissions to delete it
+$acl = Get-ACL "$webroot\App_Data\Run.Migration"
+$accessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone","FullControl","Allow")
+$acl.AddAccessRule($accessRule)
+Set-Acl "$webroot\App_Data\Run.Migration" $acl
+
+# delete deploy scripts
+If (Test-Path "$webroot\deploy.ps1"){
+	Remove-Item "$webroot\deploy.ps1"
+}
+
+If (Test-Path "$webroot\before-deploy.ps1"){
+	Remove-Item "$webroot\before-deploy.ps1"
+}
+
+# delete the appveyor deploy cache
+If (Test-Path c:\appveyor){
+	Remove-Item c:\appveyor -Force -Confirm:$False -Recurse
+}
