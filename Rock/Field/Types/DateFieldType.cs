592f2bdc9950719136729b683ade6cd1588da751
Mike Peterson <MikePeterson@ccvonline.com>
2016-02-11 11:23:20 -0700

    - Updated FieldType's ValueAsType to default to use the formatted value (not the internal value).  This fixes an issue where Sorting a ContentChannelView would not always work as expected - Updated RatingFieldType, CurrencyFieldType,TimeFieldType, BooleanFieldType to override ValueAsType using the appropriate datatype instead of just the string value

--- Rock/Field/Types/DateFieldType.cs
+++ Rock/Field/Types/DateFieldType.cs
@@ -227,7 +227,7 @@ namespace Rock.Field.Types
         }
 
         /// <summary>
-        /// Values the type of as field.
+        /// Returns the value using the most appropriate datatype
         /// </summary>
         /// <param name="parentControl">The parent control.</param>
         /// <param name="value">The value.</param>
