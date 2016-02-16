a14f57be77a0bae8487c0ae89a6b5b5262dff3ef
Mike Peterson <MikePeterson@ccvonline.com>
2016-02-16 12:43:08 -0700

    started experimenting with ccv mobile invite

---
+++ RockWeb/Plugins/church_ccv/Cms/InviteEntry.ascx.cs
@@ -0,0 +1,70 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+
+namespace RockWeb.Plugins.church_ccv.Cms
+{
+    [DisplayName( "Invite Entry" )]
+    [Category( "CCV > Cms" )]
+    [Description( "Block that helps a user send a direct message to somebody using email, text, etc" )]
+
+    [CodeEditorField( "Template", "", Rock.Web.UI.Controls.CodeEditorMode.Lava )]
+    [CodeEditorField( "Text Template", "", Rock.Web.UI.Controls.CodeEditorMode.Lava )]
+    [CodeEditorField( "Email Template", "", Rock.Web.UI.Controls.CodeEditorMode.Lava )]
+    public partial class InviteEntry : RockBlock
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !this.IsPostBack )
+            {
+                ShowContent();
+            }
+        }
+
+        public void ShowContent()
+        {
+
+        }
+    }
+}
