477b51d497bcb9529c82f7047b1191fa6d641807
Mike Peterson <MikePeterson@ccvonline.com>
2016-02-18 18:45:56 -0700

    + Fixed issue in Merge Templates that was causing Header, Footer, and any remaining global lava fields in the Body not to get merged (fixes #1396)

--- Rock/MergeTemplates/WordDocumentMergeTemplateType.cs
+++ Rock/MergeTemplates/WordDocumentMergeTemplateType.cs
@@ -373,7 +373,23 @@ namespace Rock.MergeTemplates
                         lastId++;
                     }
 
-                    HeaderFooterGlobalMerge( outputDoc, globalMergeFields );
+                    DotLiquid.Hash globalMergeHash = new DotLiquid.Hash();
+                    foreach ( var field in globalMergeFields )
+                    {
+                        globalMergeHash.Add( field.Key, field.Value );
+                    }
+
+                    HeaderFooterGlobalMerge( outputDoc, globalMergeHash );
+
+                    // sweep thru any remaining un-merged body parts for any Lava having to do with Global merge fields
+                    foreach ( var bodyTextPart in outputDoc.MainDocumentPart.Document.Body.Descendants<Text>() )
+                    {
+                        string nodeText = bodyTextPart.Text.ReplaceWordChars();
+                        if ( lavaRegEx.IsMatch( nodeText ) )
+                        {
+                            bodyTextPart.Text = nodeText.ResolveMergeFields( globalMergeHash, true, true );
+                        }
+                    }
 
                     // remove the last pagebreak
                     MarkupSimplifier.SimplifyMarkup( outputDoc, new SimplifyMarkupSettings { RemoveLastRenderedPageBreak = true } );
@@ -404,13 +420,10 @@ namespace Rock.MergeTemplates
         /// </summary>
         /// <param name="outputDoc">The output document.</param>
         /// <param name="globalMergeFields">The global merge fields.</param>
-        private void HeaderFooterGlobalMerge( WordprocessingDocument outputDoc, Dictionary<string, object> globalMergeFields )
+        private void HeaderFooterGlobalMerge( WordprocessingDocument outputDoc, DotLiquid.Hash globalMergeHash )
         {
-            DotLiquid.Hash globalMergeHash = new DotLiquid.Hash();
-            foreach ( var field in globalMergeFields )
-            {
-                globalMergeHash.Add( field.Key, field.Value );
-            }
+            // make sure that all proof codes get removed so that the lava can be found
+            MarkupSimplifier.SimplifyMarkup( outputDoc, new SimplifyMarkupSettings { RemoveProof = true } );
 
             // update the doc headers and footers for any Lava having to do with Global merge fields
             // from http://stackoverflow.com/a/19012057/1755417
