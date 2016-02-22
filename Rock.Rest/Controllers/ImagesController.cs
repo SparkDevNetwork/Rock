887fea868777a889dfbd804ccb59a68a176d4da3
Ben Wiley <ben.wiley@newspring.cc>
2016-02-21 19:29:27 -0500

    The file is received in the API

---
+++ Rock.Rest/Controllers/ImagesController.cs
@@ -0,0 +1,65 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.OData;
+using Rock.Data;
+using Rock.Model;
+using Rock.Rest.Filters;
+using Rock.Web.Cache;
+
+namespace Rock.Rest.Controllers
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class ImagesController : ApiControllerBase
+    {
+        #region Post
+
+        /// <summary>
+        /// Adds a new person and puts them into a new family
+        /// </summary>
+        /// <param name="person">The person.</param>
+        /// <returns></returns>
+        [Authenticate, Secured]
+        [HttpPost]
+        [System.Web.Http.Route("api/Images")]
+        public HttpResponseMessage PostImage([FromUri] Guid binaryFileTypeGuid)
+        {
+            var file = HttpContext.Current.Request.Files.Count > 0 ? HttpContext.Current.Request.Files[0] : null;
+            throw new NotImplementedException();
+        }
+
+        [Authenticate, Secured]
+        [HttpPut]
+        [System.Web.Http.Route("api/Images/{binaryFileId}")]
+        public void PutImage(int binaryFileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
