b283cb74f5257b325cf795332190bedbae143791
Nick Sheck <nsheck@gmail.com>
2016-03-01 16:27:21 -0700

    Easter updates

--- RockWeb/Themes/church_ccv_External_v7/Scripts/multicampus.js
+++ RockWeb/Themes/church_ccv_External_v7/Scripts/multicampus.js
@@ -14,8 +14,14 @@
 // -------------------------
 
 function setActiveCampus(campusSC, saveCampus) {
-  $('.multicampus-item.is-active').removeClass('is-active is-open').find('.multicampus-collapse').addClass('collapse').removeClass('in')
-  $('[data-campus='+campusSC+']').addClass('is-active is-open').insertBefore('.multicampus-item:first').find('.multicampus-collapse').addClass('in')
+
+  $('.multicampus-item.is-active').removeClass('is-active is-open')
+    .find('.multicampus-collapse').addClass('collapse').removeClass('in')
+
+  $('[data-campus='+campusSC+']').addClass('is-active is-open')
+    .insertBefore('.multicampus-item:first').find('.multicampus-collapse')
+    .addClass('in')
+
   if (saveCampus) {
     var campusId = findIdByShortCode(campusSC)
     Rock.utility.setContext('campuses', campusId)
@@ -34,7 +40,7 @@ function findShortCodeById(campusId) {
 
 // Only show text invite on mobile
 // Only show facebook invite on computers
-var md = new MobileDetect(window.navigator.userAgent);
+var md = new MobileDetect(window.navigator.userAgent)
 if (md.mobile()) {
   $('.js-sms').removeClass('hide')
 } else {
@@ -48,17 +54,31 @@ $('body').on('click', '.multicampus .multicampus-title', function(){
   $this.parents('.multicampus-item').toggleClass('is-open')
 })
 
+// Invite google analytics tracking
+$('body').on('click', '.js-track-sms', function(){
+  var label = $(this).data('label')
+  trackEvent('Text Invite', label)
+})
+$('body').on('click', '.js-track-email', function(){
+  var label = $(this).data('label')
+  trackEvent('Email Invite', label)
+})
+$('body').on('click', '.js-track-fb', function(){
+  var label = $(this).data('label')
+  trackEvent('Facebook Invite', label)
+})
+
 
 // Find Nearest Campus
 // -------------------------
 
 if(navigator.geolocation) {
-  $('.js-find-nearest-container').html('<button type="button" class="btn btn-default find-nearest" onClick="findNearestCampus(this)"><i class="loading-icon"></i> Find Nearest Campus</button>');
+  $('.js-find-nearest-container').html('<button type="button" class="btn btn-default find-nearest" onClick="findNearestCampus(this)"><i class="loading-icon"></i> Find Nearest Campus</button>')
 }
 
 function findNearestCampus(trigger) {
-  var $trigger = $(trigger);
-  var service = new google.maps.DistanceMatrixService();
+  var $trigger = $(trigger)
+  var service = new google.maps.DistanceMatrixService()
 
   function parseResults(response, status) {
     if (status === google.maps.DistanceMatrixStatus.OK) {
@@ -66,47 +86,43 @@ function findNearestCampus(trigger) {
       var lowest = Number.POSITIVE_INFINITY,
         lowestArrayIndex,
         tmp,
-        responseArray = response.rows[0].elements;
+        responseArray = response.rows[0].elements
 
       for (var i = 0; i < responseArray.length; i++) {
-        tmp = responseArray[i].duration.value;
+        tmp = responseArray[i].duration.value
         if (tmp < lowest) {
-          lowest = tmp;
-          lowestArrayIndex = i;
+          lowest = tmp
+          lowestArrayIndex = i
         }
       }
 
-      $trigger.removeClass('is-loading');
-      setActiveCampus(CCV.locations[lowestArrayIndex].shortcode, true);
+      $trigger.removeClass('is-loading')
+      setActiveCampus(CCV.locations[lowestArrayIndex].shortcode, true)
+      trackEvent('Find Nearest Campus', CCV.locations[lowestArrayIndex].name)
     }
   }
 
   function allLocationsGeoArray() {
-    var r = [];
+    var r = []
     for (var i = 0; i < CCV.locations.length; i++) {
-      var location = CCV.locations[i].geo;
-      var geo = new google.maps.LatLng(location.lat, location.lng);
-      r.push(geo);
+      var location = CCV.locations[i].geo
+      var geo = new google.maps.LatLng(location.lat, location.lng)
+      r.push(geo)
     }
-    return r;
+    return r
   }
 
   if(navigator.geolocation) {
-    $trigger.addClass('is-loading');
+    $trigger.addClass('is-loading')
     navigator.geolocation.getCurrentPosition(function(p) {
-      var userLocation = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+      var userLocation = new google.maps.LatLng(p.coords.latitude, p.coords.longitude)
       service.getDistanceMatrix(
         {
           origins: [userLocation],
           destinations: allLocationsGeoArray(),
           travelMode: google.maps.TravelMode.DRIVING
-        }, parseResults);
-    });
+        }, parseResults
+      )
+    })
   }
-
-  // ga('send', {
-  //   hitType: 'event',
-  //   eventCategory: 'Christmas Invite Page',
-  //   eventAction: 'Find Nearest Campus'
-  // });
 }
