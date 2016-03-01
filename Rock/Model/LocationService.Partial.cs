c5bd90e99cb78078e488df7a6922e47a63f055e6
David Turner <david@sparkdevnetwork.org>
2016-03-01 04:07:39 -0700

    - Fixed exception in check-in when device was not associated with campus location

--- Rock/Model/LocationService.Partial.cs
+++ Rock/Model/LocationService.Partial.cs
@@ -374,9 +374,9 @@ namespace Rock.Model
             return this.Context.Database.SqlQuery<int>( string.Format(
                 @"
                 WITH CTE AS (
-                    SELECT [Id], [ParentLocationId] FROM [Location] WHERE [Id]={0}
+                    SELECT [Id], [ParentLocationId], [Name] FROM [Location] WHERE [Id]={0}
                     UNION ALL
-                    SELECT [a].[Id], [a].[ParentLocationId] FROM [Location] [a]
+                    SELECT [a].[Id], [a].[ParentLocationId], [Name] FROM [Location] [a]
                     INNER JOIN CTE ON CTE.[ParentLocationId] = [a].[Id]
                 )
                 SELECT [Id]
