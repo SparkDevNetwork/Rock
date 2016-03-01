2161058006a77e5a725da5675224bcc01bb56b41
edmistj <jon@sparkdevnetwork.org>
2016-03-01 15:39:30 -0700

    - new rock theme object

---
+++ Rock/Web/UI/RockTheme.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using dotless.Core;
+using dotless.Core.configuration;
+
+namespace Rock.Web.UI
+{
+    public class RockTheme
+    {
+        static private string _themeDirectory = HttpRuntime.AppDomainAppPath + "Themes";
+        
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logical path.
+        /// </summary>
+        /// <value>
+        /// The logical path.
+        /// </value>
+        public string RelativePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the physical path.
+        /// </summary>
+        /// <value>
+        /// The physical path.
+        /// </value>
+        public string AbsolutePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is system.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is system; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsSystem { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [allows compile].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [allows compile]; otherwise, <c>false</c>.
+        /// </value>
+        public bool AllowsCompile { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RockTheme"/> class.
+        /// </summary>
+        public RockTheme()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RockTheme"/> class.
+        /// </summary>
+        /// <param name="themeName">Name of the theme.</param>
+        public RockTheme(string themeName )
+        {
+            string themePath = _themeDirectory + @"\" + themeName;
+
+            if (Directory.Exists( themePath ) )
+            {
+                DirectoryInfo themeDirectory = new DirectoryInfo( themePath );
+                this.Name = themeDirectory.Name;
+                this.AbsolutePath = themeDirectory.FullName;
+                this.RelativePath = "/Themes/" + this.Name;
+
+                this.IsSystem = File.Exists( themeDirectory.FullName + @"\.system" );
+                this.AllowsCompile = !File.Exists( themeDirectory.FullName + @"\Styles\.nocompile" );
+            }
+        }
+
+        /// <summary>
+        /// Gets the themes.
+        /// </summary>
+        /// <returns></returns>
+        public static List<RockTheme> GetThemes()
+        {
+            List<RockTheme> themes = new List<RockTheme>();
+
+            DirectoryInfo themeDirectory = new DirectoryInfo( _themeDirectory );
+
+            var themeDirectories = themeDirectory.GetDirectories();
+
+            foreach ( var theme in themeDirectories )
+            {
+                themes.Add( new RockTheme( theme.Name ) );
+            }
+
+            return themes;
+        }
+
+        /// <summary>
+        /// Clones the theme.
+        /// </summary>
+        public static bool CloneTheme(string sourceTheme, string targetTheme, out string messages)
+        {
+            messages = string.Empty;
+            bool resultSucess = true;
+
+            string sourceFullPath = _themeDirectory + @"\" + sourceTheme;
+            string targetFullPath = _themeDirectory + @"\" + targetTheme;
+
+            if ( !Directory.Exists( targetFullPath ) )
+            {
+                try
+                {
+                    DirectoryInfo diSource = new DirectoryInfo( sourceFullPath );
+                    DirectoryInfo diTarget = new DirectoryInfo( targetFullPath );
+
+                    CopyAll( diSource, diTarget );
+
+                    // delete the .system file if it exists
+                    string systemFile = targetFullPath + @"\.system";
+                    if (File.Exists( systemFile ) )
+                    {
+                        File.Delete( systemFile );
+                    }
+                }
+                catch ( Exception ex )
+                {
+                    resultSucess = false;
+                    messages = ex.Message;
+                }
+            }
+            else
+            {
+                resultSucess = false;
+                messages = "The target theme name already exists.";
+            }
+            
+            return resultSucess;
+        }
+
+        /// <summary>
+        /// Clones the theme.
+        /// </summary>
+        /// <param name="sourceTheme">The source theme.</param>
+        /// <param name="targetTheme">The target theme.</param>
+        /// <returns></returns>
+        public static bool CloneTheme( string sourceTheme, string targetTheme )
+        {
+            string messages = string.Empty;
+            return CloneTheme( sourceTheme, targetTheme, out messages );
+        }
+
+        /// <summary>
+        /// Deletes the theme.
+        /// </summary>
+        /// <param name="themeName">Name of the theme.</param>
+        /// <param name="messages">The messages.</param>
+        /// <returns></returns>
+        public static bool DeleteTheme(string themeName, out string messages )
+        {
+            messages = string.Empty;
+            bool resultSucess = true;
+
+            var theme = new RockTheme( themeName );
+
+            if ( !theme.IsSystem )
+            {
+                try
+                {
+                    Directory.Delete( theme.AbsolutePath, true );
+                }
+                catch ( Exception ex )
+                {
+                    resultSucess = false;
+                    messages = ex.Message;
+                }
+            } 
+            else
+            {
+                resultSucess = false;
+                messages = "Can not delete a system theme.";
+            }
+
+            return resultSucess;
+        }
+
+        /// <summary>
+        /// Deletes the theme.
+        /// </summary>
+        /// <param name="themeName">Name of the theme.</param>
+        /// <returns></returns>
+        public static bool DeleteTheme( string themeName )
+        {
+            string messages = string.Empty;
+            return DeleteTheme( themeName, out messages );
+        }
+
+
+        /// <summary>
+        /// Copies all.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="target">The target.</param>
+        private static void CopyAll( DirectoryInfo source, DirectoryInfo target )
+        {
+            // from: msdn.microsoft.com/en-us/library/system.io.directoryinfo.aspx
+
+            Directory.CreateDirectory( target.FullName );
+
+            // Copy each file into the new directory.
+            foreach ( FileInfo fi in source.GetFiles() )
+            {
+                fi.CopyTo( Path.Combine( target.FullName, fi.Name ), true );
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach ( DirectoryInfo diSourceSubDir in source.GetDirectories() )
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory( diSourceSubDir.Name );
+                CopyAll( diSourceSubDir, nextTargetSubDir );
+            }
+        }
+    }
+}
---
+++ RockWeb/Blocks/Cms/ThemeList.ascx.cs
@@ -0,0 +1,194 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+using System.Web;
+using Rock.Web.UI;
+
+namespace RockWeb.Blocks.Cms
+{
+    /// <summary>
+    /// Block that lists themes in the theme folder
+    /// </summary>
+    [DisplayName( "Theme List" )]
+    [Category( "CMS" )]
+    [Description( "Lists themes in the Theme folder." )]
+    public partial class ThemeList : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+        // used for private variables
+
+        #endregion
+
+        #region Properties
+
+        // used for public / protected properties
+
+        #endregion
+
+        #region Base Control Methods
+
+        //  overrides of the base RockBlock methods (i.e. OnInit, OnLoad)
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            gList.GridRebind += gList_GridRebind;
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                BindGrid();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the SaveClick event of the mdThemeClone control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void mdThemeClone_SaveClick( object sender, EventArgs e )
+        {
+            mdThemeClone.Hide();
+
+            string resultMessages = string.Empty;
+            var cloneWasSuccessful = RockTheme.CloneTheme( hfClonedThemeName.Value, tbNewThemeName.Text, out resultMessages );
+
+            if ( cloneWasSuccessful )
+            {
+                nbMessages.NotificationBoxType = NotificationBoxType.Success;
+                nbMessages.Text = string.Format( "The {0} theme has been successfully cloned to {1}.", hfClonedThemeName.Value, tbNewThemeName.Text );
+                BindGrid();
+            }
+            else
+            {
+                nbMessages.NotificationBoxType = NotificationBoxType.Danger;
+                nbMessages.Text = resultMessages;
+            }
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the gPledges control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void gList_GridRebind( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the gCloneTheme control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void gCloneTheme_Click( object sender, RowEventArgs e )
+        {
+            //RockTheme source = new RockTheme( e.RowKeyValue.ToString() );
+            hfClonedThemeName.Value = e.RowKeyValue.ToString();
+            mdThemeClone.Show();
+        }
+
+        /// <summary>
+        /// Handles the Delete event of the gThemes control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void gThemes_Delete( object sender, RowEventArgs e )
+        {
+            string messages = string.Empty;
+
+            bool deleteSuccess = RockTheme.DeleteTheme( e.RowKeyValue.ToString(), out messages );
+
+            if ( deleteSuccess )
+            {
+                nbMessages.NotificationBoxType = NotificationBoxType.Success;
+                nbMessages.Text = string.Format( "The {0} theme has been successfully deleted.", e.RowKeyValue );
+                BindGrid();
+            }
+            else
+            {
+                nbMessages.NotificationBoxType = NotificationBoxType.Danger;
+                nbMessages.Text = "Could not delete theme. Message: " + messages;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            DirectoryInfo themeDirectory = new DirectoryInfo( HttpRuntime.AppDomainAppPath + "Themes" );
+
+            var themes = RockTheme.GetThemes();
+
+            gThemes.DataSource = themes.ToList();
+            gThemes.DataBind();
+        }
+
+        #endregion
+    }
+}
