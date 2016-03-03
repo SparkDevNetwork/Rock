9dc8207b004c27b282a65b309383fbb42028bb36
mkinyon <mavhuntrx@gmail.com>
2016-03-02 17:03:50 -0700

    Added two new badges + Campus With Leader Badge + Coaches Badge

---
+++ church.ccv.Badges/Person/CampusWithLeader.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI.Controls;
+
+namespace Rock.PersonProfile.Badge
+{
+    /// <summary>
+    /// Campus with Leader Badge
+    /// </summary>
+    [Description( "Campus with Leader Badge" )]
+    [Export( typeof( BadgeComponent ) )]
+    [ExportMetadata( "ComponentName", "Campus with Leader" )]
+    public class CampusWithLeader : HighlightLabelBadge
+    {
+
+        /// <summary>
+        /// Gets the badge label
+        /// </summary>
+        /// <param name="person">The person.</param>
+        /// <returns></returns>
+        public override HighlightLabel GetLabel( Person person )
+        {
+            if ( ParentPersonBlock != null )
+            {
+                // Campus is associated with the family group(s) person belongs to.
+                var families = ParentPersonBlock.PersonGroups( Rock.SystemGuid.GroupType.GROUPTYPE_FAMILY );
+                if ( families != null )
+                {
+                    var rockContext = new RockContext();
+
+                    var label = new HighlightLabel();
+                    label.LabelType = LabelType.Campus;
+
+                    var campusNames = new List<string>();
+                    var campusLeaders = new List<Person>();
+
+                    foreach ( int campusId in families
+                        .Where( g => g.CampusId.HasValue )
+                        .Select( g => g.CampusId )
+                        .Distinct()
+                        .ToList() )
+                    {
+                        var campus = Rock.Web.Cache.CampusCache.Read( campusId );
+
+                        campusNames.Add( campus.Name );
+                        campusLeaders.Add( new PersonAliasService( rockContext ).GetPerson( (int)campus.LeaderPersonAliasId ) );
+                    }
+                        
+
+                    label.Text = campusNames.ToList().AsDelimited( ", " );
+                    label.ToolTip = campusLeaders.Select( p => p.NickName + " " + p.LastName ).ToList().AsDelimited( ", " );
+
+                    return label;
+                }
+            }
+
+            return null;
+
+        }
+
+    }
+}
---
+++ church.ccv.Badges/Person/Coaches.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Web.UI;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+
+namespace church.ccv.Badges.Person
+{
+    /// <summary>
+    /// Coaches Badge
+    /// </summary>
+    [Description( "Displays the coaches for the specific person." )]
+    [Export( typeof( Rock.PersonProfile.BadgeComponent ) )]
+    [ExportMetadata( "ComponentName", "Coaches" )]
+
+    [TextField( "Group Types", "List of group types to display." )]
+    [TextField( "Label", "Label to display badge.", true, "Coach" )]
+    class Coaches : Rock.PersonProfile.BadgeComponent
+    {
+        public override void Render( PersonBadgeCache badge, HtmlTextWriter writer )
+        {
+            var rockContext = new RockContext();
+
+            var label = GetAttributeValue( badge, "Label" );
+            var groupTypes = GetAttributeValue( badge, "GroupTypes" );
+            var groupTypesIds = groupTypes.Split( ',' ).AsIntegerList();
+
+            var groups = new GroupMemberService( rockContext ).Queryable()
+                                                .Where( m => groupTypesIds.Contains( m.Group.GroupTypeId )
+                                                    && m.GroupMemberStatus != GroupMemberStatus.Inactive
+                                                    && m.Group.IsActive != false
+                                                    && m.PersonId == Person.Id
+                                                    && m.GroupRole.IsLeader != true )
+                                                .Select( m => m.Group.Id )
+                                                .ToList();
+
+            List<string> groupCoaches = new GroupMemberService( rockContext ).Queryable()
+                                                .Where( m => groups.Contains( m.Group.Id )
+                                                   && m.GroupMemberStatus != GroupMemberStatus.Inactive
+                                                   && m.Group.IsActive != false
+                                                   && m.GroupRole.IsLeader == true )
+                                                .Select( m => m.Person.NickName + " " + m.Person.LastName )
+                                                .ToList();
+            if ( groupCoaches.Any() )
+            {
+                writer.Write(
+                    @"<script>
+                        $(document).ready(function(){ $('[data-toggle=tooltip]').tooltip() });
+                    </script>
+
+                    <style>
+                        .label-coach {
+                            background-color: #ee7624;
+                        }
+                    </style>
+
+                    <span data-toggle='tooltip' title='" + string.Join( ",", groupCoaches ) +
+                    "' class='label label-coach'>" + label + "</span>" );
+            }
+        }
+    }
+}
