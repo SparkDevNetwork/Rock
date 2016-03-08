3e3b699ac1cdfdf00e5309d5b26ff9d818a41e9d
edmistj <jon@sparkdevnetwork.org>
2016-03-07 22:34:00 -0700

    - updates

--- Rock.Migrations/Migrations/201603021659169_PersonEra.cs
+++ Rock.Migrations/Migrations/201603021659169_PersonEra.cs
@@ -29,13 +29,19 @@ namespace Rock.Migrations
         /// </summary>
         public override void Up()
         {
-          
+
             // todo
             // - add stored proc
 
+            // add job service
+            Sql( @"  INSERT INTO [ServiceJob]
+                          ([IsSystem], [IsActive], [Name], [Description], [Class], [CronExpression], [NotificationStatus], [Guid])
+                          VALUES 
+                          (0, 0, 'Family Analytics', 'Job that populates Rock''s family analytics', 'Rock.Jobs.CalculateFamilyAnalytics', '0 0 20 ? * TUE *', 1, '623F4751-C654-FEB7-45B7-59685B1F60AE')" );
+
             // create person attributes
-            RockMigrationHelper.UpdatePersonAttributeCategory( "eRA", "fa fa-shield", "Person attributes related to Rock's eRA features.", SystemGuid.Category.PERSON_ATTRIBUTES_ERA );
-            RockMigrationHelper.UpdatePersonAttribute( SystemGuid.FieldType.BOOLEAN, SystemGuid.Category.PERSON_ATTRIBUTES_ERA, "Currently an eRA", "core_CurrentlyAnEra", "", "", 1, "false", SystemGuid.Attribute.PERSON_ERA_CURRENTLY_AN_ERA );
+            RockMigrationHelper.UpdatePersonAttributeCategory( "Family Analytics", "fa fa-shield", "Person attributes related to Rock's family analytics features.", SystemGuid.Category.PERSON_ATTRIBUTES_ERA );
+            RockMigrationHelper.UpdatePersonAttribute( SystemGuid.FieldType.BOOLEAN, SystemGuid.Category.PERSON_ATTRIBUTES_ERA, "Currently an eRA", "core_CurrentlyAnEra", "", "", 1, "", SystemGuid.Attribute.PERSON_ERA_CURRENTLY_AN_ERA );
             RockMigrationHelper.UpdatePersonAttribute( SystemGuid.FieldType.DATE, SystemGuid.Category.PERSON_ATTRIBUTES_ERA, "eRA Start Date", "core_EraStartDate", "", "", 2, "", SystemGuid.Attribute.PERSON_ERA_START_DATE );
             RockMigrationHelper.UpdatePersonAttribute( SystemGuid.FieldType.DATE, SystemGuid.Category.PERSON_ATTRIBUTES_ERA, "eRA End Date", "core_EraEndDate", "", "", 3, "", SystemGuid.Attribute.PERSON_ERA_END_DATE );
             RockMigrationHelper.UpdatePersonAttribute( SystemGuid.FieldType.DATE, SystemGuid.Category.PERSON_ATTRIBUTES_ERA, "First Attended", "core_EraFirstAttended", "", "", 4, "", SystemGuid.Attribute.PERSON_ERA_FIRST_ATTENDED );
@@ -59,8 +65,58 @@ namespace Rock.Migrations
             AddSecurityToAttribute( SystemGuid.Attribute.PERSON_ERA_TIMES_GIVEN_6 );
 
             // add attributes block to the person profile page
-            RockMigrationHelper.AddBlock( SystemGuid.Page.EXTENDED_ATTRIBUTES, "", "D70A59DC-16BE-43BE-9880-59598FA7A94C", "eRA Attributes", "SectionB3", "", "", 0, "CDE833AA-159D-30B5-419F-CD6EFBB05887" );
+            RockMigrationHelper.AddBlock( SystemGuid.Page.EXTENDED_ATTRIBUTES, "", "D70A59DC-16BE-43BE-9880-59598FA7A94C", "Family Analytics", "SectionB3", "", "", 0, "CDE833AA-159D-30B5-419F-CD6EFBB05887" );
             RockMigrationHelper.AddBlockAttributeValue( "CDE833AA-159D-30B5-419F-CD6EFBB05887", "EC43CF32-3BDF-4544-8B6A-CE9208DD7C81", SystemGuid.Category.PERSON_ATTRIBUTES_ERA );
+
+            // reorder the current blocks
+            Sql( @"  UPDATE [Block]
+	                SET [Order] = 1 
+	                WHERE [Guid] = '46D254C2-5A36-4F99-97A3-45DA8A49DB90'
+
+                  UPDATE [Block]
+	                SET [Order] = 2 
+	                WHERE [Guid] = 'FFC9DF57-3E18-492C-B622-3EA167D7EBA1'" );
+
+            // add era badge
+            Sql( @"DECLARE @LavaBadgeEntityTypeId int = (SELECT TOP 1 [Id] FROM [EntityType] WHERE [Guid] = '95912004-62B5-4460-951F-D752427D44FE')
+  DECLARE @LavaAttributeId int = (SELECT TOP 1 [Id] FROM [Attribute] WHERE [Guid] = '01C9BA59-D8D4-4137-90A6-B3C06C70BBC3')
+  DECLARE @BadgeEntityId int 
+  
+
+  INSERT INTO [PersonBadge]
+  ([Name], [Description], [EntityTypeId], [Order], [Guid])
+  VALUES
+	('eRA', 'Shows if someone is an eRA.', @LavaBadgeEntityTypeId, 10, '7FC986B9-CA1E-CBB7-4E63-C79EAC34F39D')
+SET @BadgeEntityId = SCOPE_IDENTITY()
+
+
+INSERT INTO [AttributeValue]
+([IsSystem], [AttributeId], [EntityId], [Value], [Guid])
+VALUES
+(0, @LavaAttributeId, @BadgeEntityId, '{% assign isEra = Person | Attribute:''core_CurrentlyAnEra'' %}
+
+{% if isEra == ''Yes'' %}
+
+<div class=""badge badge-era"" data-toggle=""tooltip"" data-original-title=""{{ Person.NickName}} become an eRA on {{ Person | Attribute:''core_EraStartDate''}}"">
+    <span>eRA</span>
+</div>
+
+{% endif %}', newid())
+
+DECLARE @BadgeListAttributeId int = (SELECT TOP 1 [Id] FROM [Attribute] WHERE [Guid] = 'F5AB231E-3836-4D52-BD03-BF79773C237A')
+DECLARE @BadgeBlockId int = (SELECT TOP 1 [Id] FROM [Block] WHERE [Guid] = '98A30DD7-8665-4C6D-B1BB-A8380E862A04')
+DECLARE @BadgeGuid nvarchar(50) = (SELECT TOP 1 convert(nvarchar(50),[Guid]) FROM [PersonBadge] WHERE [Id] = @BadgeEntityId)
+
+IF EXISTS (SELECT *  FROM  [AttributeValue] WHERE [AttributeId] = @BadgeListAttributeId AND [EntityId] =  @BadgeBlockId)
+	UPDATE [AttributeValue]
+		SET [Value] = [Value] + ',' + @BadgeGuid
+	WHERE
+		[AttributeId] = @BadgeListAttributeId AND [EntityId] =  @BadgeBlockId
+ELSE
+	INSERT INTO [AttributeValue]
+		([IsSystem], [AttributeId], [EntityId], [Value], [Guid])
+	VALUES
+		(0, @BadgeListAttributeId, @BadgeBlockId, @BadgeGuid, newid())" );
         }
         
         /// <summary>
@@ -68,6 +124,9 @@ namespace Rock.Migrations
         /// </summary>
         public override void Down()
         {
+            // delete job
+            Sql( @"DELETE FROM [ServiceJob] WHERE [Guid] = '623F4751-C654-FEB7-45B7-59685B1F60AE'" );
+            
             // delete security for attribites
             DeleteSecurityFromAttribute( SystemGuid.Attribute.PERSON_ERA_CURRENTLY_AN_ERA );
             DeleteSecurityFromAttribute( SystemGuid.Attribute.PERSON_ERA_START_DATE );
