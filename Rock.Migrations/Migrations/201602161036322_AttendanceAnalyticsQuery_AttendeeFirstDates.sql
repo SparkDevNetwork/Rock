f7d8cf5528d8d186cbbc548831b229159d4fde1b
David Turner <david@sparkdevnetwork.org>
2016-03-09 14:11:05 -0700

    - Update stored procedures to not recompile

--- Rock.Migrations/Migrations/201602161036322_AttendanceAnalyticsQuery_AttendeeFirstDates.sql
+++ Rock.Migrations/Migrations/201602161036322_AttendanceAnalyticsQuery_AttendeeFirstDates.sql
@@ -34,6 +34,7 @@ CREATE PROCEDURE [dbo].[spCheckin_AttendanceAnalyticsQuery_AttendeeFirstDates]
 	, @EndDate datetime = NULL
 	, @CampusIds varchar(max) = NULL
 	, @IncludeNullCampusIds bit = 0
+	WITH RECOMPILE
 
 AS
 
