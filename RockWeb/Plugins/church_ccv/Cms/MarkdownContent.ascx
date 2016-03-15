2101557e7e5d292923bc82536efd9f8364fed1b1
Mike Peterson <MikePeterson@ccvonline.com>
2016-03-15 15:59:11 -0700

    added MarkdownContent block

---
+++ RockWeb/Plugins/church_ccv/Cms/MarkdownContent.ascx
@@ -0,0 +1,38 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="MarkdownContent.ascx.cs" Inherits="RockWeb.Plugins.church_ccv.Cms.MarkdownContent" %>
+
+<asp:UpdatePanel ID="upDetail" runat="server">
+    <ContentTemplate>
+        <div class="js-markdown-content">
+            <span class="js-markdown-view">
+                <asp:Literal ID="lContent" runat="server" />
+            </span>
+
+            <div class="js-markdown-block">
+                <asp:Panel ID="pnlEdit" runat="server" CssClass="js-md-edit" Style="display: none">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:MarkdownEditor ID="mdEdit" Rows="10" runat="server" />
+                        </div>
+                    </div>
+
+                    <div class="actions">
+                        <asp:LinkButton ID="btnSave" runat="server" CssClass="btn btn-primary btn-xs" Text="Save" OnClick="btnSave_Click" />
+                        <asp:LinkButton ID="btnCancel" runat="server" CssClass="btn btn-default btn-xs" Text="Cancel" OnClick="btnCancel_Click" />
+                    </div>
+                </asp:Panel>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+                Sys.Application.add_load(function () {
+
+                    $('.js-markdown-view').dblclick(function () {
+                        $(this).closest('.js-markdown-content').find('.js-markdown-view').hide();
+                        $(this).closest('.js-markdown-content').find('.js-md-edit').show();
+                    });
+                });
+            });
+        </script>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/church_ccv/Cms/MarkdownContent.ascx.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using Rock.Attribute;
+using Rock;
+
+namespace RockWeb.Plugins.church_ccv.Cms
+{
+    [DisplayName( "Markdown Content" )]
+    [Category( "CCV > Cms" )]
+    [Description( "Block for editing content using Markdown" )]
+
+    [TextField( "MarkdownContent", category: "CustomSetting" )]
+    public partial class MarkdownContent : church.ccv.Utility.Web.BaseContentBlock
+    {
+        /// <summary>
+        /// Implement to show the content. For example, simply have it do "lContent.Text = this.GetContentHtml();"
+        /// </summary>
+        public override void ShowContent()
+        {
+            mdEdit.Text = this.GetAttributeValue( "MarkdownContent" );
+            lContent.Text = this.GetContentHtml();
+        }
+
+        /// <summary>
+        /// Gets the content template.
+        /// </summary>
+        /// <returns></returns>
+        public override string GetContentTemplate()
+        {
+
+            var markdown = this.GetAttributeValue( "MarkdownContent" ) ?? string.Empty;
+            if ( string.IsNullOrWhiteSpace( markdown.Trim() ) )
+            {
+                markdown = "<small>Edit</small>";
+            }
+
+            return markdown.ConvertMarkdownToHtml( true );
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnEdit control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnEdit_Click( object sender, System.EventArgs e )
+        {
+
+            pnlEdit.Visible = true;
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnSave control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnSave_Click( object sender, System.EventArgs e )
+        {
+            FlushCachedContent();
+            this.SetAttributeValue( "MarkdownContent", mdEdit.Text );
+            this.SaveAttributeValues();
+            ShowContent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnCancel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void btnCancel_Click( object sender, System.EventArgs e )
+        {
+            ShowContent();
+        }
+    }
+}
