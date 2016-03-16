236457b8e14dfa0644ed721868be9b46dbe51e46
Mike Peterson <MikePeterson@ccvonline.com>
2016-03-15 18:31:43 -0700

    fix GatewayDetail from core

--- RockWeb/Blocks/Finance/GatewayDetail.ascx.cs
+++ RockWeb/Blocks/Finance/GatewayDetail.ascx.cs
@@ -18,6 +18,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Web.UI.WebControls;
 using Rock;
 using Rock.Constants;
 using Rock.Data;
