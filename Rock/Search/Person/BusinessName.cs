14aee695cfa6c9fecb4dd4f70d2fb4c2c8e8da97
nairdo <nairdo@gmail.com>
2016-03-16 15:17:25 -0700

    + Added new search service that allows searching for businesses.

---
+++ Rock/Search/Person/BusinessName.cs
@@ -0,0 +1,68 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+namespace Rock.Search.Person
+{
+    /// <summary>
+    /// Searches for businesses with matching names
+    /// </summary>
+    [Description("Business Name Search")]
+    [Export(typeof(SearchComponent))]
+    [ExportMetadata("ComponentName", "Business Name")]
+    public class BusinessName : SearchComponent
+    {
+        /// <summary>
+        /// Gets the attribute value defaults.
+        /// </summary>
+        /// <value>
+        /// The attribute defaults.
+        /// </value>
+        public override Dictionary<string, string> AttributeValueDefaults
+        {
+            get
+            {
+                var defaults = new Dictionary<string, string>();
+                defaults.Add( "SearchLabel", "Business" );
+                return defaults;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of matching businesses
+        /// </summary>
+        /// <param name="searchterm"></param>
+        /// <returns></returns>
+        public override IQueryable<string> Search( string searchterm )
+        {
+            var recordTypeValueId = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSON_RECORD_TYPE_BUSINESS.AsGuid() ).Id;
+
+            return new PersonService( new RockContext() ).Queryable()
+                .Where( q => q.RecordTypeValueId == recordTypeValueId && q.LastName.Contains( searchterm ) )
+                .OrderBy( q => q.LastName )
+                .Select( b => b.LastName )
+                .Distinct();
+        }
+    }
+}
