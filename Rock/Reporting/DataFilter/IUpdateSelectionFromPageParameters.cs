1a23bbac27ed4103a41c8478e12c163441e42e3a
Mike Peterson <mikepeterson@ccvonline.com>
2016-04-01 16:39:09 -0700

    -Update copyright headers on all cs files to include the new standard copyright header (now without the year)

--- Rock/Reporting/DataFilter/IUpdateSelectionFromPageParameters.cs
+++ Rock/Reporting/DataFilter/IUpdateSelectionFromPageParameters.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright>
-// Copyright 2013 by the Spark Development Network
+// Copyright by the Spark Development Network
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 // limitations under the License.
 // </copyright>
 //
-
 namespace Rock.Reporting.DataFilter
 {
     /// <summary>
