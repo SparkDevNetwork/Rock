e535d93dd1edb8bd49fa0b13ee210c1fbcb991c5
Mike Peterson <MikePeterson@ccvonline.com>
2016-04-05 16:06:58 -0700

    latest RockPage.cs from core (fixes recent GoogleAnalytics issue)

--- Rock/Web/UI/RockPage.cs
+++ Rock/Web/UI/RockPage.cs
@@ -1530,7 +1530,11 @@ namespace Rock.Web.UI
         public string ResolveRockUrlIncludeRoot( string url )
         {
             string virtualPath = this.ResolveRockUrl( url );
-            return string.Format( "{0}://{1}{2}", Context.Request.Url.Scheme, Context.Request.Url.Authority, virtualPath );
+            if ( Context.Request.UrlReferrer != null )
+            {
+                return string.Format( "{0}://{1}{2}", Context.Request.UrlReferrer.Scheme, Context.Request.UrlReferrer.Authority, virtualPath );
+            }
+            return GlobalAttributesCache.Read().GetValue("PublicApplicationRoot").EnsureTrailingForwardslash() + virtualPath;
         }
 
         /// <summary>
@@ -2277,7 +2281,7 @@ namespace Rock.Web.UI
                 if ( AddScriptTags )
                 {
                     l.Text = string.Format( @"
-    <script type=""text/javascript"">������ 
+    <script type=""text/javascript""> 
 {0}
     </script>
 
