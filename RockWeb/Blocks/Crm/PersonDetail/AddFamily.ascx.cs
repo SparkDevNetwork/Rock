dbd69e591444e92bf3df2b465ae721611b82bef7
David Turner <david@sparkdevnetwork.org>
2016-04-12 15:03:16 -0700

    + Fixed issue with new family block saving phone numbers without a country code and added migration to fix all missing country code values.

--- RockWeb/Blocks/Crm/PersonDetail/AddFamily.ascx.cs
+++ RockWeb/Blocks/Crm/PersonDetail/AddFamily.ascx.cs
@@ -453,6 +453,11 @@ namespace RockWeb.Blocks.Crm.PersonDetail
                         contactInfoRow.HomePhoneNumber = PhoneNumber.FormattedNumber( homePhoneNumber.CountryCode, homePhoneNumber.Number );
                         contactInfoRow.HomePhoneCountryCode = homePhoneNumber.CountryCode;
                     }
+                    else
+                    {
+                        contactInfoRow.HomePhoneNumber = string.Empty;
+                        contactInfoRow.HomePhoneCountryCode = string.Empty;
+                    }
                 }
 
                 if ( _cellPhone != null )
@@ -462,6 +467,11 @@ namespace RockWeb.Blocks.Crm.PersonDetail
                     {
                         contactInfoRow.CellPhoneNumber = PhoneNumber.FormattedNumber( cellPhoneNumber.CountryCode, cellPhoneNumber.Number );
                         contactInfoRow.CellPhoneCountryCode = cellPhoneNumber.CountryCode;                     
+                    }
+                    else
+                    {
+                        contactInfoRow.CellPhoneNumber = string.Empty;
+                        contactInfoRow.CellPhoneCountryCode = string.Empty;
                     }
                 }
 
