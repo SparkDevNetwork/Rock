4cd336dc6c0c6385763875160c0a7d136ac285a5
Mike Peterson <MikePeterson@ccvonline.com>
2016-04-18 10:41:31 -0700

    - add com_centralaz LavaTester block to sourcecontrol since our production site uses it

---
+++ RockWeb/Plugins/com_centralaz/Utility/LavaTester.ascx
@@ -0,0 +1,57 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="LavaTester.ascx.cs" Inherits="RockWeb.Plugins.com_centralaz.Utility.LavaTester" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
+
+            <div class="panel-heading">
+                <h1 class="panel-title">Lava Tester</h1>
+                <div class="panel-labels">
+                    <Rock:RockCheckBox ID="cbEnableDebug" runat="server" Checked="false" Text="Enable Debug?" CssClass="pull-right" Help="If enabled, extra Lava debug information will be included at the bottom but it will slow down tremendously. You can also use the <a href='http://www.churchitnetwork.com/ModelMap'>ModelMap block</a> to see which properties exist on various entities."/>
+                </div>
+            </div>
+            <div class="panel-body form-group">
+                <fieldset>
+
+                    <Rock:NotificationBox ID="nbInstructions" runat="server" Title="Instructions" Text="Select one of the entities below, type some lava, and press the Test button." NotificationBoxType="Info" Dismissable="true" />
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <Rock:PersonPicker ID="ppPerson" Label="Person" runat="server" />
+                        </div>
+                        <div class="col-md-9">
+                            <Rock:GroupPicker ID="gpGroups" runat="server" Label="Group"/>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-3"><Rock:WorkflowTypePicker ID="wfpWorkflowType" runat="server" Label="Workflow Type" OnSelectItem="wfpWorkflowType_SelectItem" /></div>
+                                <div class="col-md-9"><Rock:DataDropDownList ID="ddlWorkflows" runat="server" Label="Workflow (instances)" SourceTypeName="Rock.Model.Workflow, Rock" DataTextField="Name" DataValueField="Id" PropertyName="Name" Visible="false" OnSelectedIndexChanged="ddlWorkflows_SelectedIndexChanged" /></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceLava" runat="server" Label="Lava" EditorMode="Lava" EditorTheme="Rock" EditorHeight="150" Placeholder="test" />
+                        </div>
+                    </div>
+
+                    <div class="actions">
+                        <Rock:BootstrapButton ID="bbTest" runat="server" CssClass="btn btn-primary" OnClick="bbTest_Click">Test</Rock:BootstrapButton>
+                    </div>
+
+                    <h3>Output</h3>
+                    <div class="well" style="background-color: #fff">
+                        <asp:Literal ID="litOutput" runat="server"></asp:Literal>
+                    </div>
+
+                    <h3 runat="server" id="h3DebugTitle" visible="false">Lava Reference / Debug</h3>
+                    <asp:Literal ID="litDebug" runat="server"></asp:Literal>
+                </fieldset>
+            </div>
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/com_centralaz/Utility/LavaTester.ascx.cs
@@ -0,0 +1,219 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+
+namespace RockWeb.Plugins.com_centralaz.Utility
+{
+    /// <summary>
+    /// Template block for developers to use to start a new block.
+    /// </summary>
+    [DisplayName( "Lava Tester" )]
+    [Category( "com_centralaz > Utility" )]
+    [Description( "Allows you to pick a person entity and test your lava." )]
+    public partial class LavaTester : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+        // used for private variables
+        Dictionary<string,object> mergeFields = Rock.Web.Cache.GlobalAttributesCache.GetMergeFields( null );
+        private readonly string _USER_PREF_KEY = "MyLavaTestText";
+        #endregion
+
+        #region Properties
+
+        // used for public / protected properties
+
+        #endregion
+
+        #region Base Control Methods
+
+        //  overrides of the base RockBlock methods (i.e. OnInit, OnLoad)
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+            
+            if ( string.IsNullOrEmpty( ceLava.Text ) )
+            {
+                var text = GetUserPreference( _USER_PREF_KEY );
+                ceLava.Text = text;
+
+                // Only show instructions the first time.
+                if ( !string.IsNullOrEmpty( text ) )
+                {
+                    nbInstructions.Visible = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            litDebug.Text = "";
+
+            if ( ! Page.IsPostBack )
+            {
+                mergeFields.Add( "CurrentPerson", CurrentPerson );
+                if ( cbEnableDebug.Checked )
+                {
+                    litDebug.Text = mergeFields.lavaDebugInfo();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        // helper functional methods (like BindGrid(), etc.)
+
+        #endregion
+        protected void bbTest_Click( object sender, EventArgs e )
+        {
+            nbInstructions.Visible = false;
+
+            try
+            {
+                // Save lava test string for future use.
+                SetUserPreference( _USER_PREF_KEY, ceLava.Text );
+
+                RockContext rockContext = new RockContext();
+                PersonService personService = new PersonService( rockContext );
+                Person person;
+                if ( ppPerson.PersonId.HasValue )
+                {
+                    person = personService.Get( ppPerson.PersonId ?? -1, false );
+                }
+                else
+                {
+                    person = CurrentPerson;
+                }
+
+                if ( gpGroups != null && gpGroups.SelectedValueAsInt().HasValue )
+                {
+                    GroupService groupService = new GroupService( rockContext );
+                    mergeFields.Add( "Group", groupService.Get( gpGroups.SelectedValueAsInt() ?? -1 ) );
+                }
+
+                if ( ddlWorkflows != null && ddlWorkflows.Items.Count > 0 && ddlWorkflows.SelectedValueAsInt().HasValue )
+                {
+                    WorkflowService workflowService = new WorkflowService( rockContext );
+                    if ( mergeFields.ContainsKey( "Workflow" ) )
+                    {
+                        mergeFields.Remove( "Workflow" );
+                    }
+                    mergeFields.Add( "Workflow", workflowService.Get( ddlWorkflows.SelectedValueAsInt() ?? -1 ) );
+                }
+
+                // Get Lava
+                mergeFields.Add( "CurrentPerson", CurrentPerson );
+                mergeFields.Add( "Person", person );
+
+                ResolveLava();
+            }
+            catch( Exception ex )
+            {
+                //LogException( ex );
+                litDebug.Text = "<pre>" + ex.StackTrace + "</pre>";
+            }
+        }
+
+        protected void wfpWorkflowType_SelectItem( object sender, EventArgs e )
+        {
+            RockContext rockContext = new RockContext();
+            WorkflowService workflowService = new WorkflowService( rockContext );
+            int? workflowTypeId = wfpWorkflowType.SelectedValueAsInt();
+            if ( workflowTypeId.HasValue )
+            {
+                var workflows = workflowService.Queryable().AsNoTracking().Where( w => w.WorkflowTypeId == workflowTypeId.Value ).ToList();
+                ddlWorkflows.DataSource = workflows;
+                ddlWorkflows.DataBind();
+                ddlWorkflows.Visible = true;
+
+                if ( workflows.Count > 0 )
+                {
+                    mergeFields.Add( "Workflow", workflows[0] );
+                    ResolveLava();
+                }
+
+            }
+        }
+
+        protected void ddlWorkflows_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            RockContext rockContext = new RockContext();
+            WorkflowService workflowService = new WorkflowService( rockContext );
+            mergeFields.Add( "Workflow", workflowService.Get( ddlWorkflows.SelectedValueAsInt() ?? -1 ) );
+
+            ResolveLava();
+        }
+
+        protected void ResolveLava()
+        {
+            string lava = ceLava.Text;
+            litOutput.Text = lava.ResolveMergeFields( mergeFields );
+            if ( cbEnableDebug.Checked )
+            {
+                litDebug.Text = mergeFields.lavaDebugInfo();
+                h3DebugTitle.Visible = true;
+            }
+        }
+}
+}
