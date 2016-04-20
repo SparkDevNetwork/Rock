4e42bbe5c2f9e5bf5d13949a0d3d6de1452d62be
edmistj <jon@sparkdevnetwork.org>
2016-04-20 14:52:35 -0700

    - updates on the steps taken blocks

--- RockWeb/Plugins/church_ccv/Steps/StepsTaken.ascx
+++ RockWeb/Plugins/church_ccv/Steps/StepsTaken.ascx
@@ -26,17 +26,66 @@
             <div class="panel-body">
                 <div class="panel-settings js-settings-panel" style="display: none;">
                     <div class="row">
-                        <div class="col-md-4">
-                            <Rock:RockDropDownList ID="ddlSundayDates" Label="Recent Dates" runat="server" Help="The last 12 Sundays." />
-                        </div>
-                        <div class="col-md-4">
-                            <Rock:DatePicker ID="dpSundayPicker" Label="Specific Date" runat="server" Help="Select a specific date you would like to report on. The system will select the Sunday Date for the date you select." />
-                        </div>
+                        <div class="col-md-8">
+                            <Rock:SlidingDateRangePicker ID="drpDateRange" runat="server" EnabledSlidingDateRangeTypes="Previous, Last, Current, DateRange" EnabledSlidingDateRangeUnits="Week, Month, Year" />
+                         </div>
                         <div class="col-md-4 text-right">
-                            <asp:LinkButton ID="lbSetDate" runat="server" CssClass="btn btn-primary btn-sm margin-t-lg" Text="Set Date"  />
+                            <asp:LinkButton ID="lbSetDateRange" runat="server" CssClass="btn btn-primary btn-sm margin-t-lg" Text="Update" OnClick="lbSetDateRange_Click"  />
                         </div>
                     </div>
                 </div>
+
+                <ul class="nav nav-pills margin-b-md">
+                    <li id="liCampus" runat="server" class="active">
+                        <asp:LinkButton ID="lbCampus" runat="server" Text="Campus" OnClick="lbTab_Click" />
+                    </li>
+                    <li id="liPastor" runat="server">
+                        <asp:LinkButton ID="lbPastor" runat="server" Text="Pastor" OnClick="lbTab_Click" />
+                    </li>
+                    <li id="liAdults" runat="server" >
+                        <asp:LinkButton ID="lbAdults" runat="server" Text="Adults" OnClick="lbTab_Click" />
+                    </li>
+                    <li id="liStepDetails" runat="server">
+                        <asp:LinkButton ID="lbStepDetails" runat="server" Text="Step Details" OnClick="lbTab_Click" />
+                    </li>
+                </ul>
+
+                <asp:Panel ID="pnlCampus" runat="server">
+                    <h4>Not Implemented</h4>
+                </asp:Panel>
+
+                <asp:Panel ID="pnlPastor" runat="server" Visible="false">
+                    <h4>Not Implemented</h4>
+                </asp:Panel>
+
+                <asp:Panel ID="pnlAdults" runat="server" Visible="false">
+                    <h4>Not Implemented</h4>
+                </asp:Panel>
+
+                <asp:Panel ID="pnlStepDetails" runat="server" Visible="false">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2><asp:Literal ID="lDetailCampus" runat="server" Text="All Campuses" /></h2>
+                        </div>
+                        <div class="col-md-6">
+                            <Rock:CampusPicker ID="cpDetailCampus" runat="server" OnSelectedIndexChanged="cpDetailCampus_SelectedIndexChanged" AutoPostBack="true" />
+                        </div>
+                    </div>
+                    
+                    <div class="grid">
+                         <Rock:GridFilter ID="gfStepDetails" runat="server">
+                            <Rock:RockDropDownList ID="ddlMeasureType" runat="server" Label="Step Type" />
+                        </Rock:GridFilter>
+                        <Rock:Grid ID="gStepDetails" runat="server">
+                            <Columns>
+                                <asp:BoundField DataField="DateTaken" HeaderText="Date Taken" dataformatstring="{0:M/d/yyyy}" />
+                                <asp:BoundField DataField="StepMeasureTitle" HeaderText="Step" />
+                                <asp:BoundField DataField="FullName" HeaderText="Name" />
+                                <asp:BoundField DataField="Campus" HeaderText="Campus" />
+                            </Columns>
+                        </Rock:Grid>
+                    </div>
+                </asp:Panel>
 
             </div>
         
--- RockWeb/Plugins/church_ccv/Steps/StepsTaken.ascx.cs
+++ RockWeb/Plugins/church_ccv/Steps/StepsTaken.ascx.cs
@@ -32,13 +32,14 @@ using Rock.Attribute;
 using church.ccv.Steps;
 using church.ccv.Steps.Model;
 using System.Drawing;
+using System.Data.Entity;
 
 namespace RockWeb.Plugins.church_ccv.Steps
 {
     /// <summary>
     /// A block to show a dasboard of measure for the pastor.
     /// </summary>
-    [DisplayName( "StepsTaken" )]
+    [DisplayName( "Steps Taken" )]
     [Category( "CCV > Steps" )]
     [Description( "A block to show the number of steps taken." )]
 
@@ -46,8 +47,26 @@ namespace RockWeb.Plugins.church_ccv.Steps
     {
         #region Fields
 
-        
 
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the active tab.
+        /// </summary>
+        /// <value>
+        /// The active tab.
+        /// </value>
+        protected string ActiveTab { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date range.
+        /// </summary>
+        /// <value>
+        /// The date range.
+        /// </value>
+        protected DateRange DateRange { get; set; }
         #endregion
 
         #region Base Control Methods
@@ -65,6 +84,10 @@ namespace RockWeb.Plugins.church_ccv.Steps
             // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
             this.BlockUpdated += Block_BlockUpdated;
             this.AddConfigurationUpdateTrigger( upnlContent );
+
+            gfStepDetails.ApplyFilterClick += GfStepDetails_ApplyFilterClick;
+            gfStepDetails.DisplayFilterValue += GfStepDetails_DisplayFilterValue;
+            gStepDetails.GridRebind += GStepDetails_GridRebind;
         }
 
         /// <summary>
@@ -75,9 +98,18 @@ namespace RockWeb.Plugins.church_ccv.Steps
         {
             base.OnLoad( e );
 
+            DateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( drpDateRange.DelimitedValues );
+
             if ( !Page.IsPostBack )
             {
-                
+                drpDateRange.SlidingDateRangeMode = SlidingDateRangePicker.SlidingDateRangeType.Current;
+                drpDateRange.TimeUnit = SlidingDateRangePicker.TimeUnitType.Year;
+
+                DateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( drpDateRange.DelimitedValues );
+
+                SetDateLabel();
+                LoadCampuses();
+                LoadStepTypes();
             }
         }
 
@@ -97,12 +129,256 @@ namespace RockWeb.Plugins.church_ccv.Steps
 
         }
 
+        /// <summary>
+        /// Handles the Click event of the lbSetDateRange control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbSetDateRange_Click( object sender, EventArgs e )
+        {
+            SetDateLabel();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbTab control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbTab_Click( object sender, EventArgs e )
+        {
+            LinkButton lb = sender as LinkButton;
+            if ( lb != null )
+            {
+                ActiveTab = lb.ID;
+                ShowTab();
+            }
+        }
+
+        /// <summary>
+        /// Handles the SelectedIndexChanged event of the cpDetailCampus control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void cpDetailCampus_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            ShowStepDetails();
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the GStepDetails control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="GridRebindEventArgs"/> instance containing the event data.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void GStepDetails_GridRebind( object sender, GridRebindEventArgs e )
+        {
+            ShowStepDetails();
+        }
+
+        /// <summary>
+        /// Handles the ApplyFilterClick event of the GfStepDetails control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void GfStepDetails_ApplyFilterClick( object sender, EventArgs e )
+        {
+            gfStepDetails.SaveUserPreference( "Measure Type", ddlMeasureType.SelectedValue != string.Empty ? ddlMeasureType.SelectedValue : string.Empty );
+
+            ShowStepDetails();
+        }
+
+        /// <summary>
+        /// Gfs the step details_ display filter value.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void GfStepDetails_DisplayFilterValue( object sender, GridFilter.DisplayFilterValueArgs e )
+        {
+            switch ( e.Key )
+            {
+                case "Measure Type":
+                    int? measureId = e.Value.AsInteger();
+
+                    if ( measureId.HasValue )
+                    {
+                        e.Value = new StepMeasureService( new RockContext() ).Get( measureId.Value).Title;
+                    }
+
+                    break;
+            }
+        }
         #endregion
 
         #region Methods
 
-        
-        #endregion
+        /// <summary>
+        /// Loads the step types.
+        /// </summary>
+        private void LoadStepTypes()
+        {
+            using ( RockContext rockContext = new RockContext() ) {
 
+                var measureTypes = new StepMeasureService( rockContext ).Queryable()
+                                    .Where( m => m.IsActive && !m.IsTbd )
+                                    .Select( m => new { m.Id, m.Title } )
+                                    .OrderBy( m => m.Title);
+
+                ddlMeasureType.DataTextField = "Title";
+                ddlMeasureType.DataValueField = "Id";
+                ddlMeasureType.DataSource = measureTypes.ToList();
+                ddlMeasureType.DataBind();
+
+                ddlMeasureType.Items.Insert( 0, "" );
+
+                ddlMeasureType.SelectedValue = gfStepDetails.GetUserPreference( "Measure Type" );
+            }
+        }
+
+        /// <summary>
+        /// Sets the date label.
+        /// </summary>
+        private void SetDateLabel()
+        {
+            if ( DateRange.Start.HasValue && DateRange.End.HasValue )
+            {
+                hlDate.Text = string.Format( "{0} - {1}", DateRange.Start.Value.ToShortDateString(), DateRange.End.Value.ToShortDateString() );
+            }
+            else if ( DateRange.Start.HasValue )
+            {
+                hlDate.Text = string.Format( "Since {0}", DateRange.Start.Value.ToShortDateString() );
+            }
+            else if ( DateRange.End.HasValue )
+            {
+                hlDate.Text = string.Format( "Before {0}", DateRange.End.Value.ToShortDateString() );
+            }
+            else
+            {
+                hlDate.Text = "No Date Range Provided";
+            }
+        }
+
+        /// <summary>
+        /// Shows the tab.
+        /// </summary>
+        private void ShowTab()
+        {
+            liCampus.RemoveCssClass( "active" );
+            pnlCampus.Visible = false;
+
+            liPastor.RemoveCssClass( "active" );
+            pnlPastor.Visible = false;
+
+            liAdults.RemoveCssClass( "active" );
+            pnlAdults.Visible = false;
+
+            liStepDetails.RemoveCssClass( "active" );
+            pnlStepDetails.Visible = false;
+
+            switch ( ActiveTab ?? string.Empty )
+            {
+                case "lbPastor":
+                    {
+                        liPastor.AddCssClass( "active" );
+                        pnlPastor.Visible = true;
+                        //BindPaymentsGrid();
+                        break;
+                    }
+
+                case "lbAdults":
+                    {
+                        liAdults.AddCssClass( "active" );
+                        pnlAdults.Visible = true;
+                        //BindLinkagesGrid();
+                        break;
+                    }
+
+                case "lbStepDetails":
+                    {
+                        liStepDetails.AddCssClass( "active" );
+                        pnlStepDetails.Visible = true;
+                        ShowStepDetails();
+                        break;
+                    }
+
+                default:
+                    {
+                        liCampus.AddCssClass( "active" );
+                        pnlCampus.Visible = true;
+                        //BindRegistrationsGrid();
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Loads the campuses.
+        /// </summary>
+        private void LoadCampuses()
+        {
+            cpDetailCampus.Campuses = CampusCache.All();
+        }
+
+        private void ShowStepDetails(int? campusId = null )
+        {
+            if ( cpDetailCampus.SelectedCampusId == null )
+            {
+                lDetailCampus.Text = "All Campuses ";
+            }
+            else
+            {
+                campusId = cpDetailCampus.SelectedItem.Value.AsInteger();
+                lDetailCampus.Text = string.Format( "{0} Campus", cpDetailCampus.SelectedItem.Text );
+            }
+
+            using (RockContext rockContext = new RockContext() )
+            {
+                StepTakenService stepTakenService = new StepTakenService( rockContext );
+
+                var query = stepTakenService.Queryable("StepMeasure").AsNoTracking();
+                
+                if ( campusId.HasValue )
+                {
+                    query = query.Where( s => s.CampusId == campusId.Value );
+                }
+                
+                if ( DateRange != null && DateRange.Start.HasValue )
+                {
+                    query = query.Where( s => s.DateTaken >= DateRange.Start.Value );
+                }
+                
+                if ( DateRange != null && DateRange.End.HasValue )
+                {
+                    query = query.Where( s => s.DateTaken <= DateRange.End.Value );
+                }
+
+                if ( !string.IsNullOrWhiteSpace(ddlMeasureType.SelectedValue ))
+                {
+                    int? measureId = ddlMeasureType.SelectedValue.AsInteger();
+                    query = query.Where( s => s.StepMeasureId == measureId );
+                }
+
+                var results = query.Select( s =>
+                                new {
+                                    s.Id,
+                                    s.DateTaken,
+                                    StepMeasureTitle = s.StepMeasure.Title,
+                                    s.StepMeasureId,
+                                    s.SundayDate,
+                                    s.PersonAlias.PersonId,
+                                    FirstName = s.PersonAlias.Person.FirstName,
+                                    NickName = s.PersonAlias.Person.NickName,
+                                    LastName = s.PersonAlias.Person.LastName,
+                                    FullName = s.PersonAlias.Person.LastName + ", " + s.PersonAlias.Person.NickName,
+                                    Campus = s.Campus.Name
+                                }
+                );
+
+                gStepDetails.SetLinqDataSource( results.OrderBy(s => s.DateTaken) );
+                gStepDetails.DataBind();
+            }
+        }
+        #endregion
     }
 }
