9e0f3b35fb7cf53a99bd7d6abd56b519e51e7b2d
nairdo <nairdo@gmail.com>
2016-05-04 10:48:35 -0700

    + Added new Spouse Transform which does what you think it would do.

---
+++ com.centralaz.DataTransform/Person/SpouseTransform.cs
@@ -0,0 +1,105 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Reporting;
+
+namespace com.centralaz.DataTransform.Person
+{
+    /// <summary>
+    /// Person Spouse Transformation
+    /// </summary>
+    [Description( "Transform result to spouse" )]
+    [Export( typeof( DataTransformComponent ) )]
+    [ExportMetadata( "ComponentName", "Person Spouse Transformation" )]
+    public class SpouseTransform : DataTransformComponent<Rock.Model.Person>
+    {
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public override string Title
+        {
+            get { return "com_centralaz: Spouse"; }
+        }
+
+        /// <summary>
+        /// Gets the name of the transformed entity type.
+        /// </summary>
+        /// <value>
+        /// The name of the transformed entity type.
+        /// </value>
+        public override string TransformedEntityTypeName
+        {
+            get { return "Rock.Model.Person"; }
+        }
+
+        /// <summary>
+        /// Gets the expression.
+        /// </summary>
+        /// <param name="serviceInstance">The service instance.</param>
+        /// <param name="parameterExpression">The parameter expression.</param>
+        /// <param name="whereExpression">The where expression.</param>
+        /// <returns></returns>
+        public override Expression GetExpression( IService serviceInstance, ParameterExpression parameterExpression, Expression whereExpression )
+        {
+            IQueryable<int> idQuery = serviceInstance.GetIds( parameterExpression, whereExpression );
+            return BuildExpression( serviceInstance, idQuery, parameterExpression );
+        }
+
+        /// <summary>
+        /// Gets the expression.
+        /// </summary>
+        /// <param name="serviceInstance">The service instance.</param>
+        /// <param name="personQueryable">The person queryable.</param>
+        /// <param name="parameterExpression">The parameter expression.</param>
+        /// <returns></returns>
+        public override Expression GetExpression( IService serviceInstance, IQueryable<Rock.Model.Person> personQueryable, ParameterExpression parameterExpression )
+        {
+            return BuildExpression( serviceInstance, personQueryable.Select( p => p.Id ), parameterExpression );
+        }
+
+        /// <summary>
+        /// Builds the expression to get spouses of the given id.  This will be all adult members of the id in the idQuery except himself/herself.
+        /// </summary>
+        /// <param name="serviceInstance">The service instance.</param>
+        /// <param name="idQuery">The id query.</param>
+        /// <param name="parameterExpression">The parameter expression.</param>
+        /// <returns></returns>
+        private Expression BuildExpression( IService serviceInstance, IQueryable<int> idQuery, ParameterExpression parameterExpression )
+        {
+            Guid adultGuid = Rock.SystemGuid.GroupRole.GROUPROLE_FAMILY_MEMBER_ADULT.AsGuid();
+            Guid childGuid = Rock.SystemGuid.GroupRole.GROUPROLE_FAMILY_MEMBER_CHILD.AsGuid();
+
+            var qry = new PersonService( (RockContext)serviceInstance.Context ).Queryable()
+                .Where( p => p.Members.Where( a => a.GroupRole.Guid == adultGuid && ! idQuery.Contains( a.PersonId ) )
+                    .Any( a => a.Group.Members.Any( c => idQuery.Contains( c.PersonId ) ) ) );
+
+            return FilterExpressionExtractor.Extract<Rock.Model.Person>( qry, parameterExpression, "p" );
+        }
+    }
+}
