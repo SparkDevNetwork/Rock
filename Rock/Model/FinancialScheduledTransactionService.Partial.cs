542b6c5cd0af6961814c09efa088df864d5b83b1
David Turner <david@sparkdevnetwork.org>
2016-05-05 06:34:57 -0700

    - Updated transaction download process so that it does not send reciept if transaction was a reversal (failed transaction).

--- Rock/Model/FinancialScheduledTransactionService.Partial.cs
+++ Rock/Model/FinancialScheduledTransactionService.Partial.cs
@@ -288,8 +288,6 @@ namespace Rock.Model
 
                         // Calculate whether a transaction needs to be added
                         var txnAmount = CalculateTransactionAmount( payment, txns );
-
-                        // Only consider transactions that have not already been added
                         if ( txnAmount != 0.0M )
                         {
                             scheduledTransactionIds.Add( scheduledTransaction.Id );
@@ -403,7 +401,7 @@ namespace Rock.Model
                             }
 
                             // If the amount to apply was negative, update all details to be negative (absolute value was used when allocating to accounts)
-                            if ( txnAmount < 0 )
+                            if ( txnAmount < 0.0M )
                             {
                                 foreach ( var txnDetail in transaction.TransactionDetails )
                                 {
@@ -429,7 +427,7 @@ namespace Rock.Model
 
                             batch.Transactions.Add( transaction );
 
-                            if ( recieptEmail.HasValue )
+                            if ( txnAmount > 0.0M && recieptEmail.HasValue )
                             {
                                 newTransactions.Add( transaction );
                             }
@@ -441,7 +439,7 @@ namespace Rock.Model
                             }
                             batchSummary[batch.Guid].Add( txnAmount );
 
-                            if ( txnAmount > 0 )
+                            if ( txnAmount > 0.0M )
                             {
                                 totalAdded++;
                             }
