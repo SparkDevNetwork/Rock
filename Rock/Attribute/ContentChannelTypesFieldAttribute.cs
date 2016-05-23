706ec7c887e37e2b4a9f079391968ba481e43e05
Mike Peterson <MikePeterson@ccvonline.com>
2016-05-23 15:57:44 -0700

    - Fix compiler warnings

--- Rock/Attribute/ContentChannelTypesFieldAttribute.cs
+++ Rock/Attribute/ContentChannelTypesFieldAttribute.cs
@@ -28,6 +28,16 @@ namespace Rock.Attribute
     [AttributeUsage( AttributeTargets.Class, AllowMultiple = true, Inherited = true )]
     public class ContentChannelTypesFieldAttribute : FieldAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentChannelTypesFieldAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="required">if set to <c>true</c> [required].</param>
+        /// <param name="defaultGroupTypeGuids">The default group type guids.</param>
+        /// <param name="category">The category.</param>
+        /// <param name="order">The order.</param>
+        /// <param name="key">The key.</param>
         public ContentChannelTypesFieldAttribute( string name, string description = "", bool required = true, string defaultGroupTypeGuids = "", string category = "", int order = 0, string key = null )
             : base( name, description, required, defaultGroupTypeGuids, category, order, key, typeof( Rock.Field.Types.ContentChannelTypesFieldType ).FullName )
         {
