18555cfe98de55bb070731f9b9e776a7ca5516f7
tcavaletto <Taylor.Cavaletto@centralAZ.com>
2016-06-01 09:37:18 -0700

    + Added Email Topic Subscription Preference block

---
+++ RockWeb/Plugins/com_centralaz/Utility/EmailTopicSubscriptionPreference.ascx
@@ -0,0 +1,22 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="EmailTopicSubscriptionPreference.ascx.cs" Inherits="RockWeb.Plugins.com_centralaz.Utility.EmailTopicSubscriptionPreference" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <Rock:NotificationBox ID="nbConfigurationError" runat="server" Visible="false" NotificationBoxType="Warning" Title="Configuration Error" Text="This block is not yet configured or it's misconfigured."></Rock:NotificationBox>
+        <asp:HiddenField ID="hfAttributeId" runat="server" />
+        <asp:HiddenField ID="hfAttributeKey" runat="server" />
+        <asp:Panel ID="pnlViewPreferences" runat="server">
+            <Rock:NotificationBox ID="nbSuccess" runat="server" Visible="false" NotificationBoxType="Success" Text="Your preference was saved."></Rock:NotificationBox>
+            <asp:Literal ID="lPreferences" runat="server" /></h1>
+            <asp:LinkButton ID="lbEditPreferences" runat="server" CssClass="btn btn-primary btn-xs" OnClick="lbEditPreferences_Click"><i class="fa fa-pencil"></i> Edit Subscription Preferences</asp:LinkButton>
+        </asp:Panel>
+        <asp:Panel ID="pnlEditPreferences" runat="server" Visible="false">
+            <Rock:NotificationBox ID="nbFail" runat="server" Visible="false" NotificationBoxType="Danger" Text="Hmmm...something went wrong and your preference was not saved. If it happens again please let us know."></Rock:NotificationBox>
+            <Rock:RockCheckBoxList ID="cblPreference" runat="server" RepeatDirection="Vertical" />
+            <div class="actions">
+                <asp:LinkButton ID="btnSave" runat="server" AccessKey="s" Text="Save" CssClass="btn btn-primary" OnClick="btnSave_Click" />
+                <asp:LinkButton ID="btnCancel" runat="server" AccessKey="c" Text="Cancel" CssClass="btn btn-link" CausesValidation="false" OnClick="btnCancel_Click" />
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/com_centralaz/Utility/EmailTopicSubscriptionPreference.ascx.cs
@@ -0,0 +1,271 @@
+﻿// <copyright>
+// Copyright 2013 by the Spark Development Network
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+using Rock.Web.UI;
+using System.Text;
+
+namespace RockWeb.Plugins.com_centralaz.Utility
+{
+    [DisplayName( "Email Topic Subscription Preference" )]
+    [Category( "com_centralaz > Utility" )]
+    [Description( "Displays and or sets the user's email subscriptions" )]
+    [TextField( "Label Text", "The Text that will be displayed as the checkboxlist's label.", true, "Subscribed Topics:" )]
+    [AttributeField( "Subscription Preference Attribute Guid", "Guid of the person attribute that holds each person's frequency choice.  Note: The attribute must be of type DefinedType.", required: true, defaultValue: "1E372FF6-93D9-4D42-9107-4FAD1E452218" )]
+    public partial class EmailTopicSubscriptionPreference : Rock.Web.UI.RockBlock
+    {
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                if ( CurrentPerson != null )
+                {
+                    ShowDetail();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the Click event of the btnSave control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnSave_Click( object sender, EventArgs e )
+        {
+            nbFail.Visible = false;
+            nbSuccess.Visible = false;
+            try
+            {
+                if ( CurrentPerson != null )
+                {
+                    CurrentPerson.LoadAttributes();
+                    CurrentPerson.SetAttributeValue( hfAttributeKey.Value, cblPreference.SelectedValues.AsDelimited( "," ) );
+                    CurrentPerson.SaveAttributeValues();
+                }
+
+                nbSuccess.Visible = true;
+                ShowDetail();
+            }
+            catch
+            {
+                nbFail.Visible = true;
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnCancel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnCancel_Click( object sender, EventArgs e )
+        {
+            nbSuccess.Visible = false;
+            ShowDetail();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbEditPreferences control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbEditPreferences_Click( object sender, EventArgs e )
+        {
+            BindOptionsToList();
+            pnlViewPreferences.Visible = false;
+            pnlEditPreferences.Visible = true;
+        }
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            ShowDetail();
+        }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Shows the detail.
+        /// </summary>
+        private void ShowDetail()
+        {
+            pnlEditPreferences.Visible = false;
+            pnlViewPreferences.Visible = true;
+
+            DefinedTypeCache definedType = GetDefinedType();
+            if ( definedType == null )
+            {
+                nbConfigurationError.Visible = true;
+                return;
+            }
+
+            List<Guid> subscribedGuidList = new List<Guid>();
+            CurrentPerson.LoadAttributes();
+            var subscribedDefinedValueGuids = CurrentPerson.GetAttributeValue( hfAttributeKey.Value );
+            if ( !String.IsNullOrWhiteSpace( subscribedDefinedValueGuids ) )
+            {
+                subscribedGuidList = subscribedDefinedValueGuids.SplitDelimitedValues().AsGuidList();
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine( String.Format( "<b>{0}</b><br>", GetAttributeValue( "LabelText" ) ) );
+            foreach ( var value in definedType.DefinedValues )
+            {
+                sb.AppendLine( String.Format( "<i class='fa fa-{1}'></i>  {0}<br>", value.Value, subscribedGuidList.Contains( value.Guid ) ? "check-square-o" : "square-o" ) );
+            }
+
+            lPreferences.Text = sb.ToString();
+        }
+
+        /// <summary>
+        /// Binds the options to list.
+        /// </summary>
+        private void BindOptionsToList()
+        {
+            try
+            {
+                DefinedTypeCache definedType = GetDefinedType();
+                if ( definedType == null )
+                {
+                    nbConfigurationError.Visible = true;
+                    return;
+                }
+
+                // Bind the definedType to our radio button list
+                var ds = definedType.DefinedValues
+                    .Select( v => new
+                    {
+                        Name = v.Value,
+                        v.Description,
+                        v.Guid
+                    } );
+
+                cblPreference.SelectedIndex = -1;
+                cblPreference.DataSource = ds;
+                cblPreference.DataTextField = "Name";
+                cblPreference.DataValueField = "Guid";
+                cblPreference.TextAlign = TextAlign.Left;
+                cblPreference.Label = GetAttributeValue( "LabelText" );
+                cblPreference.DataBind();
+
+                // set the person's current value as the selected one
+                CurrentPerson.LoadAttributes();
+
+                // The Guids that are put into the data list will be in lowercase as per
+                // https://msdn.microsoft.com/en-us/library/97af8hh4(v=vs.110).aspx
+                // so we need to make sure we're comparing with the lowercase of whatever
+                // was stored.
+                var personAttributeValueGuid = CurrentPerson.GetAttributeValue( hfAttributeKey.Value );
+                if ( personAttributeValueGuid != null )
+                {
+                    try
+                    {
+                        cblPreference.SetValues( personAttributeValueGuid.SplitDelimitedValues() );
+                    }
+                    catch
+                    { }
+                }
+
+            }
+            catch ( Exception ex )
+            {
+                nbConfigurationError.Visible = true;
+                nbConfigurationError.Text = "There appears to be a problem with something we did not expect. We've reported this to the system administrators.";
+                ExceptionLogService.LogException( new Exception( "Unable to load Email Topic Subscription Preference block.", ex ), null );
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the defined.
+        /// </summary>
+        /// <returns></returns>
+        private DefinedTypeCache GetDefinedType()
+        {
+            DefinedTypeCache definedType = null;
+
+            nbConfigurationError.Visible = false;
+            Guid? frequencyAttribute = GetAttributeValue( "SubscriptionPreferenceAttributeGuid" ).AsGuidOrNull();
+            if ( !frequencyAttribute.HasValue )
+            {
+                return definedType;
+            }
+
+            var personAttribute = Rock.Web.Cache.AttributeCache.Read( frequencyAttribute.Value );
+            if ( personAttribute == null )
+            {
+                return definedType;
+            }
+
+            hfAttributeKey.Value = personAttribute.Key;
+
+            if ( personAttribute.QualifierValues.Count > 0 )
+            {
+                var qualifierValue = personAttribute.QualifierValues.First( qv => qv.Key == "definedtype" ).Value;
+                if ( qualifierValue == null || qualifierValue.Value == null )
+                {
+                    return definedType;
+                }
+
+                definedType = DefinedTypeCache.Read( int.Parse( qualifierValue.Value ) );
+            }
+
+            return definedType;
+        }
+
+        #endregion
+    }
+}
