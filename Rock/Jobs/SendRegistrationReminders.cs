aa4aa333dbabe0e8774a5adb2726fbbce0993da8
tcavaletto <Taylor.Cavaletto@centralAZ.com>
2016-06-07 15:04:17 -0700

    + Added .ics download button to the event detail page.

--- Rock/Jobs/SendRegistrationReminders.cs
+++ Rock/Jobs/SendRegistrationReminders.cs
@@ -17,8 +17,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
-
+using System.Text;
 using Quartz;
 
 using Rock.Attribute;
@@ -85,7 +86,30 @@ namespace Rock.Jobs
                         string message = template.ReminderEmailTemplate.ResolveMergeFields( mergeFields );
 
                         var recipients = new List<string> { registration.ConfirmationEmail };
-                        Email.Send( from, fromName, subject, recipients, message );
+
+                        List<System.Net.Mail.Attachment> attachments = null;
+                        if ( registration.RegistrationInstance.Linkages.FirstOrDefault() != null )
+                        {
+                            try
+                            {
+                                var eventItemOccurrence = registration.RegistrationInstance.Linkages.FirstOrDefault().EventItemOccurrence;
+                                int insertIndex = eventItemOccurrence.Schedule.iCalendarContent.IndexOf( "END:VEVENT" );
+                                string location = !string.IsNullOrWhiteSpace( eventItemOccurrence.Location ) ? eventItemOccurrence.Location : "";
+                                string nameLocation = string.Format( "SUMMARY: {0}\r\nLOCATION: {1}\r\n", eventItemOccurrence.EventItem.Name, location );
+                                StringBuilder sb = new StringBuilder();
+                                sb.Append( eventItemOccurrence.Schedule.iCalendarContent.Insert( insertIndex, nameLocation ) );
+
+                                // convert string to stream
+                                byte[] byteArray = Encoding.UTF8.GetBytes( sb.ToString() );
+                                MemoryStream stream = new MemoryStream( byteArray );
+                                var attachment = new System.Net.Mail.Attachment( stream, string.Format( "{0}.ics", eventItemOccurrence.EventItem.Name ), "text/plain" );
+                                attachments = new List<System.Net.Mail.Attachment>();
+                                attachments.Add( attachment );
+                            }
+                            catch { }
+                        }
+
+                        Email.Send( from, fromName, subject, recipients, message, attachments: attachments );
                     }
 
                     instance.SendReminderDateTime = now;
