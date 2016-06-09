1e0901e03087bd9d8ce87a83ef50f451869296d8
Mike Peterson <MikePeterson@ccvonline.com>
2016-06-08 23:28:21 -0700

    updates from core

--- Rock/Attribute/ScheduleFieldAttribute.cs
+++ Rock/Attribute/ScheduleFieldAttribute.cs
@@ -28,6 +28,16 @@ namespace Rock.Attribute
     [AttributeUsage( AttributeTargets.Class, AllowMultiple = true, Inherited = true )]
     public class ScheduleFieldAttribute : FieldAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScheduleFieldAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="required">if set to <c>true</c> [required].</param>
+        /// <param name="defaultScheduleGuids">The default schedule guids.</param>
+        /// <param name="category">The category.</param>
+        /// <param name="order">The order.</param>
+        /// <param name="key">The key.</param>
         public ScheduleFieldAttribute( string name = "Schedule", string description = "", bool required = true, string defaultScheduleGuids = "", string category = "", int order = 0, string key = null )
             : base( name, description, required, defaultScheduleGuids, category, order, key, typeof( Rock.Field.Types.ScheduleFieldType ).FullName )
         {
