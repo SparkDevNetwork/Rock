fbb1f617bc518737e7e99af7a5562a1631306ad9
edmistj <jon@sparkdevnetwork.org>
2016-07-07 10:55:38 -0700

    - fixed typo in lava docs

--- Rock/Attribute/Helper.cs
+++ Rock/Attribute/Helper.cs
@@ -18,6 +18,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -1079,7 +1080,50 @@ namespace Rock.Attribute
             }
         }
 
+        /// <summary>
+        /// Gets the attribute value expression.
+        /// </summary>
+        /// <param name="attributeValues">The attribute values.</param>
+        /// <param name="attributeValueParameter">The attribute value parameter.</param>
+        /// <param name="parentIdProperty">The parent identifier property.</param>
+        /// <param name="attributeId">The attribute identifier.</param>
+        /// <returns></returns>
+        public static Expression GetAttributeValueExpression( IQueryable<AttributeValue> attributeValues, ParameterExpression attributeValueParameter, Expression parentIdProperty, int attributeId )
+        {
+            MemberExpression attributeIdProperty = Expression.Property( attributeValueParameter, "AttributeId" );
+            MemberExpression entityIdProperty = Expression.Property( attributeValueParameter, "EntityId" );
+            Expression attributeIdConstant = Expression.Constant( attributeId );
 
+            Expression attributeIdCompare = Expression.Equal( attributeIdProperty, attributeIdConstant );
+            Expression entityIdCompre = Expression.Equal( entityIdProperty, Expression.Convert( parentIdProperty, typeof( int? ) ) );
+            Expression andExpression = Expression.And( attributeIdCompare, entityIdCompre );
+
+            var match = new Expression[] {
+                Expression.Constant(attributeValues),
+                Expression.Lambda<Func<AttributeValue, bool>>( andExpression, new ParameterExpression[] { attributeValueParameter })
+            };
+
+            Expression whereExpression = Expression.Call( typeof( Queryable ), "Where", new Type[] { typeof( AttributeValue ) }, match );
+
+            var attributeCache = AttributeCache.Read( attributeId );
+            var attributeValueFieldName = "Value";
+            Type attributeValueFieldType = typeof( string );
+            if ( attributeCache != null )
+            {
+                attributeValueFieldName = attributeCache.FieldType.Field.AttributeValueFieldName;
+                attributeValueFieldType = attributeCache.FieldType.Field.AttributeValueFieldType;
+            }
+
+            MemberExpression valueProperty = Expression.Property( attributeValueParameter, attributeValueFieldName );
+
+            Expression valueLambda = Expression.Lambda( valueProperty, new ParameterExpression[] { attributeValueParameter } );
+
+            Expression selectValue = Expression.Call( typeof( Queryable ), "Select", new Type[] { typeof( AttributeValue ), attributeValueFieldType }, whereExpression, valueLambda );
+
+            Expression firstOrDefault = Expression.Call( typeof( Queryable ), "FirstOrDefault", new Type[] { attributeValueFieldType }, selectValue );
+
+            return firstOrDefault;
+        }
     }
 
     /// <summary>
---
+++ Rock/Lava/Blocks/Execute.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotLiquid;
+using Rock.Data;
+using Rock.Model;
+using CSScriptLibrary;
+
+namespace Rock.Lava.Blocks
+{
+    public class Execute : DotLiquid.Block
+    {
+        private RuntimeType _runtimeType = RuntimeType.SCRIPT;
+        private List<string> _imports = new List<string>();
+
+        public override void Initialize( string tagName, string markup, List<string> tokens )
+        {
+            string[] parms = markup.Split( ' ' );
+
+            foreach ( var parm in parms )
+            {
+                string[] setting = parm.Split( '=' );
+
+                if ( setting.Length == 2 )
+                {
+                    switch ( setting[0] )
+                    {
+                        case "type":
+                            {
+                                string value = CleanInput( setting[1] ).ToUpper();
+                                _runtimeType = value.ConvertToEnum<RuntimeType>( RuntimeType.SCRIPT );
+                                break;
+                            }
+                        case "import":
+                            {
+                                _imports = setting[1].Split( ',' ).ToList();
+                                break;
+                            }
+                    }
+                }
+            }
+
+            base.Initialize( tagName, markup, tokens );
+        }
+
+        public override void Render( Context context, TextWriter result )
+        {
+            string userScript = @"return ""Watson, can you hear me?"";"; // inital script here was just for testing
+
+            using ( TextWriter temp = new StringWriter() )
+            {
+                base.Render( context, temp );
+
+                userScript = temp.ToString();
+
+                if ( _runtimeType == RuntimeType.SCRIPT )
+                {
+                    // add default convience import
+                    _imports.Insert( 0, "Rock.Data" );
+                    _imports.Insert( 0, "Rock.Model" );
+                    _imports.Insert( 0, "Rock" );
+                    _imports.Insert( 0, "System" );
+
+                    // treat this as a script
+                    string imports = string.Empty;
+
+                    // create needed imports
+                    foreach ( string import in _imports )
+                    {
+                        string importStatement = string.Format( "using {0};", CleanInput( import ).Trim() );
+
+                        // ensure the import statement isn't already used
+                        if ( !imports.Contains( importStatement ) )
+                        {
+                            imports = imports + Environment.NewLine + importStatement;
+                        }
+                    }
+
+
+                    // build class for the script
+                    string script = string.Format( @"{0}
+                                     public class Script
+                                     {{
+                                         public string Execute()
+                                         {{
+                                             {1}
+                                         }}
+                                     }}", imports, userScript );
+
+                    dynamic csScript = CSScript.Evaluator.LoadCode<ILavaScript>( script );
+
+                    string scriptResult = csScript.Execute();
+                    result.Write( scriptResult );
+                }
+                else
+                {
+                    // treat this like a class
+                    dynamic csScript = CSScript.Evaluator.LoadCode<ILavaScript>( userScript );
+                    string scriptResult = csScript.Execute();
+                    result.Write( scriptResult );
+                }
+
+
+            }
+        }
+
+        private string CleanInput( string input )
+        {
+            return input.Replace( "\"", "" ).Replace( @"\", "" );
+        }
+    }
+
+    /// <summary>
+    /// Enum for the type of runtime
+    /// </summary>
+    enum RuntimeType
+    {
+        SCRIPT, CLASS
+    }
+
+
+    /// <summary>
+    /// Generic interface for the lava classes to ensure we call Execute and not care about the class name they use
+    /// </summary>
+    public interface ILavaScript
+    {
+        string Execute();
+    }
+}
