bfd7baf4d50d15420c7f0870d7259e75c4a36162
Mike Peterson <MikePeterson@ccvonline.com>
2016-07-21 16:42:18 -0700

    BulkPersonInactivator tool is functional

--- RockWeb/Plugins/church_ccv/Core/BulkPersonInactivator.ascx
+++ RockWeb/Plugins/church_ccv/Core/BulkPersonInactivator.ascx
@@ -1,23 +1,68 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="BulkPersonInactivator.ascx.cs" Inherits="RockWeb.Plugins.church_ccv.Core.BulkPersonInactivator" %>
 
+<script src="/SignalR/hubs"></script>
+<script type="text/javascript">
+    $(function () {
+        var proxy = $.connection.rockMessageHub;
+
+        proxy.client.hideProgressBar = function () {
+            $('.js-notification').fadeOut();
+        }
+
+        proxy.client.receiveNotification = function (percent, resultText) {
+
+            var $progressBar = $('.js-notification').find('.js-progress-bar');
+            $progressBar.show();
+            $('.js-inactivate-btn').hide();
+            $progressBar.attr('aria-valuenow', percent).css('width', percent + '%');
+            $progressBar.find('.sr-only').text(percent + '% complete');
+            $('.js-notification').find('.js-result-text').html(resultText);
+
+            if (percent == 'completed') {
+                $progressBar.hide();
+                $('.js-notification').find('.js-result-text').html(resultText);
+                $('.js-inactivate-btn').show();
+            }
+
+            if (percent == 'exception')
+            {
+                $('.js-notification').find('.js-result-text').html(resultText);
+            }
+        }
+
+        $.connection.hub.start().done(function () { });
+    })
+</script>
+
 <asp:UpdatePanel ID="upnlContent" runat="server">
     <ContentTemplate>
 
-        <Rock:NotificationBox ID="nbConfigurationWarning" runat="server" NotificationBoxType="Warning" Visible="false" Text="A Query or DataView need to specified in block settings." />
-        
         <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
-        
+
             <div class="panel-heading">
                 <h4 class="panel-title pull-left">Bulk Person Inactivator</h4>
             </div>
             <div class="panel-body">
-                <Rock:RockLiteral ID="lDataViewName" runat="server" Label="DataView" />
+                <Rock:DataViewPicker ID="dvpDataview" runat="server" Label="DataView" Help="Choose the Dataview that contains the list of people to deactivate"  Required="true" AutoPostBack="true" OnSelectedIndexChanged="dvpDataview_SelectedIndexChanged" EntityTypeId="15" />
                 <Rock:RockLiteral ID="lRecordCount" runat="server" Label="Record Count" />
 
-                
-                <asp:LinkButton ID="btnInactivateRecords" runat="server" CssClass="btn btn-primary" Text="Inactivate Records" OnClick="btnInactivateRecords_Click" />
+                <Rock:RockLiteral ID="lUpdatedRecordStatus" runat="server" Label="Update Record Status" />
+                <Rock:RockLiteral ID="lRecordStatusReason" runat="server" Label="Update Record Status Reason" />
+                <Rock:RockTextBox ID="tbInactiveReasonNote" runat="server" Label="Inactive Reason Note" Text="Inactive for 8+ months" />
+
+                <div class="js-notification">
+                    <div class="js-result-text"></div>
+                    <div class='progress'>
+                        <div class='progress-bar js-progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width: 0%;'>
+                            <span class='sr-only'>0% Complete</span>
+                        </div>
+                    </div>
+                </div>
+
+                <asp:LinkButton ID="btnInactivateRecords" runat="server" CssClass="btn btn-primary js-inactivate-btn" Text="Inactivate Records" OnClick="btnInactivateRecords_Click" />
+                <asp:LinkButton ID="btnCancel" runat="server" CssClass="btn btn-danger js-cancel-btn" Text="Cancel" OnClick="btnCancel_Click" Visible="false" />
             </div>
-        
+
         </asp:Panel>
 
     </ContentTemplate>
--- RockWeb/Plugins/church_ccv/Core/BulkPersonInactivator.ascx.cs
+++ RockWeb/Plugins/church_ccv/Core/BulkPersonInactivator.ascx.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Microsoft.AspNet.SignalR;
 using Rock;
 using Rock.Data;
 using Rock.Model;
 using Rock.Web.Cache;
-using Rock.Web.UI.Controls;
-using Rock.Attribute;
 using Rock.Web.UI;
 
 namespace RockWeb.Plugins.church_ccv.Core
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Rock.Web.UI.RockBlock" />
     [DisplayName( "Bulk Person Inactivator" )]
     [Category( "CCV > Core" )]
     [Description( "Block for mass updating person records to inactive" )]
 
-    [CodeEditorField( "Query", "The Query that determines the list of people that need to be inactivated. Leave this blank to use the DataView instead.", CodeEditorMode.Sql, order: 10 )]
-    [DataViewField( "DataView", "The DataView that determines the list of people that need to be inactivated.", order: 11 )]
     public partial class BulkPersonInactivator : RockBlock
     {
+        /// <summary>
+        /// This holds the reference to the RockMessageHub SignalR Hub context.
+        /// </summary>
+        private IHubContext _hubContext = GlobalHost.ConnectionManager.GetHubContext<RockMessageHub>();
+
+
         #region Base Control Methods
 
         /// <summary>
@@ -33,6 +38,8 @@ namespace RockWeb.Plugins.church_ccv.Core
         protected override void OnInit( EventArgs e )
         {
             base.OnInit( e );
+
+            RockPage.AddScriptLink( "~/Scripts/jquery.signalR-2.2.0.min.js", fingerprint: false );
 
             // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
             this.BlockUpdated += Block_BlockUpdated;
@@ -66,85 +73,169 @@ namespace RockWeb.Plugins.church_ccv.Core
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void Block_BlockUpdated( object sender, EventArgs e )
         {
-
+            ShowDetail();
         }
 
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Shows the detail.
+        /// </summary>
         protected void ShowDetail()
         {
-            var query = this.GetAttributeValue( "Query" );
-            nbConfigurationWarning.Visible = false;
+            var recordStatusInactive = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_INACTIVE.AsGuid() );
+            var inactiveReasonNoActivity = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_REASON_NO_ACTIVITY.AsGuid() );
+            lUpdatedRecordStatus.Text = recordStatusInactive.Value;
+            lRecordStatusReason.Text = inactiveReasonNoActivity.Value;
+            lRecordCount.Text = "-";
 
-            if ( !string.IsNullOrEmpty( query ) )
+            var dataViewId = dvpDataview.SelectedValue.AsIntegerOrNull();
+            if ( dataViewId.HasValue )
             {
-                lDataViewName.Text = string.Format( "SQL: <pre>{0}</pre>", query.Truncate( 255 ) );
-                var dataTable = DbService.GetDataTable( query, System.Data.CommandType.Text, null );
-                lRecordCount.Text = dataTable.Rows.Count.ToString();
-            }
-            else
-            {
-                var dataViewGuid = this.GetAttributeValue( "DataView" ).AsGuidOrNull();
-                if ( dataViewGuid.HasValue )
+                var rockContext = new RockContext();
+                var dataView = new DataViewService( rockContext ).Get( dataViewId.Value );
+                if ( dataView != null )
                 {
-                    var rockContext = new RockContext();
-                    var dataView = new DataViewService( rockContext ).Get( dataViewGuid.Value );
-                    if ( dataView != null )
-                    {
-                        lDataViewName.Text = dataView.Name;
-                        List<string> errorMessages;
-                        var qry = dataView.GetQuery( null, null, out errorMessages );
-                        lRecordCount.Text = qry.Count().ToString();
-                    }
-                    else
-                    {
-                        nbConfigurationWarning.Visible = true;
-                    }
-                }
-                else
-                {
-                    nbConfigurationWarning.Visible = true;
+                    List<string> errorMessages;
+                    var qry = dataView.GetQuery( null, null, out errorMessages );
+                    lRecordCount.Text = qry.Count().ToString();
                 }
             }
         }
 
+        private static bool _cancel = false;
 
+        /// <summary>
+        /// Handles the Click event of the btnInactivateRecords control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void btnInactivateRecords_Click( object sender, EventArgs e )
         {
             List<int> personIds = null;
-            var query = this.GetAttributeValue( "Query" );
+            string newInactiveReasonNote = tbInactiveReasonNote.Text;
 
-            if ( !string.IsNullOrEmpty( query ) )
+            var dataViewId = dvpDataview.SelectedValue.AsIntegerOrNull();
+            if ( dataViewId.HasValue )
             {
-                var dataTable = DbService.GetDataTable( query, System.Data.CommandType.Text, null );
-                personIds = dataTable.Select().Select( a => (int)a["Id"] ).OrderBy( a => a ).ToList();
-            }
-            else
-            {
-                var dataViewGuid = this.GetAttributeValue( "DataView" ).AsGuidOrNull();
-                if ( dataViewGuid.HasValue )
+                var rockContext = new RockContext();
+                var dataView = new DataViewService( rockContext ).Get( dataViewId.Value );
+                if ( dataView != null )
                 {
-                    var rockContext = new RockContext();
-                    var dataView = new DataViewService( rockContext ).Get( dataViewGuid.Value );
-                    if ( dataView != null )
-                    {
 
-                        List<string> errorMessages;
-                        var qry = dataView.GetQuery( null, null, out errorMessages );
-                        personIds = qry.Select( a => a.Id ).OrderBy( a => a ).ToList();
-                    }
+                    List<string> errorMessages;
+                    var qry = dataView.GetQuery( null, null, out errorMessages );
+                    personIds = qry.Select( a => a.Id ).OrderBy( a => a ).ToList();
                 }
             }
 
+
             if ( personIds != null )
             {
-                // TODO
+                var recordStatusInactive = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_INACTIVE.AsGuid() );
+                var inactiveReasonNoActivity = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_REASON_NO_ACTIVITY.AsGuid() );
+                btnCancel.Visible = true;
+                _cancel = false;
+
+
+                System.Threading.Tasks.Task.Run( () =>
+                {
+                    int currentPersonId = 0;
+                    try
+                    {
+                        int progress = 0;
+                        int countInactivated = 0;
+                        int countAlreadyInactive = 0;
+                        int count = personIds.Count();
+                        int lastPercent = -1;
+                        foreach ( var personId in personIds )
+                        {
+                            currentPersonId = personId;
+                            if ( _cancel )
+                            {
+                                // if the webpage time to load back up on the client
+                                System.Threading.Thread.Sleep( 2000 );
+                                break;
+                            }
+
+                            List<string> changes = new List<string>();
+                            using ( var rockContext = new RockContext() )
+                            {
+                                var personService = new PersonService( rockContext );
+
+                                var person = personService.Get( personId );
+                                History.EvaluateChange( changes, "Record Status", DefinedValueCache.GetName( person.RecordStatusValueId ), recordStatusInactive.Value );
+                                if ( person.RecordStatusValueId != recordStatusInactive.Id )
+                                {
+                                    person.RecordStatusValueId = recordStatusInactive.Id;
+
+                                    History.EvaluateChange( changes, "Inactive Reason", DefinedValueCache.GetName( person.RecordStatusReasonValueId ), inactiveReasonNoActivity.Value );
+                                    person.RecordStatusReasonValueId = inactiveReasonNoActivity.Id;
+
+                                    if ( !string.IsNullOrWhiteSpace( newInactiveReasonNote ) )
+                                    {
+                                        History.EvaluateChange( changes, "Inactive Reason Note", person.InactiveReasonNote, newInactiveReasonNote );
+                                        person.InactiveReasonNote = newInactiveReasonNote;
+                                    }
+
+                                    HistoryService.AddChanges( rockContext, typeof( Person ), Rock.SystemGuid.Category.HISTORY_PERSON_DEMOGRAPHIC_CHANGES.AsGuid(), person.Id, changes );
+                                    countInactivated++;
+
+                                    rockContext.SaveChanges();
+                                }
+                                else
+                                {
+                                    countAlreadyInactive++;
+                                }
+
+                                progress++;
+
+                                int percent = (int)( (double)( progress * 100 ) / count );
+                                if ( lastPercent != percent )
+                                {
+                                    _hubContext.Clients.All.receiveNotification( percent, string.Format( "{0}/{1}", progress, count ) );
+                                }
+
+                                lastPercent = percent;
+                            }
+                        }
+
+                        _hubContext.Clients.All.receiveNotification( _cancel ? "cancelled" : "completed", string.Format(
+                            @"
+<pre>
+{0} records selected: 
+    {1} records inactivated successfully
+    {2} already set to inactive
+</pre>", count, countInactivated, countAlreadyInactive ) );
+                    }
+                    catch ( Exception ex )
+                    {
+                        _hubContext.Clients.All.receiveNotification( "exception", string.Format( "PersonId:{0}, Ex:{1}", currentPersonId, ex.Message ) );
+                    }
+                } );
             }
         }
 
 
         #endregion
+
+        /// <summary>
+        /// Handles the SelectedIndexChanged event of the dvpDataview control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void dvpDataview_SelectedIndexChanged( object sender, EventArgs e )
+        {
+            ShowDetail();
+        }
+
+
+        protected void btnCancel_Click( object sender, EventArgs e )
+        {
+            _cancel = true;
+            btnCancel.Visible = false;
+        }
     }
 }
