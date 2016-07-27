adf3e0283ccb59fff4369b7c7aaa51fc125b8850
David Turner <david@sparkdevnetwork.org>
2016-07-26 19:25:57 -0700

    - Fixed issue with attribute editor not setting key correctly if property was being set in markup (i.e. ShowInGrid)

--- Rock/Web/UI/Controls/AttributeEditor.cs
+++ Rock/Web/UI/Controls/AttributeEditor.cs
@@ -625,8 +625,6 @@ namespace Rock.Web.UI.Controls
                 _btnCancel.Click += btnCancel_Click;
                 Controls.Add( _btnCancel );
 
-                _tbName.Attributes["onblur"] = string.Format( "populateAttributeKey('{0}','{1}')", _tbName.ClientID, _tbKey.ClientID );
-
                 _controlsLoaded = true;
             }
         }
@@ -705,6 +703,8 @@ namespace Rock.Web.UI.Controls
         /// <param name="writer">An <see cref="T:System.Web.UI.HtmlTextWriter" /> that represents the output stream to render HTML content on the client.</param>
         protected override void Render( HtmlTextWriter writer )
         {
+            _tbName.Attributes["onblur"] = string.Format( "populateAttributeKey('{0}','{1}')", _tbName.ClientID, _tbKey.ClientID );
+
             writer.RenderBeginTag( HtmlTextWriterTag.Fieldset );
 
             writer.RenderBeginTag( HtmlTextWriterTag.Legend );
