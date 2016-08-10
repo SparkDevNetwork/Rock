5451bfc4521a7ffb204ad1e467adda6075517d41
David Turner <david@sparkdevnetwork.org>
2016-08-10 12:53:54 -0700

    - Update migration for store category

--- Rock.Migrations/Migrations/201608101948321_StoreCategoryHeader.Designer.cs
+++ Rock.Migrations/Migrations/201608101948321_StoreCategoryHeader.Designer.cs
@@ -7,13 +7,13 @@ namespace Rock.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class addmigrationstorecategoryheader : IMigrationMetadata
+    public sealed partial class StoreCategoryHeader : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(addmigrationstorecategoryheader));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(StoreCategoryHeader));
         
         string IMigrationMetadata.Id
         {
-            get { return "201608091716425_add-migration store-category-header"; }
+            get { return "201608101948321_StoreCategoryHeader"; }
         }
         
         string IMigrationMetadata.Source
