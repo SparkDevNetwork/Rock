49f5e29dafd9117dcd301b7098434d6944489672
benwiley <ben.wiley@newspring.cc>
2016-08-21 15:35:53 -0400

    [ci skip] Backfill script in progress

--- Dev Tools/Sql/NewSpring_22_MetricValueBackfill.sql
+++ Dev Tools/Sql/NewSpring_22_MetricValueBackfill.sql
@@ -4,6 +4,10 @@
 
    ====================================================== */
 
+DECLARE @foreignKey AS NVARCHAR(20) = 'Metrics 2.0 - Backfi';
+DECLARE @metricValueType AS INT = 0;
+DECLARE @CreatedDateTime AS DATETIME = GETDATE();
+
 IF object_id('tempdb..#metricTypes') IS NOT NULL
 BEGIN
 	DROP TABLE #metricTypes;
@@ -12,13 +16,30 @@ END
 CREATE TABLE #metricTypes (
 	Id INT IDENTITY(1,1),
 	CategoryId INT,
-	MetricId INT
+	MetricId INT,
+	MetricTitle NVARCHAR(100)
+);
+
+IF object_id('tempdb..#currentResult') IS NOT NULL
+BEGIN
+	DROP TABLE #currentResult;
+END
+
+CREATE TABLE #currentResult (
+	Value INT,
+	MetricValueDateTime DATETIME,
+	Campus INT,
+	[Group] INT,
+	Schedule INT,
+	DidAttend INT,
+	MetricValueGuid UNIQUEIDENTIFIER DEFAULT NEWID()
 );
 
 INSERT INTO #metricTypes
 SELECT
 	c.Id AS CategoryId,
-	m.Id AS MetricId
+	m.Id AS MetricId,
+	m.Title AS MetricTitle
 FROM
 	Metric m
 	JOIN MetricCategory mc ON mc.MetricId = m.Id
@@ -32,6 +53,7 @@ DECLARE @sundayDate AS DATE = '01-04-2015';
 DECLARE @scopeIndex AS INT = (SELECT MIN(Id) FROM #metricTypes);
 DECLARE @numItems AS INT = (SELECT COUNT(1) + @scopeIndex FROM #metricTypes);
 DECLARE @metricId AS INT;
+DECLARE @metricName AS NVARCHAR(100);
 DECLARE @groupType AS INT;
 DECLARE @sourceSql AS NVARCHAR(MAX);
 DECLARE @startPattern AS NVARCHAR(10);
@@ -43,15 +65,16 @@ DECLARE @msg AS NVARCHAR(MAX);
 
 WHILE @sundayDate < GETDATE()
 BEGIN
-	SET @scopeIndex = (SELECT MIN(Id) FROM #metricTypes);
+	SET @scopeIndex = (SELECT Id FROM #metricTypes WHERE MetricId = 2936)--(SELECT MIN(Id) FROM #metricTypes);
 
 	WHILE @scopeIndex < @numItems
 	BEGIN
 		SET @metricId = (SELECT MetricId FROM #metricTypes WHERE Id = @scopeIndex);
+		SET @metricName = (SELECT MetricTitle FROM #metricTypes WHERE Id = @scopeIndex);
 		SET @sourceSql = (SELECT SourceSql FROM Metric WHERE Id = @metricId);
 		SET @groupType = (SELECT EntityTypeQualifierValue FROM MetricPartition WHERE MetricId = @metricId AND Label = 'Group');
 
-		SET @msg = 'Creating metric values for metric id ' + CONVERT(NVARCHAR(20), @metricId) + ' / ' + CONVERT(NVARCHAR(20), @sundayDate); 
+		SET @msg = 'Creating metric values for ' + @metricName + ' (Id: ' + CONVERT(NVARCHAR(20), @metricId) + ') / ' + CONVERT(NVARCHAR(20), @sundayDate); 
 		RAISERROR ( @msg, 0, 0 ) WITH NOWAIT;
 		
 		-- Replace grouptype lava
@@ -66,16 +89,117 @@ BEGIN
 		SET @sourceSql = REPLACE(@sourceSql, 'DECLARE @RecentSundayDate AS DATE = CONVERT(DATE, DATEADD(DAY, 1 - DATEPART(DW, @Today), @Today));', '');
 		SET @sourceSql = REPLACE(@sourceSql, 'StartDateTime >= @RecentSundayDate', CONCAT('StartDateTime >= ''', @sundayDate, ''''));
 		SET @sourceSql = REPLACE(@sourceSql, 'AND StartDateTime < DATEADD(day, 1, @Today)', CONCAT('AND StartDateTime < DATEADD(WEEK, 1, ''', @sundayDate, ''')'));
+		SET @sourceSql = REPLACE(@sourceSql, 'StartDateTime >= @RecentSundayDate', CONCAT('StartDateTime >= ''', @sundayDate, ''''));
+		SET @sourceSql = REPLACE(@sourceSql, 'AND StartDateTime < DATEADD(day, 1, @Today)', CONCAT('AND StartDateTime < DATEADD(WEEK, 1, ''', @sundayDate, ''')'));
 		
+		TRUNCATE TABLE #currentResult;
+		--INSERT INTO  #currentResult (Value, MetricValueDateTime, Campus, [Group], Schedule, DidAttend)
 		EXEC(@sourceSql);
-		RETURN;
 
-		/* Exec sourceSql */
+		INSERT INTO MetricValue(
+			MetricValueType,
+			YValue,
+			MetricId,
+			MetricValueDateTime,
+			CreatedDateTime,
+			[Guid],
+			ForeignKey
+		)
+		SELECT 
+			@metricValueType,
+			cr.Value,
+			@metricId,
+			cr.MetricValueDateTime,
+			@CreatedDateTime,
+			cr.MetricValueGuid,
+			@foreignKey
+		FROM 
+			#currentResult cr;
 
-		/* Store result in metricValues */
+		-- Group
+		INSERT INTO MetricValuePartition(
+			MetricPartitionId,
+			MetricValueId,
+			EntityId,
+			CreatedDateTime,
+			[Guid],
+			ForeignKey
+		)
+		SELECT 
+			(SELECT mp.Id FROM MetricPartition mp WHERE mp.MetricId = @metricId AND mp.Label = 'Group') AS MetricPartitionId,
+			(SELECT Id FROM MetricValue WHERE [Guid] = cr.MetricValueGuid) AS MetricValueId,
+			cr.[Group] AS EntityId,
+			@CreatedDateTime AS CreatedDateTime,
+			NEWID() AS [Guid],
+			@foreignKey AS ForeignKey
+		FROM 
+			#currentResult cr;
+
+		-- Schedule
+		INSERT INTO MetricValuePartition(
+			MetricPartitionId,
+			MetricValueId,
+			EntityId,
+			CreatedDateTime,
+			[Guid],
+			ForeignKey
+		)
+		SELECT 
+			(SELECT mp.Id FROM MetricPartition mp WHERE mp.MetricId = @metricId AND mp.Label = 'Schedule') AS MetricPartitionId,
+			(SELECT Id FROM MetricValue WHERE [Guid] = cr.MetricValueGuid) AS MetricValueId,
+			cr.Schedule AS EntityId,
+			@CreatedDateTime AS CreatedDateTime,
+			NEWID() AS [Guid],
+			@foreignKey AS ForeignKey
+		FROM 
+			#currentResult cr;
+
+		-- Campus
+		INSERT INTO MetricValuePartition(
+			MetricPartitionId,
+			MetricValueId,
+			EntityId,
+			CreatedDateTime,
+			[Guid],
+			ForeignKey
+		)
+		SELECT 
+			(SELECT mp.Id FROM MetricPartition mp WHERE mp.MetricId = @metricId AND mp.Label = 'Campus') AS MetricPartitionId,
+			(SELECT Id FROM MetricValue WHERE [Guid] = cr.MetricValueGuid) AS MetricValueId,
+			cr.Campus AS EntityId,
+			@CreatedDateTime AS CreatedDateTime,
+			NEWID() AS [Guid],
+			@foreignKey AS ForeignKey
+		FROM 
+			#currentResult cr;
+		
+		-- Did Attend
+		INSERT INTO MetricValuePartition(
+			MetricPartitionId,
+			MetricValueId,
+			EntityId,
+			CreatedDateTime,
+			[Guid],
+			ForeignKey
+		)
+		SELECT 
+			(SELECT mp.Id FROM MetricPartition mp WHERE mp.MetricId = @metricId AND mp.Label = 'Did Attend') AS MetricPartitionId,
+			(SELECT Id FROM MetricValue WHERE [Guid] = cr.MetricValueGuid) AS MetricValueId,
+			cr.DidAttend AS EntityId,
+			@CreatedDateTime AS CreatedDateTime,
+			NEWID() AS [Guid],
+			@foreignKey AS ForeignKey
+		FROM 
+			#currentResult cr;
+		return;
 
 		SET @scopeIndex = @scopeIndex + 1;
 	END
 
 	SET @sundayDate = DATEADD(WEEK, 1, @sundayDate);
 END
+
+/*
+DELETE FROM MetricValuePartition WHERE MetricValueId IN (SELECT Id FROM MetricValue WHERE ForeignKey = 'Metrics 2.0 - Backfi');
+DELETE FROM MetricValue WHERE ForeignKey = 'Metrics 2.0 - Backfi';
+*/
