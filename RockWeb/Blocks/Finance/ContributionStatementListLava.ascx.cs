57c612f9688c207553ce26cbed2ec723f7e15416
Tyler Schrock <tschrock123@gmail.com>
2016-08-30 14:58:58 -0400

    - Fix additional merge fields missed in ad47233

--- RockWeb/Blocks/Finance/ContributionStatementListLava.ascx.cs
+++ RockWeb/Blocks/Finance/ContributionStatementListLava.ascx.cs
@@ -134,7 +134,7 @@ namespace RockWeb.Blocks.Finance
                                 .OrderByDescending(y => y.Year);
 
             var mergeFields = new Dictionary<string, object>();
-            mergeFields.Add( "DetailPage", LinkedPageUrl( "DetailPage", null ) );
+            mergeFields.Add( "DetailPage", LinkedPageRoute( "DetailPage" ) );
             mergeFields.Add( "StatementYears", yearQry.Take( numberOfYears ) );
             
             var template = GetAttributeValue( "LavaTemplate" );
