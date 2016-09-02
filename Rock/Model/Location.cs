a27aad15312f604f9d1dd4923906a6f7b9e4c9b4
Garrett Johnson <nvisiondesign@gmail.com>
2016-09-02 16:52:12 -0700

    Make title string optional

--- Rock/Model/Location.cs
+++ Rock/Model/Location.cs
@@ -613,7 +613,7 @@ namespace Rock.Model
         /// </summary>
         /// <param name="title">A unused <see cref="System.String"/> containing the location name parameters.</param>
         /// <returns>A <see cref="System.String"/> containing the link to Google Maps for this location.</returns>
-        public virtual string GoogleMapLink( string title )
+        public virtual string GoogleMapLink( string title = "" )
         {
             string qParm = this.GetFullStreetAddress();
             // Remove linebreaks and commas
