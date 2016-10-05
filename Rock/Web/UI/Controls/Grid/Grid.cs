7f3bed029538e1d4f2c4ea58f0d09b9bd51cd298
cbump <cbump@secc.org>
2016-10-05 11:15:25 -0400

    #1791 - Updating the Excel export and PersonData methods to honor the RowFilter.

--- Rock/Web/UI/Controls/Grid/Grid.cs
+++ Rock/Web/UI/Controls/Grid/Grid.cs
@@ -1563,11 +1563,11 @@ namespace Rock.Web.UI.Controls
                     }
 
                     // print data
-                    foreach ( DataRow row in data.Rows )
+                    foreach ( DataRowView rowView in data.DefaultView )
                     {
                         for ( int i = 0; i < data.Columns.Count; i++ )
                         {
-                            SetExcelValue( worksheet.Cells[rowCounter, i + 1], row[i] );
+                            SetExcelValue( worksheet.Cells[rowCounter, i + 1], rowView.Row[i] );
                         }
 
                         rowCounter++;
@@ -2183,8 +2183,9 @@ namespace Rock.Web.UI.Controls
                 {
                     DataTable data = this.DataSourceAsDataTable;
 
-                    foreach ( DataRow row in data.Rows )
+                    foreach ( DataRowView rowView in data.DefaultView )
                     {
+                        DataRow row = rowView.Row;
                         object dataKey = row[dataKeyColumn];
                         if ( !keysSelected.Any() || keysSelected.Contains( dataKey ) )
                         {
