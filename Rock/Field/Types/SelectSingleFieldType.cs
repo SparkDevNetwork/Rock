1e2b8935d45c8a789d08d673263e5e572dc52d75
Jorge Recio <jrecio@willowcreek.org>
2016-10-05 11:16:10 -0500

    Added missing lava tip.

--- Rock/Field/Types/SelectSingleFieldType.cs
+++ Rock/Field/Types/SelectSingleFieldType.cs
@@ -64,7 +64,7 @@ namespace Rock.Field.Types
             tb.AutoPostBack = true;
             tb.TextChanged += OnQualifierUpdated;
             tb.Label = "Values";
-            tb.Help = "The source of the values to display in a list.  Format is either 'value1,value2,value3,...', 'value1^text1,value2^text2,value3^text3,...', or a SQL Select statement that returns result set with a 'Value' and 'Text' column.";
+            tb.Help = "The source of the values to display in a list.  Format is either 'value1,value2,value3,...', 'value1^text1,value2^text2,value3^text3,...', or a SQL Select statement that returns result set with a 'Value' and 'Text' column <span class='tip tip-lava'></span>.";
 
             var ddl = new RockDropDownList();
             controls.Add( ddl );
