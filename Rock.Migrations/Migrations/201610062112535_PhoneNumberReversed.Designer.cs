473cacd7834bc40b0550f7e1ba90d3dc90d83b5a
David Turner <david@sparkdevnetwork.org>
2016-10-06 20:09:44 -0400

    - Redo Migration

--- Rock.Migrations/Migrations/201610062112535_PhoneNumberReversed.Designer.cs
+++ Rock.Migrations/Migrations/201610062112535_PhoneNumberReversed.Designer.cs
@@ -7,13 +7,13 @@ namespace Rock.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class BodyPageCSSContentChannelNoContent : IMigrationMetadata
+    public sealed partial class PhoneNumberReversed : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(BodyPageCSSContentChannelNoContent));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(PhoneNumberReversed));
         
         string IMigrationMetadata.Id
         {
-            get { return "201609221633157_BodyPageCSS-ContentChannelNoContent"; }
+            get { return "201610062112535_PhoneNumberReversed"; }
         }
         
         string IMigrationMetadata.Source
