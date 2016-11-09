59db343244f05585f69ca9a6b70b3dea68720c8d
Mike Peterson <MikePeterson@ccvonline.com>
2016-11-09 14:01:24 -0700

    - more progress on ProcessAnalyticsDimPerson to create the populate SQL including AttributeValues fields

---
+++ Rock/Jobs/ProcessAnalyticsFactFinancialTransaction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Rock.Data;
+using Rock.Reporting;
+using Rock.Web.Cache;
+
+namespace Rock.Jobs
+{
+    /// <summary>
+    /// Job to take care of doing the ETL for the analytic tables that support AnalyticsFactFinancialTransaction
+    /// </summary>
+    /// <seealso cref="Quartz.IJob" />
+    [DisallowConcurrentExecution]
+    public class ProcessAnalyticsFactFinancialTransaction : IJob
+    {
+        /// <summary> 
+        /// Empty constructor for job initialization
+        /// <para>
+        /// Jobs require a public empty constructor so that the
+        /// scheduler can instantiate the class whenever it needs.
+        /// </para>
+        /// </summary>
+        public ProcessAnalyticsFactFinancialTransaction()
+        {
+        }
+
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public void Execute( IJobExecutionContext context )
+        {
+            // TODO: We might not need this, and could simply have a migration to have a Rock.Jobs.RunSQL job execute [spAnalytics_ETL_FinancialTransaction]
+
+        }
+    }
+}
