d8b720390b88210c3868b1ef7cac457c9264bb59
Mike Peterson <MikePeterson@ccvonline.com>
2016-11-08 17:34:21 -0700

    - started on AnalyticsDimPersonHistorical Models, Job, and ETL

---
+++ Rock/Jobs/ProcessAnalyticsDimPerson.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Rock.Data;
+using Rock.Reporting;
+using Rock.Web.Cache;
+
+namespace Rock.Jobs
+{
+    /// <summary>
+    /// Job to take care of schema changes ( dynamic Attribute Value Fields ) and data updates to AnalyticsDimPerson
+    /// </summary>
+    /// <seealso cref="Quartz.IJob" />
+    [DisallowConcurrentExecution]
+    public class ProcessAnalyticsDimPerson : IJob
+    {
+        /// <summary> 
+        /// Empty constructor for job initialization
+        /// <para>
+        /// Jobs require a public empty constructor so that the
+        /// scheduler can instantiate the class whenever it needs.
+        /// </para>
+        /// </summary>
+        public ProcessAnalyticsDimPerson()
+        {
+        }
+
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public void Execute( IJobExecutionContext context )
+        {
+            var dataSet = DbService.GetDataSetSchema( "SELECT * FROM [AnalyticsDimPersonHistorical] where 1=0", System.Data.CommandType.Text, null );
+            var dataTable = dataSet.Tables[0];
+            var currentDatabaseAttributeFields = dataTable.Columns.OfType<DataColumn>().Where( a => a.ColumnName.StartsWith( "attribute_" ) );
+
+            var personAttributes = EntityHelper.GetEntityFields( typeof( Rock.Model.Person ) )
+                .Where( a => a.FieldKind == FieldKind.Attribute && a.AttributeGuid.HasValue )
+                .Select( a => AttributeCache.Read( a.AttributeGuid.Value ) )
+                .Where( a => a != null )
+                .ToList();
+
+            const string dateTimeSqlFieldType = "datetime";
+            const string numericSqlFieldType = "[decimal](29,4)";
+            const string defaultSqlFieldType = "nvarchar(max)";
+
+            using ( var rockContext = new RockContext() )
+            {
+                // add any AttributeFields that aren't already fields on AnalyticsDimPersonHistorical
+                // TODO: Limit to only personAttributes where 'IsAnalytic'
+                foreach ( var personAttribute in personAttributes )
+                {
+                    var columnName = string.Format( "attribute_{0}", personAttribute.Key.RemoveSpecialCharacters() );
+                    var databaseColumn = currentDatabaseAttributeFields.Where( a => a.ColumnName == columnName ).FirstOrDefault();
+                    var personAttributeFieldName = personAttribute.FieldType.Field.AttributeValueFieldName;
+                    string sqlFieldType;
+                    if ( personAttributeFieldName == "ValueAsDateTime" )
+                    {
+                        sqlFieldType = dateTimeSqlFieldType;
+                    }
+                    else if ( personAttributeFieldName == "ValueAsNumeric" )
+                    {
+                        sqlFieldType = numericSqlFieldType;
+                    }
+                    else
+                    {
+                        sqlFieldType = defaultSqlFieldType;
+                    }
+
+                    var addColumnSQL = string.Format(
+                            "ALTER TABLE [AnalyticsDimPersonHistorical] ADD [{0}] {1} null",
+                            columnName,
+                            sqlFieldType );
+
+                    var dropColumnSql = string.Format(
+                            "ALTER TABLE [AnalyticsDimPersonHistorical] DROP COLUMN [{0}]",
+                            columnName );
+
+                    if ( databaseColumn == null )
+                    {
+                        // doesn't exist as a field on the AnalyticsDimPersonHistorical table, so create it
+                        rockContext.Database.ExecuteSqlCommand( addColumnSQL );
+                    }
+                    else
+                    {
+                        // it does exist as a field on the AnalyticsDimPersonHistorical table, but make sure the datatype is correct
+                        bool dropCreate = false;
+
+                        if ( databaseColumn.DataType == typeof( int ) )
+                        {
+                            dropCreate = sqlFieldType != numericSqlFieldType;
+                        }
+                        else if ( databaseColumn.DataType == typeof( DateTime ) )
+                        {
+                            dropCreate = sqlFieldType != dateTimeSqlFieldType;
+                        }
+                        else
+                        {
+                            dropCreate = sqlFieldType != defaultSqlFieldType;
+                        }
+
+                        if ( dropCreate || true)
+                        {
+                            // the attribute fieldtype must have changed. Drop and recreate the column
+                            rockContext.Database.ExecuteSqlCommand( dropColumnSql );
+                            rockContext.Database.ExecuteSqlCommand( addColumnSQL );
+                        }
+                    }
+                }
+
+                // remove any AnalyticsDimPersonHistorical attribute fields that aren't attributes on Person
+                // TODO: Limit to only personAttributes where 'IsAnalytic'
+                var personAttributeColumnNames = personAttributes.Select( a => string.Format( "attribute_{0}", a.Key.RemoveSpecialCharacters() ) ).ToList();
+                foreach ( var databaseAttributeField in currentDatabaseAttributeFields )
+                {
+                    if ( !personAttributeColumnNames.Contains( databaseAttributeField.ColumnName ) )
+                    {
+                        var dropColumnSql = string.Format(
+                            "ALTER TABLE [AnalyticsDimPersonHistorical] DROP COLUMN [{0}]",
+                            databaseAttributeField.ColumnName );
+
+                        rockContext.Database.ExecuteSqlCommand( dropColumnSql );
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Rock/Model/AnalyticsDimPersonCurrent.cs
@@ -0,0 +1,133 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Model
+{
+    /// <summary>
+    /// Represents the current record from AnalyticsDimPersonHistorical
+    /// NOTE: AnalyticsDimPersonCurrent is simply a de-normalized sql VIEW based on AnalyticsDimPersonHistorical
+    /// </summary>
+    [Table( "AnalyticsDimPersonCurrent" )]
+    [DataContract]
+    public class AnalyticsDimPersonCurrent : AnalyticsDimPersonBase<AnalyticsDimPersonCurrent>
+    {
+        #region Denormalized Lookup Values
+
+        /// <summary>
+        /// Gets or sets the marital status.
+        /// </summary>
+        /// <value>
+        /// The marital status.
+        /// </value>
+        [DataMember]
+        public string MaritalStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the connection status.
+        /// </summary>
+        /// <value>
+        /// The connection status.
+        /// </value>
+        [DataMember]
+        public string ConnectionStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the review reason.
+        /// </summary>
+        /// <value>
+        /// The review reason.
+        /// </value>
+        [DataMember]
+        public string ReviewReason { get; set; }
+
+        /// <summary>
+        /// Gets or sets the record status.
+        /// </summary>
+        /// <value>
+        /// The record status.
+        /// </value>
+        [DataMember]
+        public string RecordStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets the record status reason.
+        /// </summary>
+        /// <value>
+        /// The record status reason.
+        /// </value>
+        [DataMember]
+        public string RecordStatusReason { get; set; }
+
+        /// <summary>
+        /// Gets or sets the record type value.
+        /// </summary>
+        /// <value>
+        /// The record type value.
+        /// </value>
+        [DataMember]
+        public string RecordType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the suffix value.
+        /// </summary>
+        /// <value>
+        /// The suffix value.
+        /// </value>
+        [DataMember]
+        public string Suffix { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title value.
+        /// </summary>
+        /// <value>
+        /// The title value.
+        /// </value>
+        [DataMember]
+        public string Title { get; set; }
+
+        #endregion
+
+        #region Enums as Text
+
+        /// <summary>
+        /// Gets or sets the gender text.
+        /// </summary>
+        /// <value>
+        /// The gender text.
+        /// </value>
+        [DataMember]
+        public string GenderText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email preference text.
+        /// </summary>
+        /// <value>
+        /// The email preference text.
+        /// </value>
+        [DataMember]
+        public string EmailPreferenceText { get; set; }
+
+        #endregion
+    }
+}
