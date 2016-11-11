7a4d3b8d1613e5a66ff2c35abf8566b5b2146a56
Jon Edmiston <jon@sparkdevnetwork.org>
2016-11-11 15:48:58 -0700

    - added new string extension .IsNotNullOrWhitespace() to replace !string.IsNullOrWhitespace("")

--- Rock/Utility/ExtensionMethods/StringExtensions.cs
+++ Rock/Utility/ExtensionMethods/StringExtensions.cs
@@ -49,6 +49,16 @@ namespace Rock
         }
 
         /// <summary>
+        /// Determines whether the string is not null or whitespace.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns></returns>
+        public static bool IsNotNullOrWhitespace( this string str )
+        {
+            return !string.IsNullOrWhiteSpace( str );
+        }
+
+        /// <summary>
         /// Determines whether the string is made up of only digits
         /// </summary>
         /// <param name="str">The string.</param>
