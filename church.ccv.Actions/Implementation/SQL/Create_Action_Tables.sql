16c18c3e437961a83ef10d6ec9cbfd322b3a13f5
Jered McFerron <jeredmcferron@ccv.church>
2016-11-16 10:57:38 -0700

    Added Starting Point as an Action Updated all Action columns in the Adult / Student tables (not Person tables) to be NOT NULL

--- church.ccv.Actions/Implementation/SQL/Create_Action_Tables.sql
+++ church.ccv.Actions/Implementation/SQL/Create_Action_Tables.sql
@@ -5,30 +5,31 @@
 CREATE TABLE [dbo]._church_ccv_Actions_History_Adult( 
  [Id] int IDENTITY(1,1) NOT NULL
 ,[Date] date
-,[CampusId] int
-,[RegionId] int
-,[TotalPeople] int
-,[Baptised] int 
-,[ERA] int
-,[Give] int
-,[Member] int
-,[Serving] int
-,[PeerLearning] int
-,[Mentored] int
-,[Teaching] int
-,[PeerLearning_NH] int
-,[PeerLearning_YA] int
-,[Mentored_NH] int
-,[Mentored_YA] int
-,[Mentored_NS] int
-,[Teaching_NH_SubSection] int
-,[Teaching_NH] int
-,[Teaching_YA_Section] int
-,[Teaching_YA] int
-,[Teaching_NS_SubSection] int 
-,[Teaching_NS] int
-,[Teaching_NG_Section] int
-,[Teaching_NG] int
+,[CampusId] int NOT NULL
+,[RegionId] int NOT NULL
+,[TotalPeople] int NOT NULL
+,[Baptised] int NOT NULL
+,[ERA] int NOT NULL
+,[Give] int NOT NULL
+,[Member] int NOT NULL
+,[StartingPoint] int NOT NULL
+,[Serving] int NOT NULL
+,[PeerLearning] int NOT NULL
+,[Mentored] int NOT NULL
+,[Teaching] int NOT NULL
+,[PeerLearning_NH] int NOT NULL
+,[PeerLearning_YA] int NOT NULL
+,[Mentored_NH] int NOT NULL
+,[Mentored_YA] int NOT NULL
+,[Mentored_NS] int NOT NULL
+,[Teaching_NH_SubSection] int NOT NULL
+,[Teaching_NH] int NOT NULL
+,[Teaching_YA_Section] int NOT NULL
+,[Teaching_YA] int NOT NULL
+,[Teaching_NS_SubSection] int NOT NULL
+,[Teaching_NS] int NOT NULL
+,[Teaching_NG_Section] int NOT NULL
+,[Teaching_NG] int NOT NULL
 ,CreatedDateTime datetime
 ,ModifiedDateTime datetime
 ,CreatedByPersonAliasId datetime
@@ -52,6 +53,7 @@ CREATE TABLE [dbo]._church_ccv_Actions_History_Adult_Person(
   ERA bit NOT NULL, 
   Give bit NOT NULL, 
   Member date, 
+  StartingPoint date,
   Serving bit NOT NULL, 
   PeerLearning bit NOT NULL, 
   Mentored bit NOT NULL, 
@@ -91,20 +93,21 @@ CREATE TABLE [dbo]._church_ccv_Actions_History_Adult_Person(
 CREATE TABLE [dbo]._church_ccv_Actions_History_Student( 
  [Id] int IDENTITY(1,1) NOT NULL
 ,[Date] date
-,[CampusId] int
-,[RegionId] int
-,[TotalPeople] int
-,[Baptised] int 
-,[ERA] int
-,[Give] int
-,[Member] int
-,[Serving] int
-,[PeerLearning] int
-,[Mentored] int
-,[Teaching] int
-,[PeerLearning_NG] int
-,[Mentored_NG] int
-,[Teaching_Undefined] int
+,[CampusId] int NOT NULL
+,[RegionId] int NOT NULL
+,[TotalPeople] int NOT NULL
+,[Baptised] int  NOT NULL
+,[ERA] int NOT NULL
+,[Give] int NOT NULL
+,[Member] int NOT NULL
+,[StartingPoint] int NOT NULL
+,[Serving] int NOT NULL
+,[PeerLearning] int NOT NULL
+,[Mentored] int NOT NULL
+,[Teaching] int NOT NULL
+,[PeerLearning_NG] int NOT NULL
+,[Mentored_NG] int NOT NULL
+,[Teaching_Undefined] int NOT NULL
 ,CreatedDateTime datetime
 ,ModifiedDateTime datetime
 ,CreatedByPersonAliasId datetime
@@ -129,6 +132,7 @@ CREATE TABLE [dbo]._church_ccv_Actions_History_Student(
   ERA bit NOT NULL, 
   Give bit NOT NULL, 
   Member date, 
+  StartingPoint date,
   Serving bit NOT NULL, 
   PeerLearning bit NOT NULL, 
   Mentored bit NOT NULL, 
