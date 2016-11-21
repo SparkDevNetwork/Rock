24a21e5f41ea1dabd3782803c9e49ae89f252ff1
Chris McGrath <chrismcgrath929@gmail.com>
2016-11-21 09:20:52 -0600

    Updated formatting of Get Spouse SQL function to match standards

--- database/Functions/ufnCrm_GetSpousePersonIdFromPersonId.sql
+++ database/Functions/ufnCrm_GetSpousePersonIdFromPersonId.sql
@@ -1,11 +1,11 @@
 /*
 <doc>
 	<summary>
- 		This function returns the most likely spouse for the person id provided
+ 		This function returns the most likely spouse for the person [Id] provided
 	</summary>
 
 	<returns>
-		Person Id of the most likely spouse. 
+		Person [Id] of the most likely spouse. 
 	</returns>
 	<remarks>
 		
@@ -18,30 +18,37 @@
 </doc>
 */
 
-ALTER FUNCTION [dbo].[ufnCrm_GetSpousePersonIdFromPersonId]( @PersonId int ) 
-
-RETURNS int AS
+ALTER FUNCTION [dbo].[ufnCrm_GetSpousePersonIdFromPersonId] ( 
+	@PersonId INT 
+) 
+RETURNS INT 
+AS
 BEGIN
 	
-	RETURN (SELECT TOP 1 S.ID
-		from [Group] F
-		join GroupType GT on F.GroupTypeId = GT.ID
-		join GroupMember FM on FM.GroupId = F.ID
-		join Person P on P.ID = FM.PersonId
-		join GroupTypeRole R on R.ID = FM.GroupRoleId
-		join GroupMember FM2 on FM2.GroupID = F.ID
-		join Person S on S.ID = FM2.PersonID
-		join GroupTypeRole R2 on R2.ID = FM2.GroupRoleId
-		where GT.Guid = '790E3215-3B10-442B-AF69-616C0DCB998E' -- Family
-		and P.Id = @PersonID
-		and R.Guid = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42' -- Person must be an Adult
-		and R2.Guid = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42' -- Potential spouse must be an Adult
-		and P.MaritalStatusValueId = 143 -- Person must be Married
-		and S.MaritalStatusValueId = 143 -- Potential spouse must be Married
-		and FM.PersonID != FM2.PersonID -- Cannot be married to yourself
-		and (P.Gender != S.Gender OR P.Gender = 0 OR S.Gender = 0) -- Genders cannot match if both are known
-		order by ABS(DATEDIFF(day, ISNULL(P.BirthDate, '1/1/0001'), ISNULL(S.BirthDate, '1/1/0001'))) -- If multiple results, choose nearest in age
-			, S.Id -- Sort by ID so that the same result is always returned
+	RETURN (
+		SELECT 
+			TOP 1 S.[Id]
+		FROM 
+			[Group] F
+			INNER JOIN [GroupType] GT ON F.[GroupTypeId] = GT.[Id]
+			INNER JOIN [GroupMember] FM ON FM.[GroupId] = F.[Id]
+			INNER JOIN [Person] P ON P.[Id] = FM.[PersonId]
+			INNER JOIN [GroupTypeRole] R ON R.[Id] = FM.[GroupRoleId]
+			INNER JOIN [GroupMember] FM2 ON FM2.[GroupID] = F.[Id]
+			INNER JOIN [Person] S ON S.[Id] = FM2.[PersonId]
+			INNER JOIN [GroupTypeRole] R2 ON R2.[Id] = FM2.[GroupRoleId]
+		WHERE 
+			GT.[Guid] = '790E3215-3B10-442B-AF69-616C0DCB998E' -- Family
+			AND P.[Id] = @PersonID
+			AND R.[Guid] = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42' -- Person must be an Adult
+			AND R2.[Guid] = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42' -- Potential spouse must be an Adult
+			AND P.[MaritalStatusValueId] = 143 -- Person must be Married
+			AND S.[MaritalStatusValueId] = 143 -- Potential spouse must be Married
+			AND FM.[PersonId] != FM2.[PersonId] -- Cannot be married to yourself
+			AND (P.[Gender] != S.[Gender] OR P.[Gender] = 0 OR S.[Gender] = 0) -- Genders cannot match if both are known
+		ORDER BY 
+			ABS(DATEDIFF(DAY, ISNULL(P.[BirthDate], '1/1/0001'), ISNULL(S.[BirthDate], '1/1/0001'))) -- If multiple results, choose nearest in age
+			, S.[Id] -- Sort by Id so that the same result is always returned
 	)
 
 END
