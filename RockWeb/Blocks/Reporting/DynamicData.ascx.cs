169db2e4489c6315dce2c47ec18a22c9069247ae
Mike Peterson <MikePeterson@ccvonline.com>
2016-11-29 16:32:38 -0700

    Cherry Pick from core: - Updated DynamicData so it doesn't have to load the query when showing the settings (#1795)
    - Added a GetDataSetSchema static method to DbService for cases when you need to know which columns the query would return but don't need the data

--- RockWeb/Blocks/Reporting/DynamicData.ascx.cs
+++ RockWeb/Blocks/Reporting/DynamicData.ascx.cs
@@ -309,8 +309,9 @@ namespace RockWeb.Blocks.Reporting
         /// Gets the data.
         /// </summary>
         /// <param name="errorMessage">The error message.</param>
+        /// <param name="schemaOnly">if set to <c>true</c> [schema only].</param>
         /// <returns></returns>
-        private DataSet GetData( out string errorMessage )
+        private DataSet GetData( out string errorMessage, bool schemaOnly = false  )
         {
             errorMessage = string.Empty;
 
@@ -332,7 +333,14 @@ namespace RockWeb.Blocks.Reporting
                     var parameters = GetParameters();
                     int timeout = GetAttributeValue( "Timeout" ).AsInteger();
 
-                    return DbService.GetDataSet( query, GetAttributeValue( "StoredProcedure" ).AsBoolean( false ) ? CommandType.StoredProcedure : CommandType.Text, parameters, timeout );
+                    if ( schemaOnly )
+                    {
+                        return DbService.GetDataSetSchema( query, GetAttributeValue( "StoredProcedure" ).AsBoolean( false ) ? CommandType.StoredProcedure : CommandType.Text, parameters, timeout );
+                    }
+                    else
+                    {
+                        return DbService.GetDataSet( query, GetAttributeValue( "StoredProcedure" ).AsBoolean( false ) ? CommandType.StoredProcedure : CommandType.Text, parameters, timeout);
+                    }
                 }
                 catch ( System.Exception ex )
                 {
@@ -410,7 +418,9 @@ namespace RockWeb.Blocks.Reporting
         {
             var showGridFilterControls = GetAttributeValue( "ShowGridFilter" ).AsBoolean();
             string errorMessage = string.Empty;
-            var dataSet = GetData( out errorMessage );
+
+            // get just the schema of the data until we actually need the data
+            var dataSetSchema = GetData( out errorMessage, true );
 
             if ( !string.IsNullOrWhiteSpace( errorMessage ) )
             {
@@ -425,7 +435,7 @@ namespace RockWeb.Blocks.Reporting
 
                 var mergeFields = GetDynamicDataMergeFields();
 
-                if ( dataSet != null )
+                if ( dataSetSchema != null )
                 {
                     string formattedOutput = GetAttributeValue( "FormattedOutput" );
 
@@ -433,6 +443,9 @@ namespace RockWeb.Blocks.Reporting
                     if ( !string.IsNullOrWhiteSpace( GetAttributeValue( "PageTitleLava" ) ) || !string.IsNullOrWhiteSpace( formattedOutput ) )
                     {
                         int i = 1;
+                        
+                        // Formatted output needs all the rows, so get the data regardless of the setData parameter
+                        var dataSet = GetData( out errorMessage);
                         foreach ( DataTable dataTable in dataSet.Tables )
                         {
                             var dropRows = new List<DataRowDrop>();
@@ -470,6 +483,16 @@ namespace RockWeb.Blocks.Reporting
                         bool personReport = GetAttributeValue( "PersonReport" ).AsBoolean();
 
                         int tableId = 0;
+                        DataSet dataSet;
+                        if ( setData == false )
+                        {
+                            dataSet = dataSetSchema;
+                        }
+                        else
+                        {
+                            dataSet = GetData( out errorMessage );
+                        }
+                         
                         foreach ( DataTable dataTable in dataSet.Tables )
                         {
                             var div = new HtmlGenericControl( "div" );
