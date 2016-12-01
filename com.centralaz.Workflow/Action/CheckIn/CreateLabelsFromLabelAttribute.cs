020a1a83f117e32c42c3e6ae8acd7d076d2e10dd
tcavaletto <taylor.cavaletto@centralaz.com>
2016-12-01 12:21:17 -0700

    - Added two new Workflow actions that add the ability to limit label printing to one per day per group.

---
+++ com.centralaz.Workflow/Action/CheckIn/CreateLabelsFromLabelAttribute.cs
@@ -0,0 +1,242 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.CheckIn;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+using Rock;
+using Rock.Workflow;
+using Rock.Workflow.Action.CheckIn;
+
+namespace com.centralaz.Workflow.Action.CheckIn
+{
+    /// <summary>
+    /// Saves the selected check-in data as attendance.
+    /// </summary>
+    [ActionCategory( "com_centralaz: Check-In" )]
+    [Description( "Saves the selected check-in data as attendance after getting a security code for the checkin." )]
+    [Export( typeof( ActionComponent ) )]
+    [ExportMetadata( "ComponentName", "Create Labels From Label Attribute (CentralAZ)" )]
+
+    [WorkflowAttribute( "Will Label Print Attribute", "The attribute that determines whether the label will print." )]
+    public class CreateLabelsFromLabelAttribute : CheckInActionComponent
+    {
+        /// <summary>
+        /// Executes the specified workflow.
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="action">The workflow action.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="errorMessages">The error messages.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override bool Execute( RockContext rockContext, WorkflowAction action, Object entity, out List<string> errorMessages )
+        {
+            var checkInState = GetCheckInState( entity, out errorMessages );
+
+            var labels = new List<CheckInLabel>();
+
+            if ( checkInState != null )
+            {
+                var family = checkInState.CheckIn.CurrentFamily;
+                if ( family != null )
+                {
+                    var commonMergeFields = Rock.Lava.LavaHelper.GetCommonMergeFields( null );
+                    var groupMemberService = new GroupMemberService( rockContext );
+
+                    var familyLabels = new List<Guid>();
+
+                    var people = family.GetPeople( true );
+                    foreach ( var person in people )
+                    {
+                        foreach ( var groupType in person.GetGroupTypes( true ) )
+                        {
+                            groupType.Labels = new List<CheckInLabel>();
+
+                            bool canPrintLabels = true;
+                            Guid guid = GetAttributeValue( action, "WillLabelPrintAttribute" ).AsGuid();
+                            if ( !guid.IsEmpty() )
+                            {
+                                var attribute = AttributeCache.Read( guid, rockContext );
+                                if ( attribute != null )
+                                {
+                                    if ( attribute.EntityTypeId == new Rock.Model.Workflow().TypeId )
+                                    {
+                                        canPrintLabels = action.Activity.Workflow.GetAttributeValue( attribute.Key ).AsBoolean( resultIfNullOrEmpty: true );
+                                    }
+                                    else if ( attribute.EntityTypeId == new Rock.Model.WorkflowActivity().TypeId )
+                                    {
+                                        action.Activity.GetAttributeValue( attribute.Key ).AsBoolean( resultIfNullOrEmpty: true );
+                                    }
+                                }
+                            }
+
+                            if ( canPrintLabels )
+                            {
+                                var personLabels = new List<Guid>();
+
+                                var groupTypeLabels = GetGroupTypeLabels( groupType.GroupType );
+
+                                var PrinterIPs = new Dictionary<int, string>();
+
+                                foreach ( var labelCache in groupTypeLabels )
+                                {
+                                    foreach ( var group in groupType.GetGroups( true ) )
+                                    {
+                                        foreach ( var location in group.GetLocations( true ) )
+                                        {
+                                            if ( labelCache.LabelType == KioskLabelType.Family )
+                                            {
+                                                if ( familyLabels.Contains( labelCache.Guid ) )
+                                                {
+                                                    break;
+                                                }
+                                                else
+                                                {
+                                                    familyLabels.Add( labelCache.Guid );
+                                                }
+                                            }
+                                            else if ( labelCache.LabelType == KioskLabelType.Person )
+                                            {
+                                                if ( personLabels.Contains( labelCache.Guid ) )
+                                                {
+                                                    break;
+
+                                                }
+                                                else
+                                                {
+                                                    personLabels.Add( labelCache.Guid );
+                                                }
+                                            }
+
+                                            var mergeObjects = new Dictionary<string, object>();
+                                            foreach ( var keyValue in commonMergeFields )
+                                            {
+                                                mergeObjects.Add( keyValue.Key, keyValue.Value );
+                                            }
+
+                                            mergeObjects.Add( "Location", location );
+                                            mergeObjects.Add( "Group", group );
+                                            mergeObjects.Add( "Person", person );
+                                            mergeObjects.Add( "People", people );
+                                            mergeObjects.Add( "GroupType", groupType );
+
+                                            var groupMembers = groupMemberService.Queryable().AsNoTracking()
+                                                .Where( m =>
+                                                    m.PersonId == person.Person.Id &&
+                                                    m.GroupId == group.Group.Id )
+                                                .ToList();
+                                            mergeObjects.Add( "GroupMembers", groupMembers );
+
+                                            var label = new CheckInLabel( labelCache, mergeObjects );
+                                            label.FileGuid = labelCache.Guid;
+                                            label.PrintFrom = checkInState.Kiosk.Device.PrintFrom;
+                                            label.PrintTo = checkInState.Kiosk.Device.PrintToOverride;
+
+                                            if ( label.PrintTo == PrintTo.Default )
+                                            {
+                                                label.PrintTo = groupType.GroupType.AttendancePrintTo;
+                                            }
+
+                                            if ( label.PrintTo == PrintTo.Kiosk )
+                                            {
+                                                var device = checkInState.Kiosk.Device;
+                                                if ( device != null )
+                                                {
+                                                    label.PrinterDeviceId = device.PrinterDeviceId;
+                                                }
+                                            }
+                                            else if ( label.PrintTo == PrintTo.Location )
+                                            {
+                                                var deviceId = location.Location.PrinterDeviceId;
+                                                if ( deviceId != null )
+                                                {
+                                                    label.PrinterDeviceId = deviceId;
+                                                }
+                                            }
+
+                                            if ( label.PrinterDeviceId.HasValue )
+                                            {
+                                                if ( PrinterIPs.ContainsKey( label.PrinterDeviceId.Value ) )
+                                                {
+                                                    label.PrinterAddress = PrinterIPs[label.PrinterDeviceId.Value];
+                                                }
+                                                else
+                                                {
+                                                    var printerDevice = new DeviceService( rockContext ).Get( label.PrinterDeviceId.Value );
+                                                    if ( printerDevice != null )
+                                                    {
+                                                        PrinterIPs.Add( printerDevice.Id, printerDevice.IPAddress );
+                                                        label.PrinterAddress = printerDevice.IPAddress;
+                                                    }
+                                                }
+                                            }
+
+                                            groupType.Labels.Add( label );
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return true;
+
+            }
+
+            return false;
+        }
+
+        private List<KioskLabel> GetGroupTypeLabels( GroupTypeCache groupType )
+        {
+            var labels = new List<KioskLabel>();
+
+            //groupType.LoadAttributes();
+            foreach ( var attribute in groupType.Attributes.OrderBy( a => a.Value.Order ) )
+            {
+                if ( attribute.Value.FieldType.Guid == Rock.SystemGuid.FieldType.BINARY_FILE.AsGuid() &&
+                    attribute.Value.QualifierValues.ContainsKey( "binaryFileType" ) &&
+                    attribute.Value.QualifierValues["binaryFileType"].Value.Equals( Rock.SystemGuid.BinaryFiletype.CHECKIN_LABEL, StringComparison.OrdinalIgnoreCase ) )
+                {
+                    Guid? binaryFileGuid = groupType.GetAttributeValue( attribute.Key ).AsGuidOrNull();
+                    if ( binaryFileGuid != null )
+                    {
+                        var labelCache = KioskLabel.Read( binaryFileGuid.Value );
+                        labelCache.Order = attribute.Value.Order;
+                        if ( labelCache != null )
+                        {
+                            labels.Add( labelCache );
+                        }
+                    }
+                }
+            }
+
+            return labels;
+        }
+    }
+}
---
+++ com.centralaz.Workflow/Action/CheckIn/SaveAttendanceAndSetLabelAttribute.cs
@@ -0,0 +1,188 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+using Rock;
+using Rock.Attribute;
+using Rock.CheckIn;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Workflow;
+using Rock.Workflow.Action.CheckIn;
+
+namespace com.centralaz.Workflow.Action.CheckIn
+{
+    /// <summary>
+    /// Saves the selected check-in data as attendance.
+    /// </summary>
+    [ActionCategory( "com_centralaz: Check-In" )]
+    [Description( "Saves the selected check-in data as attendance after getting a security code for the checkin." )]
+    [Export( typeof( ActionComponent ) )]
+    [ExportMetadata( "ComponentName", "Save Attendance and Set Label Attribute (CentralAZ)" )]
+
+    [WorkflowAttribute( "Will Label Print Attribute", "The attribute that determines whether the label will print." )]
+    [IntegerField( "Number Of Alpha Characters", "Used to decide how many alpha characters should be included in the prefix of the security code.", true, 2 )]
+    [IntegerField( "Number Of Numeric Characters", "Used to decide how many numeric characters should be included in the suffix of the security code. It is recommended to set no less than 4 to avoid problems obtaining a unique number.", true, 4 )]
+    public class SaveAttendanceAndSetLabelAttribute : CheckInActionComponent
+    {
+        /// <summary>
+        /// Executes the specified workflow.
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="action">The workflow action.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="errorMessages">The error messages.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override bool Execute( RockContext rockContext, WorkflowAction action, Object entity, out List<string> errorMessages )
+        {
+            var checkInState = GetCheckInState( entity, out errorMessages );
+            if ( checkInState != null )
+            {
+                AttendanceCode attendanceCode = null;
+                DateTime startDateTime = RockDateTime.Now;
+
+                bool canPrintLabelMultipleTimes = true;
+                if ( checkInState.CheckinTypeId.HasValue )
+                {
+                    var checkinType = GroupTypeCache.Read( checkInState.CheckinTypeId.Value );
+                    if ( checkinType != null )
+                    {
+                        checkinType.LoadAttributes();
+                        canPrintLabelMultipleTimes = checkinType.GetAttributeValue( "CanPrintLabelMultipleTimes" ).AsBoolean( resultIfNullOrEmpty: true );
+                    }
+                }
+
+                bool reuseCodeForFamily = checkInState.CheckInType != null && checkInState.CheckInType.ReuseSameCode;
+                int securityCodeAlphaLength = GetAttributeValue( action, "NumberOfAlphaCharacters" ).AsInteger();
+                int securityCodeNumericLength = GetAttributeValue( action, "NumberOfNumericCharacters" ).AsInteger();
+
+                var attendanceCodeService = new AttendanceCodeService( rockContext );
+                var attendanceService = new AttendanceService( rockContext );
+                var groupMemberService = new GroupMemberService( rockContext );
+                var personAliasService = new PersonAliasService( rockContext );
+
+
+                var family = checkInState.CheckIn.CurrentFamily;
+                if ( family != null )
+                {
+                    foreach ( var person in family.GetPeople( true ) )
+                    {
+                        if ( reuseCodeForFamily && attendanceCode != null )
+                        {
+                            person.SecurityCode = attendanceCode.Code;
+                        }
+                        else
+                        {
+                            attendanceCode = AttendanceCodeService.GetNew( securityCodeAlphaLength, securityCodeNumericLength, isRandomized: false );
+                            person.SecurityCode = attendanceCode.Code;
+                        }
+
+                        foreach ( var groupType in person.GetGroupTypes( true ) )
+                        {
+                            foreach ( var group in groupType.GetGroups( true ) )
+                            {
+                                if ( groupType.GroupType.AttendanceRule == AttendanceRule.AddOnCheckIn &&
+                                    groupType.GroupType.DefaultGroupRoleId.HasValue &&
+                                    !groupMemberService.GetByGroupIdAndPersonId( group.Group.Id, person.Person.Id, true ).Any() )
+                                {
+                                    var groupMember = new GroupMember();
+                                    groupMember.GroupId = group.Group.Id;
+                                    groupMember.PersonId = person.Person.Id;
+                                    groupMember.GroupRoleId = groupType.GroupType.DefaultGroupRoleId.Value;
+                                    groupMemberService.Add( groupMember );
+                                }
+
+                                foreach ( var location in group.GetLocations( true ) )
+                                {
+                                    foreach ( var schedule in location.GetSchedules( true ) )
+                                    {
+                                        // Only create one attendance record per day for each person/schedule/group/location
+                                        var attendance = attendanceService.Get( startDateTime, location.Location.Id, schedule.Schedule.Id, group.Group.Id, person.Person.Id );
+                                        if ( attendance == null )
+                                        {
+                                            var primaryAlias = personAliasService.GetPrimaryAlias( person.Person.Id );
+                                            if ( primaryAlias != null )
+                                            {
+                                                attendance = rockContext.Attendances.Create();
+                                                attendance.LocationId = location.Location.Id;
+                                                attendance.CampusId = location.CampusId;
+                                                attendance.ScheduleId = schedule.Schedule.Id;
+                                                attendance.GroupId = group.Group.Id;
+                                                attendance.PersonAlias = primaryAlias;
+                                                attendance.PersonAliasId = primaryAlias.Id;
+                                                attendance.DeviceId = checkInState.Kiosk.Device.Id;
+                                                attendance.SearchTypeValueId = checkInState.CheckIn.SearchType.Id;
+                                                attendanceService.Add( attendance );
+
+                                                SetCanPrintLabelValue( rockContext, action, true );
+                                            }
+                                        }
+                                        else
+                                        {
+                                            SetCanPrintLabelValue( rockContext, action, canPrintLabelMultipleTimes );
+                                        }
+
+                                        attendance.CreatedDateTime = RockDateTime.Now;
+                                        attendance.AttendanceCodeId = attendanceCode.Id;
+                                        attendance.StartDateTime = startDateTime;
+                                        attendance.EndDateTime = null;
+                                        attendance.DidAttend = true;
+
+                                        KioskLocationAttendance.AddAttendance( attendance );
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                rockContext.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        private void SetCanPrintLabelValue( RockContext rockContext, WorkflowAction action, bool value )
+        {
+            Guid guid = GetAttributeValue( action, "WillLabelPrintAttribute" ).AsGuid();
+            if ( !guid.IsEmpty() )
+            {
+                var attribute = AttributeCache.Read( guid, rockContext );
+                if ( attribute != null )
+                {
+                    if ( attribute.EntityTypeId == new Rock.Model.Workflow().TypeId )
+                    {
+                        action.Activity.Workflow.SetAttributeValue( attribute.Key, value.ToString() );
+                        action.AddLogEntry( string.Format( "Set '{0}' attribute to '{1}'.", attribute.Name, value ) );
+                    }
+                    else if ( attribute.EntityTypeId == new Rock.Model.WorkflowActivity().TypeId )
+                    {
+                        action.Activity.SetAttributeValue( attribute.Key, value.ToString() );
+                        action.AddLogEntry( string.Format( "Set '{0}' attribute to '{1}'.", attribute.Name, value ) );
+                    }
+                }
+            }
+        }
+    }
+}
