29f5da45504000e252beec3a0e359c0ee7f3044d
Mike Peterson <MikePeterson@ccvonline.com>
2016-12-05 11:02:42 -0700

    update comments on AnalyticsDimFamily/SourceGroup

--- Rock/Model/AnalyticsSourceGroupHistorical.cs
+++ Rock/Model/AnalyticsSourceGroupHistorical.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 using Rock.Data;
 
 namespace Rock.Model
