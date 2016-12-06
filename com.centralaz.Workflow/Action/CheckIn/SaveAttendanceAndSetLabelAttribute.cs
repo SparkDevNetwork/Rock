776808baaf6a425afe37fb0d90861f4e250a7642
tcavaletto <taylor.cavaletto@centralaz.com>
2016-12-06 14:37:39 -0700

    - Checkin Types Attribute no longer required

--- com.centralaz.Workflow/Action/CheckIn/SaveAttendanceAndSetLabelAttribute.cs
+++ com.centralaz.Workflow/Action/CheckIn/SaveAttendanceAndSetLabelAttribute.cs
@@ -40,7 +40,7 @@ namespace com.centralaz.Workflow.Action.CheckIn
     [ExportMetadata( "ComponentName", "Save Attendance and Set Label Attribute (CentralAZ)" )]
 
     [WorkflowAttribute( "Will Label Print Attribute", "The attribute that determines whether the label will print." )]
-    [GroupTypesField( "Checkin Types To Limit", "The Checkin Types that will be limited to one label print per group per schedule" )]
+    [GroupTypesField( "Checkin Types To Limit", "The Checkin Types that will be limited to one label print per group per schedule", false )]
     [IntegerField( "Number Of Alpha Characters", "Used to decide how many alpha characters should be included in the prefix of the security code.", true, 2 )]
     [IntegerField( "Number Of Numeric Characters", "Used to decide how many numeric characters should be included in the suffix of the security code. It is recommended to set no less than 4 to avoid problems obtaining a unique number.", true, 4 )]
     public class SaveAttendanceAndSetLabelAttribute : CheckInActionComponent
