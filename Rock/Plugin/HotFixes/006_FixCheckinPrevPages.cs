0bd346d01b4b1f5cacbfc0fb6ca24fbd7674c65f
nairdo <nairdo@gmail.com>
2016-12-12 11:03:44 -0700

    - Moved hotfix migrations for 1.6.1 into core migrations.

--- Rock/Plugin/HotFixes/006_FixCheckinPrevPages.cs
+++ Rock/Plugin/HotFixes/006_FixCheckinPrevPages.cs
@@ -27,18 +27,18 @@ namespace Rock.Plugin.HotFixes
         /// </summary>
         public override void Up()
         {
-            Sql( @"
-    DECLARE @AttributeId INT = ( SELECT TOP 1 [Id] FROM [Attribute] WHERE [Guid] = '3D24A4D2-90AF-4FDD-8CE2-7D1F9B76104B' )
-    UPDATE [AttributeValue] 
-	SET [value] = '67bd09b0-0c6e-44e7-a8eb-0e71551f3e6b'
-	WHERE [AttributeId] = @AttributeId
+//  Moved to core migration: 201612121647292_HotFixesFrom6_1
+//            Sql( @"
+//    DECLARE @AttributeId INT = ( SELECT TOP 1 [Id] FROM [Attribute] WHERE [Guid] = '3D24A4D2-90AF-4FDD-8CE2-7D1F9B76104B' )
+//    UPDATE [AttributeValue] 
+//	SET [value] = '67bd09b0-0c6e-44e7-a8eb-0e71551f3e6b'
+//	WHERE [AttributeId] = @AttributeId
 
-    SET @AttributeId = ( SELECT TOP 1 [Id] FROM [Attribute] WHERE [Guid] = '39D260A5-A976-4DA9-B3E0-7381E9B8F3D5' )
-    UPDATE [AttributeValue] 
-	SET [value] = 'a1cbdaa4-94dd-4156-8260-5a3781e39fd0'
-	WHERE [AttributeId] = @AttributeId
-" );
-
+//    SET @AttributeId = ( SELECT TOP 1 [Id] FROM [Attribute] WHERE [Guid] = '39D260A5-A976-4DA9-B3E0-7381E9B8F3D5' )
+//    UPDATE [AttributeValue] 
+//	SET [value] = 'a1cbdaa4-94dd-4156-8260-5a3781e39fd0'
+//	WHERE [AttributeId] = @AttributeId
+//" );
         }
 
         /// <summary>
