d153137163036b81d6a56c331514037f6c711f1c
Jon Edmiston <jon@sparkdevnetwork.org>
2016-12-29 22:54:34 -0700

    - additional work on shortcodes

---
+++ Rock/Lava/Shortcodes/RockLavaShortcodeBlockBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotLiquid;
+
+using Rock.Utility;
+
+namespace Rock.Lava.Shortcodes
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="DotLiquid.Block" />
+    public class RockLavaShortcodeBlockBase : DotLiquid.Block, IRockStartup, IRockShortcode
+    {
+        
+        /// <summary>
+        /// Renders the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="result">The result.</param>
+        public override void Render( Context context, TextWriter result )
+        {
+            base.Render( context, result );
+        }
+
+        /// <summary>
+        /// All IRockStartup classes will be run in order by this value. If class does not depend on an order, return zero.
+        /// </summary>
+        /// <value>
+        /// The order.
+        /// </value>
+        public int StartupOrder { get { return 0; } }
+
+        /// <summary>
+        /// Method that will be run at Rock startup
+        /// </summary>
+        public virtual void OnStartup()
+        {
+        }
+
+    }
+}
