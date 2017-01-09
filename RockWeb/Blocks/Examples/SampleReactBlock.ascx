3b64338de7998ed4daf48e4fc60de6a3db3ae49a
James Baxley <james.baxley@ad.newspring.cc>
2017-01-09 15:02:39 -0500

    created sample counter block

---
+++ RockWeb/Blocks/Examples/SampleReactBlock.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="SampleReactBlock.ascx.cs" Inherits="RockWeb.Blocks.Examples.SampleReactBlock" %>
+<%--<div data-react-block data-initial-props='<asp:Literal ID="props" runat="server" />'></div>--%>
+
+<div data-react-block data-initial-props='<%= props %>'></div>
---
+++ RockWeb/Blocks/Examples/SampleReactBlock.ascx.cs
@@ -0,0 +1,72 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.Net;
+using System.Web.UI;
+using Rock;
+using Rock.Utility;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web;
+using Rock.Web.UI;
+
+namespace RockWeb.Blocks.Examples
+{
+    /// <summary>
+    /// A sample block that uses many of the Rock UI controls.
+    /// </summary>
+    [DisplayName("Sample React Block")]
+    [Category("Examples")]
+    [Description("Creates a generic counter to showcase React integration")]
+
+    [IntegerField("StartingNumber", "Specify a value to start the counter with", required: false)]
+    public partial class SampleReactBlock : RockBlock
+    {
+        protected string props = "";
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!Page.IsPostBack)
+            { 
+                var startingNumber = GetAttributeValue("StartingNumber").ToStringSafe().AsIntegerOrNull();
+                var initialProps = new InitialProps(startingNumber);
+                props = initialProps.ToJson();
+            }
+        }
+
+        class InitialProps
+        {
+            public int startingNumber;
+            public InitialProps(int? start)
+            {
+                startingNumber = start ?? 0;
+            }
+        }
+    }
+}
