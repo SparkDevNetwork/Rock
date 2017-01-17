9381d9955fc595b352335063ba17921f74ed97cd
dcs619 <dcs619@gmail.com>
2017-01-17 14:15:06 -0500

    Update delete obsolete accounts [ci skip]

--- Dev Tools/Sql/NewSpring_27_DeleteObsoleteSavedAccounts.sql
+++ Dev Tools/Sql/NewSpring_27_DeleteObsoleteSavedAccounts.sql
@@ -16,42 +16,41 @@
 
 */
 
-select fa.id RockSavedAccountId, fa.Name, fa.PersonAliasId, fa.ReferenceNumber, fa.CreatedDateTime, fa.ModifiedDateTime, fa.TransactionCode, pd.AccountNumberMasked, p.email 'rockEmails'
-from FinancialPersonSavedAccount fa
-inner join FinancialPaymentDetail pd
-on fa.FinancialPaymentDetailId = pd.id
-inner join PersonAlias pa
-on fa.PersonAliasId = pa.Id
-inner join Person p
-on pa.PersonId = p.id
-left outer join [Imports]..nmiAccounts a
-on fa.ReferenceNumber = a.vault_id
-and pd.AccountNumberMasked = a.account
-where a.vault_id is null
-and fa.CreatedDateTime < CONVERT(DATE, getdate())
+SELECT fa.id RockSavedAccountId, fa.Name, fa.PersonAliasId, fa.ReferenceNumber, fa.CreatedDateTime, fa.ModifiedDateTime, fa.TransactionCode, pd.AccountNumberMasked, p.email 'rockEmails'
+FROM FinancialPersonSavedAccount fa
+INNER JOIN FinancialPaymentDetail pd
+	ON fa.FinancialPaymentDetailId = pd.id
+INNER JOIN PersonAlias pa
+	ON fa.PersonAliasId = pa.Id
+INNER JOIN Person p
+	ON pa.PersonId = p.id
+LEFT OUTER JOIN [Imports]..nmiAccounts a
+	ON fa.ReferenceNumber = a.customer_vault_id
+	AND pd.AccountNumberMasked = a.account
+WHERE a.customer_vault_id IS NULL
+	AND fa.CreatedDateTime < CONVERT(DATE, GETDATE())
 
-drop table [Imports]..nmiDeletes
-select fa.id SavedAccountId, fa.Name, fa.PersonAliasId, fa.ReferenceNumber, fa.CreatedDateTime, fa.ModifiedDateTime, fa.TransactionCode, pd.AccountNumberMasked, p.email 'rockEmails'
---into [Imports]..nmiDeletes
-from FinancialPersonSavedAccount fa
-inner join FinancialPaymentDetail pd
-on fa.FinancialPaymentDetailId = pd.id
-inner join PersonAlias pa
-on fa.PersonAliasId = pa.Id
-inner join Person p
-on pa.PersonId = p.id
-left join [Imports]..nmiAccounts a
-on fa.ReferenceNumber = a.vault_id
-and pd.AccountNumberMasked = a.account
-where a.vault_id is null
-and fa.CreatedDateTime < CONVERT(DATE, getdate())
-
-begin transaction
-
-delete from FinancialPersonSavedAccount 
-where Id IN (select SavedAccountId from [Imports]..nmiDeletes)
+--DROP TABLE [Imports]..nmiDeletes
+SELECT fa.*, pd.AccountNumberMasked, pd.CurrencyTypeValueId, pd.CreditCardTypeValueId, p.email 'PersonEmail' 
+--INTO [Imports]..nmiDeletes
+FROM FinancialPersonSavedAccount fa
+INNER JOIN FinancialPaymentDetail pd
+	ON fa.FinancialPaymentDetailId = pd.id
+INNER JOIN PersonAlias pa
+	ON fa.PersonAliasId = pa.Id
+INNER JOIN Person p
+	ON pa.PersonId = p.id
+LEFT JOIN [Imports]..nmiAccounts a
+	ON fa.ReferenceNumber = a.customer_vault_id	
+WHERE a.customer_vault_id IS NULL
+	AND fa.CreatedDateTime < CONVERT(DATE, GETDATE())
 
 
---rollback transaction
+BEGIN TRANSACTION
 
---commit transaction
+DELETE FROM FinancialPersonSavedAccount 
+WHERE [Id] IN (SELECT Id from [Imports]..nmiDeletes)
+
+--ROLLBACK TRANSACTION
+
+--COMMIT TRANSACTION
