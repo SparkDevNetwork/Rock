51879298d00bb06f54622ce4e77d12cfa00e8850
mikepetersonccv <mike@mikeapril.com>
2017-01-18 21:53:28 -0700

    - started on MigrateInteractionsData job

---
+++ Rock/Jobs/MigrateInteractionsData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Rock.Jobs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Quartz.IJob" />
+    [DisallowConcurrentExecution]
+    public class MigrateInteractionsData : IJob
+    {
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public void Execute( IJobExecutionContext context )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
