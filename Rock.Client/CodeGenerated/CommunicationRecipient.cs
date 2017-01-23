2e2e3614619b9e478f1efb4ab7185ebe1108c5c8
Mike Peterson <mike@sparkabilitygroup.com>
2017-01-23 16:58:01 -0700

    - update codegen'd

--- Rock.Client/CodeGenerated/CommunicationRecipient.cs
+++ Rock.Client/CodeGenerated/CommunicationRecipient.cs
@@ -137,9 +137,6 @@ namespace Rock.Client
     public partial class CommunicationRecipient : CommunicationRecipientEntity
     {
         /// <summary />
-        public ICollection<CommunicationRecipientActivity> Activities { get; set; }
-
-        /// <summary />
         public Dictionary<string, Object> AdditionalMergeValues { get; set; }
 
         /// <summary />
