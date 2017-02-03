b6c666f46639760b462013542fc42ef7c26a0386
David Turner <david@sparkdevnetwork.org>
2017-02-03 13:55:35 -0700

    - Updated version to 1.7 and copyright to 2017

--- RockJobSchedulerService/Properties/AssemblyInfo.cs
+++ RockJobSchedulerService/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Spark Development Network" )]
 [assembly: AssemblyProduct( "RockJobSchedulerService" )]
-[assembly: AssemblyCopyright( "Copyright © Spark Development Network 2011-2016" )]
+[assembly: AssemblyCopyright( "Copyright © Spark Development Network 2011-2017" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.6.0.0" )]
-[assembly: AssemblyFileVersion( "1.6.0.0" )]
+[assembly: AssemblyVersion( "1.7.0.0" )]
+[assembly: AssemblyFileVersion( "1.7.0.0" )]
