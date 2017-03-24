6e75a6c3e9ec2f7dfe8def96e0728b3abfb7cd27
Taylor Cavaletto <Taylor.Cavaletto@CentralAZ.com>
2017-03-24 06:51:41 -0700

    - Registration Entry no longer inserts nicknames into Registration Text Attribute form fields ( Fixes #2040). (#2063)
    +Updated Registration Entry block so that it no longer inserts nicknames into Registration Text Attribute form fields ( Fixes #2040).
    
    * - RegistrationEntry now checks whether a field is a PersonField using field.FieldSource

--- RockWeb/Blocks/Event/RegistrationEntry.ascx.cs
+++ RockWeb/Blocks/Event/RegistrationEntry.ascx.cs
@@ -1385,8 +1385,9 @@ namespace RockWeb.Blocks.Event
                         foreach( var field in RegistrationTemplate.Forms
                             .SelectMany( f => f.Fields )
                             .Where( f => 
-                                f.PersonFieldType == RegistrationPersonFieldType.FirstName ||
-                                f.PersonFieldType == RegistrationPersonFieldType.LastName ) )
+                                ( f.PersonFieldType == RegistrationPersonFieldType.FirstName ||
+                                f.PersonFieldType == RegistrationPersonFieldType.LastName ) &&
+                                f.FieldSource == RegistrationFieldSource.PersonField ) )
                         {
                             registrant.FieldValues.AddOrReplace( field.Id, 
                                 new FieldValueObject( field, field.PersonFieldType == RegistrationPersonFieldType.FirstName ? CurrentPerson.NickName : CurrentPerson.LastName ) );
@@ -4553,8 +4554,9 @@ namespace RockWeb.Blocks.Event
                             object dbValue = null;
 
                             if ( field.ShowCurrentValue ||
-                                field.PersonFieldType == RegistrationPersonFieldType.FirstName ||
-                                field.PersonFieldType == RegistrationPersonFieldType.LastName )
+                                ( ( field.PersonFieldType == RegistrationPersonFieldType.FirstName || 
+                                field.PersonFieldType == RegistrationPersonFieldType.LastName ) &&
+                                field.FieldSource == RegistrationFieldSource.PersonField ) )
                             {
                                 dbValue = registrant.GetRegistrantValue( null, person, family, field, rockContext );
                             }
