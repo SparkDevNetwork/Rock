7a6148af71be9d63a14ea2d09e23827a70ade08b
David Turner <david@sparkdevnetwork.org>
2017-03-24 18:11:40 -0700

    + Fixed issue with not being able to unselect all the phone types on the directory block (Fixes #1983).

--- RockWeb/Blocks/CRM/PersonDirectory.ascx.cs
+++ RockWeb/Blocks/CRM/PersonDirectory.ascx.cs
@@ -50,8 +50,7 @@ namespace RockWeb.Blocks.Crm
     [IntegerField( "Last Name Characters Required", "The number of characters that need to be entered before allowing a search.", false, 3, "", 5 )]
     [BooleanField( "Show Email", "Should email address be included in the directory?", true, "", 6)]
     [BooleanField( "Show Address", "Should email address be included in the directory?", true, "", 7 )]
-    [DefinedValueField( Rock.SystemGuid.DefinedType.PERSON_PHONE_TYPE, "Show Phones", "The phone numbers to be included in the directory",
-        false, true, "407e7e45-7b2e-4fcd-9605-ecb1339f2453,aa8732fb-2cea-4c76-8d6d-6aaa2c6a4303,2cc66d5a-f61c-4b74-9af9-590a9847c13c", "", 8 )]
+    [DefinedValueField( Rock.SystemGuid.DefinedType.PERSON_PHONE_TYPE, "Show Phones", "The phone numbers to be included in the directory", false, true, "", "", 8 )]
     [BooleanField( "Show Birthday", "Should email address be included in the directory?", true, "", 9 )]
     [BooleanField( "Show Gender", "Should email address be included in the directory?", true, "", 10 )]
     [BooleanField( "Show Grade", "Should grade be included in the directory?", false, "", 11 )]
