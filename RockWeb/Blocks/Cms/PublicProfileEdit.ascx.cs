4b6e1d7e1c39ec1199f4032c308ed76dd4b5eb79
Tyler Schrock <tschrock123@gmail.com>
2017-03-27 10:34:00 -0400

    - Fix Public Profile Edit saving wrong Record Status (Fixes #2096)

--- RockWeb/Blocks/Cms/PublicProfileEdit.ascx.cs
+++ RockWeb/Blocks/Cms/PublicProfileEdit.ascx.cs
@@ -357,7 +357,7 @@ namespace RockWeb.Blocks.Cms
                             if ( headOfHousehold != null )
                             {
                                 DefinedValueCache dvcConnectionStatus = DefinedValueCache.Read( headOfHousehold.ConnectionStatusValueId ?? 0 );
-                                DefinedValueCache dvcRecordStatus = DefinedValueCache.Read( headOfHousehold.ConnectionStatusValueId ?? 0 );
+                                DefinedValueCache dvcRecordStatus = DefinedValueCache.Read( headOfHousehold.RecordStatusValueId ?? 0 );
                                 if ( dvcConnectionStatus != null )
                                 {
                                     groupMember.Person.ConnectionStatusValueId = dvcConnectionStatus.Id;
