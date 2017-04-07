de0620d133af0fa4383fa8a6a158e54e3794df39
Mike Peterson <mike@sparkabilitygroup.com>
2017-04-07 09:14:05 -0700

    - Move Fundraising Hotfix migration from Rock.Migrations to Rock.Plugin.Hotfixes

--- Rock/Plugin/HotFixes/022_Fundraising.cs
+++ Rock/Plugin/HotFixes/022_Fundraising.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Rock.Plugin;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 
-namespace Rock.Migrations.HotFixMigrations
+namespace Rock.Plugin.HotFixes
 {
-    [MigrationNumber( 7, "1.6.2" )]
+    [MigrationNumber( 22, "1.6.2" )]
     public class Fundraising : Migration
     {
         /// <summary>
