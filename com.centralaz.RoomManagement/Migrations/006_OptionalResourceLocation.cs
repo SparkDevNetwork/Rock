c1d77b5148d829d549ef31a1e31740aaedf6d1e6
nairdo <nairdo@gmail.com>
2017-04-18 16:55:30 -0700

    + Added optional Location for the Resource model

---
+++ com.centralaz.RoomManagement/Migrations/006_OptionalResourceLocation.cs
@@ -0,0 +1,39 @@
+﻿// <copyright>
+// Copyright by the Central Christian Church
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using Rock.Plugin;
+
+namespace com.centralaz.RoomManagement.Migrations
+{
+    [MigrationNumber( 6, "1.6.0" )]
+    public class OptionalResourceLocation : Migration
+    {
+        public override void Up()
+        {
+            AddColumn( "[dbo].[_com_centralaz_RoomManagement_Resource]", "LocationId", c => c.Int() );
+            //AddForeignKey( "[dbo].[_com_centralaz_RoomManagement_Resource]", "LocationId", "dbo.Location", "Id" );
+            Sql( @"
+                ALTER TABLE [dbo].[_com_centralaz_RoomManagement_Resource]  WITH CHECK ADD  CONSTRAINT [FK__com_centralaz_RoomManagement_Resource_Location] FOREIGN KEY([LocationId])
+                REFERENCES [dbo].[Location] ([Id])
+" );
+        }
+        public override void Down()
+        {
+            DropForeignKey( "[dbo].[_com_centralaz_RoomManagement_Resource]", "LocationId" );
+            DropColumn( "[dbo].[_com_centralaz_RoomManagement_Resource]", "LocationId" );
+        }
+    }
+}
--- com.centralaz.RoomManagement/Model/Resource.cs
+++ com.centralaz.RoomManagement/Model/Resource.cs
@@ -41,6 +41,9 @@ namespace com.centralaz.RoomManagement.Model
         public int? CampusId { get; set; }
 
         [DataMember]
+        public int? LocationId { get; set; }
+
+        [DataMember]
         public int? ApprovalGroupId { get; set; }
 
         [DataMember]
@@ -56,6 +59,8 @@ namespace com.centralaz.RoomManagement.Model
         public virtual Category Category { get; set; }
 
         public virtual Campus Campus { get; set; }
+
+        public virtual Location Location { get; set; }
 
         public virtual Group ApprovalGroup { get; set; }
 
@@ -75,6 +80,7 @@ namespace com.centralaz.RoomManagement.Model
         {
             this.HasRequired( r => r.Category ).WithMany().HasForeignKey( r => r.CategoryId ).WillCascadeOnDelete( false );
             this.HasOptional( r => r.Campus ).WithMany().HasForeignKey( r => r.CampusId ).WillCascadeOnDelete( false );
+            this.HasOptional( r => r.Location ).WithMany().HasForeignKey( r => r.LocationId ).WillCascadeOnDelete( false );
             this.HasOptional( r => r.ApprovalGroup ).WithMany().HasForeignKey( r => r.ApprovalGroupId ).WillCascadeOnDelete( false );
         }
     }
