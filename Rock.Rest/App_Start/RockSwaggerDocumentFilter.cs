0c17eb6e7f86feef02fa2bb0102f3e2bd779371f
Mike Peterson <mike@sparkabilitygroup.com>
2017-04-18 18:13:11 -0700

    - Added Swagger Documentation

---
+++ Rock.Rest/App_Start/RockSwaggerDocumentFilter.cs
@@ -0,0 +1,74 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace Rock.Rest
+{
+    public class RockSwaggerDocumentFilter : IDocumentFilter
+    {
+        // The default doc is huge and very slow, so this attempts to narrow down what is included
+        public void Apply( SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer )
+        {
+            // to help make the doc smaller, remove paths for the following actions
+            string[] actionsToHide = new string[]
+            {
+                "/DataView/",
+                "/LaunchWorkflow/",
+                "/DeleteAttributeValue/",
+                "/AttributeValue/",
+                "/SetAttributeValue/",
+                "/SetContext/",
+            };
+
+            Dictionary<string, PathItem> pathsToKeep = new Dictionary<string, PathItem>();
+
+            foreach ( var path in swaggerDoc.paths )
+            {
+                if ( !actionsToHide.Any( a => path.Key.Contains( a ) ) )
+                {
+                    pathsToKeep.Add( path.Key, path.Value );
+                }
+            }
+
+            string[] namespacesToHide = new string[]
+            {
+                "System.Web.Http.OData",
+                "Microsoft.Data.Edm",
+                "Microsoft.Data.OData"
+            };
+
+            swaggerDoc.paths = pathsToKeep.OrderBy( a => a.Key ).ToDictionary( x => x.Key, x => x.Value );
+
+            Dictionary<string, Schema> definitionsToKeep = new Dictionary<string, Schema>();
+            foreach ( var definition in schemaRegistry.Definitions )
+            {
+                if ( !namespacesToHide.Any( a => definition.Key.Contains( a ) ) )
+                {
+                    definitionsToKeep.Add( definition.Key, definition.Value );
+                }
+
+                // to help make the doc smaller, don't include references to other models in the properties of an item
+                definition.Value.properties = definition.Value.properties.Where( a => string.IsNullOrEmpty( a.Value.@ref ) ).ToDictionary( k => k.Key, v => v.Value );
+            }
+
+            swaggerDoc.definitions = definitionsToKeep.OrderBy( a => a.Key ).ToDictionary( x => x.Key, x => x.Value );
+        }
+    }
+}
---
+++ Rock.Rest/App_Start/RockSwaggerOperationFilter.cs
@@ -0,0 +1,43 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.Linq;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace Rock.Rest
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Swashbuckle.Swagger.IOperationFilter" />
+    public class RockSwaggerOperationFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Applies the specified operation.
+        /// </summary>
+        /// <param name="operation">The operation.</param>
+        /// <param name="schemaRegistry">The schema registry.</param>
+        /// <param name="apiDescription">The API description.</param>
+        public void Apply( Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription )
+        {
+            // limit swagger docs to only json to help speed up the Swagger UI (supporting XML is slower since the Swagger UI javascript has to create an example in XML format for all the models)
+            operation.consumes = operation.consumes?.Where( a => a == "application/json" ).ToList();
+            operation.produces = operation.produces?.Where( a => a == "application/json" ).ToList();
+            operation.operationId = apiDescription.ID.Replace( '/', '_' ).RemoveSpecialCharacters();
+        }
+    }
+}
