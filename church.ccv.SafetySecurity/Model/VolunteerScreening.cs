2fa1f751a2569d2b881cc52c8f0afdc9609b8dca
Jered McFerron <jeredmcferron@ccv.church>
2017-04-24 16:40:22 -0700

    Initial Commit of the Volunteer Screening project. While not 100% finished, it is functional.

---
+++ church.ccv.SafetySecurity/Model/VolunteerScreening.cs
@@ -0,0 +1,56 @@
+﻿using Rock.Data;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace church.ccv.SafetySecurity.Model
+{
+        [Table( "_church_ccv_SafetySecurity_VolunteerScreening" )]
+        [DataContract]
+        public class VolunteerScreening : Model<VolunteerScreening>, IRockEntity
+        {
+            public enum Types
+            {
+                Legacy,
+                Normal
+            }
+
+            [DataMember]
+            public DateTime Date { get; set; }
+
+            [DataMember]
+            public int PersonAliasId { get; set; }
+
+            [DataMember]
+            public int Type { get; set; }
+
+            [DataMember]
+            public int? Application_WorkflowTypeId { get; set; }
+
+            [DataMember]
+            public int? Application_WorkflowId { get; set; }
+
+            [DataMember]
+            public DateTime? BGCheck_Result_Date { get; set; }
+
+            [DataMember]
+            public Guid? BGCheck_Result_DocGuid { get; set; }
+
+            [DataMember]
+            public string BGCheck_Result_Value { get; set; }
+
+            // These are legacy values used for scanning in old applications
+            [DataMember]
+            public int? Legacy_Application_DocFileId { get; set; }
+        
+            [DataMember]
+            public int? Legacy_CharacterReference1_DocFileId { get; set; }
+
+            [DataMember]
+            public int? Legacy_CharacterReference2_DocFileId { get; set; }
+
+            [DataMember]
+            public int? Legacy_CharacterReference3_DocFileId { get; set; }
+            //
+        }
+}
