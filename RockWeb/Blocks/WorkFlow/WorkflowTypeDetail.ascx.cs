02573ba0c441838958bd95a06db821461bf913d8
Daniel Hazelbaker <daniel@blueboxmoon.com>
2017-04-28 04:45:49 -0700

    - Fixed WorkflowTypeDetail when editing a workflow with a large number of attributes. (#2143)

--- RockWeb/Blocks/WorkFlow/WorkflowTypeDetail.ascx.cs
+++ RockWeb/Blocks/WorkFlow/WorkflowTypeDetail.ascx.cs
@@ -668,7 +668,7 @@ This {{ Workflow.WorkflowType.WorkTerm }} does not currently require your attent
 
                 // Delete any workflow actions of this type
                 int loopCounter = 0;
-                foreach ( var action in workflowActionService.Queryable().Where( a => a.ActionTypeId == actionType.Id ) )
+                foreach ( var action in workflowActionService.Queryable().Where( a => a.ActionTypeId == actionType.Id ).ToList() )
                 {
                     workflowActionService.Delete( action );
                     loopCounter++;
@@ -693,7 +693,7 @@ This {{ Workflow.WorkflowType.WorkTerm }} does not currently require your attent
             {
                 // Delete any workflow activities of this type
                 int loopCounter = 0;
-                foreach ( var activity in workflowActivityService.Queryable().Where( a => a.ActivityTypeId == activityType.Id ) )
+                foreach ( var activity in workflowActivityService.Queryable().Where( a => a.ActivityTypeId == activityType.Id ).ToList() )
                 {
                     workflowActivityService.Delete( activity );
                     loopCounter++;
@@ -2004,7 +2004,7 @@ This {{ Workflow.WorkflowType.WorkTerm }} does not currently require your attent
 
             // Delete any of those attributes that were removed in the UI
             var selectedAttributeGuids = attributes.Select( a => a.Guid );
-            foreach ( var attr in existingAttributes.Where( a => !selectedAttributeGuids.Contains( a.Guid ) ) )
+            foreach ( var attr in existingAttributes.Where( a => !selectedAttributeGuids.Contains( a.Guid ) ).ToList() )
             {
                 attributeService.Delete( attr );
                 rockContext.SaveChanges();
