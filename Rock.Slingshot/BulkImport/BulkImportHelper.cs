b3325b9ea6a1fb79c52ce50847d5d4a2bd3ff62a
Mike Peterson <mike@sparkabilitygroup.com>
2017-05-18 15:41:53 -0700

    - Fix BulkImport only putting one address for families that had the same address as their work and home address

--- Rock.Slingshot/BulkImport/BulkImportHelper.cs
+++ Rock.Slingshot/BulkImport/BulkImportHelper.cs
@@ -733,7 +733,7 @@ WHERE gta.GroupTypeId IS NULL" );
             foreach ( var personImport in personImports )
             {
                 progress++;
-                if ( progress % 100 == 0 )
+                if ( progress % 100 == 0 && personUpdatesMS > 0)
                 {
                     if ( initiatedWithWebRequest && HttpContext.Current?.Response?.IsClientConnected != true )
                     {
@@ -945,7 +945,7 @@ WHERE gta.GroupTypeId IS NULL" );
             foreach ( var familyRecord in personsIdsForPersonImport.GroupBy( a => a.FamilyId ) )
             {
                 // get the distinct addresses for each family in our import
-                var familyAddresses = familyRecord.Where( a => a.PersonImport?.Addresses != null ).SelectMany( a => a.PersonImport.Addresses ).DistinctBy( a => new { a.Street1, a.Street2, a.City, a.County, a.State, a.Country, a.PostalCode } );
+                var familyAddresses = familyRecord.Where( a => a.PersonImport?.Addresses != null ).SelectMany( a => a.PersonImport.Addresses ).DistinctBy( a => new { a.GroupLocationTypeValueId, a.Street1, a.Street2, a.City, a.County, a.State, a.Country, a.PostalCode } );
 
                 foreach ( var address in familyAddresses )
                 {
