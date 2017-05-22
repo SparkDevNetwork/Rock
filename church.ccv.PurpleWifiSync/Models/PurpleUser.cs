1905bbce3656727b0092fdf7693b81f6ba57d316
jeredmcferron@gmail.com <jeredmcferron@gmail.com>
2017-05-22 16:57:42 -0700

    Model for the purple wifi lookup table

---
+++ church.ccv.PurpleWifiSync/Models/PurpleUser.cs
@@ -0,0 +1,17 @@
+﻿using Rock.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace church.ccv.CCVPurpleWifiSync.Model
+{
+        [Table( "_church_ccv_PurpleWifiSync_PurpleUser" )]
+        [DataContract]
+        public class PurpleUser : Model<PurpleUser>, IRockEntity
+        {
+            [DataMember]
+            public int PurpleId { get; set; }
+
+            [DataMember]
+            public int PersonAliasId { get; set; }
+        }
+}
