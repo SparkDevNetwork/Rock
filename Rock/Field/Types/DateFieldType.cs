b09adc3a5cfd03f4f4e894ad2b4626072f9f6c8a
Mike Peterson <mike@sparkabilitygroup.com>
2017-05-31 14:42:54 -0700

    - Fix DateTimeFieldType not working after adding Control Type option to DateFieldType

--- Rock/Field/Types/DateFieldType.cs
+++ Rock/Field/Types/DateFieldType.cs
@@ -72,6 +72,26 @@ namespace Rock.Field.Types
         }
 
         /// <summary>
+        /// The DateFormat configuration control
+        /// </summary>
+        protected RockTextBox _tbDateFormat;
+
+        /// <summary>
+        /// The 'Display as Elapsed Time' configuration control
+        /// </summary>
+        protected RockCheckBox _cbDisplayDiff;
+        
+        /// <summary>
+        /// The Date Picker Control Type configuration control
+        /// </summary>
+        protected RockDropDownList _ddlDatePickerMode;
+        
+        /// <summary>
+        /// The Display Current configuration control
+        /// </summary>
+        protected RockCheckBox _cbDisplayCurrent;
+
+        /// <summary>
         /// Creates the HTML controls required to configure this type of field
         /// </summary>
         /// <returns></returns>
@@ -79,34 +99,34 @@ namespace Rock.Field.Types
         {
             var controls = base.ConfigurationControls();
 
-            var tbDateFormat = new RockTextBox();
-            controls.Add( tbDateFormat );
-            tbDateFormat.Label = "Date Format";
-            tbDateFormat.Help = "The format string to use for date (default is system short date).";
+            _tbDateFormat = new RockTextBox();
+            controls.Add( _tbDateFormat );
+            _tbDateFormat.Label = "Date Format";
+            _tbDateFormat.Help = "The format string to use for date (default is system short date).";
 
-            var cbDisplayDiff = new RockCheckBox();
-            controls.Add( cbDisplayDiff );
-            cbDisplayDiff.Label = "Display as Elapsed Time";
-            cbDisplayDiff.Text = "Yes";
-            cbDisplayDiff.Help = "Display value as an elapsed time.";
+            _cbDisplayDiff = new RockCheckBox();
+            controls.Add( _cbDisplayDiff );
+            _cbDisplayDiff.Label = "Display as Elapsed Time";
+            _cbDisplayDiff.Text = "Yes";
+            _cbDisplayDiff.Help = "Display value as an elapsed time.";
 
-            var ddlDatePickerMode = new RockDropDownList();
-            controls.Add( ddlDatePickerMode );
-            ddlDatePickerMode.Items.Clear();
-            ddlDatePickerMode.Items.Add( new ListItem( "Date Picker", DatePickerControlType.DatePicker.ConvertToString() ) );
-            ddlDatePickerMode.Items.Add( new ListItem( "Date Parts Picker", DatePickerControlType.DatePartsPicker.ConvertToString() ) );
-            ddlDatePickerMode.Label = "Control Type";
-            ddlDatePickerMode.Help = "Select 'Date Picker' to use a DatePicker, or 'Date Parts Picker' to select Month, Day and Year individually";
-            ddlDatePickerMode.AutoPostBack = true;
-            ddlDatePickerMode.SelectedIndexChanged += OnQualifierUpdated;
+            _ddlDatePickerMode = new RockDropDownList();
+            controls.Add( _ddlDatePickerMode );
+            _ddlDatePickerMode.Items.Clear();
+            _ddlDatePickerMode.Items.Add( new ListItem( "Date Picker", DatePickerControlType.DatePicker.ConvertToString() ) );
+            _ddlDatePickerMode.Items.Add( new ListItem( "Date Parts Picker", DatePickerControlType.DatePartsPicker.ConvertToString() ) );
+            _ddlDatePickerMode.Label = "Control Type";
+            _ddlDatePickerMode.Help = "Select 'Date Picker' to use a DatePicker, or 'Date Parts Picker' to select Month, Day and Year individually";
+            _ddlDatePickerMode.AutoPostBack = true;
+            _ddlDatePickerMode.SelectedIndexChanged += OnQualifierUpdated;
 
-            var cbDisplayCurrent = new RockCheckBox();
-            controls.Add( cbDisplayCurrent );
-            cbDisplayCurrent.AutoPostBack = true;
-            cbDisplayCurrent.CheckedChanged += OnQualifierUpdated;
-            cbDisplayCurrent.Label = "Display Current Option";
-            cbDisplayCurrent.Text = "Yes";
-            cbDisplayCurrent.Help = "Include option to specify value as the current date.";
+            _cbDisplayCurrent = new RockCheckBox();
+            controls.Add( _cbDisplayCurrent );
+            _cbDisplayCurrent.AutoPostBack = true;
+            _cbDisplayCurrent.CheckedChanged += OnQualifierUpdated;
+            _cbDisplayCurrent.Label = "Display Current Option";
+            _cbDisplayCurrent.Text = "Yes";
+            _cbDisplayCurrent.Help = "Include option to specify value as the current date.";
 
             return controls;
         }
