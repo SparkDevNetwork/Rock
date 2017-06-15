6c1c5f1e4d69a61e2f8d9d13a77491cf535fcea6
Mike Peterson <mike@sparkabilitygroup.com>
2017-06-14 18:00:17 -0700

    - Refactored email-editor to JQuery pattern

--- RockWeb/Scripts/email-editor.js
+++ RockWeb/Scripts/email-editor.js
@@ -1,124 +1,122 @@
-var initJquery = function ()
+(function ($)
 {
-  loadScript("./Scripts/dragula.min.js", initEditor);
+  'use strict';
+  window.Rock = window.Rock || {};
+  Rock.controls = Rock.controls || {};
 
-  // wire up the clicking on components to call the 
-  // parent window to handle the properties editing
-  $(document).on('click', '.component', function (e)
+  Rock.controls.emailEditor = (function ()
   {
-    if ($(this).attr('data-state') == 'component') {
-      handleComponentClick(this, e);
-    }
-  });
-};
+    var exports = {
+      initialize: function (options)
+      {
+        if (!options.id) {
+          throw 'id is required';
+        }
 
-var handleComponentClick = function (el, e)
-{
-  e.preventDefault(); // prevent things like links from taking you off the page
+        var self = this;
 
-  e.stopImmediatePropagation(); // prevent event from bubbling up to parents
-  var componentType = "";
+        // configure and load the dragula script
+        var drake = dragula([document.getElementById('editor-toolbar'), document.querySelector('.dropzone')], {
+          isContainer: function (el)
+          {
+            return el.classList.contains('dropzone');
+          },
+          copy: function (el, source)
+          {
+            return source === document.getElementById('editor-toolbar')
+          },
+          accepts: function (el, target)
+          {
+            return target !== document.getElementById('editor-toolbar')
+          },
+          ignoreInputTextSelection: true
+        })
+        .on('drag', function (el)
+        {
+          if ($(el).attr('data-state') == 'component') {
+            self.handleComponentClick(el, event);
+          }
+          $('body').addClass('state-drag');
+        })
+        .on('dragend', function (el)
+        {
+          $('body').removeClass('state-drag');
+        })
+        .on('drop', function (el)
+        {
+          if ($(el).attr('data-state') == 'template') {
+            // replace the template contents
+            el.innerHTML = $(el).attr('data-content');
+            $(el).attr('data-state', 'component');
+            el.removeAttribute("data-content");
+          }
+        });
 
-  var classNames = $(el).attr("class").toString().split(' ');
-  $.each(classNames, function (i, className)
-  {
-    if (className.indexOf('component-') == 0) {
-      componentParts = className.split('-');
-      componentType = componentParts[1];
-    }
-  });
+        // add autoscroll capabilities during dragging
+        $(window).mousemove(function (e)
+        {
+          if (drake.dragging) {
+            var $iframeEl = $(window.frameElement);
+            var $parentWindow = $(this.parent.window);
+            var clientY = e.clientY + $iframeEl.offset().top;
+            var parentWindowHeight = $parentWindow.height();
+            var scrollLevel = $parentWindow.scrollTop();
+            var mousePositionProportion = (clientY - scrollLevel) / parentWindowHeight;
 
-  // clear the class noting that a component is selected
-  $('.component.selected').removeClass('selected');
+            if (mousePositionProportion > .90) {
+              scrollLevel += 20;
+              $parentWindow.scrollTop(scrollLevel);
+            }
+            else if (mousePositionProportion < .10 && scrollLevel != 0) {
+              scrollLevel -= 20;
+              $parentWindow.scrollTop(scrollLevel);
+            }
+          }
+        });
 
-  // add selected class to this component
-  $(el).addClass('selected');
+        this.initializeEventHandlers();
+      },
+      initializeEventHandlers: function ()
+      {
+        var self = this;
+        // wire up the clicking on components to call the 
+        // parent window to handle the properties editing
+        $(document).on('click', '.component', function (e)
+        {
+          if ($(this).attr('data-state') == 'component') {
+            self.handleComponentClick(this, e);
+          }
+        });
+      },
+      handleComponentClick: function (el, e)
+      {
+        e.preventDefault(); // prevent things like links from taking you off the page
 
-  parent.loadPropertiesPage(componentType, $(el));
-};
+        e.stopImmediatePropagation(); // prevent event from bubbling up to parents
+        var componentType = "";
 
-var initEditor = function ()
-{
+        var classNames = $(el).attr("class").toString().split(' ');
+        $.each(classNames, function (i, className)
+        {
+          if (className.indexOf('component-') == 0) {
+            var componentParts = className.split('-');
+            componentType = componentParts[1];
+          }
+        });
 
-  // configure and load the dragula script
-  var drake = dragula([document.getElementById('editor-toolbar'), document.querySelector('.dropzone')], {
-    isContainer: function (el)
-    {
-      return el.classList.contains('dropzone');
-    },
-    copy: function (el, source)
-    {
-      return source === document.getElementById('editor-toolbar')
-    },
-    accepts: function (el, target)
-    {
-      return target !== document.getElementById('editor-toolbar')
-    },
-    ignoreInputTextSelection: true
-  })
-  .on('drag', function (el)
-  {
-    if ($(el).attr('data-state') == 'component') {
-      handleComponentClick(el, event);
-    }
-    $('body').addClass('state-drag');
-  })
-  .on('dragend', function (el)
-  {
-    $('body').removeClass('state-drag');
-  })
-  .on('drop', function (el)
-  {
-    if ($(el).attr('data-state') == 'template') {
-      // replace the template contents
-      el.innerHTML = $(el).attr('data-content');
-      $(el).attr('data-state', 'component');
-      el.removeAttribute("data-content");
-    }
-  });
+        // clear the class noting that a component is selected
+        $('.component.selected').removeClass('selected');
 
-  // add autoscroll capabilities during dragging
-  $(window).mousemove(function (e)
-  {
-    if (drake.dragging) {
-      var $iframeEl = $(window.frameElement);
-      var $parentWindow =$(this.parent.window);
-      var clientY = e.clientY + $iframeEl.offset().top;
-      var parentWindowHeight = $parentWindow.height();
-      var scrollLevel = $parentWindow.scrollTop();
-      var mousePositionProportion = (clientY - scrollLevel) / parentWindowHeight;
+        // add selected class to this component
+        $(el).addClass('selected');
 
-      if (mousePositionProportion > .90 ) {
-        scrollLevel += 20;
-        $parentWindow.scrollTop(scrollLevel);
+        parent.loadPropertiesPage(componentType, $(el));
       }
-      else if (mousePositionProportion < .10 && scrollLevel != 0) {
-        scrollLevel -= 20;
-        $parentWindow.scrollTop(scrollLevel);
-      }
-    }
-  });
-};
+    };
 
-// load dependencies
-// note dragula will be loaded by the jquery loader to ensure both are fully loaded before dragula is initiated
-loadScript("./Scripts/jquery-1.12.4.min.js", initJquery);
+    return exports;
+  }());
+}(jQuery));
 
+Rock.controls.emailEditor.initialize({ id: 'editor-toolbar' });
 
-// -- Helper Functions
-function loadScript(url, callback)
-{
-  // Adding the script tag to the head as suggested before
-  var head = document.getElementsByTagName('head')[0];
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = url;
-
-  // Then bind the event to the callback function.
-  // There are several events for cross browser compatibility.
-  script.onreadystatechange = callback;
-  script.onload = callback;
-
-  // Fire the loading
-  head.appendChild(script);
-}
