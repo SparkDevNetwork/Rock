7040e95c031ca365134e3e2e4330f472cf717044
cupshir <christopherupshir@ccvonline.com>
2017-06-29 15:14:32 -0700

    Created Powershell script that syncs the content folder from development server to local workstation. Updated gitignore file to ignore content folder of RockWeb project.

---
+++ RockWeb/Sync-DevContentToLocal.ps1
@@ -0,0 +1,19 @@
+﻿##
+##  Script that will sync the content folder on the Development server
+##     to the content folder on the local computer
+##
+##  TODO: Update the $localRockProjectFolder variable with the location of
+##     the Rock project on your local computer
+##
+
+# Update this variable before running the first time
+$LocalRockProjectFolder = 'D:\github\Rock'
+
+# Development content folder location
+$sourceContentFolder = '\\ccvdev\c$\inetpub\wwwroot\Content'
+
+# Local content folder location
+$destinationContentFolder = $LocalRockProjectFolder + '\RockWeb\Content'
+
+# Command to sync the two locations
+robocopy $sourceContentFolder $destinationContentFolder /MIR
