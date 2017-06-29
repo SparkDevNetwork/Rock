b348ed3004390eef95fe87ece1d21330835f3270
cupshir <christopherupshir@ccvonline.com>
2017-06-29 16:08:13 -0700

    Updated comments to reflect changes

--- RockWeb/Sync-DevContentToLocal.ps1
+++ RockWeb/Sync-DevContentToLocal.ps1
@@ -2,9 +2,6 @@
 ##  Script that will sync the content folder on the Development server
 ##     to the content folder on the local computer
 ##
-##  TODO: Update the $localRockProjectFolder variable with the location of
-##     the Rock project on your local computer
-##
 
 # Development content folder location
 $sourceContentFolder = '\\ccvdev\c$\inetpub\wwwroot\Content'
