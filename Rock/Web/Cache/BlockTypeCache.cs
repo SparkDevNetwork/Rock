779bbb14ec36975dc4ac20c1e6bc3484a5d99f88
Mike Peterson <mike@sparkabilitygroup.com>
2017-07-10 11:16:54 -0700

    - Added IsCommon as a Field on BlockType to signify which blocktypes are commonly used - Migration Rollups

--- Rock/Web/Cache/BlockTypeCache.cs
+++ Rock/Web/Cache/BlockTypeCache.cs
@@ -60,6 +60,14 @@ namespace Rock.Web.Cache
         public bool IsSystem { get; set; }
 
         /// <summary>
+        /// Gets or sets a value indicating whether this blocktype is commonly used
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is common; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsCommon { get; private set; }
+
+        /// <summary>
         /// Gets or sets the path.
         /// </summary>
         /// <value>
@@ -107,6 +115,7 @@ namespace Rock.Web.Cache
         /// The security actions.
         /// </value>
         public ConcurrentDictionary<string, string> SecurityActions { get; set; }
+        
 
         #endregion
 
@@ -148,6 +157,7 @@ namespace Rock.Web.Cache
                 SetCache( blockType.Guid.ToString(), blockType.Id, guidCachePolicy );
 
                 this.IsSystem = blockType.IsSystem;
+                this.IsCommon = blockType.IsCommon;
                 this.Path = blockType.Path;
                 this.Name = blockType.Name;
                 this.Description = blockType.Description;
