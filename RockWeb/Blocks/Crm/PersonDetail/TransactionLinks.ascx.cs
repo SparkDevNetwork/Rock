4da8bc4b3bbd9aff0b31b62b7733932a9dfbd652
Mike Peterson <mike@sparkabilitygroup.com>
2017-07-14 18:04:02 -0700

    - Added new TransactionLinks block that uses the new person token logic

---
+++ RockWeb/Blocks/Crm/PersonDetail/TransactionLinks.ascx.cs
@@ -0,0 +1,107 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.Web.UI;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI;
+
+namespace RockWeb.Blocks.Crm.PersonDetail
+{
+    /// <summary>
+    /// Block for displaying the history of changes to a particular user.
+    /// </summary>
+    [DisplayName( "Person Transaction Links" )]
+    [Category( "CRM > Person Detail" )]
+    [Description( "Block for displaying links to adding and scheduling transactions for a person." )]
+
+    [LinkedPage( "Add Transaction Page", "", true, Rock.SystemGuid.Page.ADD_TRANSACTION, "", 0 )]
+    public partial class TransactionLinks : PersonBlock
+    {
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+        }
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            //
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnAddTransaction control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnAddTransaction_Click( object sender, EventArgs e )
+        {
+            var page = new PageService( new RockContext() ).Get( this.GetAttributeValue( "AddTransactionPage" ).AsGuid() );
+            if ( page != null )
+            {
+                var personKey = this.Person.GetImpersonationToken( DateTime.Now.AddMinutes( 5 ), 1, page.Id );
+                Response.Redirect( string.Format( "~/AddTransaction?Person={0}", personKey ) );
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the btnAddScheduledTransaction control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnAddScheduledTransaction_Click( object sender, EventArgs e )
+        {
+            // just send them the same page as Add Transaction page since you can add a scheduled transaction there either way
+            btnAddTransaction_Click( sender, e );
+        }
+
+        #endregion
+    }
+}
+
