702bed940a6447cb508b7d2be787db95e65b466b
David Turner <david@sparkdevnetwork.org>
2017-07-18 09:51:03 -0700

    - Update the Person Follow Add workflow action to be able to follow any entity type.

--- Rock/Workflow/Action/People/PersonFollowAdd.cs
+++ Rock/Workflow/Action/People/PersonFollowAdd.cs
@@ -26,7 +26,7 @@ using Rock.Attribute;
 using Rock.Data;
 using Rock.Model;
 using Rock.Web.Cache;
-
+using Rock.Field;
 
 namespace Rock.Workflow.Action
 {
@@ -41,7 +41,7 @@ namespace Rock.Workflow.Action
     [WorkflowAttribute( "Person", "Workflow attribute that contains the person who is following the entity.", true, "", "", 0, null,
         new string[] { "Rock.Field.Types.PersonFieldType" } )]
     [EntityTypeField( "Entity Type", "Workflow attribute that contains the entity type to follow.", true, "", 1, "EntityType" )]
-    [WorkflowTextOrAttribute( "Entity To Follow", "Attribute Value", "The Entity Id or an attribute that contains the person or group to follow. <span class='tip tip-lava'></span>", true, "", "", 2, "Entity" )]
+    [WorkflowTextOrAttribute( "Entity To Follow", "Attribute Value", "The Entity Id or Guid or an attribute that contains the entity to follow. <span class='tip tip-lava'></span>", true, "", "", 2, "Entity" )]
     public class PersonFollowAdd : ActionComponent
     {
         /// <summary>
@@ -58,7 +58,6 @@ namespace Rock.Workflow.Action
 
             // get person
             PersonAlias personAlias = null;
-
             string personAttributeValue = GetAttributeValue( action, "Person" );
             Guid? guidPersonAttribute = personAttributeValue.AsGuidOrNull();
             if ( guidPersonAttribute.HasValue )
@@ -95,66 +94,79 @@ namespace Rock.Workflow.Action
             }
 
             //get entity
-            int entityId = 0;
-            string attributeEntity = GetAttributeValue( action, "Entity" ).ResolveMergeFields( GetMergeFields( action ) );
-            int? intEntity = attributeEntity.AsIntegerOrNull();
-            Guid? guidEntity = attributeEntity.AsGuidOrNull();
-            if ( intEntity.HasValue )
+            IEntity entityToFollow = GetEntity( entityType, rockContext, action );
+            if ( entityToFollow != null )
             {
-                entityId = intEntity.Value;
-            }
-            else if ( guidEntity.HasValue )
-            {
-                var attribute = AttributeCache.Read( guidEntity.Value, rockContext );
-                if ( attribute != null )
+                var followingService = new FollowingService( rockContext );
+
+                var following = followingService.Queryable()
+                    .FirstOrDefault( f =>
+                        f.EntityTypeId == entityType.Id &&
+                        f.EntityId == entityToFollow.Id &&
+                        f.PersonAlias.Person.Id == personAlias.PersonId );
+
+                if ( following == null )
                 {
-                    Guid? entityValue = action.GetWorklowAttributeValue( guidEntity.Value ).AsGuidOrNull();
-                    if ( entityValue == null )
-                    {
-                        action.AddLogEntry( "Invalid valid Value for Entity attribute" );
-                        return false;
-                    }
-                    switch ( attribute.FieldType.Class )
-                    {
-                        case "Rock.Field.Types.PersonFieldType":
-                            var person = new PersonAliasService( rockContext ).Queryable()
-                                          .Where( a => a.Guid.Equals( entityValue.Value ) )
-                                          .Select( a => a.Person )
-                                          .FirstOrDefault();
-                            if ( person == null )
-                            {
-                                action.AddLogEntry( "InValid Entity: Person not found" );
-                                return false;
-                            }
-                            entityId = person.Id;
-                            break;
-                        case "Rock.Field.Types.GroupFieldType":
-                            var group = new GroupService( rockContext ).Get( entityValue.Value );
-                            if ( group == null )
-                            {
-                                action.AddLogEntry( "InValid Entity: Group not found" );
-                                return false;
-                            }
-                            entityId = group.Id;
-                            break;
-                    }
+                    following = new Following();
+                    following.EntityTypeId = entityType.Id;
+                    following.EntityId = entityToFollow.Id;
+                    following.PersonAliasId = personAlias.Id;
+                    followingService.Add( following );
+                    rockContext.SaveChanges();
                 }
+
+                return true;
             }
             else
             {
-                action.AddLogEntry( "Invalid valid Value for Entity attribute" );
+                action.AddLogEntry( "Invalid Value for Entity attribute" );
+
                 return false;
             }
 
-            var followingService = new FollowingService( rockContext );
-            var following = new Following();
-            following.EntityTypeId = entityType.Id;
-            following.EntityId = entityId;
-            following.PersonAliasId = personAlias.Id;
-            followingService.Add( following );
-            rockContext.SaveChanges();
+        }
 
-            return true;
+        /// <summary>
+        /// Gets the entity to follow
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <param name="rockContext">The rock context.</param>
+        /// <param name="action">The action.</param>
+        /// <returns></returns>
+        private IEntity GetEntity( EntityTypeCache entityType, RockContext rockContext, WorkflowAction action )
+        {
+            string entityValue = GetAttributeValue( action, "Entity" ).ResolveMergeFields( GetMergeFields( action ) );
+
+            // If an ID was specified, just use that as the entity id to follow
+            int? intEntity = entityValue.AsIntegerOrNull();
+            if ( intEntity.HasValue )
+            {
+                return new EntityTypeService( rockContext ).GetEntity( entityType.Id, intEntity.Value );
+            }
+
+            Guid? guidEntity = entityValue.AsGuidOrNull();
+            if ( guidEntity.HasValue )
+            {
+                // If the value is a Guid, it could either be a guid of an attribute, or the entity's guid.
+                // Check for an attribute first.
+                var attribute = AttributeCache.Read( guidEntity.Value, rockContext );
+                if ( attribute != null )
+                {
+                    // It was for an attribute. That attribute's field type needs to be an IEntityFieldType (like person or group)
+                    var entityFieldType = attribute.FieldType.Field as IEntityFieldType;
+                    if ( entityFieldType != null )
+                    {
+                        return entityFieldType.GetEntity( action.GetWorklowAttributeValue( guidEntity.Value ) );
+                    }
+                }
+                else
+                {
+                    // It was not for an attribute, so it must be the entity's guid
+                    return new EntityTypeService( rockContext ).GetEntity( entityType.Id, guidEntity.Value );
+                }
+            }
+
+            return null;
         }
     }
 }
