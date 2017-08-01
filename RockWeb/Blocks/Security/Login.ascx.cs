9121f5b38799760b580bc27ec530293fa96723d6
Mike Peterson <mike@sparkabilitygroup.com>
2017-07-31 20:05:52 -0700

    - Updated Invalid Token warning messing on Login block

--- RockWeb/Blocks/Security/Login.ascx.cs
+++ RockWeb/Blocks/Security/Login.ascx.cs
@@ -57,7 +57,7 @@ Sorry, your account has been locked.  Please contact our office at {{ 'Global' |
     [CodeEditorField( "Prompt Message", "Optional text (HTML) to display above username and password fields.", CodeEditorMode.Html, CodeEditorTheme.Rock, 100, false, @"", "", 10 )]
     [LinkedPage( "Redirect Page", "Page to redirect user to upon successful login. The 'returnurl' query string will always override this setting for database authenticated logins. Redirect Page Setting will override third-party authentication 'returnurl'.", false, "", "", 11 )]
 
-    [CodeEditorField( "Invalid PersonToken Text", "The text to show when a person is logged out due to an invalid persontoken. <span class='tip tip-lava'></span>.", CodeEditorMode.Html, CodeEditorTheme.Rock, 100, false, @"<div class='alert alert-warning'>Invalid Token Specified</div>", "", 12 )]
+    [CodeEditorField( "Invalid PersonToken Text", "The text to show when a person is logged out due to an invalid persontoken. <span class='tip tip-lava'></span>.", CodeEditorMode.Html, CodeEditorTheme.Rock, 100, false, @"<div class='alert alert-warning'>The login token you provided is no longer valid. Please login below.</div>", "", 12 )]
     public partial class Login : Rock.Web.UI.RockBlock
     {
         #region Base Control Methods
