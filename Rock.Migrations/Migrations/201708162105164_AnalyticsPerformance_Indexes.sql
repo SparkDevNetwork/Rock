46720a76e8fb31aaafc2d0a691bd267bfe7550dd
David Turner <david@sparkdevnetwork.org>
2017-08-16 16:19:26 -0700

    + Improved the performance of Attendance and Giving Analytics reporting.

---
+++ Rock.Migrations/Migrations/201708162105164_AnalyticsPerformance_Indexes.sql
@@ -0,0 +1,9 @@
+﻿CREATE NONCLUSTERED INDEX [IX_AccountId_TransactionId_Amount]
+ON [dbo].[FinancialTransactionDetail] ([AccountId])
+INCLUDE ([TransactionId],[Amount])
+GO
+
+CREATE NONCLUSTERED INDEX [IX_TransactionDateTime_SourceType_AuthorizedPerson_PaymentDetails]
+ON [dbo].[FinancialTransaction] ([TransactionDateTime])
+INCLUDE ([Id],[SourceTypeValueId],[AuthorizedPersonAliasId],[FinancialPaymentDetailId])
+GO
