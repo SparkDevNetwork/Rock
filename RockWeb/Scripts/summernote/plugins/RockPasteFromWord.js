319af72d08106c35c3275b53847da5eaae31022e
Mike Peterson <mike@sparkabilitygroup.com>
2017-08-25 17:22:17 -0700

    - Updated Summernote Rock Plugins so that 'Paste from Word' (and other sources), Direct Paste, and 'Paste from Text' does a much better job of carrying over the source formatting

--- RockWeb/Scripts/summernote/plugins/RockPasteFromWord.js
+++ RockWeb/Scripts/summernote/plugins/RockPasteFromWord.js
@@ -7,37 +7,38 @@
     // remove the summernote clipboard keydown handler since this can cause problems on FF and IE, and we are handling it here anyways
     $(context.layoutInfo.note).off('summernote.keydown');
   
+    // handle all paste events from summernote with this RockPasteFromWord plugin. It will handle non-Word content too.
     $(context.layoutInfo.note).on('summernote.paste', function (we, e)
     {
-        // catch the paste event and do either the rockpastetext or rockpastefromword if we can figure out if they are pasting from word
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-            // if they are using IE. Sorry, they'll have to use the pastefromword button since IE doesn't tell us what type of data is getting pasted
-            e.preventDefault();
-            context.layoutInfo.toolbar.find('.js-rockpastetext').click();
-        } else {
-            var clipboardData = ((typeof (e.originalEvent.clipboardData) != 'undefined' && e.originalEvent.clipboardData) || (typeof (window.clipboardData) != 'undefined' && window.clipboardData));
-            if (clipboardData) {
-                var types = clipboardData.types;
-                if (((types instanceof DOMStringList) && types.contains("text/rtf")) || (types.indexOf && types.indexOf('text/rtf') !== -1)) {
-                    e.preventDefault();
-                    doPasteFromWord(e, "text/html");
-                } else {
-                    // sometimes Microsoft Edge will support the clipboardData api, but doesn't tell us about types other than html/plain
-                    e.preventDefault();
-                    doPasteFromWord(e, "text/plain");
-                }
-
+      // catch the paste event and do the rockpastefromword. 
+      var clipboardData = ((typeof (e.originalEvent.clipboardData) != 'undefined' && e.originalEvent.clipboardData) || (typeof (window.clipboardData) != 'undefined' && window.clipboardData));
+      if (clipboardData) {
+          var types = clipboardData.types;
+          if (types) {
+            // the clipboard has types defined, so we have some idea of what to do with it
+            if (((types instanceof DOMStringList) && types.contains("text/rtf")) || (types.indexOf && types.indexOf('text/rtf') !== -1)) {
+              // text/rtf is one of the types, so it is probably from a word document, but we want the text/html version of it
+              e.preventDefault();
+              doPasteFromWord(e, "text/html");
             } else {
-                // if the browser doesn't have a Clipboard API, always prompt since we don't know what is on the clipboard and we don't want word data (should only happen on MS Edge)
-                // As of 7/15/2016, Microsoft Edge doesn't have ClipboardAPI! see https://developer.microsoft.com/en-us/microsoft-edge/platform/status/clipboardapi
-                e.preventDefault();
-                context.layoutInfo.toolbar.find('.js-rockpastetext').click();
+              // we don't know if this is a word paste, so we'll start with no assumption
+              e.preventDefault();
+              doPasteFromWord(e, "text/plain");
             }
-        }
-    });
+          }
+          else {
+            // we don't know if this is a word paste, and we don't know types (this is probably IE11) so we'll start with no assumption
+            e.preventDefault();
+            doPasteFromWord(e, "text/plain");
+          }
 
+      } else {
+          // if the browser doesn't have a Clipboard API, click the paste word button which will prompt them to paste content into a dialog first if needed
+          e.preventDefault();
+          context.layoutInfo.toolbar.find('.js-rockpastefromword').click();
+      }
+        
+    });
 
     // from https://github.com/StudioJunkyard/summernote-cleaner
     var cleanText = function (txt) {
@@ -90,12 +91,17 @@
         footer: footer
     }).render().appendTo($(document.body));
 
-    $dialog.find('.js-paste-word-btn').on('click', { dialog: $dialog }, function (a) {
+    $dialog.find('.js-paste-word-btn').on('click', { dialog: $dialog }, function (a)
+    {
         var $dialog = a.data.dialog;
         ui.hideDialog($dialog);
 
         context.invoke('editor.restoreRange');
-        var pastedContent = $dialog.find('.js-paste-area').html();
+        var $pasteArea = $dialog.find('.js-paste-area');
+        var pastedContent = $pasteArea.html();
+        
+        // now that we have the pastedContent from the div, remove the html from the dom so that any css doesn't spill over into the doc
+        $pasteArea.html('');
 
         var imgBase64 = /.*src=\"data:image\/([a-zA-Z]*);base64,([^\"]*)\"/g;
         
@@ -111,17 +117,29 @@
           pastedContent = "";
         }
 
-        // if copying from Word into Edge, the html might be a giant glob of stuff but with a StartFragment/EndFragment section that we can grab
+        // if copying from Word, the html might be a giant glob of stuff but with a StartFragment/EndFragment section that we can grab
+        var isWordDocument = /<meta.*content="Word.Document">/g.test(pastedContent);
+        var containsHtmlTags = /<[a-z][\s\S]*>/i.test(pastedContent);
         var wordPastedFragment = /<!--StartFragment-->*[\s\S]*<!--EndFragment-->/g
-        if (!!navigator.userAgent.match(/Edge\/\d+/) && wordPastedFragment.test(pastedContent))
+        var cleanedContent;
+        if (isWordDocument && wordPastedFragment.test(pastedContent)) {
+          // we know we are getting a StartFragment/EndFragment paste from Word, so lets start with the fragment
+          cleanedContent = pastedContent.match(wordPastedFragment)[0];
+        }
+        else if ( containsHtmlTags )
         {
-          pastedContent = pastedContent.match(wordPastedFragment)[0];
+          // the pastedContent seems to contain HTML tags and probably isn't from a Word Document, so let's start with the raw paste 
+          cleanedContent = pastedContent;
+        }
+        else {
+          // just plain text, so convert newlines to html breaks to help it look the same
+          cleanedContent = pastedContent.replace(/(\n)/g, '<br />');
         }
 
-        var cleaned = cleanText(pastedContent).trim();
-        var cleaned = cleanParagraphs(cleaned);
-        if (cleaned && cleaned != '') {
-          context.invoke('editor.pasteHTML', cleaned);
+        cleanedContent = cleanText(cleanedContent).trim();
+        cleanedContent = cleanParagraphs(cleanedContent);
+        if (cleanedContent && cleanedContent != '') {
+          context.invoke('editor.pasteHTML', cleanedContent);
         }
     });
 
