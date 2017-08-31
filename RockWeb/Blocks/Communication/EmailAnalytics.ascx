be5b5c1e9f90b6c396fa35acb0d5bc8cfebeea12
Mike Peterson <mike@sparkabilitygroup.com>
2017-08-31 13:48:50 -0700

    - started on Email Analytics

---
+++ RockWeb/Blocks/Communication/EmailAnalytics.ascx
@@ -0,0 +1,74 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="EmailAnalytics.ascx.cs" Inherits="RockWeb.Blocks.Communication.EmailAnalytics" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <asp:HiddenField ID="hfCommunicationId" runat="server" />
+        <asp:HiddenField ID="hfCommunicationListGroupId" runat="server" />
+        <div class="panel panel-block panel-analytics">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-line-chart"></i>&nbsp;<asp:Literal ID="lTitle" runat="server" Text="Email Analytics" /></h1>
+
+                <div class="panel-labels">
+                </div>
+            </div>
+
+            <div class="panel-body">
+                <canvas id="clicksLineChartCanvas" runat="server" />
+            </div>
+        </div>
+
+
+
+        <script>
+            Sys.Application.add_load(function () {
+                // Workaround for Chart.js not working in IE11 (supposed to be fix in chart.js 2.7)
+                // see https://github.com/chartjs/Chart.js/issues/4633
+                Number.MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || 9007199254740991;
+                Number.MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER || -9007199254740991;
+
+                var linechartCtx = $('#<%=clicksLineChartCanvas.ClientID%>')[0].getContext('2d');
+                var clicksLineChart = new Chart(linechartCtx, {
+                    type: 'line',
+                    data: {
+                        labels: [
+                            new Date('2017-07-30T13:16:18.9843825-07:00'),
+                            new Date('2017-08-01T13:16:18.9843825-07:00'),
+                            new Date('2017-08-02T13:16:18.9843825-07:00'),
+                            new Date('2017-08-03T13:16:18.9843825-07:00'),
+                            new Date('2017-08-04T13:16:18.9843825-07:00'),
+                            new Date('2017-08-05T13:16:18.9843825-07:00'),
+                        ],
+                        datasets: [{
+                            type: 'line',
+                            label: 'dataset 1',
+                            //backgroundColor: 'blue',
+                            //borderColor: 'green',
+                            data: [
+                                100,
+                                200,
+                                300,
+                                100,
+                                50,
+                                400
+                            ]
+                        }],
+                    },
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                                //display: true,
+                                time: {
+                                    unit: 'day',
+                                    //format: 'MM/DD/YYYY HH:mm'
+                                }
+                            }]
+                        }
+                    }
+                    
+
+                });
+            });
+        </script>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Blocks/Communication/EmailAnalytics.ascx.cs
@@ -0,0 +1,146 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using Rock.Attribute;
+using Rock.Web.UI;
+
+namespace RockWeb.Blocks.Communication
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DisplayName( "Email Analytics" )]
+    [Category( "Communication" )]
+    [Description( "Shows a graph of email statistics optionally limited to a specific communication or communication list." )]
+    public partial class EmailAnalytics : RockBlock
+    {
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // TODO: Should we have this local in our repo, and should we think about getting rid of flot?
+            RockPage.AddScriptLink( "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js", false );
+            RockPage.AddScriptLink( "/Scripts/moment.min.js", true );
+
+             // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+             this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+
+            var date = RockDateTime.Now.ToString( "o" );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                ShowCharts();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            ShowCharts();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Shows the charts.
+        /// </summary>
+        public void ShowCharts()
+        {
+            var rockContext = new RockContext();
+            hfCommunicationId.Value = this.PageParameter( "CommunicationId" );
+            hfCommunicationListGroupId.Value = this.PageParameter( "CommunicationListId" );
+
+            int? communicationId = hfCommunicationId.Value.AsIntegerOrNull();
+            int? communicationListGroupId = hfCommunicationListGroupId.Value.AsIntegerOrNull();
+            if ( communicationId.HasValue )
+            {
+                // specific communication specified
+                var communication = new CommunicationService( rockContext ).Get( communicationId.Value );
+                if ( communication != null )
+                {
+                    lTitle.Text = "Email Analytics: " + communication.Name;
+                }
+                else
+                {
+                    // TODO: Invalid Communication specified
+                }
+            }
+            else if ( communicationListGroupId.HasValue )
+            {
+                // specific communicationgroup specified
+                var communicationListGroup = new GroupService( rockContext ).Get( communicationListGroupId.Value );
+                if ( communicationListGroup != null )
+                {
+                    lTitle.Text = "Email Analytics: " + communicationListGroup.Name;
+                }
+                else
+                {
+                    // TODO: Invalid CommunicationGroup specified
+                }
+
+                lTitle.Text = "Email Analytics: " + communicationListGroup.Name;
+            }
+            else
+            {
+                // no specific communication or list specific, so just show overall stats
+                lTitle.Text = "Email Analytics";
+            }
+
+            // TODO
+        }
+
+        #endregion
+    }
+}
