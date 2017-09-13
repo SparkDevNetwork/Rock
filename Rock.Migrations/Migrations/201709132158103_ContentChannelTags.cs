d180923f46e32d1fe4ed0d31a013abe507e50ab2
David Turner <david@sparkdevnetwork.org>
2017-09-13 15:38:47 -0700

    - Redo migration after pull request

--- Rock.Migrations/Migrations/201709132158103_ContentChannelTags.cs
+++ Rock.Migrations/Migrations/201709132158103_ContentChannelTags.cs
@@ -22,7 +22,7 @@ namespace Rock.Migrations
     /// <summary>
     ///
     /// </summary>
-    public partial class ContentChannelItemCategoryTags : Rock.Migrations.RockMigration
+    public partial class ContentChannelTags : Rock.Migrations.RockMigration
     {
         /// <summary>
         /// Operations to be performed during the upgrade process.
