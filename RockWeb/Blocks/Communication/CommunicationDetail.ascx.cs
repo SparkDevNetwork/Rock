df637580ef79c5811e8d055894b725fd475e3c78
Mike Peterson <mike@sparkabilitygroup.com>
2017-10-06 14:43:41 -0700

    - More tweaks to secure the VIEW and EDIT of communications

--- RockWeb/Blocks/Communication/CommunicationDetail.ascx.cs
+++ RockWeb/Blocks/Communication/CommunicationDetail.ascx.cs
@@ -668,7 +668,7 @@ namespace RockWeb.Blocks.Communication
                                 btnDeny.Visible = true;
                                 btnEdit.Visible = true;
                             }
-                            btnCancel.Visible = true;
+                            btnCancel.Visible = communication.IsAuthorized( Rock.Security.Authorization.EDIT, CurrentPerson );
                             break;
                         }
                     case CommunicationStatus.Approved:
@@ -679,7 +679,7 @@ namespace RockWeb.Blocks.Communication
 
 
                             btnCancel.Visible = hasPendingRecipients;
-                            btnCopy.Visible = true;
+                            btnCopy.Visible = communication.IsAuthorized( Rock.Security.Authorization.EDIT, CurrentPerson );
                             break;
                         }
                 }
