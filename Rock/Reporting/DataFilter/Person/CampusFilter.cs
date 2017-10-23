44a2210ac8d1e70f5572373a3a91c9eee1cdc7c6
David Turner <david@sparkdevnetwork.org>
2017-10-23 10:41:13 -0700

    + Added new Campus filters for person data views that allow filtering based only on active campuses (existing filters include inactive campuses).

--- Rock/Reporting/DataFilter/Person/CampusFilter.cs
+++ Rock/Reporting/DataFilter/Person/CampusFilter.cs
@@ -59,6 +59,28 @@ namespace Rock.Reporting.DataFilter.Person
             get { return "Additional Filters"; }
         }
 
+        /// <summary>
+        /// Gets the control class name.
+        /// </summary>
+        /// <value>
+        /// The name of the control class.
+        /// </value>
+        internal virtual string ControlClassName
+        {
+            get { return "js-campus-picker"; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether to include inactive campuses.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [include inactive]; otherwise, <c>false</c>.
+        /// </value>
+        internal virtual bool IncludeInactive
+        {
+            get { return true; }
+        }
+
         #endregion
 
         #region Public Methods
@@ -87,14 +109,14 @@ namespace Rock.Reporting.DataFilter.Person
         /// </value>
         public override string GetClientFormatSelection( Type entityType )
         {
-            return @"
-function() {
-    var campusPicker = $('.js-campus-picker', $content);
+            return string.Format( @"
+function() {{
+    var campusPicker = $('.{0}', $content);
     var campusName = $(':selected', campusPicker).text();
 
     return 'Campus: ' + campusName;
-}
-";
+}}
+", ControlClassName );
         }
 
         /// <summary>
@@ -130,8 +152,8 @@ function() {
             CampusPicker campusPicker = new CampusPicker();
             campusPicker.ID = filterControl.ID + "_0";
             campusPicker.Label = string.Empty;
-            campusPicker.CssClass = "js-campus-picker";
-            campusPicker.Campuses = CampusCache.All();
+            campusPicker.CssClass = $"{ControlClassName}";
+            campusPicker.Campuses = CampusCache.All( IncludeInactive );
 
             filterControl.Controls.Add( campusPicker );
 
