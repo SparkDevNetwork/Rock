90aec34e7fc999596dcab714523c48d0fbddbbe9
David Turner <david@sparkdevnetwork.org>
2017-12-05 16:56:33 -0700

    - Updated migration to add the snapchat attribute

--- Rock.Migrations/Migrations/201712051648183_AddSocialFieldType.cs
+++ Rock.Migrations/Migrations/201712051648183_AddSocialFieldType.cs
@@ -14,6 +14,7 @@
 // limitations under the License.
 // </copyright>
 //
+
 namespace Rock.Migrations
 {
     using System;
@@ -31,19 +32,20 @@ namespace Rock.Migrations
         {
             // Attrib for BlockType: Person Bio:Social Media Category
             RockMigrationHelper.UpdateBlockTypeAttribute( "0F5922BB-CD68-40AC-BF3C-4AAB1B98760C", "309460EF-0CC5-41C6-9161-B3837BA3D374", "Social Media Category", "SocialMediaCategory", "", "The Attribute Category to display attributes from", 14, @"DD8F467D-B83C-444F-B04C-C681167046A1", "FD51EC2E-D660-4B79-95C7-39214D4BEA8E" );
-
     
             RockMigrationHelper.UpdateFieldType( "Social Media Account", "Used to configure and display the social Network accounts.", "Rock", "Rock.Field.Types.SocialMediaAccountFieldType", Rock.SystemGuid.FieldType.SOCIAL_MEDIA_ACCOUNT );
+
             Sql( string.Format( @"
-            DECLARE @socialAccountFieldType int
-                SET @socialAccountFieldType = ( SELECT [Id] FROM [FieldType] WHERE [Guid] = '{0}')
-                UPDATE [Attribute]
-                SET [FieldTypeId] = @socialAccountFieldType
-                WHERE [Guid] in ('{1}','{2}','{3}')", 
-                Rock.SystemGuid.FieldType.SOCIAL_MEDIA_ACCOUNT, 
-                Rock.SystemGuid.Attribute.PERSON_FACEBOOK,
-                Rock.SystemGuid.Attribute.PERSON_TWITTER,
-                Rock.SystemGuid.Attribute.PERSON_INSTAGRAM ) );
+    DECLARE @socialAccountFieldType int = ( SELECT [Id] FROM [FieldType] WHERE [Guid] = '{0}' )
+    UPDATE [Attribute] SET 
+        [IconCssClass] = '',
+        [FieldTypeId] = @socialAccountFieldType
+    WHERE [Guid] in ('{1}','{2}','{3}')
+",          Rock.SystemGuid.FieldType.SOCIAL_MEDIA_ACCOUNT, 
+            Rock.SystemGuid.Attribute.PERSON_FACEBOOK,
+            Rock.SystemGuid.Attribute.PERSON_TWITTER, 
+            Rock.SystemGuid.Attribute.PERSON_INSTAGRAM ) );
+
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_FACEBOOK, "name", "Facebook", "ECFCD6FC-6E2C-40CC-8F60-DFA256C29C7A" );
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_FACEBOOK, "iconcssclass", "fa fa-facebook", "FC271703-58E5-45EC-8A48-B0D5B58C606A" );
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_FACEBOOK, "color", "#44619d", "4FF8C097-9C2D-4022-BF83-A110A1DEE56C" );
@@ -62,6 +64,7 @@ namespace Rock.Migrations
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_INSTAGRAM, "texttemplate", "<a href=''{{value}}'' target=''_blank''>{{ value | Url:''segments'' | Last }}</a>", "02820F4F-476A-448F-A869-14206625670C" );
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_INSTAGRAM, "baseurl", "http://www.instagram.com/", "A00E1DE5-F37F-4CBE-8FE4-F89CBC1AE055" );
 
+            RockMigrationHelper.UpdatePersonAttribute( Rock.SystemGuid.FieldType.SOCIAL_MEDIA_ACCOUNT, Rock.SystemGuid.Category.PERSON_ATTRIBUTES_SOCIAL, "Snapchat", "Snapchat", "", "Link to person's Snapchat page", 3, "", Rock.SystemGuid.Attribute.PERSON_SNAPCHAT );
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_SNAPCHAT, "name", "Snapchat", "19283998-F3D7-41A1-B9F5-36FE17CC4566" );
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_SNAPCHAT, "iconcssclass", "fa fa-snapchat-ghost", "E9168011-2719-40EB-A082-9337B5F52233" );
             RockMigrationHelper.UpdateAttributeQualifier( Rock.SystemGuid.Attribute.PERSON_SNAPCHAT, "color", "#FFFC00", "C396FB2E-E38B-4D9E-9DFC-3BC9F2D04C9A" );
