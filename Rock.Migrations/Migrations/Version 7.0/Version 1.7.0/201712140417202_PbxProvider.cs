ea5cf742bc105313e4f9a8b7ca89fd3e09752a85
Mike Peterson <mike@sparkabilitygroup.com>
2017-12-15 17:20:40 -0700

    - Added new FontAwesomeConfiguration block to Pbx Migration

--- Rock.Migrations/Migrations/Version 7.0/Version 1.7.0/201712140417202_PbxProvider.cs
+++ Rock.Migrations/Migrations/Version 7.0/Version 1.7.0/201712140417202_PbxProvider.cs
@@ -47,7 +47,14 @@ namespace Rock.Migrations
             // Attrib Value for Block:Components, Attribute:Support Ordering Page: Phone Systems, Site: Rock RMS              
             RockMigrationHelper.AddBlockAttributeValue("231B1FBC-7399-4453-A012-66E9A2783DE3","A4889D7B-87AA-419D-846C-3E618E79D875",@"True");
             // Attrib Value for Block:Components, Attribute:core.CustomGridColumnsConfig Page: Phone Systems, Site: Rock RMS              
-            RockMigrationHelper.AddBlockAttributeValue("231B1FBC-7399-4453-A012-66E9A2783DE3","DACB8566-FD46-4905-BCAD-460BEECCFF5A",@"");  
+            RockMigrationHelper.AddBlockAttributeValue("231B1FBC-7399-4453-A012-66E9A2783DE3","DACB8566-FD46-4905-BCAD-460BEECCFF5A",@"");
+
+            // Font Awesome Page/Block
+            RockMigrationHelper.AddPage( true, "B4A24AB7-9369-4055-883F-4F4892C39AE3", "D65F783D-87A9-4CC9-8110-E83466A0EADB", "Font Awesome Settings", "", "BB2AF2B3-6D06-48C6-9895-EDF2BA254533", "fa fa-flag" ); // Site:Rock RMS
+            RockMigrationHelper.UpdateBlockType( "Font Awesome Settings", "Block that can be used to configure Font Awesome", "~/Blocks/Cms/FontAwesomeSettings.ascx", "CMS", "5150C021-3BA5-4CCC-B943-52AABD49D481" );
+            // Add Block to Page: Font Awesome Settings, Site: Rock RMS
+            RockMigrationHelper.AddBlock( true, "BB2AF2B3-6D06-48C6-9895-EDF2BA254533", "", "5150C021-3BA5-4CCC-B943-52AABD49D481", "Font Awesome Settings", "Main", @"", @"", 0, "F820E008-7754-4A92-ADB9-80D1BF662E2B" );
+
         }
 
         /// <summary>
@@ -55,6 +62,11 @@ namespace Rock.Migrations
         /// </summary>
         public override void Down()
         {
+            // Remove Block: Font Awesome Settings, from Page: Font Awesome Settings, Site: Rock RMS
+            RockMigrationHelper.DeleteBlock( "F820E008-7754-4A92-ADB9-80D1BF662E2B" );
+            RockMigrationHelper.DeleteBlockType( "5150C021-3BA5-4CCC-B943-52AABD49D481" ); // Font Awesome Settings
+            RockMigrationHelper.DeletePage( "BB2AF2B3-6D06-48C6-9895-EDF2BA254533" ); //  Page: Font Awesome Settings, Layout: Full Width, Site: Rock RMS
+
             // Attrib for BlockType: Person Bio:Enable Call Origination              
             RockMigrationHelper.DeleteAttribute("5E066824-0ECE-46D1-B1B3-98E9D8D2FD5B");
 
