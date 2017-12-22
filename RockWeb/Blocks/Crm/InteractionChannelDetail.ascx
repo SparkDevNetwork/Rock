35fb4cc42cc9efcb215f7edc970b8db98d24cb34
Shivam <shivpat.1990@gmail.com>
2017-12-22 23:40:34 +0530

    Added Interaction Channel List and Detail to list, display and update the content Channel

---
+++ RockWeb/Blocks/Crm/InteractionChannelDetail.ascx
@@ -0,0 +1,90 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="InteractionChannelDetail.ascx.cs" Inherits="RockWeb.Blocks.Crm.InteractionChannelDetail" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlDetails" CssClass="panel panel-block" runat="server">
+            <asp:HiddenField ID="hfChannelId" runat="server" />
+            <div class="panel-heading">
+                <h1 class="panel-title">
+                    <i class="fa fa-random"></i>
+                    <asp:Literal ID="lTitle" runat="server" />
+                </h1>
+            </div>
+            <div class="panel-body">
+                <Rock:NotificationBox ID="nbWarningMessage" runat="server" NotificationBoxType="Warning" Visible="false" />
+                <fieldset id="fieldsetViewDetails" runat="server">
+
+                    <asp:Literal ID="lContent" runat="server"></asp:Literal>
+
+                    <div class="actions">
+                        <asp:LinkButton ID="btnEdit" runat="server" AccessKey="m" ToolTip="Alt+m" Text="Edit" CssClass="btn btn-primary" OnClick="btnEdit_Click" CausesValidation="false" />
+                        <Rock:ModalAlert ID="mdDeleteWarning" runat="server" />
+                        <asp:LinkButton ID="btnDelete" runat="server" Text="Delete" CssClass="btn btn-link" OnClick="btnDelete_Click" CausesValidation="false" />
+                    </div>
+                </fieldset>
+                <div id="pnlEditDetails" runat="server">
+
+                    <asp:ValidationSummary ID="valChannel" runat="server" HeaderText="Please Correct the Following" CssClass="alert alert-danger" />
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <Rock:DataTextBox ID="tbName" runat="server"
+                                SourceTypeName="Rock.Model.InteractionChannel, Rock" PropertyName="Name" />
+                        </div>
+                        <div class="col-md-6">
+                            <Rock:NumberBox ID="nbRetentionDuration" runat="server" Label="Retention Duration" NumberType="Integer" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceChannelList" Visible="True" runat="server" Label="Channel List Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the Interactions block when viewing channel list." />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceChannelDetail" Visible="True" runat="server" Label="Channel Detail Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the Interaction Channel Details block when viewing a interaction channel. This allows you to customize the layout of a channel." />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceComponentList" Visible="True" runat="server" Label="Component List Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the block when viewing component list." />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceComponentDetail" Visible="True" runat="server" Label="Component Detail Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the Interaction Component Details block when viewing a interaction component. This allows you to customize the layout of a component." />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceSessionList" Visible="True" runat="server" Label="Session List Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the block when viewing session list." />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceInteractionList" Visible="True" runat="server" Label="Interaction List Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the block when viewing interaction list." />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <Rock:CodeEditor ID="ceInteractionDetail" Visible="True" runat="server" Label="Interaction Detail Lava Template" EditorMode="Lava" EditorHeight="275"
+                                Help="This Lava template will be used by the Interaction Details block when viewing a interaction. This allows you to customize the layout of a interaction." />
+                        </div>
+                    </div>
+
+                    <div class="actions">
+                        <asp:LinkButton ID="btnSave" runat="server" AccessKey="s" ToolTip="Alt+s" Text="Save" CssClass="btn btn-primary" OnClick="btnSave_Click" />
+                        <asp:LinkButton ID="btnCancel" runat="server" AccessKey="c" ToolTip="Alt+c" Text="Cancel" CssClass="btn btn-link" CausesValidation="false" OnClick="btnCancel_Click" />
+                    </div>
+
+                </div>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Blocks/Crm/InteractionChannelList.ascx.cs
@@ -0,0 +1,239 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Crm
+{
+    /// <summary>
+    /// List all the Interaction Channel.
+    /// </summary>
+    [DisplayName( "Interaction Channel List" )]
+    [Category( "CRM" )]
+    [Description( "List all the Interaction Channel" )]
+
+    [LinkedPage( "Session List Page", "Page reference to the session list page. This will be included as a variable in the Lava.", false, order: 0 )]
+    [LinkedPage( "Component List Page", "Page reference to the component list page. This will be included as a variable in the Lava.", false, order: 1 )]
+    [CodeEditorField( "Default Template", "The Lava template to use as default.", Rock.Web.UI.Controls.CodeEditorMode.Lava, Rock.Web.UI.Controls.CodeEditorTheme.Rock, 300, order: 2, defaultValue: @"
+{% if InteractionChannel != null and InteractionChannel != '' %}
+
+                    {% if InteractionChannel.UsesSession == false and ComponentListPage != ''  %}
+                        <a href = '{{ ComponentListPage }}?channelId={{ InteractionChannel.Id }}' >
+                    {% elseif InteractionChannel.UsesSession == true and SessionListPage != ''  %}
+                        <a href = '{{ SessionListPage }}?channelId={{ InteractionChannel.Id }}' >
+                    {% endif %}
+                    <div class='row'>
+                        <div class='col-md-6'>
+                            {% if InteractionChannel.Name != '' %}
+                                <dl>
+                               <dt>Name</dt>
+                               <dd>{{ InteractionChannel.Name }}<dd/>
+                               </dl>
+                            {% endif %}
+                            {% if InteractionChannel.RetentionDuration != '' %}
+                                <dl>
+                               <dt>Retention Duration</dt
+                               <dd>{{ InteractionChannel.RetentionDuration }}<dd/>
+                            </dl>
+                            {% endif %}
+                        </div>
+                        <div class='col-md-6'>
+                            {% if InteractionChannel.ChannelTypeMediumValue != null and InteractionChannel.ChannelTypeMediumValue != '' %}
+                            <dl>
+                               <dt>Name</dt
+                               <dd>{{ InteractionChannel.ChannelTypeMediumValue.Value }}<dd/>
+                            </dl>
+                            {% endif %}
+                        </div>
+                    </div>
+                    {% if (InteractionChannel.UsesSession == false and ComponentListPage != '') or (InteractionChannel.UsesSession == true and SessionListPage != '') %}
+                        </a>
+                    {% endif %}
+{% endif %}" )]
+    public partial class InteractionChannelList : Rock.Web.UI.RockBlock
+    {
+        #region Fields
+
+        private const string MEDIUM_TYPE_FILTER = "Medium Type";
+
+        #endregion
+
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                BindFilter();
+                ShowList();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        // handlers called by the controls on your block
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            ShowList();
+        }
+
+        /// <summary>
+        /// Handles the Apply Filter event for the GridFilter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void gfFilter_ApplyFilterClick( object sender, EventArgs e )
+        {
+            gfFilter.SaveUserPreference( MEDIUM_TYPE_FILTER, ddlMediumValue.SelectedValue );
+            ShowList();
+        }
+
+        /// <summary>
+        /// Handles displaying the stored filter values.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e as DisplayFilterValueArgs (hint: e.Key and e.Value).</param>
+        protected void gfFilter_DisplayFilterValue( object sender, GridFilter.DisplayFilterValueArgs e )
+        {
+
+            switch ( e.Key )
+            {
+                case "Medium Type":
+                    var mediumTypeValueId = e.Value.AsIntegerOrNull();
+                    if ( mediumTypeValueId.HasValue )
+                    {
+                        var mediumTypeValue = DefinedValueCache.Read( mediumTypeValueId.Value );
+                        e.Value = mediumTypeValue.Value;
+                    }
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Binds the filter.
+        /// </summary>
+        private void BindFilter()
+        {
+            var definedType = DefinedTypeCache.Read( Rock.SystemGuid.DefinedType.INTERACTION_CHANNEL_MEDIUM.AsGuid() );
+            ddlMediumValue.BindToDefinedType( definedType, true );
+
+            var channelMediumValueId = gfFilter.GetUserPreference( MEDIUM_TYPE_FILTER ).AsIntegerOrNull();
+            ddlMediumValue.SetValue( channelMediumValueId );
+        }
+
+        /// <summary>
+        /// Shows the list.
+        /// </summary>
+        public void ShowList()
+        {
+
+            var rockContext = new RockContext();
+
+            InteractionChannelService interactionChannelService = new InteractionChannelService( rockContext );
+            var interactionChannels = interactionChannelService.Queryable();
+
+            var channelMediumValueId = gfFilter.GetUserPreference( MEDIUM_TYPE_FILTER ).AsIntegerOrNull();
+            if ( channelMediumValueId.HasValue )
+            {
+                interactionChannels = interactionChannels.Where( a => a.ChannelTypeMediumValueId == channelMediumValueId.Value );
+            }
+
+            var channelItems = new List<ChannelItem>();
+            foreach ( var interacationChannel in interactionChannels )
+            {
+                if ( !interacationChannel.IsAuthorized( Authorization.VIEW, CurrentPerson ) )
+                {
+                    continue;
+                }
+                string template = GetAttributeValue( "DefaultTemplate" );
+                var mergeFields = Rock.Lava.LavaHelper.GetCommonMergeFields( this.RockPage, this.CurrentPerson );
+                mergeFields.AddOrIgnore( "Person", CurrentPerson );
+                mergeFields.Add( "ComponentListPage", LinkedPageRoute( "ComponentListPage" ) );
+                mergeFields.Add( "SessionListPage", LinkedPageRoute( "SessionListPage" ) );
+                mergeFields.Add( "InteractionChannel", interacationChannel );
+                if ( !string.IsNullOrEmpty( interacationChannel.ChannelListTemplate ) )
+                {
+                    template = interacationChannel.ChannelListTemplate;
+                }
+                var html = template.ResolveMergeFields( mergeFields );
+                channelItems.Add( new ChannelItem
+                {
+                    Id = interacationChannel.Id,
+                    ChannelHtml = html
+                } );
+            }
+
+
+            rptChannel.DataSource = channelItems;
+            rptChannel.DataBind();
+        }
+
+        #endregion
+
+    }
+
+    public class ChannelItem
+    {
+        public int Id { get; set; }
+        public string ChannelHtml { get; set; }
+    }
+}
