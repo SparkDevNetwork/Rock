85036c51f2d31f15fe9d7627bf5ea5eedf882bac
Mike Peterson <mike@sparkabilitygroup.com>
2018-01-04 17:18:33 -0700

    - Added Update Persisted Dataviews Job

--- Rock.Migrations/Migrations/201801032335306_DataViewPersisted.cs
+++ Rock.Migrations/Migrations/201801032335306_DataViewPersisted.cs
@@ -18,7 +18,7 @@ namespace Rock.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     /// <summary>
     ///
     /// </summary>
@@ -32,28 +32,54 @@ namespace Rock.Migrations
             CreateTable(
                 "dbo.DataViewPersistedValue",
                 c => new
-                    {
-                        DataViewId = c.Int(nullable: false),
-                        EntityId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => new { t.DataViewId, t.EntityId })
-                .ForeignKey("dbo.DataView", t => t.DataViewId, cascadeDelete: true)
-                .Index(t => t.DataViewId);
-            
-            AddColumn("dbo.DataView", "PersistedScheduleIntervalMinutes", c => c.Int());
-            AddColumn("dbo.DataView", "PersistedLastRefreshDateTime", c => c.DateTime());
+                {
+                    DataViewId = c.Int( nullable: false ),
+                    EntityId = c.Int( nullable: false ),
+                } )
+                .PrimaryKey( t => new { t.DataViewId, t.EntityId } )
+                .ForeignKey( "dbo.DataView", t => t.DataViewId, cascadeDelete: true )
+                .Index( t => t.DataViewId );
+
+            AddColumn( "dbo.DataView", "PersistedScheduleIntervalMinutes", c => c.Int() );
+            AddColumn( "dbo.DataView", "PersistedLastRefreshDateTime", c => c.DateTime() );
+
+            // Add Update Persisted DataViews Job 
+            Sql( @"
+    INSERT INTO [dbo].[ServiceJob] (
+         [IsSystem]
+        ,[IsActive]
+        ,[Name]
+        ,[Description]
+        ,[Class]
+        ,[CronExpression]
+        ,[NotificationStatus]
+        ,[Guid]
+    )
+    VALUES (
+         0 
+        ,1 
+        ,'Update Persisted DataViews'
+        ,'Job to makes sure that persisted dataviews are updated based on their schedule interval.'
+        ,'Rock.Jobs.UpdatePersistedDataviews'
+        ,'0 0/1 * 1/1 * ? *'
+        ,3
+        ,'11900FEC-B5D4-4CF8-8B48-136F5BF06CB0')
+" );
         }
-        
+
         /// <summary>
         /// Operations to be performed during the downgrade process.
         /// </summary>
         public override void Down()
         {
-            DropForeignKey("dbo.DataViewPersistedValue", "DataViewId", "dbo.DataView");
-            DropIndex("dbo.DataViewPersistedValue", new[] { "DataViewId" });
-            DropColumn("dbo.DataView", "PersistedLastRefreshDateTime");
-            DropColumn("dbo.DataView", "PersistedScheduleIntervalMinutes");
-            DropTable("dbo.DataViewPersistedValue");
+            // Delete the 'Update Persisted DataViews' job
+            Sql( "DELETE FROM [ServiceJob] where [Guid] = '11900FEC-B5D4-4CF8-8B48-136F5BF06CB0'" );
+
+            DropForeignKey( "dbo.DataViewPersistedValue", "DataViewId", "dbo.DataView" );
+            DropIndex( "dbo.DataViewPersistedValue", new[] { "DataViewId" } );
+            DropColumn( "dbo.DataView", "PersistedLastRefreshDateTime" );
+            DropColumn( "dbo.DataView", "PersistedScheduleIntervalMinutes" );
+            DropTable( "dbo.DataViewPersistedValue" );
         }
     }
 }
--- RockWeb/Blocks/Reporting/DataViewDetail.ascx
+++ RockWeb/Blocks/Reporting/DataViewDetail.ascx
@@ -30,7 +30,7 @@
                                 <Rock:RockDropDownList ID="ddlTransform" runat="server" Label="Post-filter Transformation" />
                                 <Rock:CategoryPicker ID="cpCategory" runat="server" EntityTypeName="Rock.Model.DataView" Label="Category" Required="true" />
                                 <Rock:NumberBox ID="nbPersistedScheduleIntervalMinutes" runat="server" NumberType="Integer" Required="false" Label="Persisted Schedule Interval (Minutes)" MinimumValue="1" MaxLength="9"
-                                    Help="To persist this dataview, enter how often this dataview should be persisted (cached). A peristed dataview can sometimes improve performance. Leave this blank to not persist this dataview." />
+                                    Help="To persist this dataview, enter how often this dataview should be persisted (cached). A persisted dataview can improve performance, especially for complex dataviews. Leave this blank to not persist this dataview." />
                             </div>
                         </div>
                         
