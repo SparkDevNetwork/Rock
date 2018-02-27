42dbb68e251ace797621dc88dbb903d6b449d215
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-02-26 17:37:30 -0700

    - Updated Front Porch Block Settings, Defined Values, redirect catch for administrator, validation, httpcookie, add device

--- RockWeb/Blocks/Core/CaptivePortal.ascx
+++ RockWeb/Blocks/Core/CaptivePortal.ascx
@@ -2,7 +2,8 @@
 
 <asp:UpdatePanel ID="upnlContent" runat="server">
     <ContentTemplate>
-    
+        <Rock:NotificationBox ID="nbAlert" runat="server" NotificationBoxType="Danger" />
+        <asp:ValidationSummary ID="valCaptivePortal" runat="server" HeaderText="Please Correct the Following" CssClass="alert alert-danger" ValidationGroup="CaptivePortal" />
         <asp:Panel ID="pnlDetails" CssClass="panel panel-block" runat="server">
             <div class="panel-heading">
                 <h1 class="panel-title"><i class="fa fa-wifi"></i> Wifi Welcome</h1>
@@ -11,25 +12,25 @@
                 
                 <div class="row">
                     <div class="col-md-10">
-                        <Rock:RockTextBox ID="tbFirstName" runat="server" Required="false" Label="First Name" />
+                        <Rock:RockTextBox ID="tbFirstName" runat="server" Required="false" Label="First Name" ValidationGroup="CaptivePortal" />
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-10">
-                        <Rock:RockTextBox ID="tbLastName" runat="server" Required="false" Label="Last Name"/>
+                        <Rock:RockTextBox ID="tbLastName" runat="server" Required="false" Label="Last Name" ValidationGroup="CaptivePortal" />
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-10">
-                        <Rock:RockTextBox ID="tbMobilePhone" runat="server" Required="false" Label="Mobile Number" />
+                        <Rock:RockTextBox ID="tbMobilePhone" runat="server" Required="false" Label="Mobile Number" ValidationGroup="CaptivePortal" />
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="col-md-10">
-                        <Rock:RockTextBox ID="tbEmail" runat="server" Required="false" Label="Email Address" />
+                        <Rock:RockTextBox ID="tbEmail" runat="server" Required="false" Label="Email Address" ValidationGroup="CaptivePortal" />
                     </div>
                 </div>
 
@@ -43,14 +44,14 @@
                 <%-- Checkbox here to indicate agreement with T&C --%>
                 <div class="row" >
                     <div class="col-md-10" style="display: inline-block;">
-                        <Rock:RockCheckBox ID="cbAcceptTAC" runat="server" Text="I Accept" />
+                        <Rock:RockCheckBox ID="cbAcceptTAC" runat="server" Text="I Accept" ValidationGroup="CaptivePortal" />
                     </div>
                 </div>
 
                 <%-- Button here to connect --%>
                 <div class="actions row">
                     <div class="col-md-10">
-                        <asp:LinkButton ID="btnConnect" runat="server" Text="Connect To WiFi" CssClass="btn btn-primary" OnClick="btnConnect_Click" style="width:100%;" />
+                        <asp:LinkButton ID="btnConnect" runat="server" Text="Connect To WiFi" CssClass="btn btn-primary" OnClick="btnConnect_Click" style="width:100%;" ValidationGroup="CaptivePortal" />
                     </div>
                 </div>
             </div>
--- RockWeb/Blocks/Core/CaptivePortal.ascx.cs
+++ RockWeb/Blocks/Core/CaptivePortal.ascx.cs
@@ -36,83 +36,244 @@ namespace RockWeb.Blocks.Core
     [DisplayName( "WiFi Welcome" )]
     [Category( "Core" )]
     [Description( "Controls access to WiFi." )]
-    // put block properties here to set visibility
+    [TextField( "MAC Address Param", "The query string parameter used for the MAC Address", true, "client_mac", "", 0, "MacAddressParam" )]
+    [TextField( "Release Link", "URL to direct users to", true, "", "", 1, "ReleaseLink" )]
+    [BooleanField("Show First Name", "Show or hide the First Name field. If it is visible then it will be required.", true, "", 2, "ShowFirstName", IsRequired = true )]
+    [BooleanField( "Show Last Name", "Show or hide the Last Name field. If it is visible then it will be required.", true, "", 3, "ShowLastName", IsRequired = true )]
+    [BooleanField( "Show Mobile Phone", "Show or hide the Mobile Phone Number field. If it is visible then it will be required.", true, "", 4, "ShowMobilePhone", IsRequired = true )]
+    [BooleanField( "Show Email", "Show or hide the Email field. If it is visible then it will be required.", true, "", 5, "ShowEmail", IsRequired = true )]
+    [BooleanField( "Show Acceptance Checkbox", "Show or hide the \"I Accept\" checkbox. If it is visible then it will be required. This should be visible if the \"Terms And Conditions\" are also visible.", true, "", 6, "ShowAccept", IsRequired = true )]
+    [TextField( "Acceptance Checkbox Label", "Text used to signify user agreement with the Terms and Conditions", true, "I Accept", "", 7, "AcceptanceLabel" )]
+    [TextField( "Button Text", "Text to display on the button", true, "Connect To WiFi", "", 8, "ButtonText" )]
+    [BooleanField( "Show Legal Note", "Show or hide the Terms and Conditions. This should be always be visible unless users are being automatically connected without any agreement needed.", true, "", 9, "ShowLegalNote", IsRequired = true )]
+    [CodeEditorField ( "Legal Note", "A legal note outlining the Terms and Conditions for using WiFi", CodeEditorMode.Html, CodeEditorTheme.Rock, 400, false, defaultLegalNote, "", 10, "LegalNote" )]
     public partial class CaptivePortal : RockBlock
     {
-        string macAddress;
 
-        protected void Page_Load( object sender, EventArgs e )
+        #region DefaultLegalNote
+        protected const string defaultLegalNote = @"
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <title></title>
+</head>
+<body>
+    <h1>Terms & Conditions</h1>
+    <p>
+        This free WiFi service(""Service"") is provided by {{ 'Global' | Attribute:' OrganizationName' }}
+        (""Organization"") to its guests.Please read the Service Terms and Conditions below. To use the Service, users must accept these Service Terms and Conditions.
+    </p>
+
+    <p>
+        1. The Service allows the users to access the Internet via the Wi-Fi network provided by the Organization by using the user's Wi-Fi-enabled device. In order to use the Service, the
+        users must use a Wi-Fi-enabled device and related software.It is the user's responsibility to ensure that the user's device works with the service
+   </p>
+
+    <p>
+        2. The Organization may from time to time modify or enhance or suspend the Service
+    </p>
+
+    <p>
+        3. The users acknowledges and consents that:
+    </p>
+
+    <p>
+        &nbsp;&nbsp;(a) The Service has to be operated properly in accordance with the recommended practice, and with the appropriate hardware and software installed;
+    </p>
+
+    <p>
+        &nbsp;&nbsp;(b) The provisioning of the Service may reveal location-specific data, usage and retention of which are subject to the local standard privacy policy and jurisdiction;
+    </p>
+
+    <p>
+        &nbsp;&nbsp;(c) Every user is entitled to 20 continuous minutes free WiFi service every day at the Company's designated locations(s). If the connection is disconnected within the 20 minutes due to any reason, the users cannot use the Service again on the same day;
+    </p>
+
+    <p>
+        &nbsp;&nbsp;(d) The Organization excludes all liability or responsibility for any cost, claim, damage, or loss to the user or to any third party whether direct or indirect of any kind including revenue, loss or profits or any consequential loss in contract,
+        tort, under any statute or otherwise( including negligence ) arising out of or in any way related to the Service( including, but not limited to, any loss to the user arising from a suspension of the Service or Wi-Wi disconnection or degrade of Service quality); and
+     </p>
+
+    <p>
+        &nbsp;&nbsp;(e) The Organization will not be liable to the user or any other person for any loss or damage resulting from a delay or failure to perform these Terms and Conditions in whole or in part where such delay or failure is due to causes beyond the Organization's reasonable control, or which is not occasioned by its fault or negligence, including
+        acts or omissions of third parties( including telecommunications network operators, Information Service content providers, and equipment suppliers), shortage of components, war, the threat of imminent war, riots or other acts of civil disobedience,
+        insurrection, acts of God, restraints imposed by governments or any other supranational legal authority, industrial or trade disputes, fires, explosions, storms, floods, lightening, earthquakes and other natural calamities.
+    </p>
+
+    <p>
+        4. The user's use of the Service is subject to the coverage and connectivity conditions of the Service network and the Organization makes no guarantee regarding the service performance and availability of the service network. The Organization hereby expressly reserves the right to cease the
+        provisioning of the Service in the event the same is being substantially affected by reasons beyond the control of the Organization.
+    </p>
+
+</body>
+</html>";
+        #endregion
+
+        protected string macAddress;
+
+        protected override void OnLoad( EventArgs e )
         {
-            macAddress = Request.Params["client_mac"];
-            
-            if ( !IsPostBack)
-            {
-                //Uri fpUrl = Request.UrlReferrer;
-                //string frontPorchHost = fpUrl.Host;
+            base.OnLoad( e );
 
-                ViewState["FrontPorchUrl"] = Request.UrlReferrer.ToString();
-                // If we are not going to show anything just forward the user on
-                if ( ShowControls() )
+            macAddress = Request.Params[GetAttributeValue( "MacAddressParam" )];
+            bool newDevice = false;
+
+            if ( !IsPostBack )
+            {
+                newDevice = CreateDeviceIfNew();
+
+                if(newDevice)
                 {
-                    Response.Redirect( string.Format("{0}/client_mac={1}",ViewState["FrontPorchUrl"].ToString(), macAddress ) );
+                    CreateDeviceCookie();
+                }
+                
+                // 2 direct connect path
+                if ( !ShowControls() )
+                {
+                    string redirectUrl = string.Format( "{0}?{1}={2}", GetAttributeValue( "ReleaseLink" ), GetAttributeValue( "MacAddressParam" ), macAddress );
+                    
+                    // Nothing to show then just redirect back to FP with the mac
+                    if ( IsUserAuthorized( Rock.Security.Authorization.ADMINISTRATE ) )
+                    {
+                        nbAlert.Text = string.Format( "If you did not have Administrate permissions on this block, you would have been redirected to here: <a href='{0}'>{0}</a>.", redirectUrl );
+                    }
+                    else
+                    {
+                        Response.Redirect( redirectUrl );
+                    }
                 }
 
                 bool isNewDevice = CreateDeviceIfNew();
-                int? rockUserId = Session["RockUserId"].ToString().AsIntegerOrNull();
-                
-                if ( rockUserId != null )
-                {
-                    LinkDeviceToPerson(rockUserId);
-                    Prefill(rockUserId);
-                    ExpireCookie();
+                int? rockUserId = null;
 
+                if ( Session["RockUserId"] != null )
+                {
+                    // 3 user logged in path
+                    rockUserId = Session["RockUserId"].ToString().AsIntegerOrNull();
+                    Prefill( rockUserId );
+                    // Link person to device
                 }
                 else
                 {
-
+                    //4 user not logged in path
+                    // Create a new person
+                    // link new person to device
                 }
-
-
             }
-
-            
-
-
         }
-
+        
         /// <summary>
         /// Creates the device if new.
         /// </summary>
         /// <returns>Returns true if the device was created, false it already existed</returns>
         private bool CreateDeviceIfNew()
         {
-            // Check to see if the device exists
+            // If this cookie exists then we can assume that the creation process has already taken place and we can save the hit to the DB.
+            if ( Request.Cookies["rock_wifi"] != null )
+            {
+                return false;
+            }
 
-            return true;
+            // Check to see if the device exists
+            RockContext rockContext = new RockContext();
+            PersonalDeviceService personalDeviceService = new PersonalDeviceService( rockContext );
+            PersonalDevice personalDevice = personalDeviceService.GetByMACAddress( macAddress );
+
+            if (personalDevice == null)
+            {
+                personalDevice = new PersonalDevice();
+                personalDevice.MACAddress = macAddress;
+
+                // Parse the UA string and try to get the info we want
+                UAParser.ClientInfo client = UAParser.Parser.GetDefault().Parse( Request.UserAgent );
+
+                // Get the device type Mobile or Computer
+                DefinedTypeCache definedTypeCache = DefinedTypeCache.Read( Rock.SystemGuid.DefinedType.PERSONAL_DEVICE_TYPE.AsGuid() );
+                DefinedValueCache definedValueCache = null;
+
+                string clientType = Request.Browser.IsMobileDevice == true ? "Mobile" : "Computer";
+
+                if ( definedTypeCache != null )
+                {
+                    definedValueCache = definedTypeCache.DefinedValues.FirstOrDefault( v => v.Value == clientType );
+
+                    if ( definedValueCache == null )
+                    {
+                        definedValueCache = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSONAL_DEVICE_TYPE_COMPUTER.AsGuid() );
+                    }
+                }
+
+                personalDevice.PersonalDeviceTypeValueId = definedValueCache != null ? definedValueCache.Id : ( int? ) null;
+
+                // get the OS
+                string platform = client.OS.Family;
+
+                definedTypeCache = DefinedTypeCache.Read( Rock.SystemGuid.DefinedType.PERSONAL_DEVICE_PLATFORM.AsGuid() );
+
+                if (definedTypeCache != null )
+                {
+                    definedValueCache = definedTypeCache.DefinedValues.FirstOrDefault( v => v.Value == platform );
+
+                    if (definedValueCache == null )
+                    {
+                        definedValueCache = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.PERSONAL_DEVICE_PLATFORM_OTHER.AsGuid() );
+                    }
+                }
+
+                personalDevice.PlatformValueId = definedValueCache != null ? definedValueCache.Id : ( int? ) null;
+
+                // Get the OS version
+                personalDevice.DeviceVersion = string.Format(
+                    "{0}.{1}.{2}.{3}",
+                    client.OS.Major ?? "0",
+                    client.OS.Minor ?? "0",
+                    client.OS.Patch ?? "0",
+                    client.OS.PatchMinor ?? "0" );
+
+                // Add and save it
+                personalDeviceService.Add( personalDevice );
+                rockContext.SaveChanges();
+                return true;
+            }
+
+            return false;
         }
 
         private void LinkDeviceToPerson(int? rockUserId)
         {
-            if ( rockUserId == null )
-                return;
+            RockContext rockContext = new RockContext();
 
-        }
+            Person person = new PersonService( rockContext ).GetByUserLoginId( rockUserId.Value );
 
-        private void CreateDeviceCookie()
-        {
-            // If I don't know this device and person then create a cookie
-            HttpCookie httpcookie = new HttpCookie("ROCK_PERSONALDEVICE_ADDRESS", macAddress);
-            httpcookie.Expires = DateTime.MaxValue;
-        }
-
-        protected void Prefill(int? rockUserId)
-        {
-            // If they are logged into Rock then fill out the controls that are visible (use block properties)
-            if ( rockUserId == null )
+            if ( person == null )
             {
                 return;
             }
 
+            
+        }
+
+        /// <summary>
+        /// Creates the device cookie if it does not exist.
+        /// </summary>
+        private void CreateDeviceCookie()
+        {
+            if ( Request.Cookies["rock_wifi"] != null )
+            {
+                HttpCookie httpcookie = new HttpCookie( "rock_wifi" );
+                httpcookie.Expires = DateTime.MaxValue;
+                httpcookie.Values.Add( "ROCK_PERSONALDEVICE_ADDRESS", macAddress );
+                Response.Cookies.Add( httpcookie );
+            }
+        }
+
+        /// <summary>
+        /// Prefills the visible fields with info from the specified rock user
+        /// </summary>
+        /// <param name="rockUserId">The rock user identifier.</param>
+        protected void Prefill(int? rockUserId)
+        {
             Person person = new PersonService( new RockContext() ).GetByUserLoginId( rockUserId.Value );
 
             if (person == null)
@@ -120,7 +281,7 @@ namespace RockWeb.Blocks.Core
                 return;
             }
 
-            if (tbFirstName.Visible == true)
+            if ( tbFirstName.Visible == true )
             {
                 tbFirstName.Text = person.FirstName;
             }
@@ -139,42 +300,63 @@ namespace RockWeb.Blocks.Core
             {
                 tbEmail.Text = person.Email;
             }
-
-            AttributeValueService a = new AttributeValueService( new RockContext());
-
         }
 
         private void ExpireCookie()
         {
-            if ( Request.Cookies["ROCK_PERSONALDEVICE_ADDRESS"] != null )
+            if ( Request.Cookies["rock_wifi"] != null )
             {
-                Response.Cookies["ROCK_PERSONALDEVICE_ADDRESS"].Expires = DateTime.Now.AddDays( -1 );
+                Response.Cookies["rock_wifi"].Expires = DateTime.Now.AddDays( -1 );
             }
         }
 
+        /// <summary>
+        /// Handles the Click event of the btnConnect control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void btnConnect_Click( object sender, EventArgs e )
         {
-            // Verify required fields
-
             // Send them back to Front Porch
-            Response.Redirect( string.Format( "{0}/client_mac={1}", ViewState["FrontPorchUrl"].ToString(), macAddress ) );
+            Response.Redirect( string.Format( "{0}?{1}={2}", GetAttributeValue( "ReleaseLink" ), GetAttributeValue( "MacAddressParam" ), macAddress ) );
 
         }
 
+        /// <summary>
+        /// Shows the controls according to the attribute values. If they are visible then they are also required.
+        /// </summary>
+        /// <returns>If any control is visible then true, else false.</returns>
         protected bool ShowControls()
         {
-            bool showNothing = false;
-            // get the attributes and show the desired controls. If they are visible then they are required
+            tbFirstName.Visible = GetAttributeValue( "ShowFirstName" ).AsBoolean();
+            tbFirstName.Required = GetAttributeValue( "ShowFirstName" ).AsBoolean();
 
+            tbLastName.Visible = GetAttributeValue( "ShowLastName" ).AsBoolean();
+            tbLastName.Required = GetAttributeValue( "ShowLastName" ).AsBoolean();
 
+            tbMobilePhone.Visible = GetAttributeValue( "ShowMobilePhone" ).AsBoolean();
+            tbMobilePhone.Required = GetAttributeValue( "ShowMobilePhone" ).AsBoolean();
 
+            tbEmail.Visible = GetAttributeValue( "ShowEmail" ).AsBoolean();
+            tbEmail.Required = GetAttributeValue( "ShowEmail" ).AsBoolean();
+            
+            cbAcceptTAC.Visible = GetAttributeValue( "ShowAccept" ).AsBoolean();
+            cbAcceptTAC.Required = GetAttributeValue( "ShowAccept" ).AsBoolean();
+            cbAcceptTAC.Label = GetAttributeValue( "AcceptanceLabel" );
 
-            var legalNotice = System.IO.File.ReadAllText( Server.MapPath( "/Content/ExternalSite/WiFiLegalNotice.html" ) );
-            iframeLegalNotice.Attributes["srcdoc"] = legalNotice.ResolveMergeFields( Rock.Lava.LavaHelper.GetCommonMergeFields(this.RockPage) );
-            iframeLegalNotice.Src = "javascript: window.frameElement.getAttribute('srcdoc');";
+            btnConnect.Text = GetAttributeValue( "ButtonText" );
 
+            if ( iframeLegalNotice.Visible = GetAttributeValue( "ShowLegalNote" ).AsBoolean() )
+            {
+                iframeLegalNotice.Attributes["srcdoc"] = GetAttributeValue( "LegalNote" ).ResolveMergeFields( Rock.Lava.LavaHelper.GetCommonMergeFields( this.RockPage ) );
+                iframeLegalNotice.Src = "javascript: window.frameElement.getAttribute('srcdoc');";
+            }
 
-            return showNothing;
+            if ( tbFirstName.Visible || tbLastName.Visible || tbMobilePhone.Visible || tbEmail.Visible || cbAcceptTAC.Visible || iframeLegalNotice.Visible )
+            {
+                return true;
+            }
+                return false;
         }
     }
 
