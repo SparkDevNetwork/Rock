6dd1b6a4a67ea56ed4fa3391bb19450e9b9276f8
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-03-13 17:21:21 -0700

    - Added class to detect legacy lava and generate SQL scripts to update it

---
+++ Rock.Tests/Rock/Lava/LegacyLavaUpdaterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xunit;
+
+using Rock.Lava;
+using System.Collections.Generic;
+
+namespace Rock.Tests.Rock.Lava
+{
+    public class LegacyLavaUpdaterTest
+    {
+        [Fact]
+        public void UpdateLegacyLava()
+        {
+            LegacyLavaUpdater legacyLavaUpdater = new LegacyLavaUpdater();
+            legacyLavaUpdater.FindLegacyLava();
+
+            Assert.NotEmpty( legacyLavaUpdater.SQLUpdateScripts );
+
+        }
+    }
+}
