516241465ce89277211837de57d41e3015cc479d
David Turner <david@sparkdevnetwork.org>
2018-03-15 15:13:58 -0700

    - Commented out the most recent hotfix migration from v74 (added them to migration rollups)

--- Rock/Plugin/HotFixes/048_InteractionSessionPerformance.cs
+++ Rock/Plugin/HotFixes/048_InteractionSessionPerformance.cs
@@ -34,72 +34,72 @@ namespace Rock.Plugin.HotFixes
         /// </summary>
         public override void Up()
         {
-            // JE: Fix missing period in confirm message
-            Sql( @"
-    UPDATE [Attribute]
-        SET
-            [DefaultValue] = '{0}, Your account has been confirmed.  Thank you for creating the account.'
-    WHERE
-        [Guid] = '22FCB059-AA40-45D9-BECB-C22D15A3D41A'
-" );
+//            // JE: Fix missing period in confirm message
+//            Sql( @"
+//    UPDATE [Attribute]
+//        SET
+//            [DefaultValue] = '{0}, Your account has been confirmed.  Thank you for creating the account.'
+//    WHERE
+//        [Guid] = '22FCB059-AA40-45D9-BECB-C22D15A3D41A'
+//" );
 
-            // Fix the Interaction Detail page setting on Interaction Session List Block
-            RockMigrationHelper.AddBlockAttributeValue( "0FCDA1B8-B3F1-4E78-8FCF-E81F2CA77D05", "922BDC9E-D1ED-4553-B84D-1C301B291F5F", @"b6f6ab6f-a572-45fe-a143-2e4b8f192c8d" );
+//            // Fix the Interaction Detail page setting on Interaction Session List Block
+//            RockMigrationHelper.AddBlockAttributeValue( "0FCDA1B8-B3F1-4E78-8FCF-E81F2CA77D05", "922BDC9E-D1ED-4553-B84D-1C301B291F5F", @"b6f6ab6f-a572-45fe-a143-2e4b8f192c8d" );
 
-            // Add an index to help with performance of the Session List block
-            Sql( @"
-    IF EXISTS ( SELECT * FROM sys.indexes WHERE NAME = 'IX_PersonAliasId_InteractionSessionId' AND object_id = OBJECT_ID('Interaction') )
-	DROP INDEX [IX_PersonAliasId_InteractionSessionId] ON [dbo].[Interaction]
+//            // Add an index to help with performance of the Session List block
+//            Sql( @"
+//    IF EXISTS ( SELECT * FROM sys.indexes WHERE NAME = 'IX_PersonAliasId_InteractionSessionId' AND object_id = OBJECT_ID('Interaction') )
+//	DROP INDEX [IX_PersonAliasId_InteractionSessionId] ON [dbo].[Interaction]
 
-    CREATE NONCLUSTERED INDEX [IX_PersonAliasId_InteractionSessionId]
-    ON [dbo].[Interaction] ([PersonAliasId],[InteractionSessionId])
-    INCLUDE ([InteractionDateTime],[InteractionComponentId])
-" );
+//    CREATE NONCLUSTERED INDEX [IX_PersonAliasId_InteractionSessionId]
+//    ON [dbo].[Interaction] ([PersonAliasId],[InteractionSessionId])
+//    INCLUDE ([InteractionDateTime],[InteractionComponentId])
+//" );
 
-            // Update the default session list template to link pages directly to the page that was viewed
-            RockMigrationHelper.UpdateBlockTypeAttribute( "EA90EF4F-C783-48CD-B575-AD785DE896E9", "1D0D3794-C210-48A8-8C68-3FBEC08A6BA5", "Default Template", "DefaultTemplate", "", @"The Lava template to use as default.", 2, @"
-{% if InteractionChannel != null and InteractionChannel != '' %}
-    {% for session in WebSessions %}
-        <div class='panel panel-widget'>
-	        <header class='panel-heading clearfix'>
-	        <div class='pull-left'>
-		        <h4 class='panel-title'>
-		            {{ session.PersonAlias.Person.FullName }}
-			        <small>
-			            Started {{ session.StartDateTime }} / 
-			            Duration: {{ session.StartDateTime | HumanizeTimeSpan:session.EndDateTime, 1 }}
-			        </small>
-		        </h4>
-		        <span class='label label-primary'></span>
-		        <span class='label label-info'>{{ InteractionChannel.Name }}</span>
-		        </div> 
-		        {% assign icon = '' %}
-		        {% case session.InteractionSession.DeviceType.ClientType %}
-			        {% when 'Desktop' %}{% assign icon = 'fa-desktop' %}
-			        {% when 'Tablet' %}{% assign icon = 'fa-tablet' %}
-			        {% when 'Mobile' %}{% assign icon = 'fa-mobile-phone' %}
-			        {% else %}{% assign icon = '' %}
-		        {% endcase %}
-		        {% if icon != '' %}
-    		        <div class='pageviewsession-client pull-right'>
-                        <div class='pull-left'>
-                            <small>{{ session.InteractionSession.DeviceType.Application }} <br>
-                            {{ session.InteractionSession.DeviceType.OperatingSystem }} </small>
-                        </div>
-                        <i class='fa {{ icon }} fa-2x pull-right'></i>
-                    </div>
-                {% endif %}
-	        </header>
-	        <div class='panel-body'>
-		        <ol>
-		        {% for interaction in session.Interactions %}
-    			    <li><a href = '{{ interaction.InteractionData }}'>{{ interaction.InteractionComponent.Name }}</a></li>
-		        {% endfor %}				
-		        </ol>
-	        </div>
-        </div>
-    {% endfor %}
-{% endif %}", "DF74EDE5-3B7D-4A79-B1F2-499D18FE6F2C" );
+//            // Update the default session list template to link pages directly to the page that was viewed
+//            RockMigrationHelper.UpdateBlockTypeAttribute( "EA90EF4F-C783-48CD-B575-AD785DE896E9", "1D0D3794-C210-48A8-8C68-3FBEC08A6BA5", "Default Template", "DefaultTemplate", "", @"The Lava template to use as default.", 2, @"
+//{% if InteractionChannel != null and InteractionChannel != '' %}
+//    {% for session in WebSessions %}
+//        <div class='panel panel-widget'>
+//	        <header class='panel-heading clearfix'>
+//	        <div class='pull-left'>
+//		        <h4 class='panel-title'>
+//		            {{ session.PersonAlias.Person.FullName }}
+//			        <small>
+//			            Started {{ session.StartDateTime }} / 
+//			            Duration: {{ session.StartDateTime | HumanizeTimeSpan:session.EndDateTime, 1 }}
+//			        </small>
+//		        </h4>
+//		        <span class='label label-primary'></span>
+//		        <span class='label label-info'>{{ InteractionChannel.Name }}</span>
+//		        </div> 
+//		        {% assign icon = '' %}
+//		        {% case session.InteractionSession.DeviceType.ClientType %}
+//			        {% when 'Desktop' %}{% assign icon = 'fa-desktop' %}
+//			        {% when 'Tablet' %}{% assign icon = 'fa-tablet' %}
+//			        {% when 'Mobile' %}{% assign icon = 'fa-mobile-phone' %}
+//			        {% else %}{% assign icon = '' %}
+//		        {% endcase %}
+//		        {% if icon != '' %}
+//    		        <div class='pageviewsession-client pull-right'>
+//                        <div class='pull-left'>
+//                            <small>{{ session.InteractionSession.DeviceType.Application }} <br>
+//                            {{ session.InteractionSession.DeviceType.OperatingSystem }} </small>
+//                        </div>
+//                        <i class='fa {{ icon }} fa-2x pull-right'></i>
+//                    </div>
+//                {% endif %}
+//	        </header>
+//	        <div class='panel-body'>
+//		        <ol>
+//		        {% for interaction in session.Interactions %}
+//    			    <li><a href = '{{ interaction.InteractionData }}'>{{ interaction.InteractionComponent.Name }}</a></li>
+//		        {% endfor %}				
+//		        </ol>
+//	        </div>
+//        </div>
+//    {% endfor %}
+//{% endif %}", "DF74EDE5-3B7D-4A79-B1F2-499D18FE6F2C" );
 
         }
 
