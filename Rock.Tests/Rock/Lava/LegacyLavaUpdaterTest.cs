f60a72394a328dedb4e50094ab89964122684a3e
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-03-16 14:26:04 -0700

    - Improved logic, added more tests, refactored and simplified code.

--- Rock.Tests/Rock/Lava/LegacyLavaUpdaterTest.cs
+++ Rock.Tests/Rock/Lava/LegacyLavaUpdaterTest.cs
@@ -18,5 +18,69 @@ namespace Rock.Tests.Rock.Lava
             Assert.NotEmpty( legacyLavaUpdater.SQLUpdateScripts );
 
         }
+
+        /// <summary>
+        /// This test is to confirm that the _url string is not replaced if it isn't in a lava tag
+        /// </summary>
+        [Fact]
+        public void ReplaceUrlOnNonLavaString()
+        {
+            LegacyLavaUpdater legacyLavaUpdater = new LegacyLavaUpdater();
+            bool isUpdated = false;
+
+            string beforeText = @"This should not get changed query_strings_from_url {{ Person | Attribute: ''Facebook'', ''Url'' }}";
+            string afterText = legacyLavaUpdater.ReplaceUrl( beforeText, ref isUpdated );
+            Assert.Equal( beforeText, afterText );
+        }
+
+        /// <summary>
+        /// Confirms that the _url string is replaced if it is in a lava tag
+        /// </summary>
+        [Fact]
+        public void ReplaceUrlOnLavaString()
+        {
+            LegacyLavaUpdater legacyLavaUpdater = new LegacyLavaUpdater();
+            bool isUpdated = false;
+
+            string beforeText = "<p>_url Legacy Lava {{ Person.Facebook_url }} Legacy Lava </p>";
+            string expectedText = "<p>_url Legacy Lava {{ Person.Facebook,'Url' }} Legacy Lava </p>";
+
+            string afterText = legacyLavaUpdater.ReplaceUrl( beforeText, ref isUpdated );
+            Assert.Equal( expectedText, afterText );
+        }
+
+        /// <summary>
+        /// Confirms that the URL replacer will loop through the entire string.
+        /// </summary>
+        [Fact]
+        public void ReplaceMultipleUrlOnLavaString()
+        {
+            LegacyLavaUpdater legacyLavaUpdater = new LegacyLavaUpdater();
+            bool isUpdated = false;
+
+            string beforeText = "<p>_url Legacy Lava {{ Person.Facebook_url }} {{ Person.Twitter_url }} {{ Person.SnapChat_url }} {{ Person.Instagram_url }} Legacy Lava </p>";
+            string expectedText = "<p>_url Legacy Lava {{ Person.Facebook,'Url' }} {{ Person.Twitter,'Url' }} {{ Person.SnapChat,'Url' }} {{ Person.Instagram,'Url' }} Legacy Lava </p>";
+
+            string afterText = legacyLavaUpdater.ReplaceUrl( beforeText, ref isUpdated );
+            Assert.Equal( expectedText, afterText );
+        }
+
+        [Fact]
+        public void CheckSystemEmail()
+        {
+            LegacyLavaUpdater legacyLavaUpdater = new LegacyLavaUpdater();
+            legacyLavaUpdater.CheckSystemEmail();
+        }
+
+        [Fact]
+        public void CheckDotNotation()
+        {
+            bool isUpdated = false;
+            string beforeText = "{% for group in GroupType.Groups %}{% for location in group.Locations %}{% for schedule in location.Schedules %}{{schedule.Name}}{% endfor %}{% endfor %}{% endfor %}";
+            string expectedText = "{% for group in GroupType.Groups %}{% for location in group.Locations %}{% for schedule in location.Schedules %}{{schedule.Name}}{% endfor %}{% endfor %}{% endfor %}";
+            LegacyLavaUpdater legacyLavaUpdater = new LegacyLavaUpdater();
+            string afterText = legacyLavaUpdater.ReplaceDotNotation(beforeText, ref isUpdated );
+            Assert.Equal( expectedText, afterText );
+        }
     }
 }
