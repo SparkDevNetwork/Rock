03bdfd0f8d972efbe9cd5c765735ef5d48571a26
Jon Edmiston <jonathan.edmiston@gmail.com>
2018-03-26 11:20:31 -0700

    - edits

--- Rock.Mailgun/Communication/Transport/MailgunHttp.cs
+++ Rock.Mailgun/Communication/Transport/MailgunHttp.cs
@@ -46,10 +46,10 @@ namespace Rock.Communication.Transport
 
     [TextField( "Base URL", "The API URL provided by Mailgun, keep the default in most cases", true, @"https://api.mailgun.net/v3", "", 0, "BaseURL" )]
     [TextField( "Resource", "The URL part provided by Mailgun, keep the default in most cases", true, @"{domian}/messages", "", 1, "Resource" )]
-    [TextField("Domain", "The email domain e.g. sparkdevnetwork.org", true, "", "", 2, "Domain" )]
-    [TextField( "API Key", "The API Key provided by Mailgun, starts with \"key-\"", true, "", "", 3, "APIKey" )]
+    [TextField( "Domain", "The email domain (e.g. rocksolidchurchdemo.com).", true, "", "", 2, "Domain" )]
+    [TextField( "API Key", "The API Key provided by Mailgun (starts with \"key-\").", true, "", "", 3, "APIKey" )]
     [BooleanField( "Track Opens", "", true, "", 4, "TrackOpens" )]
-    public class MailGunHtml : TransportComponent
+    public class MailgunHttp : TransportComponent
     {
         /// <summary>
         /// Gets the response returned from the Mailgun API REST call.
