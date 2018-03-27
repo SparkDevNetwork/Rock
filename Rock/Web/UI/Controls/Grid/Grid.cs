b91a32b7dd81cf83d059aceae0a75c72c04c8df5
David Turner <david@sparkdevnetwork.org>
2018-03-27 10:52:41 -0700

    + Fixed exception that would occur if creating a communication on a page who's URL was longer than 200 characters (i.e. a shared attendance analytics link).

--- Rock/Web/UI/Controls/Grid/Grid.cs
+++ Rock/Web/UI/Controls/Grid/Grid.cs
@@ -1437,7 +1437,7 @@ namespace Rock.Web.UI.Controls
 
                     if ( rockPage.Request != null && rockPage.Request.Url != null )
                     {
-                        communication.UrlReferrer = rockPage.Request.Url.AbsoluteUri;
+                        communication.UrlReferrer = rockPage.Request.Url.AbsoluteUri.TrimForMaxLength( communication, "UrlReferrer" );
                     }
 
                     communicationService.Add( communication );
