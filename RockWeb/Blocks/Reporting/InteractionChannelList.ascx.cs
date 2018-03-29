bfffee2e14abbd62a337f09383684b9c3ca466c4
Shivam Kishor <shivpat.1990@gmail.com>
2018-03-29 23:08:53 +0530

    + Added the ability to filter interaction blocks by a specific person through the query string.
    # Conflicts:
    #	Rock/Rock.csproj

--- RockWeb/Blocks/Reporting/InteractionChannelList.ascx.cs
+++ RockWeb/Blocks/Reporting/InteractionChannelList.ascx.cs
@@ -58,6 +58,7 @@ namespace RockWeb.Blocks.Reporting
     </a>
 {% endif %}" )]
 
+    [InteractionChannelsField( "Interaction Channels", "Select interaction channel to limit the display. No selection will show all.", false,  "", "", order: 3 )]
     public partial class InteractionChannelList : Rock.Web.UI.RockBlock
     {
         #region Fields
@@ -181,6 +182,19 @@ namespace RockWeb.Blocks.Reporting
                     channelQry = channelQry.Where( a => a.ChannelTypeMediumValueId == channelMediumValueId.Value );
                 }
 
+                if ( !string.IsNullOrWhiteSpace( GetAttributeValue( "InteractionChannels" ) ) )
+                {
+                    var selectedChannelIds = Array.ConvertAll( GetAttributeValue( "InteractionChannels" ).Split( ',' ), s => new Guid( s ) ).ToList();
+                    channelQry = channelQry.Where( a => selectedChannelIds.Contains( a.Guid ) );
+                }
+
+                var personAliasId = GetPersonAliasId();
+                if ( personAliasId.HasValue )
+                {
+                    var interactionQry = new InteractionService( rockContext ).Queryable();
+                    channelQry = channelQry.Where( a => interactionQry.Any( b => b.PersonAliasId == personAliasId.Value && b.InteractionComponent.ChannelId == a.Id ) );
+                }
+
                 // Parse the default template so that it does not need to be parsed multiple times
                 var defaultTemplate = Template.Parse( GetAttributeValue( "DefaultTemplate" ) );
                 var options = new Rock.Lava.CommonMergeFieldsOptions();
@@ -215,6 +229,31 @@ namespace RockWeb.Blocks.Reporting
                 rptChannel.DataSource = channelItems;
                 rptChannel.DataBind();
             }
+        }
+
+        /// <summary>
+        /// Get the person alias through query list or context.
+        /// </summary>
+        public int? GetPersonAliasId()
+        {
+            int? personId = PageParameter( "PersonId" ).AsIntegerOrNull();
+            int? personAliasId = PageParameter( "PersonAliasId" ).AsIntegerOrNull();
+
+            if ( personId.HasValue )
+            {
+                personAliasId = new PersonAliasService( new RockContext() ).GetPrimaryAliasId( personId.Value );
+            }
+
+            if ( !personAliasId.HasValue )
+            {
+                var person = ContextEntity<Person>();
+                if ( person != null )
+                {
+                    personAliasId = person.PrimaryAliasId;
+                }
+            }
+
+            return personAliasId;
         }
 
         #endregion
