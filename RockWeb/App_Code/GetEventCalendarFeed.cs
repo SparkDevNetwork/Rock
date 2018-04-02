4b0ebb6fca4951662c4b57c1ac324f2d7a20e724
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-04-02 15:42:45 -0700

    - Added security and parsing query string

--- RockWeb/App_Code/GetEventCalendarFeed.cs
+++ RockWeb/App_Code/GetEventCalendarFeed.cs
@@ -30,6 +30,7 @@ using Rock.Security;
 using Rock.Web.Cache;
 
 using DDay.iCal;
+using System.Globalization;
 
 namespace RockWeb
 {
@@ -49,23 +50,25 @@ namespace RockWeb
             }
         }
 
-        public void ProcessRequest( HttpContext context )
+        public void ProcessRequest( HttpContext httpContext )
         {
-            request = context.Request;
-            response = context.Response;
+            request = httpContext.Request;
+            response = httpContext.Response;
+
+            if ( !ValidateSecurity( httpContext ) )
+            {
+                return;
+            }
 
             RockContext rockContext = new RockContext();
-            CalendarProps calendarProps = ValidateRequestData( context );
+            CalendarProps calendarProps = ValidateRequestData( httpContext );
 
             if ( calendarProps == null )
             {
                 return;
             }
 
-            if ( !ValidateSecurity( context ) )
-            {
-                return;
-            }
+            
 
 
             iCalendar icalendar = new iCalendar();
@@ -75,61 +78,148 @@ namespace RockWeb
         }
 
 
-        private void SendNotAuthorized( HttpContext context )
+        private void SendNotAuthorized( HttpContext httpContext )
         {
-            context.Response.StatusCode = HttpStatusCode.Forbidden.ConvertToInt();
-            context.Response.StatusDescription = "Not authorized to view calendar.";
-            context.ApplicationInstance.CompleteRequest();
+            httpContext.Response.StatusCode = HttpStatusCode.Forbidden.ConvertToInt();
+            httpContext.Response.StatusDescription = "Not authorized to view calendar.";
+            httpContext.ApplicationInstance.CompleteRequest();
         }
 
-        private void SendBadRequest( HttpContext context, string addlInfo = "" )
+        private void SendBadRequest( HttpContext httpContext, string addlInfo = "" )
         {
-            context.Response.StatusCode = HttpStatusCode.BadRequest.ConvertToInt();
-            context.Response.StatusDescription = "Request is inavalid or malformed. " + addlInfo;
+            httpContext.Response.StatusCode = HttpStatusCode.BadRequest.ConvertToInt();
+            httpContext.Response.StatusDescription = "Request is inavalid or malformed. " + addlInfo;
         }
 
         private bool ValidateSecurity( HttpContext context )
         {
+            int calendarId;
+            if ( request.QueryString["calendarid"] == null || !int.TryParse( request.QueryString["calendarId"], out calendarId ) )
+            {
+                SendNotAuthorized( context );
+                return false;
+            }
+
             RockContext rockContext = new RockContext();
+            EventCalendarService eventCalendarService = new EventCalendarService( rockContext );
+            EventCalendar eventCalendar = eventCalendarService.Get( calendarId );
+
+            if ( eventCalendar == null )
+            {
+                SendNotAuthorized( context );
+                return false;
+            }
+
+            // If this is a public calendar then just return true
+            if ( eventCalendar.IsAllowedByDefault( "View" ) )
+            {
+                return true;
+            }
 
             UserLogin currentUser = new UserLoginService( rockContext ).GetByUserName( UserLogin.GetCurrentUserName() );
             Person currentPerson = currentUser != null ? currentUser.Person : null;
 
-            EventCalendarService eventCalendarService = new EventCalendarService( rockContext );
+            if ( currentPerson != null && eventCalendar.IsAuthorized( Rock.Security.Authorization.VIEW, currentPerson ) )
+            {
+                return true;
+            }
 
-            EventCalendar eventCalendar = eventCalendarService.Get( 1 );
-
-            //eventCalendar.IsAuthorized( Authorization.VIEW, currentPerson )
-            
-            return true;
+            SendNotAuthorized( context );
+            return false;
         }
 
+        /// <summary>
+        /// Validates the request data.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
         private CalendarProps ValidateRequestData( HttpContext context )
         {
             CalendarProps calendarProps = new CalendarProps();
 
-            // CalendarID is required
-            int calendarId;
-            if ( request.QueryString["calendarid"] == null || !int.TryParse(request.QueryString["calendarid"], out calendarId ) )
-            {
-                SendBadRequest( context, "Calendar ID is missing or not valid." );
-                return null;
-            }
-            
-            // Campus, audiences, startdate, enddate are optional
+            // Security check made sure the calendar ID is good so no need to check it again.
+            calendarProps.CalendarId = int.Parse( request.QueryString["calendarid"] );
 
+            string campusIdQueryString = request.QueryString["campusIds"] != null ? request.QueryString["campusIds"] : string.Empty;
+            calendarProps.CampusIds = ParseIds( campusIdQueryString );
 
+            string audienceIdQueryString = request.QueryString["audienceIds"] != null ? request.QueryString["audienceIds"] : string.Empty;
+            calendarProps.AudienceIds = ParseIds( audienceIdQueryString );
+
+            string startDate = request.QueryString["startDate"] != null ? request.QueryString["startDate"] : string.Empty;
+            calendarProps.StartDate = DateTime.ParseExact( startDate, "yyyyMMDD", CultureInfo.InvariantCulture );
+
+            string endDate = request.QueryString["endDate"] != null ? request.QueryString["endDate"] : string.Empty;
+            calendarProps.EndDate = DateTime.ParseExact( endDate, "yyyyMMDD", CultureInfo.InvariantCulture );
 
             return calendarProps;
         }
 
+
+        /// <summary>
+        /// Parses a query string for a list of Ids
+        /// </summary>
+        /// <returns></returns>
+        private List<int> ParseIds( string queryParamemter )
+        {
+            List<string> stringIdList = new List<string>();
+            List<int> intIdList = new List<int>();
+
+            if ( queryParamemter.IsNotNullOrWhitespace() )
+            {
+                stringIdList = queryParamemter.Split( ',' ).ToList();
+
+                foreach ( string stringId in stringIdList )
+                {
+                    int intId;
+                    if ( int.TryParse( stringId, out intId ) )
+                    {
+                        intIdList.Add( intId );
+                    }
+                }
+            }
+
+            return intIdList;
+        }
+
+        /// <summary>
+        /// CalendarId is required. CampusIds, AudienceIds, Startdate, and Enddate are optional.
+        /// StartDate defaults to the current date, EndDate defaults to the currentDate + 2 months.
+        /// </summary>
         private class CalendarProps
         {
+            DateTime _startDate;
+            DateTime _endDate;
+
             public int CalendarId { get; set; }
             public List<int> CampusIds { get; set; }
             public List<int> AudienceIds { get; set; }
-            public DateTime startDate { get; set; }
-            public DateTime endDate { get; set; }
+            public DateTime StartDate
+            {
+                get
+                {
+                    return _startDate != null ? _startDate : DateTime.Now.Date;
+                }
+
+                set
+                {
+                    _startDate = value;
+                }
+            }
+
+            public DateTime EndDate
+            {
+                get
+                {
+                    return _endDate != null ? _endDate : DateTime.Now.AddMonths( 2 ).Date;
+                }
+
+                set
+                {
+                    _endDate = value;
+                }
+            }
+
 
         }
 
