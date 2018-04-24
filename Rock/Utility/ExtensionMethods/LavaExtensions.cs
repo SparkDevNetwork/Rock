8f9e485c3d31ac1f837a96d826700cfc18d48a61
Mike Peterson <mike@sparkabilitygroup.com>
2018-04-23 17:08:27 -0700

    - Update HtmlEditor to help prevent an issue where switching to WYSIWYG mode can mess up your content if there are Lava commands. Now it will detect if there are lava commands and automatically start in code editor mode. It will also show a warning and prevent you from going to WYSIWYG mode if there are any lava commands. For example, if the code contains a {% for %} loop around a <tr>, it'll detect that and prevent WYSIWYG mode so that you don't mess up your code. (Fixes #1810 )

--- Rock/Utility/ExtensionMethods/LavaExtensions.cs
+++ Rock/Utility/ExtensionMethods/LavaExtensions.cs
@@ -727,6 +727,11 @@ namespace Rock
         private static Regex hasLavaMergeFields = new Regex( @"(?<=\{).+(?<=\})", RegexOptions.Compiled );
 
         /// <summary>
+        /// Compiled RegEx for detecting if a string has Lava {% %} command fields
+        /// </summary>
+        private static Regex hasLavaCommandFields = new Regex( @"(?<=\{%).+(?<=%\})", RegexOptions.Compiled );
+
+        /// <summary>
         /// Compiled RegEx for detecting if a string uses the Legacy "GlobalAttribute." syntax
         /// </summary>
         private static Regex hasLegacyGlobalAttributeLavaMergeFields = new Regex( @"(?<=\{).+GlobalAttribute.+(?<=\})", RegexOptions.Compiled );
@@ -740,11 +745,36 @@ namespace Rock
         public static bool HasMergeFields( this string content )
         {
             if ( content == null )
+            {
                 return false;
+            }
 
-            // If there's no merge codes, just return the content
-            if (!hasLavaMergeFields.IsMatch( content ))
+            // If there are no lava codes, return false
+            if ( !hasLavaMergeFields.IsMatch( content ) )
+            {
                 return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Determines whether the string potentially has lava command {% %} fields in it.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <returns></returns>
+        public static bool HasLavaCommandFields( this string content )
+        {
+            if ( content == null )
+            {
+                return false;
+            }
+
+            // If there are no lava command fields, return false
+            if ( !hasLavaCommandFields.IsMatch( content ) )
+            {
+                return false;
+            }
 
             return true;
         }
