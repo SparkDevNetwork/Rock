8b8690dc7d152c961ecc069c9335d124112936fe
Garrett Johnson <nvisiondesign@gmail.com>
2018-04-27 12:18:21 -0700

    - Fixed Sticky headers for jQuery 3

--- RockWeb/Scripts/Rock/coreListeners.js
+++ RockWeb/Scripts/Rock/coreListeners.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
-  Sys.Application.add_load(function () {
-    $('.js-sticky-headers').stickyTableHeaders();
-  })
-});
+Sys.Application.add_load(function () {
+  var navbarFixedTop = document.querySelector('.navbar-fixed-top');
+  var stickyHeaders = document.querySelector('.js-sticky-headers');
+
+  if ($(navbarFixedTop).length){
+    $(stickyHeaders).stickyTableHeaders({fixedOffset: $(navbarFixedTop)});
+  } else {
+    $(stickyHeaders).stickyTableHeaders();
+  }
+})
