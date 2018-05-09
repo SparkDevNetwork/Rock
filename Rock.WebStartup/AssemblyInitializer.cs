434550dd12351c231d62be45e018c39fde6b5caa
Mike Peterson <mike@sparkabilitygroup.com>
2018-05-09 15:26:28 -0700

    - Cleaned up code in TextFieldType - Update WebStartup to give more accurate debug timings - Updated AttributeEditor to initialize Qualifier Values when adding a new attribute (this will reduce the need for field types to deal with missing qualifiers) - Fixed compiler warning in RockControlGallery

--- Rock.WebStartup/AssemblyInitializer.cs
+++ Rock.WebStartup/AssemblyInitializer.cs
@@ -15,12 +15,15 @@ namespace Rock
         {
             try
             {
+                var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
                 if ( System.Web.Hosting.HostingEnvironment.IsDevelopmentEnvironment )
                 {
                     System.Diagnostics.Debug.WriteLine( string.Format( "Application_Initialize: {0}", RockDateTime.Now.ToString( "hh:mm:ss.FFF" ) ) );
+                    new Rock.Model.AttributeService( new Data.RockContext() ).Get( 0 );
+                    System.Diagnostics.Debug.WriteLine( string.Format( "ConnectToDatabase - {0} ms", stopwatch.Elapsed.TotalMilliseconds ) );
+                    stopwatch.Restart();
                 }
-
-                var stopwatch = System.Diagnostics.Stopwatch.StartNew();
 
                 // Step 1: Load registered HTTP Modules - http://blog.davidebbo.com/2011/02/register-your-http-modules-at-runtime.html
                 var activeHttpModules = Rock.Web.HttpModules.HttpModuleContainer.GetActiveComponents(); // takes 8 seconds :(
@@ -32,7 +35,7 @@ namespace Rock
 
                 if ( System.Web.Hosting.HostingEnvironment.IsDevelopmentEnvironment )
                 {
-                    System.Diagnostics.Debug.WriteLine( string.Format( "WebStartup.Initialize/ConnectToDatabase - {0} ms", stopwatch.Elapsed.TotalMilliseconds ) );
+                    System.Diagnostics.Debug.WriteLine( string.Format( "WebStartup.Initialize - {0} ms", stopwatch.Elapsed.TotalMilliseconds ) );
                 }
             }
             catch ( Exception ) { } // incase something bad happens when access the database, like a problem with a migration
