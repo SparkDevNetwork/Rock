be533b71b16483a94e122c42a1523e03479ae905
jeredmcferron@gmail.com <jeredmcferron@gmail.com>
2018-05-10 15:32:54 -0700

    More hero image cleanup Fixed missions page arrow text Added support for a campuses map on 'our-campuses' page Archived unused map files

---
+++ RockWeb/Themes/church_ccv_External_v8/Scripts/our-campuses-map.js
@@ -0,0 +1,52 @@
+
+
+//
+// MAP CAMPUS MODAL
+// --------------------------------------------------
+
+
+// Imports
+// -------------------------
+
+// @prepros-prepend "_map.js"
+
+// Load the Google maps api dynamically to avoid errors when double loading
+// The URL string is from https://github.com/SparkDevNetwork/Rock/blob/582e01a72f41ab33ad41335c01c365523b35be5b/Rock/Web/UI/RockPage.cs#L1045
+Rock.controls.util.loadGoogleMapsApi('https://maps.googleapis.com/maps/api/js?sensor=false&libraries=drawing&key=' + googleApiKey)
+
+
+// Modal Functionality
+// -------------------------
+
+window.CCV = window.CCV || {}
+
+CCV.showMapCampusModal = function() {
+  if (!CCV.campusModalMapHasBeenDrawn) {
+    window.campusModalMap = new CCV.campusInfoWindowMap(document.getElementById('our-campuses-map'))
+
+    options = {
+      mapTypeId: 'CCV',
+      
+      panControl: this.usePanControl,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.TOP_RIGHT
+      },
+      streetViewControl: false,
+      mapTypeControl: false,
+      gestureHandling: 'greedy'
+    }
+    window.campusModalMap.mapOptions = options;
+
+    campusModalMap.selectCampus = function (campusId) {
+      CCV.selectCampus(campusId)
+    }
+    campusModalMap.draw()
+    CCV.campusModalMapHasBeenDrawn = true
+  }
+}
+
+$(window).on('googleMapsIsLoaded', function(){
+   CCV.showMapCampusModal();
+});
