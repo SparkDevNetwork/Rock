29dee0df9ef6a92677abb29815860beb5d4dc70a
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-05-23 17:01:27 -0700

    - Add CacheManager block

---
+++ RockWeb/Blocks/Administration/CacheManager.ascx
@@ -0,0 +1,25 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="CacheManager.ascx.cs" Inherits="CacheManager" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <div class="panel panel-block">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-tachometer"></i>Cache Manager</h1>
+            </div>
+            <div class="panel-body">
+                <Rock:NotificationBox ID="nbMessage" runat="server" Visible="false" />
+                <asp:ValidationSummary ID="valSummary" runat="server" HeaderText="Please Correct the Following" CssClass="alert alert-validation" />
+
+                <div class="row">
+                    <div class="col-md-6"> put the cache tags here </div>
+                    <div class="col-md-3"> put the cache stats here </div>
+                    <div class="col-md-3"> put the clear cache button and ddl here</div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Blocks/Administration/CacheManager.ascx.cs
@@ -0,0 +1,42 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Cache;
+using Rock.Data;
+using Rock.Model;
+using Rock.SystemKey;
+using Rock.Utility.Settings.DataAutomation;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+[DisplayName( "Cache Manager" )]
+[Category( "Administration" )]
+[Description( "Block used to view cache statistics and clear the existing cache." )]
+public partial class CacheManager : RockBlock
+{
+    protected void Page_Load( object sender, EventArgs e )
+    {
+
+    }
+}
