8c898218f5874a8aa7e07c8cf470535036e40e01
Mike Peterson <mike@sparkabilitygroup.com>
2018-06-04 14:46:59 -0700

    - more progress on ConentChannelViewDetail

--- RockWeb/Blocks/Cms/ContentChannelViewDetail.ascx
+++ RockWeb/Blocks/Cms/ContentChannelViewDetail.ascx
@@ -9,9 +9,72 @@
             <Rock:ModalDialog ID="mdSettings" runat="server" OnSaveClick="mdSettings_SaveClick">
                 <Content>
                     <div class="row">
+                        <div class="col-md-12">
+                            <Rock:RockDropDownList ID="ddlContentChannel" runat="server" Label="Content Channel" Help="Limits content channel items to a specific channel and enables Social Media Settings to be configurable, or leave blank to leave unrestricted." AutoPostBack="true" OnSelectedIndexChanged="ddlContentChannel_SelectedIndexChanged" />
+                            <Rock:RockTextBox ID="tbContentChannelQueryParameter" runat="server" Label="Content Channel Query Parameter" Help="Specify the URL parameter to use to determine which Content Channel Item to show, or leave blank to use whatever the first parameter is. 
+The type of the value will determine how the content channel item will be determined as follows:
+
+Integer - ContentChannelItem Id
+String - ContentChannelItem Slug
+Guid - ContentChannelItem Guid" />
+
+                            <Rock:CodeEditor ID="ceLavaTemplate" runat="server" Label="Lava Template" Help="The template to use when formatting the content channel item." EditorMode="Lava" EditorTheme="Rock" EditorHeight="200" />
+                            <Rock:NumberBox ID="rbOutputCacheDuration" runat="server" Label="Output Cache Duration" Help="Number of seconds to cache the resolved output. Only cache the output if you are not personalizing the output based on current user, current page, or any other merge field value." />
+                            <Rock:RockCheckBox ID="cbSetPageTitle" runat="server" Label="Set Page Title" Help="Determines if the block should set the page title with the channel name or content item." />
+                        </div>
+                    </div>
+                    <div class="row">
+
                         <div class="col-md-6">
+                            <fieldset>
+                                <legend>Interactions</legend>
+
+                                <Rock:RockDropDownList ID="ddlInteractionChannel" runat="server" Label="Interaction Channel" Help="The Interaction Channel to log interactions to. Leave blank to not log interactions." AutoPostBack="true" OnSelectedIndexChanged="ddlInteractionChannel_SelectedIndexChanged" />
+                                <Rock:RockTextBox ID="tbInteractionOperation" runat="server" Label="Interaction Operation" />
+                                <Rock:RockCheckBox ID="cbWriteInteractionOnlyIfIndividualLoggedIn" runat="server" Label="Write Interaction Only If Individual Logged In" Help="Set to true to only launch a workflow for logged in users, or set to false to launch for both logged in and anonymous users." />
+
+
+                            </fieldset>
+
+                            <fieldset>
+                                <legend>Social Media Settings</legend>
+                                <Rock:RockDropDownList ID="ddlMetaDescriptionAttribute" Label="Meta Description Attribute" Help="Attribute to use for the page's meta description." runat="server" />
+
+                                <Rock:RockDropDownList ID="tbOpenGraphType" Label="Open Graph Type (og:type)" runat="server">
+                                    <asp:ListItem />
+                                    <asp:ListItem Text="article" />
+                                    <asp:ListItem Text="website" />
+                                    <asp:ListItem Text="book" />
+                                    <asp:ListItem Text="place" />
+                                    <asp:ListItem Text="product" />
+                                    <asp:ListItem Text="profile" />
+                                    <asp:ListItem Text="video.episode" />
+                                    <asp:ListItem Text="video.movie" />
+                                    <asp:ListItem Text="video.other" />
+                                    <asp:ListItem Text="video.tv_show" />
+                                </Rock:RockDropDownList>
+                                <Rock:RockDropDownList ID="ddlOpenGraphTitleAttribute" Label="Open Graph Title (og:title) Attribute" runat="server" />
+                                <Rock:RockDropDownList ID="ddlOpenGraphDescriptionAttribute" Label="Open Graph Description (og:description) Attribute" runat="server" />
+                                <Rock:RockDropDownList ID="ddlOpenGraphImageAttribute" Label="Open Graph Image (og:image) Attribute" runat="server" />
+
+                                <Rock:RockDropDownList ID="ddlTwitterTitle" Label="Twitter Title (twitter:title) Attribute" runat="server" />
+                                <Rock:RockDropDownList ID="ddlTwitterDescription" Label="Twitter Description (twitter:description) Attribute" runat="server" />
+                                <Rock:RockDropDownList ID="ddlTwitterImage" Label="Twitter Image (twitter:image) Attribute" runat="server" />
+                                <Rock:RockRadioButtonList ID="rbTwitterCard" Label="Twitter Card (twitter:card)" runat="server">
+                                    <asp:ListItem Text="none" Value="none" />
+                                    <asp:ListItem Text="Summary" Value="summary" />
+                                    <asp:ListItem Text="Large Image" Value="summary_large_image" />
+                                </Rock:RockRadioButtonList>
+
+                            </fieldset>
                         </div>
                         <div class="col-md-6">
+                            <fieldset>
+                                <legend>Workflows</legend>
+                                <Rock:WorkflowTypePicker ID="wtpWorkflowType" runat="server" Label="Workflow Type" Help="The workflow type to launch when the content is viewed." OnSelectItem="wtpWorkflowType_SelectItem" />
+                                <Rock:RockCheckBox ID="cbLaunchWorkflowOnlyIfIndividualLoggedIn" runat="server" Label="Launch Workflow Only If Individual Logged In" Help="Set to true to only launch a workflow for logged in users, or set to false to launch for both logged in and anonymous users." />
+                                <Rock:RockCheckBox ID="cbLaunchWorkflowOncePerPerson" runat="server" Label="Launch Workflow Once Per Person" />
+                            </fieldset>
                         </div>
                     </div>
                 </Content>
--- RockWeb/Blocks/Cms/ContentChannelViewDetail.ascx.cs
+++ RockWeb/Blocks/Cms/ContentChannelViewDetail.ascx.cs
@@ -45,26 +45,48 @@ namespace RockWeb.Blocks.Cms
     [Category( "CMS" )]
     [Description( "Block to display a specific content channel item." )]
 
-    // Block Properties
-    [LavaCommandsField( "Enabled Lava Commands", "The Lava commands that should be enabled for this content channel item block.", false, order: 0 )]
 
-    // Custom Settings
-    [ContentChannelTypeField( "Content Channel Type", "The content channel type that the content channel item ", false, "", "CustomSetting" )]
+    [LavaCommandsField( "Enabled Lava Commands", "The Lava commands that should be enabled for this content channel item block.", false, category: "CustomSetting" )]
+
+    [ContentChannelField( "Content Channel", "Limits content channel items to a specific channel, or leave blank to leave unrestricted.", false, "", category: "CustomSetting" )]
+    [TextField( "Content Channel Query Parameter", @"
+Specify the URL parameter to use to determine which Content Channel Item to show, or leave blank to use whatever the first parameter is. 
+The type of the value will determine how the content channel item will be determined as follows:
+
+Integer - ContentChannelItem Id
+String - ContentChannelItem Slug
+Guid - ContentChannelItem Guid
+
+", required: false, category: "CustomSetting" )]
+
+
     [CodeEditorField( "Lava Template", "The template to use when formatting the content channel item.", CodeEditorMode.Lava, CodeEditorTheme.Rock, 200, false, category: "CustomSetting", defaultValue: @"
 <h1>{{ Item.Title }}</h1>
 {{ Item.Content }}" )]
 
-    [IntegerField( "Output Cache Duration", "Number of seconds to cache the resolved output. Only cache the output if you are not personalizing the output based on current user, current page, or any other merge field value.", false, 0, "CustomSetting", 0, "OutputCacheDuration" )]
-    [BooleanField( "Set Page Title", "Determines if the block should set the page title with the channel name or content item.", false, "CustomSetting" )]
+    [IntegerField( "Output Cache Duration", "Number of seconds to cache the resolved output. Only cache the output if you are not personalizing the output based on current user, current page, or any other merge field value.", required: false, key: "OutputCacheDuration", category: "CustomSetting" )]
+    [BooleanField( "Set Page Title", "Determines if the block should set the page title with the channel name or content item.", category: "CustomSetting" )]
 
-    [TextField( "Meta Description Attribute", "Attribute to use for storing the description attribute.", false, "", "CustomSetting" )]
-    //[TextField( "Meta Image Attribute", "Attribute to use for storing the image attribute.", false, "", "CustomSetting" )]
+    [InteractionChannelField( "Interaction Channel", "The Interaction Channel to log interactions to. Leave blank to not log interactions.", category: "CustomSetting", required: false )]
+    [TextField( "Interaction Operation", "", defaultValue: "View", category: "Interactions" )]
+    [BooleanField( "Write Interaction Only If Individual Logged In", "Set to true to only write interactions for logged in users, or set to false to write interactions for both logged in and anonymous users.", category: "CustomSetting" )]
 
-    [InteractionChannelField( "Interaction Channel", "", category: "CustomSetting" )]
-    [TextField( "Interaction Operation", "", category: "CustomSetting", defaultValue: "View" )]
+    [WorkflowTypeField( "Workflow Type", "The workflow type to launch when the content is viewed.", category: "CustomSetting" )]
+    [BooleanField( "Launch Workflow Only If Individual Logged In", "Set to true to only launch a workflow for logged in users, or set to false to launch for both logged in and anonymous users.", category: "CustomSetting" )]
+    [BooleanField( "Launch Workflow Once Per Person", "This setting keeps the workflow from launching on subsequent views by the same person. For this to remain accurate the workflows of this type should not be deleted.", category: "CustomSetting" )]
+
+    [TextField( "Meta Description Attribute", required: false, category: "CustomSetting" )]
+    [TextField( "Open Graph Type", required: false, category: "CustomSetting" )]
+    [TextField( "Open Graph Title Attribute", required: false, category: "CustomSetting" )]
+    [TextField( "Open Graph Description Attribute", required: false, category: "CustomSetting" )]
+    [TextField( "Open Graph Image Attribute", required: false, category: "CustomSetting" )]
+    [TextField( "Twitter Title Attribute", required: false, category: "CustomSetting" )]
+    [TextField( "Twitter Title Description", required: false, category: "CustomSetting" )]
+    [TextField( "Twitter Title Image", required: false, category: "CustomSetting" )]
+    [TextField( "Twitter Title Attribute", required: false, category: "CustomSetting" )]
+    [TextField( "Twitter Card", required: false, category: "CustomSetting" )]
     public partial class ContentChannelViewDetail : RockBlockCustomSettings
     {
-
         #region Settings
 
         /// <summary>
@@ -73,8 +95,33 @@ namespace RockWeb.Blocks.Cms
         protected override void ShowSettings()
         {
             pnlSettings.Visible = true;
+            ddlContentChannel.Items.Clear();
+            ddlContentChannel.Items.Add( new ListItem() );
+            foreach ( var contentChannel in CacheContentChannel.All().OrderBy( a => a.Name ) )
+            {
+                ddlContentChannel.Items.Add( new ListItem( contentChannel.Name, contentChannel.Guid.ToString() ) );
+            }
 
-            // TODO
+            ddlContentChannel.SetValue( this.GetAttributeValue( "ContentChannel" ).AsGuidOrNull() );
+
+            tbContentChannelQueryParameter.Text = this.GetAttributeValue( "ContentChannelQueryParameter" );
+            ceLavaTemplate.Text = this.GetAttributeValue( "LavaTemplate" );
+            rbOutputCacheDuration.Text = this.GetAttributeValue( "OutputCacheDuration" );
+            cbSetPageTitle.Checked = this.GetAttributeValue( "SetPageTitle" ).AsBoolean();
+
+            ddlInteractionChannel.Items.Clear();
+            ddlInteractionChannel.Items.Add( new ListItem() );
+            foreach ( var interactionChannel in CacheInteractionChannel.All().OrderBy( a => a.Name ) )
+            {
+                ddlInteractionChannel.Items.Add( new ListItem( interactionChannel.Name, interactionChannel.Guid.ToString() ) );
+            }
+
+
+            tbInteractionOperation.Text = this.GetAttributeValue( "InteractionOperation" );
+            cbWriteInteractionOnlyIfIndividualLoggedIn.Checked = this.GetAttributeValue( "WriteInteractionOnlyIfIndividualLoggedIn" ).AsBoolean();
+
+            //ddlMetaDescriptionAttribute
+
 
             mdSettings.Show();
         }
@@ -96,5 +143,20 @@ namespace RockWeb.Blocks.Cms
         }
 
         #endregion Settings
+
+        protected void ddlContentChannel_SelectedIndexChanged( object sender, EventArgs e )
+        {
+
+        }
+
+        protected void ddlInteractionChannel_SelectedIndexChanged( object sender, EventArgs e )
+        {
+
+        }
+
+        protected void wtpWorkflowType_SelectItem( object sender, EventArgs e )
+        {
+
+        }
     }
 }
