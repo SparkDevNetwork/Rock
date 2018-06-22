15b59934c1f0728143b6152763ddc5dab0fa98e2
Mike Peterson <mike@sparkabilitygroup.com>
2018-06-22 16:46:14 -0700

    - Add VersionInfo to Rock.WebStartup

--- Rock.WebStartup/Properties/AssemblyInfo.cs
+++ Rock.WebStartup/Properties/AssemblyInfo.cs
@@ -15,7 +15,6 @@
 // </copyright>
 //
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Web;
 
@@ -23,13 +22,6 @@ using System.Web;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rock.WebStartup")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rock.WebStartup")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -38,19 +30,6 @@ using System.Web;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("515e22e4-4b9b-4886-8477-e5b312b75eb4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // The following attribute will set an initializer to be run early in the ASP.Net pipeline on application
 // start. Your can find more information on how this works at: https://haacked.com/archive/2010/05/16/three-hidden-extensibility-gems-in-asp-net-4.aspx/
