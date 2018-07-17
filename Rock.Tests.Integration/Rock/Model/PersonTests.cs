e97a6b2622e648802843371c6e3ff02eb1b0407b
nairdo <nairdo@gmail.com>
2018-07-16 20:58:24 -0700

    + Added new integration tests project.  See Rock.Tests.Integration README for details.

---
+++ Rock.Tests.Integration/Rock/Model/PersonTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Rock.Model;
+using Rock.Cache;
+
+namespace Rock.Tests.Integration.Rock.Model
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void GraduatesThisYear()
+        {
+            InitGlobalAttributesCache();
+            var Person = new Person();
+            Person.GradeOffset = 1;
+
+            Assert.IsTrue( Person.GraduationYear == RockDateTime.Now.AddYears( 1 ).Year );
+        }
+
+        private static void InitGlobalAttributesCache()
+        {
+            DateTime today = RockDateTime.Now;
+            CacheGlobalAttributes globalAttributes = CacheGlobalAttributes.Get();
+            globalAttributes.SetValue( "GradeTransitionDate", string.Format( "{0}/{1}", today.Month, today.Day ), false );
+        }
+    }
+}
