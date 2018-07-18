100988c6df84264e64a609778639506e5f741e19
Mike Peterson <mike@sparkabilitygroup.com>
2018-07-18 13:33:48 -0700

    - Fixed backwards binary-compatibility issue that was causing v7 plugins to get incorrect results when getting the Id or Guid property of a CacheObject.

--- Rock/Web/Cache/EntityItemCache.cs
+++ Rock/Web/Cache/EntityItemCache.cs
@@ -23,22 +23,70 @@ using Newtonsoft.Json;
 namespace Rock.Web.Cache
 {
     /// <summary>
-    /// Generic Item Cache
+    /// Generic Item Cache for Entity.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     [Serializable]
     [DataContract]
-    public abstract class ItemCache<T> : IItemCache
+    public abstract class EntityItemCache<T> : IItemCache
         where T : IItemCache
     {
-        private const string _AllRegion = "AllItems";
+        /***********
+            NOTE: The properties (Id, Guid, ForeignKey, etc) need to be declared here (on the bottom-most class) to support backward binary-compatibility with pre-v8 plugins.
+            It seems to be related to issues discussed in https://blogs.msdn.microsoft.com/ericlippert/2010/03/29/putting-a-base-in-the-middle/
 
-        // This static field will be different for each generic type. See (https://www.jetbrains.com/help/resharper/2018.1/StaticMemberInGenericType.html)
-        // This is intentional behaviour in this case.
-        private static readonly object _obj = new object();
+            So, this EntityItemCache<T> is intentionally somewhat of a duplicate of ItemCache<T> because of this issue. However, the duplicate methods call ItemCache<T> manually
+            to minimize the amount of duplicate code.
+        ************/
 
-        private static readonly string KeyPrefix = $"{typeof( T ).Name}";
-        private static string AllKey => $"{typeof( T ).Name}:All";
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the identifier 
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        [DataMember]
+        public virtual int Id { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier.
+        /// </summary>
+        /// <value>
+        /// The unique identifier.
+        /// </value>
+        [DataMember]
+        public virtual Guid Guid { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the foreign identifier.
+        /// </summary>
+        /// <value>
+        /// The foreign identifier.
+        /// </value>
+        [DataMember]
+        public virtual int? ForeignId { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the foreign unique identifier.
+        /// </summary>
+        /// <value>
+        /// The foreign unique identifier.
+        /// </value>
+        [DataMember]
+        public virtual Guid? ForeignGuid { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets the foreign key.
+        /// </summary>
+        /// <value>
+        /// The foreign key.
+        /// </value>
+        [DataMember]
+        public virtual string ForeignKey { get; protected set; }
+
+        #endregion
 
         #region Protected Methods
 
@@ -47,9 +95,9 @@ namespace Rock.Web.Cache
         /// </summary>
         /// <param name="key">The key.</param>
         /// <returns></returns>
-        internal protected static string QualifiedKey( string key )
+        protected static string QualifiedKey( string key )
         {
-            return $"{KeyPrefix}:{key}";
+            return ItemCache<T>.QualifiedKey( key );
         }
 
         /// <summary>
@@ -58,9 +106,9 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         /// <param name="itemFactory">The item factory.</param>
         /// <returns></returns>
-        internal protected static T GetOrAddExisting( int key, Func<T> itemFactory )
+        protected static T GetOrAddExisting( int key, Func<T> itemFactory )
         {
-            return GetOrAddExisting( key.ToString(), itemFactory );
+            return ItemCache<T>.GetOrAddExisting( key, itemFactory );
         }
 
         /// <summary>
@@ -70,9 +118,9 @@ namespace Rock.Web.Cache
         /// <param name="itemFactory">The item factory.</param>
         /// <param name="expiration">The expiration.</param>
         /// <returns></returns>
-        internal protected static T GetOrAddExisting( int key, Func<T> itemFactory, TimeSpan expiration )
+        protected static T GetOrAddExisting( int key, Func<T> itemFactory, TimeSpan expiration )
         {
-            return GetOrAddExisting( key.ToString(), itemFactory, expiration );
+            return ItemCache<T>.GetOrAddExisting( key, itemFactory, expiration );
         }
 
         /// <summary>
@@ -81,9 +129,9 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         /// <param name="itemFactory">The item factory.</param>
         /// <returns></returns>
-        internal protected static T GetOrAddExisting( string key, Func<T> itemFactory )
+        protected static T GetOrAddExisting( string key, Func<T> itemFactory )
         {
-            return GetOrAddExisting( key, itemFactory, TimeSpan.MaxValue );
+            return ItemCache<T>.GetOrAddExisting( key, itemFactory );
         }
 
         /// <summary>
@@ -93,28 +141,9 @@ namespace Rock.Web.Cache
         /// <param name="itemFactory">The item factory.</param>
         /// <param name="expiration">The expiration.</param>
         /// <returns></returns>
-        internal protected static T GetOrAddExisting( string key, Func<T> itemFactory, TimeSpan expiration )
+        protected static T GetOrAddExisting( string key, Func<T> itemFactory, TimeSpan expiration )
         {
-            string qualifiedKey = QualifiedKey( key );
-
-            var value = RockCacheManager<T>.Instance.Cache.Get( qualifiedKey );
-
-            RockCache.UpdateCacheHitMiss( key, value != null );
-
-            if ( value != null )
-            {
-                return value;
-            }
-
-            if ( itemFactory == null ) return default( T );
-
-            value = itemFactory();
-            if ( value != null )
-            {
-                UpdateCacheItem( key, value, expiration );
-            }
-
-            return value;
+            return ItemCache<T>.GetOrAddExisting( key, itemFactory, expiration );
         }
 
         /// <summary>
@@ -123,46 +152,9 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         /// <param name="item">The item.</param>
         /// <param name="expiration">The expiration.</param>
-        internal protected static void UpdateCacheItem( string key, T item, TimeSpan expiration )
+        protected static void UpdateCacheItem( string key, T item, TimeSpan expiration )
         {
-            string qualifiedKey = QualifiedKey( key );
-
-            // Add the item to cache
-            RockCacheManager<T>.Instance.AddOrUpdate( qualifiedKey, item, expiration );
-
-            // Do any postcache processing that this item cache type may need to do
-            item.PostCached();
-
-            AddToAllIds( key );
-        }
-
-        /// <summary>
-        /// Ensure that the Key is part of the AllIds list
-        /// </summary>
-        /// <param name="key">The key.</param>
-        private static void AddToAllIds( string key )
-        {
-            // Get the dictionary of all item ids
-            var allKeys = RockCacheManager<List<string>>.Instance.Cache.Get( AllKey, _AllRegion );
-            if ( allKeys == null )
-            {
-                // All hasn't been asked for yet, so it doesn't need to be updated. Leave it null
-                return;
-            }
-
-            if ( allKeys.Contains( key ) )
-            {
-                // already has it so no need to update the cache
-                return;
-            }
-
-            // If the key is not part of the dictionary all ready
-            lock ( _obj )
-            {
-                // Add it.
-                allKeys.Add( key, true );
-                RockCacheManager<List<string>>.Instance.AddOrUpdate( AllKey, _AllRegion, allKeys );
-            }
+            ItemCache<T>.UpdateCacheItem( key, item, expiration );
         }
 
         /// <summary>
@@ -170,30 +162,18 @@ namespace Rock.Web.Cache
         /// </summary>
         /// <returns></returns>
         /// <param name="keyFactory">All keys factory.</param>
-        internal protected static List<string> GetOrAddKeys( Func<List<string>> keyFactory )
+        protected static List<string> GetOrAddKeys( Func<List<string>> keyFactory )
         {
-            var value = RockCacheManager<List<string>>.Instance.Cache.Get( AllKey, _AllRegion );
-            if ( value != null )
-            {
-                return value;
-            }
-
-            return keyFactory == null ? new List<string>() : AddKeys( keyFactory );
+            return ItemCache<T>.GetOrAddKeys( keyFactory );
         }
 
         /// <summary>
         /// Adds the keys.
         /// </summary>
         /// <param name="keyFactory">All keys factory.</param>
-        internal protected static List<string> AddKeys( Func<List<string>> keyFactory )
+        protected static List<string> AddKeys( Func<List<string>> keyFactory )
         {
-            var allKeys = keyFactory?.Invoke();
-            if ( allKeys != null )
-            {
-                RockCacheManager<List<string>>.Instance.AddOrUpdate( AllKey, _AllRegion, allKeys );
-            }
-
-            return allKeys;
+            return ItemCache<T>.AddKeys( keyFactory );
         }
 
         #endregion
@@ -229,7 +209,7 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         public static void Remove( int key )
         {
-            Remove( key.ToString() );
+            ItemCache<T>.Remove( key );
         }
 
         /// <summary>
@@ -239,12 +219,12 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         internal static void FlushItem( int key )
         {
-            FlushItem( key.ToString() );
+            ItemCache<T>.FlushItem( key );
         }
 
         internal static void AddToAllIds( int key)
         {
-            AddToAllIds( key.ToString() );
+            ItemCache<T>.AddToAllIds( key );
         }
 
         /// <summary>
@@ -254,9 +234,7 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         internal static void FlushItem( string key )
         {
-            var qualifiedKey = QualifiedKey( key );
-
-            RockCacheManager<T>.Instance.Cache.Remove( qualifiedKey );
+            ItemCache<T>.FlushItem( key );
         }
 
         /// <summary>
@@ -265,17 +243,7 @@ namespace Rock.Web.Cache
         /// <param name="key">The key.</param>
         public static void Remove( string key )
         {
-            FlushItem( key );
-
-            var allIds = RockCacheManager<List<string>>.Instance.Cache.Get( AllKey, _AllRegion ) ?? new List<string>();
-            if ( !allIds.Contains( key ) )
-                return;
-
-            lock ( _obj )
-            {
-                allIds.Remove( key );
-                RockCacheManager<List<string>>.Instance.AddOrUpdate( AllKey, _AllRegion, allIds );
-            }
+            ItemCache<T>.Remove( key );
         }
 
         /// <summary>
@@ -283,10 +251,8 @@ namespace Rock.Web.Cache
         /// </summary>
         public static void Clear()
         {
-            RockCacheManager<T>.Instance.Cache.Clear();
-            RockCacheManager<List<string>>.Instance.Cache.Remove( AllKey, _AllRegion );
+            ItemCache<T>.Clear();
         }
-
 
         /// <summary>
         /// Method that is called by the framework immediately after being added to cache
