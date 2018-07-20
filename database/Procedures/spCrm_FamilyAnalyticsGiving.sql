7c4afdc934748fdb7aae5f24ccf3a27fb96787ca
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2018-07-20 03:19:09 -0700

    - Fixed compiler warnings and code generation.

--- database/Procedures/spCrm_FamilyAnalyticsGiving.sql
+++ database/Procedures/spCrm_FamilyAnalyticsGiving.sql
@@ -26,8 +26,7 @@
 	    DECLARE @cACTIVE_RECORD_STATUS_VALUE_GUID uniqueidentifier = '618F906C-C33D-4FA3-8AEF-E58CB7B63F1E'
 	    DECLARE @cPERSON_RECORD_TYPE_VALUE_GUID uniqueidentifier = '36CF10D6-C695-413D-8E7C-4546EFEF385E'
 	    DECLARE @cFAMILY_GROUPTYPE_GUID uniqueidentifier = '790E3215-3B10-442B-AF69-616C0DCB998E'
-	    DECLARE @cADULT_ROLE_GUID uniqueidentifier = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42'	
-	    DECLARE @cCONTRIBUTION_TYPE_VALUE_GUID uniqueidentifier = '2D607262-52D6-4724-910D-5C6E8FB89ACC'
+	    DECLARE @cADULT_ROLE_GUID uniqueidentifier = '2639F9A5-2AAE-4E48-A8C3-4FFE86681E42'
 
 	    DECLARE @cATTRIBUTE_FIRST_GAVE uniqueidentifier  = 'EE5EC76A-D4B9-56B5-4B48-29627D945F10'
 	    DECLARE @cATTRIBUTE_LAST_GAVE uniqueidentifier  = '02F64263-E290-399E-4487-FC236F4DE81F'
@@ -40,7 +39,6 @@
 	    DECLARE @PersonRecordTypeValueId int = (SELECT TOP 1 [Id] FROM [DefinedValue] WHERE [Guid] = @cPERSON_RECORD_TYPE_VALUE_GUID)
 	    DECLARE @FamilyGroupTypeId int = (SELECT TOP 1 [Id] FROM [GroupType] WHERE [Guid] = @cFAMILY_GROUPTYPE_GUID)
 	    DECLARE @AdultRoleId int = (SELECT TOP 1 [Id] FROM [GroupTypeRole] WHERE [Guid] = @cADULT_ROLE_GUID)
-	    DECLARE @ContributionTypeId int = (SELECT TOP 1 [Id] FROM [DefinedValue] WHERE [Guid] = @cCONTRIBUTION_TYPE_VALUE_GUID)
 
 	
 	    -- calculate dates for queries
@@ -88,7 +86,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[GivingGroupId] = i.[GivingGroupId]
-							    AND ft.TransactionTypeValueId = @ContributionTypeId) AS [FirstContributionDate]
+							    AND fa.[IsTaxDeductible] = 'true') AS [FirstContributionDate]
 			    , 0 AS [IsSystem]
 			    , newid() AS [Guid]
 			    , getdate() AS [CreateDate]
@@ -132,7 +130,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[Id] = i.[PersonId] -- match by person id
-							    AND ft.TransactionTypeValueId = @ContributionTypeId) AS [FirstContributionDate]
+							    AND fa.[IsTaxDeductible] = 'true') AS [FirstContributionDate]
 			    , 0 AS [IsSystem]
 			    , newid() AS [Guid]
 			    , getdate() AS [CreateDate]
@@ -179,7 +177,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[GivingGroupId] = i.[GivingGroupId]
-							    AND ft.TransactionTypeValueId = @ContributionTypeId) AS [LastContributionDate]
+							    AND fa.[IsTaxDeductible] = 'true') AS [LastContributionDate]
 			    , 0 AS [IsSystem]
 			    , newid() AS [Guid]
 			    , getdate() AS [CreateDate]
@@ -223,7 +221,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[Id] = i.[PersonId]
-							    AND ft.TransactionTypeValueId = @ContributionTypeId) AS [LastContributionDate]
+							    AND fa.[IsTaxDeductible] = 'true') AS [LastContributionDate]
 			    , 0 AS [IsSystem]
 			    , newid() AS [Guid]
 			    , getdate() AS [CreateDate]
@@ -270,7 +268,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[GivingGroupId] = i.[GivingGroupId]
-							    AND ft.TransactionTypeValueId = @ContributionTypeId
+							    AND fa.[IsTaxDeductible] = 'true'
 							    AND ft.TransactionDateTime >= @SundayGivingDurationShort
 							    AND ft.TransactionDateTime <= @SundayDateStart) AS [GiftCountDurationShort]
 			    , 0 AS [IsSystem]
@@ -316,7 +314,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[Id] = i.[PersonId] -- match by person id
-							    AND ft.TransactionTypeValueId = @ContributionTypeId
+							    AND fa.[IsTaxDeductible] = 'true'
 							    AND ft.TransactionDateTime >= @SundayGivingDurationShort
 							    AND ft.TransactionDateTime <= @SundayDateStart) AS [GiftCountDurationShort]
 			    , 0 AS [IsSystem]
@@ -365,7 +363,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[GivingGroupId] = i.[GivingGroupId]
-							    AND ft.TransactionTypeValueId = @ContributionTypeId
+							    AND fa.[IsTaxDeductible] = 'true'
 							    AND ft.TransactionDateTime >= @SundayGivingDurationLong
 							    AND ft.TransactionDateTime <= @SundayDateStart) AS [GiftCountDurationLong]
 			    , 0 AS [IsSystem]
@@ -411,7 +409,7 @@
 							    INNER JOIN [FinancialAccount] fa ON fa.[Id] = ftd.AccountId
 						    WHERE 
 							    gp.[Id] = i.[PersonId] -- match by person id
-							    AND ft.TransactionTypeValueId = @ContributionTypeId
+							    AND fa.[IsTaxDeductible] = 'true'
 							    AND ft.TransactionDateTime >= @SundayGivingDurationLong
 							    AND ft.TransactionDateTime <= @SundayDateStart) AS [GiftCountDurationLong]
 			    , 0 AS [IsSystem]
