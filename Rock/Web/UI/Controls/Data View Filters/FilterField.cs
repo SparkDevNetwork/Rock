SparkDevNetwork/Rock

commit 3df97833db105224169b9700d64f94f8f87174ba

- Fixed Reporting and DataViews to enforce required fields that are part of Data Select and Data Filter components

Rock/Web/UI/Controls/Data View Filters/FilterField.cs
@@ -279,6 +279,39 @@ namespace Rock.Web.UI.Controls
         }
 
         /// <summary>
+        /// Gets or sets the validation group.
+        /// </summary>
+        /// <value>
+        /// The validation group.
+        /// </value>
+        public string ValidationGroup
+        {
+            get
+            {
+                return ViewState["ValidationGroup"] as string;
+            }
+
+            set
+            {
+                ViewState["ValidationGroup"] = value;
+                SetFilterControlsValidationGroup( value );
+            }
+        }
+
+        /// <summary>
+        /// Sets the filter controls validation group.
+        /// </summary>
+        /// <param name="validationGroup">The validation group.</param>
+        private void SetFilterControlsValidationGroup( string validationGroup )
+        {
+            var rockBlock = this.RockBlock();
+            if ( filterControls != null && rockBlock != null )
+            {
+                rockBlock.SetValidationGroup( filterControls, validationGroup );
+            }
+        }
+
+        /// <summary>
         /// Gets whether the Checkbox is checked or not (not factoring in if it is showing)
         /// </summary>
         /// <value>
@@ -444,6 +477,8 @@ namespace Rock.Web.UI.Controls
             {
                 filterControls = new Control[0];
             }
+
+            SetFilterControlsValidationGroup( this.ValidationGroup );
 
             ddlFilterType.AutoPostBack = true;
             ddlFilterType.SelectedIndexChanged += ddlFilterType_SelectedIndexChanged;
@@ -611,7 +646,7 @@ namespace Rock.Web.UI.Controls
             {
                 //// EntityFieldFilter renders the checkbox itself (see EntityFieldFilter.cs),
                 //// so only render the checkbox if we are hiding filter criteria and it isn't an entity field filter
-                if ( !( component is Rock.Reporting.DataFilter.EntityFieldFilter ) || HideFilterCriteria)
+                if ( !( component is Rock.Reporting.DataFilter.EntityFieldFilter ) || HideFilterCriteria )
                 {
                     cbIncludeFilter.Text = this.Label;
                     cbIncludeFilter.RenderControl( writer );
@@ -649,7 +684,7 @@ namespace Rock.Web.UI.Controls
 
         void ddlFilterType_SelectedIndexChanged( object sender, EventArgs e )
         {
-            FilterEntityTypeName = ( (DropDownList)sender ).SelectedValue;
+            FilterEntityTypeName = ( ( DropDownList ) sender ).SelectedValue;
 
             if ( SelectionChanged != null )
             {

Rock/Web/UI/RockBlock.cs
@@ -1043,29 +1043,40 @@ namespace Rock.Web.UI
         /// <param name="validationGroup">A <see cref="System.String"/> representing the name of the validation group.</param>
         public void SetValidationGroup( ControlCollection controls, string validationGroup )
         {
+            Control[] controlArray = controls.OfType<Control>().ToArray();
+            SetValidationGroup( controlArray, validationGroup );
+        }
+
+        /// <summary>
+        /// Sets the validation group.
+        /// </summary>
+        /// <param name="controls">The controls to include in the validation group.</param>
+        /// <param name="validationGroup">A <see cref="System.String" /> representing the name of the validation group.</param>
+        public void SetValidationGroup( Control[] controls, string validationGroup )
+        {
             if ( controls != null )
             {
                 foreach ( Control control in controls )
                 {
                     if ( control is Rock.Web.UI.Controls.IHasValidationGroup )
                     {
-                        var rockControl = (Rock.Web.UI.Controls.IHasValidationGroup)control;
+                        var rockControl = ( Rock.Web.UI.Controls.IHasValidationGroup ) control;
                         rockControl.ValidationGroup = SetValidationGroup( rockControl.ValidationGroup, validationGroup );
                     }
 
                     if ( control is ValidationSummary )
                     {
-                        var validationSummary = (ValidationSummary)control;
+                        var validationSummary = ( ValidationSummary ) control;
                         validationSummary.ValidationGroup = SetValidationGroup( validationSummary.ValidationGroup, validationGroup );
                     }
                     else if ( control is BaseValidator )
                     {
-                        var validator = (BaseValidator)control;
+                        var validator = ( BaseValidator ) control;
                         validator.ValidationGroup = SetValidationGroup( validator.ValidationGroup, validationGroup );
                     }
                     else if ( control is IButtonControl )
                     {
-                        var button = (IButtonControl)control;
+                        var button = ( IButtonControl ) control;
                         button.ValidationGroup = SetValidationGroup( button.ValidationGroup, validationGroup );
                     }
                     else

RockWeb/Blocks/Reporting/ReportDetail.ascx.cs
@@ -1279,8 +1279,22 @@ namespace RockWeb.Blocks.Reporting
                 var dataSelectComponent = GetDataSelectComponent( rockContext, fieldSelection.AsInteger() );
                 if ( dataSelectComponent != null )
                 {
-                    dataSelectComponent.CreateChildControls( phDataSelectControls );
+                    Control[] dataSelectControls = dataSelectComponent.CreateChildControls( phDataSelectControls );
+                    SetDataSelectControlsValidationGroup( dataSelectControls, this.BlockValidationGroup );
                 }
+            }
+        }
+
+        /// <summary>
+        /// Sets the data select controls validation group.
+        /// </summary>
+        /// <param name="dataSelectControls">The data select controls.</param>
+        /// <param name="validationGroup">The validation group.</param>
+        private void SetDataSelectControlsValidationGroup( Control[] dataSelectControls, string validationGroup )
+        {
+            if ( dataSelectControls != null )
+            {
+                this.SetValidationGroup( dataSelectControls, validationGroup );
             }
         }
 

RockWeb/Blocks/Reporting/DataViewDetail.ascx.cs
@@ -1016,6 +1016,7 @@ $(document).ready(function() {
         {
             FilterGroup groupControl = sender as FilterGroup;
             FilterField filterField = new FilterField();
+            filterField.ValidationGroup = this.BlockValidationGroup;
             filterField.DataViewFilterGuid = Guid.NewGuid();
             filterField.DeleteClick += filterControl_DeleteClick;
             groupControl.Controls.Add( filterField );
@@ -1117,6 +1118,7 @@ $(document).ready(function() {
                 if ( filter.ExpressionType == FilterExpressionType.Filter )
                 {
                     var filterControl = new FilterField();
+                    filterControl.ValidationGroup = this.BlockValidationGroup;
                     parentControl.Controls.Add( filterControl );
                     filterControl.DataViewFilterGuid = filter.Guid;
                     filterControl.ID = string.Format( "ff_{0}", filterControl.DataViewFilterGuid.ToString( "N" ) );

commit 0a76ed1e3350bf15b2c5b50ce177e2ccbc8cacdf

- Fixed null exception in filter controls that got introduced with 3df97833db105224169b9700d64f94f8f87174ba

Rock/Web/UI/Controls/Data View Filters/FilterField.cs
@@ -305,7 +305,7 @@ namespace Rock.Web.UI.Controls
         private void SetFilterControlsValidationGroup( string validationGroup )
         {
             var rockBlock = this.RockBlock();
-            if ( filterControls != null && rockBlock != null )
+            if ( filterControls != null && rockBlock != null && validationGroup != null )
             {
                 rockBlock.SetValidationGroup( filterControls, validationGroup );
             }

Rock/Web/UI/RockBlock.cs
@@ -1054,7 +1054,7 @@ namespace Rock.Web.UI
         /// <param name="validationGroup">A <see cref="System.String" /> representing the name of the validation group.</param>
         public void SetValidationGroup( Control[] controls, string validationGroup )
         {
-            if ( controls != null )
+            if ( controls != null && validationGroup != null )
             {
                 foreach ( Control control in controls )
                 {

RockWeb/Blocks/Reporting/ReportDetail.ascx.cs
@@ -1292,7 +1292,7 @@ namespace RockWeb.Blocks.Reporting
         /// <param name="validationGroup">The validation group.</param>
         private void SetDataSelectControlsValidationGroup( Control[] dataSelectControls, string validationGroup )
         {
-            if ( dataSelectControls != null )
+            if ( dataSelectControls != null && validationGroup != null )
             {
                 this.SetValidationGroup( dataSelectControls, validationGroup );
             }