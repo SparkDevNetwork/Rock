SparkDevNetwork/Rock

commit fbcb68b78ed9ea68876b9771e265d5b31d1917ea

+ Added type="tel" to telephone inputs

Rock/Web/UI/Controls/PhoneNumberBox.cs
@@ -237,12 +237,12 @@ namespace Rock.Web.UI.Controls
         /// </value>
         public string CountryCode
         {
-            get 
+            get
             {
                 EnsureChildControls();
                 return _hfCountryCode.Value;
             }
-            set 
+            set
             {
                 EnsureChildControls();
                 if ( !string.IsNullOrWhiteSpace( value ) )
@@ -501,11 +501,13 @@ namespace Rock.Web.UI.Controls
                 this.Attributes["placeholder"] = Placeholder;
             }
 
+            this.Attributes["type"] = "tel";
+
             base.RenderControl( writer );
 
             writer.RenderEndTag();              // div.input-group
 
             this.CssClass = cssClass;
-        }   
+        }
     }
 }

commit 15a5816b89031774404e55c8c5ed0ee3f70d76e7

- Fixes phone formatting bug introduced by commit fbcb68b

Rock/Web/UI/Controls/PhoneNumberBox.cs
@@ -350,7 +350,7 @@ namespace Rock.Web.UI.Controls
         }
     }
 
-    $('div.phone-number-box input:text').on('change', function(e) {
+    $('div.phone-number-box input.js-phone-format').on('change', function(e) {
         phoneNumberBoxFormatNumber($(this));
     });
 
@@ -358,7 +358,7 @@ namespace Rock.Web.UI.Controls
         e.preventDefault();
         $(this).closest('div.input-group').find('input:hidden').val($(this).html());
         $(this).closest('div.input-group-btn').find('button').html($(this).html() + ' <span class=""caret""></span>');
-        phoneNumberBoxFormatNumber($(this).closest('div.input-group').find('input:text').first());
+        phoneNumberBoxFormatNumber($(this).closest('div.input-group').find('input.js-phone-format').first());
     });
 " );
 
@@ -495,7 +495,7 @@ namespace Rock.Web.UI.Controls
 
             _hfCountryCode.RenderControl( writer );
 
-            ( (WebControl)this ).AddCssClass( "form-control" );
+            ( (WebControl)this ).AddCssClass( "form-control js-phone-format" );
             if ( !string.IsNullOrWhiteSpace( Placeholder ) )
             {
                 this.Attributes["placeholder"] = Placeholder;
@@ -503,11 +503,11 @@ namespace Rock.Web.UI.Controls
 
             this.Attributes["type"] = "tel";
 
+            ( ( WebControl ) this ).AddCssClass( cssClass );
+
             base.RenderControl( writer );
 
             writer.RenderEndTag();              // div.input-group
-
-            this.CssClass = cssClass;
         }
     }
 }