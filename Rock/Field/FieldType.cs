SparkDevNetwork/Rock

commit 609b62e0574adab00d39a1c0dbcf82015a83a55d

- Cleaned up FieldType so it doesn't include CompareValue with CompareType is IsBlank/IsNotBlank

Rock/Field/FieldType.cs
@@ -435,10 +435,21 @@ namespace Rock.Field
                         values.Add( compare );
                     }
 
-                    string value = GetFilterValueValue( filterControl.Controls[1].Controls[0], configurationValues );
-                    if ( value != null )
+                    ComparisonType? comparisonType = compare.ConvertToEnumOrNull<ComparisonType>();
+                    if ( comparisonType.HasValue )
                     {
-                        values.Add( value );
+                        if ( ( ComparisonType.IsBlank | ComparisonType.IsNotBlank ).HasFlag( comparisonType.Value ) )
+                        {
+                            // if using IsBlank or IsNotBlank, we don't care about the value, so don't try to grab it from the UI
+                        }
+                        else
+                        {
+                            string value = GetFilterValueValue( filterControl.Controls[1].Controls[0], configurationValues );
+                            if ( value != null )
+                            {
+                                values.Add( value );
+                            }
+                        }
                     }
                 }
                 catch

commit 5a7a8fcbbe083dd2460714c5c5ccfa3def9bad72

- Fixed issue that was introduced in 609b62e0574adab00d39a1c0dbcf82015a83a55d preventing SingleSelect, etc DataFields from working

Rock/Field/FieldType.cs
@@ -436,21 +436,20 @@ namespace Rock.Field
                     }
 
                     ComparisonType? comparisonType = compare.ConvertToEnumOrNull<ComparisonType>();
-                    if ( comparisonType.HasValue )
+                    if ( comparisonType.HasValue && ( ComparisonType.IsBlank | ComparisonType.IsNotBlank ).HasFlag( comparisonType.Value ) )
                     {
-                        if ( ( ComparisonType.IsBlank | ComparisonType.IsNotBlank ).HasFlag( comparisonType.Value ) )
+                        // if using IsBlank or IsNotBlank, we don't care about the value, so don't try to grab it from the UI
+                        values.Add( string.Empty );
+                    }
+                    else
+                    {
+                        string value = GetFilterValueValue( filterControl.Controls[1].Controls[0], configurationValues );
+                        if ( value != null )
                         {
-                            // if using IsBlank or IsNotBlank, we don't care about the value, so don't try to grab it from the UI
-                        }
-                        else
-                        {
-                            string value = GetFilterValueValue( filterControl.Controls[1].Controls[0], configurationValues );
-                            if ( value != null )
-                            {
-                                values.Add( value );
-                            }
+                            values.Add( value );
                         }
                     }
+
                 }
                 catch
                 {