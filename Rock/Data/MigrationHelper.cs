SparkDevNetwork/Rock

commit 1ef9bb68e3015bb3273742b8e5491852c2008cec

- Update MigrationHelper to support adding Site global blocks

Rock/Data/MigrationHelper.cs
@@ -857,6 +857,28 @@ namespace Rock.Data
         /// <param name="guid">The unique identifier.</param>
         public void AddBlock( bool skipIfAlreadyExists, string pageGuid, string layoutGuid, string blockTypeGuid, string name, string zone, string preHtml, string postHtml, int order, string guid )
         {
+            AddBlock( skipIfAlreadyExists, pageGuid.AsGuidOrNull(), layoutGuid.AsGuidOrNull(), null, blockTypeGuid.AsGuidOrNull(), name, zone, preHtml, postHtml, order, guid );
+        }
+
+        /// <summary>
+        /// Adds a new Block of the given block type to the given page (optional) or layout (optional) or site (optional),
+        /// setting its values with the given parameter values. If only the layout is given,
+        /// edit/configuration authorization will also be inserted into the Auth table
+        /// for the admin role (GroupId 2).
+        /// </summary>
+        /// <param name="skipIfAlreadyExists">if set to <c>true</c> [skip if already exists].</param>
+        /// <param name="pageGuid">The page unique identifier.</param>
+        /// <param name="layoutGuid">The layout unique identifier.</param>
+        /// <param name="siteGuid">The site unique identifier.</param>
+        /// <param name="blockTypeGuid">The block type unique identifier.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="zone">The zone.</param>
+        /// <param name="preHtml">The pre HTML.</param>
+        /// <param name="postHtml">The post HTML.</param>
+        /// <param name="order">The order.</param>
+        /// <param name="guid">The unique identifier.</param>
+        public void AddBlock( bool skipIfAlreadyExists, Guid? pageGuid, Guid? layoutGuid, Guid? siteGuid, Guid? blockTypeGuid, string name, string zone, string preHtml, string postHtml, int order, string guid )
+        {
             var sb = new StringBuilder();
             sb.Append( @"
                 DECLARE @PageId int
@@ -864,20 +886,30 @@ namespace Rock.Data
 
                 DECLARE @LayoutId int
                 SET @LayoutId = null
+
+                DECLARE @SiteId int
+                SET @SiteId = null
 " );
 
-            if ( !string.IsNullOrWhiteSpace( pageGuid ) )
+            if ( pageGuid.HasValue )
             {
                 sb.AppendFormat( @"
                 SET @PageId = (SELECT [Id] FROM [Page] WHERE [Guid] = '{0}')
 ", pageGuid );
             }
 
-            if ( !string.IsNullOrWhiteSpace( layoutGuid ) )
+            if ( layoutGuid.HasValue )
             {
                 sb.AppendFormat( @"
                 SET @LayoutId = (SELECT [Id] FROM [Layout] WHERE [Guid] = '{0}')
 ", layoutGuid );
+            }
+
+            if ( siteGuid.HasValue )
+            {
+                sb.AppendFormat( @"
+                SET @SiteId = (SELECT [Id] FROM [Site] WHERE [Guid] = '{0}')
+", siteGuid );
             }
 
             sb.AppendFormat( @"
@@ -889,11 +921,11 @@ namespace Rock.Data
 
                 DECLARE @BlockId int
                 INSERT INTO [Block] (
-                    [IsSystem],[PageId],[LayoutId],[BlockTypeId],[Zone],
+                    [IsSystem],[PageId],[LayoutId],[SiteId],[BlockTypeId],[Zone],
                     [Order],[Name],[PreHtml],[PostHtml],[OutputCacheDuration],
                     [Guid])
                 VALUES(
-                    1,@PageId,@LayoutId,@BlockTypeId,'{1}',
+                    1,@PageId,@LayoutId,@SiteId,@BlockTypeId,'{1}',
                     {2},'{3}','{4}','{5}',0,
                     '{6}')
                 SET @BlockId = SCOPE_IDENTITY()
@@ -906,8 +938,8 @@ namespace Rock.Data
                     postHtml.Replace( "'", "''" ),
                     guid );
 
-            // If adding a layout block, give edit/configuration authorization to admin role
-            if ( string.IsNullOrWhiteSpace( pageGuid ) )
+            // If adding a layout or site block, give edit/configuration authorization to admin role
+            if ( !pageGuid.HasValue )
                 sb.Append( @"
                 INSERT INTO [Auth] ([EntityTypeId],[EntityId],[Order],[Action],[AllowOrDeny],[SpecialRole],[GroupId],[Guid])
                     VALUES(@EntityTypeId,@BlockId,0,'Edit','A',0,2,NEWID())

commit c5bbf21745c8c81a323d79d7c10b2cf8c846cc29

- Fix for MigrationHelper adding a new block incorrectly (introduced with 1ef9bb68e3015bb3273742b8e5491852c2008cec)

Rock/Data/MigrationHelper.cs
@@ -905,7 +905,8 @@ namespace Rock.Data
 ", layoutGuid );
             }
 
-            if ( siteGuid.HasValue )
+            // if this is Site Global block (no Page or Layout), set the SiteId
+            if ( !pageGuid.HasValue && !layoutGuid.HasValue && siteGuid.HasValue )
             {
                 sb.AppendFormat( @"
                 SET @SiteId = (SELECT [Id] FROM [Site] WHERE [Guid] = '{0}')