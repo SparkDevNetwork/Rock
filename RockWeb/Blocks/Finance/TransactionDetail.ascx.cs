e6c288652d0faf0640fa7ee96105787065aac478
nairdo <nairdo@gmail.com>
2018-10-10 13:57:16 -0700

    - Do not allow changing a transaction's payment gateway unless it is new.

--- RockWeb/Blocks/Finance/TransactionDetail.ascx.cs
+++ RockWeb/Blocks/Finance/TransactionDetail.ascx.cs
@@ -369,6 +369,7 @@ namespace RockWeb.Blocks.Finance
                     txn = new FinancialTransaction();
                     txnService.Add( txn );
                     txn.BatchId = batchId;
+                    txn.FinancialGatewayId = gpPaymentGateway.SelectedValueAsInt();
                 }
                 else
                 {
@@ -393,7 +394,8 @@ namespace RockWeb.Blocks.Finance
                 txn.TransactionDateTime = dtTransactionDateTime.SelectedDateTime;
                 txn.TransactionTypeValueId = ddlTransactionType.SelectedValue.AsInteger();
                 txn.SourceTypeValueId = ddlSourceType.SelectedValueAsInt();
-                txn.FinancialGatewayId = gpPaymentGateway.SelectedValueAsInt();
+                // DO NOT ALLOW changing a payment gateway once it's already saved.
+                //txn.FinancialGatewayId = gpPaymentGateway.SelectedValueAsInt();
                 txn.TransactionCode = tbTransactionCode.Text;
                 txn.FinancialPaymentDetail.CurrencyTypeValueId = ddlCurrencyType.SelectedValueAsInt();
                 txn.FinancialPaymentDetail.CreditCardTypeValueId = ddlCreditCardType.SelectedValueAsInt();
