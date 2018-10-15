49f622253f603da69d4c61744a7dc8c4fa502ca7
GerhardPowell <gerhard@sparkdevnetwork.org>
2018-10-15 16:04:58 -0700

    - Hide DefinedValueRange filter because it is not supported

--- Rock/Field/Types/DefinedValueRangeFieldType.cs
+++ Rock/Field/Types/DefinedValueRangeFieldType.cs
@@ -301,6 +301,20 @@ namespace Rock.Field.Types
         #region Filter Control
 
         /// <summary>
+        /// Creates the control needed to filter (query) values using this field type.
+        /// </summary>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="id">The identifier.</param>
+        /// <param name="required">if set to <c>true</c> [required].</param>
+        /// <param name="filterMode">The filter mode.</param>
+        /// <returns></returns>
+        public override System.Web.UI.Control FilterControl( System.Collections.Generic.Dictionary<string, ConfigurationValue> configurationValues, string id, bool required, Rock.Reporting.FilterMode filterMode )
+        {
+            // This field type does not support filtering
+            return null;
+        }
+
+        /// <summary>
         /// Gets the filter compare control.
         /// </summary>
         /// <param name="configurationValues">The configuration values.</param>
@@ -312,6 +326,15 @@ namespace Rock.Field.Types
         {
             // This fieldtype does not support filtering
             return null;
+        }
+
+        /// <summary>
+        /// Determines whether this filter has a filter control
+        /// </summary>
+        /// <returns></returns>
+        public override bool HasFilterControl()
+        {
+            return false;
         }
 
         #endregion
