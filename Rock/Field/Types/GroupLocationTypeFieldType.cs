74e3b8faf7105387731f0e076edec4e523b801e1
GerhardPowell <gerhard@sparkdevnetwork.org>
2018-10-18 15:56:14 -0700

    - Fixed GroupLocationType filter not showing control if there is no value in the list

--- Rock/Field/Types/GroupLocationTypeFieldType.cs
+++ Rock/Field/Types/GroupLocationTypeFieldType.cs
@@ -159,7 +159,7 @@ namespace Rock.Field.Types
         {
             ListControl editControl;
 
-            editControl = new Rock.Web.UI.Controls.RockDropDownList { ID = id }; 
+            editControl = new Rock.Web.UI.Controls.RockDropDownList { ID = id };
             editControl.Items.Add( new ListItem() );
 
             if ( configurationValues != null && configurationValues.ContainsKey( GROUP_TYPE_KEY ) )
@@ -168,26 +168,22 @@ namespace Rock.Field.Types
                 if ( Guid.TryParse( configurationValues[GROUP_TYPE_KEY].Value, out groupTypeGuid ) )
                 {
                     var groupType = GroupTypeCache.Get( groupTypeGuid );
-                    if (groupType != null)
+                    if ( groupType != null )
                     {
                         var locationTypeValues = groupType.LocationTypeValues;
                         if ( locationTypeValues != null )
                         {
-                            if ( locationTypeValues.Any() )
+                            foreach ( var locationTypeValue in locationTypeValues )
                             {
-                                foreach ( var locationTypeValue in locationTypeValues )
-                                {
-                                    editControl.Items.Add( new ListItem( locationTypeValue.Value, locationTypeValue.Id.ToString() ) );
-                                }
-
-                                return editControl;
+                                editControl.Items.Add( new ListItem( locationTypeValue.Value, locationTypeValue.Id.ToString() ) );
                             }
+
                         }
                     }
                 }
             }
             
-            return null;
+            return editControl;
         }
 
         /// <summary>
@@ -208,10 +204,10 @@ namespace Rock.Field.Types
                     definedValue = DefinedValueCache.Get( definedValueId.Value );
                 }
 
-                return definedValue?.Guid.ToString();
+                return definedValue?.Guid.ToString() ?? string.Empty;
             }
 
-            return string.Empty;
+            return null;
         }
 
         /// <summary>
