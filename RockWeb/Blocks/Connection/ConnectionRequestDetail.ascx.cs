86cc407c61cb1508754609d390c36c521a29c42d
Bob Rufenacht <33356643+bobrufenacht@users.noreply.github.com>
2018-11-16 16:01:48 -0500

    Closes #3421
    Closes #3421 Adds Attributes to Connection Requests.

--- RockWeb/Blocks/Connection/ConnectionRequestDetail.ascx.cs
+++ RockWeb/Blocks/Connection/ConnectionRequestDetail.ascx.cs
@@ -178,6 +178,14 @@ namespace RockWeb.Blocks.Connection
             else
             {
                 hfGroupMemberAttributeValues.Value = GetGroupMemberAttributeValues();
+
+                var connectionRequest = new ConnectionRequest();
+                connectionRequest.Id = hfConnectionRequestId.ValueAsInt();
+                connectionRequest.ConnectionOpportunityId = hfConnectionOpportunityId.ValueAsInt();
+                connectionRequest.LoadAttributes();
+                phEditAttributes.Controls.Clear();
+                Rock.Attribute.Helper.AddEditControls( connectionRequest, phEditAttributes, true, BlockValidationGroup );
+
             }
         }
 
@@ -410,7 +418,11 @@ namespace RockWeb.Blocks.Connection
                         connectionRequestService.Add( connectionRequest );
                     }
 
+                    connectionRequest.LoadAttributes( rockContext );
+                    Rock.Attribute.Helper.GetEditValues( phEditAttributes, connectionRequest );
+
                     rockContext.SaveChanges();
+                    connectionRequest.SaveAttributeValues( rockContext );
 
                     if ( newConnectorPersonAliasId.HasValue && !newConnectorPersonAliasId.Equals( oldConnectorPersonAliasId ) )
                     {
@@ -1439,6 +1451,11 @@ namespace RockWeb.Blocks.Connection
                     else
                     {
                         ShowEditDetails( connectionRequest, rockContext );
+
+                        // This is adding a request, show as Edit since it doesn't exist yet.
+                        connectionRequest.LoadAttributes();
+                        phEditAttributes.Controls.Clear();
+                        Rock.Attribute.Helper.AddEditControls( connectionRequest, phEditAttributes, false, BlockValidationGroup );
                     }
                 }
             }
@@ -1632,6 +1649,10 @@ namespace RockWeb.Blocks.Connection
                         lblWorkflows.Visible = false;
                     }
                 }
+
+                connectionRequest.LoadAttributes();
+                phAttributes.Controls.Clear();
+                Rock.Attribute.Helper.AddDisplayControls( connectionRequest, phAttributes, null, false, false );
 
                 BindConnectionRequestActivitiesGrid( connectionRequest, new RockContext() );
                 BindConnectionRequestWorkflowsGrid();
