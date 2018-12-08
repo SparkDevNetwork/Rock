76235c1109972ba046314a01faf9a5340979871a
Justin Eveland <jbeveland27@gmail.com>
2018-12-07 21:35:46 -0500

    Added AppVeyor deploy scripts

---
+++ RockWeb/before-deploy.ps1
@@ -0,0 +1,82 @@
+# This script is run by AppVeyor's deploy agent before the deploy
+Import-Module WebAdministration
+
+# Get the application (web root) and the root folder
+$webroot = $env:RockWebRootPath
+$rootfolder = Split-Path -Parent $webroot
+
+Write-Output "Running pre-deploy script"
+Write-Output "--------------------------------------------------"
+Write-Output "Root folder: $rootfolder"
+Write-Output "Web root folder: $webroot"
+Write-Output "Running script as: $env:userdomain\$env:username"
+
+# stop execution of the deploy if the moves fail
+$ErrorActionPreference = "Stop"
+
+### stop web publishing service - needed to allow the deploy to overwrite the sql server spatial types
+### Write-Host "Stopping Web Publishing Service"
+### stop-service -servicename w3svc
+
+# stop web site and app pool
+$Site = Get-WebSite -Name "$env:APPLICATION_SITE_NAME"
+If ( $Site.State -eq "Started" ) {
+	Write-Host "Stopping Website"
+	Stop-Website -Name "$env:APPLICATION_SITE_NAME"
+}
+If ( (Get-WebAppPoolState -Name $Site.applicationPool).Value -eq "Started" ) {
+	Write-Host "Stopping ApplicationPool"
+	Stop-WebAppPool -Name $Site.applicationPool
+}
+
+# wait for 10 seconds before continuing
+Start-Sleep 10
+
+# delete the contents of the temp directory or create it
+If (Test-Path "$rootfolder\temp"){
+	Remove-Item "$rootfolder\temp\*" -Force -Confirm:$False -Recurse
+} Else {
+  New-Item -ItemType Directory -Force -Path "$rootfolder\temp"
+}
+
+# delete the contents of the temp\Plugins directory or create it
+If (Test-Path "$rootfolder\temp\Plugins"){
+	Remove-Item "$rootfolder\temp\Plugins\*" -Force -Confirm:$False -Recurse
+} Else {
+  New-Item -ItemType Directory -Force -Path "$rootfolder\temp\Plugins"
+}
+
+# move content folder to temp
+#Write-Host "Moving content folder to temp directory"
+#Move-Item "$webroot\Content" "$rootfolder\temp\" -Force
+
+# move LEGACY content folder to temp
+#Write-Host "Moving LEGACY ARENA folder to temp directory"
+#Move-Item "$webroot\arena" "$rootfolder\temp\" -Force
+
+# move App_Data to temp
+#Write-Host "Moving App_Data folder to temp directory"
+#Move-Item "$webroot\App_Data" "$rootfolder\temp\" -Force
+
+# move custom themes to temp
+#Write-Host "Moving Themes\Ulfberht folder to temp directory"
+#Move-Item "$webroot\Themes\Ulfberht" "$rootfolder\temp\" -Force
+
+# move custom Children's check-in theme to temp (since it contains live files)
+# Write-Host "Moving Themes\CheckinKids_CentralAZ folder to temp directory"
+# If (Test-Path "$rootfolder\temp\CheckinKids_CentralAZ"){
+# 	Remove-Item "$rootfolder\temp\CheckinKids_CentralAZ\*" -Force -Confirm:$False -Recurse
+# } Else {
+#   New-Item -ItemType Directory -Force -Path "$rootfolder\temp\CheckinKids_CentralAZ"
+# }
+#Move-Item "$webroot\Themes\CheckinKids_CentralAZ\Assets" "$rootfolder\temp\CheckinKids_CentralAZ\Assets" -Force
+
+# move non com_lcbcchurch plugins to temp\Plugins folder
+Write-Host "Moving non com_lcbcchurch Plugins to temp Plugins directory"
+$files = GCI -path "$webroot\Plugins\" | Where-Object {$_.name -ne "com_lcbcchurch" -and $_.name -ne ".gitignore" -and $_.name -ne "readme.txt"}
+foreach ($file in $files) { Move-Item  "$webroot\Plugins\$file" -Destination "$rootfolder\temp\Plugins" -Force }
+
+# move a robots file if it exists
+If (Test-Path "$webroot\robots.txt"){
+	Move-Item "$webroot\robots.txt" "$rootfolder\temp"
+}
---
+++ RockWeb/deploy.ps1
@@ -0,0 +1,99 @@
+# This script is run by AppVeyor's deploy agent after the deploy
+Import-Module WebAdministration
+
+# Get the application (web root) and the root folder
+$webroot = $env:RockWebRootPath
+$rootfolder = Split-Path -Parent $webroot
+
+Write-Output "Running post-deploy script"
+Write-Output "--------------------------------------------------"
+Write-Output "Root folder: $rootfolder"
+Write-Output "Web root folder: $webroot"
+Write-Output "Running script as: $env:userdomain\$env:username"
+
+# ensure that the compilation debug is false
+(Get-Content "$webroot\web.config").Replace('<compilation debug="true"', '<compilation debug="false"') | Set-Content "$webroot\web.config"
+
+# delete the content directory if it exists as it was added by the deploy
+# If (Test-Path "$webroot\Content"){
+# 	Remove-Item "$webroot\Content" -Force -Confirm:$False -Recurse
+# }
+
+# move Content directory back from temp
+# Write-Host "Moving Contents folder back from temp directory"
+# Move-Item "$rootfolder\temp\Content" "$webroot\"
+
+# move LEGACY directory back from temp
+# Write-Host "Moving LEGACY ARENA folder back from temp directory"
+# Move-Item "$rootfolder\temp\arena" "$webroot\"
+
+# move App_Data directory back from temp
+# Write-Host "Moving App_Data folder back from temp directory"
+# Move-Item "$rootfolder\temp\App_Data" "$webroot\"
+
+# move custom themes back from temp
+# Write-Host "Moving Themes\Ulfberht folder back from temp directory"
+# Move-Item "$rootfolder\temp\Ulfberht" "$webroot\Themes"
+
+# remove any items that were deployed into the CheckinKids_CentralAZ Assets folder
+# ...otherwise the move back from temp will not work correctly.
+# If (Test-Path "$webroot\Themes\CheckinKids_CentralAZ\Assets"){
+# 	Remove-Item "$webroot\Themes\CheckinKids_CentralAZ\Assets" -Force -Confirm:$False -Recurse
+# }
+
+# move custom Children's check-in theme Assets folder back from temp
+# Write-Host "Moving Themes\CheckinKids_CentralAZ\Assets folder back from temp directory"
+# Move-Item "$rootfolder\temp\CheckinKids_CentralAZ\Assets" "$webroot\Themes\CheckinKids_CentralAZ" -Force
+
+# move non com_lcbcchurch plugins back from temp\Plugins folder
+Write-Host "Moving non com_lcbcchurch Plugins back from temp Plugins directory"
+$files = GCI -path "$rootfolder\temp\Plugins" | Where-Object {$_.name -ne "com_lcbcchurch"}
+foreach ($file in $files) { Move-Item  "$rootfolder\temp\Plugins\$file" -Destination "$webroot\Plugins\" -Force }
+
+# move a robots file back from temp if it exists
+If (Test-Path "$rootfolder\temp\robots.txt"){
+	Write-Host "Moving robots.txt file back from temp directory"
+	Move-Item "$rootfolder\temp\robots.txt" "$webroot"
+}
+
+# copy any dlls from the manual-fix\bin folder to the web bin folder
+Write-Host "Copying any manual-fix\bin DLLs to web bin folder"
+$files = GCI -path "$rootfolder\manual-fixes\bin"
+foreach ($file in $files) { Copy-Item  "$rootfolder\manual-fixes\bin\$file" -Destination "$webroot\bin\" -Force}
+
+# copy new connection string file
+Write-Host "Copying new web.ConnectionStrings.config to web dir"
+Copy-Item "$rootfolder\config\web.ConnectionStrings.config" $webroot -force
+
+# start web publishing service
+Write-Host "Starting Web Publishing Service"
+start-service -servicename w3svc
+
+# start web site and app pool
+Write-Host "Starting ApplicationPool and Website"
+Start-WebAppPool -Name (Get-Website -Name "$env:APPLICATION_SITE_NAME").applicationPool
+Start-Website -Name "$env:APPLICATION_SITE_NAME"
+
+# create empty migration flag
+New-Item "$webroot\App_Data\Run.Migration" -type file -force
+
+# set acl on migration flag file so the app has permissions to delete it
+Write-Host "Setting read-write on the Run.Migration file"
+$acl = Get-ACL "$webroot\App_Data\Run.Migration"
+$accessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone","FullControl","Allow")
+$acl.AddAccessRule($accessRule)
+Set-Acl "$webroot\App_Data\Run.Migration" $acl
+
+# delete deploy scripts
+If (Test-Path "$webroot\deploy.ps1"){
+	Remove-Item "$webroot\deploy.ps1"
+}
+
+If (Test-Path "$webroot\before-deploy.ps1"){
+	Remove-Item "$webroot\before-deploy.ps1"
+}
+
+# delete the appveyor deploy cache
+If (Test-Path c:\appveyor){
+	Remove-Item c:\appveyor -Force -Confirm:$False -Recurse
+}
