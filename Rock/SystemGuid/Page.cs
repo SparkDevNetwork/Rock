bb86e8771335d7a55883a88d371f09f61b3da3e9
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2019-01-14 16:48:23 -0700

    - Added insert of post lava merge message text to CommunicationRecipient.SentMessage and use that for display in the SMS Conversations block. - Added insert of incoming SMS message to CommunicationResponse even if the Phone number is configured to forward the message to a response recipient.

--- Rock/SystemGuid/Page.cs
+++ Rock/SystemGuid/Page.cs
@@ -1394,10 +1394,18 @@ namespace Rock.SystemGuid
         public const string SITES = "7596D389-4EAB-4535-8BEE-229737F46F44";
 
         /// <summary>
-        /// Gets the SMS From Values page guid
+        /// Gets the SMS Phone Numbers page guid
         /// ParentPage: Communications
         /// </summary>
+        [Obsolete("Use SMS_PHONE_NUMBERS instead")]
+        [RockObsolete("9.0")]
         public const string SMS_FROM_VALUES = "3F1EA6E5-6C61-444A-A80E-5B66F96F521B";
+
+        /// <summary>
+        /// The SMS phone numbers
+        /// ParentPage: Communications
+        /// </summary>
+        public const string SMS_PHONE_NUMBERS = "3F1EA6E5-6C61-444A-A80E-5B66F96F521B";
 
         /// <summary>
         /// Gets the SQL Command page guid
