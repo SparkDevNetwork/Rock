4bfef6ada0e45a2c435a3b2526127dd18a4a60aa
sam-crisp <44408754+sam-crisp@users.noreply.github.com>
2019-02-06 13:33:20 -0600

    BEMA: Add Last Updated Column to Workflow lists
    Created a custom column on all workflow grids that shows the last time an attribute was updated in the  workflow (NOT when the workflow modified datetime was, as this is updated at every process workflow job run).

---
+++ com.lcbcchurch.Workflow/Migrations/005_WorkflowListAddLastUpdated.cs
@@ -0,0 +1,43 @@
+﻿// <copyright>
+// Copyright by Central Christian Church
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock;
+using Rock.Plugin;
+namespace com.lcbcchurch.Workflow.Migrations
+{
+    [MigrationNumber( 5, "1.0.14" )]
+    public class WorkflowListAddLastUpdated : Migration
+    {
+        public override void Up()
+        {
+            // Add Custom Column to system block on Page 288: Workflow List, Manage Workflows
+
+            // Attrib Value for Block:Workflow List, Attribute:core.CustomGridColumnsConfig Page: Manage Workflows, Site: Rock RMS
+            RockMigrationHelper.AddBlockAttributeValue("BCC61035-DA99-47EE-A376-71D430455DB4","84B029A1-E1AD-4064-B1D2-C837C21BC5EF",@"{""ColumnsConfig"":[{""HeaderText"":""Last Updated"",""HeaderClass"":"""",""ItemClass"":"""",""LavaTemplate"":""{% workflow id:'{{Row.Id}}' %}\n {% assign lastModified = workflow.CreatedDateTime %}\n {% attribute where:'EntityTypeId == 113 && EntityTypeQualifierColumn == \""WorkflowTypeId\"" && EntityTypeQualifierValue == \""{{workflow.WorkflowTypeId}}\""'%}\n \n {% for attributeItem in attributeItems %}\n {% attributevalue where:'AttributeId == {{attributeItem.Id}} && EntityId == {{workflow.Id}}' %}\n {% for attributevalueItem in attributevalueItems %}\n {% if attributevalueItem.ModifiedDateTime > lastModified %}\n {% assign lastModified = attributevalueItem.ModifiedDateTime %}\n {% endif %}\n {% endfor %}\n {% endattributevalue %}\n {% endfor %}\n \n {% endattribute %}\n {{lastModified | HumanizeDateTime}}\n{% endworkflow %}"",""PositionOffsetType"":0,""PositionOffset"":7}]}");
+
+        }
+
+        public override void Down()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
