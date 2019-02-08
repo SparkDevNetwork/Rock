959dfbcb2d2f5c3b63c36644f7849de669179367
Justin Eveland <jbeveland27@gmail.com>
2019-02-08 00:54:02 -0500

    Fixed bad guids

--- com.lcbcchurch.Groups/Migrations/002_ModifyKidCrewMemberAttributes.cs
+++ com.lcbcchurch.Groups/Migrations/002_ModifyKidCrewMemberAttributes.cs
@@ -20,12 +20,12 @@ namespace com.lcbcchurch.Groups.Migrations
 
             // GroupType: kidCrew Adult Volunteers // Add Group Type Member Attribute: Environment
             RockMigrationHelper.AddGroupTypeGroupMemberAttribute( "093DAFC7-2F18-46D3-99F9-C3BEC4A8C07F", "BD0D9B57-2A41-4490-89FF-F01DAB7D4904", "Environment", @"", 2, "", "8E245EF6-5D65-441B-BF2D-C6B1CCFDDD64" );
-            RockMigrationHelper.AddAttributeQualifier( "8E245EF6-5D65-441B-BF2D-C6B1CCFDDD64", "values", "Sunrise Pointe,Oyster Bay,Treasure Cove,The Pier,Wheelhouse,Wheelhouse Jr.", "36123D22-A785-479D-82C2-BF4A3BBDDCEB" );
-            RockMigrationHelper.AddAttributeQualifier( "8E245EF6-5D65-441B-BF2D-C6B1CCFDDD64", "enhancedselection", "False", "4171D3D0-0143-4A77-A7C2-59A3B2503B2D" );
+            RockMigrationHelper.AddAttributeQualifier( "8E245EF6-5D65-441B-BF2D-C6B1CCFDDD64", "values", "Sunrise Pointe,Oyster Bay,Treasure Cove,The Pier,Wheelhouse,Wheelhouse Jr", "19ABB37D-D3EE-47CE-81F1-E5C79E9ACAAE" );
+            RockMigrationHelper.AddAttributeQualifier( "8E245EF6-5D65-441B-BF2D-C6B1CCFDDD64", "enhancedselection", "False", "5ED6B813-30A6-4B32-AFBA-39765D743E8A" );
 
             // GroupType: kidCrew Student Volunteers // Add Group Type Member Attribute: Environment
             RockMigrationHelper.AddGroupTypeGroupMemberAttribute( "ABD85A23-17F5-4EE0-A0C6-A88B6F8DFD77", "BD0D9B57-2A41-4490-89FF-F01DAB7D4904", "Environment", @"", 2, "", "55CF7957-8E1D-4428-9901-29B74F5FD988" );
-            RockMigrationHelper.AddAttributeQualifier( "55CF7957-8E1D-4428-9901-29B74F5FD988", "values", "Sunrise Pointe,Oyster Bay,Treasure Cove,The Pier,Wheelhouse,Wheelhouse Jr.", "7B056A11-6556-4B6A-B5B7-45BFAF6EAFB0" );
+            RockMigrationHelper.AddAttributeQualifier( "55CF7957-8E1D-4428-9901-29B74F5FD988", "values", "Sunrise Pointe,Oyster Bay,Treasure Cove,The Pier,Wheelhouse,Wheelhouse Jr", "7B056A11-6556-4B6A-B5B7-45BFAF6EAFB0" );
             RockMigrationHelper.AddAttributeQualifier( "55CF7957-8E1D-4428-9901-29B74F5FD988", "enhancedselection", "False", "EFCB9337-6A0E-46EB-A3C8-7FBFC16CE983" );
 
             // Set the new Group Member Attributes to be grid columns
