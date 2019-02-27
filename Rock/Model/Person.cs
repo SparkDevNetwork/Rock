SparkDevNetwork/Rock

commit 19e2a0825878e9a768de8bca5023b4f9d9fc5b4a

- Improve performance of Pledge Analytics and Giving Analytics by making Person.GivingLeaderId a persisted column instead of using a calculated field

Rock/Model/Person.cs
@@ -2031,6 +2031,7 @@ namespace Rock.Model
             // NOTE: This is also done on GroupMember.PostSaveChanges in case Role or family membership changes
             PersonService.UpdatePersonAgeClassification( this.Id, dbContext as RockContext );
             PersonService.UpdatePrimaryFamily( this.Id, dbContext as RockContext );
+            PersonService.UpdateGivingLeaderId( this.Id, dbContext as RockContext );
         }
 
         /// <summary>

Rock.Slingshot/BulkImport/BulkImportHelper.cs
@@ -1370,9 +1370,10 @@ UPDATE [AttributeValue] SET ValueAsDateTime =
                 Debug.WriteLine( "ValueAsDateTime RowsUpdated: " + rowsUpdated.ToString() );
             }
 
-            // since we bypassed Rock SaveChanges when Inserting Person records, sweep thru and ensure the AgeClassification and PrimaryFamily is set
+            // since we bypassed Rock SaveChanges when Inserting Person records, sweep thru and ensure the AgeClassification, PrimaryFamily, and GivingLeaderId is set
             PersonService.UpdatePersonAgeClassificationAll( rockContext );
             PersonService.UpdatePrimaryFamilyAll( rockContext );
+            PersonService.UpdateGivingLeaderIdAll( rockContext );
 
             stopwatchTotal.Stop();
             if ( personsToInsert.Any() || groupMemberRecordsToInsertList.Any() || familiesToInsert.Any() )

Rock/Jobs/RockCleanup.cs
@@ -296,6 +296,13 @@ namespace Rock.Jobs
                 int primaryFamilyUpdates = PersonService.UpdatePrimaryFamilyAll( personRockContext );
             }
 
+            // Ensures the GivingLeaderId is correct for all person records in the database
+            using ( var personRockContext = new Rock.Data.RockContext() )
+            {
+                personRockContext.Database.CommandTimeout = commandTimeout;
+                int primaryFamilyUpdates = PersonService.UpdateGivingLeaderIdAll( personRockContext );
+            }
+
             // update any updated or incorrect age classifications on persons
             using ( var personRockContext = new Rock.Data.RockContext() )
             {

Rock/Model/GroupMember.cs
@@ -486,6 +486,7 @@ namespace Rock.Model
                 {
                     PersonService.UpdatePersonAgeClassification( this.PersonId, dbContext as RockContext );
                     PersonService.UpdatePrimaryFamily( this.PersonId, dbContext as RockContext );
+                    PersonService.UpdateGivingLeaderId( this.PersonId, dbContext as RockContext );
                 }
             }
         }

Rock/Model/PersonService.Partial.cs
@@ -533,7 +533,8 @@ namespace Rock.Model
             /// <returns></returns>
             public int ConfidenceScore
             {
-                get {
+                get
+                {
                     int total = 0;
 
                     if ( FirstNameMatched )
@@ -3233,6 +3234,83 @@ FROM (
             p.PrimaryFamilyId IS NULL
             OR (p.PrimaryFamilyId != pf.CalculatedPrimaryFamilyId)
             )" );
+
+            if ( personId.HasValue )
+            {
+                sqlUpdateBuilder.Append( $" AND ( p.Id = @personId) " );
+            }
+
+            sqlUpdateBuilder.Append( @"    ) x " );
+
+            if ( personId.HasValue )
+            {
+                return rockContext.Database.ExecuteSqlCommand( sqlUpdateBuilder.ToString(), new System.Data.SqlClient.SqlParameter( "@personId", personId.Value ) );
+            }
+            else
+            {
+                return rockContext.Database.ExecuteSqlCommand( sqlUpdateBuilder.ToString() );
+            }
+        }
+
+        /// <summary>
+        /// Ensures the GivingLeaderId is correct for the specified person
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <param name="rockContext">The rock context.</param>
+        /// <returns></returns>
+        public static bool UpdateGivingLeaderId( int personId, RockContext rockContext )
+        {
+            int recordsUpdated = UpdatePersonGivingLeaderId( personId, rockContext );
+            return recordsUpdated != 0;
+        }
+
+        /// <summary>
+        /// Ensures the GivingLeaderId is correct for all person records in the database
+        /// </summary>
+        /// <param name="rockContext">The rock context.</param>
+        /// <returns></returns>
+        public static int UpdateGivingLeaderIdAll( RockContext rockContext )
+        {
+            return UpdatePersonGivingLeaderId( null, rockContext );
+        }
+
+        /// <summary>
+        /// Updates the person giving leader identifier for the specified person, or for all persons in the database if personId is null.
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <param name="rockContext">The rock context.</param>
+        /// <returns></returns>
+        private static int UpdatePersonGivingLeaderId( int? personId, RockContext rockContext )
+        {
+            var sqlUpdateBuilder = new StringBuilder();
+            sqlUpdateBuilder.Append( @"
+UPDATE x
+SET x.GivingLeaderId = x.CalculatedGivingLeaderId
+FROM (
+	SELECT p.Id
+		,p.NickName
+		,p.LastName
+		,p.GivingLeaderId
+		,isnull(pf.CalculatedGivingLeaderId, p.Id) CalculatedGivingLeaderId
+	FROM Person p
+	OUTER APPLY (
+		SELECT TOP 1 p2.[Id] CalculatedGivingLeaderId
+		FROM [GroupMember] gm
+		INNER JOIN [GroupTypeRole] r ON r.[Id] = gm.[GroupRoleId]
+		INNER JOIN [Person] p2 ON p2.[Id] = gm.[PersonId]
+		WHERE gm.[GroupId] = p.GivingGroupId
+			AND p2.[IsDeceased] = 0
+			AND p2.[GivingGroupId] = p.GivingGroupId
+		ORDER BY r.[Order]
+			,p2.[Gender]
+			,p2.[BirthYear]
+			,p2.[BirthMonth]
+			,p2.[BirthDay]
+		) pf
+	WHERE (
+			p.GivingLeaderId IS NULL
+			OR (p.GivingLeaderId != pf.CalculatedGivingLeaderId)
+			)" );
 
             if ( personId.HasValue )
             {

Rock/Plugin/HotFixes/068_MigrationRollupsForV8_7_5.cs
@@ -1,0 +1,75 @@
+﻿namespace Rock.Plugin.HotFixes
+{
+    [MigrationNumber( 68, "1.8.6" )]
+    public class MigrationRollupsForV8_7_5 : Migration
+    {
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+            CreatePersonGivingLeaderIdPersistedIndexed();
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+
+        }
+
+        /// <summary>
+        /// Re-creates the person GivingLeaderID column as a computed-persisted column and adds an index on it
+        /// </summary>
+        private void CreatePersonGivingLeaderIdPersistedIndexed()
+        {
+            Sql( @"IF EXISTS (
+		SELECT *
+		FROM sys.indexes
+		WHERE name = 'IX_GivingLeaderId'
+			AND object_id = OBJECT_ID('Person')
+		)
+BEGIN
+	DROP INDEX IX_GivingLeaderId ON [Person]
+END
+
+ALTER TABLE Person
+DROP COLUMN GivingLeaderId
+
+ALTER TABLE Person ADD GivingLeaderId INT NULL
+
+CREATE INDEX IX_GivingLeaderId ON [Person] ([GivingLeaderId])
+
+UPDATE x
+SET x.GivingLeaderId = x.CalculatedGivingLeaderId
+FROM (
+	SELECT p.Id
+		,p.NickName
+		,p.LastName
+		,p.GivingLeaderId
+		,isnull(pf.CalculatedGivingLeaderId, p.Id) CalculatedGivingLeaderId
+	FROM Person p
+	OUTER APPLY (
+		SELECT TOP 1 p2.[Id] CalculatedGivingLeaderId
+		FROM [GroupMember] gm
+		INNER JOIN [GroupTypeRole] r ON r.[Id] = gm.[GroupRoleId]
+		INNER JOIN [Person] p2 ON p2.[Id] = gm.[PersonId]
+		WHERE gm.[GroupId] = p.GivingGroupId
+			AND p2.[IsDeceased] = 0
+			AND p2.[GivingGroupId] = p.GivingGroupId
+		ORDER BY r.[Order]
+			,p2.[Gender]
+			,p2.[BirthYear]
+			,p2.[BirthMonth]
+			,p2.[BirthDay]
+		) pf
+	WHERE (
+			p.GivingLeaderId IS NULL
+			OR (p.GivingLeaderId != pf.CalculatedGivingLeaderId)
+			)
+	) x" );
+        }
+
+    }
+}

Rock/Rock.csproj
@@ -527,6 +527,7 @@
     <Compile Include="Plugin\HotFixes\063_WhitelistBlacklist.cs" />
     <Compile Include="Plugin\HotFixes\065_ThresholdValue.cs" />
     <Compile Include="Plugin\HotFixes\064_MigrationRollupsForV8_7_2.cs" />
+    <Compile Include="Plugin\HotFixes\068_MigrationRollupsForV8_7_5.cs" />
     <Compile Include="Plugin\HotFixes\067_MigrationRollupsForV8_7_4.cs" />
     <Compile Include="Plugin\HotFixes\066_MigrationRollupsForV8_7_3.cs" />
     <Compile Include="Plugin\HotFixes\HotFixMigrationResource.Designer.cs">

commit 846b529f2275553e1dde08874b3a4adaef9c6b15

- Fix exception when adding or editing a person record (introduced with https://github.com/sparkdevnetwork/rock/commit/19e2a0825878e9a768de8bca5023b4f9d9fc5b4a)

Rock/Model/Person.cs
@@ -324,15 +324,13 @@ namespace Rock.Model
         }
 
         /// <summary>
-        /// Gets or sets the giving leader identifier. This is a computed column and can be used
-        /// in LinqToSql queries, but there is no in-memory calculation. Avoid using property outside
-        /// a linq query
+        /// Gets or sets the giving leader identifier.
+        /// Note: This is computed on save, so any manual changes to this will be ignored.
         /// </summary>
         /// <value>
         /// The giving leader identifier.
         /// </value>
         [DataMember]
-        [DatabaseGenerated( DatabaseGeneratedOption.Computed )]
         public int GivingLeaderId { get; set; }
 
         /// <summary>
@@ -482,6 +480,7 @@ namespace Rock.Model
 
         /// <summary>
         /// Gets or sets the age classification of the Person.
+        /// Note: This is computed on save, so any manual changes to this will be ignored.
         /// </summary>
         /// <value>
         /// A <see cref="Rock.Model.AgeClassification"/> enum value representing the Person's age classification.  Valid values are <c>AgeClassification.Unknown</c> if the Person's age is unknown,
@@ -492,7 +491,8 @@ namespace Rock.Model
         public AgeClassification AgeClassification { get; set; }
 
         /// <summary>
-        /// Gets or sets the group id for the primary family
+        /// Gets or sets the group id for the primary family.
+        /// Note: This is computed on save, so any manual changes to this will be ignored.
         /// </summary>
         /// <value>
         /// The primary family id.