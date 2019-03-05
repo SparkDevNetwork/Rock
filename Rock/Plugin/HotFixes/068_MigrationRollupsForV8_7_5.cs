45bca75d3893ef1e4228a3c4ab283ab57fffb858
Jay Martinez <jay@sparkdevnetwork.org>
2019-03-05 10:26:59 -0700

    -added hot fix migrations for v8.7

---
+++ Rock/Plugin/HotFixes/068_MigrationRollupsForV8_7_5.cs
@@ -0,0 +1,39 @@
+﻿namespace Rock.Plugin.HotFixes
+{
+    [MigrationNumber( 68, "1.8.6" )]
+    public class MigrationRollupsForV8_7_5 : Migration
+    {
+        /// <summary>
+        /// The commands to run to migrate plugin to the specific version
+        /// </summary>
+        public override void Up()
+        {
+            UpdatedGoogleMapsShortCode();
+        }
+
+        /// <summary>
+        /// The commands to undo a migration from a specific version
+        /// </summary>
+        public override void Down()
+        {
+
+        }
+
+        /// <summary>
+        /// SK: Fixed Typo in Google Maps Lava Shortcode Marker
+        /// </summary>
+        private void UpdatedGoogleMapsShortCode()
+        {
+            Sql( @"UPDATE
+	[LavaShortcode]
+SET
+	[Documentation] = REPLACE([Documentation],'markerannimation','markeranimation'),
+	[Markup] = REPLACE([Markup],'markerannimation','markeranimation'),
+	[Parameters] = REPLACE([Parameters],'markerannimation','markeranimation')
+WHERE
+	[Guid]='FE298210-1307-49DF-B28B-3735A414CCA0'
+" );
+        }
+
+    }
+}
