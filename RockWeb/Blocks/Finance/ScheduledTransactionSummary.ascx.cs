d39e00b546a2a14b5f97b458ff93ff8eabbc6f59
Garrett Johnson <nvisiondesign@gmail.com>
2019-04-05 12:20:38 -0700

    - Rename references to liquid to lava in block descriptions and help text

--- RockWeb/Blocks/Finance/ScheduledTransactionSummary.ascx.cs
+++ RockWeb/Blocks/Finance/ScheduledTransactionSummary.ascx.cs
@@ -39,7 +39,7 @@ namespace RockWeb.Blocks.Finance
     [DisplayName( "Scheduled Transaction Summary" )]
     [Category( "Finance" )]
     [Description( "Block that shows a summary of the scheduled transactions for the currently logged in user." )]
-    [CodeEditorField( "Template", "Liquid template for the content to be placed on the page.", CodeEditorMode.Lava, CodeEditorTheme.Rock, 400, true, @"{% include '~~/Assets/Lava/ScheduledTransactionSummary.lava'  %}", "", 1 )]
+    [CodeEditorField( "Template", "Lava template for the content to be placed on the page.", CodeEditorMode.Lava, CodeEditorTheme.Rock, 400, true, @"{% include '~~/Assets/Lava/ScheduledTransactionSummary.lava'  %}", "", 1 )]
     [LinkedPage("Manage Scheduled Transactions Page", "Link to be used for managing an individual's scheduled transactions.", false, "", "", 3)]
     [LinkedPage( "Transaction History Page", "Link to use for viewing an individual's transaction history.", false, "", "", 4 )]
     [LinkedPage("Transaction Entry Page", "Link to use when adding new transactions.", false, "", "", 5)]
@@ -125,7 +125,7 @@ namespace RockWeb.Blocks.Finance
                     transactionService.GetStatus( schedule, out errorMsgs );
 
                     decimal totalAmount = 0;
-                    
+
                     Dictionary<string, object> scheduleSummary = new Dictionary<string, object>();
                     scheduleSummary.Add("Id", schedule.Id);
                     scheduleSummary.Add("Guid", schedule.Guid);
@@ -159,7 +159,7 @@ namespace RockWeb.Blocks.Finance
                     scheduleSummary.Add("UrlEncryptedKey", schedule.UrlEncodedKey);
                     scheduleSummary.Add("Frequency",  schedule.TransactionFrequencyValue.Value);
                     scheduleSummary.Add("FrequencyDescription", schedule.TransactionFrequencyValue.Description);
-                    
+
                     List<Dictionary<string, object>> summaryDetails = new List<Dictionary<string,object>>();
 
                     foreach ( FinancialScheduledTransactionDetail detail in schedule.ScheduledTransactionDetails )
@@ -202,10 +202,10 @@ namespace RockWeb.Blocks.Finance
             string content = GetAttributeValue( "Template" ).ResolveMergeFields( scheduleValues );
 
             lContent.Text = content;
-            
+
         }
 
 
         #endregion
-    }    
+    }
 }
