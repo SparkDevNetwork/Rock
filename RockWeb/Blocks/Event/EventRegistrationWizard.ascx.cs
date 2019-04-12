4a9a2b66f4523607e4b296b37779027001355b52
Shaun Cummings <shaun@sparkabilitygroup.com>
2019-04-12 17:22:52 -0600

    Initial commit:  - added RegistrationTemplatesField attribute and field type  - added DefaultBooleanValue to BoolenFieldAttribute (from v9)  - added EventRegistrationWizard block  - added block setting attributes to EventRegistrationWizard block

---
+++ RockWeb/Blocks/Event/EventRegistrationWizard.ascx.cs
@@ -0,0 +1,211 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Event
+{
+    /// <summary>
+    /// A wizard to simplify creation of Event Registrations.
+    /// </summary>
+    [DisplayName( "Event Registration Wizard" )]
+    [Category( "Event" )]
+    [Description( "A wizard to simplify creation of Event Registrations." )]
+
+    /*
+    Internal Name    PageId    Guid
+    Event Detail    396    7fb33834-f40a-4221-8849-bb8c06903b04
+    Event Occurrence    402    4b0c44ee-28e3-4753-a95b-8c57cd958fd1
+    Registration Instance    404    844dc54b-daec-47b3-a63a-712dd6d57793
+        */
+
+    [AccountField(
+        "Default Account",
+        Key = AttributeKey.DefaultAccount,
+        Description = "Select the default financial account which will be pre-filled if a cost is set on the new registration instance.",
+        Category = "",
+        IsRequired = false,
+        DefaultValue = "",
+        Order = 0 )]
+
+    [EventCalendarField(
+        "Default Calendar",
+        Key = AttributeKey.DefaultCalendar,
+        Description = "The default calendar which will be pre-selected if the staff person is permitted to create new calendar events.",
+        Category = "",
+        IsRequired = false,
+        DefaultValue = "",
+        Order = 1 )]
+
+    [RegistrationTemplatesField(
+        "Available Registration Templates",
+        Key = AttributeKey.AvailableRegistrationTemplates,
+        Description = "The list of templates the staff person can pick from – not all templates need to be available to all blocks.",
+        Category = "",
+        IsRequired = false,
+        DefaultValue = "",
+        Order = 2 )]
+
+    [GroupField(
+        "Root Group",
+        Key = AttributeKey.RootGroup,
+        Description = "This is the \"root\" of the group tree which will be offered for the staff person to pick the parent group from – limiting where the new group can be created.",
+        Category = "",
+        IsRequired = false,
+        DefaultValue = "",
+        Order = 3 )]
+
+    [LinkedPage(
+        "Group Viewer Page",
+        Key = AttributeKey.GroupViewerPage,
+        Description = "Determines which page the link in the final confirmation screen will take you to.",
+        Category = "",
+        IsRequired = false,
+        DefaultValue = Rock.SystemGuid.Page.GROUP_VIEWER,
+        Order = 4 )]
+
+    [BooleanField(
+        "Set Registration Instance Active",
+        Key = AttributeKey.SetRegistrationInstanceActive,
+        Description = "If unchecked, the new registration instance will be created, but marked as \"inactive\".",
+        Category = "",
+        DefaultBooleanValue = true,
+        Order = 5 )]
+
+    [BooleanField(
+        "Enable Calendar Events",
+        Key = AttributeKey.EnableCalendarEvents,
+        Description = "If calendar events are not enabled, registrations and groups will be created and linked, but not linked to any calendar event.",
+        Category = "",
+        DefaultBooleanValue = true,
+        Order = 6 )]
+
+    [BooleanField(
+        "Allow Creating New Calendar Events",
+        Key = AttributeKey.AllowCreatingNewCalendarEvents,
+        Description = "If set to \"Yes\", the staff person will be offered the \"New Event\" tab to create a new event and a new occurrence of that event, rather than only picking from existing events.",
+        Category = "",
+        DefaultBooleanValue = false,
+        Order = 7 )]
+
+    [MemoField(
+        "Instructions Lava Template",
+        Key = AttributeKey.InstructionsLavaTemplate,
+        Description = "Instructions added here will appear at the top of each page.",
+        Category = "",
+        IsRequired = false,
+        DefaultValue = "",
+        Order = 8 )]
+
+    //[WorkflowTypeField(
+    //    "Completion Workflow",
+    //    Key = AttributeKey.CompletionWorkflow,
+    //    Description = "A workflow that will be launched when a new registration is created.",
+    //    Category = "",
+    //    IsRequired = false,
+    //    DefaultValue = "",
+    //    FieldTypeAssembly = "Rock.Field.Types.WorkflowTypesFieldType",
+    //    Order = 9 )]
+
+    [WorkflowTypeField( "Completion Workflow", "A workflow that will be launched when a new registration is created.", true, false, "", "", 9, AttributeKey.CompletionWorkflow )]
+
+    public partial class EventRegistrationWizard : RockBlock
+    {
+        protected static class AttributeKey
+        {
+            public const string DefaultAccount = "DefaultAccount";
+            public const string DefaultCalendar = "DefaultCalendar";
+            public const string AvailableRegistrationTemplates = "AvailableRegistrationTemplates";
+            public const string RootGroup = "RootGroup";
+            public const string GroupViewerPage = "GroupViewerPage";
+            public const string SetRegistrationInstanceActive = "SetRegistrationInstanceActive";
+            public const string EnableCalendarEvents = "EnableCalendarEvents";
+            public const string AllowCreatingNewCalendarEvents = "AllowCreatingNewCalendarEvents";
+            public const string InstructionsLavaTemplate = "InstructionsLavaTemplate";
+            public const string CompletionWorkflow = "CompletionWorkflow";
+        }
+
+        #region Private Variables
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Control Methods
+
+        /// <summary>
+        /// Restores the view-state information from a previous user control request that was saved by the <see cref="M:System.Web.UI.UserControl.SaveViewState" /> method.
+        /// </summary>
+        /// <param name="savedState">An <see cref="T:System.Object" /> that represents the user control state to be restored.</param>
+        protected override void LoadViewState( object savedState )
+        {
+            base.LoadViewState( savedState );
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // Tell the browsers to not cache. This will help prevent browser using stale wizard stuff after navigating away from this page
+            Page.Response.Cache.SetCacheability( System.Web.HttpCacheability.NoCache );
+            Page.Response.Cache.SetExpires( DateTime.UtcNow.AddHours( -1 ) );
+            Page.Response.Cache.SetNoStore();
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+        }
+
+        /// <summary>
+        /// Saves any user control view-state changes that have occurred since the last page postback.
+        /// </summary>
+        /// <returns>
+        /// Returns the user control's current view state. If there is no view state associated with the control, it returns null.
+        /// </returns>
+        protected override object SaveViewState()
+        {
+            return base.SaveViewState();
+        }
+
+        #endregion
+
+    }
+}
