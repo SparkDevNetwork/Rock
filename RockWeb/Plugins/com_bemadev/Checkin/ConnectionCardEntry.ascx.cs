6a55518803951302b9b1d8f4f393137b3a1b4f85
Taylor Cavaletto <tjcavaletto@gmail.com>
2019-04-15 07:54:11 -0700

    - Prayer Requests created from the Weekend Attendance block are automatically approved

--- RockWeb/Plugins/com_bemadev/Checkin/ConnectionCardEntry.ascx.cs
+++ RockWeb/Plugins/com_bemadev/Checkin/ConnectionCardEntry.ascx.cs
@@ -1111,7 +1111,7 @@ namespace RockWeb.Plugins.com_bemadev.CheckIn
                     }
                 }
 
-                prayerRequest.IsApproved = false;
+                prayerRequest.IsApproved = true;
                 prayerRequest.IsActive = true;
                 prayerRequest.AllowComments = GetAttributeValue( "DefaultAllowCommentsChecked" ).AsBooleanOrNull() ?? true;
                 prayerRequest.IsPublic = GetAttributeValue( "DefaultToPublic" ).AsBooleanOrNull() ?? false;
