2db78acb4bf75ecee0e7f4aafc2d8f9357ce90bd
Justin Eveland <jbeveland27@gmail.com>
2019-05-09 16:19:32 -0400

    Adjusting build scripts to better handle third-party plugins

--- RockWeb/before-deploy.ps1
+++ RockWeb/before-deploy.ps1
@@ -14,9 +14,9 @@ Write-Output "Running script as: $env:userdomain\$env:username"
 # stop execution of the deploy if the moves fail
 $ErrorActionPreference = "Stop"
 
-### stop web publishing service - needed to allow the deploy to overwrite the sql server spatial types
-### Write-Host "Stopping Web Publishing Service"
-### stop-service -servicename w3svc
+# stop web publishing service - needed to allow the deploy to overwrite the sql server spatial types
+Write-Host "Stopping Web Publishing Service"
+stop-service -servicename w3svc
 
 # stop web site and app pool
 $Site = Get-WebSite -Name "$env:APPLICATION_SITE_NAME"
@@ -34,31 +34,194 @@ write-output "$(Get-Date -Format G) Waiting 10 seconds for IIS to shutdown..."
 Start-Sleep -s 10
 write-output "$(Get-Date -Format G) Continuing on..."
 
-# backup web.config file
-If (Test-Path "$webroot\web.config"){
-	Write-Host "Moving web.config to config dir"
-	Copy-Item "$webroot\web.config" "$rootfolder\config" -force
+# load the app offline template
+If (Test-Path "$webroot\app_offline-template.htm"){
+	Write-Host "Loading the app offline template"
+	Copy-Item "$webroot\app_offline-template.htm" "$webroot\app_offline.htm" -force
 }
 
-# backup connection string file
-If (Test-Path "$webroot\web.connectionstrings.config"){
-	Write-Host "Moving web.connectionstrings.config to config dir"
-	Copy-Item "$webroot\web.connectionstrings.config" "$rootfolder\config" -force
+####################################################################################################
+####################################################################################################
+# Borrowed from NewPointe deploy script
+
+if([string]::IsNullOrWhiteSpace($env:APPLICATION_PATH)) {
+    Write-Error "APPLICATION_PATH is not set, aborting!";
+    exit;
+}
+if([string]::IsNullOrWhiteSpace($env:APPVEYOR_JOB_ID)) {
+    Write-Error "APPVEYOR_JOB_ID is not set, aborting!";
+    exit;
 }
 
-# delete the contents of the temp directory or create it
-If (Test-Path "$rootfolder\temp"){
-	Remove-Item "$rootfolder\temp\*" -Force -Confirm:$False -Recurse
-} Else {
-  New-Item -ItemType Directory -Force -Path "$rootfolder\temp"
+$RootLocation = $env:APPLICATION_PATH;
+$TempLocation = Join-Path $env:Temp $env:APPVEYOR_JOB_ID;
+New-Item $TempLocation -ItemType Directory | Out-Null;
+
+function Join-Paths {
+    $path, $parts= $args;
+    foreach ($part in $parts) {
+        $path = Join-Path $path $part;
+    }
+    return $path;
 }
 
-# delete the contents of the temp\Plugins directory or create it
-If (Test-Path "$rootfolder\temp\Plugins"){
-	Remove-Item "$rootfolder\temp\Plugins\*" -Force -Confirm:$False -Recurse
-} Else {
-  New-Item -ItemType Directory -Force -Path "$rootfolder\temp\Plugins"
+$FileBackupLocation = Join-Path $TempLocation "SavedFiles";
+function Backup-RockFile([string] $RockWebFile) {
+    $RockLocation = Join-Path $RootLocation $RockWebFile;
+    $BackupLocation = Join-Path $FileBackupLocation $RockWebFile;
+    if (Test-Path $RockLocation) {
+        Write-Host "Backing up '$RockWebFile'";
+        $BackupParentLocation = Split-Path $BackupLocation;
+        New-Item $BackupParentLocation -ItemType Directory -Force | Out-Null
+        Move-Item $RockLocation $BackupLocation;
+    }
+    else {
+        Write-Warning "Could not backup '$RockWebFile': Location does not exist.";
+    }
 }
+
+# function Copy-DirectoryContentsRecursivelyWithSaneLinkHandling([string] $DirectoryToCopy, [string] $Destination) {
+#     New-Item -ItemType Directory $Destination -Force | Out-Null;
+#     foreach($Child in Get-ChildItem $DirectoryToCopy) {
+#         if($Child.LinkType) {
+#             $LinkTarget, $OtherTargets = $Child.Target;
+#             New-Item -ItemType $Child.LinkType -Path $Destination -Name $Child.Name -Target $LinkTarget -Force | Out-Null;
+#         }
+#         elseif($Child.PSIsContainer) {
+#             Copy-DirectoryContentsRecursivelyWithSaneLinkHandling (Join-Path $DirectoryToCopy $Child.Name) (Join-Path $Destination $Child.Name);
+#         }
+#         else {
+#             Copy-Item (Join-Path $DirectoryToCopy $Child.Name) (Join-Path $Destination $Child.Name) -Force;
+#         }
+#     }
+# }
+
+
+# if(Test-Path "env:DEPLOY_DEBUG") {
+#     Write-Host "================= DEBUG ==================";
+#     Write-Host "Working Directories: $(Get-Location)";
+#     Write-Host "Environment:";
+#     Get-ChildItem "env:";
+# }
+
+Write-Host "Mode: Pre-deploy";
+Write-Host "Application: $env:APPVEYOR_PROJECT_NAME";
+Write-Host "Build Number: $env:APPVEYOR_BUILD_VERSION";
+Write-Host "Deploy Location: $RootLocation";
+Write-Host "==========================================";
+
+# 1. Save or restore a backup of the website folder
+
+# $InProgressBackupLocation = "$($RootLocation.TrimEnd("/\\")).backup.deploy-in-progress";
+# if (Test-Path $InProgressBackupLocation) {
+#     Write-Host "Detected a deployment backup, assuming old deployment failed and restoring...";
+#     Remove-Item -Recurse -Force $RootLocation\*;
+#     Copy-DirectoryContentsRecursivelyWithSaneLinkHandling $InProgressBackupLocation $RootLocation;
+# }
+# else {
+#     Write-Host "Creating a deployment backup (If something fails please run the deployment again)...";
+#     Copy-DirectoryContentsRecursivelyWithSaneLinkHandling $RootLocation $InProgressBackupLocation;
+# }
+
+
+# 2. Save server-specifig files like static files, logs, plugin packages, and caches
+
+Write-Host "Saving server-specific files";
+
+Backup-RockFile "web.config"
+Backup-RockFile "web.connectionstrings.config"
+Backup-RockFile "Assets\Fonts\FontAwesome"
+Backup-RockFile "Styles\FontAwesome"
+
+# Save theme customizations and generated theme css
+$FilesToSave = "checkin-theme.css","bootstrap.css","theme.css","email-editor.css","_css-overrides.less","_variable-overrides.less";
+
+$ThemesLocation = Join-Path $RootLocation "Themes";
+foreach ($Theme in Get-ChildItem $ThemesLocation) {
+
+    if (-Not (Test-Path (Join-Paths $Theme.Name "Styles" ".nocompile"))) {
+
+        foreach($File in $FilesToSave) {
+            $LocalPath = Join-Paths "Themes" $Theme.Name "Styles" $File;
+            if (Test-Path (Join-Path $RootLocation $LocalPath)) {
+                Backup-RockFile $LocalPath;
+            }
+        }
+
+    }
+
+}
+
+Write-Host "Before-Deploy script finished successfully";
+
+
+# # 3. Put the app into maintenence mode
+
+# # Apparently just adding an app_offline.htm file isn't enough
+# # See: https://web.archive.org/web/20160704222144/http://blog.kurtschindler.net/more-app_offline-htm-woes/
+
+# # Complete steps:
+# #  - Add app_offline.htm
+# #  - Replace web.config with an empty one w/ a high change notification setting
+# #  - Request a page from the site
+
+# Write-Host "Putting application in maintenence mode";
+
+# Move-Item -Path (Join-Path $RootLocation "app_offline-template.htm") -Destination (Join-Path $RootLocation "app_offline.htm") -ErrorAction SilentlyContinue;
+
+# Set-Content (Join-Path $RootLocation "web.config") @'
+# <?xml version="1.0" encoding="utf-8" ?>
+# <configuration>
+#     <system.web>
+#         <httpRuntime waitChangeNotification="300" maxWaitChangeNotification="300"/>
+#     </system.web>
+#     <system.webServer>
+#         <modules runAllManagedModulesForAllRequests="true" />
+#     </system.webServer>
+# </configuration>
+# '@
+
+# Invoke-WebRequest "http://localhost/"
+
+####################################################################################################
+####################################################################################################
+
+
+
+
+
+# # backup web.config file
+# If (Test-Path "$webroot\web.config"){
+# 	Write-Host "Moving web.config to config dir"
+# 	Copy-Item "$webroot\web.config" "$rootfolder\config" -force
+# }
+
+# # backup connection string file
+# If (Test-Path "$webroot\web.connectionstrings.config"){
+# 	Write-Host "Moving web.connectionstrings.config to config dir"
+# 	Copy-Item "$webroot\web.connectionstrings.config" "$rootfolder\config" -force
+# }
+
+
+# # load the app offline template
+# If (Test-Path "$webroot\app_offline-template.htm"){
+# 	Write-Host "Loading the app offline template"
+# 	Copy-Item "$webroot\app_offline-template.htm" "$webroot\app_offline.htm" -force
+# }
+
+# # delete the contents of the temp directory or create it
+# If (Test-Path "$rootfolder\temp"){
+# 	Remove-Item "$rootfolder\temp\*" -Force -Confirm:$False -Recurse
+# } Else {
+#   New-Item -ItemType Directory -Force -Path "$rootfolder\temp"
+# }
+
+# # delete the contents of the temp\Plugins directory or create it
+# If (Test-Path "$rootfolder\temp\Plugins"){
+# 	Remove-Item "$rootfolder\temp\Plugins\*" -Force -Confirm:$False -Recurse
+# } Else {
+#   New-Item -ItemType Directory -Force -Path "$rootfolder\temp\Plugins"
+# }
 
 # move content folder to temp
 #Write-Host "Moving content folder to temp directory"
@@ -85,13 +248,13 @@ If (Test-Path "$rootfolder\temp\Plugins"){
 # }
 #Move-Item "$webroot\Themes\CheckinKids_CentralAZ\Assets" "$rootfolder\temp\CheckinKids_CentralAZ\Assets" -Force
 
-# move non com_lcbcchurch plugins to temp\Plugins folder
-If (Test-Path "$webroot\Plugins\"){
-	Write-Host "Moving non com_lcbcchurch/com_bemadev/com_bemaservices/org_newpointe Plugins to temp Plugins directory"
-	$files = GCI -path "$webroot\Plugins\" | Where-Object {$_.name -ne "com_lcbcchurch" -and $_.name -ne "com_bemadev" -and $_.name -ne "com_bemaservices" -and $_.name -ne "org_newpointe" -and $_.name -ne "com_shepherdchurch" -and $_.name -ne ".gitignore" -and $_.name -ne "readme.txt"}
-	foreach ($file in $files) { Move-Item  "$webroot\Plugins\$file" -Destination "$rootfolder\temp\Plugins" -Force }
-}
-# move a robots file if it exists
-If (Test-Path "$webroot\robots.txt"){
-	Move-Item "$webroot\robots.txt" "$rootfolder\temp"
-}
+# # move non com_lcbcchurch plugins to temp\Plugins folder
+# If (Test-Path "$webroot\Plugins\"){
+# 	Write-Host "Moving non com_lcbcchurch/com_bemadev/com_bemaservices/org_newpointe Plugins to temp Plugins directory"
+# 	$files = GCI -path "$webroot\Plugins\" | Where-Object {$_.name -ne "com_lcbcchurch" -and $_.name -ne "com_bemadev" -and $_.name -ne "com_bemaservices" -and $_.name -ne "org_newpointe" -and $_.name -ne "com_shepherdchurch" -and $_.name -ne ".gitignore" -and $_.name -ne "readme.txt"}
+# 	foreach ($file in $files) { Move-Item  "$webroot\Plugins\$file" -Destination "$rootfolder\temp\Plugins" -Force }
+# }
+# # move a robots file if it exists
+# If (Test-Path "$webroot\robots.txt"){
+# 	Move-Item "$webroot\robots.txt" "$rootfolder\temp"
+# }
--- RockWeb/deploy.ps1
+++ RockWeb/deploy.ps1
@@ -11,18 +11,165 @@ Write-Output "Root folder: $rootfolder"
 Write-Output "Web root folder: $webroot"
 Write-Output "Running script as: $env:userdomain\$env:username"
 
-# move connection string file back from config
-Write-Host "Copying web.ConnectionStrings.config to web dir"
-If (Test-Path "$rootfolder\config\web.connectionstrings.config"){
-	Write-Host "Moving web.connectionstrings.config from config dir"
-	Copy-Item "$rootfolder\config\web.connectionstrings.config" "$webroot" -force
+####################################################################################################
+####################################################################################################
+# Borrowed from NewPointe deploy script
+
+if([string]::IsNullOrWhiteSpace($env:APPLICATION_PATH)) {
+    Write-Error "APPLICATION_PATH is not set, aborting!";
+    exit;
+}
+if([string]::IsNullOrWhiteSpace($env:APPVEYOR_JOB_ID)) {
+    Write-Error "APPVEYOR_JOB_ID is not set, aborting!"
+    exit;
 }
 
-# move web.config file back from temp (restarts the app pool)
-If (Test-Path "$rootfolder\config\web.config"){
-	Write-Host "Moving web.config from config dir"
-	Copy-Item "$rootfolder\config\web.config" "$webroot" -force
+$RootLocation = $env:APPLICATION_PATH;
+$TempLocation = Join-Path $env:Temp $env:APPVEYOR_JOB_ID;
+
+function Join-Paths {
+    $path, $parts= $args;
+    foreach ($part in $parts) {
+        $path = Join-Path $path $part;
+    }
+    return $path;
 }
+
+function Get-VersionId([string] $FileName) {
+    $Parts = $FileName -split "-";
+    if($Parts.Length -gt 0) {
+        return $Parts[0] -replace "\D+" -as [Int];
+    }
+    else {
+        return 0;
+    }
+}
+
+function Expand-RockPlugin([string] $PluginPath, [string] $DestinationPath) {
+    
+    $PackageHash = (Get-FileHash $PluginPath).Hash;
+    $TempZip = Join-Paths $TempLocation "$PackageHash.zip";
+    Copy-Item $PluginPath $TempZip;
+    Expand-Archive $TempZip $DestinationPath -Force;
+    Remove-Item $TempZip -Force;
+}
+
+function Restore-RockPlugin([string] $PluginPackagePath) {
+
+    $PackageHash = (Get-FileHash $PluginPackagePath).Hash;
+    $PackageTempLocation = Join-Path $TempLocation $PackageHash;
+
+    New-Item $PackageTempLocation -ItemType Directory | Out-Null;
+    Expand-RockPlugin $PluginPackagePath $PackageTempLocation;
+
+    $ContentPath = Join-Path $PackageTempLocation "content";
+    if(Test-Path $ContentPath) {
+        Get-ChildItem $ContentPath | Copy-Item -Destination $RootLocation -Recurse -Container -Force
+    }
+    
+    Remove-Item $PackageTempLocation -Recurse -Force;
+}
+
+function Copy-DirectoryContentsRecursivelyWithSaneLinkHandling([string] $DirectoryToCopy, [string] $Destination) {
+    New-Item -ItemType Directory $Destination -Force | Out-Null;
+    foreach($Child in Get-ChildItem $DirectoryToCopy) {
+        if($Child.LinkType) {
+            $Dest = Join-Path $Destination $Child.Name;
+            if(Test-Path $Dest) {
+                Remove-Item $Dest -Recurse -Force
+            }
+            $LinkTarget, $OtherTargets = $Child.Target;
+            New-Item -ItemType $Child.LinkType $Dest -Target $LinkTarget -Force | Out-Null;
+        }
+        elseif($Child.PSIsContainer) {
+            Copy-DirectoryContentsRecursivelyWithSaneLinkHandling (Join-Path $DirectoryToCopy $Child.Name) (Join-Path $Destination $Child.Name);
+        }
+        else {
+            Copy-Item (Join-Path $DirectoryToCopy $Child.Name) (Join-Path $Destination $Child.Name) -Force;
+        }
+    }
+}
+
+
+# if(Test-Path "env:DEPLOY_DEBUG") {
+#     Write-Host "================= DEBUG ==================";
+#     Write-Host "Working Directories: $(Get-Location)";
+#     Write-Host "Environment:";
+#     Get-ChildItem "env:";
+# }
+
+Write-Host "Mode: Post-deploy";
+Write-Host "Application: $env:APPVEYOR_PROJECT_NAME";
+Write-Host "Build Number: $env:APPVEYOR_BUILD_VERSION";
+Write-Host "Deploy Location: $RootLocation";
+Write-Host "==========================================";
+
+
+# 1. Restore server-specific files like static files, logs, plugin packages, and caches
+
+Write-Host "Restoring server-specific files";
+$FileBackupLocation = Join-Path $TempLocation "SavedFiles";
+Copy-DirectoryContentsRecursivelyWithSaneLinkHandling $FileBackupLocation $RootLocation;
+
+# 2. Reinstall plugins
+
+Write-Host "Reinstalling Plugin Files";
+
+$InstalledPluginsPath = Join-Paths $RootLocation "App_Data" "RockShop";
+if(Test-Path $InstalledPluginsPath) {
+
+    $InstalledPlugins = Get-ChildItem $InstalledPluginsPath;
+    foreach ($Plugin in $InstalledPlugins) {
+
+        $PluginVersions = Get-ChildItem $Plugin.FullName;
+        if($PluginVersions.Count -gt 0) {
+
+            $LatestVersion = $PluginVersions  | Sort-Object {Get-VersionId $_.BaseName} | Select-Object -Last 1;
+            Write-Host "Restoring ${LatestVersion.Name}";
+            Restore-RockPlugin $LatestVersion.FullName;
+        }
+    }
+}
+
+# 3. Clean up temp folder
+
+#Remove-Item $TempLocation -Recurse -Force;
+
+# # 4. Take the app out of maintenence mode
+
+# Write-Host "Taking application out of maintenence mode";
+
+# Move-Item -Path (Join-Path $RootLocation "app_offline.htm") -Destination (Join-Path $RootLocation "app_offline-template.htm") -ErrorAction SilentlyContinue;
+# Remove-Item -Path (Join-Path $RootLocation "app_offline.htm") -ErrorAction SilentlyContinue;
+
+# 5. Move the backup so we know we completed successfully
+
+# $InProgressBackupLocation = "$($RootLocation.TrimEnd("/\\")).backup.deploy-in-progress";
+# $SuccessBackupLocation = "$($RootLocation.TrimEnd("/\\")).backup";
+# Remove-Item $SuccessBackupLocation -Recurse -Force | Out-Null;
+# Move-Item $InProgressBackupLocation $SuccessBackupLocation -Force;
+
+
+
+
+
+
+
+## These steps taken care of by #1 above
+
+# # move connection string file back from config
+# Write-Host "Copying web.ConnectionStrings.config to web dir"
+# If (Test-Path "$rootfolder\config\web.connectionstrings.config"){
+# 	Write-Host "Moving web.connectionstrings.config from config dir"
+# 	Copy-Item "$rootfolder\config\web.connectionstrings.config" "$webroot" -force
+# }
+
+# # move web.config file back from temp (restarts the app pool)
+# If (Test-Path "$rootfolder\config\web.config"){
+# 	Write-Host "Moving web.config from config dir"
+# 	Copy-Item "$rootfolder\config\web.config" "$webroot" -force
+# }
+
 
 # ensure that the compilation debug is false
 (Get-Content "$webroot\web.config").Replace('<compilation debug="true"', '<compilation debug="false"') | Set-Content "$webroot\web.config"
@@ -58,30 +205,23 @@ If (Test-Path "$rootfolder\config\web.config"){
 # Write-Host "Moving Themes\CheckinKids_CentralAZ\Assets folder back from temp directory"
 # Move-Item "$rootfolder\temp\CheckinKids_CentralAZ\Assets" "$webroot\Themes\CheckinKids_CentralAZ" -Force
 
-# move non com_lcbcchurch plugins back from temp\Plugins folder
-Write-Host "Moving non com_lcbcchurch/com_bemadev/com_bemaservices/org_newpointe Plugins back from temp Plugins directory"
-$files = GCI -path "$rootfolder\temp\Plugins" | Where-Object {$_.name -ne "com_lcbcchurch" -and $_.name -ne "com_bemadev" -and $_.name -ne "com_bemaservices" -and $_.name -ne "org_newpointe" -and $_.name -ne "com_shepherdchurch"}
-foreach ($file in $files) { Move-Item  "$rootfolder\temp\Plugins\$file" -Destination "$webroot\Plugins\" -Force }
 
-# move a robots file back from temp if it exists
-If (Test-Path "$rootfolder\temp\robots.txt"){
-	Write-Host "Moving robots.txt file back from temp directory"
-	Move-Item "$rootfolder\temp\robots.txt" "$webroot"
-}
+## These steps taken care of by #2 above
+# # move non com_lcbcchurch plugins back from temp\Plugins folder
+# Write-Host "Moving non com_lcbcchurch/com_bemadev/com_bemaservices/org_newpointe Plugins back from temp Plugins directory"
+# $files = GCI -path "$rootfolder\temp\Plugins" | Where-Object {$_.name -ne "com_lcbcchurch" -and $_.name -ne "com_bemadev" -and $_.name -ne "com_bemaservices" -and $_.name -ne "org_newpointe" -and $_.name -ne "com_shepherdchurch"}
+# foreach ($file in $files) { Move-Item  "$rootfolder\temp\Plugins\$file" -Destination "$webroot\Plugins\" -Force }
 
-# copy any dlls from the manual-fix\bin folder to the web bin folder
-Write-Host "Copying any manual-fix\bin DLLs to web bin folder"
-$files = GCI -path "$rootfolder\manual-fixes\bin"
-foreach ($file in $files) { Copy-Item  "$rootfolder\manual-fixes\bin\$file" -Destination "$webroot\bin\" -Force}
+# # move a robots file back from temp if it exists
+# If (Test-Path "$rootfolder\temp\robots.txt"){
+# 	Write-Host "Moving robots.txt file back from temp directory"
+# 	Move-Item "$rootfolder\temp\robots.txt" "$webroot"
+# }
 
-# start web publishing service
-Write-Host "Starting Web Publishing Service"
-start-service -servicename w3svc
-
-# start web site and app pool
-Write-Host "Starting ApplicationPool and Website"
-Start-WebAppPool -Name (Get-Website -Name "$env:APPLICATION_SITE_NAME").applicationPool
-Start-Website -Name "$env:APPLICATION_SITE_NAME"
+# # copy any dlls from the manual-fix\bin folder to the web bin folder
+# Write-Host "Copying any manual-fix\bin DLLs to web bin folder"
+# $files = GCI -path "$rootfolder\manual-fixes\bin"
+# foreach ($file in $files) { Copy-Item  "$rootfolder\manual-fixes\bin\$file" -Destination "$webroot\bin\" -Force}
 
 # create empty migration flag
 New-Item "$webroot\App_Data\Run.Migration" -type file -force
@@ -92,6 +232,15 @@ $acl = Get-ACL "$webroot\App_Data\Run.Migration"
 $accessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone","FullControl","Allow")
 $acl.AddAccessRule($accessRule)
 Set-Acl "$webroot\App_Data\Run.Migration" $acl
+
+# start web publishing service
+Write-Host "Starting Web Publishing Service"
+start-service -servicename w3svc
+
+# start web site and app pool
+Write-Host "Starting ApplicationPool and Website"
+Start-WebAppPool -Name (Get-Website -Name "$env:APPLICATION_SITE_NAME").applicationPool
+Start-Website -Name "$env:APPLICATION_SITE_NAME"
 
 # delete deploy scripts
 If (Test-Path "$webroot\deploy.ps1"){
