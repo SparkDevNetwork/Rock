4915116eaee08e94d86f44c71673eb2b2fe00942
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2019-06-03 16:36:51 -0700

    - Added migration with values for the AssessmentType badge fields.

--- Rock.Migrations/Migrations/201906032320193_AssessmentBadges.cs
+++ Rock.Migrations/Migrations/201906032320193_AssessmentBadges.cs
@@ -1,4 +1,4 @@
-// <copyright>
+﻿// <copyright>
 // Copyright by the Spark Development Network
 //
 // Licensed under the Rock Community License (the "License");
@@ -13,12 +13,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-//
+
 namespace Rock.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     /// <summary>
     ///
     /// </summary>
@@ -29,19 +29,33 @@ namespace Rock.Migrations
         /// </summary>
         public override void Up()
         {
-            AddColumn("dbo.AssessmentType", "IconCssClass", c => c.String(maxLength: 100));
-            AddColumn("dbo.AssessmentType", "BadgeColor", c => c.String(maxLength: 7));
-            AddColumn("dbo.AssessmentType", "BadgeSummaryLava", c => c.String());
+            AddColumn( "dbo.AssessmentType", "IconCssClass", c => c.String( maxLength: 100 ) );
+            AddColumn( "dbo.AssessmentType", "BadgeColor", c => c.String( maxLength: 7 ) );
+            AddColumn( "dbo.AssessmentType", "BadgeSummaryLava", c => c.String() );
+
+            UpdateAssessmentTypeFields();
         }
-        
+
         /// <summary>
         /// Operations to be performed during the downgrade process.
         /// </summary>
         public override void Down()
         {
-            DropColumn("dbo.AssessmentType", "BadgeSummaryLava");
-            DropColumn("dbo.AssessmentType", "BadgeColor");
-            DropColumn("dbo.AssessmentType", "IconCssClass");
+            DropColumn( "dbo.AssessmentType", "BadgeSummaryLava" );
+            DropColumn( "dbo.AssessmentType", "BadgeColor" );
+            DropColumn( "dbo.AssessmentType", "IconCssClass" );
+        }
+
+        /// <summary>
+        /// Updates the assessment type fields used for badges.
+        /// </summary>
+        private void UpdateAssessmentTypeFields()
+        {
+            Sql( $"UPDATE [AssessmentType] SET IconCssClass = 'fa fa-chart-bar', BadgeColor = '#4E79A7' WHERE [Guid] = '{SystemGuid.AssessmentType.DISC}'" );
+            Sql( $"UPDATE [AssessmentType] SET IconCssClass = 'fa fa-gift', BadgeColor = '#8CD17D' WHERE [Guid] = '{SystemGuid.AssessmentType.GIFTS}' " );
+            Sql( $"UPDATE [AssessmentType] SET IconCssClass = 'fa fa-handshake', BadgeColor = '#E15759' WHERE [Guid] = '{SystemGuid.AssessmentType.CONFLICT}'" );
+            Sql( $"UPDATE [AssessmentType] SET IconCssClass = 'fa fa-theater-masks', BadgeColor = '#499894' WHERE [Guid] = '{SystemGuid.AssessmentType.EQ}'" );
+            Sql( $"UPDATE [AssessmentType] SET IconCssClass = 'fa fa-key ', BadgeColor = '#F28E2B' WHERE [Guid] = '{SystemGuid.AssessmentType.MOTIVATORS}'" );
         }
     }
 }
