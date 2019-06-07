568bb49aeccb467be558b6442220e044f0689d21
Mike Peterson <mike@sparkabilitygroup.com>
2019-06-06 19:32:48 -0700

    - Fixed issue with Conditional Fields in Event Registration where the conditional fields would not render and validate if the Save/Next button was clicked before the form had a chance to process the conditional field logic.

---
+++ RockWeb/Scripts/Rock/Controls/fieldVisibilityWrapper.js
@@ -0,0 +1,17 @@
+﻿(function (Sys) {
+    'use strict';
+    Sys.Application.add_load(function () {
+
+        // since some of the edit controls do a postback to show/hide other controls, we want to disable buttons to prevent a double postback
+        $('select.js-prevent-double-postback,input.js-prevent-double-postback').on('change', function () {
+            var inputOnChange = $(this).attr('onchange');
+            if (inputOnChange && inputOnChange.indexOf('__doPostBack') > 0) {
+                var $blockButtons = $(this).closest('.js-block-instance').find('a');
+                $blockButtons.attr('disabled', 'disabled')
+                    .prop('disabled', true)
+                    .attr('disabled', 'disabled')
+                    .addClass('disabled')
+            };
+        });
+    });
+}(Sys));
