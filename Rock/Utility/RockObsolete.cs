96772cf0651b09d53931b22d89ae3f2d73b31717
Mike Peterson <mike@sparkabilitygroup.com>
2019-06-27 10:51:05 -0700

    - Update comments on RockObsolete to include the process for it

--- Rock/Utility/RockObsolete.cs
+++ Rock/Utility/RockObsolete.cs
@@ -20,6 +20,19 @@ namespace Rock
 {
     /// <summary>
     /// Marks the version at which an [Obsolete] item became obsolete. If the HotFix version matters then include it, otherwise only the major version is required (e.g. 1.8, 1.8.5, 1.9.1, 1.10 ).
+    /// The process for this is:
+    /// At the beginning of a new version, we might update [Obsolete]/[RockObsolete] methods as follows
+    ///  + Last N public releases and develop: Warning
+    ///  + N + 1 major public releases ago: Error( set[Obsolete] error parameter to true which will prevent it from getting compiled if used)
+    ///  + N + 2 major public versions ago:: Delete the obsolete item
+    ///  + You can use the CodeGenerator and set the 'Report Obsolete' option to find all those.
+    ///
+    /// Example: If the last major public release is 1.9, we'll decide how many versions ago that N should be. Let's say that N is 2
+    ///   + 1.10 (develop) warnings for new obsolete methods that are added
+    ///   + 1.9 warnings
+    ///   + 1.8 warnings (if N is 2)
+    ///   + 1.7 errors
+    ///   + 1.6 delete
     /// </summary>
     /// <seealso cref="System.Attribute" />
     [AttributeUsage( AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate, Inherited = false )]
