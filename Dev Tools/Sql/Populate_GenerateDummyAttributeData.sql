1c3786a3e0e5a423c91fbbeb18a3990020a15f69
Mike Peterson <mike@sparkdevnetwork.org>
2019-07-25 10:40:51 -0700

    Merge branch 'release-1.9.0' into hotfix-1.9.1
    # Conflicts:
    #	Dev Tools/Sql/Populate_GenerateDummyAttributeData.sql
    #	Rock/Rock.csproj

--- Dev Tools/Sql/Populate_GenerateDummyAttributeData.sql
+++ Dev Tools/Sql/Populate_GenerateDummyAttributeData.sql
@@ -3,12 +3,79 @@
 /* Only use this on a test development database that you won't be keeping for very long since it might have unwanted side effects within the Rock application */
 
 
-declare @maxAttributeValueRowsPerRun int = 500000;
+declare @maxAttributeValues int = 5000000;
+declare @currentAttributeCount int = (select count(*) from Attribute)
+declare @currentAttributeValueCount int = (select count(*) from AttributeValue)
+
+if (@currentAttributeCount < 100000)
+begin
+INSERT INTO [dbo].[Attribute]
+           ([IsSystem]
+           ,[FieldTypeId]
+           ,[EntityTypeId]
+           ,[EntityTypeQualifierColumn]
+           ,[EntityTypeQualifierValue]
+           ,[Key]
+           ,[Name]
+           ,[Description]
+           ,[Order]
+           ,[IsGridColumn]
+           ,[DefaultValue]
+           ,[IsMultiValue]
+           ,[IsRequired]
+           ,[Guid]
+           ,[CreatedDateTime]
+           ,[ModifiedDateTime]
+           ,[CreatedByPersonAliasId]
+           ,[ModifiedByPersonAliasId]
+           ,[ForeignKey]
+           ,[IconCssClass]
+           ,[AllowSearch]
+           ,[ForeignGuid]
+           ,[ForeignId]
+           ,[IsIndexEnabled]
+           ,[IsAnalytic]
+           ,[IsAnalyticHistory]
+           ,[IsActive]
+           ,[EnableHistory])
+           select
+		   IsSystem,
+           FieldTypeId,
+           EntityTypeId,
+           EntityTypeQualifierColumn,
+           EntityTypeQualifierValue,
+           [Key],
+           [Name],
+           [Description],
+           [Order],
+           IsGridColumn,
+           DefaultValue,
+           IsMultiValue,
+           IsRequired,
+           newid(),
+           CreatedDateTime,
+           ModifiedDateTime,
+           CreatedByPersonAliasId,
+           ModifiedByPersonAliasId,
+           ForeignKey,
+           IconCssClass,
+           AllowSearch,
+           ForeignGuid,
+           ForeignId,
+           IsIndexEnabled,
+           IsAnalytic,
+           IsAnalyticHistory,
+           IsActive,
+           EnableHistory from Attribute
+end
+
 
 
 /* This will double (up to 500000 rows at a time) the amount of Attribute Values in the system. Run this multiple times until you have as much Attribute value data as you want */
 /* NOTE: You may occasionally get an index violation since the EntityIds are just random numbers, because AttributeId+EntityId must be unique. 
   If you do get the index error, just try again */
+ while (@currentAttributeValueCount < @maxAttributeValues)
+ begin
 INSERT INTO [dbo].[AttributeValue] (
 	[IsSystem],
 	[AttributeId],
@@ -24,7 +91,7 @@ INSERT INTO [dbo].[AttributeValue] (
 	[ForeignGuid],
 	[ForeignId]
 	)
-SELECT TOP (@maxAttributeValueRowsPerRun) av.IsSystem,
+SELECT TOP (100000) av.IsSystem,
 	av.AttributeId,
 	abs(Cast(checksum(newid()) AS INT)) EntityId,
 	av.[Value],
@@ -38,3 +105,5 @@ SELECT TOP (@maxAttributeValueRowsPerRun) av.IsSystem,
 	av.ForeignGuid,
 	av.ForeignId
 FROM AttributeValue av
+set @currentAttributeValueCount = (select count(*) from AttributeValue)
+end
