1c176c3d17b6c37926ec3e338c8c4e8cb5969c38
Mike Peterson <mike@sparkdevnetwork.org>
2019-07-26 15:09:44 -0700

    + Updated Slingshot to support imports of Person Search Keys, 'Receipt' transaction types, and Locations with a location type of 'Other'. Also updated imports of Financial Transactions to match 'Anonymous Giver' if they are not associated with a specific person.

---
+++ Rock/SystemGuid/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Rock.SystemGuid
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Person
+    {
+        /// <summary>
+        /// The Guid of the 'Giver Anonymous' person that ships with core
+        /// </summary>
+        public const string GIVER_ANONYMOUS = "802235DC-3CA5-94B0-4326-AACE71180F48";
+    }
+}
