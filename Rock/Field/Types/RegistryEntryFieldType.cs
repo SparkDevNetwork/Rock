c43dcde2f8d0cdce3004eb0385005502ad820688
Mike Peterson <mike@sparkdevnetwork.org>
2019-07-26 15:17:43 -0700

    - Ran Rock\Dev Tools\Applications\EnsureCopyrightHeader\EnsureCopyrightHeader.sln to ensure that copyright headers are correct on all the .cs files in Rock

--- Rock/Field/Types/RegistryEntryFieldType.cs
+++ Rock/Field/Types/RegistryEntryFieldType.cs
@@ -1,4 +1,9 @@
-﻿// You may obtain a copy of the License at
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 // http://www.rockrms.com/license
 //
