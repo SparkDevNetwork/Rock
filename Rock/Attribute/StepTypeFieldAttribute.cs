f879f33fe757d75ba1edff16f3168bdca1ddf705
Ben Wiley <ben@sparkabilitygroup.com>
2019-08-08 15:36:57 -0400

    + Add steps bulk entry block

--- Rock/Attribute/StepTypeFieldAttribute.cs
+++ Rock/Attribute/StepTypeFieldAttribute.cs
@@ -19,13 +19,13 @@ using System;
 namespace Rock.Attribute
 {
     /// <summary>
-    /// Field Attribute to select a Step Program.
+    /// Field Attribute to select a Step Type.
     /// </summary>
     [AttributeUsage( AttributeTargets.Class, AllowMultiple = true, Inherited = true )]
-    public class StepProgramFieldAttribute : SelectFieldAttribute
+    public class StepTypeFieldAttribute : SelectFieldAttribute
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="StepProgramFieldAttribute" /> class.
+        /// Initializes a new instance of the <see cref="StepTypeFieldAttribute" /> class.
         /// </summary>
         /// <param name="name">The name.</param>
         /// <param name="description">The description.</param>
@@ -34,8 +34,8 @@ namespace Rock.Attribute
         /// <param name="category">The category.</param>
         /// <param name="order">The order.</param>
         /// <param name="key">The key.</param>
-        public StepProgramFieldAttribute( string name = "", string description = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null )
-            : base( name, description, required, defaultValue, category, order, key, typeof( Rock.Field.Types.StepProgramFieldType ).FullName )
+        public StepTypeFieldAttribute( string name = "", string description = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null )
+            : base( name, description, required, defaultValue, category, order, key, typeof( Rock.Field.Types.StepTypeFieldType ).FullName )
         {
         }
     }
