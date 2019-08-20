4dce067ad7c171b83e55bf32924fd910ccda15e0
Mike Peterson <mike@sparkdevnetwork.org>
2019-08-19 20:13:12 -0700

    - Added PersistedDataset lava filter and UpdatePersistedDatasets job

---
+++ Rock/Jobs/UpdatePersistedDatasets.cs
@@ -0,0 +1,122 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Quartz;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+
+namespace Rock.Jobs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="Quartz.IJob" />
+    [DisplayName( "Update Persisted Datasets" )]
+    [Description( "This job will update the persisted data in any Persisted Datasets that need to be refreshed." )]
+
+    [DisallowConcurrentExecution]
+    public class UpdatePersistedDatasets : IJob
+    {
+
+        /// <summary>
+        /// Empty constructor for job initialization
+        /// <para>
+        /// Jobs require a public empty constructor so that the
+        /// scheduler can instantiate the class whenever it needs.
+        /// </para>
+        /// </summary>
+        public UpdatePersistedDatasets()
+        {
+        }
+
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public void Execute( IJobExecutionContext context )
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+            StringBuilder results = new StringBuilder();
+            int updatedDatasetCount = 0;
+            var errors = new List<string>();
+            List<Exception> exceptions = new List<Exception>();
+
+            using ( var rockContext = new RockContext() )
+            {
+                var currentDateTime = RockDateTime.Now;
+
+                // get a list of all the data views that need to be refreshed
+                var expiredPersistedDataset = new PersistedDatasetService( rockContext ).Queryable()
+                    .Where( a =>
+                        a.LastRefreshDateTime == null
+                        ||  ( System.Data.Entity.SqlServer.SqlFunctions.DateAdd( "mi", a.RefreshIntervalMinutes.Value, a.LastRefreshDateTime.Value ) < currentDateTime )                        );
+
+                var expiredPersistedDatasetList = expiredPersistedDataset.ToList();
+                foreach ( var persistedDataset in expiredPersistedDatasetList )
+                {
+                    var name = persistedDataset.Name;
+                    try
+                    {
+                        context.UpdateLastStatusMessage( $"Updating {persistedDataset.Name}" );
+                        persistedDataset.UpdateResultData();
+                        rockContext.SaveChanges();
+                        updatedDatasetCount++;
+                    }
+                    catch ( Exception ex )
+                    {
+                        // Capture and log the exception because we're not going to fail this job
+                        // unless all the data views fail.
+                        var errorMessage = $"An error occurred while trying to update persisted dataset '{name}' so it was skipped. Error: {ex.Message}";
+                        errors.Add( errorMessage );
+                        var ex2 = new Exception( errorMessage, ex );
+                        exceptions.Add( ex2 );
+                        ExceptionLogService.LogException( ex2, null );
+                        continue;
+                    }
+                }
+            }
+
+            // Format the result message
+            results.AppendLine( $"Updated {updatedDatasetCount} {"persisted dataset".PluralizeIf( updatedDatasetCount != 1 )}" );
+            context.Result = results.ToString();
+
+            if ( errors.Any() )
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine();
+                sb.Append( "Errors: " );
+                errors.ForEach( e => { sb.AppendLine(); sb.Append( e ); } );
+                string errorMessage = sb.ToString();
+                context.Result += errorMessage;
+                // We're not going to throw an aggregate exception unless there were no successes.
+                // Otherwise the status message does not show any of the success messages in
+                // the last status message.
+                if ( updatedDatasetCount == 0 )
+                {
+                    throw new AggregateException( exceptions.ToArray() );
+                }
+            }
+        }
+    }
+}
