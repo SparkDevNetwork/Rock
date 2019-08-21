79e5c300179e6f73ade5308b4a34d1baf90c80a3
Mike Peterson <mike@sparkdevnetwork.org>
2019-08-20 17:00:05 -0700

    - Added PersistedDataset List/Detail blocks

---
+++ RockWeb/Blocks/Cms/PersistedDatasetList.ascx
@@ -0,0 +1,35 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PersistedDatasetList.ascx.cs" Inherits="RockWeb.Blocks.Cms.PersistedDatasetList" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <Rock:ModalAlert ID="mdGridWarning" runat="server" />
+
+        <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
+
+            <div class="panel-heading">
+                <h1 class="panel-title">
+                    <i class="fa fa-database"></i>
+                    Persisted Dataset List
+                </h1>
+            </div>
+            <div class="panel-body">
+
+                <div class="grid grid-panel">
+                    <Rock:Grid ID="gList" runat="server" AllowSorting="true" OnRowSelected="gList_RowSelected">
+                        <Columns>
+                            <Rock:RockBoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                            <Rock:RockBoundField DataField="AccessKey" HeaderText="AccessKey" SortExpression="AccessKey" />
+                            <Rock:DateTimeField DataField="LastRefreshDateTime" HeaderText="Last Refresh" SortExpression="LastRefreshDateTime" />
+                            <Rock:LinkButtonField HeaderText="Refresh" CssClass="btn btn-default btn-sm fa fa-refresh" ToolTip="Rebuild the persisted dataset" OnClick="lbRefresh_Click" HeaderStyle-HorizontalAlign="Center" ItemStyle-HorizontalAlign="Center" OnDataBound="btnRefresh_DataBound" />
+                            <Rock:DeleteField OnClick="gList_DeleteClick" />
+                        </Columns>
+                    </Rock:Grid>
+                </div>
+
+            </div>
+
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Blocks/Cms/PersistedDatasetList.ascx.cs
@@ -0,0 +1,245 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Cms
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DisplayName( "Persisted Dataset List" )]
+    [Category( "CMS" )]
+    [Description( "Lists Persisted Datasets" )]
+
+    [LinkedPage(
+        "Detail Page",
+        Key = AttributeKey.DetailPage )]
+
+    public partial class PersistedDatasetList : RockBlock, ICustomGridColumns
+    {
+
+        #region Attribute Keys
+
+        /// <summary>
+        /// Keys to use for Block Attributes
+        /// </summary>
+        protected static class AttributeKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        #endregion Attribute Keys
+
+        #region PageParameterKeys
+
+        /// <summary>
+        /// Keys to use for Page Parameters
+        /// </summary>
+        protected static class PageParameterKey
+        {
+            public const string PersistedDatasetId = "PersistedDatasetId";
+        }
+
+        #endregion PageParameterKey
+
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+            gList.GridRebind += gList_GridRebind;
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+
+            // Block Security and special attributes (RockPage takes care of View)
+            bool canAddEditDelete = IsUserAuthorized( Authorization.EDIT );
+
+
+            gList.DataKeyNames = new string[] { "Id" };
+            gList.Actions.ShowAdd = canAddEditDelete;
+            gList.IsDeleteEnabled = canAddEditDelete;
+            gList.Actions.AddClick += gList_AddClick;
+            gList.GridRebind += gList_GridRebind;
+            gList.EntityTypeId = EntityTypeCache.Get<PersistedDataset>().Id;
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                BindGrid();
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the gList control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="Rock.Web.UI.Controls.GridRebindEventArgs"/> instance containing the event data.</param>
+        private void gList_GridRebind( object sender, Rock.Web.UI.Controls.GridRebindEventArgs e )
+        {
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the AddClick event of the gList control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void gList_AddClick( object sender, EventArgs e )
+        {
+            NavigateToLinkedPage( AttributeKey.DetailPage, PageParameterKey.PersistedDatasetId, 0 );
+        }
+
+        /// <summary>
+        /// Handles the RowSelected event of the gList control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="Rock.Web.UI.Controls.RowEventArgs"/> instance containing the event data.</param>
+        protected void gList_RowSelected( object sender, Rock.Web.UI.Controls.RowEventArgs e )
+        {
+            NavigateToLinkedPage( AttributeKey.DetailPage, PageParameterKey.PersistedDatasetId, e.RowKeyId );
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbRefresh control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void lbRefresh_Click( object sender, RowEventArgs e )
+        {
+            var rockContext = new RockContext();
+            PersistedDatasetService persistedDatasetService = new PersistedDatasetService( rockContext );
+            PersistedDataset persistedDataset = persistedDatasetService.Get( e.RowKeyId );
+            persistedDataset.UpdateResultData();
+            rockContext.SaveChanges();
+
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the DeleteClick event of the gList control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="Rock.Web.UI.Controls.RowEventArgs"/> instance containing the event data.</param>
+        protected void gList_DeleteClick( object sender, Rock.Web.UI.Controls.RowEventArgs e )
+        {
+            var rockContext = new RockContext();
+            PersistedDatasetService persistedDatasetService = new PersistedDatasetService( rockContext );
+            PersistedDataset persistedDataset = persistedDatasetService.Get( e.RowKeyId );
+            if ( persistedDataset != null )
+            {
+                string errorMessage;
+                if ( !persistedDatasetService.CanDelete( persistedDataset, out errorMessage ) )
+                {
+                    mdGridWarning.Show( errorMessage, ModalAlertType.Information );
+                    return;
+                }
+
+                persistedDatasetService.Delete( persistedDataset );
+                rockContext.SaveChanges();
+            }
+
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Handles the DataBound event of the btnRefresh control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RowEventArgs"/> instance containing the event data.</param>
+        protected void btnRefresh_DataBound( object sender, RowEventArgs e )
+        {
+            var button = sender as LinkButton;
+            var persistedDataSet = e.Row.DataItem as PersistedDataset;
+            if ( button != null && persistedDataSet != null )
+            {
+                button.Enabled = persistedDataSet.AllowManualRefresh;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Binds the grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            RockContext rockContext = new RockContext();
+            PersistedDatasetService persistedDatasetService = new PersistedDatasetService( rockContext );
+
+            // Use AsNoTracking() since these records won't be modified, and therefore don't need to be tracked by the EF change tracker
+            var qry = persistedDatasetService.Queryable().AsNoTracking();
+
+            var sortProperty = gList.SortProperty;
+            if ( gList.AllowSorting && sortProperty != null )
+            {
+                qry = qry.Sort( sortProperty );
+            }
+            else
+            {
+                qry = qry.OrderBy( a => a.Name ).ThenBy( a => a.AccessKey );
+            }
+
+            gList.SetLinqDataSource( qry );
+            gList.DataBind();
+        }
+
+        #endregion
+    }
+}
