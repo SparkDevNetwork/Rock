564d5e25c0bc5ee5ff933839b1a0c721aa78a243
Mike Peterson <mike@sparkdevnetwork.org>
2019-09-18 17:59:42 -0700

    + Added new POST ~/api/Attendances/Import endpoint that can be used to quickly bulk import Attendance Records

---
+++ Rock/Bulk/BulkImport/AttendancesImport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Data;
+
+namespace Rock.BulkImport
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [RockClientInclude( "Import to POST to ~/api/Attendances/Import" )]
+    public class AttendancesImport
+    {
+        /// <summary>
+        /// Gets or sets the attendances.
+        /// </summary>
+        /// <value>
+        /// The attendances.
+        /// </value>
+        public List<AttendanceImport> Attendances { get; set; }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"{Attendances.Count} Attendances";
+        }
+    }
+}
