fdc95c8badb8ac9bd8c9b744114c8ade5b05ffc9
Mike Peterson <mike@sparkdevnetwork.org>
2019-10-30 20:11:37 -0700

    - Removed code that was made obsolete in 1.7 - Marked obsolete methods for 1.8 as Obsolete with Error

--- Rock/Model/CodeGenerated/NoteTypeService.cs
+++ Rock/Model/CodeGenerated/NoteTypeService.cs
@@ -102,9 +102,6 @@ namespace Rock.Model
             target.BackgroundColor = source.BackgroundColor;
             target.BinaryFileTypeId = source.BinaryFileTypeId;
             target.BorderColor = source.BorderColor;
-            #pragma warning disable 612, 618
-            target.CssClass = source.CssClass;
-            #pragma warning restore 612, 618
             target.EntityTypeId = source.EntityTypeId;
             target.EntityTypeQualifierColumn = source.EntityTypeQualifierColumn;
             target.EntityTypeQualifierValue = source.EntityTypeQualifierValue;
