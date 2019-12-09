SparkDevNetwork/Rock

commit 09dbacd9d7c8e052e4874c1a212e56209ef4f86a

- Fixed issue where Lava entity commands might get an error when attempting to lazy load properties on the items - Made small performance improvement in entity commands (1ms) when looking up which entity service to use

Rock/Lava/Blocks/RockEntity.cs
@@ -77,26 +77,26 @@ namespace Rock.Lava.Blocks
 
             bool hasFilter = false;
 
+            var modelName = string.Empty;
+
             // get a service for the entity based off it's friendly name
-            var entityTypes = EntityTypeCache.All();
-
-            var model = string.Empty;
-
             if ( _entityName == "business" )
             {
-                model = "Rock.Model.Person";
+                modelName = "Rock.Model.Person";
             }
             else
             {
-                model = "Rock.Model." + _entityName;
+                modelName = "Rock.Model." + _entityName;
             }
 
             // Check first to see if this is a core model
-            var entityTypeCache = entityTypes.Where( e => String.Equals( e.Name, model, StringComparison.OrdinalIgnoreCase ) ).FirstOrDefault();
+            var entityTypeCache = EntityTypeCache.Get( modelName );
 
-            // If not, look for first plug-in model that has same friendly name
             if ( entityTypeCache == null )
             {
+                var entityTypes = EntityTypeCache.All();
+
+                // If not, look for first plug-in model that has same friendly name
                 entityTypeCache = entityTypes
                     .Where( e =>
                         e.IsEntity &&
@@ -105,13 +105,13 @@ namespace Rock.Lava.Blocks
                         e.FriendlyName.RemoveSpaces().ToLower() == _entityName )
                     .OrderBy( e => e.Id )
                     .FirstOrDefault();
-            }
 
-            // If still null check to see if this was a duplicate class and full class name was used as entity name
-            if ( entityTypeCache == null )
-            {
-                model = _entityName.Replace( '_', '.' );
-                entityTypeCache = entityTypes.Where( e => String.Equals( e.Name, model, StringComparison.OrdinalIgnoreCase ) ).FirstOrDefault();
+                // If still null check to see if this was a duplicate class and full class name was used as entity name
+                if ( entityTypeCache == null )
+                {
+                    modelName = _entityName.Replace( '_', '.' );
+                    entityTypeCache = entityTypes.Where( e => String.Equals( e.Name, modelName, StringComparison.OrdinalIgnoreCase ) ).FirstOrDefault();
+                }
             }
 
             if ( entityTypeCache != null )
@@ -208,7 +208,9 @@ namespace Rock.Lava.Blocks
                     }
 
                     // Make the query from the expression
-                    MethodInfo getMethod = serviceInstance.GetType().GetMethod( "GetNoTracking", new Type[] { typeof( ParameterExpression ), typeof( Expression ), typeof( Rock.Web.UI.Controls.SortProperty ), typeof( int? ) } );
+                    // NOTE: We can't use AsNoTracking because lazy-loading doesn't always work https://stackoverflow.com/a/20290275/1755417
+                    MethodInfo getMethod = serviceInstance.GetType().GetMethod( "Get", new Type[] { typeof( ParameterExpression ), typeof( Expression ), typeof( Rock.Web.UI.Controls.SortProperty ), typeof( int? ) } );
+
                     if ( getMethod != null )
                     {
                         // get a listing of ids and build it into the query expression

Rock/Data/Service.cs
@@ -97,6 +97,7 @@ namespace Rock.Data
 
         /// <summary>
         /// Gets an <see cref="IQueryable{T}"/> list of all models
+        /// Note: You can sometimes improve performance by using Queryable().AsNoTracking(), but be careful. Lazy-Loading doesn't always work with AsNoTracking  https://stackoverflow.com/a/20290275/1755417
         /// </summary>
         /// <returns></returns>
         public virtual IQueryable<T> Queryable()
@@ -201,7 +202,8 @@ namespace Rock.Data
 
         /// <summary>
         /// Gets the model with the id value but doesn't load it into the EF ChangeTracker.
-        /// Use this if you won't be making any changes to the record
+        /// Use this if you won't be making any changes to the record and don't need lazy loading
+        /// Note: Lazy-Loading doesn't always work with AsNoTracking  https://stackoverflow.com/a/20290275/1755417
         /// </summary>
         /// <param name="id">id</param>
         /// <returns></returns>
@@ -212,7 +214,8 @@ namespace Rock.Data
 
         /// <summary>
         /// Gets the model with the Guid value but doesn't load it into the EF ChangeTracker.
-        /// Use this if you won't be making any changes to the record
+        /// Use this if you won't be making any changes to the record and don't need lazy loading
+        /// Note: Lazy-Loading doesn't always work with AsNoTracking  https://stackoverflow.com/a/20290275/1755417
         /// </summary>
         /// <param name="guid">The GUID.</param>
         /// <returns></returns>
@@ -258,6 +261,8 @@ namespace Rock.Data
 
         /// <summary>
         /// Gets a list of items that match the specified expression with EF tracking disabled.
+        /// Use this if you won't be making any changes to the records and don't need lazy loading
+        /// Note: Lazy-Loading doesn't always work with AsNoTracking  https://stackoverflow.com/a/20290275/1755417
         /// </summary>
         /// <param name="parameterExpression">The parameter expression.</param>
         /// <param name="whereExpression">The where expression.</param>
@@ -281,7 +286,8 @@ namespace Rock.Data
 
         /// <summary>
         /// Gets the specified parameter expression with no tracking.
-        /// </summary>
+        /// Use this if you won't be making any changes to the records and don't need lazy loading
+        /// Note: Lazy-Loading doesn't always work with AsNoTracking  https://stackoverflow.com/a/20290275/1755417/// </summary>
         /// <param name="parameterExpression">The parameter expression.</param>
         /// <param name="whereExpression">The where expression.</param>
         /// <param name="sortProperty">The sort property.</param>

Rock/Web/Cache/Entities/EntityTypeCache.cs
@@ -36,7 +36,7 @@ namespace Rock.Web.Cache
     {
         #region Static Fields
 
-        private static readonly ConcurrentDictionary<string, int> EntityTypes = new ConcurrentDictionary<string, int>();
+        private static readonly ConcurrentDictionary<string, int> EntityTypes = new ConcurrentDictionary<string, int>( StringComparer.OrdinalIgnoreCase );
 
         #endregion
 

commit 51ea9807690435f2aa555c7e4ad4a0846e113001

- Fixed issue introduced with 09dbacd9d7c8e052e4874c1a212e56209ef4f86a where RockEntity was creating an EntityType if the EntityType wasn't found, instead of just return null if it wasn't found.

Rock/Lava/Blocks/RockEntity.cs
@@ -89,8 +89,8 @@ namespace Rock.Lava.Blocks
                 modelName = "Rock.Model." + _entityName;
             }
 
-            // Check first to see if this is a core model
-            var entityTypeCache = EntityTypeCache.Get( modelName );
+            // Check first to see if this is a core model. use the createIfNotFound = false option
+            var entityTypeCache = EntityTypeCache.Get( modelName, false );
 
             if ( entityTypeCache == null )
             {