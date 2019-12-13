2a5e1977acb804bf159f9f7030ee1e9fbeb6ac60
Garrett Johnson <nvisiondesign@gmail.com>
2019-12-13 15:33:17 -0700

    - Fixed JS Style on Rock Controls.

--- RockWeb/Scripts/Rock/Controls/rockCheckBoxList.js
+++ RockWeb/Scripts/Rock/Controls/rockCheckBoxList.js
@@ -12,24 +12,16 @@
                 var checkboxes = $checkBoxList.find('input');
                 var isValid = false;
                 for (var i = 0; i < checkboxes.length; i++) {
-                    {
-                        if (checkboxes[i].checked) {
-                            {
-                                isValid = true;
-                                break;
-                            }
-                        }
+                    if (checkboxes[i].checked) {
+                        isValid = true;
+                        break;
                     }
                 }
 
                 if (isValid) {
-                    {
                         $checkBoxList.removeClass('has-error');
-                    }
                 } else {
-                    {
                         $checkBoxList.addClass('has-error');
-                    }
                 }
 
                 args.IsValid = isValid;
