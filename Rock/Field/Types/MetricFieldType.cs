2596f2f6da910edd11d7436d70f8d2ee3ed6a916
Shaun Cummings <shaun@sparkabilitygroup.com>
2020-01-06 18:56:44 -0700

    + Added new MetricItemPicker control and MetricField attribute.

---
+++ Rock/Field/Types/MetricFieldType.cs
@@ -0,0 +1,246 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Constants;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace Rock.Field.Types
+{
+    /// <summary>
+    /// Metric Field Type.  Stored as Metric's Guid.
+    /// </summary>
+    public class MetricFieldType : FieldType, IEntityFieldType
+    {
+
+        #region Configuration
+
+        /// <summary>
+        /// Returns a list of the configuration keys
+        /// </summary>
+        /// <returns></returns>
+        public override List<string> ConfigurationKeys()
+        {
+            var configKeys = base.ConfigurationKeys();
+            return configKeys;
+        }
+
+        /// <summary>
+        /// Creates the HTML controls required to configure this type of field
+        /// </summary>
+        /// <returns></returns>
+        public override List<Control> ConfigurationControls()
+        {
+            List<Control> controls = new List<Control>();
+            return controls;
+        }
+
+        /// <summary>
+        /// Gets the configuration value.
+        /// </summary>
+        /// <param name="controls">The controls.</param>
+        /// <returns></returns>
+        public override Dictionary<string, ConfigurationValue> ConfigurationValues( List<Control> controls )
+        {
+            Dictionary<string, ConfigurationValue> configurationValues = new Dictionary<string, ConfigurationValue>();
+            return configurationValues;
+        }
+
+        /// <summary>
+        /// Sets the configuration value.
+        /// </summary>
+        /// <param name="controls"></param>
+        /// <param name="configurationValues"></param>
+        public override void SetConfigurationValues( List<Control> controls, Dictionary<string, ConfigurationValue> configurationValues )
+        {
+        }
+
+        #endregion
+
+        #region Formatting
+
+        /// <summary>
+        /// Returns the field's current value(s)
+        /// </summary>
+        /// <param name="parentControl">The parent control.</param>
+        /// <param name="value">Information about the value</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="condensed">Flag indicating if the value should be condensed (i.e. for use in a grid column)</param>
+        /// <returns></returns>
+        public override string FormatValue( Control parentControl, string value, Dictionary<string, ConfigurationValue> configurationValues, bool condensed )
+        {
+            string formattedValue = string.Empty;
+
+            Guid? guid = value.AsGuidOrNull();
+            if ( guid.HasValue )
+            {
+                using ( var rockContext = new RockContext() )
+                {
+                    var service = new MetricService( rockContext );
+                    var metric = service.GetNoTracking( guid.Value );
+
+                    if ( metric != null )
+                    {
+                        formattedValue = metric.Title;
+                    }
+                }
+            }
+
+            return base.FormatValue( parentControl, formattedValue, null, condensed );
+        }
+
+        #endregion
+
+        #region Edit Control 
+
+        /// <summary>
+        /// Creates the control(s) necessary for prompting user for a new value
+        /// </summary>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="id"></param>
+        /// <returns>
+        /// The control
+        /// </returns>
+        public override Control EditControl( Dictionary<string, ConfigurationValue> configurationValues, string id )
+        {
+            return new MetricItemPicker { ID = id };
+        }
+
+        /// <summary>
+        /// Reads new values entered by the user for the field
+        /// returns Metric.Guid
+        /// </summary>
+        /// <param name="control">Parent control that controls were added to in the CreateEditControl() method</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <returns></returns>
+        public override string GetEditValue( Control control, Dictionary<string, ConfigurationValue> configurationValues )
+        {
+            var picker = control as MetricItemPicker;
+
+            if ( picker != null )
+            {
+                int? id = picker.SelectedValue.AsIntegerOrNull();
+                if ( id.HasValue )
+                {
+                    using ( var rockContext = new RockContext() )
+                    {
+                        var metric = new MetricService( rockContext ).GetNoTracking( id.Value );
+
+                        if ( metric != null )
+                        {
+                            return metric.Guid.ToString();
+                        }
+                    }
+                }
+
+                return string.Empty;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the value.
+        /// value is an Account.Guid
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="value">The value.</param>
+        public override void SetEditValue( Control control, Dictionary<string, ConfigurationValue> configurationValues, string value )
+        {
+            var picker = control as MetricItemPicker;
+
+            if ( picker != null )
+            {
+                Guid guid = value.AsGuid();
+
+                // get the item (or null) and set it
+                var metric = new MetricService( new RockContext() ).Get( guid );
+                picker.SetValue( metric );
+            }
+        }
+
+        #endregion
+
+        #region Entity Methods
+
+        /// <summary>
+        /// Gets the edit value as the IEntity.Id
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <returns></returns>
+        public int? GetEditValueAsEntityId( Control control, Dictionary<string, ConfigurationValue> configurationValues )
+        {
+            Guid guid = GetEditValue( control, configurationValues ).AsGuid();
+            var item = new MetricService( new RockContext() ).Get( guid );
+            return item != null ? item.Id : (int?)null;
+        }
+
+        /// <summary>
+        /// Sets the edit value from IEntity.Id value
+        /// </summary>
+        /// <param name="control">The control.</param>
+        /// <param name="configurationValues">The configuration values.</param>
+        /// <param name="id">The identifier.</param>
+        public void SetEditValueFromEntityId( Control control, Dictionary<string, ConfigurationValue> configurationValues, int? id )
+        {
+            var item = new MetricService( new RockContext() ).Get( id ?? 0 );
+            string guidValue = item != null ? item.Guid.ToString() : string.Empty;
+            SetEditValue( control, configurationValues, guidValue );
+        }
+
+        /// <summary>
+        /// Gets the entity.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public IEntity GetEntity( string value )
+        {
+            return GetEntity( value, null );
+        }
+
+        /// <summary>
+        /// Gets the entity.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="rockContext">The rock context.</param>
+        /// <returns></returns>
+        public IEntity GetEntity( string value, RockContext rockContext )
+        {
+            Guid? guid = value.AsGuidOrNull();
+            if ( guid.HasValue )
+            {
+                rockContext = rockContext ?? new RockContext();
+                return new MetricService( rockContext ).Get( guid.Value );
+            }
+
+            return null;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Rock/Web/UI/Controls/Pickers/MetricItemPicker.cs
@@ -0,0 +1,177 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+namespace Rock.Web.UI.Controls
+{
+    /// <summary>
+    /// Selects one or more metrics.
+    /// </summary>
+    public class MetricItemPicker : ItemPicker
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            SetExtraRestParams();
+
+            this.IconCssClass = "fa fa-filter";
+            base.OnInit( e );
+        }
+
+        /// <summary>
+        /// Sets the extra rest parameters.
+        /// </summary>
+        private void SetExtraRestParams()
+        {
+            ItemRestUrlExtraParams = string.Empty;
+
+            if ( this.CategoryGuids?.Any() == true )
+            {
+                string includedCategoryIds = this.CategoryGuids
+                    .Select( a => CategoryCache.Get( a ) )
+                    .Where( a => a != null )
+                    .Select( a => a.Id )
+                    .ToList().AsDelimited( "," );
+
+                AddRestQueryValue( "includedCategoryIds", includedCategoryIds );
+            }
+        }
+
+        /// <summary>
+        /// Adds a name/value pair to ItemRestUrlExtraParams with the appropriate delimiter.
+        /// </summary>
+        /// <param name="name">The name of the query string variable.</param>
+        /// <param name="value">The value of the variable.</param>
+        private void AddRestQueryValue( string name, string value )
+        {
+            if ( string.IsNullOrWhiteSpace( this.ItemRestUrlExtraParams ) )
+            {
+                ItemRestUrlExtraParams = "?" + name + "=" + value;
+            }
+            else
+            {
+                ItemRestUrlExtraParams += "&" + name + "=" + value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets an optional list of category guids to limit metrics by
+        /// </summary>
+        /// <value>
+        /// The category guids.
+        /// </value>
+        public List<Guid> CategoryGuids
+        {
+            get
+            {
+                return ViewState["CategoryGuids"] as List<Guid>;
+            }
+
+            set
+            {
+                ViewState["CategoryGuids"] = value;
+                SetExtraRestParams();
+            }
+        }
+
+        /// <summary>
+        /// Gets the item rest URL.
+        /// </summary>
+        /// <value>
+        /// The item rest URL.
+        /// </value>
+        public override string ItemRestUrl => "~/api/Categories/GetMetricChildren/";
+
+        /// <summary>
+        /// Sets the value.
+        /// </summary>
+        /// <param name="metric">The metric.</param>
+        public void SetValue( Metric metric )
+        {
+            if ( metric != null )
+            {
+                ItemId = metric.Id.ToString();
+                ItemName = metric.Title;
+            }
+            else
+            {
+                ItemId = Constants.None.IdValue;
+                ItemName = Constants.None.TextHtml;
+            }
+        }
+
+        /// <summary>
+        /// Sets the values.
+        /// </summary>
+        /// <param name="metrics">The metrics.</param>
+        public void SetValues( IEnumerable<Metric> metrics )
+        {
+            var metricList = metrics.ToList();
+
+            if ( metricList.Any() )
+            {
+                var ids = new List<string>();
+                var names = new List<string>();
+
+                foreach ( var metric in metricList )
+                {
+                    if ( metric != null )
+                    {
+                        ids.Add( metric.Id.ToString() );
+                        names.Add( metric.Title );
+                    }
+                }
+
+                ItemIds = ids;
+                ItemNames = names;
+            }
+            else
+            {
+                ItemId = Constants.None.IdValue;
+                ItemName = Constants.None.TextHtml;
+            }
+        }
+
+        /// <summary>
+        /// Sets the value on select.
+        /// </summary>
+        protected override void SetValueOnSelect()
+        {
+            var metric = new MetricService( new RockContext() ).Get( ItemId.AsInteger() );
+            SetValue( metric );
+        }
+
+        /// <summary>
+        /// Sets the values on select.
+        /// </summary>
+        protected override void SetValuesOnSelect()
+        {
+            var metrics = new MetricService( new RockContext() ).Queryable().Where( m => ItemIds.Contains( m.Id.ToString() ) );
+            this.SetValues( metrics );
+        }
+    }
+}
