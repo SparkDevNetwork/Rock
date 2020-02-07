SparkDevNetwork/Rock

commit 416cb2dca5e193071f5b88b1fb7cabaa32bdbb0d

- Updated PageZoneBlocksEditor (CMS Configuration > Pages ) to show "invalid zones" if there are any blocks that are in zones that aren't in the current layout zones. This will help so that blocks from invalid zones can be identified and moved to a valid zone.

RockWeb/Blocks/Cms/PageZoneBlocksEditor.ascx.cs
@@ -206,6 +206,8 @@ namespace RockWeb.Blocks.Cms
             int pageId = hfPageId.Value.AsInteger();
             var page = PageCache.Get( pageId );
 
+            hlInvalidZoneWarning.Visible = _invalidPageZones.Contains( zoneName );
+
             lZoneTitle.Text = string.Format( "{0} Zone", zoneName );
             lZoneIcon.Text = "<i class='fa fa-th-large'></i>";
             if ( page != null )
@@ -231,6 +233,19 @@ namespace RockWeb.Blocks.Cms
             }
         }
 
+        private string[] _invalidPageZones
+        {
+            get
+            {
+                return ViewState["_invalidPageZones"] as string[];
+            }
+
+            set
+            {
+                ViewState["_invalidPageZones"] = value;
+            }
+        }
+
         /// <summary>
         /// Loads the drop downs.
         /// </summary>
@@ -243,6 +258,7 @@ namespace RockWeb.Blocks.Cms
             var page = PageCache.Get( pageId );
             if ( page != null )
             {
+                // get the valid zones from the Layout for this page
                 var zoneNames = FindZoneNames( page );
 
                 foreach ( var zoneName in zoneNames )
@@ -250,6 +266,14 @@ namespace RockWeb.Blocks.Cms
                     var zoneBlockCount = page.Blocks.Where( a => a.Zone.Equals( zoneName, StringComparison.OrdinalIgnoreCase ) ).Count();
                     ddlZones.Items.Add( new ListItem( string.Format( "{0} ({1})", zoneName, zoneBlockCount ), zoneName ) );
                     ddlMoveToZoneList.Items.Add( new ListItem( zoneName, zoneName ) );
+                }
+
+                // get any zones from blocks that have a zone that isn't part of the layout, then add those the list, but not the MoveToZoneList
+                _invalidPageZones = page.Blocks.Select( a => a.Zone ).ToList().Where( z => !zoneNames.Contains( z ) ).ToArray();
+                foreach ( var invalidPageZone in _invalidPageZones )
+                {
+                    var zoneBlockCount = page.Blocks.Where( a => a.Zone.Equals( invalidPageZone, StringComparison.OrdinalIgnoreCase ) ).Count();
+                    ddlZones.Items.Add( new ListItem( string.Format( "{0} ({1})", invalidPageZone, zoneBlockCount ), invalidPageZone ) );
                 }
 
                 // default to Main Zone (if there is one)

RockWeb/Blocks/Cms/PageZoneBlocksEditor.ascx
@@ -11,6 +11,8 @@
                     <asp:Literal ID="lZoneTitle" runat="server" /></h1>
                 </div>
 
+                <Rock:HighlightLabel ID="hlInvalidZoneWarning" runat="server" LabelType="Danger" CssClass="margin-l-md" Text="Invalid Zone" ToolTip="This zone is no longer part of the zones for this layout." Visible="false" />
+
                 <div class="pull-right">
                     <div class="input-group input-group-sm">
                         <Rock:RockDropDownList ID="ddlZones" runat="server" Label="" AutoPostBack="true" OnSelectedIndexChanged="ddlZones_SelectedIndexChanged" />

commit 4c8274e5cee158674b3f8c28490eeea671d457ea

- Fix exception in PageZoneBlocksEditor introduced with 416cb2dca5e193071f5b88b1fb7cabaa32bdbb0d

RockWeb/Blocks/Cms/PageZoneBlocksEditor.ascx.cs
@@ -206,7 +206,7 @@ namespace RockWeb.Blocks.Cms
             int pageId = hfPageId.Value.AsInteger();
             var page = PageCache.Get( pageId );
 
-            hlInvalidZoneWarning.Visible = _invalidPageZones.Contains( zoneName );
+            hlInvalidZoneWarning.Visible = _invalidPageZones != null && _invalidPageZones.Contains( zoneName );
 
             lZoneTitle.Text = string.Format( "{0} Zone", zoneName );
             lZoneIcon.Text = "<i class='fa fa-th-large'></i>";
@@ -237,7 +237,7 @@ namespace RockWeb.Blocks.Cms
         {
             get
             {
-                return ViewState["_invalidPageZones"] as string[];
+                return ViewState["_invalidPageZones"] as string[] ?? new string[0];
             }
 
             set