f248851afe7daf19739347b4ebd5013fa16ec8d3
Jason Hendee <jason.hendee@sparkdevnetwork.org>
2020-02-25 14:02:54 -0700

    - Added EF migration for "Made small adjustments to Advanced Group Placement based on feedback from review."

--- Rock/Plugin/HotFixes/100_MigrationRollupsFor10_2_1.cs
+++ Rock/Plugin/HotFixes/100_MigrationRollupsFor10_2_1.cs
@@ -28,13 +28,13 @@ namespace Rock.Plugin.HotFixes
         /// </summary>
         public override void Up()
         {
-            RockMigrationHelper.AddPage( true, "7F79E512-B9DB-4780-9887-AD6D63A39050", "D65F783D-87A9-4CC9-8110-E83466A0EADB", "Email Analytics", "", "DF014200-72A3-48A0-A953-E594E5410E36", "" ); // Site:Rock RMS
-            RockMigrationHelper.AddPageRoute( "DF014200-72A3-48A0-A953-E594E5410E36", "EmailAnalytics", "DB116A39-BCE2-40D1-9604-FE955472DD35" );// for Page:Email Analytics
-            RockMigrationHelper.UpdateBlockType( "Email Analytics", "Shows a graph of email statistics optionally limited to a specific communication or communication list.", "~/Blocks/Communication/EmailAnalytics.ascx", "Communication", "7B506760-93FA-4FBF-9FB5-0D9C3E36DCCD" );
+            //RockMigrationHelper.AddPage( true, "7F79E512-B9DB-4780-9887-AD6D63A39050", "D65F783D-87A9-4CC9-8110-E83466A0EADB", "Email Analytics", "", "DF014200-72A3-48A0-A953-E594E5410E36", "" ); // Site:Rock RMS
+            //RockMigrationHelper.AddPageRoute( "DF014200-72A3-48A0-A953-E594E5410E36", "EmailAnalytics", "DB116A39-BCE2-40D1-9604-FE955472DD35" );// for Page:Email Analytics
+            //RockMigrationHelper.UpdateBlockType( "Email Analytics", "Shows a graph of email statistics optionally limited to a specific communication or communication list.", "~/Blocks/Communication/EmailAnalytics.ascx", "Communication", "7B506760-93FA-4FBF-9FB5-0D9C3E36DCCD" );
 
-            // Add Block to Page: Email Analytics Site: Rock RMS
-            RockMigrationHelper.AddBlock( true, "DF014200-72A3-48A0-A953-E594E5410E36".AsGuid(), null, "C2D29296-6A87-47A9-A753-EE4E9159C4C4".AsGuid(), "7B506760-93FA-4FBF-9FB5-0D9C3E36DCCD".AsGuid(), "Email Analytics", "Main", @"", @"", 0, "DC951B4F-0F07-47C3-A279-D1AFA1C50549" );
-            Sql( @"UPDATE [Block] SET [Order] = 0 WHERE [Guid] = 'DC951B4F-0F07-47C3-A279-D1AFA1C50549'" );  // Page: Email Analytics,  Zone: Main,  Block: Email Analytics
+            //// Add Block to Page: Email Analytics Site: Rock RMS
+            //RockMigrationHelper.AddBlock( true, "DF014200-72A3-48A0-A953-E594E5410E36".AsGuid(), null, "C2D29296-6A87-47A9-A753-EE4E9159C4C4".AsGuid(), "7B506760-93FA-4FBF-9FB5-0D9C3E36DCCD".AsGuid(), "Email Analytics", "Main", @"", @"", 0, "DC951B4F-0F07-47C3-A279-D1AFA1C50549" );
+            //Sql( @"UPDATE [Block] SET [Order] = 0 WHERE [Guid] = 'DC951B4F-0F07-47C3-A279-D1AFA1C50549'" );  // Page: Email Analytics,  Zone: Main,  Block: Email Analytics
         }
 
         /// <summary>
--- Rock/Plugin/HotFixes/102_RapidAttendanceEntry.cs
+++ Rock/Plugin/HotFixes/102_RapidAttendanceEntry.cs
@@ -28,18 +28,18 @@ namespace Rock.Plugin.HotFixes
         /// </summary>
         public override void Up()
         {
-            RockMigrationHelper.AddPage( true, "78B79290-3234-4D8C-96D3-1901901BA1DD", "D65F783D-87A9-4CC9-8110-E83466A0EADB", "Attendance List", "", "D56CD916-C3C7-4277-BEBA-0FA4C21A758D", "" ); // Site:Rock RMS
-            RockMigrationHelper.UpdateBlockType( "Attendance List", "Block for displaying the attendance history of a person or a group.", "~/Blocks/CheckIn/AttendanceList.ascx", "Checkin", "678ED4B6-D76F-4D43-B069-659E352C9BD8" );
-            RockMigrationHelper.AddBlock( true, "D56CD916-C3C7-4277-BEBA-0FA4C21A758D".AsGuid(), null, "C2D29296-6A87-47A9-A753-EE4E9159C4C4".AsGuid(), "678ED4B6-D76F-4D43-B069-659E352C9BD8".AsGuid(), "Attendance List", "Main", @"", @"", 0, "9590C855-903E-4CA4-9296-FB9D6F02FA41" );
+            //RockMigrationHelper.AddPage( true, "78B79290-3234-4D8C-96D3-1901901BA1DD", "D65F783D-87A9-4CC9-8110-E83466A0EADB", "Attendance List", "", "D56CD916-C3C7-4277-BEBA-0FA4C21A758D", "" ); // Site:Rock RMS
+            //RockMigrationHelper.UpdateBlockType( "Attendance List", "Block for displaying the attendance history of a person or a group.", "~/Blocks/CheckIn/AttendanceList.ascx", "Checkin", "678ED4B6-D76F-4D43-B069-659E352C9BD8" );
+            //RockMigrationHelper.AddBlock( true, "D56CD916-C3C7-4277-BEBA-0FA4C21A758D".AsGuid(), null, "C2D29296-6A87-47A9-A753-EE4E9159C4C4".AsGuid(), "678ED4B6-D76F-4D43-B069-659E352C9BD8".AsGuid(), "Attendance List", "Main", @"", @"", 0, "9590C855-903E-4CA4-9296-FB9D6F02FA41" );
 
-            // Attrib for BlockType: Rapid Attendance Entry:Add Family Page              
-            RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "6C2ED1FA-218B-4ACC-B661-A2618F310CD4", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Add Family Page", "AddFamilyPage", "Add Family Page", @"Page used for adding new families.", 0, @"", "CB4EDB5A-2919-48C3-BB9F-434C1024AF92" );
-            // Attrib for BlockType: Rapid Attendance Entry:Attendance List Page             
-            RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "6C2ED1FA-218B-4ACC-B661-A2618F310CD4", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Attendance List Page", "AttendanceListPage", "Attendance List Page", @"Page used to show the attendance list.", 1, @"", "2AA8EFA0-2B21-4A36-9326-5F71C621FBD7" );
-            // Attrib Value for Block:Rapid Attendance Entry, Attribute:Add Family Page Page: Rapid Attendance Entry, Site: Rock RMS           
-            RockMigrationHelper.AddBlockAttributeValue( "24560306-8535-4119-BA4A-CBC172C3832C", "CB4EDB5A-2919-48C3-BB9F-434C1024AF92", @"6A11A13D-05AB-4982-A4C2-67A8B1950C74" );
-            // Attrib Value for Block:Rapid Attendance Entry, Attribute:Attendance List Page Page: Rapid Attendance Entry, Site: Rock RMS   
-            RockMigrationHelper.AddBlockAttributeValue( "24560306-8535-4119-BA4A-CBC172C3832C", "2AA8EFA0-2B21-4A36-9326-5F71C621FBD7", @"D56CD916-C3C7-4277-BEBA-0FA4C21A758D" );
+            //// Attrib for BlockType: Rapid Attendance Entry:Add Family Page              
+            //RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "6C2ED1FA-218B-4ACC-B661-A2618F310CD4", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Add Family Page", "AddFamilyPage", "Add Family Page", @"Page used for adding new families.", 0, @"", "CB4EDB5A-2919-48C3-BB9F-434C1024AF92" );
+            //// Attrib for BlockType: Rapid Attendance Entry:Attendance List Page             
+            //RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "6C2ED1FA-218B-4ACC-B661-A2618F310CD4", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Attendance List Page", "AttendanceListPage", "Attendance List Page", @"Page used to show the attendance list.", 1, @"", "2AA8EFA0-2B21-4A36-9326-5F71C621FBD7" );
+            //// Attrib Value for Block:Rapid Attendance Entry, Attribute:Add Family Page Page: Rapid Attendance Entry, Site: Rock RMS           
+            //RockMigrationHelper.AddBlockAttributeValue( "24560306-8535-4119-BA4A-CBC172C3832C", "CB4EDB5A-2919-48C3-BB9F-434C1024AF92", @"6A11A13D-05AB-4982-A4C2-67A8B1950C74" );
+            //// Attrib Value for Block:Rapid Attendance Entry, Attribute:Attendance List Page Page: Rapid Attendance Entry, Site: Rock RMS   
+            //RockMigrationHelper.AddBlockAttributeValue( "24560306-8535-4119-BA4A-CBC172C3832C", "2AA8EFA0-2B21-4A36-9326-5F71C621FBD7", @"D56CD916-C3C7-4277-BEBA-0FA4C21A758D" );
         }
 
         /// <summary>
