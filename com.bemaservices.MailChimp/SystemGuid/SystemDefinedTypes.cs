38e0c54519ed0341ce587d64da9bab5ce3484592
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2020-03-25 15:05:17 -0400

    Intial Project
    This adds the project to the solution, and adds the reference to Rock Web.  I also Created the SystemGuids for Defined Types.

---
+++ com.bemaservices.MailChimp/SystemGuid/SystemDefinedTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.bemaservices.MailChimp.SystemGuid
+{
+    public static class SystemDefinedTypes
+    {
+        /// <summary>
+        /// The open connects digest system email Guid
+        /// </summary>
+        public static string MAIL_CHIMP_ACCOUNTS = "EBA82C23-F5A7-41C3-9722-987E61E7F7FC";
+
+        public static string MAIL_CHIMP_LISTS = "0ED80CA8-987E-4A00-8CA5-56D0A4BDD629";
+    }
+}
