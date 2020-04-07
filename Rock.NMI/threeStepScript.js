SparkDevNetwork/Rock

commit 542e175d2861da09f805317032ddf809330e5009

+ Improved validation and error handling for NMI Gateway (Fixes #2084)

Rock.NMI/threeStepScript.js
@@ -1,0 +1,163 @@
+﻿Sys.Application.add_load(function () {
+    /* Threestep gateway related */
+    
+    var $iframeStep2 = $('.js-step2-iframe');
+    if ($iframeStep2.length == 0) {
+        // no step2 iframe shown yet
+        return;
+    }
+
+    // {{validationGroup}} will get replaced with whatever the validationGroup is
+    var validationGroup = '{{validationGroup}}';
+    var $step2Submit = $('.js-step2-submit');
+    var $step2Url = $('.js-step2-url');
+    var $updateProgress = $('#updateProgress');
+    var $addressControl = $('.js-billingaddress-control');
+
+    // {{postbackControlReference}} will get replaced with whatever the postback control reference is
+    var postbackControlReferenceScript = "javascript:{{postbackControlReference}}";
+
+    // Posts the iframe (step 2)
+    $step2Submit.off('click').on('click', function (e) {
+
+        e.preventDefault();
+
+        var $submitButton = $(this);
+        if ($submitButton.attr('disabled')) {
+            return;
+        }
+
+        // If there is a payment type selection, use the selected one.
+        // If there isn't a payment type selection (like on the registration blocks) it is CreditCard
+        var paymentType = $('.js-payment-tab').val() ?? 'CreditCard';
+
+        if (typeof (Page_ClientValidate) == 'function') {
+            if (Page_IsValid && Page_ClientValidate(validationGroup)) {
+                $submitButton.attr('disabled', 'disabled');
+
+                var src = $step2Url.val();
+                var $form = $iframeStep2.contents().find('#Step2Form');
+                var $cbBillingAddressCheckbox = $('.js-billing-address-checkbox');
+                var populateBilling = true;
+
+                if ($cbBillingAddressCheckbox.length == 0) {
+                    populateBilling = true;
+                } else {
+                    populateBilling = ($cbBillingAddressCheckbox.is(':visible') && ($cbBillingAddressCheckbox.prop('checked')));
+                }
+
+                if (populateBilling && $addressControl.length) {
+                    $form.find('.js-billing-address1').val($('.js-street1', $addressControl).val());
+                    $form.find('.js-billing-city').val($('.js-city', $addressControl).val());
+                    $form.find('.js-billing-state').val($('.js-state', $addressControl).val());
+                    $form.find('.js-billing-postal').val($('.js-postal-code', $addressControl).val());
+                    $form.find('.js-billing-country').val($('.js-country', $addressControl).val());
+                }
+            }
+
+            if (paymentType == 'CreditCard') {
+                var $cardFirstName = $('.js-creditcard-firstname');
+                var $cardLastName = $('.js-creditcard-lastname');
+                var $cardFullName = $('.js-creditcard-fullname');
+                var $cardNumber = $('.js-creditcard-number');
+                var $cardExpMonth = $('.js-monthyear-month');
+                var $cardExpYear = $('.js-monthyear-year');
+                var $cardCVV = $('.js-creditcard-cvv');
+                var validationMessage = '';
+                if ($cardNumber.val() == '') {
+                    validationMessage += '<li>' + 'Card number is required' + '</li>'
+                }
+
+                if ($cardExpMonth.val().trim() == '' || $cardExpYear.val().trim() == '') {
+                    validationMessage += '<li>' + 'Expiration is required' + '</li>'
+                }
+
+                if ($cardCVV.val().trim() == '') {
+                    validationMessage += '<li>' + 'CVV is required' + '</li>'
+                }
+
+                if (validationMessage != '') {
+                    var $creditCardValidationNotification = $('.js-creditcard-validation-notification');
+                    $('.js-notification-text', $creditCardValidationNotification).html('<ul>' + validationMessage + '</ul>');
+                    $creditCardValidationNotification.show();
+                    $submitButton.removeAttr('disabled');
+                    return false;
+                }
+
+                $form.find('.js-cc-first-name').val($cardFirstName.val());
+                $form.find('.js-cc-last-name').val($cardLastName.val());
+                $form.find('.js-cc-full-name').val($cardFullName.val());
+                $form.find('.js-cc-number').val($cardNumber.val());
+                var mm = $cardExpMonth.val();
+                var yy = $cardExpYear.val();
+                mm = mm.length == 1 ? '0' + mm : mm;
+                yy = yy.length == 4 ? yy.substring(2, 4) : yy;
+                $form.find('.js-cc-expiration').val(mm + yy);
+                $form.find('.js-cc-cvv').val($cardCVV.val());
+            } else {
+                var $achAccountName = $('.js-ach-accountname');
+                var $achAccountNumber = $('.js-ach-accountnumber');
+                var $achAccountRoutingNumber = $('.js-ach-routingnumber');
+                var $achAccountType = $('.js-ach-accounttype').find('input:checked');
+
+                var validationMessage = '';
+                if ($achAccountName.val().trim() == '') {
+                    validationMessage += '<li>' + 'Account name is required' + '</li>'
+                }
+                if ($achAccountNumber.val().trim() == '') {
+                    validationMessage += '<li>' + 'Account number is required' + '</li>'
+                }
+                if ($achAccountRoutingNumber.val().trim() == '') {
+                    validationMessage += '<li>' + 'Routing number is required' + '</li>'
+                }
+                if (validationMessage != '') {
+                    var $achValidationNotification = $('.js-ach-validation-notification');
+                    $('.js-notification-text', $achValidationNotification).html('<ul>' + validationMessage + '</ul>');
+                    $achValidationNotification.show();
+                    $submitButton.removeAttr('disabled');
+                    return false;
+                }
+
+                $form.find('.js-account-name').val($achAccountName.val());
+                $form.find('.js-account-number').val($achAccountNumber.val());
+                $form.find('.js-routing-number').val($achAccountRoutingNumber.val());
+                $form.find('.js-account-type').val($achAccountType.val());
+                $form.find('.js-entity-type').val('personal');
+            }
+
+            $updateProgress.show();
+            $form.attr('action', src);
+            $form.submit();
+        }
+    });
+
+    // Evaluates the current url whenever the iframe is loaded and if it includes a qrystring parameter
+    // The qry parameter value is saved to a hidden field and a post back is performed
+    $iframeStep2.off('load').on('load', function (e) {
+        try {
+            var location = this.contentWindow.location;
+            var qryString = this.contentWindow.location.search;
+            if (qryString && qryString != '' && qryString.startsWith('?token-id')) {
+                $('.js-step2-returnquerystring').val(qryString);
+                window.location = postbackControlReferenceScript;
+            } else {
+                if ($('.js-step2-autosubmit').val() == 'true') {
+                    $('#updateProgress').show();
+                    var src = $('.js-step2-url').val();
+                    var $form = $('#iframeStep2').contents().find('#Step2Form');
+                    $form.attr('action', src);
+                    $form.submit();
+                    $('#updateProgress').hide();
+                }
+            }
+        }
+        catch (err) {
+            // some sort of exception was generated. This can be caused by a user attempting to put html or tags for input values.
+            var $creditCardOrAchValidationNotification = $('.js-creditcard-validation-notification, .js-ach-validation-notification');
+            $('#updateProgress').hide();
+            $('.js-notification-text', $creditCardOrAchValidationNotification).text('Error submitting payment information. This might be caused by invalid values.');
+            $creditCardOrAchValidationNotification.show();
+            console.log(err);
+        }
+    });
+});

Rock.NMI/Gateway.cs
@@ -390,7 +390,7 @@ namespace Rock.NMI
                     }
 
                     // write result error as an exception
-                    ExceptionLogService.LogException( new Exception( $@"Error processing NMI transaction.
+                    ExceptionLogService.LogException( new NMIGatewayException( $@"Error processing NMI transaction.
 Result Code:  {threeStepChangeStep3Response.ResultCode} ({resultCodeMessage}).
 Result text: {threeStepChangeStep3Response.ResultText}.
 Amount: {threeStepChangeStep3Response.Amount}.
@@ -741,7 +741,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                     }
                     catch ( Exception ex )
                     {
-                        ExceptionLogService.LogException( ex );
+                        ExceptionLogService.LogException( new NMIGatewayException( "Unexpected PaymentStatus response from NMI Gateway", ex ) );
                         subscription = null;
                     }
 
@@ -818,7 +818,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                 }
                 catch ( Exception ex )
                 {
-                    ExceptionLogService.LogException( ex );
+                    ExceptionLogService.LogException( new NMIGatewayException( "Unexpected GetPayments Response from NMI Gateway", ex ) );
                     queryTransactionsResponse = null;
                 }
 
@@ -911,7 +911,9 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             catch ( WebException webException )
             {
                 string message = GetResponseMessage( webException.Response.GetResponseStream() );
-                throw new Exception( webException.Message + " - " + message );
+                var nmiGatewayException = new NMIGatewayException( webException.Message + " - " + message, webException );
+                ExceptionLogService.LogException( nmiGatewayException );
+                throw nmiGatewayException;
             }
 
             return paymentList;
@@ -1098,7 +1100,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                     }
                     catch ( Exception ex )
                     {
-                        ExceptionLogService.LogException( ex );
+                        ExceptionLogService.LogException( new NMIGatewayException( "Unexpected ThreeStep Response from NMI Gateway", ex ) );
                         postResult = null;
                     }
                 }
@@ -1108,7 +1110,9 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             catch ( WebException webException )
             {
                 string message = GetResponseMessage( webException.Response.GetResponseStream() );
-                throw new Exception( webException.Message + " - " + message );
+                var nmiGatewayException = new NMIGatewayException( webException.Message + " - " + message, webException );
+                ExceptionLogService.LogException( nmiGatewayException );
+                throw nmiGatewayException;
             }
             catch ( Exception ex )
             {
@@ -1245,7 +1249,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                     }
                     catch ( Exception ex )
                     {
-                        ExceptionLogService.LogException( ex );
+                        ExceptionLogService.LogException( new NMIGatewayException( "Unexpected DirectPost Response from NMI Gateway", ex ) );
                         postResult = null;
                     }
                 }
@@ -1255,7 +1259,10 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             catch ( WebException webException )
             {
                 string message = GetResponseMessage( webException.Response.GetResponseStream() );
-                throw new Exception( webException.Message + " - " + message );
+
+                var nmiGatewayException = new NMIGatewayException( webException.Message + " - " + message, webException );
+                ExceptionLogService.LogException( nmiGatewayException );
+                throw nmiGatewayException;
             }
             catch ( Exception ex )
             {
@@ -1341,7 +1348,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                 }
 
                 // write result error as an exception
-                var exception = new Exception( $"Error processing NMI transaction. Result Code:  {chargeResponse.ResponseCode} ({resultCodeMessage}). Result text: {chargeResponse.ResponseText} " );
+                var exception = new NMIGatewayException( $"Error processing NMI transaction. Result Code:  {chargeResponse.ResponseCode} ({resultCodeMessage}). Result text: {chargeResponse.ResponseText} " );
                 ExceptionLogService.LogException( exception );
 
                 return null;
@@ -1493,7 +1500,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                     }
 
                     // write result error as an exception
-                    var exception = new Exception( $"Error processing NMI subscription. Result Code:  {addSubscriptionResponse.ResponseCode} ({resultCodeMessage}). Result text: {addSubscriptionResponse.ResponseText} " );
+                    var exception = new NMIGatewayException( $"Error processing NMI subscription. Result Code:  {addSubscriptionResponse.ResponseCode} ({resultCodeMessage}). Result text: {addSubscriptionResponse.ResponseText} " );
                     ExceptionLogService.LogException( exception );
 
                     return null;
@@ -1512,7 +1519,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                 }
                 catch ( Exception ex )
                 {
-                    throw new Exception( $"Exception getting Customer Information for Scheduled Payment.", ex );
+                    throw new NMIGatewayException( $"Exception getting Customer Information for Scheduled Payment.", ex );
                 }
 
                 var scheduledTransaction = new FinancialScheduledTransaction();
@@ -1530,7 +1537,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                 }
                 catch ( Exception ex )
                 {
-                    throw new Exception( $"Exception getting Scheduled Payment Status. {errorMessage}", ex );
+                    throw new NMIGatewayException( $"Exception getting Scheduled Payment Status. {errorMessage}", ex );
                 }
 
                 return scheduledTransaction;
@@ -1648,7 +1655,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                 }
                 catch ( Exception ex )
                 {
-                    throw new Exception( $"Exception getting Scheduled Payment Status. {errorMessage}", ex );
+                    throw new NMIGatewayException( $"Exception getting Scheduled Payment Status. {errorMessage}", ex );
                 }
 
                 return true;
@@ -1723,7 +1730,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             }
             catch ( Exception ex )
             {
-                ExceptionLogService.LogException( ex );
+                ExceptionLogService.LogException( new NMIGatewayException( "Unexpected QuerySubscriptions Response from NMI Gateway", ex ) );
                 querySubscriptionsResponse = null;
             }
 
@@ -1759,7 +1766,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             }
             catch ( Exception ex )
             {
-                ExceptionLogService.LogException( ex );
+                ExceptionLogService.LogException( new NMIGatewayException( "Unexpected CustomerVault response from NMI Gateway", ex ) );
                 customerVaultQueryResponse = null;
             }
 
@@ -1781,21 +1788,6 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             /// </summary>
             public ReferencePaymentInfoRequired()
                 : base( "NMI gateway requires a token or customer reference" )
-            {
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <seealso cref="System.ArgumentNullException" />
-        public class NullFinancialGatewayException : ArgumentNullException
-        {
-            /// <summary>
-            /// Initializes a new instance of the <see cref="NullFinancialGatewayException"/> class.
-            /// </summary>
-            public NullFinancialGatewayException()
-                : base( "Unable to determine financial gateway" )
             {
             }
         }
@@ -1901,9 +1893,9 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
             {
                 tokenResponse = nmiHostedPaymentControl.PaymentInfoTokenRaw.FromJsonOrThrow<TokenizerResponse>();
             }
-            catch (Exception ex)
+            catch ( Exception ex )
             {
-                ExceptionLogService.LogException( ex );
+                ExceptionLogService.LogException( new NMIGatewayException( "Unexpected Token Response from NMI Gateway", ex ) );
                 tokenResponse = null;
             }
 
@@ -1996,7 +1988,7 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
                 }
 
                 // write result error as an exception
-                var exception = new Exception( $"Error creating NMI customer. Result Code:  {createCustomerResponse.ResponseCode} ({resultCodeMessage}). Result text: {createCustomerResponse.ResponseText} " );
+                var exception = new NMIGatewayException( $"Error creating NMI customer. Result Code:  {createCustomerResponse.ResponseCode} ({resultCodeMessage}). Result text: {createCustomerResponse.ResponseText} " );
                 ExceptionLogService.LogException( exception );
 
                 return null;
@@ -2041,5 +2033,21 @@ Transaction id: {threeStepChangeStep3Response.TransactionId}.
         }
 
         #endregion IHostedGatewayComponent
+
+        #region NMI Specific
+
+        /// <summary>
+        /// Gets the three step javascript which will validate the inputs and submit payment details to NMI gateway
+        /// </summary>
+        /// <param name="validationGroup">The validation group.</param>
+        /// <param name="postbackControlReference">The postback control reference.</param>
+        /// <returns></returns>
+        public static string GetThreeStepJavascript( string validationGroup, string postbackControlReference )
+        {
+            var script = Scripts.threeStepScript.Replace( "{{validationGroup}}", validationGroup ).Replace( "{{postbackControlReference}}", postbackControlReference );
+            return script;
+        }
+
+        #endregion
     }
 }

Rock.NMI/NMITypes.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Rock.Utility;
@@ -2202,7 +2202,7 @@ namespace Rock.NMI
         public string SubscriptionId { get; set; }
     }
 
-    internal class ThreeStepBilling: BaseResponse
+    internal class ThreeStepBilling : BaseResponse
     {
         [JsonProperty( "billing-id" )]
         public string BillingId { get; set; }
@@ -2331,10 +2331,16 @@ namespace Rock.NMI
 
             if ( friendlyMessage.IsNullOrWhiteSpace() )
             {
-                // This can happen if using the same card number and amount within a short window ( maybe around 20 minutes? )
                 if ( apiMessage.StartsWith( "Duplicate transaction REFID:" ) )
                 {
-                    friendlyMessage = "Duplicate transaction detected";
+                    // This can happen if using the same card number and amount within a short window ( maybe around 20 minutes? )
+                    return "Duplicate transaction detected";
+                }
+
+                var friendlyMessagePartial = FriendlyMessageMap.Where( k => apiMessage.StartsWith( k.Key ) ).Select( a => a.Value ).FirstOrDefault();
+                if ( friendlyMessagePartial != null )
+                {
+                    return friendlyMessagePartial;
                 }
             }
 
@@ -2519,8 +2525,11 @@ namespace Rock.NMI
             { "ccnumber is empty", "Invalid Credit Card Number" },
             { "Expiration date must be a present or future month and year", "Invalid Expiration Date" },
             { "ccexp is empty", "Invalid Expiration Date" },
-            { "CVV must be 3 or 4 digits", "Invalid CVV" },
             { "cvv is empty", "Invalid CVV" },
+
+            // Partial matches on CreditCard error
+            { "Required Field cc_number is Missing or Empty", "Invalid Credit Card Number" },
+            { "CVV must be 3 or 4 digits", "Invalid CVV" },
 
             // ACH Related
             { "Routing number must be 6 or 9 digits and a recognizable format", "Invalid Routing Number" },
@@ -2532,9 +2541,65 @@ namespace Rock.NMI
             // This seems to happen if entering an invalid ACH Account number ??
             { "Connection to tokenization service failed", "Invalid Account Number" },
 
+            // Partial Matches on ACH errors
+            { "Check Account number must contain only digits", "Invalid Check Account Number" },
+            { "ABA number must contain only digits", "Invalid Routing Number" },
+
             // Declined
             { "FAILED", "Declined" },
             { "DECLINE", "Declined" }
         };
     }
+
+    #region NMI.Gateway Exceptions
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class NMIGatewayException : Exception
+    {
+        string _stackTrace;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NMIGatewayException"/> class.
+        /// </summary>
+        /// <param name="errorMessage">The error message.</param>
+        public NMIGatewayException( string errorMessage ) : this( errorMessage, null )
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NMIGatewayException"/> class.
+        /// </summary>
+        /// <param name="errorMessage">The error message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public NMIGatewayException( string errorMessage, Exception innerException ) : base( errorMessage, innerException )
+        {
+            // lets set the stacktrace manually to ensure we have one. Otherwise, it would usually be blank.
+            _stackTrace = new System.Diagnostics.StackTrace( true ).ToString();
+        }
+
+        /// <summary>
+        /// Gets a string representation of the immediate frames on the call stack.
+        /// </summary>
+        public override string StackTrace => this._stackTrace ?? base.StackTrace;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="System.ArgumentNullException" />
+    public class NullFinancialGatewayException : ArgumentNullException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NullFinancialGatewayException"/> class.
+        /// </summary>
+        public NullFinancialGatewayException()
+            : base( "Unable to determine financial gateway" )
+        {
+        }
+    }
+
+    #endregion NMI.Gateway Exceptions
 }

Rock.NMI/Rock.NMI.csproj
@@ -95,6 +95,7 @@
   <ItemGroup>
     <Content Include="gatewayCollectJS.js" />
     <Content Include="gatewayCollect.css" />
+    <Content Include="threeStepScript.js" />
   </ItemGroup>
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 

Rock.NMI/Scripts.Designer.cs
@@ -61,23 +61,45 @@ namespace Rock.NMI {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to function initializeCollectJS(controlId) {
-        ///    var $control = $(&apos;#&apos; + controlId);
+        ///   Looks up a localized string similar to (function ($) {
+        ///    &apos;use strict&apos;;
+        ///    window.Rock = window.Rock || {};
+        ///    Rock.NMI = Rock.NMI || {}
+        ///    Rock.NMI.controls = Rock.NMI.controls || {};
         ///
-        ///    if ($control.length == 0) {
-        ///        // control hasn&apos;t been rendered so skip
-        ///        return;
-        ///    }
-        ///    var postbackScript = $control.attr(&apos;data-postback-script&apos;);
+        ///    /** JS helper for the gatewayCollectJS */
+        ///    Rock.NMI.controls.gatewayCollectJS = (function () {
+        ///        var exports = {
+        ///            initialize: function (controlId) {
+        ///                var self = this;
+        ///                var $control = $(&apos;#&apos; + controlId);
         ///
-        ///    var enabledPaymentTypes = JSON.parse($(&apos;.js-enabled-payment-types&apos;, $control).val());;
-        ///
-        ///    var $creditCardContainer = $(&apos;.js-gateway-creditcard-iframe-container&apos;, $control);
-        ///    var $achContainer = $(&apos;.js-gateway-ach-iframe-container&apos;, $control); [rest of string was truncated]&quot;;.
+        ///                if ($control.length == 0) {
+        ///                    // control hasn&apos;t been re [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string gatewayCollectJS {
             get {
                 return ResourceManager.GetString("gatewayCollectJS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sys.Application.add_load(function () {
+        ///    /* Threestep gateway related */
+        ///    var $step2Submit = $(&apos;.js-step2-submit&apos;);
+        ///
+        ///    // {{validationGroup}} will get replaced with whatever the validationGroup is
+        ///    var validationGroup = &apos;{{validationGroup}}&apos;;
+        ///    var $step2Url = $(&apos;.js-step2-url&apos;);
+        ///    var $updateProgress = $(&apos;#updateProgress&apos;);
+        ///    var $iframeStep2 = $(&apos;.js-step2-iframe&apos;);
+        ///    var $addressControl = $(&apos;.js-billingaddress-control&apos;);
+        ///
+        ///    // {{postbackControlReference}} will get replaced  [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string threeStepScript {
+            get {
+                return ResourceManager.GetString("threeStepScript", resourceCulture);
             }
         }
     }

Rock.NMI/Scripts.resx
@@ -121,4 +121,7 @@
   <data name="gatewayCollectJS" type="System.Resources.ResXFileRef, System.Windows.Forms">
     <value>gatewaycollectjs.js;System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089;utf-8</value>
   </data>
+  <data name="threeStepScript" type="System.Resources.ResXFileRef, System.Windows.Forms">
+    <value>threestepscript.js;System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089;utf-8</value>
+  </data>
 </root>

commit 91b309b9670974f68683b9e0bf229b339df04a47

- Fixed issue where NMI Gateway would not work in Safari or IE (introduced with 542e175d2861da09f805317032ddf809330e5009)

Rock.NMI/threeStepScript.js
@@ -29,7 +29,10 @@
 
         // If there is a payment type selection, use the selected one.
         // If there isn't a payment type selection (like on the registration blocks) it is CreditCard
-        var paymentType = $('.js-payment-tab').val() ?? 'CreditCard';
+        var paymentType = $('.js-payment-tab').val();
+        if (!paymentType) {
+            paymentType = 'CreditCard';
+        }
 
         if (typeof (Page_ClientValidate) == 'function') {
             if (Page_IsValid && Page_ClientValidate(validationGroup)) {