3bacdaa5a8729748b09d2723c9c7f93ddb61ac43
Taylor Cavaletto <tjcavaletto@gmail.com>
2020-04-17 10:50:00 -0700

    - Added Copyright

--- RockWeb/Plugins/com_bemaservices/CustomBlocks/FLCC/Fundraising/FundraisingLeaderToolbox.ascx.cs
+++ RockWeb/Plugins/com_bemaservices/CustomBlocks/FLCC/Fundraising/FundraisingLeaderToolbox.ascx.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by BEMA Information Technologies
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -39,7 +55,7 @@ namespace RockWeb.Plugins.com_bemaservices.Fundraising
 </p>
 ", order: 1 )]
 
-    [LinkedPage( "Participant Page", "The partipant page for a participant of this fundraising opportunity", required: false, order: 2 )]
+    [LinkedPage( "Participant Page", "The participant page for a participant of this fundraising opportunity", required: false, order: 2 )]
     [LinkedPage( "Main Page", "The main page for the fundraising opportunity", required: false, order: 3 )]
     public partial class FundraisingLeaderToolbox : RockBlock
     {
