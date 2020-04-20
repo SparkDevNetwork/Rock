e0c09a0035d09065c341bd0d33a27b1659ee63c2
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-04-20 15:38:35 -0400

    - (Core) Provided a new "Warning" status for jobs that are able to complete, but encountered errors while processing.

---
+++ Rock/Jobs/RockJobWarningException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Jobs
+{
+    /// <summary>
+    /// This is exception type is used by rock jobs to show warnings instead of errors.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class RockJobWarningException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RockJobWarningException"/> class.
+        /// </summary>
+        public RockJobWarningException() : base() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RockJobWarningException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public RockJobWarningException(string message) : base(message) { }
+    }
+}
