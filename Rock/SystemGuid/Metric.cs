3384acfa9c0b1181cb03a661c103de65c09f0229
Jason Hendee <jason.hendee@sparkdevnetwork.org>
2020-04-23 17:57:39 -0700

    - (Reporting) Added 'Hosting Metrics' for reporting the usage of resources such as the database connection pool.

---
+++ Rock/SystemGuid/Metric.cs
@@ -0,0 +1,32 @@
+﻿namespace Rock.SystemGuid
+{
+    /// <summary>
+    /// System Metrics
+    /// </summary>
+    public class Metric
+    {
+        #region Hosting Metrics
+
+        /// <summary>
+        /// Hosting Metric hard connects per second Guid
+        /// </summary>
+        public const string HOSTING_HARD_CONNECTS_PER_SECOND = "64D538D0-EE05-4646-91F5-EBE06460BDAB";
+
+        /// <summary>
+        /// Hosting Metric number of active connections Guid
+        /// </summary>
+        public const string HOSTING_NUMBER_OF_ACTIVE_CONNECTIONS = "68C54F46-A99E-4DD1-91CA-FC5941E6CFBE";
+
+        /// <summary>
+        /// Hosting Metric number of free connections Guid
+        /// </summary>
+        public const string HOSTING_NUMBER_OF_FREE_CONNECTIONS = "8A1F73DD-4275-47C0-AF2A-6EABDA06E3C7";
+
+        /// <summary>
+        /// Hosting Metric soft connects per second Guid
+        /// </summary>
+        public const string HOSTING_SOFT_CONNECTS_PER_SECOND = "F90F9446-8754-4001-887C-1AB920968C6D";
+
+        #endregion
+    }
+}
