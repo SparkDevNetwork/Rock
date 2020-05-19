4e9a6a87d68d3f8942bb9963cbc6e2e23dd70ef2
Mike Peterson <mike@sparkabilitygroup.com>
2020-05-19 15:30:57 -0700

    + (CMS) Fixed bug that was causing the Child Content Channel Items to be ordered incorrectly.

--- RockWeb/Blocks/Cms/ContentChannelItemDetail.ascx.cs
+++ RockWeb/Blocks/Cms/ContentChannelItemDetail.ascx.cs
@@ -496,6 +496,11 @@ namespace RockWeb.Blocks.Cms
             ShowDialog( "AddChildItem", true );
         }
 
+        /// <summary>
+        /// Handles the GridReorder event of the gChildItems control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="GridReorderEventArgs"/> instance containing the event data.</param>
         private void gChildItems_GridReorder( object sender, GridReorderEventArgs e )
         {
             using ( var rockContext = new RockContext() )
@@ -504,13 +509,13 @@ namespace RockWeb.Blocks.Cms
                 if ( contentItem != null )
                 {
                     bool isFiltered = false;
-                    var items = GetChildItems( contentItem, out isFiltered ).OrderBy( a => a.Order ).ToList();
+                    var childItemAssociationList = GetChildItemAssociationList( contentItem, out isFiltered ).OrderBy( a => a.Order ).ToList();
 
                     // If the list was filtered due to VIEW security, don't sort it
                     if ( !isFiltered )
                     {
-                        var service = new ContentChannelItemService( rockContext );
-                        service.Reorder( items, e.OldIndex, e.NewIndex );
+                        var service = new ContentChannelItemAssociationService( rockContext );
+                        service.Reorder( childItemAssociationList, e.OldIndex, e.NewIndex );
                         rockContext.SaveChanges();
                     }
                 }
@@ -561,6 +566,11 @@ namespace RockWeb.Blocks.Cms
             }
         }
 
+        /// <summary>
+        /// Handles the Click event of the lbAddExistingChildItem control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void lbAddExistingChildItem_Click( object sender, EventArgs e )
         {
             int? itemId = hfId.Value.AsIntegerOrNull();
@@ -1135,13 +1145,13 @@ namespace RockWeb.Blocks.Cms
         private void BindChildItemsGrid( ContentChannelItem contentItem )
         {
             bool isFiltered = false;
-            var items = GetChildItems( contentItem, out isFiltered );
+            var childItemAssociationList = GetChildItemAssociationList( contentItem, out isFiltered );
 
             if ( contentItem.ContentChannel.ChildItemsManuallyOrdered && !isFiltered )
             {
                 gChildItems.Columns[0].Visible = true;
                 gChildItems.AllowSorting = false;
-                items = items.OrderBy( i => i.Order ).ToList();
+                childItemAssociationList = childItemAssociationList.OrderBy( i => i.Order ).ToList();
             }
             else
             {
@@ -1151,18 +1161,19 @@ namespace RockWeb.Blocks.Cms
                 SortProperty sortProperty = gChildItems.SortProperty;
                 if ( sortProperty != null )
                 {
-                    items = items.AsQueryable().Sort( sortProperty ).ToList();
+                    childItemAssociationList = childItemAssociationList.AsQueryable().Sort( sortProperty ).ToList();
                 }
                 else
                 {
-                    items = items.OrderByDescending( p => p.StartDateTime ).ToList();
+                    childItemAssociationList = childItemAssociationList.OrderByDescending( p => p.ContentChannelItem.StartDateTime ).ToList();
                 }
             }
 
             gChildItems.ObjectList = new Dictionary<string, object>();
-            items.ForEach( i => gChildItems.ObjectList.Add( i.Id.ToString(), i ) );
+            var childItemList = childItemAssociationList.Select( a => a.ChildContentChannelItem ).ToList();
+            childItemList.ForEach( i => gChildItems.ObjectList.Add( i.Id.ToString(), i ) );
 
-            gChildItems.DataSource = items.Select( i => new
+            gChildItems.DataSource = childItemList.Select( i => new
             {
                 i.Id,
                 i.Guid,
@@ -1208,16 +1219,17 @@ namespace RockWeb.Blocks.Cms
             gParentItems.DataBind();
         }
 
-        private List<ContentChannelItem> GetChildItems( ContentChannelItem contentItem, out bool isFiltered )
+        private List<ContentChannelItemAssociation> GetChildItemAssociationList( ContentChannelItem contentItem, out bool isFiltered )
         {
             isFiltered = false;
-            var items = new List<ContentChannelItem>();
+            var contentChannelItemAssociationList = new List<ContentChannelItemAssociation>();
 
-            foreach ( var item in contentItem.ChildItems.Select( a => a.ChildContentChannelItem ).ToList() )
+            foreach ( ContentChannelItemAssociation contentChannelItemAssociation in contentItem.ChildItems.ToList() )
             {
-                if ( item.IsAuthorized( Authorization.VIEW, CurrentPerson ) )
+                // see if the ContentChannel item of the ContentChannelItemAssociation is authorized
+                if ( contentChannelItemAssociation.ContentChannelItem.IsAuthorized( Authorization.VIEW, CurrentPerson ) )
                 {
-                    items.Add( item );
+                    contentChannelItemAssociationList.Add( contentChannelItemAssociation );
                 }
                 else
                 {
@@ -1225,7 +1237,7 @@ namespace RockWeb.Blocks.Cms
                 }
             }
 
-            return items;
+            return contentChannelItemAssociationList;
         }
 
         private List<ContentChannelItem> GetParentItems( ContentChannelItem contentItem )
@@ -1308,6 +1320,10 @@ namespace RockWeb.Blocks.Cms
             hfActiveDialog.Value = string.Empty;
         }
 
+        /// <summary>
+        /// When navigating to child items of childitems of childitems, the "Hierarchy" will be a list of how to navigate backwards thru the parents
+        /// </summary>
+        /// <returns></returns>
         private List<string> GetNavHierarchy()
         {
             var qryParam = PageParameter( "Hierarchy" );
