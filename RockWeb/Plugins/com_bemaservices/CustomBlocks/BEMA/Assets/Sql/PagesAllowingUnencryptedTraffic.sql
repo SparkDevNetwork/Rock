0d46331dc9948947e062679990d91f4c8864f77c
Taylor Cavaletto <tjcavaletto@gmail.com>
2020-05-29 14:48:50 -0700

    - Added Custom Reports

---
+++ RockWeb/Plugins/com_bemaservices/CustomBlocks/BEMA/Assets/Sql/PagesAllowingUnencryptedTraffic.sql
@@ -0,0 +1,7 @@
+﻿SELECT s.Id AS SiteId, s.Name AS Site, p.Id AS PageId, p.PageTitle AS Page
+FROM Page p
+JOIN Layout l ON p.LayoutId = l.Id
+JOIN Site s ON l.SiteId = s.Id
+WHERE p.RequiresEncryption = 0
+AND s.RequiresEncryption = 0
+ORDER BY s.Name, p.PageTitle
