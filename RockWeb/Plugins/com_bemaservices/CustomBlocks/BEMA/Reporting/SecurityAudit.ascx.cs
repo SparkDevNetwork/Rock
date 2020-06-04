1508e4268aca559c8eefcc5422c38b0d2fadd73d
Taylor Cavaletto <tjcavaletto@gmail.com>
2020-06-04 12:00:42 -0700

    - Updated Security Audit block

--- RockWeb/Plugins/com_bemaservices/CustomBlocks/BEMA/Reporting/SecurityAudit.ascx.cs
+++ RockWeb/Plugins/com_bemaservices/CustomBlocks/BEMA/Reporting/SecurityAudit.ascx.cs
@@ -33,14 +33,15 @@ using System.Web.UI.HtmlControls;
 using System.Data.Entity;
 using System.Data;
 using System.Text.RegularExpressions;
+using Rock.Security;
 
-namespace RockWeb.Plugins.com_bemaservices.Support
+namespace RockWeb.Plugins.com_bemaservices.CustomBlocks.Bema.Reporting
 {
     /// <summary>
     /// Template block for developers to use to start a new block.
     /// </summary>
     [DisplayName( "Security Audit" )]
-    [Category( "BEMA Services > Support" )]
+    [Category( "BEMA Services > Reporting" )]
     [Description( "Report block to give an update on a site's security" )]
 
     // Rock Admin Count Settings
@@ -64,6 +65,12 @@ namespace RockWeb.Plugins.com_bemaservices.Support
     // Person Auth Settings
     [IntegerField( "Recommended Maximum Number of Person Authorizations", "The recommended maximum number of person-specific auth rules to use for the audit.", true, 4, "Person Auth Settings", 8, AttributeKey.RecommendedMaximumPersonAuthCount )]
 
+    // Finance Data Views Settings
+    [IntegerField( "Recommended Maximum Number of Unsecured Finance Data Views", "The recommended maximum number of unsecured finance data views to use for the audit.", true, 4, "Finance Data View Settings", 9, AttributeKey.RecommendedMaximumUnsecuredFinanceDataViewCount )]
+
+    // Sensitive File Type Settings
+    [BinaryFileTypesField( "Sensitive File Types", "The file types that should not be accessible to all users.", true, "", "Sensitive File Type Settings", 10, AttributeKey.SensitiveFileTypes )]
+
     public partial class SecurityAudit : RockBlock, ICustomGridColumns
     {
         #region Fields
@@ -83,6 +90,8 @@ namespace RockWeb.Plugins.com_bemaservices.Support
             public const string RecommendedSqlLavaCommandBlocks = "RecommendedSqlLavaCommandBlocks";
             public const string SqlLavaCommandSites = "SqlLavaCommandSites";
             public const string RecommendedMaximumPersonAuthCount = "RecommendedMaximumPersonAuthCount";
+            public const string RecommendedMaximumUnsecuredFinanceDataViewCount = "RecommendedMaximumUnsecuredFinanceDataViewCount";
+            public const string SensitiveFileTypes = "SensitiveFileTypes";
         }
 
         #endregion
@@ -148,6 +157,9 @@ namespace RockWeb.Plugins.com_bemaservices.Support
             gSqlLavaCommand.GridRebind += gSqlLavaCommand_GridRebind;
             gPersonAuth.GridRebind += gPersonAuth_GridRebind;
             gUnencryptedSensitiveData.GridRebind += gUnencryptedSensitiveData_GridRebind;
+            gFinanceDataViews.GridRebind += gFinanceDataViews_GridRebind;
+            gFileTypeSecurity.GridRebind += gFileTypeSecurity_GridRebind;
+            gGlobalLavaCommands.GridRebind += gGlobalLavaCommands_GridRebind;
 
             // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
             this.BlockUpdated += Block_BlockUpdated;
@@ -214,6 +226,15 @@ namespace RockWeb.Plugins.com_bemaservices.Support
             BuildUnencryptedSensitiveDataHeader( rockContext );
             BindUnencryptedSensitiveDataGrid( rockContext );
 
+            BuildFinanceDataViewsHeader( rockContext );
+            BindFinanceDataViewsGrid( rockContext );
+
+            BuildFileTypeSecurityHeader( rockContext );
+            BindFileTypeSecurityGrid( rockContext );
+
+            BuildGlobalLavaCommandsHeader( rockContext );
+            BindGlobalLavaCommandsGrid( rockContext );
+
             PercentComplete = ( PassedChecks.ToString().AsDecimal() / TotalChecks.ToString().AsDecimal() ) * 100.0m;
 
             lChecksPassed.Text = String.Format( "{0}/{1} Checks Passed", PassedChecks.ToString(), TotalChecks.ToString() );
@@ -245,7 +266,7 @@ namespace RockWeb.Plugins.com_bemaservices.Support
                 headerCss = "header-danger";
             }
 
-            targetHeader.InnerHtml = String.Format( "<h5 class='mb-0'>{0}</h5><span class=\"pull-right\"><i class=\"fa fa-chevron-down\"></i></span>", headerText );
+            targetHeader.InnerHtml = String.Format( "<h5 class='mb-0'>{0}<span class=\"pull-right\"><i class=\"fa fa-chevron-down\"></i></span></h5>", headerText );
             targetLiteral.Text = descriptionText;
             targetHeader.AddCssClass( headerCss );
         }
@@ -586,5 +607,178 @@ namespace RockWeb.Plugins.com_bemaservices.Support
         }
 
         #endregion
+
+
+        #region Finance Data View Methods
+
+        private void BuildFinanceDataViewsHeader( RockContext rockContext )
+        {
+            List<Rock.Model.DataView> unsecuredDataViews = new List<Rock.Model.DataView>();
+            List<int> authorizedGroupIds = new List<int>();
+
+            var groupService = new GroupService( rockContext );
+            authorizedGroupIds.Add( groupService.Get( Rock.SystemGuid.Group.GROUP_ADMINISTRATORS.AsGuid() ).Id );
+            authorizedGroupIds.Add( groupService.Get( Rock.SystemGuid.Group.GROUP_FINANCE_ADMINISTRATORS.AsGuid() ).Id );
+            authorizedGroupIds.Add( groupService.Get( Rock.SystemGuid.Group.GROUP_FINANCE_USERS.AsGuid() ).Id );
+
+            var dataViews = new DataViewService( rockContext ).Queryable().AsNoTracking().Where( dv => dv.EntityType.Name.Contains( "Financ" ) ).ToList();
+            foreach ( var dataView in dataViews )
+            {
+                var authRules = Authorization.AuthRules( dataView.TypeId, dataView.Id, Authorization.VIEW );
+                if ( !authRules.Where( a => a.AllowOrDeny == 'D' && a.SpecialRole == SpecialRole.AllUsers ).Any() )
+                {
+                    unsecuredDataViews.Add( dataView );
+                }
+
+                if ( authRules.Where( a => a.AllowOrDeny == 'D' && ( !a.GroupId.HasValue || !authorizedGroupIds.Contains( a.GroupId.Value ) ) ).Any() )
+                {
+                    unsecuredDataViews.Add( dataView );
+                }
+            }
+
+            var auditValue = unsecuredDataViews.Count;
+            var auditGoal = GetAttributeValue( AttributeKey.RecommendedMaximumUnsecuredFinanceDataViewCount ).AsInteger();
+
+            var headerText = String.Format( "Unsecured Finance Data Views: {0}", auditValue );
+            var descriptionText = String.Format( "A key part of improving your site's security is to limit access to financial information. BEMA recommends allowing no more than {0} unsecured financial data views.", auditGoal );
+            GenerateHeader( headerText, descriptionText, auditValue, auditGoal, true, divHeaderFinanceDataViews, lDescriptionFinanceDataViews );
+        }
+
+        private void gFinanceDataViews_GridRebind( object sender, EventArgs e )
+        {
+            BindFinanceDataViewsGrid();
+        }
+
+        private void BindFinanceDataViewsGrid( RockContext rockContext = null )
+        {
+            if ( rockContext == null )
+            {
+                rockContext = new RockContext();
+            }
+
+
+            List<Rock.Model.DataView> unsecuredDataViews = new List<Rock.Model.DataView>();
+            List<int> authorizedGroupIds = new List<int>();
+
+            var groupService = new GroupService( rockContext );
+            authorizedGroupIds.Add( groupService.Get( Rock.SystemGuid.Group.GROUP_ADMINISTRATORS.AsGuid() ).Id );
+            authorizedGroupIds.Add( groupService.Get( Rock.SystemGuid.Group.GROUP_FINANCE_ADMINISTRATORS.AsGuid() ).Id );
+            authorizedGroupIds.Add( groupService.Get( Rock.SystemGuid.Group.GROUP_FINANCE_USERS.AsGuid() ).Id );
+
+            var dataViews = new DataViewService( rockContext ).Queryable().AsNoTracking().Where( dv => dv.EntityType.Name.Contains( "Financ" ) ).ToList();
+            foreach ( var dataView in dataViews )
+            {
+                var authRules = Authorization.AuthRules( dataView.TypeId, dataView.Id, Authorization.VIEW );
+                if ( !authRules.Where( a => a.AllowOrDeny == 'D' && a.SpecialRole == SpecialRole.AllUsers ).Any() )
+                {
+                    unsecuredDataViews.Add( dataView );
+                }
+
+                if ( authRules.Where( a => a.AllowOrDeny == 'D' && ( !a.GroupId.HasValue || !authorizedGroupIds.Contains( a.GroupId.Value ) ) ).Any() )
+                {
+                    unsecuredDataViews.Add( dataView );
+                }
+            }
+            gFinanceDataViews.DataSource = unsecuredDataViews;
+            gFinanceDataViews.DataBind();
+        }
+
+        #endregion
+
+
+        #region FileTypeSecurity Methods
+
+        private void BuildFileTypeSecurityHeader( RockContext rockContext )
+        {
+            List<BinaryFileType> atRiskFileTypes = new List<BinaryFileType>();
+            var selectedFileTypes = new BinaryFileTypeService( rockContext ).GetByGuids( GetAttributeValue( AttributeKey.SensitiveFileTypes ).SplitDelimitedValues().AsGuidList() );
+            foreach ( var fileType in selectedFileTypes )
+            {
+                var authRules = Authorization.AuthRules( fileType.TypeId, fileType.Id, Authorization.VIEW );
+                if ( !authRules.Where( a => a.AllowOrDeny == 'D' && a.SpecialRole == SpecialRole.AllUsers ).Any() )
+                {
+                    atRiskFileTypes.Add( fileType );
+                }
+            }
+
+            var auditValue = atRiskFileTypes.Count;
+            var auditGoal = 0;
+
+            var headerText = String.Format( "Unsecured Sensitive File Types: {0}", auditValue );
+            var descriptionText = String.Format( "A key part of improving your site's security is to secure file types dealing with sensitive data. BEMA recommends locking down all sensitive file types.", auditGoal );
+            GenerateHeader( headerText, descriptionText, auditValue, auditGoal, false, divHeaderFileTypeSecurity, lDescriptionFileTypeSecurity );
+        }
+
+        private void gFileTypeSecurity_GridRebind( object sender, EventArgs e )
+        {
+            BindFileTypeSecurityGrid();
+        }
+
+        private void BindFileTypeSecurityGrid( RockContext rockContext = null )
+        {
+            if ( rockContext == null )
+            {
+                rockContext = new RockContext();
+            }
+
+
+            List<BinaryFileType> atRiskFileTypes = new List<BinaryFileType>();
+            var selectedFileTypes = new BinaryFileTypeService( rockContext ).GetByGuids( GetAttributeValue( AttributeKey.SensitiveFileTypes ).SplitDelimitedValues().AsGuidList() );
+            foreach ( var fileType in selectedFileTypes )
+            {
+                var authRules = Authorization.AuthRules( fileType.TypeId, fileType.Id, Authorization.VIEW );
+                if ( !authRules.Where( a => a.AllowOrDeny == 'D' && a.SpecialRole == SpecialRole.AllUsers ).Any() )
+                {
+                    atRiskFileTypes.Add( fileType );
+                }
+            }
+            gFileTypeSecurity.DataSource = atRiskFileTypes;
+            gFileTypeSecurity.DataBind();
+        }
+
+        #endregion
+
+
+        #region GlobalLavaCommands Methods
+
+        private void BuildGlobalLavaCommandsHeader( RockContext rockContext )
+        {
+            var defaultLavaCommands = GlobalAttributesCache.Get()
+                        .GetValue( "DefaultEnabledLavaCommands" )
+                        .SplitDelimitedValues()
+                        .Where( lc => lc == "RockEntity" || lc == "Sql" || lc == "Execute" || lc == "All" )
+                        .ToList();
+
+            var auditValue = defaultLavaCommands.Count;
+            var auditGoal = 0;
+
+
+            var headerText = String.Format( "Vulnerable Default Lava Commands: {0}", auditValue );
+            var descriptionText = String.Format( "A key part of improving your site's security is limit where lava commands can be used. BEMA recommends disabling the Execute, RockEntity, and Sql commands from the Global Default Enabled Lava Commands.", auditGoal );
+            GenerateHeader( headerText, descriptionText, auditValue, auditGoal, false, divHeaderGlobalLavaCommands, lDescriptionGlobalLavaCommands );
+        }
+
+        private void gGlobalLavaCommands_GridRebind( object sender, EventArgs e )
+        {
+            BindGlobalLavaCommandsGrid();
+        }
+
+        private void BindGlobalLavaCommandsGrid( RockContext rockContext = null )
+        {
+            if ( rockContext == null )
+            {
+                rockContext = new RockContext();
+            }
+
+            var defaultLavaCommands = GlobalAttributesCache.Get()
+                        .GetValue( "DefaultEnabledLavaCommands" )
+                        .SplitDelimitedValues()
+                        .Where( lc => lc == "RockEntity" || lc == "Sql" || lc == "Execute" || lc == "All" )
+                        .ToList();
+            gGlobalLavaCommands.DataSource = defaultLavaCommands;
+            gGlobalLavaCommands.DataBind();
+        }
+
+        #endregion       
     }
 }
