5f19ec5b3c7012c13cc9afdbc503f2fe119219f8
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2020-06-09 10:31:13 -0700

    - Commented out plug-in migrations.

--- Rock/Plugin/HotFixes/126_MigrationRollupsFor10_3_7.cs
+++ Rock/Plugin/HotFixes/126_MigrationRollupsFor10_3_7.cs
@@ -29,7 +29,7 @@ namespace Rock.Plugin.HotFixes
         /// </summary>
         public override void Up()
         {
-            UpdateStructuredContentEditorDefinedValues();
+            //UpdateStructuredContentEditorDefinedValues();
         }
 
         /// <summary>
