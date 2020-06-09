e8628828064ec55032e115559226da04e525f019
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-06-09 10:52:40 -0400

    + (Core) Added new options to better control REST caching for GET results/requests.

--- Rock.Rest/App_Start/WebApiConfig.cs
+++ Rock.Rest/App_Start/WebApiConfig.cs
@@ -44,6 +44,7 @@ namespace Rock.Rest
         {
             config.EnableCors( new Rock.Rest.EnableCorsFromOriginAttribute() );
             config.Filters.Add( new Rock.Rest.Filters.ValidateAttribute() );
+            config.Filters.Add( new Rock.Rest.Filters.RockCacheabilityAttribute() );
             config.Services.Replace( typeof( IExceptionLogger ), new RockApiExceptionLogger() );
             config.Services.Replace( typeof( IExceptionHandler ), new RockApiExceptionHandler() );
             config.Formatters.Insert( 0, new Rock.Utility.RockJsonMediaTypeFormatter() );
