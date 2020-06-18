932e796d5e6bde3f98ffed123566987c0b405536
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2020-06-18 15:50:59 -0400

    Added Field Type and Category Guids.  Fixed Bugs in CreateDb.

--- com.bemaservices.HrManagement/Migrations/001_CreateDb.cs
+++ com.bemaservices.HrManagement/Migrations/001_CreateDb.cs
@@ -24,6 +24,8 @@ namespace com.bemaservices.HrManagement.Migrations
 
             // Update Field Types
             RockMigrationHelper.UpdateFieldType( "Pto Tier", "", "com.bemaservices.HrManagement", "com.bemaservices.HrManagement.Field.Types.PtoTierFieldType", SystemGuid.FieldType.PTO_TIER );
+            RockMigrationHelper.UpdateFieldType( "Pto Type", "", "com.bemaservices.HrManagement", "com.bemaservices.HrManagement.Field.Types.PtoTypeFieldType", SystemGuid.FieldType.PTO_TYPE );
+            RockMigrationHelper.UpdateFieldType( "Pto Request", "", "com.bemaservices.HrManagement", "com.bemaservices.HrManagement.Field.Types.PtoRequestFieldType", SystemGuid.FieldType.PTO_REQUEST );
 
             // Add Global Attributes
             RockMigrationHelper.AddGlobalAttribute( Rock.SystemGuid.FieldType.DATE, "", "", "Fiscal Year Start Date", "Used to determine when the Fiscal Year Starts and Ends.", 0, "2020-1-1", SystemGuid.Attribute.FISCAL_YEAR_START_DATE_ATTRIBUTE );
@@ -32,8 +34,14 @@ namespace com.bemaservices.HrManagement.Migrations
             RockMigrationHelper.AddGlobalAttribute( Rock.SystemGuid.FieldType.INTEGER, "", "", "Time Increments", "The Increment for Time Off in hours.", 0, "4", SystemGuid.Attribute.TIME_INCREMENTS_ATTRIBUTE );
 
             // Add Person Attributes
-            RockMigrationHelper.AddEntityAttribute( "Person", Rock.SystemGuid.FieldType.PERSON, "", "", "Supervisor", "Hr", "The Person that this person reports to for the purpose of Time Off Requests", 0, "", SystemGuid.Attribute.SUPERVISOR_PERSON_ATTRIBUTE );
-            RockMigrationHelper.AddEntityAttribute( "Person", SystemGuid.FieldType.PTO_TIER , "", "", "PTO Tier", "Hr", "The PTO Tier that this person's PTO is accured based upon.", 1, "", SystemGuid.Attribute.PTO_TIER_PERSON_ATTRIBUTE );
+            RockMigrationHelper.UpdatePersonAttributeCategory( "Hr", "fa fa-compass", "", SystemGuid.Category.HR_ATTRIBUTE_CATEGORY );
+
+            var categoryGuids = new List<string>();
+
+            categoryGuids.Add( SystemGuid.Category.HR_ATTRIBUTE_CATEGORY );
+
+            RockMigrationHelper.AddOrUpdatePersonAttributeByGuid( Rock.SystemGuid.FieldType.PERSON, categoryGuids, "Supervisor", "Supervisor", "Supervisor", "", "", 1 , "", SystemGuid.Attribute.SUPERVISOR_PERSON_ATTRIBUTE );
+            RockMigrationHelper.AddOrUpdatePersonAttributeByGuid( SystemGuid.FieldType.PTO_TIER, categoryGuids, "PTO Tier", "PTO Tier", "PTOTier", "", "", 2, "", SystemGuid.Attribute.PTO_TIER_PERSON_ATTRIBUTE );
 
             // Create Tables
             Sql( @"
@@ -52,12 +60,50 @@ namespace com.bemaservices.HrManagement.Migrations
                     [ForeignGuid] [uniqueidentifier] NULL,
                     [ForeignId] [int] NULL,
                 ) ON [PRIMARY]
+
+                ALTER TABLE [_com_bemaservices_HrManagement_PtoTier] ADD CONSTRAINT PK__com_bemaservices_HrManagement_PtoTier_Id PRIMARY KEY CLUSTERED ( Id );
+
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoTier]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_CreatedByPersonAliasId] FOREIGN KEY([CreatedByPersonAliasId])
                 REFERENCES [dbo].[PersonAlias] ([Id])
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoTier] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_CreatedByPersonAliasId]
+
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoTier]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_ModifiedByPersonAliasId] FOREIGN KEY([ModifiedByPersonAliasId])
                 REFERENCES [dbo].[PersonAlias] ([Id])
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoTier] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_ModifiedByPersonAliasId]
+            " );
+
+            Sql( @"
+                CREATE TABLE [dbo].[_com_bemaservices_HrManagement_PtoType](
+	                [Id] [int] IDENTITY(1,1) NOT NULL,
+                    [Name] [nvarchar](100) NULL,
+	                [Description] [nvarchar] NULL,
+                    [Color] [nvarchar](100) NULL,
+	                [IsActive] [bit] NOT NULL,
+                    [IsNegativeTimeBalanceAllowed] [bit] NOT NULL,
+                    [WorkflowTypeId] int NULL,
+	                [Guid] [uniqueidentifier] NOT NULL,
+	                [CreatedDateTime] [datetime] NULL,
+	                [ModifiedDateTime] [datetime] NULL,
+	                [CreatedByPersonAliasId] [int] NULL,
+	                [ModifiedByPersonAliasId] [int] NULL,
+	                [ForeignKey] [nvarchar](50) NULL,
+                    [ForeignGuid] [uniqueidentifier] NULL,
+                    [ForeignId] [int] NULL,
+                ) ON [PRIMARY]
+
+                ALTER TABLE [_com_bemaservices_HrManagement_PtoType] ADD CONSTRAINT PK__com_bemaservices_HrManagement_PtoType_Id PRIMARY KEY CLUSTERED ( Id );
+
+                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoType_CreatedByPersonAliasId] FOREIGN KEY([CreatedByPersonAliasId])
+                REFERENCES [dbo].[PersonAlias] ([Id])
+                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoType_CreatedByPersonAliasId]
+
+                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoType_ModifiedByPersonAliasId] FOREIGN KEY([ModifiedByPersonAliasId])
+                REFERENCES [dbo].[PersonAlias] ([Id])
+                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoType_ModifiedByPersonAliasId]
+
+                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoType_WorkflowTypeId] FOREIGN KEY([WorkflowTypeId])
+                REFERENCES [dbo].[WorkflowType] ([Id])
+                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoType_WorkflowTypeId]
             " );
 
             Sql( @"
@@ -76,6 +122,8 @@ namespace com.bemaservices.HrManagement.Migrations
                     [ForeignGuid] [uniqueidentifier] NULL,
                     [ForeignId] [int] NULL,
                 ) ON [PRIMARY]
+
+                ALTER TABLE [_com_bemaservices_HrManagement_PtoBracket] ADD CONSTRAINT PK__com_bemaservices_HrManagement_PtoBracket_Id PRIMARY KEY CLUSTERED ( Id );
 
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoBracket]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoBracket_CreatedByPersonAliasId] FOREIGN KEY([CreatedByPersonAliasId])
                 REFERENCES [dbo].[PersonAlias] ([Id])
@@ -107,6 +155,8 @@ namespace com.bemaservices.HrManagement.Migrations
                     [ForeignId] [int] NULL,
                 ) ON [PRIMARY]
 
+                ALTER TABLE [_com_bemaservices_HrManagement_PtoBracketType] ADD CONSTRAINT PK__com_bemaservices_HrManagement_PtoBracketType_Id PRIMARY KEY CLUSTERED ( Id );
+
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoBracketType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoBracketType_CreatedByPersonAliasId] FOREIGN KEY([CreatedByPersonAliasId])
                 REFERENCES [dbo].[PersonAlias] ([Id])
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoBracketType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoBracketType_CreatedByPersonAliasId]
@@ -122,38 +172,6 @@ namespace com.bemaservices.HrManagement.Migrations
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoBracketType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoBracketType_PtoTypeId] FOREIGN KEY([PtoTypeId])
                 REFERENCES [dbo].[_com_bemaservices_HrManagement_PtoType] ([Id])
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoBracketType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoBracketType_PtoTypeId]
-            " );
-
-            Sql( @"
-                CREATE TABLE [dbo].[_com_bemaservices_HrManagement_PtoType](
-	                [Id] [int] IDENTITY(1,1) NOT NULL,
-                    [Name] [nvarchar](100) NULL,
-	                [Description] [nvarchar] NULL,
-                    [Color] [nvarchar](100) NULL,
-	                [IsActive] [bit] NOT NULL,
-                    [IsNegativeTimeBalanceAllowed] [bit] NOT NULL,
-                    [WorkflowTypeId] int NULL,
-	                [Guid] [uniqueidentifier] NOT NULL,
-	                [CreatedDateTime] [datetime] NULL,
-	                [ModifiedDateTime] [datetime] NULL,
-	                [CreatedByPersonAliasId] [int] NULL,
-	                [ModifiedByPersonAliasId] [int] NULL,
-	                [ForeignKey] [nvarchar](50) NULL,
-                    [ForeignGuid] [uniqueidentifier] NULL,
-                    [ForeignId] [int] NULL,
-                ) ON [PRIMARY]
-
-                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_CreatedByPersonAliasId] FOREIGN KEY([CreatedByPersonAliasId])
-                REFERENCES [dbo].[PersonAlias] ([Id])
-                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_CreatedByPersonAliasId]
-
-                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_ModifiedByPersonAliasId] FOREIGN KEY([ModifiedByPersonAliasId])
-                REFERENCES [dbo].[PersonAlias] ([Id])
-                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_ModifiedByPersonAliasId]
-
-                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_WorkflowTypeId] FOREIGN KEY([WorkflowTypeId])
-                REFERENCES [dbo].[WorkflowType] ([Id])
-                ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoType] CHECK CONSTRAINT [FK__com_bemaservices_HrManagement_PtoTier_WorkflowTypeId]
             " );
 
             Sql( @"
@@ -174,6 +192,8 @@ namespace com.bemaservices.HrManagement.Migrations
                     [ForeignGuid] [uniqueidentifier] NULL,
                     [ForeignId] [int] NULL,
                 ) ON [PRIMARY]
+
+                ALTER TABLE [_com_bemaservices_HrManagement_PtoRequest] ADD CONSTRAINT PK__com_bemaservices_HrManagement_PtoRequest_Id PRIMARY KEY CLUSTERED ( Id );
 
                 ALTER TABLE [dbo].[_com_bemaservices_HrManagement_PtoRequest]  WITH CHECK ADD  CONSTRAINT [FK__com_bemaservices_HrManagement_PtoRequest_CreatedByPersonAliasId] FOREIGN KEY([CreatedByPersonAliasId])
                 REFERENCES [dbo].[PersonAlias] ([Id])
--- com.bemaservices.HrManagement/SystemGuid/FieldType.cs
+++ com.bemaservices.HrManagement/SystemGuid/FieldType.cs
@@ -6,22 +6,16 @@ using System.Threading.Tasks;
 
 namespace com.bemaservices.HrManagement.SystemGuid
 {
-    public static class Attribute
+    public static class FieldType
     {
         /// <summary>
         /// The open connects digest system email Guid
         /// </summary>
-        public static string FISCAL_YEAR_START_DATE_ATTRIBUTE = "28EC6E39-99C2-47BC-AD07-8F62078BBA5C";
+        public static string PTO_TIER = "45D50588-09B4-488A-A794-8C27150BA344";
 
-        public static string MINIMUM_TIME_REQUIREMENT_ATTRIBUTE = "CAE7E7C6-1C58-4EF2-8512-03C6ECD13E04";
+        public static string PTO_REQUEST = "90ECF283-5344-4168-9224-E0D26E9B7ECB";
 
-        public static string MAXIMUM_TIME_REQUIREMENT_ATTRIBUTE = "A9EA49DA-6E38-4561-8160-081425447D5B";
-
-        public static string TIME_INCREMENTS_ATTRIBUTE = "8CC50218-E841-4B04-AEA7-8BFBED752456";
-
-        public static string SUPERVISOR_PERSON_ATTRIBUTE = "67AFD5A3-28F3-404F-A3B8-88630061F294";
-
-        public static string PTO_TEIR_PERSON_ATTRIBUTE = "66AE15DA-5684-4630-9CAB-F2F329F8C895";
+        public static string PTO_TYPE = "81FD3C9B-E22A-492F-9212-93546BBE6677";
 
     }
 }
