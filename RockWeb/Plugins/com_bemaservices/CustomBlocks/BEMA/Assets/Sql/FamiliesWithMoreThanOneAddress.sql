ddc30b002ce1952952fdb05b1a6cfc0fbb18d758
Taylor Cavaletto <tjcavaletto@gmail.com>
2020-06-24 16:35:34 -0700

    - Updated MoreThanOneMailingAddress

--- RockWeb/Plugins/com_bemaservices/CustomBlocks/BEMA/Assets/Sql/FamiliesWithMoreThanOneAddress.sql
+++ RockWeb/Plugins/com_bemaservices/CustomBlocks/BEMA/Assets/Sql/FamiliesWithMoreThanOneAddress.sql
@@ -2,9 +2,8 @@ With CTE AS (
     Select  g.Id, 
             Count(Distinct gl.Id ) as HomeAddresses 
     From [Group] g 
-    Join [GroupLocation] gl on gl.GroupId = g.Id 
-    Join [DefinedValue] dv on dv.Id = gl.GroupLocationTypeValueId and dv.Guid = '8C52E53C-2A66-435A-AE6E-5EE307D9A0DC'
-    -- Only look at Home Locations 
+    Join [GroupLocation] gl on gl.GroupId = g.Id and gl.IsMailingLocation = 1
+    Join [DefinedValue] dv on dv.Id = gl.GroupLocationTypeValueId and dv.Guid = '8C52E53C-2A66-435A-AE6E-5EE307D9A0DC' -- Only look at Home Locations 
     Where g.GroupTypeId = 10 
     Group By g.Id 
     Having Count( Distinct gl.Id ) > 1 
