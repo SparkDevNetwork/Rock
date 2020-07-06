e90c6ef4fa640cd9c06b9cca6072968832ad23ab
Jason Hendee <jason.hendee@sparkdevnetwork.org>
2020-07-06 11:41:38 -0700

    - (Lava) Added IRockLavaBlock interface to allow self-closing custom Lava Commands to be listed within the 'Enabled Lava Commands' checkbox list.

---
+++ Rock/Lava/Blocks/IRockLavaBlock.cs
@@ -0,0 +1,9 @@
+﻿namespace Rock.Lava.Blocks
+{
+    /// <summary>
+    /// Interface that classes can implement to be included when searching assemblies for custom Lava Commands.
+    /// </summary>
+    public interface IRockLavaBlock
+    {
+    }
+}
