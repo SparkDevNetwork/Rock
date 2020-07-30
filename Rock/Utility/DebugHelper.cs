a7d4a5249e824da666a01f7a378209f8cab55c2b
Mike Peterson <mike@sparkabilitygroup.com>
2020-07-30 09:56:15 -0700

    - Added option to DebugHelper so that only the Timings of SqlLogging is output

--- Rock/Utility/DebugHelper.cs
+++ Rock/Utility/DebugHelper.cs
@@ -41,6 +41,8 @@ namespace Rock
         /// </summary>
         public static double _callMSTotal = 0.00;
 
+        public static bool TimingsOnly = false;
+
         private class DebugHelperUserState
         {
             public int CallNumber { get; set; }
@@ -206,7 +208,10 @@ namespace Rock
 
                 sbDebug.AppendLine( "\nEND\nGO\n\n" );
 
-                System.Diagnostics.Debug.Write( sbDebug.ToString() );
+                if ( !TimingsOnly )
+                {
+                    System.Diagnostics.Debug.Write( sbDebug.ToString() );
+                }
 
                 var sqlConnection = command.Connection as System.Data.SqlClient.SqlConnection;
 
@@ -295,7 +300,7 @@ namespace Rock
         {
             if ( _callCounts != 0 )
             {
-                Debug.WriteLine( $"####SQLLogging Summary: _callCounts:{_callCounts}, _callMSTotal:{_callMSTotal}, _callMSTotal/_callCounts:{_callMSTotal / _callCounts}####" );
+                Debug.WriteLine( $"/* ####SQLLogging Summary: _callCounts:{_callCounts}, _callMSTotal:{_callMSTotal}, _callMSTotal/_callCounts:{_callMSTotal / _callCounts}#### */" );
             }
 
             DbInterception.Remove( _debugLoggingDbCommandInterceptor );
