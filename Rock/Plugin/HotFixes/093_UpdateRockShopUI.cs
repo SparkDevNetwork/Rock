515b86f348c820eddbe4f02c69470f68dd61fe87
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2020-08-04 17:31:24 -0700

    - Commented out hofix plugin migrations.

--- Rock/Plugin/HotFixes/093_UpdateRockShopUI.cs
+++ Rock/Plugin/HotFixes/093_UpdateRockShopUI.cs
@@ -27,7 +27,7 @@ namespace Rock.Plugin.HotFixes
         /// </summary>
         public override void Up()
         {
-            UpdateRockShopFrontpageUI();
+            //UpdateRockShopFrontpageUI();
         }
 
         /// <summary>
