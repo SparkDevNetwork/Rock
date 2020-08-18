fa31d10ff4399bc6012771d3b702a114fc59fc5c
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-08-18 13:09:00 -0400

    - (Core) Fix migration name.

--- Rock.Migrations/Migrations/202008121743577_UpdatePersonalDeviceModel.Designer.cs
+++ Rock.Migrations/Migrations/202008121743577_UpdatePersonalDeviceModel.Designer.cs
@@ -7,9 +7,9 @@ namespace Rock.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class updatepersonaldevicemodel : IMigrationMetadata
+    public sealed partial class UpdatePersonalDeviceModel : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(updatepersonaldevicemodel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(UpdatePersonalDeviceModel));
         
         string IMigrationMetadata.Id
         {
