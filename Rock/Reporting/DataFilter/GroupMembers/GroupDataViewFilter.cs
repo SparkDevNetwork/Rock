69d125854602b4cac4b013ac2809eac2c70f226f
Mike Peterson <mike@sparkabilitygroup.com>
2020-09-02 10:31:07 -0700

    - Fix issue in DataViewDetail where a RelatedDataViewFilterId could be set to 0 and raise a SQL exception. Also fixed an issue where an Exception while saving a DataView could leave a DataView without a DataViewFilter.
    (cherry picked from commit f988beb1cad7018fbbc4f628e48bfd073d756b3e)

--- Rock/Reporting/DataFilter/GroupMembers/GroupDataViewFilter.cs
+++ Rock/Reporting/DataFilter/GroupMembers/GroupDataViewFilter.cs
@@ -292,7 +292,7 @@ function ()
                 return null;
             }
 
-            return ddlDataView.SelectedValue.AsIntegerOrNull();
+            return ddlDataView.SelectedValueAsId();
         }
         #endregion
 
