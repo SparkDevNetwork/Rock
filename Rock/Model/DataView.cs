SparkDevNetwork/Rock

commit 223149da855b331e9e8a505bb8014149b7aeb887

- Added additional error checking for DataViews without DataViewFilters

Rock/Model/DataView.cs
@@ -550,6 +550,12 @@ namespace Rock.Model
                 throw new RockDataViewFilterExpressionException( this.DataViewFilter, $"Unable to determine DataView EntityType for { dataViewEntityTypeCache }." );
             }
 
+            // DataViews must have a DataViewFilter (something has gone wrong it doesn't have one)
+            if ( !this.DataViewFilterId.HasValue )
+            {
+                throw new RockDataViewFilterExpressionException( this.DataViewFilter, $"DataViewFilter is null for DataView { this.Name } ({this.Id})." );
+            }
+
             bool usePersistedValues = this.PersistedScheduleIntervalMinutes.HasValue && this.PersistedLastRefreshDateTime.HasValue;
             if ( dataViewFilterOverrides != null )
             {

commit 13e2a195509c848f23300890bdc7bf88dcf8dbdb

- Fixed issue where an exception would occur when viewing a DataView (introduced with 223149da855b331e9e8a505bb8014149b7aeb887)

Rock/Model/DataView.cs
@@ -551,7 +551,9 @@ namespace Rock.Model
             }
 
             // DataViews must have a DataViewFilter (something has gone wrong it doesn't have one)
-            if ( !this.DataViewFilterId.HasValue )
+            // Note that DataViewFilterId might be null even though DataViewFilter is not null
+            // This is because the DataViewFilter might be just in memory and not saved to the database (for example, a Preview or a DynamicReport)
+            if ( this.DataViewFilter == null )
             {
                 throw new RockDataViewFilterExpressionException( this.DataViewFilter, $"DataViewFilter is null for DataView { this.Name } ({this.Id})." );
             }

RockWeb/Blocks/Reporting/DataViewDetail.ascx.cs
@@ -1017,7 +1017,10 @@ $(document).ready(function() {
             {
                 Name = dataView.Name,
                 TransformEntityTypeId = dataView.TransformEntityTypeId,
+                TransformEntityType = dataView.TransformEntityType,
                 EntityTypeId = dataView.EntityTypeId,
+                EntityType = dataView.EntityType,
+                DataViewFilterId = dataView.DataViewFilterId,
                 DataViewFilter = dataView.DataViewFilter
             };
 
@@ -1134,8 +1137,12 @@ $(document).ready(function() {
         protected void btnPreview_Click( object sender, EventArgs e )
         {
             DataView dv = new DataView();
+            var rockContext = new RockContext();
+
             dv.TransformEntityTypeId = ddlTransform.SelectedValueAsInt();
+            dv.TransformEntityType = dv.TransformEntityTypeId.HasValue ? new EntityTypeService( rockContext ).Get( dv.TransformEntityTypeId.Value ) : null;
             dv.EntityTypeId = etpEntityType.SelectedEntityTypeId;
+            dv.EntityType = etpEntityType.SelectedEntityTypeId.HasValue ? new EntityTypeService( rockContext ).Get( dv.EntityTypeId.Value ) : null;
             dv.DataViewFilter = ReportingHelper.GetFilterFromControls( phFilters );
             ShowPreview( dv );
         }