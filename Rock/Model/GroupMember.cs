0661a8e5f742627fee9dc9d4cbdf7d1debaeaba4
Mike Peterson <mike@sparkabilitygroup.com>
2020-10-08 15:11:16 -0700

    + Fixed issue in Group Placement where a member could not be removed unless the user was a Rock Admin, even if the user had Edit or Manage Member permissions on the Group. (Fixes #4395)

--- Rock/Model/GroupMember.cs
+++ Rock/Model/GroupMember.cs
@@ -28,6 +28,7 @@ using System.Runtime.Serialization;
 using Humanizer;
 
 using Rock.Data;
+using Rock.Security;
 using Rock.Transactions;
 using Rock.Web.Cache;
 using Z.EntityFramework.Plus;
@@ -312,6 +313,69 @@ namespace Rock.Model
         #endregion
 
         #region Methods
+
+        /// <summary>
+        /// A parent authority.  If a user is not specifically allowed or denied access to
+        /// this object, Rock will check the default authorization on the current type, and
+        /// then the authorization on the Rock.Security.GlobalDefault entity
+        /// </summary>
+        public override ISecured ParentAuthority
+        {
+            get
+            {
+                if ( this.Group != null )
+                {
+                    return this.Group;
+                }
+                else
+                {
+                    return base.ParentAuthority;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return <c>true</c> if the user is authorized to perform the selected action on this object.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <param name="person">The person.</param>
+        /// <returns>
+        /// <c>true</c> if the specified action is authorized; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool IsAuthorized( string action, Person person )
+        {
+            /* 2020-09-28  MDP
+             GroupMember's Group record has a special MANAGE_MEMBERS action that grants access to managing members.
+             This is effectively the same as EDIT. So, if checking security on EDIT, also check Group's
+             MANAGE_MEMBERS (in case they have MANAGE_MEMBERS but not EDIT)
+             */
+
+            if ( action.Equals( Rock.Security.Authorization.EDIT, StringComparison.OrdinalIgnoreCase ) )
+            {
+                // first, see if they auth'd using normal AUTH rules
+                var isAuthorized = base.IsAuthorized( action, person );
+                if ( !isAuthorized )
+                {
+                    // now check if they are auth'd to EDIT or MANAGE_MEMBERS on the Group
+                    var group = this.Group ?? new GroupService( new RockContext() ).Get( this.GroupId );
+
+                    if ( group != null )
+                    {
+                        // if they have EDIT on the group, they can edit GroupMember records
+                        var canEditMembers = group.IsAuthorized( Rock.Security.Authorization.EDIT, person );
+                        if ( !canEditMembers )
+                        {
+                            // if they don't have EDIT on the group, but do have MANAGE_MEMBERS, then they can 'edit' group members
+                            canEditMembers = group.IsAuthorized( Rock.Security.Authorization.MANAGE_MEMBERS, person );
+                        }
+
+                        return canEditMembers;
+                    }
+                }
+            }
+
+            return base.IsAuthorized( action, person );
+        }
 
         /// <summary>
         /// Returns a <see cref="System.String" /> that represents this instance.
