f622a784210289ba06d86e732066cb4e54562a1e
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-10-31 02:27:06 -0400

    Install StyleGuidist
    Style Guidist allows you to add notes and examples to your standard vue components and have them documented in an automatically generated style guide.

---
+++ RockWeb/Plugins/com_bemaservices/VueProjects/defaultproject/styleguide.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // set your styleguidist configuration here
+  title: 'Default Style Guide',
+  // components: 'src/components/**/*.vue',
+  // defaultExample: true,
+  // sections: [
+  //   {
+  //     name: 'First Section',
+  //     components: 'src/components/**/*.vue'
+  //   }
+  // ],
+  // webpackConfig: {
+  //   // custom config goes here
+  // },
+  exampleMode: 'expand',
+};
