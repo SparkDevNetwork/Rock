7c7ec2ab2c9b3c0a474dd84cbcf9fc851e3ab1af
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-10-31 03:27:13 -0400

    Update vue.config.js

--- RockWeb/Plugins/com_bemaservices/VueProjects/defaultproject/vue.config.js
+++ RockWeb/Plugins/com_bemaservices/VueProjects/defaultproject/vue.config.js
@@ -1,9 +1,17 @@
+// vue.config.js
+const path = require('path');
+
+const newPath = path.resolve(__dirname).replace('VueProjects', 'CustomBlocks');
+
 module.exports = {
-  publicPath: '/',
-  outputDir: '/Users/kevinmrutledge/Documents/GitHub/RockBemaDev/RockWeb/Plugins/com_bemaservices/CustomBlocks/defaultproject',
+  publicPath:
+        process.env.NODE_ENV === 'production'
+          ? newPath.split('RockWeb')[1]
+          : '/',
+
+  outputDir: newPath,
   assetsDir: './assets/',
   filenameHashing: false,
-  transpileDependencies: [
-    'vuetify',
-  ],
+
+  transpileDependencies: ['vuetify'],
 };
