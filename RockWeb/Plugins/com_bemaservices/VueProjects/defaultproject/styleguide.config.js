d0ede4d2b32e8973e4d28347e9fb877a762768fa
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-10-31 03:15:41 -0400

    Update styleguide.config.js
    Add a template with bootstrap and font awesomeheader links to the styleguidist.

--- RockWeb/Plugins/com_bemaservices/VueProjects/defaultproject/styleguide.config.js
+++ RockWeb/Plugins/com_bemaservices/VueProjects/defaultproject/styleguide.config.js
@@ -1,6 +1,43 @@
 module.exports = {
   // set your styleguidist configuration here
   title: 'Default Style Guide',
+  template: {
+    head: {
+      links: [
+        {
+          href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+          rel: 'stylesheet',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://use.fontawesome.com/releases/v5.8.2/css/all.css',
+          integrity: 'sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+          integrity: 'sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u',
+          crossorigin: 'anonymous',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
+          integrity: 'sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+    body: {
+      scripts: [
+        {
+          src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+          integrity: 'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  },
   sections: [
     {
       name: 'Introduction',
