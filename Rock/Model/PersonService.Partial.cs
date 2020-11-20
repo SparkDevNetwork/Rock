df0d43af2ab957204a8b0d307e995437a8d4133f
Mike Peterson <mike@sparkdevnetwork.org>
2020-11-20 09:51:21 -0700

    - Updated Person matching so that the person's existing email (or previous email) must match the provided email when considering the person as match. This prevents matching in cases where we should not match.

--- Rock/Model/PersonService.Partial.cs
+++ Rock/Model/PersonService.Partial.cs
@@ -317,18 +317,6 @@ namespace Rock.Model
             {
                 query = query.Where( a => a.Gender == searchParameters.Gender.Value || a.Gender == Gender.Unknown );
             }
-
-            /* 2020-11-12 MDP
-              if Email is specified and the Matched Person has an email, it MUST match the person's primary email. This avoids
-              a problem where an email could be changed on an existing person.
-              see https://app.asana.com/0/1181881054809083/1199161381220905/f for why this was done
-            */
-            if ( searchParameters.Email.IsNotNullOrWhiteSpace() )
-            {
-
-                query = query.Where( a => a.Email == searchParameters.Email || a.Email == string.Empty || a.Email == null );
-            }
-
             // Create dictionary
             var foundPeople = query
                 .Select( p => new PersonSummary()
@@ -339,7 +327,8 @@ namespace Rock.Model
                     NickName = p.NickName,
                     Gender = p.Gender,
                     BirthDate = p.BirthDate,
-                    SuffixValueId = p.SuffixValueId
+                    SuffixValueId = p.SuffixValueId,
+                    Email = p.Email
                 } )
                 .ToList()
                 .ToDictionary(
@@ -360,11 +349,10 @@ namespace Rock.Model
 
                 // OR query for email or previous email
                 var previousEmailQry = new PersonSearchKeyService( this.Context as RockContext ).Queryable();
-                Queryable( includeDeceased, includeBusinesses )
+                this.Queryable( includeDeceased, includeBusinesses )
                     .AsNoTracking()
                     .Where(
-                        p => ( p.Email != String.Empty && p.Email != null && p.Email == searchParameters.Email ) ||
-                        previousEmailQry.Any( a => a.PersonAlias.PersonId == p.Id && a.SearchValue == searchParameters.Email && a.SearchTypeValueId == searchTypeValueId )
+                        p => previousEmailQry.Any( a => a.PersonAlias.PersonId == p.Id && a.SearchValue == searchParameters.Email && a.SearchTypeValueId == searchTypeValueId )
                     )
                     .Select( p => new PersonSummary()
                     {
@@ -381,13 +369,13 @@ namespace Rock.Model
                     {
                         if ( foundPeople.ContainsKey( p.Id ) )
                         {
-                            foundPeople[p.Id].EmailMatched = true;
+                            foundPeople[p.Id].PreviousEmailMatched = true;
                         }
                         else
                         {
                             foundPeople[p.Id] = new PersonMatchResult( searchParameters, p )
                             {
-                                EmailMatched = true
+                                PreviousEmailMatched = true
                             };
                         }
                     } );
@@ -463,7 +451,7 @@ namespace Rock.Model
 
             // Find people who have a good confidence score
             var goodMatches = foundPeople.Values
-                .Where( match => match.ConfidenceScore >= MATCH_SCORE_CUTOFF )
+                .Where( match => match.MeetsMinimumConfidence )
                 .OrderByDescending( match => match.ConfidenceScore )
                 .Select( match => match.PersonId )
                 .ToList();
@@ -617,6 +605,9 @@ namespace Rock.Model
                 SuffixMatched = query.SuffixValueId.HasValue && person.SuffixValueId != null && query.SuffixValueId == person.SuffixValueId;
                 GenderMatched = query.Gender.HasValue & query.Gender == person.Gender;
 
+                EmailSearchSpecified = query.Email.IsNotNullOrWhiteSpace();
+                PrimaryEmailMatched = query.Email.IsNotNullOrWhiteSpace() && person.Email.IsNotNullOrWhiteSpace() && query.Email == person.Email;
+
                 if ( query.BirthDate.HasValue && person.BirthDate.HasValue )
                 {
                     BirthDate = query.BirthDate.Value.Month == person.BirthDate.Value.Month && query.BirthDate.Value.Day == person.BirthDate.Value.Day;
@@ -630,7 +621,11 @@ namespace Rock.Model
 
             public bool LastNameMatched { get; set; }
 
-            public bool EmailMatched { get; set; }
+            public bool EmailSearchSpecified { get; private set; }
+
+            public bool PrimaryEmailMatched { get; set; }
+
+            public bool PreviousEmailMatched { get; set; }
 
             public bool MobileMatched { get; set; }
 
@@ -644,6 +639,41 @@ namespace Rock.Model
 
             public bool BirthDateYearMatched { get; set; }
 
+            public bool MeetsMinimumConfidence
+            {
+                get
+                {
+                    if ( ConfidenceScore < MATCH_SCORE_CUTOFF )
+                    {
+                        return false;
+                    }
+
+                    if ( EmailSearchSpecified )
+                    {
+                        /* 
+                            2020-11-12 - MDP
+                            If Email is specified and the Matched Person has an email, it MUST match
+                            the person's primary email OR one of the person's previous emails.
+                            This prevents matching in cases where we should not match.
+
+                            See https://app.asana.com/0/1181881054809083/1199161381220905/f for why this was done
+                        */
+
+                        if ( PrimaryEmailMatched || PreviousEmailMatched )
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
 
             /// <summary>
             /// Calculates a score representing the likelihood this match is the correct match. Higher is better.
@@ -670,7 +700,7 @@ namespace Rock.Model
                         total += 12;
                     }
 
-                    if ( MobileMatched || EmailMatched )
+                    if ( MobileMatched || PrimaryEmailMatched || PreviousEmailMatched )
                     {
                         total += 15;
                     }
@@ -706,11 +736,14 @@ namespace Rock.Model
         private class PersonSummary
         {
             public int Id { get; set; }
+
             public string FirstName { get; set; }
 
             public string LastName { get; set; }
 
             public string NickName { get; set; }
+
+            public string Email { get; set; }
 
             public Gender Gender { get; set; }
 
