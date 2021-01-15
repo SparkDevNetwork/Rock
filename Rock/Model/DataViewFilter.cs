cfdb6360efb7f634bb13b019ced9d33da626e500
Shaun Cummings <shaun@triumph.tech>
2021-01-15 14:00:36 -0700

    - Minor grammatical correction.

--- Rock/Model/DataViewFilter.cs
+++ Rock/Model/DataViewFilter.cs
@@ -452,7 +452,7 @@ namespace Rock.Model
                             Filter expressions of these types (GroupAny/GroupAllFalse) are joined with an OR clause,
                             so they must either be defaulted to false or excluded from the where expression altogether
                             (otherwise they will return every Person record in the database, because a "True OrElse
-                            <anything>" predicate will always be true.
+                            <anything>" predicate will always be true).
 
                             Therefore, if this child filter is null, we can simply ignore it and move on to the next one.
 
