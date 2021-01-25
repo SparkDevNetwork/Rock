9f72ba353a60dd0f11fe2c1805213915fe23b9fd
Mike Peterson <mike@sparkabilitygroup.com>
2021-01-25 09:19:41 -0700

    Fix REST Startup time

---
+++ Rock.Rest/Handler/RockAssembliesResolver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Rest
+{
+    public class RockAssembliesResolver : System.Web.Http.Dispatcher.IAssembliesResolver
+    {
+        public ICollection<Assembly> GetAssemblies()
+        {
+            return Reflection.GetPluginAssemblies();
+        }
+    }
+}
