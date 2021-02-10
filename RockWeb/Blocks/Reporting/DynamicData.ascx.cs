45d3330b395a4534b8544bc4b2d18e3853ef43fe
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2021-02-10 09:53:15 -0700

    Revert "- Modified LavaDataObject to add interception of property value requests similar to Liquid Drop."
    This reverts commit 2024022f2af8c3215341d6b0ac7a5f247af8c531.
    
    (cherry picked from commit 02a0857a125cc11fd7fb29beb574294802fd6e5f)

--- RockWeb/Blocks/Reporting/DynamicData.ascx.cs
+++ RockWeb/Blocks/Reporting/DynamicData.ascx.cs
@@ -972,7 +972,7 @@ namespace RockWeb.Blocks.Reporting
         /// <summary>
         ///
         /// </summary>
-        private class DataRowDrop : LavaDataObject
+        private class DataRowDrop : DotLiquid.Drop
         {
             private readonly DataRow _dataRow;
 
@@ -981,17 +981,28 @@ namespace RockWeb.Blocks.Reporting
                 _dataRow = dataRow;
             }
 
-            protected override bool OnTryGetValue( string key, out object result )
+            public override object BeforeMethod( string method )
             {
-                if ( _dataRow.Table.Columns.Contains( key ) )
+                if ( _dataRow.Table.Columns.Contains( method ) )
                 {
-                    result = _dataRow[key];
-                    return true;
+                    return _dataRow[method];
                 }
 
-                result = null;
-                return false;
+                return null;
             }
+
+            // override for the RockDynamic class when we switch back to that from DotLiquid.Drop
+            //public override bool TryGetMember( GetMemberBinder binder, out object result )
+            //{
+            //    if ( _dataRow.Table.Columns.Contains( binder.Name ) )
+            //    {
+            //        result = _dataRow[binder.Name];
+            //        return true;
+            //    }
+
+            //    result = null;
+            //    return false;
+            //}
         }
     }
 }
