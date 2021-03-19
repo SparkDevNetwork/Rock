00be9b48a34beb961f498b472f798d96e0874869
David Leigh <to.david.leigh@gmail.com>
2021-03-20 07:06:15 +1100

    + (Lava) Updated the Lava Where filter to add support for complex filter expressions.

--- Rock.Tests.Shared/TestFramework/AssertExtensions.cs
+++ Rock.Tests.Shared/TestFramework/AssertExtensions.cs
@@ -13,7 +13,7 @@ namespace Rock.Tests.Shared
     public static partial class AssertExtensions
     {
         /// <summary>
-        /// Asserts that the two string can be considered equivalent regardless of newlines.
+        /// Asserts that the two strings can be considered equivalent regardless of newlines.
         /// </summary>
         /// <param name="expected">The expected string.</param>
         /// <param name="actual">The actual sting.</param>
@@ -33,6 +33,30 @@ namespace Rock.Tests.Shared
             }
 
             Assert.AreEqual( expected.Trim().ToStripNewlines(), actual.Trim().ToStripNewlines() );
+        }
+
+        /// <summary>
+        /// Asserts that the two strings can be considered equivalent regardless of whitespace.
+        /// </summary>
+        /// <param name="expected">The expected string.</param>
+        /// <param name="actual">The actual sting.</param>
+        public static void AreEqualIgnoreWhitespace( this Assert assert, string expected, string actual )
+        {
+            if ( expected == null
+                 && actual == null )
+            {
+                return;
+            }
+            else if ( expected == null )
+            {
+                throw new NullReferenceException( "The expected string was null" );
+            }
+            else if ( actual == null )
+            {
+                throw new NullReferenceException( "The actual string was null" );
+            }
+
+            Assert.AreEqual( expected.ToStripNewlines().RemoveSpaces(), actual.ToStripNewlines().RemoveSpaces() );
         }
 
         public static void AreEqual<T>( this Assert assert, T expected, T actual )
