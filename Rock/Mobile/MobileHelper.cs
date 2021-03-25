2ca4a453584de3db2fb8bed8a3b13a0c95cb4021
Daniel Hazelbaker <daniel@triumph.tech>
2021-03-25 15:20:28 -0700

    + (Mobile) New block to handle onboarding of a new person into the application.

--- Rock/Mobile/MobileHelper.cs
+++ Rock/Mobile/MobileHelper.cs
@@ -168,7 +168,7 @@ namespace Rock.Mobile
                 RockDateTime.Now,
                 RockDateTime.Now.Add( System.Web.Security.FormsAuthentication.Timeout ),
                 true,
-                false.ToString() );
+                username.StartsWith( "rckipid=" ).ToString() );
 
             return System.Web.Security.FormsAuthentication.Encrypt( ticket );
         }
