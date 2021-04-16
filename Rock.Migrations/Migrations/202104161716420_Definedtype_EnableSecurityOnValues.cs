9ad8c658f0cd8e8f701175407bbbfe12ee2317ea
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2021-04-16 13:40:06 -0700

    - Updated migration to set EnableSecurityOnValues to true for DefinedType.COMMUNICATION_SMS_FROM after creating the column. This will not be used until v13.0.

--- Rock.Migrations/Migrations/202104161716420_Definedtype_EnableSecurityOnValues.cs
+++ Rock.Migrations/Migrations/202104161716420_Definedtype_EnableSecurityOnValues.cs
@@ -1,4 +1,4 @@
-// <copyright>
+﻿// <copyright>
 // Copyright by the Spark Development Network
 //
 // Licensed under the Rock Community License (the "License");
@@ -30,6 +30,7 @@ namespace Rock.Migrations
         public override void Up()
         {
             AddColumn("dbo.DefinedType", "EnableSecurityOnValues", c => c.Boolean(nullable: false));
+            Sql( $"UPDATE [DefinedType] SET [EnableSecurityOnValues] = 1 WHERE [Guid] = '{Rock.SystemGuid.DefinedType.COMMUNICATION_SMS_FROM}'" );
         }
         
         /// <summary>
