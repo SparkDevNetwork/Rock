fd626527086d695c3bdcaf6008395edc19ee1bcd
Ben Wiley <ben@sparkabilitygroup.com>
2021-04-19 14:43:46 -0400

    - Add gateway shell

---
+++ Rock.PushPay/PushPayGateway.cs
@@ -0,0 +1,161 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using Rock.Financial;
+using Rock.Model;
+
+namespace Rock.PushPay
+{
+    [Description( "The PushPay Gateway." )]
+    [DisplayName( "PushPay Gateway" )]
+    [Export( typeof( GatewayComponent ) )]
+    [ExportMetadata( "ComponentName", "PushPay Gateway" )]
+    public class PushPayGateway : GatewayComponent
+    {
+        /// <summary>
+        /// Adds the scheduled payment.
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <param name="schedule">The schedule.</param>
+        /// <param name="paymentInfo">The payment info.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override FinancialScheduledTransaction AddScheduledPayment( FinancialGateway financialGateway, PaymentSchedule schedule, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Cancels the scheduled payment.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override bool CancelScheduledPayment( FinancialScheduledTransaction transaction, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Charges the specified payment info.
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <param name="paymentInfo">The payment info.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override FinancialTransaction Charge( FinancialGateway financialGateway, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Credits (Refunds) the specified transaction.
+        /// </summary>
+        /// <param name="origTransaction">The original transaction.</param>
+        /// <param name="amount">The amount.</param>
+        /// <param name="comment">The comment.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override FinancialTransaction Credit( FinancialTransaction origTransaction, decimal amount, string comment, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the payments that have been processed for any scheduled transactions
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override List<Payment> GetPayments( FinancialGateway financialGateway, DateTime startDate, DateTime endDate, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets an optional reference number needed to process future transaction from saved account.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override string GetReferenceNumber( FinancialTransaction transaction, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets an optional reference number needed to process future transaction from saved account.
+        /// </summary>
+        /// <param name="scheduledTransaction">The scheduled transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override string GetReferenceNumber( FinancialScheduledTransaction scheduledTransaction, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the scheduled payment status.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override bool GetScheduledPaymentStatus( FinancialScheduledTransaction transaction, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reactivates the scheduled payment.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override bool ReactivateScheduledPayment( FinancialScheduledTransaction transaction, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates the scheduled payment.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="paymentInfo">The payment info.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override bool UpdateScheduledPayment( FinancialScheduledTransaction transaction, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
