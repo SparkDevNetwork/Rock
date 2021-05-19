26bac7212f0c8a9b356ed2c6e0baebde4104c827
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2021-05-19 17:08:44 -0400

    + (Finance) Updated Financial Payment Details to store the expiration date and name on card un-encrypted.

--- Rock.Migrations/RockStartup/DataMigrationsStartup.cs
+++ Rock.Migrations/RockStartup/DataMigrationsStartup.cs
@@ -55,7 +55,8 @@ namespace Rock.Migrations.RockStartup
                 SystemGuid.ServiceJob.DATA_MIGRATIONS_120_ADD_COMMUNICATIONRECIPIENT_INDEX.AsGuid(),
                 SystemGuid.ServiceJob.DATA_MIGRATIONS_120_ADD_COMMUNICATION_GET_QUEUED_INDEX.AsGuid(),
                 SystemGuid.ServiceJob.DATA_MIGRATIONS_124_UPDATE_GROUP_SALUTATIONS.AsGuid(),
-                SystemGuid.ServiceJob.POST_INSTALL_DATA_MIGRATIONS.AsGuid()
+                SystemGuid.ServiceJob.POST_INSTALL_DATA_MIGRATIONS.AsGuid(),
+                SystemGuid.ServiceJob.DATA_MIGRATIONS_124_DECRYPT_FINANCIAL_PAYMENT_DETAILS.AsGuid()
             };
 
             // run any of the above jobs if they still exist (they haven't run and deleted themselves)
