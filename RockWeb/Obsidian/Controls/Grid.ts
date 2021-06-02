b88ae0b7536c78de1f30d44632984d6cc55fcc86
Ben Wiley <ben@sparkabilitygroup.com>
2021-06-02 17:12:19 -0400

    - Add large dataset grid example

--- RockWeb/Obsidian/Controls/Grid.ts
+++ RockWeb/Obsidian/Controls/Grid.ts
@@ -15,6 +15,7 @@
 // </copyright>
 //
 import { defineComponent, PropType } from 'vue';
+import JavaScriptAnchor from '../Elements/JavaScriptAnchor';
 
 export type FilterOptions = {
     Take: number;
@@ -53,6 +54,9 @@ export function getRowId ( rowData: RowData, rowIdKey: string ): RowId
 
 export default defineComponent( {
     name: 'Grid',
+    components: {
+        JavaScriptAnchor
+    },
     props: {
         gridData: {
             type: Array as PropType<RowData[]>,
@@ -66,9 +70,21 @@ export default defineComponent( {
             type: Object as PropType<SortProperty | null>,
             default: null
         },
+        pageSize: {
+            type: Number as PropType<number>,
+            default: 50
+        },
+        currentPageIndex: {
+            type: Number as PropType<number>,
+            default: 1
+        },
         rowItemText: {
             type: String as PropType<string>,
             default: 'Entity'
+        },
+        rowCountOverride: {
+            type: Number as PropType<number>,
+            default: 0
         }
     },
     data ()
@@ -80,6 +96,43 @@ export default defineComponent( {
                 sortProperty: this.sortProperty
             } as GridContext
         };
+    },
+    computed: {
+        /** The number of rows in the dataset */
+        rowCount (): number
+        {
+            if ( this.rowCountOverride )
+            {
+                return this.rowCountOverride;
+            }
+
+            return this.gridData.length;
+        },
+
+        /** How many pages are needed to display all of the rows */
+        pageCount (): number
+        {
+            return Math.ceil( this.rowCount / this.pageSize );
+        },
+
+        currentPageSet (): number[]
+        {
+            const pagesPerSet = 10;
+            const firstNumber = Math.floor( this.currentPageIndex / pagesPerSet ) * pagesPerSet + 1;
+            const set: number[] = [];
+
+            for ( let i = 0; i < pagesPerSet; i++ )
+            {
+                const pageIndex = firstNumber + i;
+
+                if ( pageIndex <= this.pageCount )
+                {
+                    set.push( pageIndex );
+                }
+            }
+
+            return set;
+        }
     },
     watch: {
         gridData ()
@@ -102,6 +155,7 @@ export default defineComponent( {
     },
     methods: {
         getRowId,
+
         getRowContext ( rowData: RowData, isHeader: boolean ): RowContext
         {
             const rowId = getRowId( rowData, this.rowIdKey );
@@ -111,6 +165,56 @@ export default defineComponent( {
                 isHeader,
                 rowId
             };
+        },
+
+        /**
+         * Set the number of rows per page
+         * @param pageSize
+         */
+        setPageSize ( pageSize: number )
+        {
+            this.$emit( 'update:pageSize', pageSize );
+        },
+
+        /**
+         * Set the current page index
+         * @param pageIndex
+         */
+        setPageIndex ( pageIndex: number )
+        {
+            this.$emit( 'update:currentPageIndex', pageIndex );
+        },
+
+        /** Go to the previous page set */
+        goToPreviousPageSet ()
+        {
+            const lowestPageInCurrentSet = this.currentPageSet[ 0 ] || 0;
+
+            if ( lowestPageInCurrentSet <= 1 )
+            {
+                return;
+            }
+
+            this.setPageIndex( lowestPageInCurrentSet - 1 );
+        },
+
+        /** Go to the next page set */
+        goToNextPageSet ()
+        {
+            const lastIndex = this.currentPageSet.length - 1;
+            const highestPageInCurrentSet = this.currentPageSet[ lastIndex ] || 0;
+
+            if ( highestPageInCurrentSet <= 1 )
+            {
+                return;
+            }
+
+            if ( highestPageInCurrentSet >= this.pageCount )
+            {
+                return;
+            }
+
+            this.setPageIndex( highestPageInCurrentSet + 1 );
         }
     },
     provide ()
@@ -139,6 +243,46 @@ export default defineComponent( {
                 <slot v-bind="getRowContext(rowData, false, )" />
             </template>
         </tbody>
+        <tfoot>
+            <tr>
+                <td class="grid-paging" colspan="6">
+                    <ul class="grid-pagesize pagination pagination-sm">
+                        <li :class="pageSize === 50 ? 'active' : ''">
+                            <JavaScriptAnchor @click="setPageSize(50)">50</JavaScriptAnchor>
+                        </li>
+                        <li :class="pageSize === 500 ? 'active' : ''">
+                            <JavaScriptAnchor @click="setPageSize(500)">500</JavaScriptAnchor>
+                        </li>
+                        <li :class="pageSize === 5000 ? 'active' : ''">
+                            <JavaScriptAnchor @click="setPageSize(5000)">5000</JavaScriptAnchor>
+                        </li>
+                    </ul>
+                    <div class="grid-itemcount">{{rowCount}} {{rowItemText}}</div>
+                    <ul v-if="pageCount > 1" class="grid-pager pagination pagination-sm">
+                        <li class="prev disabled">
+                            <JavaScriptAnchor @click="goToPreviousPageSet" class="aspNetDisabled">«</JavaScriptAnchor>
+                        </li>
+                        <li v-for="pageIndex in currentPageSet" :key="pageIndex" :class="pageIndex === currentPageIndex ? 'active' : ''">
+                            <JavaScriptAnchor @click="setPageIndex(pageIndex)">{{pageIndex}}</JavaScriptAnchor>
+                        </li>
+                        <li class="next disabled">
+                            <JavaScriptAnchor @click="goToNextPageSet" class="aspNetDisabled">»</JavaScriptAnchor>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+            <tr>
+                <td class="grid-actions" colspan="6">
+                    <JavaScriptAnchor title="Communicate" class="btn btn-grid-action btn-communicate btn-default btn-sm"><i class="fa fa-comment fa-fw"></i></JavaScriptAnchor>
+                    <JavaScriptAnchor title="Merge Person Records" class="btn btn-grid-action btn-merge btn-default btn-sm"><i class="fa fa-users fa-fw"></i></JavaScriptAnchor>
+                    <JavaScriptAnchor title="Bulk Update" class="btn btn-grid-action btn-bulk-update btn-default btn-sm"><i class="fa fa-truck fa-fw"></i></JavaScriptAnchor>
+                    <JavaScriptAnchor title="Launch Workflow" class="btn-grid-action btn-launch-workflow btn btn-default btn-sm"><i class="fa fa-cog fa-fw"></i></JavaScriptAnchor>
+                    <JavaScriptAnchor title="Export to Excel" class="btn btn-grid-action btn-excelexport btn-default btn-sm"><i class="fa fa-table fa-fw"></i></JavaScriptAnchor>
+                    <JavaScriptAnchor title="Merge Records into Merge Template" class="btn btn-grid-action btn-merge-template btn-default btn-sm"><i class="fa fa-files-o fa-fw"></i></JavaScriptAnchor>
+                    <JavaScriptAnchor accesskey="n" title="Alt+N" class="btn btn-grid-action btn-add btn-default btn-sm"><i class="fa fa-plus-circle fa-fw"></i></JavaScriptAnchor>
+                </td>
+            </tr>
+        </tfoot>
     </table>
 </div>`
 } );
