835b5c1152cf52e1fa5ca9b19214fc1b15272a74
Mike Peterson <mike@sparkdevnetwork.org>
2021-06-09 16:05:50 -0700

    - Un-obsolete LaunchWorkflowTransactions

--- Rock/Tasks/LaunchWorkflows.cs
+++ Rock/Tasks/LaunchWorkflows.cs
@@ -28,6 +28,7 @@ namespace Rock.Tasks
 {
     /// <summary>
     /// Launches multiple workflows and optionally sets the entity, name and/or attribute values
+    /// Or to use the Transaction Queue, use <seealso cref="Rock.Transactions.LaunchWorkflowsTransaction" />
     /// </summary>
     public sealed class LaunchWorkflows : BusStartedTask<LaunchWorkflows.Message>
     {
