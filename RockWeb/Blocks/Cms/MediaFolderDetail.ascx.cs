a7bef5d622d664c866b00315b0a019e272d3ebb9
Daniel Hazelbaker <daniel@triumph.tech>
2021-06-23 15:15:52 -0700

    - Updated Media Account and Media Folder detail blocks to show synchronization feedback messages in modals.

--- RockWeb/Blocks/Cms/MediaFolderDetail.ascx.cs
+++ RockWeb/Blocks/Cms/MediaFolderDetail.ascx.cs
@@ -30,6 +30,7 @@ using Rock.Security;
 using Rock.Web;
 using Rock.Web.Cache;
 using Rock.Web.UI;
+using Rock.Web.UI.Controls;
 
 namespace RockWeb.Blocks.Cms
 {
@@ -74,8 +75,6 @@ namespace RockWeb.Blocks.Cms
         protected override void OnLoad( EventArgs e )
         {
             base.OnLoad( e );
-
-            nbActionResult.Text = string.Empty;
 
             if ( !Page.IsPostBack )
             {
@@ -234,7 +233,8 @@ namespace RockWeb.Blocks.Cms
         protected void btnSyncContentChannelItems_Click( object sender, EventArgs e )
         {
             Task.Run( () => MediaFolderService.AddMissingSyncedContentChannelItems( hfId.ValueAsInt() ) );
-            nbActionResult.Text = "Content channel item creation has started and will continue in the background.";
+
+            mdSyncMessage.Show( "Content channel item creation has started and will continue in the background.", ModalAlertType.Information );
         }
 
         /// <summary>
