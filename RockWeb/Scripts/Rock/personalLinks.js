4b7c7164af7e9977da3a3a20c23f758e4d1ffd62
Jon Edmiston <jon@sparkdevnetwork.org>
2021-08-02 07:59:22 -0700

    - Updated return items to ensure that duplicate values are removed

--- RockWeb/Scripts/Rock/personalLinks.js
+++ RockWeb/Scripts/Rock/personalLinks.js
@@ -21,6 +21,10 @@
             });
         }
 
+        // Check if the return items already has this extact entry, if so remove it. This allows the new item to be at the top
+        removeDuplicates(returnItem);
+
+        // Add new item
         _returnItemArray.push(returnItem);
 
         _returnItemArray.sort(function (a, b) {
@@ -39,6 +43,37 @@
         }
 
         setLocalStorage();
+    }
+
+    function removeDuplicates(newReturnItem) {
+
+        for (var i = 0; i < _returnItemArray.length; i++) {
+            if (returnItemIsEqual(_returnItemArray[i], newReturnItem)) {
+                _returnItemArray.splice(i, 1);
+            }
+        }
+    }
+
+    // Determines if the two return items are the same item
+    function returnItemIsEqual(returnItemA, returnItemB) {
+        var isEqual = true;
+
+        if (returnItemA.type != returnItemB.type) {
+            isEqual = false;
+        }
+
+        if (returnItemA.typeOrder != returnItemB.typeOrder) {
+            isEqual = false;
+        }
+
+        if (returnItemA.itemName != returnItemB.itemName) {
+            isEqual = false;
+        }
+
+        if (returnItemA.url != returnItemB.url) {
+            isEqual = false;
+        }
+        return isEqual;
     }
 
     function getQuickReturns() {
