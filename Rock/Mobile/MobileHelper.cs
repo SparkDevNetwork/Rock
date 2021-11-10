dc5e3f998b2d27e74a7569edb3c8cb70d98971f9
Daniel Hazelbaker <daniel@triumph.tech>
2021-11-10 11:25:59 -0700

    - Added AutoRefresh setting to mobile pages that cause them to reload whenever they become visible.

--- Rock/Mobile/MobileHelper.cs
+++ Rock/Mobile/MobileHelper.cs
@@ -504,7 +504,8 @@ namespace Rock.Mobile
                     CssStyles = additionalPageSettings.CssStyles,
                     AuthorizationRules = string.Join( ",", GetOrderedExplicitAuthorizationRules( page ) ),
                     HideNavigationBar = additionalPageSettings.HideNavigationBar,
-                    ShowFullScreen = additionalPageSettings.ShowFullScreen
+                    ShowFullScreen = additionalPageSettings.ShowFullScreen,
+                    AutoRefresh = additionalPageSettings.AutoRefresh
                 };
 
                 package.Pages.Add( mobilePage );
