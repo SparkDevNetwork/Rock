89c1c715bd255228eb5d42657f1b3b14c7f3bcd7
Daniel Hazelbaker <daniel@triumph.tech>
2021-11-30 09:56:48 -0700

    - Marked a number of Obsidian related classes and interfaces as internal with comments.

---
+++ Rock/Attribute/RockInternalAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Rock.Attribute
+{
+    /// <summary>
+    /// Marks an API as internal to Rock. These APIs are not subject to the same
+    /// compatibility standards as public APIs. It may be changed or removed
+    /// without notice in any release. You should not use such APIs directly in
+    /// any plug-ins. Doing so can result in application failures when updating
+    /// to a new Rock release.
+    /// </summary>
+    [AttributeUsage(
+        AttributeTargets.Enum
+        | AttributeTargets.Class
+        | AttributeTargets.Struct
+        | AttributeTargets.Interface
+        | AttributeTargets.Event
+        | AttributeTargets.Field
+        | AttributeTargets.Method
+        | AttributeTargets.Delegate
+        | AttributeTargets.Property
+        | AttributeTargets.Constructor )]
+    public sealed class RockInternalAttribute : System.Attribute
+    {
+    }
+}
--- Rock/Financial/IObsidianHostedGatewayComponent.cs
+++ Rock/Financial/IObsidianHostedGatewayComponent.cs
@@ -15,6 +15,7 @@
 // </copyright>
 //
 
+using Rock.Attribute;
 using Rock.Model;
 
 namespace Rock.Financial
@@ -22,6 +23,15 @@ namespace Rock.Financial
     /// <summary>
     /// A Financial Gateway that has an Obsidian control
     /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         <strong>This is an internal API</strong> that supports the Rock
+    ///         infrastructure and not subject to the same compatibility standards
+    ///         as public APIs. It may be changed or removed without notice in any
+    ///         release and should therefore not be directly used in any plug-ins.
+    ///     </para>
+    /// </remarks>
+    [RockInternal]
     public interface IObsidianHostedGatewayComponent
     {
         /// <summary>
