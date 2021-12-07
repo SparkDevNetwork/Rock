8cab47ddf80cdfffead72deb31ad6542bbc51897
Kwame Agyei <kwame@accede.dev>
2021-12-07 12:06:27 +0000

    - (Core) Moved Category model to relevant folder.

--- Rock/Model/Core/Category/Category.cs
+++ Rock/Model/Core/Category/Category.cs
@@ -17,12 +17,10 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Runtime.Serialization;
 
 using Rock.Data;
-using Rock.Security;
 using Rock.Web.Cache;
 using Rock.Lava;
 
@@ -37,7 +35,6 @@ namespace Rock.Model
     [DataContract]
     public partial class Category : Model<Category>, IOrdered, ICacheable
     {
-
         #region Entity Properties
 
         /// <summary>
@@ -142,7 +139,7 @@ namespace Rock.Model
 
         #endregion
 
-        #region Virtual Properties
+        #region Navigation Properties
 
         /// <summary>
         /// Gets or sets the parent category.
@@ -171,94 +168,6 @@ namespace Rock.Model
         [DataMember]
         public virtual EntityType EntityType { get; set; }
 
-        /// <summary>
-        /// Gets the parent authority where security authorizations are being inherited from.
-        /// </summary>
-        /// <value>
-        /// The parent authority.
-        /// </value>
-        public override ISecured ParentAuthority
-        {
-            get
-            {
-                if ( ParentCategory != null )
-                {
-                    return ParentCategory;
-                }
-
-                return base.ParentAuthority;
-            }
-        }
-
-        /// <summary>
-        /// A dictionary of actions that this class supports and the description of each.
-        /// </summary>
-        public override Dictionary<string, string> SupportedActions
-        {
-            get
-            {
-                var entityTypeCache = EntityTypeCache.Get( this.EntityTypeId );
-                if ( entityTypeCache == null && this.EntityType != null )
-                {
-                    entityTypeCache = EntityTypeCache.Get( this.EntityType.Id );
-                }
-
-                if ( entityTypeCache != null )
-                {
-                    switch( entityTypeCache.Name )
-                    {
-                        case "Rock.Model.Tag":
-                            {
-                                var supportedActions = new Dictionary<string, string>();
-                                supportedActions.Add( Authorization.VIEW, "The roles and/or users that have access to view." );
-                                supportedActions.Add( Authorization.TAG, "The roles and/or users that have access to tag items." );
-                                supportedActions.Add( Authorization.EDIT, "The roles and/or users that have access to edit." );
-                                supportedActions.Add( Authorization.ADMINISTRATE, "The roles and/or users that have access to administrate." );
-                                return supportedActions;
-                            }
-                    }
-                }
-
-                return base.SupportedActions;
-            }
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether this instance is valid.
-        /// </summary>
-        /// <value>
-        ///   <c>true</c> if this instance is valid; otherwise, <c>false</c>.
-        /// </value>
-        public override bool IsValid
-        {
-            get
-            {
-                var result = base.IsValid;
-                if ( result )
-                {
-                    // make sure it isn't getting saved with a recursive parent hierarchy
-                    var parentIds = new List<int>();
-                    parentIds.Add( this.Id );
-                    var parent = this.ParentCategoryId.HasValue ? (this.ParentCategory ?? new CategoryService(new RockContext()).Get(this.ParentCategoryId.Value)) : null;
-                    while ( parent != null )
-                    {
-                        if ( parentIds.Contains( parent.Id ) )
-                        {
-                            this.ValidationResults.Add( new ValidationResult( "Parent Category cannot be a child of this category (recursion)" ) );
-                            return false;
-                        }
-                        else
-                        {
-                            parentIds.Add( parent.Id );
-                            parent = parent.ParentCategory;
-                        }
-                    }
-                }
-
-                return result;
-            }
-        }
-
         #endregion
 
         #region Public Methods
@@ -273,33 +182,6 @@ namespace Rock.Model
         {
             return this.Name;
         }
-
-        #endregion
-
-        #region ICacheable
-
-        #region ICacheable
-
-        /// <summary>
-        /// Gets the cache object associated with this Entity
-        /// </summary>
-        /// <returns></returns>
-        public IEntityCache GetCacheObject()
-        {
-            return CategoryCache.Get( this.Id );
-        }
-
-        /// <summary>
-        /// Updates any Cache Objects that are associated with this entity
-        /// </summary>
-        /// <param name="entityState">State of the entity.</param>
-        /// <param name="dbContext">The database context.</param>
-        public void UpdateCache( EntityState entityState, Rock.Data.DbContext dbContext )
-        {
-            CategoryCache.UpdateCachedEntity( this.Id, entityState );
-        }
-
-        #endregion
 
         #endregion
     }
