41280a2f44170af1d0b2dca628ce396397ee28c6
crayzd92 <daniel.vincent@triumph.tech>
2021-12-14 09:53:38 -0700

    + (Reporting) Added Week of Year, Week Counter, Leap Year Indicator, and Sunday Date Year to the Analytics Source Date information.

--- Rock/Properties/AssemblyInfo.cs
+++ Rock/Properties/AssemblyInfo.cs
@@ -18,13 +18,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "Rock" )]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible( false )]
 
@@ -33,6 +33,7 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo( "Rock.Blocks" )]
 [assembly: InternalsVisibleTo( "Rock.CodeGeneration" )]
+[assembly: InternalsVisibleTo( "Rock.Migrations" )]
 [assembly: InternalsVisibleTo( "Rock.Rest" )]
 [assembly: InternalsVisibleTo( "Rock.Tests.Shared" )]
 [assembly: InternalsVisibleTo( "Rock.Tests.UnitTests" )]
