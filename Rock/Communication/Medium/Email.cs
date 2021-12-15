a39e2e9412620d273bdfcbf4a11e85c7c79c0c3a
nairdo <nairdo@gmail.com>
2021-12-14 17:13:37 -0700

    - Changed the default value for the Email medium CSSInliningEnabled back to true following commit b606f62.

--- Rock/Communication/Medium/Email.cs
+++ Rock/Communication/Medium/Email.cs
@@ -61,7 +61,7 @@ You can view an online version of this email here:
 
     [BooleanField( "CSS Inlining Enabled",
         Description = "Enable to move CSS styles to inline attributes. This can help maximize compatibility with email clients.",
-        DefaultBooleanValue = false,
+        DefaultBooleanValue = true,
         Key = "CSSInliningEnabled",
         Order = 4)]
     public class Email : MediumComponent
