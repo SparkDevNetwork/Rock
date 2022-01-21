981e7f58f9240d704e6a8c1233999a7e52461c30
crayzd92 <daniel.vincent@triumph.tech>
2022-01-21 12:56:33 -0700

    - (CRM) Update the validation for the Person.Email property to allow more characters to better match what is allowed via RFC5322.

--- Rock/Model/CRM/Person/Person.cs
+++ Rock/Model/CRM/Person/Person.cs
@@ -362,7 +362,7 @@ namespace Rock.Model
         [DataMember]
         [Previewable]
         // DV See also: Rock.Communication.EmailAddressFieldValidator _emailAddressRegex, make sure the two stay in sync. #4829, #4867
-        [RegularExpression( @"\s*(\w+(?:[-+.]*\w+)*@\w+(?:[-.]\w+)*\.\w+(?:[-.]\w+)*)\s*", ErrorMessage = "The Email address is invalid" )]
+        [RegularExpression( @"\s*(?:[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])\s*", ErrorMessage = "The Email address is invalid" )]
         [Index( "IX_Email" )]
         public string Email { get; set; }
 
