9c565077aecf471c56c99a0042da215706f52aeb
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2022-02-03 17:07:22 -0700

    - Move migration token to 1.13.2.

--- Rock.Migrations/Migrations/RockCSharpMigrationCodeGenerator.cs
+++ Rock.Migrations/Migrations/RockCSharpMigrationCodeGenerator.cs
@@ -37,7 +37,7 @@ namespace Rock.Migrations
         /// <value>
         ///   <c>true</c> if this instance is frozen; otherwise, <c>false</c>.
         /// </value>
-        public bool IsFrozen => true;
+        public bool IsFrozen => false;
         private const string MigrationTokenOwner = "release-1.13.2";
 
         /// <summary>
