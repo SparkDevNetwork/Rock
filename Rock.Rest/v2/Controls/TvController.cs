b701fa7e7aa745248b0269c35cdc158d66b7e57d
Jon Edmiston <jon@sparkdevnetwork.org>
2022-02-11 11:25:55 -0700

    - Updates to the additional content feature rest endpoints

--- Rock.Rest/v2/Controls/TvController.cs
+++ Rock.Rest/v2/Controls/TvController.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -17,10 +34,9 @@ using Rock.Model;
 using Rock.Rest.Filters;
 using Rock.Tv;
 using Rock.Web.Cache;
-using Rock.VersionInfo;
 using Rock.Tv.Classes;
 
-namespace Rock.Rest.Controllers
+namespace Rock.Rest.v2.Controllers
 {
 
     /// <summary>
@@ -108,7 +124,8 @@ namespace Rock.Rest.Controllers
                         Model = deviceData.Model,
                         Name = deviceData.Name,
                         IsActive = true,
-                        LastSeenDateTime = RockDateTime.Now
+                        LastSeenDateTime = RockDateTime.Now,
+                        DeviceVersion = deviceData.Version
                     };
 
                     personalDeviceService.Add( personalDevice );
@@ -299,6 +316,7 @@ namespace Rock.Rest.Controllers
                 //mergeFields.Add( "PageParameter", this.Request.GetQueryNameValuePairs() );
                 mergeFields.Add( "TvShellVersion", RockRequestContext.GetHeader( "X-Rock-TvShellVersion" ) );
                 mergeFields.Add( "TvAppTheme", RockRequestContext.GetHeader( "X-Rock-AppTheme" ) );
+                mergeFields.Add( "IsDemoModeEnabled", RockRequestContext.GetHeader( "X-Rock-IsDemoModeEnabled" ) );
 
                 // Get device data
                 var deviceData = this.Request.GetHeader( "X-Rock-DeviceData" );
