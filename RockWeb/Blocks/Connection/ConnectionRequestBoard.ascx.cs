3fce135d46268734a127b9cf174fd3e903f0ed58
shivambareria <shivpat.1990@gmail.com>
2022-03-21 01:01:26 +0530

    - Minor Refactoring to match the codex.

--- RockWeb/Blocks/Connection/ConnectionRequestBoard.ascx.cs
+++ RockWeb/Blocks/Connection/ConnectionRequestBoard.ascx.cs
@@ -2451,7 +2451,7 @@ namespace RockWeb.Blocks.Connection
                     .Queryable()
                     .AsNoTracking()
                     .Where( a => a.ConnectionOpportunityId == connectionOpportunity.Id );
-                var campuses = CampusCache.All().Where( c => !c.IsActive.HasValue || c.IsActive.Value ).ToList();
+                var campuses = CampusCache.All().Where( c => c.IsActive ?? true ).ToList();
 
                 // Grant edit access to any of those in a non campus-specific connector group
                 userCanEditConnectionRequest = qryConnectionOpportunityConnectorGroups
