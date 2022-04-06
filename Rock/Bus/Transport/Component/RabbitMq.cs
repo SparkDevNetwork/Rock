SparkDevNetwork/Rock

commit 1921644cff83d147816da62aba27a7e19b80ff26

- Fix bug with rabbitmq url with older masstransit version

Rock/Bus/Transport/Component/RabbitMq.cs
@@ -78,7 +78,7 @@ namespace Rock.Bus.Transport
             return MassTransit.Bus.Factory.CreateUsingRabbitMq( configurator =>
             {
                 var user = GetUser();
-                var url = $"amqps://{user}:{GetPassword()}@{GetHost()}/{user}";
+                var url = $"rabbitmq://{user}:{GetPassword()}@{GetHost()}/{user}";
                 configurator.Host( new Uri( url ), host => { } );
                 configureEndpoints( configurator );
             } );

commit f5734a87ea3bc16ab5127bb3b5e649d64bfda742

- Updated MassTransit projects (to match versions in use in v12) and reverted 1921644cff83d147816da62aba27a7e19b80ff26, which broke TLS encryption.

Rock/Bus/Transport/Component/RabbitMq.cs
@@ -78,7 +78,7 @@ namespace Rock.Bus.Transport
             return MassTransit.Bus.Factory.CreateUsingRabbitMq( configurator =>
             {
                 var user = GetUser();
-                var url = $"rabbitmq://{user}:{GetPassword()}@{GetHost()}/{user}";
+                var url = $"amqps://{user}:{GetPassword()}@{GetHost()}/{user}";
                 configurator.Host( new Uri( url ), host => { } );
                 configureEndpoints( configurator );
             } );
@@ -92,6 +92,16 @@ namespace Rock.Bus.Transport
         /// <returns></returns>
         public override ISendEndpoint GetSendEndpoint( IBusControl bus, string queueName )
         {
+            /* 
+                 4/6/2022 - SMC
+
+                 This URL should probably be changed to use the amqps:// protocol prefix (like
+                 the one used in the GetBusControl() method above) , instead of rabbitmq://
+                 with the TLS port number (5671) hard-coded, but it is being left in a
+                 known-working configuration for the moment.
+
+            */
+
             var url = $"rabbitmq://{GetHost()}:5671/{GetUser()}/{queueName}";
             return bus.GetSendEndpoint( new Uri( url ) ).Result;
         }

Rock/Rock.csproj
@@ -144,8 +144,8 @@
     <Reference Include="Google.Cloud.Storage.V1, Version=2.4.0.0, Culture=neutral, PublicKeyToken=185c282632e132a0, processorArchitecture=MSIL">
       <HintPath>..\packages\Google.Cloud.Storage.V1.2.4.0\lib\net45\Google.Cloud.Storage.V1.dll</HintPath>
     </Reference>
-    <Reference Include="GreenPipes, Version=2.1.0.106, Culture=neutral, PublicKeyToken=b800c4cfcdeea87b, processorArchitecture=MSIL">
-      <HintPath>..\packages\GreenPipes.2.1.0\lib\net452\GreenPipes.dll</HintPath>
+    <Reference Include="GreenPipes, Version=2.1.4.118, Culture=neutral, PublicKeyToken=b800c4cfcdeea87b, processorArchitecture=MSIL">
+      <HintPath>..\packages\GreenPipes.2.1.4\lib\net452\GreenPipes.dll</HintPath>
     </Reference>
     <Reference Include="HtmlAgilityPack, Version=1.4.9.5, Culture=neutral, PublicKeyToken=bd319b19eaf3b43a, processorArchitecture=MSIL">
       <HintPath>..\packages\HtmlAgilityPack.1.4.9.5\lib\Net45\HtmlAgilityPack.dll</HintPath>
@@ -183,14 +183,14 @@
     <Reference Include="Lucene.Net.Sandbox, Version=4.0.0.0, Culture=neutral, processorArchitecture=MSIL">
       <HintPath>..\packages\Lucene.Net.Sandbox.4.8.0-beta00005\lib\net45\Lucene.Net.Sandbox.dll</HintPath>
     </Reference>
-    <Reference Include="MassTransit, Version=5.1.0.1516, Culture=neutral, PublicKeyToken=b8e0e9f2f1e657fa, processorArchitecture=MSIL">
-      <HintPath>..\packages\MassTransit.5.1.0\lib\net452\MassTransit.dll</HintPath>
+    <Reference Include="MassTransit, Version=5.5.2.0, Culture=neutral, PublicKeyToken=b8e0e9f2f1e657fa, processorArchitecture=MSIL">
+      <HintPath>..\packages\MassTransit.5.5.2\lib\net452\MassTransit.dll</HintPath>
     </Reference>
-    <Reference Include="MassTransit.AzureServiceBusTransport, Version=5.1.0.1516, Culture=neutral, PublicKeyToken=b8e0e9f2f1e657fa, processorArchitecture=MSIL">
-      <HintPath>..\packages\MassTransit.AzureServiceBus.5.1.0\lib\net452\MassTransit.AzureServiceBusTransport.dll</HintPath>
+    <Reference Include="MassTransit.AzureServiceBusTransport, Version=5.5.2.0, Culture=neutral, PublicKeyToken=b8e0e9f2f1e657fa, processorArchitecture=MSIL">
+      <HintPath>..\packages\MassTransit.AzureServiceBus.5.5.2\lib\net452\MassTransit.AzureServiceBusTransport.dll</HintPath>
     </Reference>
-    <Reference Include="MassTransit.RabbitMqTransport, Version=5.1.0.1516, Culture=neutral, PublicKeyToken=b8e0e9f2f1e657fa, processorArchitecture=MSIL">
-      <HintPath>..\packages\MassTransit.RabbitMQ.5.1.0\lib\net452\MassTransit.RabbitMqTransport.dll</HintPath>
+    <Reference Include="MassTransit.RabbitMqTransport, Version=5.5.2.0, Culture=neutral, PublicKeyToken=b8e0e9f2f1e657fa, processorArchitecture=MSIL">
+      <HintPath>..\packages\MassTransit.RabbitMQ.5.5.2\lib\net452\MassTransit.RabbitMqTransport.dll</HintPath>
     </Reference>
     <Reference Include="Microsoft.AspNet.SignalR.Core, Version=2.2.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL">
       <HintPath>..\packages\Microsoft.AspNet.SignalR.Core.2.2.0\lib\net45\Microsoft.AspNet.SignalR.Core.dll</HintPath>

Rock/packages.config
@@ -28,7 +28,7 @@
   <package id="Google.Apis.Core" version="1.42.0" targetFramework="net472" />
   <package id="Google.Apis.Storage.v1" version="1.42.0.1744" targetFramework="net472" />
   <package id="Google.Cloud.Storage.V1" version="2.4.0" targetFramework="net472" />
-  <package id="GreenPipes" version="2.1.0" targetFramework="net472" />
+  <package id="GreenPipes" version="2.1.4" targetFramework="net472" />
   <package id="HtmlAgilityPack" version="1.4.9.5" targetFramework="net472" />
   <package id="Ical.Net" version="2.3.5" targetFramework="net472" />
   <package id="IdentityModel" version="3.0.0" targetFramework="net472" />
@@ -40,9 +40,9 @@
   <package id="Lucene.Net.Queries" version="4.8.0-beta00005" targetFramework="net472" />
   <package id="Lucene.Net.QueryParser" version="4.8.0-beta00005" targetFramework="net472" />
   <package id="Lucene.Net.Sandbox" version="4.8.0-beta00005" targetFramework="net472" />
-  <package id="MassTransit" version="5.1.0" targetFramework="net472" />
-  <package id="MassTransit.AzureServiceBus" version="5.1.0" targetFramework="net472" />
-  <package id="MassTransit.RabbitMQ" version="5.1.0" targetFramework="net472" />
+  <package id="MassTransit" version="5.5.2" targetFramework="net472" />
+  <package id="MassTransit.AzureServiceBus" version="5.5.2" targetFramework="net472" />
+  <package id="MassTransit.RabbitMQ" version="5.5.2" targetFramework="net472" />
   <package id="Microsoft.AspNet.SignalR" version="2.2.0" targetFramework="net472" />
   <package id="Microsoft.AspNet.SignalR.Core" version="2.2.0" targetFramework="net472" />
   <package id="Microsoft.AspNet.SignalR.JS" version="2.2.0" targetFramework="net472" />