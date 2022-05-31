8d9158e7dcc96e7ad7464e6938ec4c2646f0485b
Jon Edmiston <jon@sparkdevnetwork.org>
2022-05-31 07:16:52 -0700

    - Refactor of the Interaction Session Data job

--- Rock/Model/Core/InteractionComponent/InteractionComponentService.cs
+++ Rock/Model/Core/InteractionComponent/InteractionComponentService.cs
@@ -113,6 +113,21 @@ namespace Rock.Model
         }
 
         /// <summary>
+        /// Returns a queryable of Interaction Components that are tied to Rock Sites with Geo Tracking enabled.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<InteractionComponent> QueryByPagesOnSitesWithGeoTracking()
+        {
+            var rockContext = this.Context as Rock.Data.RockContext;
+            var interactionChannelService = new InteractionChannelService( rockContext );
+
+            // Get a queryable of interaction channels of sites with geo tracking enabled
+            var interactionChannelQry = interactionChannelService.QueryBySitesWithGeoTracking().Select( a => a.Id );
+            
+            return this.Queryable().Where( a => interactionChannelQry.Contains( a.InteractionChannelId ) );
+        }
+
+        /// <summary>
         /// Deletes the specified GroupLocation and sets GroupLocationHistorical.GroupLocationId to NULL.
         /// Will not delete the GroupLocation and return false if the GroupLocationHistorical.GroupLocationId fails to update.
         /// Will try to determine current person alias from HttpContext.
