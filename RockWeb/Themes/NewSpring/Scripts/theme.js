0b1a7acbe8fadcca2143f078282c0e1ca6570a8c
Brian Kalwat <brian@briankalwat.com>
2022-06-23 17:01:36 -0400

    - finishing up with locations news and events

--- RockWeb/Themes/NewSpring/Scripts/theme.js
+++ RockWeb/Themes/NewSpring/Scripts/theme.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Initialize Skrollr
-    var s = skrollr.init();
+    var s = skrollr.init({
+        forceHeight: false,
+        smoothScrolling: false,
+        mobileDeceleration: 0.004,
+    });
 
     setTimeout(function () {
         skrollr.get().refresh();
     }, 0);
 
+    // Don't use Skrollr on mobile
     if (s.isMobile()) {
         s.destroy();
     }
