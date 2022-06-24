9f6d39377e64732acfc501c1c8172591e49966d1
Mike Peterson <mike@sparkdevnetwork.org>
2022-06-24 16:51:29 -0700

    - Example of EF agnostic migration

---
+++ Rock.Migrations/Migrations/RockSQLMigration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.Migrations.Migrations
+{
+    internal class RockSQLMigration : Rock.Data.IMigration
+    {
+        public void Sql( string sql )
+        {
+            throw new NotImplementedException();
+        }
+
+        public object SqlScalar( string sql )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
