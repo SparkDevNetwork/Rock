520a86d83d2b48bd1da23f21ae1a7901ec8740ce
Mike Peterson <mike@sparkdevnetwork.org>
2022-06-24 16:53:50 -0700

    - Research having EF6 Migrations scaffold directly to SQL statements so that they can easily be converted to EFCore migrations.

--- Rock.Migrations/Migrations/RockCSharpMigrationCodeGenerator.cs
+++ Rock.Migrations/Migrations/RockCSharpMigrationCodeGenerator.cs
@@ -31,6 +31,8 @@ namespace Rock.Migrations
     {
         #region overridden methods
 
+        private RockSqlServerMigrationSqlGenerator rockSqlServerMigrationSqlGenerator = new RockSqlServerMigrationSqlGenerator();
+
         /// <summary>
         /// Set <see cref="IsFrozen"/> to true if migrations are frozen in this branch, and update <see cref="MigrationTokenOwner"/>
         /// </summary>
@@ -103,13 +105,62 @@ namespace Rock.Migrations
 //
 ";
 
-            result += base.Generate( operations, @namespace, className );
+            var generatedSQLUp = rockSqlServerMigrationSqlGenerator.Generate( operations, "2008" );
+            var generatedSQLDown = rockSqlServerMigrationSqlGenerator.Generate( operations.Select(a => a.Inverse).Reverse(), "2008" );
 
-            result = result.Replace( ": DbMigration", ": Rock.Migrations.RockMigration" );
+            var upCode = generatedSQLUp.Select( a => $@"                 Sql(@""{a.Sql}"");
+" ).JoinStrings(Environment.NewLine);
 
-            result = result.Replace( "public partial class", "/// <summary>\r\n    ///\r\n    /// </summary>\r\n    public partial class" );
-            result = result.Replace( "public override void Up()", "/// <summary>\r\n        /// Operations to be performed during the upgrade process.\r\n        /// </summary>\r\n        public override void Up()" );
-            result = result.Replace( "public override void Down()", "/// <summary>\r\n        /// Operations to be performed during the downgrade process.\r\n        /// </summary>\r\n        public override void Down()" );
+            var downCode = generatedSQLDown.Select( a => $@"                 Sql(@""{a.Sql}"");
+" ).JoinStrings( Environment.NewLine );
+
+            operations = new List<System.Data.Entity.Migrations.Model.MigrationOperation>();
+
+            result = $@"
+// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the ""License"");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an ""AS IS"" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+    namespace Rock.Migrations
+    {{
+        using System;
+        using System.Data.Entity.Migrations;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public partial class {className} : Rock.Migrations.RockMigration
+        {{
+            /// <summary>
+            /// Operations to be performed during the upgrade process.
+            /// </summary>
+            public override void Up()
+            {{
+{upCode}
+            }}
+
+            /// <summary>
+            /// Operations to be performed during the downgrade process.
+            /// </summary>
+            public override void Down()
+            {{
+{downCode}
+            }}
+        }}
+    }}
+";
 
             return result;
         }
---
+++ Rock.Migrations/Migrations/RockMigrationSqlGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using System.Data.Entity.Migrations.Sql;
+using System.Data.Entity.SqlServer;
+
+namespace Rock.Migrations
+{
+    public class RockSqlServerMigrationSqlGenerator : SqlServerMigrationSqlGenerator
+    {
+        public override IEnumerable<MigrationStatement> Generate( IEnumerable<MigrationOperation> migrationOperations, string providerManifestToken )
+        {
+            var migrationStatements = base.Generate( migrationOperations, providerManifestToken );
+            return migrationStatements;
+        }
+    }
+}
