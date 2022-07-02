ccdc053e352544e44a39e75af2371ee161838af2
David Leigh <to.david.leigh@gmail.com>
2022-07-03 00:47:30 +1000

    + (Connection) Fixed the Connection Requests block to correctly show status icons in the list of requests when using the Fluid Lava engine. (Fixes #5048)

--- RockWeb/Blocks/Connection/MyConnectionOpportunities.ascx.cs
+++ RockWeb/Blocks/Connection/MyConnectionOpportunities.ascx.cs
@@ -625,7 +625,7 @@ namespace RockWeb.Blocks.Connection
                         }
                         else
                         {
-                            mergeFields.Add( "ConnectionRequestStatusIcons", connectionRequestStatusIcons );
+                            mergeFields.Add( "ConnectionRequestStatusIcons", LavaDataObject.FromAnonymousObject( connectionRequestStatusIcons ) );
                         }
 
                         mergeFields.Add( "IdleTooltip", string.Format( "Idle (no activity in {0} days)", opportunitySummary.DaysUntilRequestIdle ) );
