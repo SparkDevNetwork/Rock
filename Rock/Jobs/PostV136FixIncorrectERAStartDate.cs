fd39d15090644a86cdaac41535a9cefc29a032d3
nairdo <nairdo@gmail.com>
2022-07-18 17:11:17 -0700

    - Changed the eRA Start Date fix to a post-update job due to timeout on certain alpha test systems. That job will also be registered to run in the DataMigrationStartup class (which runs at the last step Application_Start's RunStartups())

---
+++ Rock/Jobs/PostV136FixIncorrectERAStartDate.cs
@@ -0,0 +1,68 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System.ComponentModel;
+using Quartz;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+
+namespace Rock.Jobs
+{
+    /// <summary>
+    /// A run once job for V13.6 to fix eRA Start Date data broken in v13.4
+    /// </summary>
+    /// <seealso cref="Quartz.IJob" />
+    [DisallowConcurrentExecution]
+    [DisplayName( "Rock Update Helper v13.6 - Fix Incorrect eRA Start Dates." )]
+    [Description( "This job fixes eRA Start Dates (broken in v13.4) for people who are currently eRA." )]
+
+    [IntegerField(
+        "Command Timeout",
+        AttributeKey.CommandTimeout,
+        Description = "Maximum amount of time (in seconds) to wait for each SQL command to complete. On a large database with lots of data, this could take several minutes or more.",
+        IsRequired = false,
+        DefaultIntegerValue = 3600 )]
+    public class PostV136FixIncorrectERAStartDate : IJob
+    {
+        private static class AttributeKey
+        {
+            public const string CommandTimeout = "CommandTimeout";
+        }
+
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public void Execute( IJobExecutionContext context )
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            // get the configured timeout, or default to 60 minutes if it is blank
+            var commandTimeout = dataMap.GetString( AttributeKey.CommandTimeout ).AsIntegerOrNull() ?? 3600;
+
+            // If this somehow fails and throws an exeption, the DeleteJob() call below won't run and it will try again later.
+            using ( var rockContext = new RockContext() )
+            {
+                rockContext.Database.CommandTimeout = commandTimeout;
+                rockContext.Database.ExecuteSqlCommand( Plugin.HotFixes.HotFixMigrationResource._153_FixERAStartDate_RecoverERAStartDate_Update );
+            }
+
+            // Delete the job when this run-once job completes.
+            ServiceJobService.DeleteJob( context.GetJobId() );
+        }
+    }
+}
