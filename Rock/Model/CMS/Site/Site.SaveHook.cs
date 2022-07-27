8f7e1105c6e301267be62bdaa33a374c8a00e627
Braden Cohen <braden.cohen@Yahoo.com>
2022-07-27 10:16:28 -0700

    + (Mobile) Added Deep Linking, a way to externally link directly into your mobile application.

---
+++ Rock/Model/CMS/Site/Site.SaveHook.cs
@@ -0,0 +1,60 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Threading.Tasks;
+
+using Rock.Data;
+using Rock.Web.Cache;
+
+namespace Rock.Model
+{
+    public partial class Site
+    {
+        /// <summary>
+        /// Save hook implementation for <see cref="Site"/>.
+        /// </summary>
+        /// <seealso cref="Rock.Data.EntitySaveHook{TEntity}" />
+        internal class SaveHook : EntitySaveHook<Site>
+        {
+            /// <summary>
+            /// Called after the save operation has been executed.
+            /// </summary>
+            /// <remarks>
+            /// This method is only called if <see cref="M:Rock.Data.EntitySaveHook`1.PreSave" /> returns
+            /// without error.
+            /// </remarks>
+            protected override void PostSave()
+            {
+                base.PostSave();
+
+                if ( Entity.SiteType == SiteType.Mobile )
+                {
+                    var transactionCompletedTask = DbContext.WrappedTransactionCompletedTask;
+
+                    Task.Run( async () =>
+                    {
+                        // Wait for save task to complete, then flush the cache.
+                        if ( await transactionCompletedTask )
+                        {
+                            DeepLinkCache.Flush();
+                        }
+                    } );
+                }
+            }
+        }
+    }
+}
---
+++ RockWeb/Blocks/Mobile/MobileDeepLinkDetail.ascx
@@ -0,0 +1,28 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="MobileDeepLinkDetail.ascx.cs" Inherits="RockWeb.Blocks.Mobile.MobileDeepLinkDetail" %>
+<asp:UpdatePanel ID="upContent" runat="server">
+    <ContentTemplate>
+        <Rock:NotificationBox ID="nbError" runat="server" NotificationBoxType="Danger" />
+
+        <asp:Panel ID="pnlDetails" runat="server" CssClass="panel panel-block">
+            <div class="panel-heading">
+                <h3 class="panel-title">Deep Links</h3>
+            </div>
+
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <Rock:RockTextBox ID="tbRoute" runat="server" Label="Route" Required="true" />
+                        <Rock:PagePicker ID="ppMobilePage" runat="server" Label="Mobile Page" Required="true" />
+                        <Rock:Toggle ID="tglFallbackType" runat="server" OnCheckedChanged="tglFallbackType_CheckedChanged" Label="Fallback Page" OnText="URL" OffText="Page" />
+                        <Rock:PagePicker ID="ppFallbackPage" runat="server" Label="Page" />
+                        <Rock:UrlLinkBox ID="tbFallbackUrl" runat="server" Label="URL" />
+                    </div>
+                </div>
+                <div class="actions">
+                    <asp:LinkButton ID="lbSave" runat="server" CssClass="btn btn-primary" Text="Save" OnClick="lbSave_Click" />
+                    <asp:LinkButton ID="lbCancel" runat="server" CssClass="btn btn-link" Text="Cancel" OnClick="lbCancel_Click" CausesValidation="false" />
+                </div>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
