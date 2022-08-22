19ce6428fa44a4043e576f25392d5aba64baea3e
Daniel Hazelbaker <daniel@triumph.tech>
2022-08-22 08:27:23 -0700

    - Added support for Obsidian detail blocks to enter edit based on query string parameters.

--- Rock.JavaScript.Obsidian.Blocks/src/Cms/contentCollectionDetail.ts
+++ Rock.JavaScript.Obsidian.Blocks/src/Cms/contentCollectionDetail.ts
@@ -163,12 +163,12 @@ export default defineComponent({
          * Event handler for the Cancel button being clicked while in Edit mode.
          * Handles redirect to parent page if creating a new entity.
          *
-         * @returns true if the panel should leave edit mode; otherwise false.
+         * @returns true if the panel should leave edit mode; false if it should stay in edit mode; or a string containing a redirect URL.
          */
-        const onCancelEdit = async (): Promise<boolean> => {
+        const onCancelEdit = async (): Promise<boolean | string> => {
             if (!contentCollectionEditBag.value?.idKey) {
                 if (config.navigationUrls?.[NavigationUrlKey.ParentPage]) {
-                    window.location.href = config.navigationUrls[NavigationUrlKey.ParentPage];
+                    return config.navigationUrls[NavigationUrlKey.ParentPage];
                 }
 
                 return false;
@@ -180,8 +180,10 @@ export default defineComponent({
         /**
          * Event handler for the Delete button being clicked. Sends the
          * delete request to the server and then redirects to the target page.
+         *
+         * @returns false if it should stay on the page; or a string containing a redirect URL.
          */
-        const onDelete = async (): Promise<void> => {
+        const onDelete = async (): Promise<false | string> => {
             errorMessage.value = "";
 
             const result = await invokeBlockAction<string>("Delete", {
@@ -189,10 +191,12 @@ export default defineComponent({
             });
 
             if (result.isSuccess && result.data) {
-                window.location.href = result.data;
+                return result.data;
             }
             else {
                 errorMessage.value = result.errorMessage ?? "Unknown error while trying to delete content collection.";
+
+                return false;
             }
         };
 
@@ -238,9 +242,9 @@ export default defineComponent({
          * Event handler for the panel's Save event. Send the data to the server
          * to be saved and then leave edit mode or redirect to target page.
          *
-         * @returns true if the panel should leave edit mode; otherwise false.
+         * @returns true if the panel should leave edit mode; false if it should stay in edit mode; or a string containing a redirect URL.
          */
-        const onSave = async (): Promise<boolean> => {
+        const onSave = async (): Promise<boolean | string> => {
             errorMessage.value = "";
 
             const data: DetailBlockBox<ContentCollectionBag, ContentCollectionDetailOptionsBag> = {
@@ -260,9 +264,7 @@ export default defineComponent({
                     return true;
                 }
                 else if (result.statusCode === 201 && typeof result.data === "string") {
-                    window.location.href = result.data;
-
-                    return false;
+                    return result.data;
                 }
             }
 
