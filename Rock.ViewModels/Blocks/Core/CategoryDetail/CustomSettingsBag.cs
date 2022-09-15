87453b6aefdcbf3f46ecb0a9431d1ce77faac334
shivambareria <shivpat.1990@gmail.com>
2022-09-15 22:22:39 +0530

    - Updated Category detail Obsidian block.

---
+++ Rock.ViewModels/Blocks/Core/CategoryDetail/CustomSettingsBag.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Rock.ViewModels.Blocks.Cms.ContentCollectionView;
+using Rock.ViewModels.Utility;
+
+namespace Rock.ViewModels.Blocks.Core.CategoryDetail
+{
+    /// <summary>
+    /// The settings that will be edited in the custom settings panel for the
+    /// Category Detail block.
+    /// </summary>
+    public class CustomSettingsBag
+    {
+        /// <summary>
+        /// Gets or sets the root category.
+        /// </summary>
+        public ListItemBag RootCategory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exclude categories.
+        /// </summary>
+        public List<ListItemBag> ExcludeCategories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity type identifier.
+        /// </summary>
+        public Guid? EntityTypeGuid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity type qualifier.
+        /// </summary>
+        public string EntityTypeQualifierProperty { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity type qualifier value.
+        /// </summary>
+        public string EntityTypeQualifierValue { get; set; }
+    }
+}
