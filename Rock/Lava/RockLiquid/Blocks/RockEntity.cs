4f91f00dab90d0e26d4fe257817521b9461b1e64
Jon Edmiston <jon@sparkdevnetwork.org>
2022-12-01 09:25:32 -0700

    - Fixed Lava entity command to not return entire table when providing a where filter with Id == (string). (Fixes #5236)

--- Rock/Lava/RockLiquid/Blocks/RockEntity.cs
+++ Rock/Lava/RockLiquid/Blocks/RockEntity.cs
@@ -153,7 +153,7 @@ namespace Rock.Lava.RockLiquid.Blocks
 
                         List<string> selectionParms = new List<string>();
                         selectionParms.Add( PropertyComparisonConversion( "==" ).ToString() );
-                        selectionParms.Add( parms["id"].AsInteger().ToString() );
+                        selectionParms.Add( parms["id"].AsInteger().ToString() ); // Ensure this is an integer: https://github.com/SparkDevNetwork/Rock/issues/5230
                         selectionParms.Add( propertyName );
 
                         var entityProperty = entityType.GetProperty( propertyName );
@@ -770,6 +770,14 @@ namespace Rock.Lava.RockLiquid.Blocks
                     var property = expressionParts[0];
                     var operatorType = expressionParts[1];
                     var value = expressionParts[2].Replace( "\"", "" );
+
+                    // Check if the property is Id, if so ensure that it's an integer to prevent
+                    // returning everything in the database.
+                    // https://github.com/SparkDevNetwork/Rock/issues/5236
+                    if (property == "Id")
+                    {
+                        value = value.AsInteger().ToString();
+                    }
 
                     List<string> selectionParms = new List<string>();
                     selectionParms.Add( PropertyComparisonConversion( operatorType ).ToString() );
