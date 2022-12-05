00b012a3bf2ad91565aa2aff2faa260f66d52c14
shivambareria <shivpat.1990@gmail.com>
2022-12-06 00:46:34 +0530

    + (Core) Added new security verbs to the profile blocks to allow who should see the account protection profiles. Also, added warnings to the "Edit Person" block to display account protection profile information to editors.

--- RockWeb/Blocks/Crm/PersonDetail/EditPerson.ascx.cs
+++ RockWeb/Blocks/Crm/PersonDetail/EditPerson.ascx.cs
@@ -50,6 +50,7 @@ namespace RockWeb.Blocks.Crm.PersonDetail
     [SecurityAction( SecurityActionKey.EditSMS, "The roles and/or users that can edit the SMS Enabled properties for the selected person." )]
     [SecurityAction( SecurityActionKey.EditConnectionStatus, "The roles and/or users that can edit the connection status for the selected person." )]
     [SecurityAction( SecurityActionKey.EditRecordStatus, "The roles and/or users that can edit the record status for the selected person." )]
+    [SecurityAction( SecurityActionKey.ViewProtectionProfile, "The roles and/or users that can view protection profile for the selected person." )]
 
     #region Block Attributes
 
@@ -134,6 +135,7 @@ namespace RockWeb.Blocks.Crm.PersonDetail
             public const string EditSMS = "EditSMS";
             public const string EditConnectionStatus = "EditConnectionStatus";
             public const string EditRecordStatus = "EditRecordStatus";
+            public const string ViewProtectionProfile = "ViewProtectionProfile";
         }
 
         #endregion
@@ -816,6 +818,29 @@ namespace RockWeb.Blocks.Crm.PersonDetail
         /// </summary>
         private void ShowDetails()
         {
+            if ( IsUserAuthorized( SecurityActionKey.ViewProtectionProfile ) )
+            {
+                nbAccountProtectionProfile.Visible = Person.AccountProtectionProfile > Rock.Utility.Enums.AccountProtectionProfile.Low;
+                if ( Person.AccountProtectionProfile == Rock.Utility.Enums.AccountProtectionProfile.Medium )
+                {
+                    nbAccountProtectionProfile.Text = "Use care when editing this record as the individual has a login. " +
+                        "Ensure you trust the source of the request to update their email address and/or mobile phone number as this could be used to grant access to their account.";
+                    nbAccountProtectionProfile.NotificationBoxType = NotificationBoxType.Warning;
+                }
+                else if ( Person.AccountProtectionProfile == Rock.Utility.Enums.AccountProtectionProfile.High )
+                {
+                    nbAccountProtectionProfile.Text = "Use care when editing this record as the individual has financial account information stored in Rock or is a member of a sensative security role. " +
+                        "Ensure you trust the source of the request to update their email address and/or mobile phone number as this could be used to grant access to their account.";
+                    nbAccountProtectionProfile.NotificationBoxType = NotificationBoxType.Danger;
+                }
+                else if ( Person.AccountProtectionProfile == Rock.Utility.Enums.AccountProtectionProfile.Extreme )
+                {
+                    nbAccountProtectionProfile.Text = "Use care when editing this record as the individual is in a sensitive security role. " +
+                        "Ensure you trust the source of the request to update their email address and/or mobile phone number as this could be used to grant access to their account.";
+                    nbAccountProtectionProfile.NotificationBoxType = NotificationBoxType.Danger;
+                }
+            }
+
             lTitle.Text = string.Format( "Edit: {0}", Person.FullName ).FormatAsHtmlTitle();
 
             imgPhoto.BinaryFileId = Person.PhotoId;
