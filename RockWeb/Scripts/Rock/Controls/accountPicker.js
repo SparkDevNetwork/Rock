69d3d7f968045f3ab2ce488f601e87ae1d9b61e7
Kwame_Agyei <kwame.ajyei@techvision.global>
2023-02-02 11:27:24 +0000

    - Updated toggleSelectAll on accountPicker to only expand and select nodes if selectAll is triggered and node has children.

--- RockWeb/Scripts/Rock/Controls/accountPicker.js
+++ RockWeb/Scripts/Rock/Controls/accountPicker.js
@@ -177,7 +177,9 @@
                             $control.find('.js-select-all').hide();
                         }
 
-                        self.toggleSelectAll(rockTree);
+                        if (self.selectAll) {
+                            self.toggleSelectAll(rockTree);
+                        }
                     })
                     .on('rockTree:rendered', function (evt, data) {
                         var rockTree = $control.find('.treeview').data('rockTree');
@@ -881,8 +883,8 @@
                 const $itemNameNodes = rockTree.$el.find('.rocktree-name');
 
                 const setNodeOpenState = function (node, isOpen) {
-                    node.isOpen = isOpen;
-                    if (node.hasChildren) {
+                    if (node.hasChildren && node.children) {
+                        node.isOpen = isOpen;
                         node.children.forEach(childNode => setNodeOpenState(childNode, isOpen));
                     }
                 }
@@ -895,8 +897,11 @@
                 } else {
                     // if all were already selected, toggle them to unselected
                     rockTree.setSelected([]);
+                    const $control = $('#' + this.options.controlId);
+                    const $hfItemIds = $control.find('.js-item-id-value');
+                    $hfItemIds.val('0');
                     $itemNameNodes.removeClass('selected');
-                    rockTree.nodes.forEach(node => setNodeOpenState(node,false));
+                    rockTree.nodes.forEach(node => setNodeOpenState(node, false));
                 }
 
                 rockTree.render();
