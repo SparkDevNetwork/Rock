ebf615755ec8a175e032c995cf2eed087873d961
Daniel Hazelbaker <daniel@triumph.tech>
2023-02-28 08:25:30 -0700

    - Renamed WillQueue() method to OnEnqueue() for queued transactions.

--- Rock/Transactions/AggregateAsyncTransaction.cs
+++ Rock/Transactions/AggregateAsyncTransaction.cs
@@ -50,7 +50,7 @@ namespace Rock.Transactions
         }
 
         /// <inheritdoc/>
-        public void WillEnqueue()
+        public void OnEnqueue()
         {
             _items.Enqueue( _item );
         }
