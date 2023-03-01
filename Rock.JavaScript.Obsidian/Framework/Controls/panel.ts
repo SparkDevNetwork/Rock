732163a1f9988270ac715e2e571a376efef01e92
Daniel Hazelbaker <daniel@triumph.tech>
2023-03-01 09:58:03 -0700

    - Fixed Obsidian Panel sub-header right not being right aligned.

--- Rock.JavaScript.Obsidian/Framework/Controls/panel.ts
+++ Rock.JavaScript.Obsidian/Framework/Controls/panel.ts
@@ -319,11 +319,11 @@ export default defineComponent({
         </div>
 
         <div v-if="$slots.subheaderLeft || $slots.subheaderRight" class="panel-sub-header">
-            <div v-if="$slots.subheaderLeft" class="panel-sub-header-left">
+            <div class="panel-sub-header-left">
                 <slot name="subheaderLeft" />
             </div>
 
-            <div v-if="$slots.subheaderRight" class="panel-sub-header-right">
+            <div class="panel-sub-header-right">
                 <slot name="subheaderRight" />
             </div>
         </div>
