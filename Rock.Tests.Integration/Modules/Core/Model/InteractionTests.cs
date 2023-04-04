27808dea31545f0659a477c62f047d9856cbd3fc
David Leigh <to.david.leigh@gmail.com>
2023-04-05 00:59:09 +1000

    - (Test) Added integration tests for ProcessBiAnalytics Job.

--- Rock.Tests.Integration/Modules/Core/Model/InteractionTests.cs
+++ Rock.Tests.Integration/Modules/Core/Model/InteractionTests.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rock.Data;
@@ -93,13 +109,14 @@ namespace Rock.Tests.Integration.Reporting
 
         private Rock.Model.Interaction BuildInteraction( RockContext rockContext, DateTime interactionDate )
         {
-            var interactionComponentId = ( new InteractionComponentService( rockContext ) ).Queryable().FirstOrDefault().Id;
-            var interaction = new Interaction();
-            interaction.InteractionComponentId = interactionComponentId;
-            interaction.Operation = "Test";
-            interaction.ForeignKey = interactionForeignKey;
-            interaction.InteractionDateTime = interactionDate;
+            var args = new TestDataHelper.Interactions.CreatePageViewInteractionActionArgs
+            {
+                PageIdentifier = SystemGuid.Page.EXCEPTION_LIST,
+                ForeignKey = interactionForeignKey,
+                ViewDateTime = interactionDate
+            };
 
+            var interaction = TestDataHelper.Interactions.CreatePageViewInteraction( args, rockContext );
             return interaction;
         }
 
