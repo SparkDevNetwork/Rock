4c462f5e364787280151a086eeb62cae9eb92301
ethan-sparkdevnetwork <ethan@triumph.tech>
2023-04-18 12:56:13 -0700

    - Updated RockCache CacheKeyReferences so they can be accessed by blocks such as the CacheReader block.

--- Rock/Web/Cache/RockCache.cs
+++ Rock/Web/Cache/RockCache.cs
@@ -188,7 +188,7 @@ namespace Rock.Web.Cache
         /// The object cache key references.
         /// </value>
         [RockInternal( "1.14" )]
-        internal static ConcurrentDictionary<string,CacheKeyReference> ObjectConcurrentCacheKeyReferences
+        public static ConcurrentDictionary<string,CacheKeyReference> ObjectConcurrentCacheKeyReferences
         {
             get
             {
@@ -213,7 +213,7 @@ namespace Rock.Web.Cache
         /// The string cache key references.
         /// </value>
         [RockInternal( "1.14" )]
-        internal static ConcurrentDictionary<string, CacheKeyReference> StringConcurrentCacheKeyReferences
+        public static ConcurrentDictionary<string, CacheKeyReference> StringConcurrentCacheKeyReferences
         {
             get
             {
