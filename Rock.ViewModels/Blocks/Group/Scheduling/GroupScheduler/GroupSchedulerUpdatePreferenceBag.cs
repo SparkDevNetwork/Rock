0e13bdcd172bc7ed17ae123419e6d57852a2ad4b
Jason Hendee <jason@triumph.tech>
2023-04-24 15:00:20 -0700

    - (Group) Added alternate option for a group scheduler block.
    (cherry picked from commit 516b0258ac0a958abb813e307ce9f4441f3733a5)

---
+++ Rock.ViewModels/Blocks/Group/Scheduling/GroupScheduler/GroupSchedulerUpdatePreferenceBag.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Rock.Enums.Blocks.Group.Scheduling;
+
+namespace Rock.ViewModels.Blocks.Group.Scheduling.GroupScheduler
+{
+    /// <summary>
+    /// The information needed to update a group member's scheduling preference for the group scheduler.
+    /// </summary>
+    public class GroupSchedulerUpdatePreferenceBag
+    {
+        /// <summary>
+        /// Gets or sets the attendance identifier.
+        /// </summary>
+        /// <value>
+        /// The attendance identifier.
+        /// </value>
+        public int AttendanceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the group member identifier.
+        /// </summary>
+        /// <value>
+        /// The group member identifier.
+        /// </value>
+        public int GroupMemberId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the preference that should be applied.
+        /// </summary>
+        /// <value>
+        /// The preference that should be applied.
+        /// </value>
+        public GroupSchedulerPreferenceBag SchedulePreference { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mode to be used when applying this update.
+        /// </summary>
+        /// <value>
+        /// The mode to be used when applying this update.
+        /// </value>
+        public UpdateSchedulePreferenceMode UpdateMode { get; set; }
+    }
+}
