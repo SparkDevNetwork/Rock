a5211d751c0bd913381dfc5e2783f38673b28ddc
Kwame_Agyei <kwame.ajyei@techvision.global>
2023-07-19 13:20:39 +0000

    - Added EventCalendarDetail Obsidian Block.

---
+++ Rock.ViewModels/Blocks/Event/EventCalendarDetail/EventAttributeBag.cs
@@ -0,0 +1,24 @@
+﻿using Rock.ViewModels.Utility;
+using System;
+
+namespace Rock.ViewModels.Blocks.Event.EventCalendarDetail
+{
+    public class EventAttributeBag
+    {
+        /// <summary>
+        /// Gets or sets the attribute.
+        /// </summary>
+        /// <value>
+        /// The attribute.
+        /// </value>
+        public PublicEditableAttributeBag Attribute { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the field.
+        /// </summary>
+        /// <value>
+        /// The type of the field.
+        /// </value>
+        public string FieldType { get; set; }
+    }
+}
