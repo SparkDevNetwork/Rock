0da3e820f30eac1eb99d3653dbf9c44f2ff89820
Joshua Henninger <joshua@triumph.tech>
2023-08-10 11:16:57 -0700

    - Updated Content Library API item models to support nullable published datetime values.

--- Rock/Utility/ContentLibraryApi/ContentLibraryApiItemDetailBag.cs
+++ Rock/Utility/ContentLibraryApi/ContentLibraryApiItemDetailBag.cs
@@ -78,7 +78,7 @@ namespace Rock.Utility.ContentLibraryApi
         /// <summary>
         /// Gets or sets the published date time.
         /// </summary>
-        public DateTime PublishedDateTime { get; set; }
+        public DateTime? PublishedDateTime { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the source publisher.
