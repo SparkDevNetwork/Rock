d3b6cb2c6e9fb55f04a903c3fc11d15821fc0f19
Jason Hendee <jason@triumph.tech>
2023-08-16 10:23:17 -0700

    - (Event) Added exception logging to help identify problem area(s) and a possible solution to prevent future event registrations from coming through with missing registrant data. (#5091)

---
+++ Rock/Model/Event/RegistrationTemplateFormField/RegistrationTemplateFormFieldException.cs
@@ -0,0 +1,38 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
+
+using Rock.Attribute;
+
+namespace Rock.Model
+{
+    /// <summary>
+    /// Exception to log if a registration form is unexpectedly missing fields, values, Etc.
+    /// </summary>
+    [RockInternal( "1.15.2" )]
+    public class RegistrationTemplateFormFieldException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegistrationTemplateFormFieldException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public RegistrationTemplateFormFieldException( string message ) : base( message )
+        {
+        }
+    }
+}
