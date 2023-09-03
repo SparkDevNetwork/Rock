08a648d12c1a6bac4b22eb864e065248923a9090
shivambareria <shivpat.1990@gmail.com>
2023-09-03 23:03:12 +0530

    - Updated media selector obsidian and webform field type.

--- Rock/Field/Types/MediaSelectorFieldType.cs
+++ Rock/Field/Types/MediaSelectorFieldType.cs
@@ -19,6 +19,7 @@ using System.Collections.Generic;
 using System.Web.UI;
 using Rock.Attribute;
 using Rock.Enums.Controls;
+using Rock.Media;
 using Rock.Web.UI.Controls;
 
 namespace Rock.Field.Types
@@ -111,7 +112,7 @@ namespace Rock.Field.Types
 
             var tbItemWidth = new RockTextBox();
             tbItemWidth.Label = "Item Width";
-            tbItemWidth.Help = "The width of each media item in pixes or percentage.";
+            tbItemWidth.Help = "The width of each media item in pixels or percentage.";
             tbItemWidth.AutoPostBack = true;
             tbItemWidth.TextChanged += OnQualifierUpdated;
             controls.Add( tbItemWidth );
@@ -195,6 +196,7 @@ namespace Rock.Field.Types
         public override Control EditControl( Dictionary<string, ConfigurationValue> configurationValues, string id )
         {
             var control = new Web.UI.Controls.MediaSelector { ID = id };
+            
             if ( configurationValues != null )
             {
                 if ( configurationValues.ContainsKey( MODE_TYPE ) )
