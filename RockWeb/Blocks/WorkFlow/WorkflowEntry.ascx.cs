71dc5bdf84b2cc3b8e7ec854dc8ed5dc1d10fc77
ethan-sparkdevnetwork <ethan@triumph.tech>
2023-09-08 16:27:26 -0700

    Revert "+ (Workflow) Fixed an issue in the WorkflowEntry block were a Delay action in once activity prevented the actions in subsequent activities from running until the delay succeeds. The actions without the delay will now run as expected and the action with the delay will not run until the delay succeeds. (Fixes #5512)"
    This reverts commit 2f6dea0db998c4a41883c42aa00e8440e25dc4b8.

--- RockWeb/Blocks/WorkFlow/WorkflowEntry.ascx.cs
+++ RockWeb/Blocks/WorkFlow/WorkflowEntry.ascx.cs
@@ -696,13 +696,11 @@ namespace RockWeb.Blocks.WorkFlow
                 }
 
                 activeWorkflowActivitiesList = activeWorkflowActivitiesList.OrderBy( a => a.ActivityTypeCache.Order ).ToList();
-                var activityCount = 0;
 
                 foreach ( var activity in activeWorkflowActivitiesList )
                 {
                     if ( canEdit || activity.ActivityTypeCache.IsAuthorized( Authorization.VIEW, CurrentPerson ) )
                     {
-                        activityCount++;
                         foreach ( var action in activity.ActiveActions
                             .Where( a => ( !actionId.HasValue || a.Id == actionId.Value ) ) )
                         {
@@ -747,18 +745,12 @@ namespace RockWeb.Blocks.WorkFlow
 
                                     if ( action.CompletedDateTime == null )
                                     {
-                                        if ( activityCount == activeWorkflowActivitiesList.Count() )
-                                        {
-                                            // There are no more activities so the delay has to expire before doing any more work, notify the user.
-                                            ShowMessage( NotificationBoxType.Info, string.Empty, "Workflow is delayed", true );
-                                            _action = action;
-                                            _actionType = _action.ActionTypeCache;
-                                            ActionTypeId = _actionType.Id;
+                                        ShowMessage( NotificationBoxType.Info, string.Empty, "Workflow is delayed", true );
+                                        _action = action;
+                                        _actionType = _action.ActionTypeCache;
+                                        ActionTypeId = _actionType.Id;
 
-                                            return true;
-                                        }
-                                        
-                                        break;
+                                        return true;
                                     }
 
                                 }
@@ -2267,7 +2259,6 @@ namespace RockWeb.Blocks.WorkFlow
 
                 //Don't use the default response if there is summary text or if the action is a delay, which has its own message.
                 if ( lSummary.Text.IsNullOrWhiteSpace() || ( _action != null && !(_action.ActionTypeCache.WorkflowAction is Rock.Workflow.Action.Delay ) ) )
-                //if ( lSummary.Text.IsNullOrWhiteSpace() )
                 {
                     var hideForm = _action == null || _action.Guid != previousActionGuid;
                     ShowMessage( NotificationBoxType.Success, string.Empty, responseText, hideForm );
