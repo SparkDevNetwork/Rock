fab7e5a34c6b5f207533fbd042a71c8954e0729c
PraveenMathew92 <pmathew4@asu.edu>
2023-10-05 13:27:33 -0700

    - Enable Migrations in hotfix-1.16.1

--- Rock.Migrations/Migrations/RockCSharpMigrationCodeGenerator.cs
+++ Rock.Migrations/Migrations/RockCSharpMigrationCodeGenerator.cs
@@ -37,7 +37,7 @@ namespace Rock.Migrations
         /// <value>
         ///   <c>true</c> if this instance is frozen; otherwise, <c>false</c>.
         /// </value>
-        public bool IsFrozen => true;
+        public bool IsFrozen => false;
         private const string MigrationTokenOwner = "hotfix-1.16.1";
 
         /// <summary>
