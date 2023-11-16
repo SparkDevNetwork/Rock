a83610b456e1c168ebee0be1ab1893c07af7e779
Kwame_Agyei <kwame.ajyei@techvision.global>
2023-11-16 16:07:51 +0000

    - Fixed AccountEntry block to not overwrite person info when assigning attributes.

--- Rock.Blocks/Security/AccountEntry.cs
+++ Rock.Blocks/Security/AccountEntry.cs
@@ -993,11 +993,9 @@ namespace Rock.Blocks.Security
                 var attributesForPerson = currentPerson ?? new Person();
                 attributesForPerson.LoadAttributes( rockContext );
 
-                accountEntryPersonInfoBag = new AccountEntryPersonInfoBag
-                {
-                    Attributes = attributesForPerson.GetPublicAttributesForEdit( attributesForPerson, attributeFilter: a1 => personAttributes.Any( a => a.Guid == a1.Guid ), enforceSecurity: false ),
-                    AttributeValues = attributesForPerson.GetPublicAttributeValuesForEdit( attributesForPerson, attributeFilter: a1 => personAttributes.Any( a => a.Guid == a1.Guid ), enforceSecurity: false ),
-                };
+                accountEntryPersonInfoBag = accountEntryPersonInfoBag ?? new AccountEntryPersonInfoBag();
+                accountEntryPersonInfoBag.Attributes = attributesForPerson.GetPublicAttributesForEdit( attributesForPerson, attributeFilter: a1 => personAttributes.Any( a => a.Guid == a1.Guid ), enforceSecurity: false );
+                accountEntryPersonInfoBag.AttributeValues = attributesForPerson.GetPublicAttributeValuesForEdit( attributesForPerson, attributeFilter: a1 => personAttributes.Any( a => a.Guid == a1.Guid ), enforceSecurity: false );
             }
 
             return new AccountEntryInitializationBox
