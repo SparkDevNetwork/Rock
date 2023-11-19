6aa0401030ded88a35228c2b0bd84b4c7fc16c17
Daniel Hazelbaker <daniel@blueboxmoon.com>
2023-11-19 14:55:05 -0700

    - Convert Rock.Enums and Rock.ViewModels to SDK project format.

--- Rock.ViewModels/Properties/AssemblyInfo.cs
+++ Rock.ViewModels/Properties/AssemblyInfo.cs
@@ -18,11 +18,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Rock.ViewModel")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
