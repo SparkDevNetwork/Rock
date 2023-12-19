d1584b27f6d5ac9e9cada82c3abfb377dc3ef9ee
Joe Zim <joe@triumph.tech>
2023-12-19 17:11:24 -0600

    - (Core) Added Obsidian Term Description control.

--- Rock.JavaScript.Obsidian.Blocks/src/Example/controlGallery.ts
+++ Rock.JavaScript.Obsidian.Blocks/src/Example/controlGallery.ts
@@ -255,6 +255,7 @@ import ImageUploaderGallery from "./ControlGallery/imageUploaderGallery.partial.
 import MediaPlayerGallery from "./ControlGallery/mediaPlayerGallery.partial.obs";
 import RadioButtonGallery from "./ControlGallery/radioButtonGallery.partial.obs";
 import BulletedListGallery from "./ControlGallery/bulletedListGallery.partial.obs";
+import TermDescriptionGallery from "./ControlGallery/termDescriptionGallery.partial.obs";
 
 
 // #region Control Gallery
@@ -8099,6 +8100,7 @@ const controlGalleryComponents: Record<string, Component> = [
     MediaPlayerGallery,
     RadioButtonGallery,
     BulletedListGallery,
+    TermDescriptionGallery,
 ]
     // Fix vue 3 SFC putting name in __name.
     .map(a => {
