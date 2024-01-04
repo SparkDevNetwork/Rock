78791a78bd2857a6f46308d10bde3843bdd98796
Joe Zim <joe@triumph.tech>
2024-01-04 16:26:11 -0600

    - (Core) Added Obsidian Markdown Editor control.

--- Rock.JavaScript.Obsidian.Blocks/src/Example/controlGallery.ts
+++ Rock.JavaScript.Obsidian.Blocks/src/Example/controlGallery.ts
@@ -258,6 +258,7 @@ import BulletedListGallery from "./ControlGallery/bulletedListGallery.partial.ob
 import TermDescriptionGallery from "./ControlGallery/termDescriptionGallery.partial.obs";
 import ValueFilterGallery from "./ControlGallery/valueFilterGallery.partial.obs";
 import SecurityButtonGallery from "./ControlGallery/securityButtonGallery.partial.obs";
+import MarkdownEditorGallery from "./ControlGallery/markdownEditorGallery.partial.obs";
 
 
 // #region Control Gallery
@@ -8105,6 +8106,7 @@ const controlGalleryComponents: Record<string, Component> = [
     TermDescriptionGallery,
     ValueFilterGallery,
     SecurityButtonGallery,
+    MarkdownEditorGallery,
 ]
     // Fix vue 3 SFC putting name in __name.
     .map(a => {
