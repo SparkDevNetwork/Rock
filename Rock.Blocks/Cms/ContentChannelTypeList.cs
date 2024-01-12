7d203475c9200a5b7d31dd25bdd25e802254313d
Jesse Colburn <unlearnd@gmail.com>
2024-01-12 16:10:11 -0700

    Merge branch 'feature-jc-develop-obsidian-blocks-content-channel-type-list' into develop

---
+++ Rock.Blocks/Cms/ContentChannelTypeList.cs
@@ -0,0 +1,177 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Obsidian.UI;
+using Rock.Security;
+using Rock.ViewModels.Blocks;
+using Rock.ViewModels.Blocks.Cms.ContentChannelTypeList;
+using Rock.Web.Cache;
+
+namespace Rock.Blocks.Cms
+{
+    /// <summary>
+    /// Displays a list of content channel types.
+    /// </summary>
+
+    [DisplayName( "Content Channel Type List" )]
+    [Category( "CMS" )]
+    [Description( "Displays a list of content channel types." )]
+    [IconCssClass( "fa fa-list" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    [LinkedPage( "Detail Page",
+        Description = "The page that will show the content channel type details.",
+        Key = AttributeKey.DetailPage )]
+
+    [Rock.SystemGuid.EntityTypeGuid( "afb54433-a564-4e77-a10c-8946ff9d9ec6" )]
+    [Rock.SystemGuid.BlockTypeGuid( "29227fc7-8f24-44b1-a0fb-e6a8694f1c3b" )]
+    [CustomizedGrid]
+    public class ContentChannelTypeList : RockEntityListBlockType<ContentChannelType>
+    {
+        #region Keys
+
+        private static class AttributeKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new ListBlockBox<ContentChannelTypeListOptionsBag>();
+            var builder = GetGridBuilder();
+
+            box.IsAddEnabled = GetIsAddEnabled();
+            box.IsDeleteEnabled = BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+            box.ExpectedRowCount = 15;
+            box.NavigationUrls = GetBoxNavigationUrls();
+            box.Options = GetBoxOptions();
+            box.GridDefinition = builder.BuildDefinition();
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box options required for the component to render the list.
+        /// </summary>
+        /// <returns>The options that provide additional details to the block.</returns>
+        private ContentChannelTypeListOptionsBag GetBoxOptions()
+        {
+            var options = new ContentChannelTypeListOptionsBag();
+
+            return options;
+        }
+
+        /// <summary>
+        /// Determines if the add button should be enabled in the grid.
+        /// <summary>
+        /// <returns>A boolean value that indicates if the add button should be enabled.</returns>
+        private bool GetIsAddEnabled()
+        {
+            return BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "TypeId", "((Key))" )
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override IQueryable<ContentChannelType> GetListQueryable( RockContext rockContext )
+        {
+            return base.GetListQueryable( rockContext ).Include( "Channels" );
+        }
+
+        /// <inheritdoc/>
+        protected override GridBuilder<ContentChannelType> GetGridBuilder()
+        {
+            return new GridBuilder<ContentChannelType>()
+                .WithBlock( this )
+                .AddTextField( "idKey", a => a.IdKey )
+                .AddTextField( "name", a => a.Name )
+                .AddField( "isSystem", a => a.IsSystem )
+                .AddField( "channels", a => a.Channels.Count() )
+                .AddField( "isSecurityDisabled", a => !a.IsAuthorized( Authorization.ADMINISTRATE, RequestContext.CurrentPerson ) )
+                .AddAttributeFields( GetGridAttributes() );
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="key">The identifier of the entity to be deleted.</param>
+        /// <returns>An empty result that indicates if the operation succeeded.</returns>
+        [BlockAction]
+        public BlockActionResult Delete( string key )
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                var entityService = new ContentChannelTypeService( rockContext );
+                var entity = entityService.Get( key, !PageCache.Layout.Site.DisablePredictableIds );
+
+                if ( entity == null )
+                {
+                    return ActionBadRequest( $"{ContentChannelType.FriendlyTypeName} not found." );
+                }
+
+                if ( !BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson ) )
+                {
+                    return ActionBadRequest( $"Not authorized to delete ${ContentChannelType.FriendlyTypeName}." );
+                }
+
+                if ( !entityService.CanDelete( entity, out var errorMessage ) )
+                {
+                    return ActionBadRequest( errorMessage );
+                }
+
+                entityService.Delete( entity );
+                rockContext.SaveChanges();
+
+                return ActionOk();
+            }
+        }
+
+        #endregion
+    }
+}
