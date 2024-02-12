40f7cf4ac71f040803b5d7abbd46cd94fd4314e2
PraveenMathew92 <pmathew4@asu.edu>
2024-02-12 16:52:46 -0700

    - Fix build

--- Rock.JavaScript.Obsidian/Framework/FieldTypes/videoFileField.partial.ts
+++ Rock.JavaScript.Obsidian/Framework/FieldTypes/videoFileField.partial.ts
@@ -23,7 +23,8 @@ export const enum ConfigurationValueKey {
     FileName = "fileName",
     MimeType = "mimeType",
     FilePath = "filePath",
-    FileGuid = "fileGuid"
+    FileGuid = "fileGuid",
+    BinaryFileType = "binaryFileType"
 }
 
 // The edit component can be quite large, so load it only as needed.
