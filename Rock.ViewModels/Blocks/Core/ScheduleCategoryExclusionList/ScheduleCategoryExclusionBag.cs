0fa726a55f82a110c0e691470dfb48798569e27c
Kwame_Agyei <kwame.ajyei@techvision.global>
2024-03-16 15:21:55 +0000

    - Added obsidian Schedule Category Exclusion List block.

---
+++ Rock.ViewModels/Blocks/Core/ScheduleCategoryExclusionList/ScheduleCategoryExclusionBag.cs
@@ -0,0 +1,59 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
+
+namespace Rock.ViewModels.Blocks.Core.ScheduleCategoryExclusionList
+{
+    /// <summary>
+    /// Contains the ScheduleCategoryExclusion details.
+    /// </summary>
+    public class ScheduleCategoryExclusionBag
+    {
+        /// <summary>
+        /// Gets or sets the identifier key of this entity.
+        /// </summary>
+        /// <value>
+        /// The identifier key of this entity.
+        /// </value>
+        public string IdKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start date.
+        /// </summary>
+        /// <value>
+        /// The start date.
+        /// </value>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end date.
+        /// </summary>
+        /// <value>
+        /// The end date.
+        /// </value>
+        public DateTime EndDate { get; set; }
+    }
+}
