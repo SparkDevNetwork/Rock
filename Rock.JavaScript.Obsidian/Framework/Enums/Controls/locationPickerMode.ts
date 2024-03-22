1af77f288f1b4e476f1efbaa7618b04da465c62f
Joe Zim <contact@joezimjs.com>
2024-03-22 15:14:36 -0500

    - Extract LocationPickerMode enum to a separate file.

---
+++ Rock.JavaScript.Obsidian/Framework/Enums/Controls/locationPickerMode.ts
@@ -0,0 +1,27 @@
+// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+export const LocationPickerMode = {
+    None: 0,
+    Address: 1,
+    Named: 2,
+    Point: 4,
+    Polygon: 8,
+    All: 15
+} as const;
+
+export type LocationPickerMode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
