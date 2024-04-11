8ca82a9f5212504137a9a48a27128d2703b8dfc2
jonrob <jonathan@triumph.tech>
2024-04-11 14:11:11 -0700

    - Added XML comments to view model file

--- Rock.ViewModels/Blocks/Reporting/VolunteerGenerosityAnalysis/VolunteerGenerosityAnalysisBag.cs
+++ Rock.ViewModels/Blocks/Reporting/VolunteerGenerosityAnalysis/VolunteerGenerosityAnalysisBag.cs
@@ -24,9 +24,6 @@ namespace Rock.ViewModels.Blocks.Reporting.VolunteerGenerosityAnalysis
     /// <summary>
     /// A bag that contains all data for the Volunteer Generosity Analysis block.
     /// </summary>
-    /// <summary>
-    /// A ViewModel representing the overall data for the Volunteer Generosity Analysis block.
-    /// </summary>
     public class VolunteerGenerosityDataBag
     {
         public List<VolunteerGenerosityPersonDataBag> PeopleData { get; set; } = new List<VolunteerGenerosityPersonDataBag>();
@@ -37,8 +34,17 @@ namespace Rock.ViewModels.Blocks.Reporting.VolunteerGenerosityAnalysis
     /// </summary>
     public class VolunteerGenerosityPersonDataBag
     {
+        /// <summary>
+        /// Gets or sets the person group key.
+        /// </summary>
         public string PersonGroupKey { get; set; }
+        /// <summary>
+        /// Gets or sets the person details.
+        /// </summary>
         public VolunteerGenerosityPersonDetailsBag PersonDetails { get; set; }
+        /// <summary>
+        /// Gets or sets the list of donations.
+        /// </summary>
         public List<VolunteerGenerosityDonationBag> Donations { get; set; } = new List<VolunteerGenerosityDonationBag>();
     }
 
@@ -47,15 +53,45 @@ namespace Rock.ViewModels.Blocks.Reporting.VolunteerGenerosityAnalysis
     /// </summary>
     public class VolunteerGenerosityPersonDetailsBag
     {
+        /// <summary>
+        /// Gets or sets the person identifier.
+        /// </summary>
         public string PersonId { get; set; }
+        /// <summary>
+        /// Gets or sets the person's last name.
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// Gets or sets the person's nickname.
+        /// </summary>
         public string NickName { get; set; }
+        /// <summary>
+        /// Gets or sets the person's photo URL.
+        /// </summary>
         public string PhotoUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the person's giving identifier.
+        /// </summary>
         public string GivingId { get; set; }
+        /// <summary>
+        /// Gets or sets the person's last attendance date.
+        /// </summary>
         public string LastAttendanceDate { get; set; }
+        /// <summary>
+        /// Gets or sets the Group ID.
+        /// </summary>
         public string GroupId { get; set; }
+        /// <summary>
+        /// Gets or sets the Group Name.
+        /// </summary>
         public string GroupName { get; set; }
+        /// <summary>
+        /// Gets or sets the Campus ID.
+        /// </summary>
         public string CampusId { get; set; }
+        /// <summary>
+        /// Gets or sets the Campus Short Code
+        /// </summary>
         public string CampusShortCode { get; set; }
     }
 
@@ -64,16 +100,40 @@ namespace Rock.ViewModels.Blocks.Reporting.VolunteerGenerosityAnalysis
     /// </summary>
     public class VolunteerGenerosityDonationBag
     {
+        /// <summary>
+        /// Gets or sets the month name.
+        /// </summary>
         public string MonthNameAbbreviated { get; set; }
+        /// <summary>
+        /// Gets or sets the year.
+        /// </summary>
         public string Year { get; set; }
+        /// <summary>
+        /// Gets or sets the month.
+        /// </summary>
         public string Month { get; set; }
     }
 
+    /// <summary>
+    /// Bag for person-related data in the Volunteer Generosity Analysis block.
+    /// </summary>
     public class VolunteerGenerosityPersonBag
     {
+        /// <summary>
+        /// Gets or sets the person ID.
+        /// </summary>
         public string PersonId { get; set; }
+        /// <summary>
+        /// Gets or sets the person's last name.
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// Gets or sets the person's nickname.
+        /// </summary>
         public string NickName { get; set; }
+        /// <summary>
+        /// Gets or sets the person's photo URL.
+        /// </summary>
         public string PhotoUrl { get; set; }
     }
 
