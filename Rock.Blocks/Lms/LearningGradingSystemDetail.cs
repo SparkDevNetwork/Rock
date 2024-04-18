d4fe962e339868678a489b7fc494fa4ae7ee5963
Jesse Colburn <unlearnd@gmail.com>
2024-04-18 14:09:48 -0700

    - Added Obsidian Grading System Detail and Grading System Scale List blocks.

---
+++ Rock.Blocks/Lms/LearningGradingSystemDetail.cs
@@ -0,0 +1,464 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Constants;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.ViewModels.Blocks;
+using Rock.ViewModels.Blocks.Lms.LearningGradingSystemDetail;
+using Rock.Web.Cache;
+
+namespace Rock.Blocks.Lms
+{
+    /// <summary>
+    /// Displays the details of a particular learning grading system.
+    /// </summary>
+
+    [DisplayName( "Learning Grading System Detail" )]
+    [Category( "LMS" )]
+    [Description( "Displays the details of a particular learning grading system." )]
+    [IconCssClass( "fa fa-question" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    #region Block Attributes
+
+    #endregion
+
+    [Rock.SystemGuid.EntityTypeGuid( "5fc1b0ed-cd41-4ec2-a319-6c23fac30650" )]
+    [Rock.SystemGuid.BlockTypeGuid( "a4182806-95b0-49ae-97b5-246a834156e3" )]
+    public class LearningGradingSystemDetail : RockEntityDetailBlockType<LearningGradingSystem>
+    {
+        #region Keys
+
+        private static class PageParameterKey
+        {
+            public const string LearningGradingSystemId = "LearningGradingSystemId";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string ParentPage = "ParentPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag>();
+
+            SetBoxInitialEntityState( box );
+
+            box.NavigationUrls = GetBoxNavigationUrls();
+            box.Options = GetBoxOptions( box.IsEditable );
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box options required for the component to render the view
+        /// or edit the entity.
+        /// </summary>
+        /// <param name="isEditable"><c>true</c> if the entity is editable; otherwise <c>false</c>.</param>
+        /// <returns>The options that provide additional details to the block.</returns>
+        private LearningGradingSystemDetailOptionsBag GetBoxOptions( bool isEditable )
+        {
+            var options = new LearningGradingSystemDetailOptionsBag();
+
+            return options;
+        }
+
+        /// <summary>
+        /// Validates the LearningGradingSystem for any final information that might not be
+        /// valid after storing all the data from the client.
+        /// </summary>
+        /// <param name="learningGradingSystem">The LearningGradingSystem to be validated.</param>
+        /// <param name="errorMessage">On <c>false</c> return, contains the error message.</param>
+        /// <returns><c>true</c> if the LearningGradingSystem is valid, <c>false</c> otherwise.</returns>
+        private bool ValidateLearningGradingSystem( LearningGradingSystem learningGradingSystem, out string errorMessage )
+        {
+            errorMessage = null;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Sets the initial entity state of the box. Populates the Entity or
+        /// ErrorMessage properties depending on the entity and permissions.
+        /// </summary>
+        /// <param name="box">The box to be populated.</param>
+        private void SetBoxInitialEntityState( DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag> box )
+        {
+            var entity = GetInitialEntity();
+
+            if ( entity == null )
+            {
+                box.ErrorMessage = $"The {LearningGradingSystem.FriendlyTypeName} was not found.";
+                return;
+            }
+
+            var isViewable = entity.IsAuthorized( Authorization.VIEW, RequestContext.CurrentPerson );
+            box.IsEditable = entity.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+
+            entity.LoadAttributes( RockContext );
+
+            if ( entity.Id != 0 )
+            {
+                // Existing entity was found, prepare for view mode by default.
+                if ( isViewable )
+                {
+                    box.Entity = GetEntityBagForView( entity );
+                }
+                else
+                {
+                    box.ErrorMessage = EditModeMessage.NotAuthorizedToView( LearningGradingSystem.FriendlyTypeName );
+                }
+            }
+            else
+            {
+                // New entity is being created, prepare for edit mode by default.
+                if ( box.IsEditable )
+                {
+                    box.Entity = GetEntityBagForEdit( entity );
+                }
+                else
+                {
+                    box.ErrorMessage = EditModeMessage.NotAuthorizedToEdit( LearningGradingSystem.FriendlyTypeName );
+                }
+            }
+
+            PrepareDetailBox( box, entity );
+        }
+
+        /// <summary>
+        /// Gets the entity bag that is common between both view and edit modes.
+        /// </summary>
+        /// <param name="entity">The entity to be represented as a bag.</param>
+        /// <returns>A <see cref="LearningGradingSystemBag"/> that represents the entity.</returns>
+        private LearningGradingSystemBag GetCommonEntityBag( LearningGradingSystem entity )
+        {
+            if ( entity == null )
+            {
+                return null;
+            }
+
+            return new LearningGradingSystemBag
+            {
+                IdKey = entity.IdKey,
+                Description = entity.Description,
+                IsActive = entity.IsActive,
+                Name = entity.Name
+            };
+        }
+
+        /// <summary>
+        /// Gets the bag for viewing the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity to be represented for view purposes.</param>
+        /// <returns>A <see cref="LearningGradingSystemBag"/> that represents the entity.</returns>
+        private LearningGradingSystemBag GetEntityBagForView( LearningGradingSystem entity )
+        {
+            if ( entity == null )
+            {
+                return null;
+            }
+
+            var bag = GetCommonEntityBag( entity );
+
+            bag.LoadAttributesAndValuesForPublicView( entity, RequestContext.CurrentPerson );
+
+            return bag;
+        }
+
+        /// <summary>
+        /// Gets the bag for editing the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity to be represented for edit purposes.</param>
+        /// <returns>A <see cref="LearningGradingSystemBag"/> that represents the entity.</returns>
+        private LearningGradingSystemBag GetEntityBagForEdit( LearningGradingSystem entity )
+        {
+            if ( entity == null )
+            {
+                return null;
+            }
+
+            var bag = GetCommonEntityBag( entity );
+
+            bag.LoadAttributesAndValuesForPublicEdit( entity, RequestContext.CurrentPerson );
+
+            return bag;
+        }
+
+        /// <summary>
+        /// Updates the entity from the data in the save box.
+        /// </summary>
+        /// <param name="entity">The entity to be updated.</param>
+        /// <param name="box">The box containing the information to be updated.</param>
+        /// <returns><c>true</c> if the box was valid and the entity was updated, <c>false</c> otherwise.</returns>
+        private bool UpdateEntityFromBox( LearningGradingSystem entity, DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag> box )
+        {
+            if ( box.ValidProperties == null )
+            {
+                return false;
+            }
+
+            box.IfValidProperty( nameof( box.Entity.Description ),
+                () => entity.Description = box.Entity.Description );
+
+            box.IfValidProperty( nameof( box.Entity.IsActive ),
+                () => entity.IsActive = box.Entity.IsActive );
+
+            box.IfValidProperty( nameof( box.Entity.Name ),
+                () => entity.Name = box.Entity.Name );
+
+            box.IfValidProperty( nameof( box.Entity.AttributeValues ),
+                () =>
+                {
+                    entity.LoadAttributes( RockContext );
+
+                    entity.SetPublicAttributeValues( box.Entity.AttributeValues, RequestContext.CurrentPerson );
+                } );
+
+            return true;
+        }
+
+        /// <inheritdoc/>
+        protected override LearningGradingSystem GetInitialEntity()
+        {
+            return GetInitialEntity<LearningGradingSystem, LearningGradingSystemService>( RockContext, PageParameterKey.LearningGradingSystemId );
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.ParentPage] = this.GetParentPageUrl()
+            };
+        }
+
+        /// <summary>
+        /// Attempts to load an entity to be used for an edit action.
+        /// </summary>
+        /// <param name="idKey">The identifier key of the entity to load.</param>
+        /// <param name="entity">Contains the entity that was loaded when <c>true</c> is returned.</param>
+        /// <param name="error">Contains the action error result when <c>false</c> is returned.</param>
+        /// <returns><c>true</c> if the entity was loaded and passed security checks.</returns>
+        private bool TryGetEntityForEditAction( string idKey, out LearningGradingSystem entity, out BlockActionResult error )
+        {
+            var entityService = new LearningGradingSystemService( RockContext );
+            error = null;
+
+            // Determine if we are editing an existing entity or creating a new one.
+            if ( idKey.IsNotNullOrWhiteSpace() )
+            {
+                // If editing an existing entity then load it and make sure it
+                // was found and can still be edited.
+                entity = entityService.Get( idKey, !PageCache.Layout.Site.DisablePredictableIds );
+            }
+            else
+            {
+                // Create a new entity.
+                entity = new LearningGradingSystem();
+                entityService.Add( entity );
+            }
+
+            if ( entity == null )
+            {
+                error = ActionBadRequest( $"{LearningGradingSystem.FriendlyTypeName} not found." );
+                return false;
+            }
+
+            if ( !entity.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson ) )
+            {
+                error = ActionBadRequest( $"Not authorized to edit ${LearningGradingSystem.FriendlyTypeName}." );
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        /// <summary>
+        /// Gets the box that will contain all the information needed to begin
+        /// the edit operation.
+        /// </summary>
+        /// <param name="key">The identifier of the entity to be edited.</param>
+        /// <returns>A box that contains the entity and any other information required.</returns>
+        [BlockAction]
+        public BlockActionResult Edit( string key )
+        {
+            if ( !TryGetEntityForEditAction( key, out var entity, out var actionError ) )
+            {
+                return actionError;
+            }
+
+            entity.LoadAttributes( RockContext );
+
+            var box = new DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag>
+            {
+                Entity = GetEntityBagForEdit( entity )
+            };
+
+            return ActionOk( box );
+        }
+
+        /// <summary>
+        /// Saves the entity contained in the box.
+        /// </summary>
+        /// <param name="box">The box that contains all the information required to save.</param>
+        /// <returns>A new entity bag to be used when returning to view mode, or the URL to redirect to after creating a new entity.</returns>
+        [BlockAction]
+        public BlockActionResult Save( DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag> box )
+        {
+                var entityService = new LearningGradingSystemService( RockContext );
+
+                if ( !TryGetEntityForEditAction( box.Entity.IdKey, out var entity, out var actionError ) )
+                {
+                    return actionError;
+                }
+
+                // Update the entity instance from the information in the bag.
+                if ( !UpdateEntityFromBox( entity, box ) )
+                {
+                    return ActionBadRequest( "Invalid data." );
+                }
+
+                // Ensure everything is valid before saving.
+                if ( !ValidateLearningGradingSystem( entity, out var validationMessage ) )
+                {
+                    return ActionBadRequest( validationMessage );
+                }
+
+                var isNew = entity.Id == 0;
+
+                RockContext.WrapTransaction( () =>
+                {
+                    RockContext.SaveChanges();
+                    entity.SaveAttributeValues( RockContext );
+                } );
+
+                if ( isNew )
+                {
+                    return ActionContent( System.Net.HttpStatusCode.Created, this.GetCurrentPageUrl( new Dictionary<string, string>
+                    {
+                        [PageParameterKey.LearningGradingSystemId] = entity.IdKey
+                    } ) );
+                }
+
+                // Ensure navigation properties will work now.
+                entity = entityService.Get( entity.Id );
+                entity.LoadAttributes( RockContext );
+
+                return ActionOk( GetEntityBagForView( entity ) );
+            }
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="key">The identifier of the entity to be deleted.</param>
+        /// <returns>A string that contains the URL to be redirected to on success.</returns>
+        [BlockAction]
+        public BlockActionResult Delete( string key )
+        {
+            var entityService = new LearningGradingSystemService( RockContext );
+
+            if ( !TryGetEntityForEditAction( key, out var entity, out var actionError ) )
+            {
+                return actionError;
+            }
+
+            if ( !entityService.CanDelete( entity, out var errorMessage ) )
+            {
+                return ActionBadRequest( errorMessage );
+            }
+
+            entityService.Delete( entity );
+            RockContext.SaveChanges();
+
+            return ActionOk( this.GetParentPageUrl() );
+        }
+
+        /// <summary>
+        /// Refreshes the list of attributes that can be displayed for editing
+        /// purposes based on any modified values on the entity.
+        /// </summary>
+        /// <param name="box">The box that contains all the information about the entity being edited.</param>
+        /// <returns>A box that contains the entity and attribute information.</returns>
+        [BlockAction]
+        public BlockActionResult RefreshAttributes( DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag> box )
+        {
+            if ( !TryGetEntityForEditAction( box.Entity.IdKey, out var entity, out var actionError ) )
+            {
+                return actionError;
+            }
+
+            // Update the entity instance from the information in the bag.
+            if ( !UpdateEntityFromBox( entity, box ) )
+            {
+                return ActionBadRequest( "Invalid data." );
+            }
+
+            // Reload attributes based on the new property values.
+            entity.LoadAttributes( RockContext );
+
+            var refreshedBox = new DetailBlockBox<LearningGradingSystemBag, LearningGradingSystemDetailOptionsBag>
+            {
+                Entity = GetEntityBagForEdit( entity )
+            };
+
+            var oldAttributeGuids = box.Entity.Attributes.Values.Select( a => a.AttributeGuid ).ToList();
+            var newAttributeGuids = refreshedBox.Entity.Attributes.Values.Select( a => a.AttributeGuid );
+
+            // If the attributes haven't changed then return a 204 status code.
+            if ( oldAttributeGuids.SequenceEqual( newAttributeGuids ) )
+            {
+                return ActionStatusCode( System.Net.HttpStatusCode.NoContent );
+            }
+
+            // Replace any values for attributes that haven't changed with
+            // the value sent by the client. This ensures any unsaved attribute
+            // value changes are not lost.
+            foreach ( var kvp in refreshedBox.Entity.Attributes )
+            {
+                if ( oldAttributeGuids.Contains( kvp.Value.AttributeGuid ) )
+                {
+                    refreshedBox.Entity.AttributeValues[kvp.Key] = box.Entity.AttributeValues[kvp.Key];
+                }
+            }
+
+            return ActionOk( refreshedBox );
+        }
+
+        #endregion
+    }
+}
