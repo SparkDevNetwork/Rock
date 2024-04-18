2570e883826f45e1308ac6b7b8a4613dcca445f7
Jesse Colburn <unlearnd@gmail.com>
2024-04-18 11:59:30 -0700

    - Added Obsidian Grading System List block.

---
+++ Rock.Blocks/Lms/LearningGradingSystemList.cs
@@ -0,0 +1,176 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Obsidian.UI;
+using Rock.Security;
+using Rock.ViewModels.Blocks;
+using Rock.ViewModels.Blocks.Lms.LearningGradingSystemList;
+using Rock.Web.Cache;
+
+namespace Rock.Blocks.Lms
+{
+    /// <summary>
+    /// Displays a list of learning grading systems.
+    /// </summary>
+
+    [DisplayName( "Learning Grading System List" )]
+    [Category( "LMS" )]
+    [Description( "Displays a list of learning grading systems." )]
+    [IconCssClass( "fa fa-list" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    [LinkedPage( "Detail Page",
+        Description = "The page that will show the learning grading system details.",
+        Key = AttributeKey.DetailPage )]
+
+    [Rock.SystemGuid.EntityTypeGuid( "10d43ead-972c-4d6d-b5cb-a2d463247369" )]
+    [Rock.SystemGuid.BlockTypeGuid( "f003daac-b9fe-4007-b218-983084e1126b" )]
+    [CustomizedGrid]
+    public class LearningGradingSystemList : RockEntityListBlockType<LearningGradingSystem>
+    {
+        #region Keys
+
+        private static class AttributeKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new ListBlockBox<LearningGradingSystemListOptionsBag>();
+            var builder = GetGridBuilder();
+
+            box.IsAddEnabled = GetIsAddEnabled();
+            box.IsDeleteEnabled = BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+            box.ExpectedRowCount = 2;
+            box.NavigationUrls = GetBoxNavigationUrls();
+            box.Options = GetBoxOptions();
+            box.GridDefinition = builder.BuildDefinition();
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box options required for the component to render the list.
+        /// </summary>
+        /// <returns>The options that provide additional details to the block.</returns>
+        private LearningGradingSystemListOptionsBag GetBoxOptions()
+        {
+            var options = new LearningGradingSystemListOptionsBag();
+
+            return options;
+        }
+
+        /// <summary>
+        /// Determines if the add button should be enabled in the grid.
+        /// <summary>
+        /// <returns>A boolean value that indicates if the add button should be enabled.</returns>
+        private bool GetIsAddEnabled()
+        {
+            return BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "LearningGradingSystemId", "((Key))" )
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override IQueryable<LearningGradingSystem> GetListQueryable( RockContext rockContext )
+        {
+            return base.GetListQueryable( rockContext )
+                .Include( a => a.LearningGradingSystemScales )
+                .OrderBy( a => a.Name );
+        }
+
+        /// <inheritdoc/>
+        protected override GridBuilder<LearningGradingSystem> GetGridBuilder()
+        {
+            return new GridBuilder<LearningGradingSystem>()
+                .WithBlock( this )
+                .AddTextField( "idKey", a => a.IdKey )
+                .AddTextField( "name", a => a.Name )
+                .AddTextField( "description", a => a.Description )
+                .AddField( "scalesCount", a => a.LearningGradingSystemScales.Count() )
+                .AddField( "isActive", a => a.IsActive )
+                .AddField( "isSecurityDisabled", a => !a.IsAuthorized( Authorization.ADMINISTRATE, RequestContext.CurrentPerson ) );
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="key">The identifier of the entity to be deleted.</param>
+        /// <returns>An empty result that indicates if the operation succeeded.</returns>
+        [BlockAction]
+        public BlockActionResult Delete( string key )
+        {
+            var entityService = new LearningGradingSystemService( RockContext );
+            var entity = entityService.Get( key, !PageCache.Layout.Site.DisablePredictableIds );
+
+            if ( entity == null )
+            {
+                return ActionBadRequest( $"{LearningGradingSystem.FriendlyTypeName} not found." );
+            }
+
+            if ( !BlockCache.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson ) )
+            {
+                return ActionBadRequest( $"Not authorized to delete ${LearningGradingSystem.FriendlyTypeName}." );
+            }
+
+            if ( !entityService.CanDelete( entity, out var errorMessage ) )
+            {
+                return ActionBadRequest( errorMessage );
+            }
+
+            entityService.Delete( entity );
+            RockContext.SaveChanges();
+
+            return ActionOk();
+        }
+
+        #endregion
+    }
+}
