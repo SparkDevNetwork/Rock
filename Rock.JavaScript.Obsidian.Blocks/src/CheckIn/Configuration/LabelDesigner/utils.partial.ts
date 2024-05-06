b83cd1b037925c1517d8da484dd9909084b2c31a
Daniel Hazelbaker <daniel@triumph.tech>
2024-05-06 15:51:25 -0700

    - Cleaned up dragging and resizing logic.

---
+++ Rock.JavaScript.Obsidian.Blocks/src/CheckIn/Configuration/LabelDesigner/utils.partial.ts
@@ -0,0 +1,249 @@
+import Konva from "@Obsidian/Libs/konva";
+
+// #region Worksurface Utilities
+
+/**
+ * Stashed values used by the TransformHelper.
+ */
+type TransformValues = {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+};
+
+/**
+ * Provides logic for the transformer used in resizing nodes on the canvas.
+ * This clamps the resize operations so they stay within the bounds of the
+ * label. It also enforces minimum size constraints.
+ */
+export class TransformHelper {
+    private readonly stage: Konva.Stage;
+    private readonly transformer: Konva.Transformer;
+    private readonly snapPixel: (pixel: number) => number;
+
+    private original?: TransformValues;
+    private last?: TransformValues;
+    private lastAnchor: string | null = null;
+    private node: Konva.Node | null = null;
+
+    /**
+     * Creates a new TransformHelper instance to handle transform logic for
+     * the worksurface.
+     *
+     * @param stage The stage the transformer is attached to.
+     * @param transformer The transformer.
+     * @param snapPixel The function that will handle pixel snapping.
+     */
+    constructor(stage: Konva.Stage, transformer: Konva.Transformer, snapPixel: (pixel: number) => number) {
+        this.stage = stage;
+        this.transformer = transformer;
+        this.snapPixel = snapPixel;
+
+        this.transformer.on("transformstart", event => this.transformStart(event));
+        this.transformer.on("transform", event => this.transform(event));
+        this.transformer.on("transformend", () => this.transformEnd());
+    }
+
+    /**
+     * Destroys the helper by freeing resources.
+     */
+    public destroy(): void {
+        this.transformer.off("transformstart");
+        this.transformer.off("transform");
+        this.transformer.off("transformstart");
+    }
+
+    /**
+     * Event handler for the transformstart event on the transformer.
+     *
+     * @param event The event.
+     */
+    private transformStart(event: Konva.KonvaEventObject<Event>): void {
+        this.node = event.target;
+        this.lastAnchor = null;
+
+        this.original = {
+            x: this.node.x(),
+            y: this.node.y(),
+            width: this.node.width(),
+            height: this.node.height()
+        };
+
+        this.last = { ...this.original };
+    }
+
+    /**
+     * Event handler for the transformend event on the transformer.
+     */
+    private transformEnd(): void {
+        this.last = undefined;
+        this.lastAnchor = null;
+        this.node = null;
+    }
+
+    /**
+     * Event handler for the transform event on the transformer.
+     *
+     * @param event The event.
+     */
+    private transform(event: Konva.KonvaEventObject<Event>): void {
+        if (!this.node || !this.original || !this.last || event.target.id() !== this.node.id()) {
+            return;
+        }
+
+        if (this.lastAnchor && this.transformer.getActiveAnchor() !== this.lastAnchor) {
+            this.node.setAttrs({
+                ...this.last,
+                scaleX: 1,
+                scaleY: 1
+            });
+
+            this.transformer.stopTransform();
+
+            return;
+        }
+
+        this.lastAnchor = this.transformer.getActiveAnchor();
+
+        // Determine which anchor we are working with.
+        const leftAnchor = ["top-left", "middle-left", "bottom-left"].includes(this.lastAnchor ?? "");
+        const topAnchor = ["top-left", "top-center", "top-right"].includes(this.lastAnchor ?? "");
+        const rightAnchor = ["top-right", "middle-right", "bottom-right"].includes(this.lastAnchor ?? "");
+        const bottomAnchor = ["bottom-left", "bottom-center", "bottom-right"].includes(this.lastAnchor ?? "");
+
+        // Get the new values.
+        let newX = this.node.x();
+        let newY = this.node.y();
+        let newWidth = this.node.width() * this.node.scaleX();
+        let newHeight = this.node.height() * this.node.scaleY();
+
+        // If this is a left side anchor then attempt to snap the x position,
+        // otherwise if it is a right anchor then attempt to snap the width.
+        if (leftAnchor) {
+            const snap = this.snapPixel(newX);
+
+            newWidth += newX - snap;
+            newX = snap;
+        }
+        else if (rightAnchor) {
+            newWidth = this.snapPixel(newX + newWidth) - newX;
+        }
+
+        // If this is a top side anchor then attempt to snap the y position,
+        // otherwise if it is a bottom anchor then attempt to snap the height.
+        if (topAnchor) {
+            const snap = this.snapPixel(newY);
+
+            newHeight += newY - snap;
+            newY = snap;
+        }
+        else if (bottomAnchor) {
+            newHeight = this.snapPixel(newY + newHeight) - newY;
+        }
+
+        // If this is a left anchor then clamp the x position to the left
+        // edge of the label.
+        if (leftAnchor && newX < 0) {
+            newWidth += newX;
+            newX = 0;
+        }
+
+        // If this is a top anchor then clamp the y position to the top edge
+        // of the label.
+        if (topAnchor && newY < 0) {
+            newHeight += newY;
+            newY = 0;
+        }
+
+        // If the new width is too small force it to the minimum size.
+        // Otherwise, clamp the width to the right edge of the label.
+        if (newWidth < 20) {
+            if (leftAnchor) {
+                newX = this.last.x;
+                newWidth = 20;
+            }
+            else if (rightAnchor) {
+                newWidth = 20;
+            }
+        }
+        else if (newWidth + newX > this.stage.width()) {
+            newWidth = this.stage.width() - newX;
+        }
+
+        // If the new height is too small force it to the minimum size.
+        // Otherwise, clamp the height to the bottom edge of the label.
+        if (newHeight < 20) {
+            if (topAnchor) {
+                newY = this.last.y;
+                newHeight = 20;
+            }
+            else if (bottomAnchor) {
+                newHeight = 20;
+            }
+        }
+        else if (newHeight + this.node.y() > this.stage.height()) {
+            newHeight = this.stage.height() - this.node.y();
+        }
+
+        // Set the new values.
+        this.node.setAttrs({
+            x: newX,
+            y: newY,
+            width: newWidth,
+            height: newHeight,
+            scaleX: 1,
+            scaleY: 1
+        });
+
+        // Store the last values we set for later use.
+        this.last = {
+            x: newX,
+            y: newY,
+            width: newWidth,
+            height: newHeight
+        };
+    }
+}
+
+/**
+ * Creates a function that will handle node drag bounding operations for the
+ * stage. This ensures drag operations stay within the label and also handles
+ * snapping.
+ *
+ * @param stage The stage the function will operate on.
+ * @param snapPixel The function that will handle pixel snapping.
+ * @returns A function that can be passed to the `dragBoundFunc` property.
+ */
+export function useNodeDragBoundFunc(stage: Konva.Stage, snapPixel: (pixel: number) => number): (this: Konva.Node, pos: Konva.Vector2d) => Konva.Vector2d {
+    function nodeDragBoundFunc(this: Konva.Node, pos: Konva.Vector2d): Konva.Vector2d {
+        if (!stage) {
+            return pos;
+        }
+
+        // Get the new x and y positions, clamped to the left and top edges
+        // of the label.
+        let newX = pos.x <= 0 ? 0 : snapPixel(pos.x);
+        let newY = pos.y <= 0 ? 0 : snapPixel(pos.y);
+
+        // Clamp the x position so it doesn't extend past the right edge of
+        // the label.
+        if (newX >= stage.width() - this.width()) {
+            newX = stage.width() - this.width();
+        }
+
+        // Clamp the y position so it doesn't extend past the bottom edge of
+        // the label.
+        if (newY >= stage.height() - this.height()) {
+            newY = stage.height() - this.height();
+        }
+
+        return {
+            x: newX,
+            y: newY
+        };
+    }
+
+    return nodeDragBoundFunc;
+}
+// #endregion
