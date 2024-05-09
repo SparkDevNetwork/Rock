09a520a71a4318f7b285c5bc4a88b89b5c0e8b7f
Jesse Colburn <unlearnd@gmail.com>
2024-05-09 12:25:33 -0700

    - Fixed compiler warnings and code generation.

--- Rock.ViewModels/Blocks/Event/RegistrationEntry/RegistrationEntryPaymentPlanBag.cs
+++ Rock.ViewModels/Blocks/Event/RegistrationEntry/RegistrationEntryPaymentPlanBag.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
 
 namespace Rock.ViewModels.Blocks.Event.RegistrationEntry
 {
@@ -43,11 +59,35 @@ namespace Rock.ViewModels.Blocks.Event.RegistrationEntry
         /// The start date of the recurring payments.
         /// </value>
         public DateTimeOffset StartDate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string TransactionFrequencyText { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public decimal TotalPlannedAmount { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public decimal RemainingPlannedAmount { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int RemainingNumberOfPayments { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public int ProcessedNumberOfPayments { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public decimal ProcessedPlannedAmount { get; set; }
     }
 }
