12491c85c9711687302ff8e7a265f318d9e4d397
Matthew Marron <memx1031@gmail.com>
2024-06-11 23:31:37 -0400

    add defensive check to utility addQuickReturn function

--- Rock.JavaScript.Obsidian/Framework/Utility/page.ts
+++ Rock.JavaScript.Obsidian/Framework/Utility/page.ts
@@ -173,6 +173,8 @@ export function addQuickReturn(title: string, section: string, sectionOrder?: nu
             addQuickReturn: (type: string, typeOrder: number, itemName: string) => void
         }
     }
-
-    (window["Rock"] as IRock).personalLinks.addQuickReturn(section, sectionOrder ?? 0, title);
+    let rock = window["Rock"] as IRock;
+    if (rock && rock.personalLinks) {
+        rock.personalLinks.addQuickReturn(section, sectionOrder ?? 0, title);
+    }
 }
