a3c7a453294ff9005cab0fb5720367228a2d35da
jonrob <jonathan@triumph.tech>
2024-06-13 09:53:52 -0700

    - (CRM) Added Giving Configuration Obsidian block

---
+++ Rock.JavaScript.Obsidian/Framework/ViewModels/Blocks/Crm/PersonDetail/GivingConfiguration/index.ts
@@ -0,0 +1,29 @@
+import { AccountSummaryBag } from "./accountSummaryBag";
+import { ContributionStatementBag } from "./contributionStatementBag";
+import { FinancialAccountBag } from "./financialAccountBag";
+import { FinancialPaymentDetailBag } from "./financialPaymentDetailBag";
+import { FinancialPersonSavedAccountBag } from "./financialPersonSavedAccountBag";
+import { FinancialPledgeBag } from "./financialPledgeBag";
+import { FinancialScheduledTransactionBag } from "./financialScheduledTransactionBag";
+import { FinancialScheduledTransactionDetailBag } from "./financialScheduledTransactionDetailBag";
+import { GivingConfigurationInitializationBag } from "./givingConfigurationInitializationBag";
+import { GivingConfigurationOptionsBag } from "./givingConfigurationOptionsBag";
+import { TextToGiveSettingsBag } from "./textToGiveSettingsBag";
+import { PersonAliasBag } from "./personAliasBag";
+import { PersonBag } from "./personBag";
+
+export {
+    AccountSummaryBag,
+    ContributionStatementBag,
+    FinancialPaymentDetailBag,
+    GivingConfigurationInitializationBag,
+    GivingConfigurationOptionsBag,
+    TextToGiveSettingsBag,
+    FinancialAccountBag,
+    FinancialPersonSavedAccountBag,
+    FinancialPledgeBag,
+    FinancialScheduledTransactionBag,
+    FinancialScheduledTransactionDetailBag,
+    PersonAliasBag,
+    PersonBag
+};
