f46b63fb0eb37d3bde4a44cf155fe0e8488bcbef
Daniel Hazelbaker <daniel@triumph.tech>
2024-06-21 08:12:19 -0700

    - Added initialization logic for AttendanceLabel.

--- Rock/CheckIn/v2/Labels/AttendanceLabel.cs
+++ Rock/CheckIn/v2/Labels/AttendanceLabel.cs
@@ -18,6 +18,7 @@
 using System;
 using System.Collections.Generic;
 
+using Rock.Data;
 using Rock.Model;
 using Rock.ViewModels.CheckIn;
 using Rock.Web.Cache;
@@ -100,5 +101,62 @@ namespace Rock.CheckIn.v2.Labels
         /// before the check-in session was started.
         /// </summary>
         public List<AchievementBag> PreviouslyCompletedAchievements { get; set; }
+
+        /// <summary>
+        /// Creates a new, empty, instance of the AttendanceLabel. This would
+        /// primarily be used by unit tests.
+        /// </summary>
+        internal AttendanceLabel()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttendanceLabel"/> class.
+        /// </summary>
+        /// <param name="attendance">The attendance object to initialize from.</param>
+        /// <param name="rockContext">The RockContext for database operations.</param>
+        public AttendanceLabel( Attendance attendance, RockContext rockContext )
+            : this( attendance, null, rockContext )
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttendanceLabel"/> class.
+        /// </summary>
+        /// <param name="attendance">The attendance object to initialize from.</param>
+        /// <param name="attendanceBag">The optional recorded attendance object that provides additional attendance details.</param>
+        /// <param name="rockContext">The RockContext for database operations.</param>
+        public AttendanceLabel( Attendance attendance, RecordedAttendanceBag attendanceBag, RockContext rockContext )
+        {
+            if ( attendance == null )
+            {
+                throw new ArgumentNullException( nameof( attendance ) );
+            }
+
+            var groupCache = attendance.Occurrence.GroupId.HasValue
+                ? GroupCache.Get( attendance.Occurrence.GroupId.Value, rockContext )
+                : null;
+            var locationCache = attendance.Occurrence.LocationId.HasValue
+                ? NamedLocationCache.Get( attendance.Occurrence.LocationId.Value, rockContext )
+                : null;
+            var scheduleCache = attendance.Occurrence.ScheduleId.HasValue
+                ? NamedScheduleCache.Get( attendance.Occurrence.ScheduleId.Value, rockContext )
+                : null;
+            var areaCache = GroupTypeCache.Get( groupCache.GroupTypeId, rockContext );
+
+            Area = areaCache;
+            EndDateTime = attendance.EndDateTime;
+            Group = groupCache;
+            GroupMembers = new List<GroupMember>(); // TODO: Need to fill this in, probably via lazy load.
+            InProgressAchievements = attendanceBag?.InProgressAchievements ?? new List<AchievementBag>();
+            IsFirstTime = attendance.IsFirstTime ?? false;
+            JustCompletedAchievements = attendanceBag?.JustCompletedAchievements ?? new List<AchievementBag>();
+            Location = locationCache;
+            Person = attendance.PersonAlias?.Person;
+            PreviouslyCompletedAchievements = attendanceBag?.PreviouslyCompletedAchievements ?? new List<AchievementBag>();
+            Schedule = scheduleCache;
+            SecurityCode = attendance.AttendanceCode?.Code ?? string.Empty;
+            StartDateTime = attendance.StartDateTime;
+        }
     }
 }
