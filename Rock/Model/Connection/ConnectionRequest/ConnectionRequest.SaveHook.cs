bf720145a8200bd9eb9145c03f52a5f6e846d265
kylehenning <kylebhenning@gmail.com>
2024-07-12 16:23:03 -0700

    - Added post-save logic for adding and deleting a Connection Request to maintain Order.

--- Rock/Model/Connection/ConnectionRequest/ConnectionRequest.SaveHook.cs
+++ Rock/Model/Connection/ConnectionRequest/ConnectionRequest.SaveHook.cs
@@ -173,6 +173,33 @@ namespace Rock.Model
                     rockContext.SaveChanges();
                 }
 
+                switch ( State )
+                {
+                    case EntityContextState.Added:
+                        {
+                            var connectionRequestService = new ConnectionRequestService( rockContext );
+                            var requestsOfStatus = connectionRequestService.Queryable()
+                            .Where( r =>
+                                r.ConnectionStatusId == connectionRequest.ConnectionStatusId &&
+                                r.ConnectionOpportunityId == connectionRequest.ConnectionOpportunityId &&
+                                r.Id != connectionRequest.Id );
+                            rockContext.BulkUpdate( requestsOfStatus, r => new ConnectionRequest { Order = r.Order + 1, ModifiedDateTime = r.ModifiedDateTime } );
+                            break;
+                        }
+                    case EntityContextState.Deleted:
+                        {
+                            var connectionRequestService = new ConnectionRequestService( rockContext );
+                            var requestsOfStatus = connectionRequestService.Queryable()
+                            .Where( r =>
+                                r.ConnectionStatusId == connectionRequest.ConnectionStatusId &&
+                                r.ConnectionOpportunityId == connectionRequest.ConnectionOpportunityId &&
+                                r.Order > connectionRequest.Order &&
+                                r.Id != connectionRequest.Id );
+                            rockContext.BulkUpdate( requestsOfStatus, r => new ConnectionRequest { Order = r.Order - 1, ModifiedDateTime = r.ModifiedDateTime } );
+                            break;
+                        }
+                }
+
                 var connectionStatusAutomationsQuery = new ConnectionStatusAutomationService( rockContext ).Queryable().Where( a => a.SourceStatusId == Entity.ConnectionStatusId );
 
                 if ( this.Entity._runAutomationsInPostSaveChanges && connectionStatusAutomationsQuery.Any() )
