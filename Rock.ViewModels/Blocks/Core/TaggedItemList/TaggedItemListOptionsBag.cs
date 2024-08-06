7166ed987f62dfc4d2d69bba7610d25530dc6c6a
PraveenMathew92 <pmathew4@asu.edu>
2024-08-06 15:23:40 -0700

    - Fixed missing comments

--- Rock.ViewModels/Blocks/Core/TaggedItemList/TaggedItemListOptionsBag.cs
+++ Rock.ViewModels/Blocks/Core/TaggedItemList/TaggedItemListOptionsBag.cs
@@ -19,18 +19,40 @@ namespace Rock.ViewModels.Blocks.Core.TaggedItemList
 {
     public class TaggedItemListOptionsBag
     {
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
         public string Title { get; set; }
 
+
+        /// <summary>
+        /// Gets the tag Name.
+        /// </summary>
         public string TagName { get; set; }
 
+        /// <summary>
+        /// Gets the Entity Type Name.
+        /// </summary>
         public string EntityTypeName { get; set; }
 
+        /// <summary>
+        /// Gets the Entity Type GUID.
+        /// </summary>
         public string EntityTypeGuid { get; set; }
 
+        /// <summary>
+        /// Gets if the Tag is a Person Tag.
+        /// </summary>
         public bool IsPersonTag { get; internal set; }
 
+        /// <summary>
+        /// Gets if the block needs to be hidden.
+        /// </summary>
         public bool IsBlockHidden { get; set; }
 
+        /// <summary>
+        /// Gets the Tag Id.
+        /// </summary>
         public int TagId { get; set; }
     }
 }
