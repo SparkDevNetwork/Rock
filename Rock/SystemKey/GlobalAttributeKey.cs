64245f875d26f8eca62561cd18e2cd25dd8db4b6
PraveenMathew92 <pmathew4@asu.edu>
2024-09-16 09:28:10 -0700

    - Added logic to filter out exceptions to be logged to the Exception Log

---
+++ Rock/SystemKey/GlobalAttributeKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.SystemKey
+{
+    public class GlobalAttributeKey
+    {
+
+        /// <summary>
+        /// List of Exceptions to be filtered out from the Exception Log
+        /// </summary>
+        public const string EXCEPTION_LOG_FILTER = "core_ExceptionLogFilter";
+    }
+}
