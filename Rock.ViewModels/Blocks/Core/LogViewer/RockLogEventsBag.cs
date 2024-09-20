b5b536b908d9ee9d9bbe01244d16fed4629b365b
PraveenMathew92 <pmathew4@asu.edu>
2024-09-20 13:30:23 -0700

    - Added Log Viewer (Logs) Obsidian block.

---
+++ Rock.ViewModels/Blocks/Core/LogViewer/RockLogEventsBag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Rock.ViewModels.Blocks.Core.LogViewer
+{
+    public class RockLogEventsBag
+    {
+        /// <summary>
+        /// Gets or sets the DateTime
+        /// </summary>
+        public DateTimeOffset DateTime { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Level { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Domain { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Exception { get; set; }
+    }
+}
