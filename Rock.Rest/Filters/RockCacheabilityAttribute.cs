b56e9c5c014d5aa31c35800e9e87989cb4340dbe
Daniel Hazelbaker <daniel@triumph.tech>
2024-09-27 08:19:01 -0700

    - Fixed secondary null reference exception during API if the API threw an exception and was configured to send cache response headers.

--- Rock.Rest/Filters/RockCacheabilityAttribute.cs
+++ Rock.Rest/Filters/RockCacheabilityAttribute.cs
@@ -45,6 +45,14 @@ namespace Rock.Rest.Filters
         {
             base.OnActionExecuted( actionExecutedContext );
 
+            // If an exception is thrown by the API handler then the Response
+            // object will be null. In that case we can't modify the response
+            // headers to add the cache information.
+            if ( actionExecutedContext.Response == null )
+            {
+                return;
+            }
+
             var reflectedHttpActionDescriptor = ( ReflectedHttpActionDescriptor ) actionExecutedContext.ActionContext.ActionDescriptor;
             var actionMethod = actionExecutedContext.Request.Method.Method;
             var controller = actionExecutedContext.ActionContext.ActionDescriptor.ControllerDescriptor;
