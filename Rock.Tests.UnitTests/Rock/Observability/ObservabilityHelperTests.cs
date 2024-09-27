4da5ea692cdca9696b9b0c61c2fb2ee0ef5ef39b
Daniel Hazelbaker <daniel@triumph.tech>
2024-09-26 17:01:07 -0700

    - Fix unit tests to provide node name in mock RockApp.

--- Rock.Tests.UnitTests/Rock/Observability/ObservabilityHelperTests.cs
+++ Rock.Tests.UnitTests/Rock/Observability/ObservabilityHelperTests.cs
@@ -3,6 +3,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Rock.Observability;
+using Rock.Tests.Shared;
 
 namespace Rock.Tests.UnitTests.Rock.Observability
 {
@@ -17,45 +18,48 @@ namespace Rock.Tests.UnitTests.Rock.Observability
         [TestMethod]
         public void RootActivityDoesNotExceedCap()
         {
-            // The listener is removed from the activity source when it is disposed.
-            using ( var listener = new ActivityListener() )
+            using ( var rockAppScope = TestHelper.CreateScopedRockApp() )
             {
-                var childActivityCount = 0;
-                var source = RockActivitySource.ActivitySource;
-                var expectedCount = ObservabilityHelper.SpanCountLimit - 1;
-
-                listener.ActivityStarted = activity =>
+                // The listener is removed from the activity source when it is disposed.
+                using ( var listener = new ActivityListener() )
                 {
-                    if ( activity.Parent != null )
+                    var childActivityCount = 0;
+                    var source = RockActivitySource.ActivitySource;
+                    var expectedCount = ObservabilityHelper.SpanCountLimit - 1;
+
+                    listener.ActivityStarted = activity =>
                     {
-                        childActivityCount++;
-                    }
-                };
-                listener.ShouldListenTo = src => src == source;
-                listener.SampleUsingParentId = ( ref ActivityCreationOptions<string> _ ) => ActivitySamplingResult.AllData;
-                listener.Sample = ( ref ActivityCreationOptions<ActivityContext> _ ) => ActivitySamplingResult.AllData;
-
-                ActivitySource.AddActivityListener( listener );
-
-                using ( var rootActivity = ObservabilityHelper.StartActivity( "Root Activity" ) )
-                {
-                    Assert.IsNotNull( rootActivity, "Root activity was null, configuration failed." );
-
-                    for ( int c = 0; c < 15; c++ )
-                    {
-                        using ( var childActivity = ObservabilityHelper.StartActivity( $"Child Activity {c}" ) )
+                        if ( activity.Parent != null )
                         {
-                            for ( int gc = 0; gc < 1_000; gc++ )
+                            childActivityCount++;
+                        }
+                    };
+                    listener.ShouldListenTo = src => src == source;
+                    listener.SampleUsingParentId = ( ref ActivityCreationOptions<string> _ ) => ActivitySamplingResult.AllData;
+                    listener.Sample = ( ref ActivityCreationOptions<ActivityContext> _ ) => ActivitySamplingResult.AllData;
+
+                    ActivitySource.AddActivityListener( listener );
+
+                    using ( var rootActivity = ObservabilityHelper.StartActivity( "Root Activity" ) )
+                    {
+                        Assert.IsNotNull( rootActivity, "Root activity was null, configuration failed." );
+
+                        for ( int c = 0; c < 15; c++ )
+                        {
+                            using ( var childActivity = ObservabilityHelper.StartActivity( $"Child Activity {c}" ) )
                             {
-                                using ( var grandChildActivity = ObservabilityHelper.StartActivity( $"Grand Child Activity {c}.{gc}" ) )
+                                for ( int gc = 0; gc < 1_000; gc++ )
                                 {
+                                    using ( var grandChildActivity = ObservabilityHelper.StartActivity( $"Grand Child Activity {c}.{gc}" ) )
+                                    {
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    Assert.AreEqual( expectedCount, childActivityCount, "Incorrect child activity count." );
-                    Assert.AreEqual( 15_015, rootActivity.GetTagItem( "rock.descendant_count" ), "Incorrect descendant count." );
+                        Assert.AreEqual( expectedCount, childActivityCount, "Incorrect child activity count." );
+                        Assert.AreEqual( 15_015, rootActivity.GetTagItem( "rock.descendant_count" ), "Incorrect descendant count." );
+                    }
                 }
             }
         }
