59caf84514af9bfff266a71541ba01621f9e0370
Joshua Henninger <joshua@triumph.tech>
2024-10-09 15:53:53 -0700

    - WIP: Moved common CSS classes to utils.partial.ts.

--- Rock.JavaScript.Obsidian.Blocks/src/Communication/CommunicationEntryWizard/utils.partial.ts
+++ Rock.JavaScript.Obsidian.Blocks/src/Communication/CommunicationEntryWizard/utils.partial.ts
@@ -104,6 +104,15 @@ export function getComponentTitle(componentTypeName: EditorComponentTypeName): s
     }
 }
 
+/** Add this CSS class to any temporary (runtime) element that should be stripped out when the HTML is retrieved via getHtml(). */
+export const rockRuntimeElementCssClass = "rock-runtime-element" as const;
+
+/** Add this CSS class prefix to any temporary CSS class that should be stripped out when the HTML is retrieved via getHTML(). */
+export const rockRuntimeClassCssClassPrefix = "rock-runtime-class-" as const;
+
+/** Add this CSS class to any element that should be editable inline when selected in the email designer. */
+export const rockRuntimeClassContentEditable = `${rockRuntimeClassCssClassPrefix}content-editable` as const;
+
 export function createComponentElementPlaceholder(document: Document, componentTypeName: EditorComponentTypeName): HTMLElement {
     let componentTypeCssClass: string;
     if (["one-column-section", "two-column-section", "three-column-section"].includes(componentTypeName)) {
@@ -135,7 +144,7 @@ export function createComponentElement(document: Document, componentTypeName: Ed
 
     switch (componentTypeName) {
         case "title":
-            componentElement.innerHTML = `<h1 class="rock-runtime-class-contenteditable">Title</h1>`;
+            componentElement.innerHTML = `<h1 class="${rockRuntimeClassContentEditable}">Title</h1>`;
             break;
         case "video":
             componentElement.innerHTML = `<a href=""><img src="/Assets/Images/video-placeholder.jpg" style="width: 100%;" data-imgcsswidth="full"></a>`;
@@ -150,7 +159,7 @@ export function createComponentElement(document: Document, componentTypeName: Ed
                                 <tbody>
                                     <tr>
                                         <td align="center" valign="middle" class="button-content" style="border-radius: 3px;background-color:#2baadf">
-                                            <a class="button-link rock-runtime-class-contenteditable" title="Push Me" href="http://" target="_blank" rel="noopener noreferrer" style="display: inline-block; font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #ffffff;background-color: #2baadf; padding: 15px; border: 1px solid #2baadf; border-radius: 3px;">Push Me</a>
+                                            <a class="button-link ${rockRuntimeClassContentEditable}" title="Push Me" href="http://" target="_blank" rel="noopener noreferrer" style="display: inline-block; font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #ffffff;background-color: #2baadf; padding: 15px; border: 1px solid #2baadf; border-radius: 3px;">Push Me</a>
                                         </td>
                                     </tr>
                                 </tbody>
@@ -161,21 +170,21 @@ export function createComponentElement(document: Document, componentTypeName: Ed
             </table>`;
             break;
         case "text":
-            componentElement.classList.add("rock-runtime-class-contenteditable");
+            componentElement.classList.add(rockRuntimeClassContentEditable);
             componentElement.innerHTML = `<p>Can't wait to see what you have to say!</p>`;
             break;
         case "divider":
             componentElement.innerHTML = `<hr />`;
             break;
         case "message":
-            componentElement.classList.add("rock-runtime-class-contenteditable");
+            componentElement.classList.add(rockRuntimeClassContentEditable);
             componentElement.innerText = "Message";
             break;
         case "image":
             componentElement.innerHTML = `<img src="/Assets/Images/image-placeholder.jpg" style="width: 100%;" data-imgcsswidth="full" alt="">`;
             break;
         case "code":
-            componentElement.classList.add("rock-runtime-class-contenteditable");
+            componentElement.classList.add(rockRuntimeClassContentEditable);
             componentElement.innerHTML = `Add your code here...`;
             break;
         case "rsvp":
@@ -191,7 +200,7 @@ export function createComponentElement(document: Document, componentTypeName: Ed
                                                 <tbody>
                                                     <tr>
                                                         <td align="center" valign="middle" class="rsvp-accept-content" style="font-family: Arial; font-size: 16px; padding: 15px;">
-                                                            <a class="rsvp-accept-link rock-runtime-class-contenteditable" title="Accept" href="http://" target="_blank" rel="noopener noreferrer" style="font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #FFFFFF;">Accept</a>
+                                                            <a class="rsvp-accept-link ${rockRuntimeClassContentEditable}" title="Accept" href="http://" target="_blank" rel="noopener noreferrer" style="font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #FFFFFF;">Accept</a>
                                                         </td>
                                                     </tr>
                                                 </tbody>
@@ -202,7 +211,7 @@ export function createComponentElement(document: Document, componentTypeName: Ed
                                                 <tbody>
                                                     <tr>
                                                         <td align="center" valign="middle" class="rsvp-decline-content" style="font-family: Arial; font-size: 16px; padding: 15px;">
-                                                            <a class="rsvp-decline-link rock-runtime-class-contenteditable" title="Decline" href="http://" target="_blank" style="font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #FFFFFF;">Decline</a>
+                                                            <a class="rsvp-decline-link ${rockRuntimeClassContentEditable}" title="Decline" href="http://" target="_blank" style="font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #FFFFFF;">Decline</a>
                                                         </td>
                                                     </tr>
                                                 </tbody>
