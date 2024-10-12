480d0333f1f7b0d59092d85de1e008db9aa08a84
Kwame_Agyei <kwame.ajyei@techvision.global>
2024-10-12 09:07:14 +0000

    + (Lava) Fixed issue with Lava Shortcodes setting the value of only the first parameter when a shortcode with multiple parameters is used. (Fixes #6008)

--- Rock.Lava/Core/Shortcodes/DynamicShortcode.cs
+++ Rock.Lava/Core/Shortcodes/DynamicShortcode.cs
@@ -514,7 +514,6 @@ namespace Rock.Lava
                     if ( scopeObject != null )
                     {
                         parameters.AddOrReplace( itemParts[0].Trim().ToLower(), scopeObject );
-                        break;
                     }
                 }
             }
