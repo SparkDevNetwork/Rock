ac7010114cd0ecaa42fd1a2bfa2e4717fe5984df
nairdo <nairdo@gmail.com>
2024-12-06 09:06:21 -0700

    - Updated verbiage for the amount sensitivity scale in Giving Alert configuration to clarify that leaving it blank will ignore sensitivity criteria.

--- Rock/Model/Finance/FinancialTransactionAlertType/FinancialTransactionAlertType.Logic.cs
+++ Rock/Model/Finance/FinancialTransactionAlertType/FinancialTransactionAlertType.Logic.cs
@@ -53,17 +53,17 @@ namespace Rock.Model
                 // Gratitude. Larger amount than usual.
                 return @"
  <p>
-    For gratitude alerts, the amount sensitivity scale determines when a 'larger amount than usual' gift triggers the alert.
+    For gratitude alerts, the amount sensitivity scale determines when a 'larger-than-usual' gift triggers the alert. Leave blank to ignore the sensitivity criteria.
 </p>
 <p>
-    Typical Values are shown below.
+    Typical values are shown below.
     <ul>
         <li>2 (Aggressive) - This would alert when a gift was more than 2 times the interquartile range (IQR) from their median gift amount. For a bi-weekly giver with a median gift of $400 and an IQR of $65, this alert would be generated if a gift of $530 was received.</li>
         <li>3 (Normal) - This would alert when a gift was more than 3 times the interquartile range (IQR) from their median gift amount. For a bi-weekly giver with a median gift of $400 and an IQR of $65, this alert would be generated if a gift of $595 was received.</li>
     </ul>
 </p>
 <p>
-    In the event that there is a very consistent giver — every gift is the exact same amount — we use a fallback value. The fallback amount sensitivity is calculated as 15% of the median gift amount.
+    In cases where the giver is very consistent — every gift is the exact same amount — a fallback value is used. The fallback sensitivity is calculated as 15% of the median gift amount.
 </p>";
             }
             else
@@ -72,17 +72,17 @@ namespace Rock.Model
                 // Follow-Up. The logic is flipped so the sensitivity is used to determine smaller than usual amounts."
                 return @"
  <p>
-    For follow-up alerts, the amount sensitivity scale determines when a 'smaller amount than usual' gift triggers the alert.
+    For follow-up alerts, the amount sensitivity scale determines when a 'smaller-than-usual' gift triggers the alert.
 </p>
 <p>
-    Typical Values are shown below.
+    Typical values are shown below.
     <ul>
         <li>2 (Aggressive) - This would alert when a gift is 2 times or more of the interquartile range (IQR) smaller than their median gift amount. For a bi-weekly giver with a median gift of $400 and an IQR of $65, this alert would be generated if a gift of $270 was received.</li>
         <li>3 (Normal) - This would alert when a gift is 3 times or more of the interquartile range (IQR) smaller than their median gift amount. For a bi-weekly giver with a median gift of $400 and an IQR of $65, this alert would be generated if a gift of $205 was received.</li>
     </ul>
 </p>
 <p>
-    In the event that there is a very consistent giver — every gift is the exact same amount — we use a fallback value. The fallback amount sensitivity is calculated as 15% of the median gift amount.
+    In cases where the giver is very consistent — every gift is the exact same amount — a fallback value is used. The fallback sensitivity is calculated as 15% of the median gift amount.
 </p>";
             }
         }
@@ -121,7 +121,7 @@ namespace Rock.Model
     For a follow-up alert, the frequency sensitivity scale determines when to generate an alert if a gift hasn't been received for a while, or was received later than usual.
 </p>
 <p>
-    Typical Values are shown below.
+    Typical values are shown below.
     <ul>
                              
         <li>2 (Aggressive) - This would alert when the frequency of a gift is later than 2 times of the standard deviation. For a bi-weekly giver with a mean of 14 days and a standard deviation of 3.8, this alert would be generated if no gift was received within 22 days since their last gift.</li>
@@ -129,7 +129,7 @@ namespace Rock.Model
     </ul>
 </p>
 <p>
-    In the event that there is a very consistent giver — every gift is the same number of days apart — we use a fallback value. The fallback frequency sensitivity is calculated as 15% of the average days between gifts. If that value is less than 3 days, then we again fallback to 3 days.
+    In cases where the giver is very consistent — every gift is the same number of days apart — a fallback value is used. The fallback frequency sensitivity is calculated as 15% of the average days between gifts. If that value is less than 3 days, then we again fallback to 3 days.
 </p>
 ";
             }
@@ -140,17 +140,17 @@ namespace Rock.Model
                 // Normal is 'significantly earlier than usual'
                 return @"
 <p>
-    For a gratitude alert, the frequency sensitivity scale determines when to generate an alert if a gift is earlier than usual.
+    For a gratitude alert, the frequency sensitivity scale determines when to generate an alert if a gift is earlier than usual. Leave blank to ignore the sensitivity criteria.
 </p>
 <p>
-    Typical Values are shown below.
+    Typical values are shown below.
     <ul>
         <li>2 (Aggressive) - This would alert when the frequency of a gift is earlier than 2 times the standard deviation. For a bi-weekly giver with a mean of 14 days and a standard deviation of 3.8, an alert would be generated for a gift if it has been fewer than 10 days since their last gift. (4 days early).</li>
         <li>3 (Normal) -     This would alert when the frequency of a gift is earlier than 3 times the standard deviation. For a bi-weekly giver with a mean of 14 days and a standard deviation of 3.8, an alert would be generated for a gift if it has been fewer than 7 days since their last gift. (7 days early).</li>
     </ul>
 </p>
 <p>
-    In the event that there is a very consistent giver — every gift is the same number of days apart — we use a fallback value. The fallback frequency sensitivity is calculated as 15% of the average days between gifts. If that value is less than 3 days, then we again fallback to 3 days.
+    In cases where the giver is very consistent — every gift is the same number of days apart — a fallback value is used. The fallback frequency sensitivity is calculated as 15% of the average days between gifts. If that value is less than 3 days, then we again fallback to 3 days.
 </p>
 ";
             }
