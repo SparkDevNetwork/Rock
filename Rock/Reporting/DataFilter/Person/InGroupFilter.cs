4354034bcc1631df99d9a286d5835e9d3dfe0260
Daniel Hazelbaker <daniel@triumph.tech>
2025-02-03 10:35:13 -0700

    - Disable InGroupFilter until we know how to handle postback lookups.

--- Rock/Reporting/DataFilter/Person/InGroupFilter.cs
+++ Rock/Reporting/DataFilter/Person/InGroupFilter.cs
@@ -66,7 +66,7 @@ namespace Rock.Reporting.DataFilter.Person
         }
 
         /// <inheritdoc/>
-        public override string ObsidianFileUrl => "~/Obsidian/Reporting/DataFilters/Person/inGroupFilter.obs";
+        public override string ObsidianFileUrl => null;//"~/Obsidian/Reporting/DataFilters/Person/inGroupFilter.obs";
 
         #endregion
 
