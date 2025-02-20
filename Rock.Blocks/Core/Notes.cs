dcf8f2f09b34fc7d4e6e707b597df7d8032a1158
Joe Zim <contact@joezimjs.com>
2025-02-20 15:48:40 -0600

    Revert "+ (Core) Fixed issue with the obsidian Notes block not correctly rendering markdown. (Fixes #6159)"
    This reverts commit 3a010051caf95324a9c2fdf78ff8f7b23616676d.
    
    # Conflicts:
    #	Rock.Blocks/Core/Notes.cs

--- Rock.Blocks/Core/Notes.cs
+++ Rock.Blocks/Core/Notes.cs
@@ -307,7 +307,7 @@ namespace Rock.Blocks.Core
                 IdKey = note.IdKey,
                 NoteTypeIdKey = noteType.IdKey,
                 Caption = note.Caption,
-                Text = note.Text?.ConvertMarkdownToHtml(),
+                Text = note.Text,
                 ApprovalStatus = ( noteType.RequiresApprovals ) ? note.ApprovalStatus : NoteApprovalStatus.Approved,
                 AnchorId = note.NoteAnchorId,
                 IsAlert = note.IsAlert ?? false,
