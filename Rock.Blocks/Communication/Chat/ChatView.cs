f8ddd899d37ade7304c92aac284b9658bd3fcb71
Braden Cohen <braden@triumph.tech>
2025-03-26 14:27:40 -0700

    - Add block actions to toggle additional chat settings in your profile.

--- Rock.Blocks/Communication/Chat/ChatView.cs
+++ Rock.Blocks/Communication/Chat/ChatView.cs
@@ -5,13 +5,14 @@ using Rock.SystemGuid;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using Rock.Attribute;
+using Rock.ViewModels.Blocks.Communication.Chat.ChatView;
 
 namespace Rock.Blocks.Communication.Chat
 {
     [DisplayName( "Chat View" )]
     [Category( "Communication" )]
     [Description( "Displays a chat interface utilizing the Rock StreamChat integration." )]
-    [SupportedSiteTypes( SiteType.Mobile | SiteType.Web )]
+    [SupportedSiteTypes( SiteType.Mobile, SiteType.Web )]
     [IconCssClass( "fa fa-comments" )]
 
     [Rock.SystemGuid.EntityTypeGuid( "B3D6F875-1589-4543-9E76-5C41201B465B" )]
@@ -20,6 +21,50 @@ namespace Rock.Blocks.Communication.Chat
     {
         #region Block Actions
 
+        [BlockAction]
+        public BlockActionResult GetChatPersonSettings()
+        {
+            var person = RequestContext.CurrentPerson;
+
+            if ( person == null )
+            {
+                return ActionUnauthorized( "You must be logged in to view your chat settings." );
+            }
+
+            // These settings are a little untraditional -- the properties on the Person
+            // are nullable. If they are null, Stream users are synced with the default.
+            // If they are not null, the user has overridden the default.
+            // The mobile shell only updates these properties on user interaction,
+            // so we need to send down the default values in case the user has not
+            // interacted with the chat settings yet (or is just riding the 'default' wave).
+            var chatConfig = ChatHelper.GetChatConfiguration();
+            var isOpenDirectMessageAllowedDefaultValue = chatConfig.IsOpenDirectMessagingAllowed;
+            var isProfilePublicDefaultValue = chatConfig.AreChatProfilesVisible;
+
+            return ActionOk( new
+            {
+                IsChatOpenDirectMessageAllowed = person.IsChatOpenDirectMessageAllowed ?? isOpenDirectMessageAllowedDefaultValue,
+                IsChatProfilePublic = person.IsChatProfilePublic ?? isProfilePublicDefaultValue
+            } );
+        }
+
+        [BlockAction]
+        public BlockActionResult UpdateChatPersonSettings( ChatPersonSettingsBag options )
+        {
+            var person = RequestContext.CurrentPerson;
+
+            if ( person == null )
+            {
+                return ActionUnauthorized( "You must be logged in to update your chat settings." );
+            }
+
+            person.IsChatOpenDirectMessageAllowed = options.IsChatOpenDirectMessageAllowed;
+            person.IsChatProfilePublic = options.IsChatProfilePublic;
+            RockContext.SaveChanges();
+
+            return ActionOk();
+        }
+        
         /// <summary>
         /// Gets the chat data.
         /// </summary>
@@ -27,9 +72,9 @@ namespace Rock.Blocks.Communication.Chat
         [BlockAction]
         public async Task<BlockActionResult> GetChatData()
         {
-            if( RequestContext.CurrentPerson == null )
+            if ( RequestContext.CurrentPerson == null )
             {
-                return ActionUnauthorized("You must be logged in to view chat.");
+                return ActionUnauthorized( "You must be logged in to view chat." );
             }
 
             var person = RequestContext.CurrentPerson;
@@ -45,7 +90,7 @@ namespace Rock.Blocks.Communication.Chat
                 } );
             }
         }
-        
+
         #endregion
     }
 }
---
+++ Rock.ViewModels/Blocks/Communication/Chat/ChatView/ChatPersonSettingsBag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock.ViewModels.Blocks.Communication.Chat.ChatView
+{
+    /// <summary>
+    /// A bag of settings for a chat person.
+    /// </summary>
+    public class ChatPersonSettingsBag
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the chat is open direct message allowed.
+        /// </summary>
+        public bool IsChatOpenDirectMessageAllowed { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the chat profile is public.
+        /// </summary>
+        public bool IsChatProfilePublic { get; set; }
+    }
+}
