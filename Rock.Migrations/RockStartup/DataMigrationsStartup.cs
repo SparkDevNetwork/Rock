f3b6f435d5425ef37e27bccfe60d676a4c398af7
Jason Hendee <jason@triumph.tech>
2025-04-01 11:25:20 -0700

    - Improved the performance of communication sending by including PersonAliasId in the IX_CommunicationId CommunicationRecipient index.

--- Rock.Migrations/RockStartup/DataMigrationsStartup.cs
+++ Rock.Migrations/RockStartup/DataMigrationsStartup.cs
@@ -104,7 +104,8 @@ namespace Rock.Migrations.RockStartup
             SystemGuid.ServiceJob.DATA_MIGRATIONS_170_UPDATE_PERSON_PRIMARY_PERSON_ALIAS_GUID.AsGuid(),
             SystemGuid.ServiceJob.DATA_MIGRATIONS_170_INTERACTION_INDEX_POST_MIGRATION_JOB.AsGuid(),
             SystemGuid.ServiceJob.DATA_MIGRATIONS_168_UPDATE_INDEXES.AsGuid(),
-            SystemGuid.ServiceJob.DATA_MIGRATIONS_171_MIGRATE_LOGIN_HISTORY.AsGuid()
+            SystemGuid.ServiceJob.DATA_MIGRATIONS_171_MIGRATE_LOGIN_HISTORY.AsGuid(),
+            SystemGuid.ServiceJob.DATA_MIGRATIONS_171_UPDATE_COMMUNICATIONRECIPIENT_INDEX.AsGuid()
         };
 
 
