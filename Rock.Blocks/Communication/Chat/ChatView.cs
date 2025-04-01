443d163cc4d6c9b98a3fbdc1c0c3a7ebffe5cfba
Braden Cohen <braden@triumph.tech>
2025-04-01 09:49:48 -0700

    - Add block setting to filter shared chat channels by campus.

--- Rock.Blocks/Communication/Chat/ChatView.cs
+++ Rock.Blocks/Communication/Chat/ChatView.cs
@@ -9,18 +9,73 @@ using Rock.ViewModels.Blocks.Communication.Chat.ChatView;
 
 namespace Rock.Blocks.Communication.Chat
 {
+    /// <summary>
+    /// Block for displaying a chat interface using Rock's StreamChat integration.
+    /// </summary>
     [DisplayName( "Chat View" )]
     [Category( "Communication" )]
     [Description( "Displays a chat interface utilizing the Rock StreamChat integration." )]
     [SupportedSiteTypes( SiteType.Mobile, SiteType.Web )]
     [IconCssClass( "fa fa-comments" )]
 
+    #region Block Attributes
+
+    [BooleanField( "Filter Shared Channels by Campus",
+        Description = "Only show channels that match the individual's campus or have no campus set.",
+        DefaultBooleanValue = false,
+        Key = AttributeKey.FilterSharedChannelsByCampus,
+        ControlType = Field.Types.BooleanFieldType.BooleanControlType.Checkbox,
+        Order = 0 )]
+
+    #endregion
+
     [Rock.SystemGuid.EntityTypeGuid( "B3D6F875-1589-4543-9E76-5C41201B465B" )]
     [Rock.SystemGuid.BlockTypeGuid( "723A3F70-87DC-4BA0-A6FB-0AC15B1865B0" )]
     public class ChatView : RockBlockType
     {
+        #region Properties
+
+        /// <summary>
+        /// Gets the filter shared channels by campus setting.
+        /// </summary>
+        protected bool FilterSharedChannelsByCampus => GetAttributeValue( AttributeKey.FilterSharedChannelsByCampus ).AsBoolean();
+
+        #endregion
+
+        #region Keys
+
+        /// <summary>
+        /// Keys for block attributes.
+        /// </summary>
+        private static class AttributeKey
+        {
+            /// <summary>
+            /// The filter shared channels by campus key.
+            /// </summary>
+            public const string FilterSharedChannelsByCampus = "FilterSharedChannelsByCampus";
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public override object GetMobileConfigurationValues()
+        {
+            return new
+            {
+                FilterSharedChannelsByCampus = FilterSharedChannelsByCampus
+            };
+        }
+
+        #endregion
+
         #region Block Actions
 
+        /// <summary>
+        /// Retrieves the current individual's chat settings, including personalized or default values.
+        /// </summary>
+        /// <returns>A result containing individual-specific chat configuration flags.</returns>
         [BlockAction]
         public BlockActionResult GetChatPersonSettings()
         {
@@ -31,12 +86,7 @@ namespace Rock.Blocks.Communication.Chat
                 return ActionUnauthorized( "You must be logged in to view your chat settings." );
             }
 
-            // These settings are a little untraditional -- the properties on the Person
-            // are nullable. If they are null, Stream users are synced with the default.
-            // If they are not null, the user has overridden the default.
-            // The mobile shell only updates these properties on user interaction,
-            // so we need to send down the default values in case the user has not
-            // interacted with the chat settings yet (or is just riding the 'default' wave).
+            // Pull default values from chat configuration.
             var chatConfig = ChatHelper.GetChatConfiguration();
             var isOpenDirectMessageAllowedDefaultValue = chatConfig.IsOpenDirectMessagingAllowed;
             var isProfilePublicDefaultValue = chatConfig.AreChatProfilesVisible;
@@ -48,6 +98,11 @@ namespace Rock.Blocks.Communication.Chat
             } );
         }
 
+        /// <summary>
+        /// Updates the individual's chat preferences.
+        /// </summary>
+        /// <param name="options">New chat preferences from the client.</param>
+        /// <returns>An action result indicating success or failure.</returns>
         [BlockAction]
         public BlockActionResult UpdateChatPersonSettings( ChatPersonSettingsBag options )
         {
@@ -64,11 +119,11 @@ namespace Rock.Blocks.Communication.Chat
 
             return ActionOk();
         }
-        
+
         /// <summary>
-        /// Gets the chat data.
+        /// Asynchronously retrieves the authentication information needed for chat usage.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A result containing the chat token and user key for StreamChat authentication.</returns>
         [BlockAction]
         public async Task<BlockActionResult> GetChatData()
         {
