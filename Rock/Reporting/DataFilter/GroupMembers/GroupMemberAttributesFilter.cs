bd8f02014f46bfff37e45498e15ecb435a8f0792
Joe Zimmerman <contact@joezimjs.com>
2025-05-05 15:43:44 -0500

    - (Core) Updated GroupMember/GroupMemberAttributesFilter data view filter to be Obsidian compatible.

--- Rock/Reporting/DataFilter/GroupMembers/GroupMemberAttributesFilter.cs
+++ Rock/Reporting/DataFilter/GroupMembers/GroupMemberAttributesFilter.cs
@@ -1,4 +1,4 @@
-// <copyright>
+﻿// <copyright>
 // Copyright by the Spark Development Network
 //
 // Licensed under the Rock Community License (the "License");
@@ -25,8 +25,13 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 using Rock.Data;
+using Rock.Enums.Reporting;
+using Rock.Field;
 using Rock.Model;
+using Rock.Net;
 using Rock.Utility;
+using Rock.ViewModels.Reporting;
+using Rock.ViewModels.Utility;
 using Rock.Web.Cache;
 using Rock.Web.UI.Controls;
 
@@ -38,7 +43,7 @@ namespace Rock.Reporting.DataFilter.GroupMember
     [Description( "Filter Group Members by their Attribute Values" )]
     [Export( typeof( DataFilterComponent ) )]
     [ExportMetadata( "ComponentName", "Group Member Attributes Filter" )]
-    [Rock.SystemGuid.EntityTypeGuid( "5ABED6E0-BD45-455A-B28C-72F5648A28AD")]
+    [Rock.SystemGuid.EntityTypeGuid( "5ABED6E0-BD45-455A-B28C-72F5648A28AD" )]
     public class GroupMemberAttributesFilter : EntityFieldFilter
     {
         #region Settings
@@ -139,6 +144,164 @@ namespace Rock.Reporting.DataFilter.GroupMember
             get { return "Additional Filters"; }
         }
 
+        /// <inheritdoc/>
+        public override string ObsidianFileUrl => "~/Obsidian/Reporting/DataFilters/GroupMember/groupMemberAttributesFilter.obs";
+
+        #endregion
+
+        #region Configuration
+
+        /// <inheritdoc/>
+        public override Dictionary<string, string> GetObsidianComponentData( Type entityType, string selection, RockContext rockContext, RockRequestContext requestContext )
+        {
+            var data = new Dictionary<string, string>();
+
+            var groupTypeService = new GroupTypeService( rockContext );
+
+            var groupTypes = groupTypeService.Queryable().OrderBy( a => a.Name ).ToList();
+
+            var groupTypeGuidsById = new Dictionary<int, Guid>();
+            var fieldFilterSourcesByGroupType = new Dictionary<Guid, List<FieldFilterSourceBag>>();
+
+            // Prime the dictionary with empty lists to make it easier to add the entity fields
+            foreach ( var groupTypeItem in groupTypes )
+            {
+                groupTypeGuidsById.Add( groupTypeItem.Id, groupTypeItem.Guid );
+                fieldFilterSourcesByGroupType.AddOrReplace( groupTypeItem.Guid, new List<FieldFilterSourceBag>() );
+            }
+
+            // Add a list for when no group type is selected
+            fieldFilterSourcesByGroupType.AddOrReplace( Guid.Empty, new List<FieldFilterSourceBag>() );
+
+            var attributes = AttributeCache.AllForEntityType<Rock.Model.GroupMember>().Where( a => a.IsActive ).OrderBy( a => a.Order ).ThenBy( a => a.Name ).ToList();
+            var allEntityFields = new List<EntityField>();
+            EntityHelper.AddEntityFieldsForAttributeList( allEntityFields, attributes );
+
+            // Go through all the entity fields and add them to the dictionary based on content channel type indicated by their entity type qualifier
+            foreach ( var attribute in attributes )
+            {
+                Guid groupTypeGuid = Guid.Empty;
+
+                if ( attribute.EntityTypeQualifierColumn == "GroupTypeId" )
+                {
+                    groupTypeGuid = groupTypeGuidsById.GetValueOrDefault( attribute.EntityTypeQualifierValue.AsInteger(), Guid.Empty );
+                }
+
+                var fieldType = attribute.FieldType;
+
+                var source = new FieldFilterSourceBag
+                {
+                    Guid = attribute.Guid,
+                    Type = FieldFilterSourceType.Attribute,
+                    Attribute = new PublicAttributeBag
+                    {
+                        AttributeGuid = attribute.Guid,
+                        ConfigurationValues = fieldType.Field.GetPublicConfigurationValues( attribute.ConfigurationValues, Field.ConfigurationValueUsage.Edit, null ),
+                        Description = attribute.Description,
+                        FieldTypeGuid = fieldType.Guid,
+                        Name = attribute.Name
+                    }
+                };
+
+                // Add to list for specific group type
+                if ( groupTypeGuid != Guid.Empty )
+                {
+                    fieldFilterSourcesByGroupType.GetValueOrNull( groupTypeGuid )?.Add( source );
+                }
+                // Add to each list, because it's global (not specific to a group type)
+                else
+                {
+                    foreach ( var filterSourceList in fieldFilterSourcesByGroupType )
+                    {
+                        filterSourceList.Value.Add( source );
+                    }
+                }
+
+            }
+
+            data.AddOrReplace( "fieldFilterSources", fieldFilterSourcesByGroupType.ToCamelCaseJson( false, true ) );
+
+            if ( selection.IsNullOrWhiteSpace() )
+            {
+                return data;
+            }
+
+            var config = new FilterSettings( selection );
+
+            if ( config == null )
+            {
+                return data;
+            }
+
+            var groupType = groupTypeService.Get( config.GroupTypeId ?? 0 );
+            data.AddOrReplace( "groupType", groupType?.ToListItemBag().ToCamelCaseJson( false, true ) ?? "" );
+
+            if ( config.AttributeKey.IsNullOrWhiteSpace() )
+            {
+                return data;
+            }
+
+            var entityField = allEntityFields.ToList().FindFromFilterSelection( config.AttributeKey );
+            var filterAttribute = AttributeCache.Get( entityField?.AttributeGuid ?? Guid.Empty );
+            FieldFilterRuleBag filterRule = null;
+
+            if ( entityField == null || filterAttribute == null )
+            {
+                return data;
+            }
+
+            if ( config.AttributeFilterSettings.Count == 0 )
+            {
+                filterRule = FieldVisibilityRule.GetPublicRuleBag( filterAttribute, 0, null );
+            }
+            else if ( config.AttributeFilterSettings.Count == 1 )
+            {
+                filterRule = FieldVisibilityRule.GetPublicRuleBag( filterAttribute, 0, config.AttributeFilterSettings[0] );
+            }
+            else if ( config.AttributeFilterSettings.Count == 2 )
+            {
+                filterRule = FieldVisibilityRule.GetPublicRuleBag(
+                    filterAttribute,
+                    ( ComparisonType ) ( config.AttributeFilterSettings[0].AsInteger() ),
+                    config.AttributeFilterSettings[1] );
+            }
+
+            data.AddOrReplace( "filterRule", filterRule?.ToCamelCaseJson( false, true ) );
+
+            return data;
+        }
+
+        /// <inheritdoc/>
+        public override string GetSelectionFromObsidianComponentData( Type entityType, Dictionary<string, string> data, RockContext rockContext, RockRequestContext requestContext )
+        {
+            var config = new FilterSettings();
+
+            var groupTypeGuid = data.GetValueOrNull( "groupType" )?.FromJsonOrNull<ListItemBag>()?.Value ?? string.Empty;
+            var groupType = new GroupTypeService( rockContext ).Get( groupTypeGuid.AsGuid() );
+            config.GroupTypeId = groupType?.Id;
+
+            var filterRule = data.GetValueOrNull( "filterRule" ).FromJsonOrNull<FieldFilterRuleBag>();
+            if ( filterRule == null || filterRule.AttributeGuid == null )
+            {
+                return config.ToSelectionString();
+            }
+
+            var attribute = AttributeCache.Get( filterRule.AttributeGuid.Value );
+            var entityField = EntityHelper.GetEntityFieldForAttribute( attribute );
+            var comparisonValue = new ComparisonValue
+            {
+                ComparisonType = filterRule.ComparisonType,
+                Value = filterRule.Value
+            };
+
+            var filterValue = attribute.FieldType.Field.GetPrivateFilterValue( comparisonValue, attribute.ConfigurationValues ).FromJsonOrNull<List<string>>();
+
+            config.AttributeKey = entityField.UniqueName;
+            config.AttributeFilterSettings = filterValue;
+
+            return config.ToSelectionString();
+        }
+
         #endregion
 
         #region Public Methods
@@ -175,7 +338,7 @@ namespace Rock.Reporting.DataFilter.GroupMember
         /// <returns></returns>
         public override string FormatSelection( Type entityType, string selection )
         {
-            return GetSelectedFieldName( selection );            
+            return GetSelectedFieldName( selection );
         }
 
         /// <summary>
@@ -389,7 +552,7 @@ namespace Rock.Reporting.DataFilter.GroupMember
         public override string GetSelection( Type entityType, Control[] controls, FilterMode filterMode )
         {
             var settings = new FilterSettings();
-            
+
             if ( controls.Length > 0 )
             {
                 var containerControl = controls[0] as DynamicControlsPanel;
@@ -414,12 +577,12 @@ namespace Rock.Reporting.DataFilter.GroupMember
                             var control = panelControls.FirstOrDefault( c => c.ID.EndsWith( entityField.UniqueName ) );
                             if ( control != null )
                             {
-                                    
+
                                 entityField.FieldType.Field.GetFilterValues( control, entityField.FieldConfig, filterMode ).ForEach( v => settings.AttributeFilterSettings.Add( v ) );
                             }
                         }
                     }
-                    
+
                 }
             }
 
@@ -444,7 +607,7 @@ namespace Rock.Reporting.DataFilter.GroupMember
             if ( controls.Length > 0 )
             {
                 int? groupTypeId = settings.GroupTypeId;
-                
+
                 var containerControl = controls[0] as DynamicControlsPanel;
                 if ( containerControl.Controls.Count > 0 )
                 {
@@ -507,7 +670,7 @@ namespace Rock.Reporting.DataFilter.GroupMember
         private List<EntityField> GetGroupMemberAttributes( int? groupTypeId )
         {
             List<EntityField> entityAttributeFields = new List<EntityField>();
-            
+
             var fakeGroupMember = new Rock.Model.GroupMember();
             fakeGroupMember.Group = new Rock.Model.Group();
             if ( groupTypeId.HasValue )
