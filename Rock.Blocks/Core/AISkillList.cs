b7d694ca57245be8bc2e723eaa5b5b2bd5a9242d
Daniel Hazelbaker <daniel@triumph.tech>
2025-06-04 15:52:05 -0700

    - Added list and detail blocks for AI Skills.

---
+++ Rock.Blocks/Core/AISkillList.cs
@@ -0,0 +1,177 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Obsidian.UI;
+using Rock.Security;
+using Rock.ViewModels.Blocks;
+using Rock.ViewModels.Blocks.Core.AISkillList;
+using Rock.Web.Cache;
+using Rock.Web.Cache.Entities;
+
+namespace Rock.Blocks.Core
+{
+    /// <summary>
+    /// Displays a list of ai skills.
+    /// </summary>
+
+    [DisplayName( "AI Skill List" )]
+    [Category( "Core" )]
+    [Description( "Displays a list of ai skills." )]
+    [IconCssClass( "fa fa-list" )]
+    // [SupportedSiteTypes( Model.SiteType.Web )]
+
+    [LinkedPage( "Detail Page",
+        Description = "The page that will show the ai skill details.",
+        Key = AttributeKey.DetailPage )]
+
+    [Rock.SystemGuid.EntityTypeGuid( "8c7daf4b-db53-438a-84c8-4296ba17473b" )]
+    [Rock.SystemGuid.BlockTypeGuid( "39f5c953-0080-441f-a77c-d45676147f91" )]
+    [CustomizedGrid]
+    public class AISkillList : RockEntityListBlockType<AISkill>
+    {
+        #region Keys
+
+        private static class AttributeKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        private static class NavigationUrlKey
+        {
+            public const string DetailPage = "DetailPage";
+        }
+
+        #endregion Keys
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public override object GetObsidianBlockInitialization()
+        {
+            var box = new ListBlockBox<AISkillListOptionsBag>();
+            var builder = GetGridBuilder();
+
+            box.IsAddEnabled = GetIsAddEnabled();
+            box.IsDeleteEnabled = true;
+            box.ExpectedRowCount = AISkillCache.All( RockContext ).Count;
+            box.NavigationUrls = GetBoxNavigationUrls();
+            box.Options = GetBoxOptions();
+            box.GridDefinition = builder.BuildDefinition();
+
+            return box;
+        }
+
+        /// <summary>
+        /// Gets the box options required for the component to render the list.
+        /// </summary>
+        /// <returns>The options that provide additional details to the block.</returns>
+        private AISkillListOptionsBag GetBoxOptions()
+        {
+            var options = new AISkillListOptionsBag();
+
+            return options;
+        }
+
+        /// <summary>
+        /// Determines if the add button should be enabled in the grid.
+        /// <summary>
+        /// <returns>A boolean value that indicates if the add button should be enabled.</returns>
+        private bool GetIsAddEnabled()
+        {
+            var entity = new AISkill();
+
+            return entity.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson );
+        }
+
+        /// <summary>
+        /// Gets the box navigation URLs required for the page to operate.
+        /// </summary>
+        /// <returns>A dictionary of key names and URL values.</returns>
+        private Dictionary<string, string> GetBoxNavigationUrls()
+        {
+            return new Dictionary<string, string>
+            {
+                [NavigationUrlKey.DetailPage] = this.GetLinkedPageUrl( AttributeKey.DetailPage, "AISkillId", "((Key))" )
+            };
+        }
+
+        /// <inheritdoc/>
+        protected override IQueryable<AISkill> GetListQueryable( RockContext rockContext )
+        {
+            return base.GetListQueryable( rockContext );
+        }
+
+        /// <inheritdoc/>
+        protected override GridBuilder<AISkill> GetGridBuilder()
+        {
+            return new GridBuilder<AISkill>()
+                .WithBlock( this )
+                .AddTextField( "idKey", a => a.IdKey )
+                .AddTextField( "name", a => a.Name )
+                .AddTextField( "description", a => a.Description )
+                .AddField( "isCodeType", a => a.CodeEntityTypeId.HasValue )
+                .AddField( "isEditDisabled", a => !a.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson ) )
+                .AddField( "isSecurityDisabled", a => !a.IsAuthorized( Authorization.ADMINISTRATE, RequestContext.CurrentPerson ) );
+        }
+
+        #endregion
+
+        #region Block Actions
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <param name="key">The identifier of the entity to be deleted.</param>
+        /// <returns>An empty result that indicates if the operation succeeded.</returns>
+        [BlockAction]
+        public BlockActionResult Delete( string key )
+        {
+            var entityService = new AISkillService( RockContext );
+            var entity = entityService.Get( key, !PageCache.Layout.Site.DisablePredictableIds );
+
+            if ( entity == null )
+            {
+                return ActionBadRequest( $"{AISkill.FriendlyTypeName} not found." );
+            }
+
+            if ( !entity.IsAuthorized( Authorization.EDIT, RequestContext.CurrentPerson ) )
+            {
+                return ActionBadRequest( $"Not authorized to delete {AISkill.FriendlyTypeName}." );
+            }
+
+            if ( !entityService.CanDelete( entity, out var errorMessage ) )
+            {
+                return ActionBadRequest( errorMessage );
+            }
+
+            entityService.Delete( entity );
+            RockContext.SaveChanges();
+
+            return ActionOk();
+        }
+
+        #endregion
+    }
+}
---
+++ Rock.ViewModels/Blocks/Core/AISkillDetail/AISkillBag.cs
@@ -0,0 +1,54 @@
+// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+
+using Rock.ViewModels.Utility;
+
+namespace Rock.ViewModels.Blocks.Core.AISkillDetail
+{
+    /// <summary>
+    /// The item details for the AI Skill Detail block.
+    /// </summary>
+    public class AISkillBag : EntityBagBase
+    {
+        /// <summary>
+        /// The entity type that represents the C# class that implements the
+        /// functions for this skill. If this is not null then the skill and
+        /// related functions should not allow editing beyond enabling or
+        /// disabling them.
+        /// </summary>
+        public ListItemBag CodeEntityType { get; set; }
+
+        /// <summary>
+        /// The description of the skill, which provides additional context or
+        /// information about its intended purpose and functionality.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The friendly name of the skill that will be used to identify it in
+        /// the UI.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A concise, but descriptive, hint to the language model that provides
+        /// context about when this skill's functions should be used in response
+        /// to an individual's input.
+        /// </summary>
+        public string UsageHint { get; set; }
+    }
+}
