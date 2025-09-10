a983056a9cd569b8eb2af2e119435d951a36a9c7
jonedmiston <jon@triumph.tech>
2025-09-10 15:04:26 -0700

    - Moved connections tool from person to connection skill

--- Rock.AI.Agent/Skills/ConnectionSkill.cs
+++ Rock.AI.Agent/Skills/ConnectionSkill.cs
@@ -25,9 +25,11 @@ using Microsoft.Extensions.Logging;
 
 using Rock.AI.Agent.Classes.Common;
 using Rock.AI.Agent.Classes.Entity;
+using Rock.Enums.AI.Agent;
 using Rock.Model;
 using Rock.Net;
 using Rock.SystemGuid;
+using Rock.Utility;
 using Rock.Web.Cache;
 
 namespace Rock.AI.Agent.Skills
@@ -78,6 +80,154 @@ namespace Rock.AI.Agent.Skills
             }, TimeSpan.FromMinutes( 3 ) ) as List<ConnectionTypeResult>;
 
             return RockToolResult.Success( connectionTypes );
+        }
+
+        [Description( "Returns a list of connection requests for the user." )]
+        [AgentPurpose( "Retrieves a list of connection requests." )]
+        [AgentUsage( "Requests can be filtered by connection type, connection opportunity, requester or connector. Connectors are people who are assigned a request." )]
+        [AgentToolGuid( "DC03271E-2C54-D5AF-4F18-9CCC69F25202" )]
+        public RockToolResult ListConnectionRequests(
+            string connectionTypeIdKey = null,
+            string connectionOpportunityIdKey = null,
+            string requesterPersonIdKey = null,
+            string connectorPersonIdKey = null,
+            int pageNumber = 1 )
+        {
+            // Paging
+            var basePageSize = 100;
+            var offset = ( pageNumber - 1 ) * basePageSize;
+            var take = basePageSize + 1; // N+1 to compute hasMore
+
+            var isInternal = AgentRequestContext.AudienceType == AudienceType.Internal;
+
+            // We need to get a list of connection opportunities that the current user is authorized to see.
+            // TODO: This could be optimized by creating a connection opportunity cache. 
+            var authorizedConnectionOpportunityIds = AuthorizedConnectionOpportunityIds();
+
+            var connectionRequestsQry = new ConnectionRequestService( AgentRequestContext.RockContext ).Queryable()
+                .Where( cr => authorizedConnectionOpportunityIds.Contains( cr.ConnectionOpportunityId ) );
+
+            // Filter by requester
+            if ( requesterPersonIdKey.IsNotNullOrWhiteSpace() )
+            {
+                var requesterPersonId = IdHasher.Instance.GetId( requesterPersonIdKey );
+                connectionRequestsQry = connectionRequestsQry
+                    .Where( cr => cr.PersonAlias != null && cr.PersonAlias.PersonId == requesterPersonId );
+            }
+
+            // Filter by connector
+            if ( connectorPersonIdKey.IsNotNullOrWhiteSpace() )
+            {
+                var connectorPersonId = IdHasher.Instance.GetId( connectorPersonIdKey );
+                connectionRequestsQry = connectionRequestsQry
+                    .Where( cr => cr.ConnectorPersonAlias != null && cr.ConnectorPersonAlias.PersonId == connectorPersonId );
+            }
+
+            // Filter by connection type
+            if ( connectionTypeIdKey.IsNotNullOrWhiteSpace() )
+            {
+                var connectionTypeId = IdHasher.Instance.GetId( connectionTypeIdKey );
+                connectionRequestsQry = connectionRequestsQry
+                    .Where( cr => cr.ConnectionTypeId == connectionTypeId );
+            }
+
+            // Filter by connection opportunity
+            if ( connectionOpportunityIdKey.IsNotNullOrWhiteSpace() )
+            {
+                var connectionOpportunityId = IdHasher.Instance.GetId( connectionOpportunityIdKey );
+                connectionRequestsQry = connectionRequestsQry
+                    .Where( cr => cr.ConnectionOpportunityId == connectionOpportunityId );
+            }
+
+            var connectionRequests = connectionRequestsQry
+                .Select( cr => new ConnectionRequestResult
+                {
+                    Id = cr.Id,
+                    Requester = new PersonResult
+                    {
+                        Id = cr.PersonAlias.Person.Id,
+                        FirstName = cr.PersonAlias.Person.FirstName,
+                        LastName = cr.PersonAlias.Person.LastName,
+                        NickName = cr.PersonAlias.Person.NickName,
+                        PhotoId = cr.PersonAlias.Person.PhotoId
+                    },
+                    Comments = cr.Comments,
+                    ConnectionState = new KeyNameResult { Id = ( int ) cr.ConnectionState, Name = cr.ConnectionState.ToString() },
+                    ConnectionStatus = new KeyNameResult { Id = cr.ConnectionStatus.Id, Name = cr.ConnectionStatus.Name },
+                    ConnectionOpportunity = new ConnectionOpportunityResult
+                    {
+                        Id = cr.ConnectionOpportunity.Id,
+                        Name = cr.ConnectionOpportunity.Name,
+                        ConnectionType = new ConnectionTypeResult { Id = cr.ConnectionOpportunity.ConnectionType.Id, Name = cr.ConnectionOpportunity.ConnectionType.Name }
+                    },
+                    CreatedDateTime = cr.CreatedDateTime,
+                    ModifiedDateTime = cr.ModifiedDateTime,
+                    FollowupDate = cr.FollowupDate,
+                    Campus = cr.Campus != null ? new CampusResult { Id = cr.Campus.Id, Name = cr.Campus.Name } : null,
+                    AssignedGroup = cr.AssignedGroup != null ? new GroupResult { Id = cr.AssignedGroup.Id, Name = cr.AssignedGroup.Name } : null,
+                    Connector = cr.ConnectorPersonAlias != null ? new PersonResult
+                    {
+                        Id = cr.ConnectorPersonAlias.Person.Id,
+                        FirstName = cr.ConnectorPersonAlias.Person.FirstName,
+                        LastName = cr.ConnectorPersonAlias.Person.LastName,
+                        NickName = cr.ConnectorPersonAlias.Person.NickName,
+                        PhotoId = cr.ConnectorPersonAlias.Person.PhotoId
+                    } : null,
+                    Activities = cr.ConnectionRequestActivities.Select( a => new ConnectionRequestActivityResult
+                    {
+                        Id = a.Id,
+                        ActivityType = new KeyNameResult { Id = a.ConnectionActivityTypeId, Name = a.ConnectionActivityType.Name },
+                        Note = a.Note,
+                        CreatedDateTime = a.CreatedDateTime,
+                        Connector = a.ConnectorPersonAlias != null ? new PersonResult
+                        {
+                            Id = a.CreatedByPersonAlias.Person.Id,
+                            FirstName = a.CreatedByPersonAlias.Person.FirstName,
+                            LastName = a.CreatedByPersonAlias.Person.LastName,
+                            NickName = a.CreatedByPersonAlias.Person.NickName,
+                            PhotoId = a.CreatedByPersonAlias.Person.PhotoId
+                        } : null
+                    } ).ToList(),
+                    Attributes = cr.ConnectionRequestAttributeValues
+                        .Where( a => isInternal || a.IsPublic )
+                        .Select( a =>
+                            new AttributeResult { Id = a.AttributeId, Value = a.PersistedTextValue, Name = a.Name } ).ToList()
+
+                } )
+                .OrderBy( cr => cr.Id )
+                .Skip( offset )
+                .Take( take )
+                .ToList();
+
+            // Run security on each person (removes any data they shouldn't see)
+            foreach ( var request in connectionRequests )
+            {
+                request.SanitizeForSecurity( AgentRequestContext.RockRequestContext.CurrentPerson );
+            }
+
+            var hasMore = connectionRequests.Count > basePageSize;
+            if ( hasMore )
+            {
+                connectionRequests.RemoveAt( connectionRequests.Count - 1 ); // drop lookahead row
+            }
+
+            var meta = new Dictionary<string, object>
+                {
+                    { "personKey", requesterPersonIdKey },
+                    { "pageNumber", pageNumber },
+                    { "pageSize", basePageSize },
+                    { "returnedRows", connectionRequests.Count },
+                    { "hasMore", hasMore }
+                };
+
+            if ( !connectionRequests.Any() )
+            {
+                return RockToolResult.NoData()
+                    .WithMetadata( meta );
+            }
+
+            return RockToolResult.Success( connectionRequests )
+                .WithMetadata( meta );
         }
 
         #endregion
@@ -164,6 +314,27 @@ namespace Rock.AI.Agent.Skills
             }
 
             return connectionTypeResult;
+        }
+
+        /// <summary>
+        /// Gets a list of connection opportunity ids that the current user is authorized to view.
+        /// </summary>
+        /// <returns></returns>
+        private List<int> AuthorizedConnectionOpportunityIds()
+        {
+            var authorizedConnectionOpportunityIds = new List<int>();
+
+            var connectionOpportunities = new ConnectionOpportunityService( AgentRequestContext.RockContext ).Queryable().AsNoTracking();
+
+            foreach ( var opportunity in connectionOpportunities )
+            {
+                if ( opportunity.IsAuthorized( Rock.Security.Authorization.VIEW, AgentRequestContext.RockRequestContext.CurrentPerson ) )
+                {
+                    authorizedConnectionOpportunityIds.Add( opportunity.Id );
+                }
+            }
+
+            return authorizedConnectionOpportunityIds;
         }
 
         #endregion
--- Rock.AI.Agent/Skills/PersonSkill.cs
+++ Rock.AI.Agent/Skills/PersonSkill.cs
@@ -292,6 +292,7 @@ namespace Rock.AI.Agent.Skills
         [Description( "Does a full name sounds like search for the person." )]
         [AgentPurpose( "Searches for matching people by name. This will search by exact match as well as 'Sounds Like'." )]
         [AgentUsage( "Suffixes should be provide in the format of Sr., Jr., III, IV." )]
+        [AgentUsage( "Only use this function if a full name is provided." )]
         [AgentToolReturnDescription( "A collection of summaries about the matched people. These are not full profiles. Call `GetPersonProfile` passing the personIdKey to get a person's full profile. " )]
         [AgentToolGuid( "03093B11-A02D-F794-4A5E-9AEA2C6EF63E" )]
         public RockToolResult SearchPerson( string fullName, int maxResults = 20, string campusIdKey = null )
@@ -409,6 +410,7 @@ namespace Rock.AI.Agent.Skills
         /// <returns></returns>
         [KernelFunction( "SearchPersonPartial" )]
         [AgentPurpose( "Searches for matching people by a partial first name or last name." )]
+        [AgentUsage( "Use this for partial name searches including searches for only last names." )]
         [AgentToolExample( "t decker would search the database for people who's first name starts with t and last name starts with decker." )]
         [AgentToolReturnDescription( "A collection of summaries about the matched people. These are not full profiles. Call `GetPersonProfile` passing the personIdKey to get a person's full profile." )]
         [Description( "Does a name search based on a partial search (e.g. 't dec')." )]
@@ -723,145 +725,9 @@ namespace Rock.AI.Agent.Skills
                 .WithReferenceRoute( requestContext, "View Profile", $"/person/{profileResult.IdKey}", false );
         }
 
-        [Description( "Returns a list of connection requests for the user." )]
-        [AgentToolGuid( "DC03271E-2C54-D5AF-4F18-9CCC69F25202" )]
-        public RockToolResult ListConnectionRequestsForPerson( string personIdKey, int pageNumber = 1 )
-        {
-            if ( personIdKey.IsNullOrWhiteSpace() )
-            {
-                return RockToolResult.Error( "The personIdKey is required." );
-            }
-
-            // Paging
-            var basePageSize = 100;
-            var offset = ( pageNumber - 1 ) * basePageSize;
-            var take = basePageSize + 1; // N+1 to compute hasMore
-
-            var personId = IdHasher.Instance.GetId( personIdKey );
-            var isInternal = AgentRequestContext.AudienceType == AudienceType.Internal;
-
-            // We need to get a list of connection opportunities that the current user is authorized to see.
-            // TODO: This could be optimized by creating a connection opportunity cache. 
-            var authorizedConnectionOpportunityIds = AuthorizedConnectionOpportunityIds();
-
-            var connectionRequests = new ConnectionRequestService( AgentRequestContext.RockContext ).Queryable()
-                .Where( cr =>
-                    cr.PersonAlias.PersonId == personId
-                    && authorizedConnectionOpportunityIds.Contains( cr.ConnectionOpportunityId ) )
-                .Select( cr => new ConnectionRequestResult
-                {
-                    Id = cr.Id,
-                    Requester = new PersonResult
-                    {
-                        Id = cr.PersonAlias.Person.Id,
-                        FirstName = cr.PersonAlias.Person.FirstName,
-                        LastName = cr.PersonAlias.Person.LastName,
-                        NickName = cr.PersonAlias.Person.NickName,
-                        PhotoId = cr.PersonAlias.Person.PhotoId
-                    },
-                    Comments = cr.Comments,
-                    ConnectionState = new KeyNameResult { Id = ( int ) cr.ConnectionState, Name = cr.ConnectionState.ToString() },
-                    ConnectionStatus = new KeyNameResult { Id = cr.ConnectionStatus.Id, Name = cr.ConnectionStatus.Name },
-                    ConnectionOpportunity = new ConnectionOpportunityResult
-                    {
-                        Id = cr.ConnectionOpportunity.Id,
-                        Name = cr.ConnectionOpportunity.Name,
-                        ConnectionType = new ConnectionTypeResult { Id = cr.ConnectionOpportunity.ConnectionType.Id, Name = cr.ConnectionOpportunity.ConnectionType.Name }
-                    },
-                    CreatedDateTime = cr.CreatedDateTime,
-                    ModifiedDateTime = cr.ModifiedDateTime,
-                    FollowupDate = cr.FollowupDate,
-                    Campus = cr.Campus != null ? new CampusResult { Id = cr.Campus.Id, Name = cr.Campus.Name } : null,
-                    AssignedGroup = cr.AssignedGroup != null ? new GroupResult { Id = cr.AssignedGroup.Id, Name = cr.AssignedGroup.Name } : null,
-                    Connector = cr.ConnectorPersonAlias != null ? new PersonResult
-                    {
-                        Id = cr.ConnectorPersonAlias.Person.Id,
-                        FirstName = cr.ConnectorPersonAlias.Person.FirstName,
-                        LastName = cr.ConnectorPersonAlias.Person.LastName,
-                        NickName = cr.ConnectorPersonAlias.Person.NickName,
-                        PhotoId = cr.ConnectorPersonAlias.Person.PhotoId
-                    } : null,
-                    Activities = cr.ConnectionRequestActivities.Select( a => new ConnectionRequestActivityResult
-                    {
-                        Id = a.Id,
-                        ActivityType = new KeyNameResult { Id = a.ConnectionActivityTypeId, Name = a.ConnectionActivityType.Name },
-                        Note = a.Note,
-                        CreatedDateTime = a.CreatedDateTime,
-                        Connector = a.ConnectorPersonAlias != null ? new PersonResult
-                        {
-                            Id = a.CreatedByPersonAlias.Person.Id,
-                            FirstName = a.CreatedByPersonAlias.Person.FirstName,
-                            LastName = a.CreatedByPersonAlias.Person.LastName,
-                            NickName = a.CreatedByPersonAlias.Person.NickName,
-                            PhotoId = a.CreatedByPersonAlias.Person.PhotoId
-                        } : null
-                    } ).ToList(),
-                    Attributes = cr.ConnectionRequestAttributeValues
-                        .Where( a => isInternal || a.IsPublic )
-                        .Select( a =>
-                            new AttributeResult { Id = a.AttributeId, Value = a.PersistedTextValue, Name = a.Name } ).ToList()
-
-                } )
-                .OrderBy( cr => cr.Id )
-                .Skip( offset )
-                .Take( take )
-                .ToList();
-
-            // Run security on each person (removes any data they shouldn't see)
-            foreach( var request in connectionRequests )
-            {
-                request.SanitizeForSecurity( AgentRequestContext.RockRequestContext.CurrentPerson );
-            }          
-
-            var hasMore = connectionRequests.Count > basePageSize;
-            if ( hasMore )
-            {
-                connectionRequests.RemoveAt( connectionRequests.Count - 1 ); // drop lookahead row
-            }
-
-            var meta = new Dictionary<string, object>
-                {
-                    { "personKey", personIdKey },
-                    { "pageNumber", pageNumber },
-                    { "pageSize", basePageSize },
-                    { "returnedRows", connectionRequests.Count },
-                    { "hasMore", hasMore }
-                };
-
-            if ( !connectionRequests.Any() )
-            {
-                return RockToolResult.NoData()
-                    .WithMetadata( meta );
-            }
-
-            return RockToolResult.Success( connectionRequests )
-                .WithMetadata( meta );
-        }
-
         #endregion
 
         #region Helpers
-
-        /// <summary>
-        /// Gets a list of connection opportunity ids that the current user is authorized to view.
-        /// </summary>
-        /// <returns></returns>
-        private List<int> AuthorizedConnectionOpportunityIds()
-        {
-            var authorizedConnectionOpportunityIds = new List<int>();
-
-            var connectionOpportunities = new ConnectionOpportunityService( AgentRequestContext.RockContext ).Queryable().AsNoTracking();
-
-            foreach ( var opportunity in connectionOpportunities )
-            {
-                if ( opportunity.IsAuthorized( Rock.Security.Authorization.VIEW, AgentRequestContext.RockRequestContext.CurrentPerson ) )
-                {
-                    authorizedConnectionOpportunityIds.Add( opportunity.Id );
-                }
-            }
-
-            return authorizedConnectionOpportunityIds;
-        }
 
         /// <summary>
         /// Cleans up the media views by determining the medium and adjusting the viewing location URL.
