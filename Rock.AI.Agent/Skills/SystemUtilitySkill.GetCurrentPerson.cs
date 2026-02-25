a7b334ed1e5983f33164fc0eb5aea5c0e8df66d1
Daniel Hazelbaker <daniel@triumph.tech>
2026-02-25 11:20:15 -0700

    - Added a new GetCurrentPerson skill, this will primarily be used by MCP agents.

---
+++ Rock.AI.Agent/Skills/SystemUtilitySkill.GetCurrentPerson.cs
@@ -0,0 +1,96 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+using Rock.AI.Agent.Annotations;
+using Rock.AI.Agent.Classes.Common;
+using Rock.AI.Agent.Classes.Entity;
+using Rock.Configuration;
+using Rock.Model;
+using Rock.SystemGuid;
+using Rock.Web.Cache;
+
+namespace Rock.AI.Agent.Skills
+{
+    internal partial class SystemUtilitySkill
+    {
+        #region Tool
+
+        /// <summary>
+        /// Determines a date range from a natural language string.
+        /// </summary>
+        [Description( "Gets details about the person currently logged in and interacting with the agent." )]
+        [AgentPurpose( "Gets details about the user/person currently logged in, including contact information and family members" )]
+        [AgentToolGuid( "cb9f23f1-3d21-4451-80c3-4efbd18a7fbc" )]
+        public RockToolResult GetCurrentPerson()
+        {
+            // We have to load a new Person. Because this could be inside a
+            // streaming response, the original RockContext that was used to
+            // load CurrentPerson may have been disposed by the time this tool
+            // is called. That will make navigation properties explode.
+            using var rockContext = RockApp.Current.CreateRockContext();
+            var currentPerson = AgentRequestContext.RockRequestContext.CurrentPerson != null
+                ? new PersonService( rockContext ).Get( AgentRequestContext.RockRequestContext.CurrentPerson.Id )
+                : null;
+
+            if ( currentPerson == null )
+            {
+                return Error( "There is no person currently logged in." );
+
+            }
+
+            var result = PersonResult.Basic( currentPerson );
+            var historyResult = PersonResult.NameOnly( currentPerson );
+            var family = currentPerson.GetFamily( AgentRequestContext.RockContext );
+
+            result.BirthDay = currentPerson.BirthDay;
+            result.BirthMonth = currentPerson.BirthMonth;
+            result.BirthYear = currentPerson.BirthYear;
+            result.AnniversaryDate = currentPerson.AnniversaryDate;
+            result.Email = currentPerson.Email.IfEmpty( null );
+            result.PhoneNumbers = currentPerson.PhoneNumbers.Select( GetPhoneNumberResult ).ToList();
+            result.Spouse = PersonResult.Basic( currentPerson.GetSpouse( AgentRequestContext.RockContext ) );
+            result.ChildrenInFamily = family.Members
+                .Where( m => m.Person.AgeClassification == AgeClassification.Child )
+                .Select( m => PersonResult.Basic( m.Person ) )
+                .ToList();
+
+            return Success( result )
+                .WithHistoryContent( historyResult );
+        }
+
+        /// <summary>
+        /// Gets the phone number result from a phone number. This handles
+        /// translating the NumberTypeValueId into a KeyNameResult.
+        /// </summary>
+        /// <param name="phoneNumber">The phone number to translate.</param>
+        /// <returns>An instance of <see cref="PhoneNumberResult"/> that represents <paramref name="phoneNumber"/>.</returns>
+        private PhoneNumberResult GetPhoneNumberResult( PhoneNumber phoneNumber )
+        {
+            var result = new PhoneNumberResult
+            {
+                Id = phoneNumber.Id,
+                PhoneNumber = phoneNumber.NumberFormatted,
+                IsMessagingEnabled = phoneNumber.IsMessagingEnabled,
+                IsUnlisted = phoneNumber.IsUnlisted,
+            };
+
+            if ( phoneNumber.NumberTypeValueId.HasValue )
+            {
+                var definedValue = DefinedValueCache.Get( phoneNumber.NumberTypeValueId.Value, AgentRequestContext.RockContext );
+
+                if ( definedValue != null )
+                {
+                    result.PhoneType = new KeyNameResult
+                    {
+                        Id = definedValue.Id,
+                        Name = definedValue.Value,
+                    };
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
