2587e70fc7d9a0b4a9cfc4902123b1c35e06b3fb
Daniel Hazelbaker <daniel@triumph.tech>
2026-04-17 09:57:55 -0700

    - Added support for block note project to automatically copy output to the RockWeb folder.

--- Rock.JavaScript.BlockNoteEditor/vite.config.ts
+++ Rock.JavaScript.BlockNoteEditor/vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from "vite";
 import path from "path";
+import fs from "fs";
+import { EOL } from "os";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
     plugins: [
         cssInjectedByJsPlugin(),
+        {
+            name: 'move-dist-folder',
+            closeBundle() {
+                const src = path.resolve(__dirname, "dist", "blocknoteeditor.esm.js")
+                const dest = path.resolve(__dirname, "..", "RockWeb", "Scripts", "Rock", "blocknoteeditor.esm.js")
+
+                let data = fs.readFileSync(src);
+
+                // Normalize line endings to match the OS to prevent git on Windows
+                // thinking the file has changed.
+                if (EOL === "\r\n") {
+                    data = Buffer.from(data.toString().replace(/\n/g, EOL), "utf-8");
+                }
+
+                fs.writeFileSync(dest, data);
+            }
+        }
     ],
     build: {
         lib: {
