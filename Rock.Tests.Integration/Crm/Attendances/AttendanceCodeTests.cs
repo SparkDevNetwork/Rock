e34d620c9e4f67692287dc4a07255ddfe80e29df
Daniel Hazelbaker <daniel@blueboxmoon.com>
2026-08-23 16:33:01 -0700

    - Improved attendance security code generation performance when doing sequential code generation.

--- Rock.Tests.Integration/Crm/Attendances/AttendanceCodeTests.cs
+++ Rock.Tests.Integration/Crm/Attendances/AttendanceCodeTests.cs
@@ -38,13 +38,11 @@ namespace Rock.Tests.Integration.Crm.Attendance
         [TestMethod]
         public void AvoidTripleSix()
         {
-            int alphaNumericLength = 0;
-            int alphaLength = 0;
+            int prefixLength = 0;
             int numericLength = 4;
-            bool isRandomized = false;
             string lastCode = "0665";
 
-            string code = AttendanceCodeService.GetNextNumericCodeAsString( alphaNumericLength, alphaLength, numericLength, isRandomized, lastCode );
+            string code = AttendanceCodeService.GetSequentialNumericCodeAsString( prefixLength, numericLength, lastCode );
             Assert.AreEqual( "0667", code );
         }
 
